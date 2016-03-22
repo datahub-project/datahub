9b965ba1f2a7f4027b30db03411e9b207c6d8982
jbai <jbai@linkedin.com>
2016-03-21 17:35:54 -0700

    DSS-4076, update the metric view since data model changed

--- web/public/javascripts/views/views.js
+++ web/public/javascripts/views/views.js
@@ -23,44 +23,5 @@ App.DependView = Ember.View.extend({
 });
 
 App.DetailView = Ember.View.extend({
-    templateName: 'detail',
-    didInsertElement: function() {
-        var self = this;
-        $.fn.editable.defaults.mode = 'inline';
-        $('.xeditable').editable({
-            url: function(param) {
-                if (param && param.name)
-                {
-                    var name = param.name;
-                    var val = param.value;
-                    var metricId = param.pk;
-                    var url = '/api/v1/metrics/' + metricId + '/update';
-                    var method = 'POST';
-                    var token = $("#csrfToken").val().replace('/', '');
-                    var data = {"csrfToken": token};
-                    data[name] = val;
-                    $.ajax({
-                        url: url,
-                        method: method,
-                        headers: {
-                            'Csrf-Token': token
-                        },
-                        dataType: 'json',
-                        data: data
-                    }).done(function(data, txt, xhr){
-                        if(data && data.status && data.status == "success")
-                        {
-                            console.log('Done.')
-                        }
-                        else
-                        {
-                            console.log('Failed.')
-                        }
-                    }).fail(function(xhr, txt, err){
-                        Notify.toast("Failed to update data", "Metric Update Failure", "error")
-                    })
-                }
-            }
-        });
-    }
+    templateName: 'detail'
 });
