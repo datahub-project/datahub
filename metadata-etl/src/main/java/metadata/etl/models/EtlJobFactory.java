9af205b3a3c8582ac7cd28a0e2ca386d142b4397
jbai <jbai@linkedin.com>
2016-04-26 16:08:45 -0700

    implement the Appworx log parser

--- metadata-etl/src/main/java/metadata/etl/models/EtlJobFactory.java
+++ metadata-etl/src/main/java/metadata/etl/models/EtlJobFactory.java
@@ -23,6 +23,7 @@ import metadata.etl.elasticsearch.ElasticSearchBuildIndexETL;
 import metadata.etl.git.GitMetadataEtl;
 import metadata.etl.git.MultiproductMetadataEtl;
 import metadata.etl.lineage.AzLineageMetadataEtl;
+import metadata.etl.lineage.appworx.AppworxLineageEtl;
 import metadata.etl.ownership.DaliViewOwnerEtl;
 import metadata.etl.ownership.DatasetOwnerEtl;
 import metadata.etl.ldap.LdapEtl;
@@ -51,6 +52,8 @@ public class EtlJobFactory {
         return new TeradataMetadataEtl(refId, whExecId, properties);
       case AZKABAN_LINEAGE_METADATA_ETL:
         return new AzLineageMetadataEtl(refId, whExecId, properties);
+      case APPWORX_LINEAGE_METADATA_ETL:
+        return new AppworxLineageEtl(refId, whExecId, properties);
       case HADOOP_DATASET_OWNER_ETL:
         return new DatasetOwnerEtl(refId, whExecId, properties);
       case LDAP_USER_ETL:
