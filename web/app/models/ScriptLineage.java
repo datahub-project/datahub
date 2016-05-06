2453b2fd9c4e854d143788743af68ece7a4251fa
jbai <jbai@linkedin.com>
2016-05-06 11:29:22 -0700

    bring the script finder back

---
+++ web/app/models/ScriptLineage.java
@@ -0,0 +1,9 @@
+package models;
+
+public class ScriptLineage {
+
+    public String sourceTargetType;
+    public String operation;
+    public String storageType;
+    public String abstractedObjectName;
+}
