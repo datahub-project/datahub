55d11e7022f9cba86b48da06cc0b9d51e8964cd8
jbai <jbai@linkedin.com>
2016-05-06 11:29:22 -0700

    bring the script finder back

---
+++ web/app/models/ScriptInfo.java
@@ -0,0 +1,18 @@
+package models;
+
+public class ScriptInfo {
+
+    public String scriptUrl;
+    public String gitPath;
+    public String scriptPath;
+    public String scriptType;
+    public String scriptName;
+    public String chainName;
+    public String jobName;
+    public String committerName;
+    public String committerEmail;
+    public int applicationID;
+    public int jobID;
+    public int attemptNumber;
+
+}
