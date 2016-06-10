3deeacefff78ae7b8c6c200c2504e1371cf19f4a
aig <aignospam@gmail.com>
2016-06-10 20:57:17 +0300

    add basic configuration for oracle metadata etl

--- metadata-etl/src/main/resources/jython/OracleExtract.py
+++ metadata-etl/src/main/resources/jython/OracleExtract.py
@@ -12,40 +12,49 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #
 
+# Changed by aig on 2016-06-10
+
 from com.ziclix.python.sql import zxJDBC
 import sys, os
 from wherehows.common import Constant
 from org.slf4j import LoggerFactory
 
-class TeradataExtract:
+class OracleExtract:
     def __init__(self):
         self.logger = LoggerFactory.getLogger('jython script : ' + self.__class__.__name__)
+
+    def run(self, database_name, table_name, schema_output_file, sample_output_file, sample=True):
+        """
+        The entrance of the class, extract schema and sample data
+        Notice the database need to have a order that the databases have more info (DWH_STG) should be scaned first.
+        :param database_name:
+        :param table_name:
+        :param schema_output_file:
+        :return:
+        """
+        cur = self.conn_or.cursor()
+        cur.close()
 
 if __name__ == "__main__":
     args = sys.argv[1]
 
     # connection
-    username = args[Constant.TD_DB_USERNAME_KEY]
-    password = args[Constant.TD_DB_PASSWORD_KEY]
-    JDBC_DRIVER = args[Constant.TD_DB_DRIVER_KEY]
-    JDBC_URL = args[Constant.TD_DB_URL_KEY]
+    username = args[Constant.OR_DB_USERNAME_KEY]
+    password = args[Constant.OR_DB_PASSWORD_KEY]
+    JDBC_DRIVER = args[Constant.OR_DB_DRIVER_KEY]
+    JDBC_URL = args[Constant.OR_DB_URL_KEY]
 
-    e = TeradataExtract()
-    e.conn_td = zxJDBC.connect(JDBC_URL, username, password, JDBC_DRIVER)
+    e = OracleExtract()
+    e.conn_or = zxJDBC.connect(JDBC_URL, username, password, JDBC_DRIVER)
     do_sample = True
-    if Constant.TD_LOAD_SAMPLE in args:
-        do_sample = bool(args[Constant.TD_LOAD_SAMPLE])
+    if Constant.OR_LOAD_SAMPLE in args:
+        do_sample = bool(args[Constant.OR_LOAD_SAMPLE])
     try:
-        e.conn_td.cursor().execute(
-            "SET QUERY_BAND = 'script=%s; pid=%d; ' FOR SESSION;" % ('TeradataExtract.py', os.getpid()))
-        e.conn_td.commit()
-        e.log_file = args[Constant.TD_LOG_KEY]
-        e.databases = args[Constant.TD_TARGET_DATABASES_KEY].split(',')
-        e.default_database = args[Constant.TD_DEFAULT_DATABASE_KEY]
-        index_type = {'P': 'Primary Index', 'K': 'Primary Key', 'S': 'Secondary Index', 'Q': 'Partitioned Primary Index',
-                      'J': 'Join Index', 'U': 'Unique Index'}
+        e.log_file = args[Constant.OR_LOG_KEY]
+        e.databases = args[Constant.OR_TARGET_DATABASES_KEY].split(',')
+        e.default_database = args[Constant.OR_DEFAULT_DATABASE_KEY]
 
-        e.run(None, None, args[Constant.TD_SCHEMA_OUTPUT_KEY], args[Constant.TD_SAMPLE_OUTPUT_KEY], sample=do_sample)
+        e.run(None, None, args[Constant.OR_SCHEMA_OUTPUT_KEY], args[Constant.OR_SAMPLE_OUTPUT_KEY], sample=do_sample)
     finally:
-        e.conn_td.close()
+        e.conn_or.close()
 
