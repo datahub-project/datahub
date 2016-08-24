cfd4ecb8676c25a9032c78eeefa13c25f3ba12e4
jbai <jbai@linkedin.com>
2016-08-23 17:19:33 -0700

    implement the confidential fields of metadata dashboard

---
+++ web/app/controllers/api/v1/Dashboard.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package controllers.api.v1;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dao.DashboardDAO;
+import dao.JiraDAO;
+import dao.UserDAO;
+import models.JiraTicket;
+import org.apache.commons.lang3.StringUtils;
+import play.Logger;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Dashboard extends Controller
+{
+
+    public static Result getConfidentialDatasetsPercentage(String managerId)
+    {
+        return ok(DashboardDAO.getConfidentialPercentageByManagerId(managerId));
+    }
+
+    public static Result getPagedConfidentialDatasets(String managerId)
+    {
+        int page = 1;
+        String pageStr = request().getQueryString("page");
+        if (StringUtils.isBlank(pageStr))
+        {
+            page = 1;
+        }
+        else
+        {
+            try
+            {
+                page = Integer.parseInt(pageStr);
+            }
+            catch(NumberFormatException e)
+            {
+                Logger.error("Dashboard Controller getPagedConfidentialDatasets wrong page parameter. Error message: " + e.getMessage());
+                page = 1;
+            }
+        }
+
+        int size = 10;
+        String sizeStr = request().getQueryString("size");
+        if (StringUtils.isBlank(sizeStr))
+        {
+            size = 10;
+        }
+        else
+        {
+            try
+            {
+                size = Integer.parseInt(sizeStr);
+            }
+            catch(NumberFormatException e)
+            {
+                Logger.error("Dashboard Controller getPagedConfidentialDatasets wrong size parameter. Error message: " + e.getMessage());
+                size = 10;
+            }
+        }
+
+        return ok(DashboardDAO.getPagedConfidentialDatasetsByManagerId(managerId, page, size));
+    }
+}
