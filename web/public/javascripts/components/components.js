215e2654507e26093f7bf038d073198a7e785567
Seyi Adebajo <sadebajo@linkedin.com>
2016-10-19 17:32:05 -0700

    DSS-5178: Adds templates and components DropRegion and DraggableItem

--- web/public/javascripts/components/components.js
+++ web/public/javascripts/components/components.js
@@ -121,6 +121,41 @@ App.EmberSelectorComponent = Ember.Component.extend({
   }
 });
 
+// Component  wrapper for a droppable DOM region
+App.DropRegionComponent = Ember.Component.extend({
+  classNames: ['drop-region'],
+  classNameBindings: ['dragClass'],
+  dragClass: 'deactivated',
+
+  dragLeave(e) {
+    e.preventDefault();
+    this.set('dragClass', 'deactivated');
+  },
+
+  dragOver(e) {
+    e.preventDefault();
+    this.set('dragClass', 'activated');
+  },
+
+  drop (e) {
+    const data = e.dataTransfer.getData('text/data');
+    this.sendAction('dropped', data, this.get('param'));
+    this.set('dragClass', 'deactivated');
+  }
+});
+
+// Component wrapper for a draggable item
+App.DraggableItemComponent = Ember.Component.extend({
+  tagName: 'span',
+  classNames: ['draggable-item'],
+  attributeBindings: ['draggable'],
+  draggable: 'true',
+
+  dragStart(e) {
+    return e.dataTransfer.setData('text/data', this.get('content'));
+  }
+});
+
 App.DatasetPropertyComponent = Ember.Component.extend({
 });
 
