06650c008d91434636109df3ff9f4595736cff61
Seyi Adebajo <sadebajo@linkedin.com>
2017-01-17 23:35:09 -0800

    Sets cli sass config to remove Bootstrap's JS libs since not needed initially

--- wherehows-web/ember-cli-build.js
+++ wherehows-web/ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-bootstrap-sassy': {
+      'js': false
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
