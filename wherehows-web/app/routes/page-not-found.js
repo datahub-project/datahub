ce42d567f78b7bdffbebfd919813b3068c3c96b7
Seyi Adebajo <sadebajo@linkedin.com>
2017-02-13 15:09:53 -0800

    DSS-5869 Updates wildcard route to protected route

--- wherehows-web/app/routes/page-not-found.js
+++ wherehows-web/app/routes/page-not-found.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 });
