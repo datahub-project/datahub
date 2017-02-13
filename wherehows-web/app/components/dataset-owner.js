09004d584bf21603cef2fab211a14db6bfa3b7f6
Seyi Adebajo <sadebajo@linkedin.com>
2017-02-13 14:53:45 -0800

    DSS-5869 Adds Component: dataset-owner

---
+++ wherehows-web/app/components/dataset-owner.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    owned: function(dataset) {
+      var url = '/api/v1/datasets/' + dataset.id + '/own';
+      var method = !dataset.isOwned ? 'POST' : 'DELETE';
+      var token = $("#csrfToken").val().replace('/', '');
+      var _this = this;
+      $.ajax({
+        url: url,
+        method: method,
+        headers: {
+          'Csrf-Token': token
+        },
+        dataType: 'json',
+        data: {
+          csrfToken: token
+        }
+      }).done(function(data, txt, xhr){
+        if (data.status == 'success')
+        {
+          _this.set('dataset.isOwned', !dataset.isOwned);
+          _this.set('dataset.owners', data.owners);
+        }
+      }).fail(function(xhr, txt, err){
+        console.log('Error: Could not update dataset owner.')
+      })
+    }
+  }
+});
