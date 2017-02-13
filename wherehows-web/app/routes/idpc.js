b5e6b6e870486033ccbdd710af1850767c8424b8
Seyi Adebajo <sadebajo@linkedin.com>
2017-02-13 11:52:49 -0800

    DSS-5869 Adds IDPC routes

---
+++ wherehows-web/app/routes/idpc.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  setupController(controller) {
+    const selectedUser = {
+      'userId': 'jweiner',
+      'displayName': 'jweiner',
+      'headlessTicketsCompletion': 0,
+      'totalHeadlessTickets': 0,
+      'openedHeadlessTickets': 0,
+      'closedHeadlessTickets': 0,
+      'userCompletion': 0,
+      'userTotalTickets': 0,
+      'userOpenTickets': 0,
+      'userClosedTickets': 0,
+      'url': '/idpc/jweiner'
+    };
+    const sortOptions = ['Assignee First', 'Jira Status First', 'Directory Path First'];
+
+    controller.set('breadcrumbs', controller.get('genBreadcrumbs')('/jweiner'));
+    controller.set('selectedUser', selectedUser);
+    controller.set('sortOptions', sortOptions);
+  }
+});
