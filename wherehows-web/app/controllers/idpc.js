96bffb9bc20c66fb6ce96a34993796db7768b05d
Seyi Adebajo <sadebajo@linkedin.com>
2017-02-13 14:31:42 -0800

    DSS-5869 Controller for IDPC feature page

---
+++ wherehows-web/app/controllers/idpc.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    onSelect: function (dataset, data) {
+      highlightRow(dataset, data, false);
+      if (dataset && (dataset.id != 0)) {
+        updateTimeLine(dataset.id, false);
+      }
+    }
+  },
+
+  genBreadcrumbs(urn) {
+    var breadcrumbs = []
+    var b = urn.split('/');
+    b.shift();
+    for (var i = 0; i < b.length; i++) {
+      var updatedUrn = b[i]
+      if (i === 0) {
+        breadcrumbs.push({title: b[i], urn: updatedUrn})
+      } else {
+        breadcrumbs.push({title: b[i], urn: updatedUrn})
+      }
+    }
+    return breadcrumbs
+  }
+});
