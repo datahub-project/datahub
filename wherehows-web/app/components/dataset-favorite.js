908e0b107f5b73603e89516ad2b2cd64b462c151
Seyi Adebajo <sadebajo@linkedin.com>
2017-02-13 14:52:39 -0800

    DSS-5869 Adds Component: dataset-favorite

---
+++ wherehows-web/app/components/dataset-favorite.js
@@ -0,0 +1,27 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    favorites: function(dataset) {
+      var url = '/api/v1/datasets/' + dataset.id + '/favorite'
+      var method = !dataset.isFavorite ? 'POST' : 'DELETE'
+      var token = $("#csrfToken").val().replace('/', '')
+      var _this = this
+      $.ajax({
+        url: url,
+        method: method,
+        headers: {
+          'Csrf-Token': token
+        },
+        dataType: 'json',
+        data: {
+          csrfToken: token
+        }
+      }).done(function(data, txt, xhr){
+        _this.set('dataset.isFavorite', !dataset.isFavorite)
+      }).fail(function(xhr, txt, err){
+        console.log('Error: Could not update dataset favorite.')
+      })
+    }
+  }
+});
