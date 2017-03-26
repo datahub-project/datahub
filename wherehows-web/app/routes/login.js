1281ae83526c0248bfa1eac7d4d934a738b7212c
Seyi Adebajo <sadebajo@linkedin.com>
2017-03-26 00:56:19 -0700

    Authentication: Creates a current-user service to load details on the currently logged in user. Fixes the login route: If a user lands on the login route, and they have an authenticated session, transition to the application index. Updates the authenticator to return authentication request data for local caching. Load current user when application route is first entered and whenever session is successfully authenticated. Fixed comment typo in Play Application controller.

--- wherehows-web/app/routes/login.js
+++ wherehows-web/app/routes/login.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({
+const {
+  Route,
+  get,
+  inject: { service }
+} = Ember;
+
+export default Route.extend({
+  session: service(),
+
+  /**
+   * Check is the user is currently authenticated when attempting to access
+   *   this route, if so transition to index
+   */
+  redirect() {
+    if (get(this, 'session.isAuthenticated')) {
+      this.transitionTo('index');
+    }
+  }
 });
