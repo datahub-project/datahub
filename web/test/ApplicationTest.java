c860d4293581d111eab2678b1c4290f2153b4200
Seyi Adebajo <sadebajo@linkedin.com>
2017-03-29 10:33:53 -0700

    Fixes failing test for Play controller

--- web/test/ApplicationTest.java
+++ web/test/ApplicationTest.java
@@ -8,14 +8,11 @@ import static org.fest.assertions.api.Assertions.*;
 
 
 public class ApplicationTest {
-
     private static String TEST_USER = "test";
     private static String FAKE_CSRF_TOKEN = "token";
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render(TEST_USER, FAKE_CSRF_TOKEN, true, 1234);
+        Content html = views.html.index.render();
         assertThat(contentType(html)).isEqualTo("text/html");
     }
-
-
 }
