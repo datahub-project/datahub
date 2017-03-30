094dfebf33efb8c67bcc71ca73f336a9e4538397
Seyi Adebajo <sadebajo@linkedin.com>
2017-03-29 18:44:22 -0700

    Temporarily switches locationType to hash based routes. This is due to horizontal email tasks being sent out

--- wherehows-web/config/environment.js
+++ wherehows-web/config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'wherehows-web',
     environment: environment,
     rootURL: '/',
-    locationType: 'auto',
+    locationType: 'hash',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
