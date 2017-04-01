cae270b16ddf0f535c7113d8603ad04e3ac64b8c
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-01 14:20:05 -0700

    adds the user-lookup service

---
+++ wherehows-web/tests/unit/services/user-lookup-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:user-lookup', 'Unit | Service | user lookup', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let service = this.subject();
+  assert.ok(service);
+});
