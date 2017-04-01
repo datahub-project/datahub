e0159f87805de2b9885ce58250fa3852c1c2fa40
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-01 14:25:34 -0700

    adds typeahead feature for username resolution in dataset-author component. add support for async await with babel polyfill to cli build.

--- wherehows-web/ember-cli-build.js
+++ wherehows-web/ember-cli-build.js
@@ -6,6 +6,10 @@ const MergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
   const app = new EmberApp(defaults, {
+    babel: {
+      includePolyfill: true
+    },
+
     storeConfigInMeta: false,
 
     SRI: {
