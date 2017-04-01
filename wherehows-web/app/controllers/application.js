7284d4bd864cd6290b22d29246d6714c025cd22a
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-01 14:18:38 -0700

    adds user lookup service reference to application load and primes caches on app startup

--- wherehows-web/app/controllers/application.js
+++ wherehows-web/app/controllers/application.js
@@ -10,24 +10,36 @@ const {
 export default Controller.extend({
   /**
    * User session management service
-   * @type {service}
+   * @type {Ember.Service}
    */
   session: service(),
   /**
    * Injected global search service
-   * @type {service}
+   * @type {Ember.Service}
    */
   search: service(),
   /**
    * Service to retrieve type ahead keywords for a dataset
-   * @type {service}
+   * @type {Ember.Service}
    */
   keywords: service('search-keywords'),
 
+  /**
+   * Looks up user names and properties from the partyEntities api
+   * @type {Ember.Service}
+   */
+  ldapUsers: service('user-lookup'),
+
   init() {
     this._super(...arguments);
-    get(this, 'keywords.keywordsFor')();
 
+    // Primes the cache for keywords and userEntitiesSource
+    get(this, 'keywords.keywordsFor')();
+    get(this, 'ldapUsers.fetchUserNames')();
+
+    // Sets a reference to the query resolver function that's
+    //   accessible by the global-search component rendered in the
+    //   application template
     set(this, 'sourceKeywords', get(this, 'keywords.queryResolver'));
   },
 
