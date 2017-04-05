96e8523631ae449313fb3fc420a891bf8e0ce7fe
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-05 09:02:04 -0700

    implements typeahead using category filter apis for datasets, metrics or flows. caches previously seen results in client memory. adds category to arguments provided by query params

--- wherehows-web/app/controllers/application.js
+++ wherehows-web/app/controllers/application.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 const {
   get,
-  set,
   Controller,
   inject: { service }
 } = Ember;
@@ -18,11 +17,6 @@ export default Controller.extend({
    * @type {Ember.Service}
    */
   search: service(),
-  /**
-   * Service to retrieve type ahead keywords for a dataset
-   * @type {Ember.Service}
-   */
-  keywords: service('search-keywords'),
 
   /**
    * Looks up user names and properties from the partyEntities api
@@ -34,13 +28,7 @@ export default Controller.extend({
     this._super(...arguments);
 
     // Primes the cache for keywords and userEntitiesSource
-    get(this, 'keywords.keywordsFor')();
     get(this, 'ldapUsers.fetchUserNames')();
-
-    // Sets a reference to the query resolver function that's
-    //   accessible by the global-search component rendered in the
-    //   application template
-    set(this, 'sourceKeywords', get(this, 'keywords.queryResolver'));
   },
 
   actions: {
@@ -48,9 +36,10 @@ export default Controller.extend({
      * Invokes the search service api to transition to the
      *   search results page with the search string
      * @param {String} [keyword] the search string to search for
+     * @param {String} [category] restrict search to results found here
      */
-    didSearch(keyword) {
-      get(this, 'search').showSearchResults({ keyword });
+    didSearch({ keyword, category }) {
+      get(this, 'search').showSearchResults({ keyword, category });
     }
   }
 });
