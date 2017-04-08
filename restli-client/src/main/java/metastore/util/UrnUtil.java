6ae3f9cb71ae911a543bc75e49c602c7f9458395
Yi (Alan) Wang <alyiwang@users.noreply.github.com>
2017-04-07 17:07:11 -0700

    Add metadata store access functions, update UrnUtil (#422)

---
+++ restli-client/src/main/java/metastore/util/UrnUtil.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package metastore.util;
+
+import com.linkedin.common.FabricType;
+import com.linkedin.common.urn.CorpuserUrn;
+import com.linkedin.common.urn.DataPlatformUrn;
+import com.linkedin.common.urn.DatasetUrn;
+import com.linkedin.common.urn.MemberUrn;
+import com.linkedin.common.urn.TupleKey;
+import com.linkedin.common.urn.Urn;
+import com.linkedin.dataset.BytesType;
+import com.linkedin.dataset.NullType;
+import com.linkedin.dataset.NumberType;
+import com.linkedin.dataset.PlatformNativeType;
+import com.linkedin.dataset.SchemaFieldDataType;
+import com.linkedin.dataset.SecurityClassification;
+import com.linkedin.dataset.StringType;
+import java.net.URISyntaxException;
+import java.util.Collection;
+
+
+public class UrnUtil {
+
+  /**
+   * Transform platform String into DataPlatformUrn
+   * @param platformName String
+   * @return DataPlatformUrn
+   * @throws URISyntaxException
+   */
+  public static DataPlatformUrn toDataPlatformUrn(String platformName) throws URISyntaxException {
+    return DataPlatformUrn.deserialize("urn:li:dataPlatform:" + platformName);
+  }
+
+  /**
+   * Transform platform + dataset + origin into DatasetUrn
+   * @param platformName String
+   * @param datasetName String
+   * @param origin String
+   * @return DatasetUrn
+   * @throws URISyntaxException
+   */
+  public static DatasetUrn toDatasetUrn(String platformName, String datasetName, String origin)
+      throws URISyntaxException {
+    TupleKey keys = new TupleKey("urn:li:dataPlatform:" + platformName, datasetName, toFabricType(origin).name());
+    Urn urn = new Urn("dataset", keys);
+
+    return DatasetUrn.createFromUrn(urn);
+  }
+
+  /**
+   * Transform fabric String to FabricType
+   * @param fabric String
+   * @return FabricType
+   */
+  public static FabricType toFabricType(String fabric) {
+    switch (fabric.toUpperCase()) {
+      case "PROD":
+        return FabricType.PROD;
+      case "CORP":
+        return FabricType.CORP;
+      case "EI":
+        return FabricType.EI;
+      case "DEV":
+        return FabricType.DEV;
+      default:
+        return FabricType.$UNKNOWN;
+    }
+  }
+
+  /**
+   * Transform nativeType String to PlatformNativeType enum
+   * @param nativeType String
+   * @return PlatformNativeType
+   */
+  public static PlatformNativeType toPlatformNativeType(String nativeType) {
+    switch (nativeType.toUpperCase()) {
+      case "TABLE":
+        return PlatformNativeType.TABLE;
+      case "VIEW":
+        return PlatformNativeType.VIEW;
+      case "STREAM":
+        return PlatformNativeType.STREAM;
+      case "DIRECTORY":
+        return PlatformNativeType.DIRECTORY;
+      default:
+        return PlatformNativeType.$UNKNOWN;
+    }
+  }
+
+  /**
+   * Transform security classification string to SecurityClassification
+   * @param securityClass String
+   * @return SecurityClassification
+   */
+  public static SecurityClassification toSecurityClassification(String securityClass) {
+    switch (securityClass.toUpperCase()) {
+      case "HIGHLY_CONFIDENTIAL":
+        return SecurityClassification.HIGHLY_CONFIDENTIAL;
+      case "CONFIDENTIAL":
+        return SecurityClassification.CONFIDENTIAL;
+      case "UNCLASSIFIED":
+      case "NOT CONFIDENTIAL":
+        return SecurityClassification.UNCLASSIFIED;
+      default:
+        return SecurityClassification.$UNKNOWN;
+    }
+  }
+
+  /**
+   * Find the highest security classification from classification Strings
+   * @param classifications Collection<String>
+   * @return SecurityClassification
+   */
+  public static SecurityClassification getHighestSecurityClassification(Collection<String> classifications) {
+    if (classifications.stream().anyMatch(s -> s.equalsIgnoreCase("HIGHLY_CONFIDENTIAL"))) {
+      return SecurityClassification.HIGHLY_CONFIDENTIAL;
+    } else if (classifications.stream().anyMatch(s -> s.equalsIgnoreCase("CONFIDENTIAL"))) {
+      return SecurityClassification.CONFIDENTIAL;
+    } else if (classifications.stream()
+        .anyMatch(s -> s.equalsIgnoreCase("UNCLASSIFIED") || s.equalsIgnoreCase("NOT CONFIDENTIAL"))) {
+      return SecurityClassification.UNCLASSIFIED;
+    } else {
+      return SecurityClassification.$UNKNOWN;
+    }
+  }
+
+  /**
+   * Convert data field native type string to SchemaFieldDataType
+   * @param type String
+   * @return SchemaFieldDataType
+   */
+  public static SchemaFieldDataType toFieldDataType(String type) {
+    if (type == null) {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new NullType()));
+    } else if (type.equalsIgnoreCase("NUMBER") || type.equalsIgnoreCase("LONG") || type.equalsIgnoreCase("FLOAT")) {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new NumberType()));
+    } else if (type.equalsIgnoreCase("CHAR") || type.equalsIgnoreCase("VARCHAR2") || type.equalsIgnoreCase("NVARCHAR2")
+        || type.equalsIgnoreCase("XMLTYPE")) {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new StringType()));
+    } else if (type.equalsIgnoreCase("DATE") || type.startsWith("TIMESTAMP")) {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new NumberType()));
+    } else if (type.equalsIgnoreCase("BLOB") || type.startsWith("CLOB") || type.equalsIgnoreCase("NCLOB")
+        || type.equalsIgnoreCase("RAW")) {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new BytesType()));
+    } else {
+      return new SchemaFieldDataType().setType(SchemaFieldDataType.Type.create(new StringType()));
+    }
+  }
+
+  /**
+   * Convert long memberId to MemberUrn
+   * @param memberId long
+   * @return MemberUrn
+   * @throws URISyntaxException
+   */
+  public static MemberUrn toMemberUrn(long memberId) throws URISyntaxException {
+    return MemberUrn.deserialize("urn:li:member:" + memberId);
+  }
+
+  /**
+   * Convert String corp userId to CorpuserUrn
+   * @param userId String
+   * @return CorpuserUrn
+   * @throws URISyntaxException
+   */
+  public static CorpuserUrn toCorpuserUrn(String userId) throws URISyntaxException {
+    return CorpuserUrn.deserialize("urn:li:corpuser:" + userId);
+  }
+
+  /**
+   * Split WhereHows dataset URN into two parts: platform + dataset name
+   * Also replace '/' with '.' in dataset name for Espresso and Oracle
+   * @param urn String WhereHows dataset URN
+   * @return String[] platform + dataset name
+   */
+  public static String[] splitWhUrn(String urn) {
+    int index = urn.indexOf(":///");
+    String fabric = urn.substring(0, index);
+    String dataset = urn.substring(index + 4);
+
+    if (fabric.equalsIgnoreCase("espresso") || fabric.equalsIgnoreCase("oracle")) {
+      dataset = dataset.replace("/", ".");
+    }
+    return new String[]{fabric, dataset};
+  }
+}
--- web/app/dao/MetadataStoreDao.java
+++ web/app/dao/MetadataStoreDao.java
@@ -13,39 +13,18 @@
  */
 package dao;
 
-import com.linkedin.common.urn.DatasetUrn;
-import com.linkedin.dataset.Dataset;
-import com.linkedin.dataset.DatasetKey;
-import com.linkedin.dataset.DatasetPrivacyCompliancePoliciesGetRequestBuilder;
-import com.linkedin.dataset.DatasetPrivacyCompliancePoliciesRequestBuilders;
-import com.linkedin.dataset.DatasetsGetRequestBuilder;
-import com.linkedin.dataset.DatasetsRequestBuilders;
-import com.linkedin.dataset.PrivacyCompliancePolicy;
-import com.linkedin.dataset.PrivacyCompliancePolicyKey;
 import com.linkedin.dataset.SchemaField;
 import com.linkedin.dataset.SchemaFieldArray;
 import com.linkedin.dataset.SchemaMetadata;
-import com.linkedin.dataset.SchemaMetadataFindByDatasetRequestBuilder;
-import com.linkedin.dataset.SchemaMetadataGetRequestBuilder;
-import com.linkedin.dataset.SchemaMetadataKey;
-import com.linkedin.dataset.SchemaMetadataRequestBuilders;
-import com.linkedin.r2.transport.common.Client;
-import com.linkedin.r2.transport.common.bridge.client.TransportClientAdapter;
-import com.linkedin.r2.transport.http.client.HttpClientFactory;
-import com.linkedin.restli.client.FindRequest;
-import com.linkedin.restli.client.Request;
-import com.linkedin.restli.client.ResponseFuture;
 import com.linkedin.restli.client.RestClient;
-import com.linkedin.restli.common.CollectionResponse;
-import com.linkedin.restli.common.ComplexResourceKey;
-import com.linkedin.restli.common.EmptyRecord;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import metastore.client.RestliClient;
+import metastore.client.Schemas;
 import models.DatasetColumn;
 import play.Play;
 
-import static wherehows.restli.util.UrnUtil.*;
+import static metastore.util.UrnUtil.*;
 
 
 public class MetadataStoreDao {
@@ -53,64 +32,15 @@ public class MetadataStoreDao {
   private static final String MetadataStoreURL =
       Play.application().configuration().getString("wherehows.restli.server.url");
 
-  private static final HttpClientFactory http = new HttpClientFactory();
-  private static final Client r2Client =
-      new TransportClientAdapter(http.getClient(Collections.<String, String>emptyMap()));
+  private static final RestliClient metaStore = new RestliClient(MetadataStoreURL);
+  private static final RestClient _client = metaStore.getClient();
 
-  private static final RestClient _client = new RestClient(r2Client, MetadataStoreURL);
+  private static final Schemas _schemas = new Schemas(_client);
 
-  private static final DatasetsRequestBuilders _datasetsBuilders = new DatasetsRequestBuilders();
 
-  private static final SchemaMetadataRequestBuilders _schemaMetadataBuilder = new SchemaMetadataRequestBuilders();
-
-  private static final DatasetPrivacyCompliancePoliciesRequestBuilders _privacyComplianceBuilder =
-      new DatasetPrivacyCompliancePoliciesRequestBuilders();
-
-  public static Dataset getDataset(String datasetName, String platformName, String origin) throws Exception {
-
-    DatasetKey key = new DatasetKey().setName(datasetName)
-        .setPlatform(toDataPlatformUrn(platformName))
-        .setOrigin(toFabricType(origin));
-
-    DatasetsGetRequestBuilder builder = _datasetsBuilders.get();
-    Request<Dataset> req = builder.id(new ComplexResourceKey<>(key, new EmptyRecord())).build();
-
-    // Send the request and wait for a response
-    final ResponseFuture<Dataset> responseFuture = _client.sendRequest(req);
-    return responseFuture.getResponse().getEntity();
-  }
-
-  public static SchemaMetadata getSchemaMetadata(String schemaName, String platformName, long version)
-      throws Exception {
-
-    SchemaMetadataKey key = new SchemaMetadataKey().setSchemaName(schemaName)
-        .setPlatform(toDataPlatformUrn(platformName))
-        .setVersion(version);
-
-    SchemaMetadataGetRequestBuilder builder = _schemaMetadataBuilder.get();
-    Request<SchemaMetadata> req = builder.id(new ComplexResourceKey<>(key, new EmptyRecord())).build();
-
-    ResponseFuture<SchemaMetadata> responseFuture = _client.sendRequest(req);
-    return responseFuture.getResponse().getEntity();
-  }
-
-  public static SchemaMetadata getLatestSchemaByDataset(String platformName, String datasetName, String origin)
-      throws Exception {
-    DatasetUrn urn = toDatasetUrn(platformName, datasetName, origin);
-
-    SchemaMetadataFindByDatasetRequestBuilder builder = _schemaMetadataBuilder.findByDataset();
-    FindRequest<SchemaMetadata> req = builder.datasetParam(urn).build();
-
-    ResponseFuture<CollectionResponse<SchemaMetadata>> responseFuture = _client.sendRequest(req);
-    long version = 0;
-    SchemaMetadata latestSchema = null;
-    for (SchemaMetadata sc : responseFuture.getResponse().getEntity().getElements()) {
-      if (sc.getVersion() > version) {
-        latestSchema = sc;
-        version = sc.getVersion();
-      }
-    }
-    return latestSchema;
+  public static SchemaMetadata getLatestSchemaByWhUrn(String urn) throws Exception {
+    String[] urnParts = splitWhUrn(urn);
+    return _schemas.getLatestSchemaByDataset(urnParts[0], urnParts[1], "PROD");
   }
 
   public static List<DatasetColumn> datasetColumnsMapper(SchemaFieldArray fields) {
@@ -124,23 +54,5 @@ public class MetadataStoreDao {
       columns.add(col);
     }
     return columns;
-  }
-
-  public static SchemaMetadata getLatestSchemaByWhUrn(String urn) throws Exception {
-    String[] urnParts = splitWhUrn(urn);
-    return getLatestSchemaByDataset(urnParts[0], urnParts[1], "PROD");
-  }
-
-  public static PrivacyCompliancePolicy getPrivacyCompliancePolicy(String platformName, String datasetName,
-      String origin, long version) throws Exception {
-
-    DatasetUrn urn = toDatasetUrn(platformName, datasetName, origin);
-    PrivacyCompliancePolicyKey key = new PrivacyCompliancePolicyKey().setDataset(urn).setVersion(version);
-
-    DatasetPrivacyCompliancePoliciesGetRequestBuilder builder = _privacyComplianceBuilder.get();
-    Request<PrivacyCompliancePolicy> req = builder.id(new ComplexResourceKey<>(key, new EmptyRecord())).build();
-
-    ResponseFuture<PrivacyCompliancePolicy> responseFuture = _client.sendRequest(req);
-    return responseFuture.getResponse().getEntity();
   }
 }
