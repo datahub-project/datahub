a50cb21d256b2abc2701afb60422f26e4009965d
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-25 18:48:53 -0700

    updates the datasets root reducer to use a combinedReducer from entities containing reducers abstracted for each entitiy: metrics,flows,datasets. updates datasets.page route to use async thunk lazyRequestPagedDatasets

--- wherehows-web/app/reducers/datasets.js
+++ wherehows-web/app/reducers/datasets.js
@@ -1,17 +1,32 @@
-import { initialState, receiveEntities } from 'wherehows-web/reducers/entities';
-import { ActionTypes } from 'wherehows-web/actions/datasets';
+import { initializeState, combinedReducer, receiveNodes } from 'wherehows-web/reducers/entities';
+import { ActionTypes as DatasetActions } from 'wherehows-web/actions/datasets';
+import { ActionTypes } from 'wherehows-web/actions/browse/entity';
 
 /**
- * Reduces the store state for datasets
- * @param {Object} state = initialState the slice of the state object representing datasets
+ * Takes the `datasets` slice of the state tree and performs the specified reductions for each action
+ * @param {Object} state slice of the state tree this reducer is responsible for
  * @param {Object} action Flux Standard Action representing the action to be preformed on the state
  * @prop {String} action.type actionType
  * @return {Object}
  */
-export default (state = initialState, action = {}) => {
+const aggregateReducer = (state, action = {}) => {
   switch (action.type) {
-    case ActionTypes.RECEIVE_PAGED_DATASETS:
-      return receiveEntities('datasets')(state, action.payload);
+    // Action indicating a request for datasets by page
+    case DatasetActions.SELECT_PAGED_DATASETS:
+    case DatasetActions.REQUEST_PAGED_DATASETS:
+      return Object.assign({}, state, {
+        query: Object.assign({}, state.query, {
+          page: action.payload.page
+        }),
+        baseURL: action.payload.baseURL,
+        isFetching: true
+      });
+    // Action indicating a receipt of datasets by page
+    case DatasetActions.RECEIVE_PAGED_DATASETS:
+    case DatasetActions.RECEIVE_PAGED_URN_DATASETS:
+      return Object.assign({}, state, {
+        isFetching: false
+      });
 
     case ActionTypes.SELECT_PAGED_DATASETS:
     case ActionTypes.REQUEST_PAGED_DATASETS:
@@ -24,4 +39,33 @@ export default (state = initialState, action = {}) => {
     default:
       return state;
   }
+};
+
+/**
+ * datasets root reducer
+ * @param {Object} state slice of the top-level state tree for datasets
+ * @param {Object} action Flux Standard Action representing the action to be preformed on the state
+ * @return {Object}
+ */
+export default (
+  state = initializeState(),
+  action
+) => {
+  // Extract the following props from the root state
+  //  then create a segregated object to pass to the combinedReducer. Redux.combineReducer expects a the pre-loaded
+  //  state to only contain properties that will be assigned to the each slice it is responsible for and nothing more.
+  const { byId, byPage, byUrn } = state;
+  const slice = Object.assign(
+    {},
+    {
+      byId,
+      byPage,
+      byUrn
+    }
+  );
+
+  // Merge the combined props from slice reducer functions and the current state object to an intermediate state
+  //  this intermediate state is consumed by the aggregate reducer to generate the final state for this reducer level
+  const intermediateState = Object.assign({}, state, combinedReducer(slice, action));
+  return aggregateReducer(intermediateState, action);
 };
