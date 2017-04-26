11b402450d113a98bf879b22b77bef6fb9f35381
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-26 00:52:02 -0700

    refactors createLazyRequest to use asyncExecutor for better abstraction

--- wherehows-web/app/actions/entities/entities.js
+++ wherehows-web/app/actions/entities/entities.js
@@ -1,53 +1,46 @@
-import fetch from 'ember-network/fetch';
-
 /**
- * Takes a requestActionCreator to trigger the action indicating that an entity request
+ * Takes a requestActionCreator to trigger the action indicating that a request
  *   has occurred in the application, and a receiverActionCreator to trigger, indicating
  *   that data for the request has been received.
- * @param {String} entity the entity context when creating lazyRequest
  * @param {Function} requesterActionCreator redux action creator
  * @param {Function} receiverActionCreator redux action creator
+ * @param asyncExecutor
  * @return {function(String, Number|String): Function}
  */
 const createLazyRequest = (
-  entity,
   requesterActionCreator,
-  receiverActionCreator /**
+  receiverActionCreator,
+  asyncExecutor /**
    *
-   * @param {String} pageBaseURL url for the request
-   * @param {Number|String} page
+   * @param {Object} props = {}
    */
-) => (pageBaseURL, page) =>
+) => (props = {}) =>
   /**
    * Redux Thunk. Function takes a store dispatch, and unused getState that get passed
    *   into the returned function from createAsyncThunk
    * @arg {Function} dispatch callback passed in by redux-thunk
    */
   function(dispatch) {
-    dispatch(requesterActionCreator({ pageBaseURL, page }));
-    return createAsyncThunk(entity, receiverActionCreator)(...arguments);
+    dispatch(requesterActionCreator(Object.assign({}, props)));
+    return createAsyncThunk(receiverActionCreator, asyncExecutor)(...arguments);
   };
 
 /**
  * Async action creator, fetches a list of entities for the currentPage in the
  *   store
- * @param {String} entity to extract props from in the redux store
  * @param {Function} receiverActionCreator
+ * @param {Function} asyncExecutor
  */
 const createAsyncThunk = (
-  entity,
-  receiverActionCreator /**
+  receiverActionCreator,
+  asyncExecutor /**
    *
    * @param {Function} dispatch callback for action dispatch from redux thunk
    * @param {Function} getState callback to get the current store state
    * @return {Promise.<*>}
    */
 ) => async (dispatch, getState) => {
-  // Extract the props `pageBaseURL` & `currentPage` from the redux store
-  const { [entity]: { pageBaseURL, currentPage } } = getState();
-  const pageURL = `${pageBaseURL}${currentPage}`;
-  const response = await fetch(pageURL);
-  const { status = 'error', data } = await response.json();
+  const { status = 'error', data } = await asyncExecutor(getState);
 
   if (status === 'ok') {
     return dispatch(receiverActionCreator({ data }));
