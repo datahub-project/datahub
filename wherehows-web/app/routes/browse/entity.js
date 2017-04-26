512f601af37ea596f29d16f29d6672646f423db3
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-25 21:57:27 -0700

    adds a nodelist action to browse.entity routes to fetch nodes for the current entity and urn

--- wherehows-web/app/routes/browse/entity.js
+++ wherehows-web/app/routes/browse/entity.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import route from 'ember-redux/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+import { asyncRequestNodeList } from 'wherehows-web/actions/browse/entity';
 
-export default Ember.Route.extend({
+const { Route } = Ember;
+
+const listUrl = '/api/v1/list';
+const queryParams = ['page', 'urn'];
+
+const BrowseEntityRoute = Route.extend(AuthenticatedRouteMixin, {
+  queryParams: queryParams.reduce(
+    (queryParams, param) =>
+      Object.assign({}, queryParams, {
+        [param]: { refreshModel: true }
+      }),
+    {}
+  )
 });
+
+export default route({
+  model: (dispatch, params) => dispatch(asyncRequestNodeList(params, listUrl, { queryParams }))
+})(BrowseEntityRoute);
