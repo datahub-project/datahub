6323452ab66359bf4171a701f9d3bae3500cefe1
Seyi Adebajo <sadebajo@linkedin.com>
2017-04-27 00:57:29 -0700

    adds browse feature on home / index page. updates 'home' route to index. and aftermodel redirection in browse route. styling for featurecard

--- wherehows-web/app/routes/browse.js
+++ wherehows-web/app/routes/browse.js
@@ -3,9 +3,7 @@ import route from 'ember-redux/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import { asyncRequestBrowseData } from 'wherehows-web/actions/browse';
 
-const {
-  Route
-} = Ember;
+const { Route } = Ember;
 // TODO: DSS-6581 Create URL retrieval module
 // TODO: Route should transition to browse/entity, pay attention to the fact that
 //   this route initializes store with entity metrics on entry
@@ -17,4 +15,13 @@ const entityUrls = {
 
 export default route({
   model: (dispatch, { entity = 'datasets', page = '1' }) => dispatch(asyncRequestBrowseData(page, entity, entityUrls))
-})(Route.extend(AuthenticatedRouteMixin));
+})(
+  Route.extend(AuthenticatedRouteMixin, {
+    /**
+     * Browse route does not render any content,  but hydrates the store with initial data transition to child route
+     */
+    afterModel() {
+      this.transitionTo('browse.entity', 'datasets');
+    }
+  })
+);
