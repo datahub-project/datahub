4f36c2fc738c0357775b4d76117dd6dbd54d0577
Yi Wang <ywang5@linkedin.com>
2017-05-15 17:40:49 -0700

    Kafka ETL to fetch queuring pipeline, also add topic blacklist

--- wherehows-common/src/main/java/wherehows/common/Constant.java
+++ wherehows-common/src/main/java/wherehows/common/Constant.java
@@ -251,6 +251,7 @@ public class Constant {
   public static final String ESPRESSO_OUTPUT_KEY = "espresso.metadata";
   public static final String VOLDEMORT_OUTPUT_KEY = "voldemort.metadata";
   public static final String KAFKA_OUTPUT_KEY = "kafka.metadata";
+  public static final String KAFKA_BLACKLIST_KEY = "kafka.topic_blacklist";
 
   // metadata-store restli server
   public static final String WH_RESTLI_SERVER_URL = "wherehows.restli.server.url";
