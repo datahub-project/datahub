1c8cce9e68a60bff8b20124004983774f1aaf966
Shuya Tsukamoto <shuya.tsukamoto@gmail.com>
2017-05-26 15:05:56 +0900

    Make possible to change settings via environment variables

--- docker/archive-factory/build-web.sh
+++ docker/archive-factory/build-web.sh
@@ -15,16 +15,6 @@ mv wherehows-* wherehows
 
 rm README.md
 
-# correct a few things that won't work in docker.
-
-# leave the option to change the play crypto secret later.
-sed -i 's/changeme/$PLAY_CRYPTO_SECRET/g' wherehows/conf/application.conf
-
-# localhost usually does not work within docker, give people the power
-# to change that from outside of docker with environment variables.
-sed -i 's/localhost:8888/$HDFS_HOST:8888/g' wherehows/conf/application.conf
-sed -i 's/localhost/$MYSQL_HOST/g' wherehows/conf/application.conf
-
 tar zcvf web.tar.gz wherehows
 
 cp web.tar.gz ../../web/archives
