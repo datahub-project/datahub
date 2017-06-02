8d8ace42496043b924b457fe3fefe69cce62de4e
Mars Lan <mars.th.lan@gmail.com>
2017-06-02 15:55:27 -0700

    Add missing license headers

--- wherehows-backend/app/utils/JobsUtil.java
+++ wherehows-backend/app/utils/JobsUtil.java
@@ -1,3 +1,16 @@
+/**
+ * Copyright 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
 package utils;
 
 import com.google.common.collect.ImmutableList;
@@ -13,7 +26,6 @@ import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import wherehows.common.Constant;
-import play.Logger;
 
 
 public class JobsUtil {
@@ -71,7 +83,7 @@ public class JobsUtil {
     for (File file : new File(dir).listFiles()) {
       if (file.getAbsolutePath().endsWith(".job")) {
         Properties prop = getResolvedProperties(file.toPath());
-        if (!prop.contains(Constant.JOB_DISABLED_KEY) && prop.getProperty(Constant.JOB_CRON_EXPR_KEY) != null) {
+        if (!prop.containsKey(Constant.JOB_DISABLED_KEY) && prop.containsKey(Constant.JOB_CRON_EXPR_KEY)) {
           // job name = file name without the extension.
           jobs.put(jobNameFromFile(file), prop);
         }
@@ -88,7 +100,7 @@ public class JobsUtil {
     for (File file : new File(dir).listFiles()) {
       if (file.getAbsolutePath().endsWith(".job")) {
         Properties prop = getResolvedProperties(file.toPath());
-        if (!prop.contains(Constant.JOB_DISABLED_KEY) && prop.getProperty(Constant.JOB_TYPE, "").equals(type)) {
+        if (!prop.containsKey(Constant.JOB_DISABLED_KEY) && prop.getProperty(Constant.JOB_TYPE, "").equals(type)) {
           // job name = file name without the extension.
           jobs.put(jobNameFromFile(file), prop);
         }
