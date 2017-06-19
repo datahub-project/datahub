a63f69f743d37e2c5d322d3e661f938af563120e
Seyi Adebajo <sadebajo@linkedin.com>
2017-06-19 12:44:42 -0700

    updates the index page ui with feature icons

--- wherehows-web/app/routes/index.js
+++ wherehows-web/app/routes/index.js
@@ -23,26 +23,36 @@ export default Route.extend(AuthenticatedRouteMixin, {
       {
         title: 'Browse',
         route: 'browse',
+        alt: 'Browse Icon',
+        icon: '/assets/assets/images/icons/browse.png',
         description: "Don't know where to start? Explore by categories."
       },
       {
         title: 'Script Finder',
         route: 'scripts',
+        alt: 'Script Finder Icon',
+        icon: '/assets/assets/images/icons/script-finder.png',
         description: 'Want to search for a script, chain name or job name? Explore Script Finder.'
       },
       {
         title: 'Metadata Dashboard',
         route: 'metadata',
+        alt: 'Metadata Dashboard Icon',
+        icon: '/assets/assets/images/icons/metadata.png',
         description: 'Explore Metadata Dashboard'
       },
       {
         title: 'Schema History',
         route: 'schemahistory',
+        alt: 'Schema History Icon',
+        icon: '/assets/assets/images/icons/schema.png',
         description: 'Explore Schema History'
       },
       {
         title: 'IDPC',
         route: 'idpc',
+        alt: 'IDPC Icon',
+        icon: '/assets/assets/images/icons/idpc.png',
         description: 'Explore IDPC'
       }
     ];
