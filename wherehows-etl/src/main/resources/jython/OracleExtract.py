5e0d2c01cfb13af4e1be559a792b2497793ebdd3
Yi (Alan) Wang <ywang5@linkedin.com>
2017-07-24 11:11:03 -0700

    Fix Oracle ETL job, rewrite data sampling (#612)

--- wherehows-etl/src/main/resources/jython/OracleExtract.py
+++ wherehows-etl/src/main/resources/jython/OracleExtract.py
@@ -14,55 +14,47 @@
 
 import csv
 import datetime
+import FileUtil
 import json
 import os
 import sys
 
 from com.ziclix.python.sql import zxJDBC
-from wherehows.common.schemas import SampleDataRecord
-from wherehows.common import Constant
+from distutils.util import strtobool
 from org.slf4j import LoggerFactory
-from wherehows.common.writers import FileWriter
-
-import FileUtil
+from wherehows.common import Constant
 
 
 class OracleExtract:
   table_dict = {}
-  index_dict = {}
   table_output_list = []
   field_output_list = []
+  sample_output_list = []
+
+  ignored_owner_regex = 'ANONYMOUS|PUBLIC|SYS|SYSTEM|DBSNMP|MDSYS|CTXSYS|XDB|TSMSYS|ORACLE.*|APEX.*|TEST?*|GG_.*|\$'
 
   def __init__(self):
     self.logger = LoggerFactory.getLogger('jython script : ' + self.__class__.__name__)
 
-  def get_view_info(self, database_name, view_name):
-    '''
-    :param database_name:
-    :param view_name:
-    :return:
-    '''
-    view_cols = None
-    return view_cols
-
-  def get_table_info(self, table_name, excluded_schema_list):
-    '''
+  def get_table_info(self, excluded_owner_list, table_name):
+    """
     get table, column info from Oracle all_tables
-    :param schema_name:
-    :param table_name: not used in common case
+    here Owner, Schema, Database have same meaning: a collection of tables
+    :param excluded_owner_list: schema blacklist
+    :param table_name: get specific table name, not used in common case
     :return:
-    '''
+    """
+    owner_exclusion_filter = ''
     table_name_filter = ''
-    schema_exclusion_filter = ''
+    if excluded_owner_list and len(excluded_owner_list) > 0:
+      owner_exclusion_filter = " AND NOT REGEXP_LIKE(t.OWNER, '%s') " % '|'.join(excluded_owner_list)
+      self.logger.info("Get Oracle metadata with extra excluded schema: %s" % excluded_owner_list)
     if table_name and len(table_name) > 0:
       if table_name.find('.') > 0:
         table_name_filter = " AND OWNER='%s' AND TABLE_NAME='%s' " % table_name.split('.')
       else:
         table_name_filter = " AND TABLE_NAME='%s' " % table_name
       self.logger.info("Get Oracle metadata with extra filter: %s" % table_name_filter)
-    elif excluded_schema_list and len(excluded_schema_list) > 0:
-      schema_exclusion_filter = " AND NOT REGEXP_LIKE(t.OWNER, '%s') " % excluded_schema_list
-      self.logger.info("Get Oracle metadata with extra excluded schema: %s" % excluded_schema_list)
 
     curs_meta = self.conn_db.cursor()
 
@@ -81,18 +73,18 @@ class OracleExtract:
         on c.OWNER = m.OWNER
         and c.TABLE_NAME = m.TABLE_NAME
         and c.COLUMN_NAME = m.COLUMN_NAME
-    where NOT REGEXP_LIKE(t.OWNER,
-        'ANONYMOUS|PUBLIC|SYS|SYSTEM|DBSNMP|MDSYS|CTXSYS|XDB|TSMSYS|ORACLE.*|APEX.*|TEST?*|GG_.*|\$')
-    %s /* extra excluded schema/owner */
-    %s /* table filter */
+    where NOT REGEXP_LIKE(t.OWNER, '%s')
+      %s /* extra excluded schema/owner */
+      %s /* table filter */
     order by 1,2,4
-    """ % (schema_exclusion_filter, table_name_filter)
+    """ % (self.ignored_owner_regex, owner_exclusion_filter, table_name_filter)
 
     self.logger.debug(column_info_sql)
     curs_meta.execute(column_info_sql)
 
     rows = curs_meta.fetchall()
-    self.logger.info("Fetched %d records of Oracle metadata" % curs_meta.rowcount)
+    self.logger.info("Fetched %d records of Oracle metadata" % len(rows))
+    curs_meta.close()
 
     prev_table_key = ''
     for row in rows:
@@ -101,15 +93,14 @@ class OracleExtract:
         self.table_dict[current_table_key] = {"partitioned": row[2]}
         prev_table_key = current_table_key
 
-    curs_meta.close()
+    self.logger.info("Fetched %d tables: %s" % (len(self.table_dict), self.table_dict))
     return rows
 
-
   def get_extra_table_info(self):
-    '''
+    """
     Index, Partition, Size info
     :return: index,partition,constraint
-    '''
+    """
     index_info_sql = """
     select
       i.TABLE_OWNER, i.TABLE_NAME, i.INDEX_NAME, i.INDEX_TYPE, i.UNIQUENESS,
@@ -130,9 +121,11 @@ class OracleExtract:
             where INDEX_NAME IS NOT NULL) t
         on i.OWNER = t.OWNER
         and i.INDEX_NAME = t.INDEX_NAME
+    where NOT REGEXP_LIKE(i.TABLE_OWNER, '%s')
     group by i.TABLE_OWNER, i.TABLE_NAME, i.INDEX_NAME,
       i.INDEX_TYPE, i.UNIQUENESS, t.CONSTRAINT_NAME
-    order by 1,2,3"""
+    order by 1,2,3
+    """ % self.ignored_owner_regex
 
     partition_col_sql = """
     select
@@ -141,9 +134,10 @@ class OracleExtract:
         ORDER BY c.COLUMN_POSITION),',') PARTITION_COLUMNS,
       COUNT(1) NUM_COLUMNS
     from ALL_PART_KEY_COLUMNS c
-    where c.OBJECT_TYPE = 'TABLE'
+    where c.OBJECT_TYPE = 'TABLE' and NOT REGEXP_LIKE(c.OWNER, '%s')    
     group by c.OWNER, c.NAME
-    order by 1,2"""
+    order by 1,2
+    """ % self.ignored_owner_regex
 
     curs_meta = self.conn_db.cursor()
 
@@ -157,14 +151,11 @@ class OracleExtract:
         continue
 
       self.table_dict[table_name_key]['partition_columns'] = row[2]
-    self.logger.info("Found %d record for partition info" % curs_meta.rowcount)
+    self.logger.info("Found %d record for partition info" % len(rows))
 
     curs_meta.execute(index_info_sql)
     rows = curs_meta.fetchall()
-    table_count = 0
-    current_table_name = ''
-    full_table_name = ''
-
+    curs_meta.close()
     for row in rows:
       table_name_key = "%s.%s" % (row[0], row[1])
       if table_name_key not in self.table_dict:
@@ -183,62 +174,23 @@ class OracleExtract:
           "num_of_columns": row[7]
         }
       )
-    self.logger.info("Found %d record for index info" % curs_meta.rowcount)
+    self.logger.info("Found %d record for index info" % len(rows))
 
-
-  def format_view_metadata(self, rows, schema):
-    '''
-    add view info from rows into schema
-    note : view's original name is the same as full name
-    :param rows:
-    :return:
-    '''
-    self.logger.info("")
-
-
-  def format_table_metadata(self, rows,schema):
-    '''
-    add table info from rows into schema
-    :param rows: input. each row is a database with all it's tables
+  def format_table_metadata(self, rows):
+    """
+    add table info with columns from rows into table schema
+    :param rows: input. each row is a table column
     :param schema: {schema : _, type : _, tables : ['name' : _, ... 'original_name' : _] }
     :return:
-    '''
+    """
     schema_dict = {"fields": []}
     table_record = {}
     table_idx = 0
     field_idx = 0
-    db_dict = {}
-    
-    db_output_dict = {}
-    db_idx = len(schema) - 1
-    db_output_idx = -1
 
     for row in rows:
       table_name_key = "%s.%s" % (row[0], row[1])
       table_urn = "oracle:///%s/%s" % (row[0], row[1])
-      if row[0] not in db_dict:
-          schema.append({'database': row[0], 'type': 'Oracle', 'tables': []})
-          db_idx += 1
-          db_dict[row[0]] = db_idx
-      full_name = ''
-      if row[0]:
-        full_name = row[0]
-        if row[1]:
-          full_name += '.' + row[1]
-      elif row[1]:
-        full_name = row[1]
-      original_name = row[0] + '.' + row[1]
-      if full_name not in db_output_dict:
-        schema[db_idx]['tables'].append(
-          {'name': row[1], 'type': 'Oracle', 'columns': [],'original_name': original_name})
-        db_output_idx += 1
-        db_output_dict[full_name] = db_output_idx
-      schema[db_idx]['tables'][db_output_idx]['columns'].append(
-        {'name': row[4], 'nullable': row[6], 'dataType': row[5] ,
-         'maxByteLength': row[7] , 'precision': row[8] ,
-         'scale': row[9] ,'default': row[12]})
-      column_idx = len(schema[db_idx]['tables'][db_output_idx]['columns']) - 1
-      datetime.datetime.now(), db_output_idx + 1, len(rows), row[0]))
 
       if 'urn' not in table_record or table_urn != table_record['urn']:
         # This is a new table. Let's push the previous table record into output_list
@@ -290,50 +242,46 @@ class OracleExtract:
     self.table_output_list.append(table_record)
     self.logger.info("%d Table records generated" % table_idx)
 
-
-  def get_sample_data(self, database_name, table_name):
+  def get_sample_data(self, table_fullname, num_rows):
     """
-    find the reference dataset (if it has), select top 10 from teradata
-    :return: (reference_urn, json of sample data)
+    select top rows from table as sample data
+    :return: json of sample data
     """
-    fullname = ''
+    table_urn = "oracle:///%s" % (table_fullname.replace('.', '/'))
     columns = []
-    rows_data = []
-    fullname = database_name + '."' + table_name + '"'
+    sample_data = []
 
-    sql = 'SELECT * FROM %s WHERE ROWNUM<=10' % fullname
-    curs_td = self.conn_db.cursor()
-    rows = []
+    sql = 'SELECT * FROM %s WHERE ROWNUM <= %d' % (table_fullname, num_rows)
+    cursor = self.conn_db.cursor()
     try:
-      curs_td.execute(sql)
-      rows = curs_td.fetchall()
-      for i, value in enumerate(rows[0]):
-        columns.append(curs_td.description[i][0])
-      for r in rows:
+      cursor.execute(sql)
+      rows = cursor.fetchall()
+
+      if len(rows) == 0:
+        self.logger.error("dataset {} is empty".format(table_fullname))
+        return
+
+      # retrieve column names
+      columns = [i[0] for i in cursor.description]
+      # self.logger.info("Table {} columns: {}".format(table_fullname, columns))
+
+      # retrieve data
+      for row in rows:
         row_data = []
         # encode each field to a new value
-        for i, value in enumerate(r):
-          new_value = unicode(value, errors='ignore')
-          if isinstance(value, bytearray):
-            new_value = ''.join(format(x, '02x') for x in value)
-          elif value is None:
-            new_value = ''
-          row_data.append(new_value)
-        rows_data.append(row_data)
-    except Exception, e:
-      self.logger.error('sql : ' + sql)
-      if len(rows) == 0:
-        self.logger.error("dataset {0} is empty".format(fullname))
-      else:
-        self.logger.error("dataset {0} is not accessible.".format(fullname))
-        self.logger.error('result : ' + str(rows))
-        self.logger.error(e)
-      pass
+        for value in row:
+          if value is None:
+            row_data.append('')
+          else:
+            row_data.append(unicode(value, errors='ignore'))
+        sample_data.append(row_data)
+    except Exception as ex:
+      self.logger.error("Error fetch sample for {}: {}".format(table_fullname, ex.message))
+      return
 
-    ref_urn = 'oracle:///' + fullname.replace('.', '/').replace('"', '')
-    data_with_column = map(lambda x: dict(zip(columns, x)), rows_data)
-    return ref_urn, json.dumps({'sample': data_with_column})
-
+    cursor.close()
+    data_with_column = map(lambda x: dict(zip(columns, x)), sample_data)
+    self.sample_output_list.append({'dataset_urn': table_urn, 'sample_data': data_with_column})
 
   def num_to_int(self, num):
     try:
@@ -354,66 +302,43 @@ class OracleExtract:
       writer.writerow(data)
     csvfile.close()
 
-
-  def run(self, database_name, table_name, table_output_file, field_output_file, sample_output_file, sample=False):
+  def run(self, exclude_database_list, table_name, table_file, field_file, sample_file, sample=False):
     """
     The entrance of the class, extract schema and sample data
     Notice the database need to have a order that the databases have more info (DWH_STG) should be scaned first.
-    :param database_name:
-    :param table_name:
-    :param table_output_file:
-    :param filed_output_file:
-    :param sample_output_file:
-    :param collect_sample:
+    :param exclude_database_list: list of excluded databases/owners/schemas
+    :param table_name: specific table name to query
+    :param table_file: table output csv file path
+    :param field_file: table fields output csv file path
+    :param sample_file: sample data output csv file path
+    :param sample: do sample or not
     :return:
     """
-    schema = []
-    if database_name is None and table_name is None:  # default route: process everything
-      begin = datetime.datetime.now().strftime("%H:%M:%S")
-      # table info
-      rows = self.get_table_info(None, None)
-      self.get_extra_table_info()
-      self.format_table_metadata(rows,schema)
-      end = datetime.datetime.now().strftime("%H:%M:%S")
-      self.logger.info("Collecting table info [%s -> %s]" % (str(begin), str(end)))
+    begin = datetime.datetime.now().strftime("%H:%M:%S")
+    # collect table info
+    rows = self.get_table_info(exclude_database_list, table_name)
+    self.get_extra_table_info()
+    self.format_table_metadata(rows)
+    mid = datetime.datetime.now().strftime("%H:%M:%S")
+    self.logger.info("Collecting table info [%s -> %s]" % (str(begin), str(mid)))
 
-      csv_columns = ['name', 'columns', 'schema_type', 'properties', 'urn', 'source', 'location_prefix',
-                     'parent_name', 'storage_type', 'dataset_type', 'is_partitioned']
-      self.write_csv(table_output_file, csv_columns, self.table_output_list)
+    csv_columns = ['name', 'columns', 'schema_type', 'properties', 'urn', 'source', 'location_prefix',
+                   'parent_name', 'storage_type', 'dataset_type', 'is_partitioned']
+    self.write_csv(table_file, csv_columns, self.table_output_list)
 
-      csv_columns = ['dataset_urn', 'sort_id', 'name', 'data_type', 'nullable',
-                     'size', 'precision', 'scale', 'default_value', 'doc']
-      self.write_csv(field_output_file, csv_columns, self.field_output_list)
-    scaned_dict = {}
+    csv_columns = ['dataset_urn', 'sort_id', 'name', 'data_type', 'nullable',
+                   'size', 'precision', 'scale', 'default_value', 'doc']
+    self.write_csv(field_file, csv_columns, self.field_output_list)
+
     if sample:
-      open(sample_output_file, 'wb')
-      os.chmod(sample_output_file, 0666)
-      sample_file_writer = FileWriter(sample_output_file)
-        ##writer = csv.DictWriter(csvfile, fieldnames=csv_columns, delimiter='\x1A', lineterminator='\n',
-                             ## quoting=csv.QUOTE_NONE, quotechar='\1', escapechar='\0')
-      self.logger.info("Writing to CSV file {}".format(sample_output_file))
-
       # collect sample data
-      for onedatabase in schema:
-        database_name = onedatabase['database']
-        if 'tables' in onedatabase:
-          alltables = onedatabase['tables']
-        else:
-          alltables = onedatabase['views']
+      for table in self.table_dict.keys():
+        self.get_sample_data(table, 10)
+      end = datetime.datetime.now().strftime("%H:%M:%S")
+      self.logger.info("Collecting sample data [%s -> %s]" % (str(mid), str(end)))
 
-        for onetable in alltables:
-          table_name = onetable['original_name'].split('.')[1]
-        if table_name in scaned_dict:
-          sample_record = SampleDataRecord('oracle', '/' + database_name + '/' + table_name,
-                                           scaned_dict[table_name]['ref_urn'], scaned_dict[table_name]['data'])
-        else:
-          (ref_urn, sample_data) = self.get_sample_data(database_name, table_name)
-          sample_record = SampleDataRecord('oracle', '/' + database_name + '/' + table_name, '', sample_data)
-          scaned_dict[table_name] = {'ref_urn': ref_urn, 'data': sample_data}
-          sample_file_writer.append(sample_record)
-      sample_file_writer.close()
-          #writer.writerow(sample_record)
-    #csvfile.close()
+      csv_columns = ['dataset_urn', 'sample_data']
+      self.write_csv(sample_file, csv_columns, self.sample_output_list)
 
 
 if __name__ == "__main__":
@@ -429,10 +354,10 @@ if __name__ == "__main__":
   e.conn_db = zxJDBC.connect(JDBC_URL, username, password, JDBC_DRIVER)
   collect_sample = False
   if Constant.ORA_LOAD_SAMPLE in args:
-    collect_sample = bool(args[Constant.ORA_LOAD_SAMPLE])
-  e.databases = args[Constant.ORA_EXCLUDE_DATABASES_KEY].split(',')
+    collect_sample = strtobool(args[Constant.ORA_LOAD_SAMPLE])
+  exclude_databases = filter(bool, args[Constant.ORA_EXCLUDE_DATABASES_KEY].split(','))
 
-  temp_dir = FileUtil.etl_temp_dir(args, "ORACLE");
+  temp_dir = FileUtil.etl_temp_dir(args, "ORACLE")
   table_output_file = os.path.join(temp_dir, args[Constant.ORA_SCHEMA_OUTPUT_KEY])
   field_output_file = os.path.join(temp_dir, args[Constant.ORA_FIELD_OUTPUT_KEY])
   sample_output_file = os.path.join(temp_dir, args[Constant.ORA_SAMPLE_OUTPUT_KEY])
@@ -444,7 +369,7 @@ if __name__ == "__main__":
                                ('WhereHows (Jython)', os.getpid()))
     e.conn_db.commit()
 
-    e.run(None,
+    e.run(exclude_databases,
           None,
           table_output_file,
           field_output_file,
