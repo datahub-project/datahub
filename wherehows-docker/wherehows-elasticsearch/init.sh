bcb48f892689c7b342bcfb8cce91cbfd5142d129
Andrew Park <anpark@linkedin.com>
2017-07-26 15:10:12 -0700

    updated code review feedbacks

--- wherehows-docker/wherehows-elasticsearch/init.sh
+++ wherehows-docker/wherehows-elasticsearch/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-RUN curl -XPUT '$ELASTICSEARCH_SERVER_URL/wherehows_v1' --data @/tmp/index_mapping.json
+curl -XPUT $ELASTICSEARCH_SERVER_URL/wherehows_v1 --data @/tmp/index_mapping.json
