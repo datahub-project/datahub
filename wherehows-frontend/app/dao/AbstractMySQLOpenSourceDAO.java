32516a728326c2ef835de6d4c8fc941d960404be
Yi Wang <ywang5@linkedin.com>
2017-08-09 15:23:24 -0700

    Move dataset compliance DAO to dao module

--- wherehows-frontend/app/dao/AbstractMySQLOpenSourceDAO.java
+++ wherehows-frontend/app/dao/AbstractMySQLOpenSourceDAO.java
@@ -16,23 +16,20 @@ package dao;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
-public class AbstractMySQLOpenSourceDAO
-{
-    private static String MYSQL_DB_WHEREHOWS_OPENSOURCE = "wherehows_opensource_mysql";
 
-    private static JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSource.getDataSource(MYSQL_DB_WHEREHOWS_OPENSOURCE));
+public class AbstractMySQLOpenSourceDAO {
+  private static String MYSQL_DB_WHEREHOWS_OPENSOURCE = "wherehows_opensource_mysql";
 
-    private static NamedParameterJdbcTemplate namedJdbcTemplate =
-        new NamedParameterJdbcTemplate(DataSource.getDataSource(MYSQL_DB_WHEREHOWS_OPENSOURCE));
+  private static JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSource.getDataSource(MYSQL_DB_WHEREHOWS_OPENSOURCE));
 
-    public static JdbcTemplate getJdbcTemplate()
-  {
+  private static NamedParameterJdbcTemplate namedJdbcTemplate =
+      new NamedParameterJdbcTemplate(DataSource.getDataSource(MYSQL_DB_WHEREHOWS_OPENSOURCE));
+
+  public static JdbcTemplate getJdbcTemplate() {
     return jdbcTemplate;
   }
 
-    protected static NamedParameterJdbcTemplate getNamedParameterJdbcTemplate()
-  {
+  public static NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
     return namedJdbcTemplate;
   }
-
 }
--- wherehows-frontend/app/dao/DatasetsDAO.java
+++ wherehows-frontend/app/dao/DatasetsDAO.java
@@ -408,30 +408,6 @@ public class DatasetsDAO extends AbstractMySQLOpenSourceDAO
 					+ "classification = :classification, record_owner_type = :ownerType, retention_policy = :policy, "
 					+ "geographic_affinity = :geo, modified_time = :modified";
 
-	private static final String GET_DATASAT_COMPLIANCE_BY_DATASET_ID =
-			"SELECT dataset_id, dataset_urn, compliance_purge_type, compliance_entities, confidentiality, "
-					+ "dataset_classification, field_classification, record_owner_type, retention_policy, "
-					+ "geographic_affinity, modified_by, modified_time "
-					+ "FROM dataset_compliance WHERE dataset_id = ?";
-
-	private static final String GET_DATASET_COMPLIANCE_BY_URN =
-			"SELECT dataset_id, dataset_urn, compliance_purge_type, compliance_entities, confidentiality, "
-					+ "dataset_classification, field_classification, record_owner_type, retention_policy, "
-					+ "geographic_affinity, modified_by, modified_time "
-					+ "FROM dataset_compliance WHERE dataset_urn = ?";
-
-	private final static String INSERT_DATASET_COMPLIANCE =
-			"INSERT INTO dataset_compliance (dataset_id, dataset_urn, compliance_purge_type, compliance_entities, "
-					+ "confidentiality, dataset_classification, field_classification, record_owner_type, retention_policy, "
-					+ "geographic_affinity, modified_by, modified_time) "
-					+ "VALUES (:id, :urn, :compliance_type, :compliance_entities, :confidentiality, :dataset_classification, "
-					+ ":field_classification, :ownerType, :policy, :geo, :modified_by, :modified_time) "
-					+ "ON DUPLICATE KEY UPDATE "
-					+ "compliance_purge_type = :compliance_type, compliance_entities = :compliance_entities, "
-					+ "confidentiality = :confidentiality, dataset_classification = :dataset_classification, "
-					+ "field_classification = :field_classification, record_owner_type = :ownerType, retention_policy = :policy, "
-					+ "geographic_affinity = :geo, modified_by = :modified_by, modified_time = :modified_time";
-
 
 	public static List<String> getDatasetOwnerTypes()
 	{
@@ -2282,70 +2258,5 @@ public class DatasetsDAO extends AbstractMySQLOpenSourceDAO
 		parameters.put("geo", om.writeValueAsString(record.getGeographicAffinity()));
 		parameters.put("modified", System.currentTimeMillis() / 1000);
 		getNamedParameterJdbcTemplate().update(INSERT_DATASET_SECURITY, parameters);
-	}
-
-	public static DatasetCompliance getDatasetComplianceInfoByDatasetId(int datasetId) throws Exception {
-		Map<String, Object> result = getJdbcTemplate().queryForMap(GET_DATASAT_COMPLIANCE_BY_DATASET_ID, datasetId);
-
-		return mapToDataseCompliance(result);
-	}
-
-	public static DatasetCompliance getDatasetComplianceInfoByDatasetUrn(String datasetUrn) throws Exception {
-		Map<String, Object> result = getJdbcTemplate().queryForMap(GET_DATASET_COMPLIANCE_BY_URN, datasetUrn);
-
-		return mapToDataseCompliance(result);
-	}
-
-	private static DatasetCompliance mapToDataseCompliance(Map<String, Object> result) throws IOException {
-		DatasetCompliance record = new DatasetCompliance();
-		record.setDatasetId(((Long) result.get("dataset_id")).intValue());
-		record.setDatasetUrn((String) result.get("dataset_urn"));
-		record.setComplianceType((String) result.get("compliance_purge_type"));
-		record.setComplianceEntities(
-				jsonToObject((String) result.get("compliance_entities"), new TypeReference<List<DatasetFieldEntity>>() {
-				}));
-		record.setConfidentiality((String) result.get("confidentiality"));
-		record.setDatasetClassification(
-				jsonToObject((String) result.get("dataset_classification"), new TypeReference<Map<String, Object>>() {
-				}));
-		record.setFieldClassification(
-				jsonToObject((String) result.get("field_classification"), new TypeReference<Map<String, String>>() {
-				}));
-		record.setRecordOwnerType((String) result.get("record_owner_type"));
-		record.setRetentionPolicy(
-				jsonToObject((String) result.get("retention_policy"), new TypeReference<Map<String, Object>>() {
-				}));
-		record.setGeographicAffinity(
-				jsonToObject((String) result.get("geographic_affinity"), new TypeReference<Map<String, Object>>() {
-				}));
-		record.setModifiedBy((String) result.get("modified_by"));
-		record.setModifiedTime((long) result.get("modified_time"));
-		return record;
-	}
-
-	public static void updateDatasetComplianceInfo(int datasetId, JsonNode node, String user) throws Exception {
-
-		ObjectMapper om = new ObjectMapper();
-		DatasetCompliance record = om.convertValue(node, DatasetCompliance.class);
-		if (record.getDatasetId() != null && datasetId != record.getDatasetId()) {
-			throw new IllegalArgumentException("Dataset id doesn't match.");
-		}
-
-		String urn = record.getDatasetUrn() != null ? record.getDatasetUrn() : getDatasetUrnById(datasetId);
-
-		Map<String, Object> parameters = new HashMap<>();
-		parameters.put("id", datasetId);
-		parameters.put("urn", urn);
-		parameters.put("compliance_type", record.getComplianceType());
-		parameters.put("compliance_entities", om.writeValueAsString(record.getComplianceEntities()));
-		parameters.put("confidentiality", record.getConfidentiality());
-		parameters.put("dataset_classification", om.writeValueAsString(record.getDatasetClassification()));
-		parameters.put("field_classification", om.writeValueAsString(record.getFieldClassification()));
-		parameters.put("ownerType", record.getRecordOwnerType());
-		parameters.put("policy", om.writeValueAsString(record.getRetentionPolicy()));
-		parameters.put("geo", om.writeValueAsString(record.getGeographicAffinity()));
-		parameters.put("modified_by", user);
-		parameters.put("modified_time", System.currentTimeMillis() / 1000);
-		getNamedParameterJdbcTemplate().update(INSERT_DATASET_COMPLIANCE, parameters);
 	}
 }
