ad311515415bea5ffe5d44170767c2e58d9558da
Mars Lan <mars.th.lan@gmail.com>
2017-08-09 18:09:00 -0700

    Create a base class for all pure Jython ETL jobs.

---
+++ wherehows-etl/src/main/java/metadata/etl/JythonEtlJob.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2015 LinkedIn Corp. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+package metadata.etl;
+
+import java.io.InputStream;
+import java.util.Properties;
+import metadata.etl.EtlJob;
+import wherehows.common.Constant;
+
+
+public class JythonEtlJob extends EtlJob {
+
+  public JythonEtlJob(int dbId, long whExecId, Properties prop) {
+    super(null, dbId, whExecId, prop);
+  }
+
+  @Override
+  public void extract() throws Exception {
+    String script = prop.getProperty(Constant.JOB_JYTHON_EXTRACT_KEY);
+    if (script == null) {
+      logger.info("Skipped extract as no script is defined in {}", Constant.JOB_JYTHON_EXTRACT_KEY);
+      return;
+    }
+
+    runScript(script);
+  }
+
+  @Override
+  public void transform() throws Exception {
+    String script = prop.getProperty(Constant.JOB_JYTHON_TRANSFORM_KEY);
+    if (script == null) {
+      logger.info("Skipped transform as no script is defined in {}", Constant.JOB_JYTHON_TRANSFORM_KEY);
+    }
+
+    runScript(script);
+  }
+
+  @Override
+  public void load() throws Exception {
+    String script = prop.getProperty(Constant.JOB_JYTHON_LOAD_KEY);
+    if (script == null) {
+      logger.info("Skipped load as no script is defined in {}", Constant.JOB_JYTHON_LOAD_KEY);
+    }
+
+    runScript(script);
+  }
+
+  private void runScript(String scriptFile) throws Exception {
+    logger.info("Launching jython script {}", scriptFile);
+
+    try (InputStream inputStream = classLoader.getResourceAsStream(scriptFile)) {
+      interpreter.execfile(inputStream);
+      inputStream.close();
+    }
+  }
+}
