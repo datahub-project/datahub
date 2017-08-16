8494e4bf2b7fd84a11b21d4fc06b3a353cc9d4b9
Seyi Adebajo <sadebajo@linkedin.com>
2017-08-16 13:28:13 -0700

    adds api request for suggestions. adds interfaces.

---
+++ wherehows-web/app/typings/api/datasets/compliance.d.ts
@@ -0,0 +1,43 @@
+import { ApiStatus } from 'wherehows-web/utils/api';
+
+/**
+ * Describes the prediction property in a compliance suggestion
+ */
+interface IPrediction {
+  // Percentage confidence level for this prediction
+  confidence: number;
+  // The predicted value
+  value: string;
+}
+
+/**
+ * Describes shape of a compliance auto suggestion
+ */
+export interface IComplianceSuggestion {
+  // The name of the field
+  fieldName: string;
+  // Prediction for the identifierType
+  identifierTypePrediction: IPrediction | null;
+  // Prediction for the logicalType
+  logicalTypePrediction: IPrediction | null;
+}
+
+/**
+ * Describes the expected properties for the autoClassification on a compliance suggestions api response
+ */
+interface IAutoClassification {
+  // The urn for the dataset
+  urn: string;
+  // a JSON string representing the classifications available for this dataset
+  classificationResult: string;
+  // the last modified date for the suggestion
+  lastModified: number;
+}
+
+/**
+ * Describes the expected affirmative API response for a the compliance suggestion
+ */
+export interface IComplianceSuggestionResponse {
+  status: ApiStatus;
+  autoClassification?: IAutoClassification;
+}
