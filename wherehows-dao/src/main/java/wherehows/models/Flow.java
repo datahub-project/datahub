7e643fc79a67a9e4818854e06b6e51e67a87c33f
Shridhar Sattur <ssattur@linkedin.com>
2017-08-17 16:46:30 -0700

    Converting Flow model to hibernate

--- wherehows-dao/src/main/java/wherehows/models/Flow.java
+++ wherehows-dao/src/main/java/wherehows/models/Flow.java
@@ -13,16 +13,56 @@
  */
 package wherehows.models;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import lombok.Data;
+
+
+@Data
+@Entity
+@IdClass(value = Flow.FlowKeys.class)
+@Table(name = "flow")
 public class Flow {
 
-    public Long id;
-    public String name;
-    public String path;
-    public Integer level;
-    public int jobCount;
-    public String created;
-    public String modified;
-    public String appCode;
-    public String group;
-    public Integer appId;
+  @Id
+  @Column(name = "flow_id")
+  private Long flow_id;
+
+  @Column(name = "flow_name")
+  private String name;
+
+  @Column(name = "flow_path")
+  private String path;
+
+  @Column(name = "flow_level")
+  private Integer level;
+
+  @Transient
+  private int jobCount;
+
+  @Transient
+  private String created;
+
+  @Transient
+  private String modified;
+
+  @Transient
+  private String appCode;
+
+  @Column(name = "flow_group")
+  private String group;
+
+  @Id
+  @Column(name = "app_id")
+  private Integer appId;
+
+  static class FlowKeys {
+    private String flow_id;
+    private String appId;
+  }
 }
