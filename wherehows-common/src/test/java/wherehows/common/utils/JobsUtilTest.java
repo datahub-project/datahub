94a99d2cc6aae1fedb218b9409ecd592de8355ec
Shridhar Sattur <ssattur@linkedin.com>
2017-11-13 16:59:12 -0800

    Migrating to apache commons configuration 1.10 for parsing ETL jobs configurations. (#851)

--- wherehows-common/src/test/java/wherehows/common/utils/JobsUtilTest.java
+++ wherehows-common/src/test/java/wherehows/common/utils/JobsUtilTest.java
@@ -20,6 +20,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Map;
 import java.util.Properties;
+import org.apache.commons.configuration.ConfigurationException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import wherehows.common.Constant;
@@ -30,23 +31,25 @@ import static wherehows.common.utils.JobsUtil.*;
 public class JobsUtilTest {
 
   @Test
-  public void testEnvVarResolution() throws IOException {
+  public void testEnvVarResolution() throws IOException, ConfigurationException {
     String propertyStr = "var1=foo\n" + "var2=$JAVA_HOME\n" + "var3=${JAVA_HOME}";
 
     Path path = createPropertiesFile(propertyStr);
 
-    Properties props = getResolvedProperties(path);
+    String fileName = jobNameFromPath(path);
+    String dir = path.getParent().toString();
+
+    Properties props = getJobConfigProperties(path.toFile());
 
     Assert.assertNotEquals(props, null);
     Assert.assertEquals(props.getProperty("var1", ""), "foo");
     Assert.assertTrue(props.getProperty("var2").length() > 0);
     Assert.assertEquals(props.getProperty("var2"), props.getProperty("var3"));
-
     Files.deleteIfExists(path);
   }
 
   @Test
-  public void testGetScheduledJobs() throws IOException {
+  public void testGetScheduledJobs() throws IOException, ConfigurationException {
     String propertyStr1 = "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "#job.disabled=1\n" + "job.type=TEST1";
     String propertyStr2 = "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "job.disabled=1\n" + "job.type=TEST2";
     String propertyStr3 = "job.class=test\n" + "#job.disabled=1\n" + "job.type=TEST3";
@@ -71,7 +74,7 @@ public class JobsUtilTest {
   }
 
   @Test
-  public void testGetEnabledJobs() throws IOException {
+  public void testGetEnabledJobs() throws IOException, ConfigurationException {
     String propertyStr1 = "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "#job.disabled=1\n" + "job.type=TEST1";
     String propertyStr2 = "job.class=test\n" + "#job.disabled=1\n";
 
@@ -95,9 +98,10 @@ public class JobsUtilTest {
   }
 
   @Test
-  public void testGetEnabledJobsByType() throws IOException {
+  public void testGetEnabledJobsByType() throws IOException, ConfigurationException {
     String propertyStr1 = "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "#job.disabled=1\n" + "job.type=TEST1";
-    String propertyStr2 = "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "#job.disabled=1\n" + "job.type=TEST2";
+    String propertyStr2 =
+        "job.class=test\n" + "job.cron.expr=0 0 1 * * ? *\n" + "#job.disabled=1\n" + "job.type=TEST2,TEST3";
 
     Path path1 = createPropertiesFile(propertyStr1);
     Path path2 = createPropertiesFile(propertyStr2);
@@ -114,6 +118,27 @@ public class JobsUtilTest {
 
     Files.deleteIfExists(path1);
     Files.deleteIfExists(path2);
+  }
+
+  @Test
+  public void testMultipleLinesSameProperties() throws IOException, ConfigurationException {
+    String dir = "/tmp/";
+    String propertyStr =
+        "var1=com.mysql.jdbc.driver\n" + "var1=username\n" + "var1=password\n" + "var2=3301\n" + "var2=1000\n" + "var3=http://wherehows.com\n" + "var1=mysql\n"
+            + "var4=wherehows\n" + "var5=1000,10,3\n" + "var5=true\n";;
+    Properties props = new Properties();
+    Path path = createPropertiesFile(propertyStr);
+
+    props = getJobConfigProperties(path.toFile());
+
+    Assert.assertNotEquals(props, null);
+    Assert.assertEquals(props.getProperty("var1", ""), "com.mysql.jdbc.driver,username,password,mysql");
+    Assert.assertEquals(props.getProperty("var2"), "3301,1000");
+    Assert.assertEquals(props.getProperty("var3"), "http://wherehows.com");
+    Assert.assertEquals(props.getProperty("var4"), "wherehows");
+    Assert.assertEquals(props.getProperty("var5"), "1000,10,3,true");
+
+    Files.deleteIfExists(path);
   }
 
   private Path createPropertiesFile(String content) throws IOException {
