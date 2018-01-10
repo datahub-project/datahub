6632de40b7cd6947bb2e0251521b2beac540bbba
Seyi Adebajo <sadebajo@linkedin.com>
2018-01-10 12:06:14 -0800

    fixes skipped integration tests: mocks server requests using sinon for component level request

--- wherehows-web/tests/integration/components/purge-policy-test.js
+++ wherehows-web/tests/integration/components/purge-policy-test.js
@@ -1,19 +1,22 @@
-import { moduleForComponent, test, skip } from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-import { triggerEvent } from 'ember-native-dom-helpers';
+import { triggerEvent, waitUntil, find } from 'ember-native-dom-helpers';
 import sinon from 'sinon';
+
 import { missingPolicyText, purgePolicyProps, exemptPolicy, PurgePolicy } from 'wherehows-web/constants';
 import { DatasetPlatform } from 'wherehows-web/constants/datasets/platform';
+import platforms from 'wherehows-web/mirage/fixtures/list-platforms';
+import { ApiStatus } from 'wherehows-web/utils/api';
 
 moduleForComponent('purge-policy', 'Integration | Component | purge policy', {
   integration: true,
 
   beforeEach() {
-    this.xhr = sinon.useFakeXMLHttpRequest();
+    this.server = sinon.createFakeServer();
   },
 
   afterEach() {
-    this.xhr.restore();
+    this.server.restore();
   }
 });
 
@@ -69,7 +72,7 @@ test('it renders a user message if the purge policy is not set and is in readonl
   );
 });
 
-skip('it indicates the currently selected purge policy', function(assert) {
+test('it indicates the currently selected purge policy', async function(assert) {
   assert.expect(1);
   const selectedPolicy = PurgePolicy.ManualPurge;
   const platform = DatasetPlatform.MySql;
@@ -78,22 +81,30 @@ skip('it indicates the currently selected purge policy', function(assert) {
   this.set('platform', platform);
   this.set('purgePolicy', selectedPolicy);
 
-  this.render(hbs`{{purge-policy isEditable=isEditable purgePolicy=purgePolicy platform=platform}}`);
+  this.server.respondWith('GET', '/api/v1/list/platforms', [
+    200,
+    { 'Content-Type': 'application/json' },
+    JSON.stringify({ status: ApiStatus.OK, platforms })
+  ]);
 
+  this.render(hbs`{{purge-policy isEditable=isEditable purgePolicy=purgePolicy platform=platform}}`);
+  this.server.respond();
+
+  await waitUntil(() => find(`${policyList} [type=radio][value=${selectedPolicy}]`));
   assert.ok(
     document.querySelector(`${policyList} [type=radio][value=${selectedPolicy}]`).checked,
     `${selectedPolicy} radio is checked`
   );
 });
 
-skip('it focuses the comment element for exempt policy', function(assert) {
+test('it focuses the comment element for exempt policy', function(assert) {
   assert.expect(1);
 
   const focusEditor = () => {
     assert.equal(++focusMethodCount, 1, 'focusEditor action is invoked');
   };
   let selectedPolicy = exemptPolicy;
-  let platform = purgePolicyProps[selectedPolicy].platforms[0];
+  let platform = DatasetPlatform.MySql;
   let focusMethodCount = 0;
 
   this.setProperties({
