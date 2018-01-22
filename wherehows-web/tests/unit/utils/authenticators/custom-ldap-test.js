48e7f78be2e0cbcbc4c3ea2f18347a95eee5b57e
Seyi Adebajo <sadebajo@linkedin.com>
2018-01-22 15:53:39 -0800

    moves authenticator to ts and adds unit test

---
+++ wherehows-web/tests/unit/utils/authenticators/custom-ldap-test.js
@@ -0,0 +1,38 @@
+import Authenticator from 'wherehows-web/authenticators/custom-ldap';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+import { ApiStatus } from 'wherehows-web/utils/api';
+
+module('Unit | Utility | authenticators/custom ldap', {
+  beforeEach() {
+    this.server = sinon.createFakeServer();
+  },
+
+  afterEach() {
+    this.server.restore();
+  }
+});
+
+test('Authenticate methods work as expected', async function(assert) {
+  assert.expect(2);
+
+  const authenticator = new Authenticator();
+  const data = {
+    username: 'wherehows',
+    uuid: 'wherehows-uuid'
+  };
+
+  let response;
+
+  this.server.respondWith('POST', '/authenticate', [
+    200,
+    { 'Content-Type': 'application/json' },
+    JSON.stringify({ status: ApiStatus.OK, data })
+  ]);
+
+  response = authenticator.authenticate('username', 'password');
+  this.server.respond();
+
+  assert.ok(typeof response.then === 'function', 'returns a Promise object or thennable');
+  assert.equal((await response).username, data.username, 'authenticate correctly resolves with api response');
+});
