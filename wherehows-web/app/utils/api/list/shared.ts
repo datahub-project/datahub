432de27d9b9af408b8082949b9a5ab8f41c11e02
Seyi Adebajo <sadebajo@linkedin.com>
2018-01-24 15:41:46 -0800

    move to v2 for complianceDataTypes and platforms api. refactor api root string literal to function that takes a version argument

--- wherehows-web/app/utils/api/list/shared.ts
+++ wherehows-web/app/utils/api/list/shared.ts
@@ -1,9 +1,11 @@
-import { ApiRoot } from 'wherehows-web/utils/api/shared';
+import { ApiVersion } from 'wherehows-web/utils/api';
+import { getApiRoot } from 'wherehows-web/utils/api/shared';
 
 /**
- * Defines the url to the list endpoint
- * @type {string}
+ * Composes the url to the list endpoint, will pass-through the api version if provided
+ * @param {ApiVersion} [version] the version of the api to fetch against
+ * @return {string}
  */
-const listUrlRoot = `${ApiRoot}/list`;
+const getListUrlRoot = (version?: ApiVersion) => `${getApiRoot(version)}/list`;
 
-export { listUrlRoot };
+export { getListUrlRoot };
