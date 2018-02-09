c90ff310f78d6f3bb70530ea9d9145da559e950a
Seyi Adebajo <sadebajo@linkedin.com>
2018-02-09 10:49:35 -0800

    refactors acceptance login into helper function. adds acceptance tests for tabbed dataset routes

---
+++ wherehows-web/tests/helpers/login/test-login.js
@@ -0,0 +1,16 @@
+import { visit, click, fillIn } from 'ember-native-dom-helpers';
+import { authenticationUrl, testUser, testPassword } from 'wherehows-web/tests/helpers/login/constants';
+import {
+  loginUserInput,
+  loginPasswordInput,
+  loginSubmitButton
+} from 'wherehows-web/tests/helpers/login/page-element-constants';
+
+const appLogin = async () => {
+  await visit(authenticationUrl);
+  await fillIn(loginUserInput, testUser);
+  await fillIn(loginPasswordInput, testPassword);
+  await click(loginSubmitButton);
+};
+
+export default appLogin;
