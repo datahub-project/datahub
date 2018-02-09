1bed3a03ec8a5c0709fa4dbf4a7128dd95c14937
Seyi Adebajo <sadebajo@linkedin.com>
2018-02-09 10:49:35 -0800

    refactors acceptance login into helper function. adds acceptance tests for tabbed dataset routes

--- wherehows-web/tests/acceptance/datasets/dataset/ownership-test.js
+++ wherehows-web/tests/acceptance/datasets/dataset/ownership-test.js
@@ -4,20 +4,20 @@ import defaultScenario from 'wherehows-web/mirage/scenarios/default';
 import { visit, find, currentURL, waitUntil } from 'ember-native-dom-helpers';
 import appLogin from 'wherehows-web/tests/helpers/login/test-login';
 
-moduleForAcceptance('Acceptance | datasets/dataset/properties', {
+moduleForAcceptance('Acceptance | datasets/dataset/ownership', {
   async beforeEach() {
     appLogin();
   }
 });
 
-test('visiting /datasets/dataset/properties', async function(assert) {
+test('visiting /datasets/dataset/ownership', async function(assert) {
   assert.expect(2);
   defaultScenario(server);
-  const url = '/datasets/12345/properties';
+  const url = '/datasets/12345/ownership';
 
   await visit(url);
-  assert.equal(currentURL(), url, 'properties route is visitable');
+  assert.equal(currentURL(), url, 'ownership route is visitable');
 
   await waitUntil(() => find('.ivy-tabs-tab'));
-  assert.equal(find('.ivy-tabs-tab.active').textContent.trim(), 'Properties', 'properties tab is selected');
+  assert.equal(find('.ivy-tabs-tab.active').textContent.trim(), 'Ownership', 'ownership tab is selected');
 });
--- wherehows-web/tests/acceptance/datasets/dataset/sample-test.js
+++ wherehows-web/tests/acceptance/datasets/dataset/sample-test.js
@@ -1,23 +1,23 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'wherehows-web/tests/helpers/module-for-acceptance';
-import defaultScenario from 'wherehows-web/mirage/scenarios/default';
 import { visit, find, currentURL, waitUntil } from 'ember-native-dom-helpers';
+import defaultScenario from 'wherehows-web/mirage/scenarios/default';
 import appLogin from 'wherehows-web/tests/helpers/login/test-login';
 
-moduleForAcceptance('Acceptance | datasets/dataset/properties', {
+moduleForAcceptance('Acceptance | datasets/dataset/sample', {
   async beforeEach() {
     appLogin();
   }
 });
 
-test('visiting /datasets/dataset/properties', async function(assert) {
+test('visiting /datasets/dataset/sample', async function(assert) {
   assert.expect(2);
   defaultScenario(server);
-  const url = '/datasets/12345/properties';
+  const url = '/datasets/12345/sample';
 
   await visit(url);
-  assert.equal(currentURL(), url, 'properties route is visitable');
+  assert.equal(currentURL(), url, 'sample route is visitable');
 
   await waitUntil(() => find('.ivy-tabs-tab'));
-  assert.equal(find('.ivy-tabs-tab.active').textContent.trim(), 'Properties', 'properties tab is selected');
+  assert.equal(find('.ivy-tabs-tab.active').textContent.trim(), 'Sample Data', 'sample tab is selected');
 });
