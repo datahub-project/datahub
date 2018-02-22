543a72d3647bb2f82c8c20ecce628b5c18400780
Seyi Adebajo <sadebajo@linkedin.com>
2018-02-22 14:26:19 -0800

    create api-error error subclass. checks status for error handling when reading compliance info

--- wherehows-web/app/constants/errors/errors.ts
+++ wherehows-web/app/constants/errors/errors.ts
@@ -1,14 +1,14 @@
-import { ApiStatusNumber } from 'wherehows-web/utils/api/shared';
+import { ApiResponseStatus } from 'wherehows-web/utils/api/shared';
 
 /**
  * Returns a default msg for a given status
- * @param {ApiStatusNumber} status
+ * @param {ApiResponseStatus} status
  * @returns {string}
  */
-const apiErrorStatusMessage = (status: ApiStatusNumber): string =>
+const apiErrorStatusMessage = (status: ApiResponseStatus): string =>
   (<{ [prop: number]: string }>{
-    [ApiStatusNumber.NotFound]: 'Could not find the requested resource',
-    [ApiStatusNumber.InternalServerError]: 'An error occurred with the server'
+    [ApiResponseStatus.NotFound]: 'Could not find the requested resource',
+    [ApiResponseStatus.InternalServerError]: 'An error occurred with the server'
   })[status];
 
 export { apiErrorStatusMessage };
