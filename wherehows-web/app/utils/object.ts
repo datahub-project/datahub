447452b1c77822b651998363455f6cc86414dd3c
Seyi Adebajo <sadebajo@linkedin.com>
2018-02-26 01:30:43 -0800

    abstracts the fleece function

--- wherehows-web/app/utils/object.ts
+++ wherehows-web/app/utils/object.ts
@@ -12,4 +12,28 @@ const isObject = (candidate: any): candidate is object =>
  */
 const hasEnumerableKeys = (object: any): boolean => isObject(object) && !!Object.keys(object).length;
 
-export { isObject, hasEnumerableKeys };
+/**
+ * Function interface for a identity or partial return function
+ * @interface IPartialOrIdentityTypeFn
+ * @template T
+ */
+interface IPartialOrIdentityTypeFn<T> {
+  (o: T): Partial<T> | T;
+}
+/**
+ * Non mutative object attribute deletion. Removes the specified keys from a copy of the object and returns the copy.
+ * @template T the object type to drop keys from
+ * @template K the keys to be dropped from the object
+ * @param {Array<K>} [droppedKeys=[]] the list of attributes on T to be dropped
+ * @returns {IPartialOrIdentityTypeFn<T>}
+ */
+const fleece = <T, K extends keyof T>(droppedKeys: Array<K> = []): IPartialOrIdentityTypeFn<T> => o => {
+  const partialResult = Object.assign({}, o);
+
+  return droppedKeys.reduce((partial, key) => {
+    delete partial[key];
+    return partial;
+  }, partialResult);
+};
+
+export { isObject, hasEnumerableKeys, fleece };
