5e2e555c0ab795aea31e12942bc07277b443dc36
Seyi Adebajo <sadebajo@linkedin.com>
2018-03-07 11:15:11 -0800

    adds infinte scrolling for browse datasets lists. fixes issue with testing a liUrn: missing curly braces in regexp. moves additional url/uri utils sources to ts.

--- wherehows-web/app/components/browser/browser-viewport.ts
+++ wherehows-web/app/components/browser/browser-viewport.ts
@@ -1,2 +1,17 @@
 import Component from '@ember/component';
-export default class extends Component {}
+import { action } from 'ember-decorators/object';
+
+export default class extends Component {
+  /**
+   * Closure action to request more entities
+   */
+  getNextEntities: () => void;
+
+  /**
+   * Handles the request to fetch more entities
+   */
+  @action
+  loadMoreEntities() {
+    this.getNextEntities();
+  }
+}
