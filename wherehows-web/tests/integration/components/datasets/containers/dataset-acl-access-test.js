1f1f4fd29995131571acf2f05dd260731d0a92af
Seyi Adebajo <sadebajo@linkedin.com>
2018-03-28 12:40:12 -0700

    adds integration test for container component

--- wherehows-web/tests/integration/components/datasets/containers/dataset-acl-access-test.js
+++ wherehows-web/tests/integration/components/datasets/containers/dataset-acl-access-test.js
@@ -1,15 +1,38 @@
-import { moduleForComponent, test, skip } from 'ember-qunit';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import notificationsStub from 'wherehows-web/tests/stubs/services/notifications';
+import userStub from 'wherehows-web/tests/stubs/services/current-user';
+import sinon from 'sinon';
 
 moduleForComponent(
   'datasets/containers/dataset-acl-access',
   'Integration | Component | datasets/containers/dataset acl access',
   {
-    integration: true
+    integration: true,
+
+    beforeEach() {
+      this.register('service:current-user', userStub);
+      this.register('service:notifications', notificationsStub);
+
+      this.inject.service('current-user');
+      this.inject.service('notifications');
+
+      this.server = sinon.createFakeServer();
+      this.server.respondImmediately = true;
+    },
+
+    afterEach() {
+      this.server.restore();
+    }
   }
 );
 
-skip('it renders', function(assert) {
+test('it renders', function(assert) {
+  this.server.respondWith('GET', /\/api\/v2\/datasets.*/, [
+    200,
+    { 'Content-Type': 'application/json' },
+    JSON.stringify({})
+  ]);
   this.render(hbs`{{datasets/containers/dataset-acl-access}}`);
 
   assert.equal(
