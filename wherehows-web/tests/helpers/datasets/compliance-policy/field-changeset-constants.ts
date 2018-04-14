be705831aa59be5bf4de3b3e12d719a1646a1afb
Seyi Adebajo <sadebajo@linkedin.com>
2018-04-13 18:19:26 -0700

    fixes failing tests

--- wherehows-web/tests/helpers/datasets/compliance-policy/field-changeset-constants.ts
+++ wherehows-web/tests/helpers/datasets/compliance-policy/field-changeset-constants.ts
@@ -1,13 +1,15 @@
 /**
  * A list of mock field change sets depicting the different scenarios
- * @type {Array<{isDirty: boolean; privacyPolicyExists: boolean; suggestionAuthority: boolean; suggestion: boolean; __requiresReview__: boolean; __msg__: string}>
+ * @type {Array<{isDirty: boolean; privacyPolicyExists: boolean; suggestionAuthority: boolean; suggestion?: {}; __requiresReview__: boolean; __msg__: string}>
  */
+const suggestion = { identifierType: 'same' };
 const mockFieldChangeSets = [
   {
     isDirty: true,
     privacyPolicyExists: true,
     suggestionAuthority: false,
-    suggestion: true,
+    suggestion,
+    identifierType: 'other',
     __requiresReview__: true,
     __msg__: 'Suggestion exists but user has not affirmed or ignored suggestion (suggestionAuthority)'
   },
@@ -15,7 +17,6 @@ const mockFieldChangeSets = [
     isDirty: true,
     privacyPolicyExists: false,
     suggestionAuthority: true,
-    suggestion: false,
     __requiresReview__: false,
     __msg__: 'NO suggestion, and policy does NOT exist and user has made changes to local working copy'
   },
@@ -23,8 +24,16 @@ const mockFieldChangeSets = [
     isDirty: false,
     privacyPolicyExists: false,
     suggestionAuthority: true,
-    suggestion: false,
     __requiresReview__: true,
+    __msg__: 'NO suggestion, and policy does NOT exist but user has NOT made changes to local working copy'
+  },
+  {
+    isDirty: true,
+    privacyPolicyExists: false,
+    suggestionAuthority: true,
+    suggestion,
+    identifierType: 'same',
+    __requiresReview__: false,
     __msg__: 'NO suggestion, and policy does NOT exist but user has NOT made changes to local working copy'
   }
 ];
