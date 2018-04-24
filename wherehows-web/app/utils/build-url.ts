947a6e3ca3640452d96830d453e6dc53b8ac2b61
Seyi Adebajo <sadebajo@linkedin.com>
2018-04-24 11:26:12 -0700

    redirects to https for unsecure locations on app transition

--- wherehows-web/app/utils/build-url.ts
+++ wherehows-web/app/utils/build-url.ts
@@ -39,3 +39,15 @@ export default (baseUrl: string, queryParam: string, queryValue: string): string
 
   return `${baseUrl}${separator}${queryParam}=${queryValue}`;
 };
+
+/**
+ * Sets the href on a location object if the protocol is not https
+ * @param {Location} { protocol, href }
+ */
+export const redirectToHttps = ({ protocol, href, hostname }: Location): void => {
+  const secureProtocol = 'https:';
+
+  if (protocol !== secureProtocol && hostname !== 'localhost') {
+    window.location.replace(`${secureProtocol}${href.substring(protocol.length)}`);
+  }
+};
