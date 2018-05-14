50afc983149679920f5bd1c11b16ef2cd336d40b
Seyi Adebajo <sadebajo@linkedin.com>
2018-05-14 12:29:41 -0700

    upgrades dependencies to latest allowed. adds ember-cli-dependency-lint. updates linter config for e-c-dependency-lint

--- wherehows-web/config/dependency-lint.js
+++ wherehows-web/config/dependency-lint.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = {};
+module.exports = {
+  // Valid use-case: Required addon, is already at latest version, but depends on an older
+  // dependency. PS create an add-on PR, if possible, to fix the issue first
+  allowedVersions: {
+    'ember-concurrency': '^0.8.18',
+    'ember-compatibility-helpers': '0.1.3 || 1.0.0-beta.1 || ^1.0.0',
+    'ember-getowner-polyfill': '1.2.5 || ^2.2.0'
+  }
+};
