9320f29c4e13bd5deb04852c6ffc6fb3f59a3f85
Seyi Adebajo <sadebajo@linkedin.com>
2018-05-14 10:11:51 -0700

    renames compliance metadata events. fixes heart beat timer for piwik tracking

--- wherehows-web/app/constants/analytics/event-tracking/compliance.ts
+++ wherehows-web/app/constants/analytics/event-tracking/compliance.ts
@@ -2,16 +2,16 @@
  * Enumerates the available compliance metadata events
  */
 enum ComplianceEvent {
-  Cancel = 'Cancel Edit Compliance Metadata',
-  Next = 'Next Compliance Metadata Step',
-  Previous = 'Previous Compliance Metadata Step',
-  Edit = 'Begin Edit Compliance Metadata',
-  Download = 'Download Compliance Metadata',
-  Upload = 'Upload Compliance Metadata',
-  SetUnspecifiedAsNone = 'Set Unspecified Fields As None',
-  FieldIndentifier = 'Compliance Metadata Field Identifier Selected',
-  FieldFormat = 'Compliance Metadata Field Format Selected',
-  Save = 'Save Compliance Metadata'
+  Cancel = 'CancelEditComplianceMetadata',
+  Next = 'NextComplianceMetadataStep',
+  Previous = 'PreviousComplianceMetadataStep',
+  Edit = 'BeginEditComplianceMetadata',
+  Download = 'DownloadComplianceMetadata',
+  Upload = 'UploadComplianceMetadata',
+  SetUnspecifiedAsNone = 'SetUnspecifiedFieldsAsNone',
+  FieldIndentifier = 'ComplianceMetadataFieldIdentifierSelected',
+  FieldFormat = 'ComplianceMetadataFieldFormatSelected',
+  Save = 'SaveComplianceMetadata'
 }
 
 export { ComplianceEvent };
