c726f136a1ad41c1a35b75a959d5ce752dade8e5
Seyi Adebajo <sadebajo@linkedin.com>
2018-05-17 23:46:03 -0700

    implements feature to override the retention policy on a child dataset independent of the upstream policies

---
+++ wherehows-web/app/utils/api/datasets/lineage.ts
@@ -0,0 +1,20 @@
+import { getJSON } from 'wherehows-web/utils/api/fetcher';
+import { datasetUrlByUrn } from 'wherehows-web/utils/api/datasets/shared';
+import { IDatasetView } from 'wherehows-web/typings/api/datasets/dataset';
+
+/**
+ * Constructs the url for a datasets upstreams
+ * @param {string} urn the urn for the child dataset
+ * @return {string}
+ */
+const datasetUpstreamUrlByUrn = (urn: string): string => `${datasetUrlByUrn(urn)}/upstreams`;
+
+/**
+ * Fetches the list of upstream datasets for a dataset by urn
+ * @param {string} urn urn for the child dataset
+ * @return {Promise<Array<IDatasetView>>}
+ */
+const readUpstreamDatasetsByUrn = (urn: string): Promise<Array<IDatasetView>> =>
+  getJSON<Array<IDatasetView>>({ url: datasetUpstreamUrlByUrn(urn) });
+
+export { readUpstreamDatasetsByUrn };
