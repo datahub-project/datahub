aa4f931cf423427e8a16f92a316a74a88e1517a8
Seyi Adebajo <sadebajo@linkedin.com>
2018-05-22 11:55:45 -0700

    fixes timing out test

--- wherehows-web/tests/unit/utils/notifications/notifications-test.ts
+++ wherehows-web/tests/unit/utils/notifications/notifications-test.ts
@@ -1,26 +1,23 @@
 import { notificationDialogActionFactory } from 'wherehows-web/utils/notifications/notifications';
-import { setupTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | notifications/notifications', function(hooks) {
-  setupTest(hooks);
+module('Unit | Utility | notifications/notifications');
 
-  test('it creates expected object properties', function(assert) {
-    let result = notificationDialogActionFactory();
-    assert.equal(
-      Object.keys(result)
-        .sort()
-        .toString(),
-      ['dismissedOrConfirmed', 'dialogActions'].sort().toString(),
-      'invocation result is expected shape'
-    );
+test('it creates expected object properties', function(assert) {
+  let result = notificationDialogActionFactory();
+  assert.equal(
+    Object.keys(result)
+      .sort()
+      .toString(),
+    ['dismissedOrConfirmed', 'dialogActions'].sort().toString(),
+    'invocation result is expected shape'
+  );
 
-    assert.equal(
-      Object.keys(result.dialogActions)
-        .sort()
-        .toString(),
-      ['didDismiss', 'didConfirm'].sort().toString(),
-      'dialogActions is expected shape'
-    );
-  });
+  assert.equal(
+    Object.keys(result.dialogActions)
+      .sort()
+      .toString(),
+    ['didDismiss', 'didConfirm'].sort().toString(),
+    'dialogActions is expected shape'
+  );
 });
