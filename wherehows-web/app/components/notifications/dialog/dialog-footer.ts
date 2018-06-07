6e7fad40caadb5285e72b1ae8753c4787435b1d2
Seyi Adebajo <sadebajo@linkedin.com>
2018-06-06 21:46:10 -0700

    refactors confirm dialog modal as a contextual component

---
+++ wherehows-web/app/components/notifications/dialog/dialog-footer.ts
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default class NotificationsDialogDialogFooter extends Component {
+  tagName = 'footer';
+
+  classNames = ['notification-confirm-modal__footer'];
+}
