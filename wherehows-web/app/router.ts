316440e3ee72ffe8d6dfa0605e5e94060a3727cf
Seyi Adebajo <sadebajo@linkedin.com>
2018-06-28 10:54:00 -0700

    moves router.js -> router.ts

--- wherehows-web/app/router.ts
+++ wherehows-web/app/router.ts
@@ -1,15 +1,16 @@
-import Router from '@ember/routing/router';
+import EmberRouter from '@ember/routing/router';
 import { get, getWithDefault } from '@ember/object';
-import { inject as service } from '@ember/service';
+import { inject } from '@ember/service';
 import { scheduleOnce } from '@ember/runloop';
 import config from 'wherehows-web/config/environment';
+import Metrics from 'ember-metrics';
 
-const AppRouter = Router.extend({
+const AppRouter = EmberRouter.extend({
   location: config.locationType,
 
   rootURL: config.rootURL,
 
-  metrics: service(),
+  metrics: inject(),
 
   didTransition() {
     this._super(...arguments);
@@ -24,9 +25,11 @@ const AppRouter = Router.extend({
    */
   _trackPage() {
     scheduleOnce('afterRender', null, () => {
+      //@ts-ignore types need update, location property is not a string post instantiation
       const page = get(this, 'location').getURL();
+      //@ts-ignore types need update
       const title = getWithDefault(this, 'currentRouteName', 'unknown.page.title');
-      const metrics = get(this, 'metrics');
+      const metrics = <Metrics>get(this, 'metrics');
       // Reference to the _paq queue
       //   check if we are in a browser env
       const paq = window && window._paq ? window._paq : [];
@@ -38,6 +41,7 @@ const AppRouter = Router.extend({
        * @link https://developer.piwik.org/guides/tracking-javascript-guide#internal-search-tracking
        */
       if (title.includes('search')) {
+        //@ts-ignore types need update
         const routerJs = get(this, 'router');
         const queryParams = routerJs ? get(routerJs, 'state.queryParams') : {};
         const { keyword, category = 'datasets', page = 1 } = queryParams;
