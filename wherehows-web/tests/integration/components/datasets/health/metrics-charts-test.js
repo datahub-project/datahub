188df1ddb65d00a5fe841b5a9b7363510793ef1e
cptran777 <catran@linkedin.com>
2018-08-01 16:34:55 -0700

    add tests for metrics component

--- wherehows-web/tests/integration/components/datasets/health/metrics-charts-test.js
+++ wherehows-web/tests/integration/components/datasets/health/metrics-charts-test.js
@@ -1,26 +1,48 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
+import { run } from '@ember/runloop';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | datasets/health/metrics-charts', function(hooks) {
-  setupRenderingTest(hooks);
+moduleForComponent('datasets/health/metrics-charts', 'Integration | Component | datasets/health/metrics-charts', {
+  integration: true
+});
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+const chartClass = '.viz-bar-chart';
+const labelClass = '.highcharts-label';
+const labelValueClass = `${labelClass} .highcharts-emphasized`;
+const barClass = `${chartClass}__bar`;
 
-    await render(hbs`{{datasets/health/metrics-charts}}`);
+test('it renders', async function(assert) {
+  this.render(hbs`{{datasets/health/metrics-charts}}`);
+  assert.ok(this.$(), 'Renders without errors');
+});
 
-    assert.equal(this.element.textContent.trim(), '');
+test('it provides the correct information', async function(assert) {
+  const categoryData = [{ name: 'Compliance', value: 60 }, { name: 'Ownership', value: 40 }];
+  const severityData = [
+    { name: 'Minor', value: 50, customColorClass: 'severity-chart__bar--minor' },
+    { name: 'Warning', value: 30, customColorClass: 'severity-chart__bar--warning' },
+    { name: 'Critical', value: 25, customColorClass: 'severity-chart__bar--critical' }
+  ];
+  const testAction = datum => assert.equal(datum.name, 'Warnisng', 'Action successfully passes along info');
 
-    // Template block usage:
-    await render(hbs`
-      {{#datasets/health/metrics-charts}}
-        template block text
-      {{/datasets/health/metrics-charts}}
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  this.setProperties({
+    categoryData,
+    severityData,
+    testAction
   });
+
+  this.render(hbs`{{datasets/health/metrics-charts
+                    categoryData=categoryData
+                    severityData=severityData
+                    onBarSelect=testAction}}`);
+
+  assert.ok(this.$(), 'Still renders without errors');
+  assert.equal(this.$(chartClass).length, 2, 'Renders 2 charts');
+  assert.equal(
+    this.$(`${labelValueClass}:eq(0)`)
+      .text()
+      .trim(),
+    '60%',
+    'Renders correct value for labels'
+  );
 });
