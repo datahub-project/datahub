a164cfe6bc6a36aea4731671e6335921abdad1ac
cptran777 <catran@linkedin.com>
2018-08-01 16:34:55 -0700

    add tests for metrics component

--- wherehows-web/app/components/datasets/health/metrics-charts.ts
+++ wherehows-web/app/components/datasets/health/metrics-charts.ts
@@ -1,4 +1,7 @@
 import Component from '@ember/component';
+import { IChartDatum } from 'wherehows-web/typings/app/visualization/charts';
+import { set } from '@ember/object';
+import { noop } from 'wherehows-web/utils/helpers/functions';
 
 export default class DatasetsHealthMetricsCharts extends Component {
   /**
@@ -6,4 +9,17 @@ export default class DatasetsHealthMetricsCharts extends Component {
    * @type {Array<string>}
    */
   classNames = ['dataset-health__metrics-charts'];
+
+  /**
+   * Pass through function meant to come from the dataset-health container that handles the selection of
+   * a filter, triggered when the user clicks on one of the bars of the bar chart.
+   * @param {IChartDatum} datum - the actual chart datum object so we know what was clicked
+   */
+  onFilterSelect: (datum: IChartDatum) => void;
+
+  constructor() {
+    super(...arguments);
+
+    set(this, 'onFilterSelect', this.onFilterSelect || noop);
+  }
 }
