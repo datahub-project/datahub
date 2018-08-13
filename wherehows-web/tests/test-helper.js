f37a303a1c79241c27a526fcc22bc9c267d3bafe
cptran777 <catran@linkedin.com>
2018-08-13 15:01:19 -0700

    Handling for deprecation messages to use defineProperty

--- wherehows-web/tests/test-helper.js
+++ wherehows-web/tests/test-helper.js
@@ -2,6 +2,15 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { registerDeprecationHandler } from '@ember/debug';
+
+// Suggestion to keep this here until we are ready to handle the deprecation to use definePRoperty for
+// computed properties. Otherwise, tests and build logs becoming overwhelming to look at
+registerDeprecationHandler(function(message, { id }, next) {
+  if (!message.includes('defineProperty')) {
+    next(...arguments);
+  }
+});
 
 setApplication(Application.create(config.APP));
 
