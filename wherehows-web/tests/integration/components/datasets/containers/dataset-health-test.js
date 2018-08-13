db227ac0fd191d57665c1e732011c39c8f741983
cptran777 <catran@linkedin.com>
2018-08-12 22:44:01 -0700

    Fix tests for dataset health and create health mirage api handler

--- wherehows-web/tests/integration/components/datasets/containers/dataset-health-test.js
+++ wherehows-web/tests/integration/components/datasets/containers/dataset-health-test.js
@@ -1,13 +1,20 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { waitUntil, find } from 'ember-native-dom-helpers';
+import { urn } from 'wherehows-web/mirage/fixtures/urn';
+import sinon from 'sinon';
 
-module('Integration | Component | datasets/containers/dataset-health', function(hooks) {
-  setupRenderingTest(hooks);
-  // TODO: More meaningful tests as we continue with development
-  test('it renders', async function(assert) {
-    await render(hbs`{{datasets/containers/dataset-health}}`);
-    assert.ok(this.element, 'Renders without errors');
-  });
+moduleForComponent(
+  'datasets/containers/dataset-health',
+  'Integration | Component | datasets/containers/dataset-health',
+  {
+    integration: true
+  }
+);
+
+test('it renders', async function(assert) {
+  this.set('urn', urn);
+  this.render(hbs`{{datasets/containers/dataset-health urn=urn}}`);
+
+  assert.ok(find('.dataset-health__score-table'), 'renders the health table component');
 });
