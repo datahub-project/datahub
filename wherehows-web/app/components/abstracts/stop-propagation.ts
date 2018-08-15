c1ae62e1f301b004b3735151d87b16d46fc63e45
cptran777 <catran@linkedin.com>
2018-08-15 14:15:27 -0700

    Modify stop propagation wrapper and add tests

--- wherehows-web/app/components/abstracts/stop-propagation.ts
+++ wherehows-web/app/components/abstracts/stop-propagation.ts
@@ -1,5 +1,36 @@
 import Component from '@ember/component';
 
+/**
+ * Defining our stop propagation here for DRY-ness
+ * @param {KeyboardEvent} e - keyboard keyup trigger
+ * @returns {boolean}
+ */
+function stopPropagation(e: KeyboardEvent): boolean {
+  e.stopImmediatePropagation();
+  return false;
+}
+
+/**
+ * StopPropagationBase component can be extended by another component to capture event propagation
+ * or wrap around template blocks.
+ * @example
+ * Usage case #1
+ * ```
+ * import StopPropagationBase from 'wherehows-web/abstracts/stop-propagation'
+ *
+ * export default class YourComponent extends StopPropagationBase { ... }
+ * ```
+ *
+ * Usage case #2
+ * This component can wrap around a template block. If there is a case where we do not want the
+ * wrapper to have its own element in order to reduce DOM complexity, we can use also set the
+ * component to be tagless.
+ * ```
+ * {{#abstracts/stop-propagation tagName="" as |stopPropagation|}}
+ *   <input with keyup events onkeyup={{stopPropagation}}/>
+ * {{/abstracts/stop-propagation}}
+ * ```
+ */
 export default class StopPropagationBase extends Component {
   /**
    * Prevents keys in this component from propagating to the global level and unnecessarily calling
@@ -7,7 +38,12 @@ export default class StopPropagationBase extends Component {
    * a particular component
    * @param {KeyboardEvent} e - passed in by keyboard key up trigger
    */
-  keyUp(e: KeyboardEvent) {
-    e.stopImmediatePropagation();
-  }
+  keyUp = this.tagName === '' ? undefined : stopPropagation;
+
+  /**
+   * Given to the template wrapper div tag in case we have a tagless component wrapper and keyUp()
+   * cannot be defined as a result
+   * @param {KeyboardEvent} e - passed in by keyboard key up trigger
+   */
+  stopPropagation = stopPropagation;
 }
--- wherehows-web/tests/integration/components/abstracts/stop-propagation-test.js
+++ wherehows-web/tests/integration/components/abstracts/stop-propagation-test.js
@@ -1,26 +1,46 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerKeyEvent, fillIn } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { readDatasetOwnerTypesWithoutConsumer } from 'wherehows-web/utils/api/datasets/owners';
 
 module('Integration | Component | abstracts/stop-propagation', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{abstracts/stop-propagation}}`);
-
     assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
     await render(hbs`
       {{#abstracts/stop-propagation}}
         template block text
       {{/abstracts/stop-propagation}}
     `);
-
     assert.equal(this.element.textContent.trim(), 'template block text');
+  });
+
+  test('it stops key up propagation', async function(assert) {
+    const functionShouldCall = () => {
+      assert.ok(true, 'This function was correctly called');
+    };
+
+    const functionShouldNotCall = () => {
+      assert.ok(false, 'This assertion should never run');
+    };
+
+    this.setProperties({ functionShouldCall, functionShouldNotCall });
+
+    assert.expect(2);
+    await render(hbs`
+      <div onkeyup={{functionShouldNotCall}}>
+        {{#abstracts/stop-propagation as |funcKeyUp|}}
+          <div onkeyup={{funcKeyUp}}>
+            <input id="pika-test" onkeyup={{functionShouldCall}} />
+          </div>
+        {{/abstracts/stop-propagation}}
+      </div>
+    `);
+
+    assert.ok(this.element, 'Still renders without errors');
+    this.$('#pika-test').trigger('keyup');
   });
 });
