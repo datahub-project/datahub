9661ddedf64ab0d5e7b1f5be43fead080c719098
cptran777 <catran@linkedin.com>
2018-08-16 15:01:30 -0700

    Finalize handling for api for metadata health endpoint and showing results

--- wherehows-web/app/utils/api/datasets/health.ts
+++ wherehows-web/app/utils/api/datasets/health.ts
@@ -9,6 +9,26 @@ import { IHealthScoreResponse, IDatasetHealth } from 'wherehows-web/typings/api/
  */
 const datasetHealthUrlByUrn = (urn: string): string => `${datasetUrlByUrn(urn)}/health`;
 
+/**
+ * Constant for formatting of the validator string
+ * @type {string}
+ */
+const validatorFormat = 'com.linkedin.metadata.validators';
+
+/**
+ * Useful for removing extraneous information outside of category
+ * @type {RegExp}
+ */
+const validationRegex = new RegExp(`${validatorFormat}.|Validator`, 'gi');
+
+/**
+ * Given a string in the format 'com.linkedin.metadata.validators.[Category]Validator', extract
+ * Category and return it.
+ * @param validator - Given validator string
+ * @returns {string}
+ */
+export const getCategory = (validator: string) => validator.replace(validationRegex, '');
+
 export const readDatasetHealthByUrn = async (urn: string): Promise<IDatasetHealth> => {
   const defaultResponse = { score: 0, validations: [] };
 
