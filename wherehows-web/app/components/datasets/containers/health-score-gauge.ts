e5cfe24b37c2beef7f1e29795762458a0f743d23
cptran777 <catran@linkedin.com>
2018-08-17 14:14:16 -0700

    Code cleanup for health score gauge handler

--- wherehows-web/app/components/datasets/containers/health-score-gauge.ts
+++ wherehows-web/app/components/datasets/containers/health-score-gauge.ts
@@ -4,8 +4,6 @@ import { IDatasetHealth } from 'wherehows-web/typings/api/datasets/health';
 import { readDatasetHealthByUrn } from 'wherehows-web/utils/api/datasets/health';
 import { get, set } from '@ember/object';
 
-// import { once } from '@ember/runloop';
-
 export default class HealthScoreGauge extends Component {
   /**
    * The urn identifier for the dataset
@@ -17,12 +15,14 @@ export default class HealthScoreGauge extends Component {
    * Defines the tag to be used in the rendered html element for this component
    * @type {string}
    */
-  tagName: '';
+  tagName = '';
+
+  elementId = <any>undefined;
 
   /**
    * The health score to be passed to the container
    */
-  healthScore: number;
+  healthScore: number = 0;
 
   didInsertElement() {
     get(this, 'getHealthScoreTask').perform();
