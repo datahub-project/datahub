1024a04d3bcbff751ff4036bd3d4756228193f17
cptran777 <catran@linkedin.com>
2018-08-17 13:38:42 -0700

    Add testing for global key components

--- wherehows-web/tests/integration/components/hotkeys/global-hotkeys-test.js
+++ wherehows-web/tests/integration/components/hotkeys/global-hotkeys-test.js
@@ -1,26 +1,43 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, triggerKeyEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import Service from '@ember/service';
+import { Keyboard } from 'wherehows-web/constants/keyboard';
 
 module('Integration | Component | hotkeys/global-hotkeys', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
     await render(hbs`{{hotkeys/global-hotkeys}}`);
-
     assert.equal(this.element.textContent.trim(), '');
+  });
 
-    // Template block usage:
-    await render(hbs`
-      {{#hotkeys/global-hotkeys}}
-        template block text
-      {{/hotkeys/global-hotkeys}}
-    `);
+  test('it detects target elligibility', async function(assert) {
+    const HotKeyService = Service.extend({
+      applyKeyMapping(keyCode) {
+        assert.ok(true, 'Applied key mapping is called for key: ' + keyCode);
+      }
+    });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    this.owner.register('service:hot-keys', HotKeyService);
+    assert.expect(2);
+
+    await render(hbs`<div class="ember-application">
+                       {{#hotkeys/global-hotkeys}}
+                         <div id="pika-test"></div>
+                       {{/hotkeys/global-hotkeys}}
+                     </div>`);
+
+    assert.ok(this.element, 'Still renders without errors');
+    triggerKeyEvent('#pika-test', 'keyup', Keyboard.Slash);
+
+    await render(hbs`<div class="ember-application">
+                       {{#hotkeys/global-hotkeys}}
+                         <input id="pika-test">
+                       {{/hotkeys/global-hotkeys}}
+                     </div>`);
+    // This is expected to not call our apply method, hence only 2 assertions in this test
+    triggerKeyEvent('#pika-test', 'keyup', Keyboard.Slash);
   });
 });
