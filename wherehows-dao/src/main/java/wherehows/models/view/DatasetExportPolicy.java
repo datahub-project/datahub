36f02b0de46637069091ef6d80de0ece9a46d0ae
cptran777 <catran@linkedin.com>
2018-09-04 10:42:06 -0700

    Add allargsconstructor to exportpolicy

--- wherehows-dao/src/main/java/wherehows/models/view/DatasetExportPolicy.java
+++ wherehows-dao/src/main/java/wherehows/models/view/DatasetExportPolicy.java
@@ -16,12 +16,12 @@ package wherehows.models.view;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
 
 
-@Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class DatasetExportPolicy {
   private boolean containsUserGeneratedContent;
 
