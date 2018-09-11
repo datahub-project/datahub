a3624a16710c4fe486196d64cf382f1c67ea98f8
cptran777 <catran@linkedin.com>
2018-09-11 10:06:53 -0700

    Separate compliance and retention post requests on save

--- wherehows-web/app/components/datasets/containers/dataset-compliance.ts
+++ wherehows-web/app/components/datasets/containers/dataset-compliance.ts
@@ -40,6 +40,9 @@ import { notificationDialogActionFactory } from 'wherehows-web/utils/notificatio
 import Configurator from 'wherehows-web/services/configurator';
 import { typeOf } from '@ember/utils';
 import { service } from '@ember-decorators/service';
+import { saveDatasetRetentionByUrn } from 'wherehows-web/utils/api/datasets/retention';
+import { extractRetentionFromComplianceInfo } from 'wherehows-web/utils/datasets/retention';
+import { IDatasetRetention } from 'wherehows-web/typings/api/datasets/retention';
 
 /**
  * Type alias for the response when container data items are batched
@@ -334,6 +337,30 @@ export default class DatasetComplianceContainer extends Component {
           // filter out readonly entities, then omit readonly attribute from remaining entities before save
           complianceEntities: removeReadonlyAttr(editableTags(complianceEntities))
         })
+      );
+
+      this.resetPrivacyCompliancePolicy.call(this);
+    }
+  }
+
+  /**
+   * Persists the updates to the retention policy on the remote host
+   * @return {Promise<void>}
+   */
+  @action
+  async saveRetentionPolicy(this: DatasetComplianceContainer): Promise<void> {
+    const complianceInfo = get(this, 'complianceInfo');
+    if (complianceInfo) {
+      const { complianceEntities } = complianceInfo;
+
+      await this.notifyOnSave<IDatasetRetention>(
+        saveDatasetRetentionByUrn(
+          get(this, 'urn'),
+          extractRetentionFromComplianceInfo({
+            ...complianceInfo,
+            complianceEntities: removeReadonlyAttr(editableTags(complianceEntities))
+          })
+        )
       );
 
       this.resetPrivacyCompliancePolicy.call(this);
