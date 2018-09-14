fa04b2cd952d299349a457271f56598430529cca
Ignacio Bona <ibonapiedrabuena@linkedin.com>
2018-09-14 16:40:45 -0700

    Facets: Docs and a little cleanup

---
+++ wherehows-web/app/components/get-yield.ts
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+/**
+ * Small component that will simply yield get a property of an object
+ * to improve hbs ergonomics.
+ */
+export default class GetYield extends Component.extend({ tagName: '' }) {}
--- wherehows-web/app/controllers/search.ts
+++ wherehows-web/app/controllers/search.ts
@@ -1,7 +1,13 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember-decorators/object';
 import { debounce } from '@ember-decorators/runloop';
-import { IFacetsSelectionsMap, facetToParamUrl, facetFromParamUrl } from 'wherehows-web/utils/api/search';
+import {
+  IFacetsSelectionsMap,
+  facetToParamUrl,
+  facetFromParamUrl,
+  facetToDynamicCounts,
+  IFacetsCounts
+} from 'wherehows-web/utils/api/search';
 
 // gradual refactor into es class, hence extends EmberObject instance
 export default class SearchController extends Controller {
@@ -31,13 +37,28 @@ export default class SearchController extends Controller {
    */
   header = 'Refine By';
 
+  /**
+   * Since the loading of search comes from two parts:
+   * 1. Search Call
+   * 2. During debouncing
+   *
+   * We put it as a flag to control it better
+   */
   searchLoading: boolean = false;
 
+  /**
+   * When facets change we set the flag loading and call the debounced fn
+   * @param selections facet selections
+   */
   onFacetsChange(selections: IFacetsSelectionsMap) {
     this.set('searchLoading', true);
     this.onFacetsChangeDebounced(selections);
   }
 
+  /**
+   * Will set the facets in the URL to start a model refresh (see route)
+   * @param selections Facet selections
+   */
   @debounce(1000)
   onFacetsChangeDebounced(selections: IFacetsSelectionsMap) {
     this.setProperties({
@@ -46,10 +67,27 @@ export default class SearchController extends Controller {
     });
   }
 
+  /**
+   * Will translate backend fields into a dynamic facet
+   * count structure.
+   */
+  @computed('model')
+  get facetCounts(): IFacetsCounts {
+    return facetToDynamicCounts(this.model);
+  }
+
+  /**
+   * Will read selections from URL and translate it into
+   * our selections object
+   */
   @computed('facets')
-  get facetsSelections() {
+  get facetsSelections(): IFacetsSelectionsMap {
     return facetFromParamUrl(this.facets || '');
   }
+
+  /**
+   * Will return false if there is data to display
+   */
   @computed('model.data.length')
   get showNoResult() {
     return this.model.data ? this.model.data.length === 0 : true;
