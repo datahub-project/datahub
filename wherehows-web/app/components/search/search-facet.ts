7268750adac27c568fa1926b46935a838eacbba6
Ignacio Bona <ibonapiedrabuena@linkedin.com>
2018-09-14 16:47:28 -0700

    Facets: Docs and a little cleanup

--- wherehows-web/app/components/search/search-facet.ts
+++ wherehows-web/app/components/search/search-facet.ts
@@ -1,16 +1,25 @@
 import Component from '@ember/component';
 import { computed } from '@ember-decorators/object';
+import { IFacetSelections } from 'wherehows-web/utils/api/search';
 
+/**
+ * Presentation component of a facet
+ */
 export default class SearchFacet extends Component {
-  // tagName = '';
+  /**
+   * Facet selections
+   */
+  selections: IFacetSelections;
 
-  selections: any;
-
+  /**
+   * Computed property to check if there is any selection in the
+   * facet. If that is the case, a clear button will show up.
+   */
   @computed('selections')
   get showClear(): boolean {
     const selections = this.selections || {};
-    return Object.keys(selections).reduce((willShow: boolean, selectionKey: string) => {
-      return willShow || selections[selectionKey];
+    return Object.keys(selections).reduce((willShowClear: boolean, selectionKey: string) => {
+      return willShowClear || selections[selectionKey];
     }, false);
   }
 }
