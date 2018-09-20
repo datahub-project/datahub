31992aad936810a3e53f5b96f90a93e6652eaec8
Seyi Adebajo <sadebajo@linkedin.com>
2018-09-20 12:59:38 -0700

    removes safari...

--- wherehows-web/testem.js
+++ wherehows-web/testem.js
@@ -1,7 +1,7 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: ['Chrome', 'Safari', 'Firefox'],
+  launch_in_ci: ['Chrome', 'Firefox'],
   launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
