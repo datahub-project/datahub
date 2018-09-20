76f55d913ad1a7f8903eb5fdbb204414691e3b8f
Seyi Adebajo <sadebajo@linkedin.com>
2018-09-20 09:24:45 -0700

    propagates qps to container

--- wherehows-web/app/routes/datasets/dataset/compliance.ts
+++ wherehows-web/app/routes/datasets/dataset/compliance.ts
@@ -1,32 +1,30 @@
-import { set } from '@ember/object';
-import { descendantDatasetRouteClassFactory, Tabs } from 'wherehows-web/constants/datasets/shared';
+import {
+  descendantDatasetRouteClassFactory,
+  setPropertyOnAncestorController,
+  Tabs
+} from 'wherehows-web/constants/datasets/shared';
 import DatasetComplianceController from 'wherehows-web/controllers/datasets/dataset/compliance';
 import { refreshModelForQueryParams } from 'wherehows-web/utils/helpers/routes';
-import { IDatasetView } from 'wherehows-web/typings/api/datasets/dataset';
+import { isTagFilter, TagFilter } from 'wherehows-web/constants';
 import DatasetController from 'wherehows-web/controllers/datasets/dataset';
 
 export default class Compliance extends descendantDatasetRouteClassFactory({ selectedTab: Tabs.Compliance }) {
   queryParams = refreshModelForQueryParams(['fieldFilter']);
 
-  setupController(controller: DatasetComplianceController, model: IDatasetView): void {
-    const { setPropOnAncestorController } = this;
+  async model(): Promise<DatasetController['model'] | object> {
+    return { ...this.controllerFor('datasets.dataset').model };
+  }
+
+  setupController(controller: DatasetComplianceController, model: object): void {
+    const { fieldFilter } = controller;
 
     super.setupController(controller, model);
 
-    // set the compliance tagFilter on the ancestor controller
-    setPropOnAncestorController('complianceTagFilter', controller.fieldFilter);
+    // set the complianceTagFilter property on the ancestor controller
+    setPropertyOnAncestorController(
+      this,
+      'complianceTagFilter',
+      isTagFilter(fieldFilter) ? fieldFilter : TagFilter.showAll
+    );
   }
-
-  /**
-   * Sets a DatasetController property K to the supplied value
-   * @template K keyof DatasetController
-   * @param {K} prop the property on the DatasetController to be set
-   * @param {DatasetController[K]} value value to be applied to prop
-   * @returns {DatasetController[K]}
-   * @memberof Compliance
-   */
-  setPropOnAncestorController = <K extends keyof DatasetController>(
-    prop: K,
-    value: DatasetController[K]
-  ): DatasetController[K] => set(this.controllerFor('datasets.dataset'), prop, value);
 }
