1801b2943859b9f4df1520e4113c1fe5b429e15f
cptran777 <catran@linkedin.com>
2018-09-25 22:31:08 -0700

    Use retention policy for form and create separate reset retention

--- wherehows-web/app/utils/datasets/retention.ts
+++ wherehows-web/app/utils/datasets/retention.ts
@@ -1,26 +1,5 @@
 import { IComplianceInfo } from 'wherehows-web/typings/api/datasets/compliance';
-import { IDatasetRetention } from 'wherehows-web/typings/api/datasets/retention';
 import { nullify, Nullify, Omit, pick } from 'wherehows-web/utils/object';
-import { isExempt } from 'wherehows-web/constants';
-
-/**
- * Extracts values from an IComplianceInfo instance to create an instance of IDatasetRetention
- * @param {IComplianceInfo} complianceInfo the compliance info object
- * @returns {IDatasetRetention}
- */
-const extractRetentionFromComplianceInfo = (complianceInfo: IComplianceInfo): IDatasetRetention => {
-  let { datasetUrn, compliancePurgeNote, complianceType } = pick(complianceInfo, [
-    'complianceType',
-    'compliancePurgeNote',
-    'datasetUrn'
-  ]);
-
-  return {
-    purgeNote: complianceType && isExempt(complianceType) ? compliancePurgeNote : '',
-    purgeType: complianceType,
-    datasetUrn: datasetUrn!
-  };
-};
 
 /**
  * Makes values in IComplianceInfo that are present / have a corollary in IDatasetRetention type null i.e. keys
@@ -38,4 +17,4 @@ const nullifyRetentionFieldsOnComplianceInfo = (
   ...nullify(pick(complianceInfo, ['complianceType', 'compliancePurgeNote']))
 });
 
-export { extractRetentionFromComplianceInfo, nullifyRetentionFieldsOnComplianceInfo };
+export { nullifyRetentionFieldsOnComplianceInfo };
