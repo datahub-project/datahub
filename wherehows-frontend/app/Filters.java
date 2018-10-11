dcd7646279f5454763769d6777c2d3b5ca8f264d
Mars Lan <malan@linkedin.com>
2018-10-10 18:16:14 -0700

    Remove extra spaces
    MP: wherehows-frontend
    TO_COMMIT: 9a0d6c86d51935f9848b12662a1e6a35e56356ff
    FROM_COMMIT: 9a0d6c86d51935f9848b12662a1e6a35e56356ff

--- wherehows-frontend/app/Filters.java
+++ wherehows-frontend/app/Filters.java
@@ -11,7 +11,6 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-
 import javax.inject.Inject;
 import play.api.mvc.EssentialFilter;
 import play.filters.gzip.GzipFilter;
--- wherehows-frontend/app/security/DummyLoginModule.java
+++ wherehows-frontend/app/security/DummyLoginModule.java
@@ -11,7 +11,6 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
-
 package security;
 
 import javax.security.auth.Subject;
