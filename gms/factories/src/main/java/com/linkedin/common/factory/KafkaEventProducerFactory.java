505a1a167961bb6e371cbb00f251349655d89c46
Kerem Sahin <ksahin@linkedin.com>
2019-09-03 12:53:03 -0700

    Add back gms.properties (ignoreIfNotExists) to enable Dockerless deployment of datahub-gms

--- gms/factories/src/main/java/com/linkedin/common/factory/KafkaEventProducerFactory.java
+++ gms/factories/src/main/java/com/linkedin/common/factory/KafkaEventProducerFactory.java
@@ -10,11 +10,13 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
 import java.util.Properties;
 
 @Configuration
+@PropertySource(value = "classpath:gms.properties", ignoreResourceNotFound = true)
 public class KafkaEventProducerFactory {
 
   @Autowired
