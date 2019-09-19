59ed313bcd03d34aa587f32941315362bd061d51
Seyi Adebajo <sadebajo@linkedin.com>
2019-09-18 17:19:59 -0700

    Fixes login tests related to token removal

--- datahub-web/packages/data-portal/tests/helpers/login/test-login.ts
+++ datahub-web/packages/data-portal/tests/helpers/login/test-login.ts
@@ -1,17 +1,15 @@
 import { visit, click, fillIn } from '@ember/test-helpers';
-import { authenticationUrl, testUser, testPassword, testVipToken } from 'wherehows-web/tests/helpers/login/constants';
+import { authenticationUrl, testUser, testPassword } from 'wherehows-web/tests/helpers/login/constants';
 import {
   loginUserInput,
   loginPasswordInput,
-  loginSubmitButton,
-  loginVipTokenInput
+  loginSubmitButton
 } from 'wherehows-web/tests/helpers/login/page-element-constants';
 
 const appLogin = async (): Promise<void> => {
   await visit(authenticationUrl);
   await fillIn(loginUserInput, testUser);
   await fillIn(loginPasswordInput, testPassword);
-  await fillIn(loginVipTokenInput, testVipToken.toString());
   await click(loginSubmitButton);
 };
 
