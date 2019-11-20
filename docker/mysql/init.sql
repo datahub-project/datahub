ee47abe624232cffacd845224bb7142622f586c0
Kerem Sahin <ksahin@linkedin.com>
2019-11-20 00:52:54 -0800

    Add creation of default user to MySQL initialization

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -9,3 +9,20 @@ create table metadata_aspect (
   createdfor                    varchar(255),
   constraint pk_metadata_aspect primary key (urn,aspect,version)
 );
+
+-- create default records for datahub user
+insert into metadata_aspect (urn, aspect, version, metadata, createdon, createdby) values(
+  'urn:li:corpuser:datahub',
+  'com.linkedin.identity.CorpUserInfo',
+  0,
+  '{"displayName":"Data Hub","active":true,"fullName":"Data Hub","email":"datahub@linkedin.com"}',
+  now(),
+  'urn:li:principal:datahub'
+), (
+  'urn:li:corpuser:datahub',
+  'com.linkedin.identity.CorpUserEditableInfo',
+  0,
+  '{"skills":[],"teams":[],"pictureLink":"https://raw.githubusercontent.com/linkedin/WhereHows/datahub/datahub-web/packages/data-portal/public/assets/images/default_avatar.png"}',
+  now(),
+  'urn:li:principal:datahub'
+);
