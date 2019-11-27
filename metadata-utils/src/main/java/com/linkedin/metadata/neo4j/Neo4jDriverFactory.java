fdc191330d61dd3ae2b2109d6b5c97ed1241b1ab
Kerem Sahin <ksahin@linkedin.com>
2019-11-26 22:19:46 -0800

    Enable datahub-mae-consumer job to build graph as well

---
+++ metadata-utils/src/main/java/com/linkedin/metadata/neo4j/Neo4jDriverFactory.java
@@ -0,0 +1,13 @@
+package com.linkedin.metadata.neo4j;
+
+import javax.annotation.Nonnull;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+
+
+public class Neo4jDriverFactory {
+  public static Driver createInstance(@Nonnull String uri, @Nonnull String username, @Nonnull String password) {
+    return GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+  }
+}
