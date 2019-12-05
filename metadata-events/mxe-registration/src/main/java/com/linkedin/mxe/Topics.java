6b137a862be757e8af9416246e61ba847233cfe2
Kerem Sahin <ksahin@linkedin.com>
2019-12-04 17:51:52 -0800

    Removing unnecessary classes for mxe-registration

--- metadata-events/mxe-registration/src/main/java/com/linkedin/mxe/Topics.java
+++ metadata-events/mxe-registration/src/main/java/com/linkedin/mxe/Topics.java
@@ -7,10 +7,6 @@ public class Topics {
   public static final String METADATA_GRAPH_EVENT = "MetadataGraphEvent";
   public static final String METADATA_SEARCH_EVENT = "MetadataSearchEvent";
 
-  public static final String DEV_METADATA_AUDIT_EVENT = "MetadataAuditEvent_dev";
-  public static final String DEV_METADATA_CHANGE_EVENT = "MetadataChangeEvent_dev";
-  public static final String DEV_FAILED_METADATA_CHANGE_EVENT = "FailedMetadataChangeEvent_dev";
-
   private Topics() {
     // Util class
   }
