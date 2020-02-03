7952a69f8b9b4f9523bfde6788079e40085d833a
Seyi Adebajo <seyi@outlook.com>
2020-02-02 21:55:55 -0800

    Fixes failing tests

--- datahub-web/packages/data-portal/tests/integration/components/datasets/containers/dataset-ownership-test.js
+++ datahub-web/packages/data-portal/tests/integration/components/datasets/containers/dataset-ownership-test.js
@@ -3,37 +3,28 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, waitUntil, find } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { urn } from 'wherehows-web/mirage/fixtures/urn';
-import sinon from 'sinon';
 import { resetConfig, setMockConfig } from 'wherehows-web/services/configurator';
 import { noop } from 'wherehows-web/utils/helpers/functions';
+import { startMirage } from 'wherehows-web/initializers/ember-cli-mirage';
 
 module('Integration | Component | datasets/containers/dataset ownership', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
     setMockConfig({});
-    this.sinonServer = sinon.createFakeServer();
-    this.sinonServer.respondImmediately = true;
   });
 
   hooks.afterEach(function() {
-    this.sinonServer.restore();
     resetConfig();
   });
 
   test('it renders', async function(assert) {
     const lookupClass = '.dataset-owner-table__add-owner';
     this.setProperties({ urn: urn, setOwnershipRuleChange: noop });
-    this.sinonServer.respondWith('GET', /\/api\/v2\/datasets.*/, [
-      200,
-      { 'Content-Type': 'application/json' },
-      JSON.stringify({})
-    ]);
-    this.sinonServer.respondWith('GET', '/api/v1/owner/types', [
-      200,
-      { 'Content-Type': 'application/json' },
-      JSON.stringify([])
-    ]);
+    const mirage = startMirage();
+
+    mirage.get('/api/v2/datasets', () => ({}));
+    mirage.get('/api/v1/owners/types/', () => []);
 
     await render(hbs`{{datasets/containers/dataset-ownership setOwnershipRuleChange=setOwnershipRuleChange urn=urn}}`);
 
@@ -43,5 +34,6 @@ module('Integration | Component | datasets/containers/dataset ownership', functi
       'Add an owner',
       'shows dataset authors component'
     );
+    mirage.shutdown();
   });
 });
