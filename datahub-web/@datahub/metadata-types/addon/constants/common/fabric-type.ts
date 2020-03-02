c52692644d37dd5ce63ca4d4646b04b158e1fde0
Mars Lan <mars.th.lan@gmail.com>
2020-03-02 06:23:50 -0800

    Drop LI internal fabrics

--- datahub-web/@datahub/metadata-types/addon/constants/common/fabric-type.ts
+++ datahub-web/@datahub/metadata-types/addon/constants/common/fabric-type.ts
@@ -12,17 +12,5 @@ export enum FabricType {
   // Designates production fabrics, such as prod-ltx1, prod-lva1 etc.
   PROD = 'PROD',
   // Designates corporation fabrics, such as corp-eat1, corp-lca1 etc.
-  CORP = 'CORP',
-  // Designates infrastructure testing fabrics, such as lit-lca1-1.
-  LIT = 'LIT',
-  // Designates Prime fabric group for project Einstein within Linkedin.
-  PRIME = 'PRIME',
-  // Designates production fabrics deployed in the Mergers and Acquisitions network (MANDA).
-  MANDA = 'MANDA',
-  // Designates production fabrics deployed in the Azure control plane.
-  AZURECONTROL = 'AZURECONTROL',
-  // Designates production fabrics deployed in the Azure.
-  AZUREPROD = 'AZUREPROD',
-  // Designates Early-Integration fabrics deployed in Azure
-  AZUREEI = 'AZUREEI'
+  CORP = 'CORP'
 }
