eb25774b97aafc1d49e030266a750ca0383656a2
Mars Lan <mars.th.lan@gmail.com>
2020-04-02 07:19:52 -0700

    fix: elasticsearch not starting on Mac (#1621)
    The `chown 1000:1000` trick mentioned in https://discuss.elastic.co/t/elastic-elasticsearch-docker-not-assigning-permissions-to-data-directory-on-run/65812/4 only works on Linux. Make the directory world writeable instead so that it works on both Mac & Linux.

--- docker/quickstart/quickstart.sh
+++ docker/quickstart/quickstart.sh
@@ -5,6 +5,6 @@ mkdir -p ${DATA_STORAGE_FOLDER}
 
 # https://discuss.elastic.co/t/elastic-elasticsearch-docker-not-assigning-permissions-to-data-directory-on-run/65812/4
 mkdir -p ${DATA_STORAGE_FOLDER}/elasticsearch
-sudo chown 1000:1000 ${DATA_STORAGE_FOLDER}/elasticsearch
+sudo chmod 777 ${DATA_STORAGE_FOLDER}/elasticsearch
 
 docker-compose pull && docker-compose up --build
