2b9c8f768029a27d632bb3be068fcccdc4da65f8
Mars Lan <mars.th.lan@gmail.com>
2020-05-12 09:32:05 -0700

    fix: use semantic instead of literal comparison in DefaultEqualityTester (#1665)
    This fixes https://github.com/linkedin/datahub/issues/1663
    Also change the signature of EqualityTester to compare only DataTemplates

--- metadata-dao/src/main/java/com/linkedin/metadata/dao/equality/EqualityTester.java
+++ metadata-dao/src/main/java/com/linkedin/metadata/dao/equality/EqualityTester.java
@@ -1,12 +1,13 @@
 package com.linkedin.metadata.dao.equality;
 
+import com.linkedin.data.template.DataTemplate;
 import javax.annotation.Nonnull;
 
 
 /**
  * An interface for testing equality between two objects of the same type.
  */
-public interface EqualityTester<T> {
+public interface EqualityTester<T extends DataTemplate> {
 
   boolean equals(@Nonnull T o1, @Nonnull T o2);
 }
