62bcee35abff0a75411db40c13f9445a45c55bb5
Liangjun <liajiang@expediagroup.com>
2020-05-20 14:24:23 -0500

    rename to dataprocess entity

--- li-utils/src/main/java/com/linkedin/common/urn/DataProcessUrn.java
+++ li-utils/src/main/java/com/linkedin/common/urn/DataProcessUrn.java
@@ -9,8 +9,8 @@ import java.net.URISyntaxException;
 
 import static com.linkedin.common.urn.UrnUtils.toFabricType;
 
-public class DataJobUrn extends Urn {
-    public static final String ENTITY_TYPE = "dataJob";
+public class DataProcessUrn extends Urn {
+    public static final String ENTITY_TYPE = "dataProcess";
 
     private final String dataJobNameEntity;
 
@@ -20,7 +20,7 @@ public class DataJobUrn extends Urn {
 
     private final FabricType originEntity;
 
-    public DataJobUrn(String orchestrator, String name, FabricType origin) {
+    public DataProcessUrn(String orchestrator, String name, FabricType origin) {
         super(ENTITY_TYPE, String.format(CONTENT_FORMAT, orchestrator, name, origin.name()));
         this.dataJobOrchestrator = orchestrator;
         this.dataJobNameEntity = name;
@@ -39,29 +39,29 @@ public class DataJobUrn extends Urn {
         return originEntity;
     }
 
-    public static DataJobUrn createFromString(String rawUrn) throws URISyntaxException {
+    public static DataProcessUrn createFromString(String rawUrn) throws URISyntaxException {
         String content = new Urn(rawUrn).getContent();
         String[] parts = content.substring(1, content.length() - 1).split(",");
-        return new DataJobUrn(parts[0], parts[1], toFabricType(parts[2]));
+        return new DataProcessUrn(parts[0], parts[1], toFabricType(parts[2]));
     }
 
-    public static DataJobUrn deserialize(String rawUrn) throws URISyntaxException {
+    public static DataProcessUrn deserialize(String rawUrn) throws URISyntaxException {
         return createFromString(rawUrn);
     }
 
     static {
-        Custom.registerCoercer(new DirectCoercer<DataJobUrn>() {
-            public Object coerceInput(DataJobUrn object) throws ClassCastException {
+        Custom.registerCoercer(new DirectCoercer<DataProcessUrn>() {
+            public Object coerceInput(DataProcessUrn object) throws ClassCastException {
                 return object.toString();
             }
 
-            public DataJobUrn coerceOutput(Object object) throws TemplateOutputCastException {
+            public DataProcessUrn coerceOutput(Object object) throws TemplateOutputCastException {
                 try {
-                    return DataJobUrn.createFromString((String) object);
+                    return DataProcessUrn.createFromString((String) object);
                 } catch (URISyntaxException e) {
                     throw new TemplateOutputCastException("Invalid URN syntax: " + e.getMessage(), e);
                 }
             }
-        }, DataJobUrn.class);
+        }, DataProcessUrn.class);
     }
 }
