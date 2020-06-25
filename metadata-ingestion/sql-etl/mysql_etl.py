22de61bcfb8d667203526d628d9a2bdb5dae448b
Mars Lan <mars.th.lan@gmail.com>
2020-06-25 15:28:13 -0700

    feature(ingest): add bigquery ETL script (#1711)
    Also fix minor issues in the common script

--- metadata-ingestion/sql-etl/mysql_etl.py
+++ metadata-ingestion/sql-etl/mysql_etl.py
@@ -1,5 +1,6 @@
 from common import run
 
+# See https://github.com/PyMySQL/PyMySQL for more details
 URL = '' # e.g. mysql+pymysql://username:password@hostname:port
 OPTIONS = {} # e.g. {"encoding": "latin1"}
 PLATFORM = 'mysql'
