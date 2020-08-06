e5a139ac2ff0e7a21562bcfc6927ba418c91ec1c
John Plaisted <jplaisted@linkedin.com>
2020-08-06 12:37:36 -0700

    Use multistage build with buildkit to cut down on Dockerfiles.
    See also https://github.com/docker/cli/issues/1134.

--- docker/dev.sh
+++ docker/dev.sh
@@ -4,13 +4,13 @@
 # YOU MUST BUILD VIA GRADLE BEFORE RUNNING THIS.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && \
-  docker-compose \
+  DOCKER_BUILDKIT=1 docker-compose \
     -f docker-compose.yml \
     -f docker-compose.override.yml \
     -f docker-compose.dev.yml \
     pull \
 && \
-  docker-compose -p datahub \
+  DOCKER_BUILDKIT=1 docker-compose -p datahub \
     -f docker-compose.yml \
     -f docker-compose.override.yml \
     -f docker-compose.dev.yml \
