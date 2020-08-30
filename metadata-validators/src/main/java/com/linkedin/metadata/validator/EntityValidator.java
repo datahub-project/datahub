8ff54480e7077ee82cc9963924454496e09f64b6
Na Zhang <nazhang@linkedin.com>
2020-08-29 22:00:22 -0700

    add new entity union and validation

--- metadata-validators/src/main/java/com/linkedin/metadata/validator/EntityValidator.java
+++ metadata-validators/src/main/java/com/linkedin/metadata/validator/EntityValidator.java
@@ -1,7 +1,9 @@
 package com.linkedin.metadata.validator;
 
 import com.linkedin.data.schema.RecordDataSchema;
+import com.linkedin.data.schema.UnionDataSchema;
 import com.linkedin.data.template.RecordTemplate;
+import com.linkedin.data.template.UnionTemplate;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -20,6 +22,10 @@ public class EntityValidator {
 
   // A cache of validated classes
   private static final Set<Class<? extends RecordTemplate>> VALIDATED = ConcurrentHashMap.newKeySet();
+
+  // A cache of validated classes
+  private static final Set<Class<? extends UnionTemplate>> UNION_VALIDATED = ConcurrentHashMap.newKeySet();
+
 
   private EntityValidator() {
     // Util class
@@ -58,6 +64,30 @@ public class EntityValidator {
 
     validateEntitySchema(ValidationUtils.getRecordSchema(clazz));
     VALIDATED.add(clazz);
+  }
+
+  /**
+   * Similar to {@link #validateEntityUnionSchema(UnionDataSchema, String)} but take a {@link Class} instead and caches results.
+   */
+  public static void validateEntityUnionSchema(@Nonnull Class<? extends UnionTemplate> clazz) {
+    if (UNION_VALIDATED.contains(clazz)) {
+      return;
+    }
+
+    validateEntityUnionSchema(ValidationUtils.getUnionSchema(clazz), clazz.getCanonicalName());
+    UNION_VALIDATED.add(clazz);
+  }
+
+  /**
+   * Validates the union of entity model defined in com.linkedin.metadata.entity.
+   *
+   * @param schema schema for the model
+   */
+  public static void validateEntityUnionSchema(@Nonnull UnionDataSchema schema, @Nonnull String entityClassName) {
+
+    if (!ValidationUtils.isUnionWithOnlyComplexMembers(schema)) {
+      ValidationUtils.invalidSchema("Entity '%s' must be a union containing only record type members", entityClassName);
+    }
   }
 
   /**
