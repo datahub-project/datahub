8d536a54d3d115afa3da9adfc6af956274a62e34
John Plaisted <jplaisted@linkedin.com>
2020-08-31 16:45:38 -0700

    Break dependency of metadata-dao on metadata-models.
    This also breaks a few others transitively.

--- metadata-dao/src/test/java/com/linkedin/metadata/dao/BaseLocalDAOTest.java
+++ metadata-dao/src/test/java/com/linkedin/metadata/dao/BaseLocalDAOTest.java
@@ -25,8 +25,7 @@ import org.mockito.stubbing.OngoingStubbing;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static com.linkedin.metadata.testing.AuditStamps.*;
-import static com.linkedin.metadata.utils.TestUtils.*;
+import static com.linkedin.metadata.utils.AuditStamps.*;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
