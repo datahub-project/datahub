9bf35835e0313f470b4b86483de1dc80f16e09ee
John Plaisted <jplaisted@linkedin.com>
2020-09-25 10:27:43 -0700

    Add new spring factories to customize metadata event topic names.
    New env vars:
    
    - METADATA_CHANGE_EVENT_NAME: The name of the metadata change event topic.
    - METADATA_AUDIT_EVENT_NAME: The name of the metadata audit event topic.
    - FAILED_METADATA_CHANGE_EVENT_NAME: The name of the failed metadata change event topic.
    
    This will need to be consistent throughout your ecosystem.
    
    CLOSES: #1840

--- gms/factories/src/main/java/com/linkedin/dataprocess/factory/DataProcessDAOFactory.java
+++ gms/factories/src/main/java/com/linkedin/dataprocess/factory/DataProcessDAOFactory.java
@@ -1,11 +1,13 @@
 package com.linkedin.dataprocess.factory;
 
+import com.linkedin.common.factory.TopicConventionFactory;
 import com.linkedin.common.urn.DataProcessUrn;
 import com.linkedin.metadata.aspect.DataProcessAspect;
 import com.linkedin.metadata.dao.EbeanLocalDAO;
 import com.linkedin.metadata.dao.producer.KafkaMetadataEventProducer;
 import com.linkedin.metadata.dao.producer.KafkaProducerCallback;
 import com.linkedin.metadata.snapshot.DataProcessSnapshot;
+import com.linkedin.mxe.TopicConvention;
 import io.ebean.config.ServerConfig;
 import org.apache.kafka.clients.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,11 +25,12 @@ public class DataProcessDAOFactory {
   ApplicationContext applicationContext;
 
   @Bean(name = "dataProcessDAO")
-  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer"})
+  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer", TopicConventionFactory.TOPIC_CONVENTION_BEAN})
   protected EbeanLocalDAO<DataProcessAspect, DataProcessUrn> createInstance() {
     KafkaMetadataEventProducer<DataProcessSnapshot, DataProcessAspect, DataProcessUrn> producer =
         new KafkaMetadataEventProducer(DataProcessSnapshot.class, DataProcessAspect.class,
-            applicationContext.getBean(Producer.class), new KafkaProducerCallback());
+            applicationContext.getBean(Producer.class), applicationContext.getBean(TopicConvention.class),
+            new KafkaProducerCallback());
 
     return new EbeanLocalDAO<>(DataProcessAspect.class, producer, applicationContext.getBean(ServerConfig.class),
         DataProcessUrn.class);
