cbc35faf747fb9ffa48481d369ece2c23649461b
John Plaisted <jplaisted@linkedin.com>
2020-09-29 13:59:34 -0700

    Add new spring factories to customize metadata event topic names. (#1894)
    New env vars:
    
    - METADATA_CHANGE_EVENT_NAME: The name of the metadata change event topic.
    - METADATA_AUDIT_EVENT_NAME: The name of the metadata audit event topic.
    - FAILED_METADATA_CHANGE_EVENT_NAME: The name of the failed metadata change event topic.
    
    This will need to be consistent throughout your ecosystem.
    
    CLOSES: #1840

--- gms/factories/src/main/java/com/linkedin/dataset/factory/DatasetDaoFactory.java
+++ gms/factories/src/main/java/com/linkedin/dataset/factory/DatasetDaoFactory.java
@@ -1,11 +1,13 @@
 package com.linkedin.dataset.factory;
 
+import com.linkedin.common.factory.TopicConventionFactory;
 import com.linkedin.common.urn.DatasetUrn;
 import com.linkedin.metadata.aspect.DatasetAspect;
 import com.linkedin.metadata.dao.EbeanLocalDAO;
 import com.linkedin.metadata.dao.producer.KafkaMetadataEventProducer;
 import com.linkedin.metadata.dao.producer.KafkaProducerCallback;
 import com.linkedin.metadata.snapshot.DatasetSnapshot;
+import com.linkedin.mxe.TopicConvention;
 import io.ebean.config.ServerConfig;
 import org.apache.kafka.clients.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,12 @@ public class DatasetDaoFactory {
   ApplicationContext applicationContext;
 
   @Bean(name = "datasetDao")
-  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer"})
+  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer", TopicConventionFactory.TOPIC_CONVENTION_BEAN})
   protected EbeanLocalDAO<DatasetAspect, DatasetUrn> createInstance() {
     KafkaMetadataEventProducer<DatasetSnapshot, DatasetAspect, DatasetUrn> producer =
         new KafkaMetadataEventProducer(DatasetSnapshot.class, DatasetAspect.class,
-            applicationContext.getBean(Producer.class), new KafkaProducerCallback());
+            applicationContext.getBean(Producer.class), applicationContext.getBean(TopicConvention.class),
+            new KafkaProducerCallback());
 
     return new EbeanLocalDAO<>(DatasetAspect.class, producer, applicationContext.getBean(ServerConfig.class),
         DatasetUrn.class);
--- gms/factories/src/main/java/com/linkedin/identity/factory/CorpUserDaoFactory.java
+++ gms/factories/src/main/java/com/linkedin/identity/factory/CorpUserDaoFactory.java
@@ -1,10 +1,12 @@
 package com.linkedin.identity.factory;
 
+import com.linkedin.common.factory.TopicConventionFactory;
 import com.linkedin.common.urn.CorpuserUrn;
 import com.linkedin.metadata.aspect.CorpUserAspect;
 import com.linkedin.metadata.dao.EbeanLocalDAO;
 import com.linkedin.metadata.dao.producer.KafkaMetadataEventProducer;
 import com.linkedin.metadata.snapshot.CorpUserSnapshot;
+import com.linkedin.mxe.TopicConvention;
 import io.ebean.config.ServerConfig;
 import org.apache.kafka.clients.producer.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,12 +24,12 @@ public class CorpUserDaoFactory {
   ApplicationContext applicationContext;
 
   @Bean(name = "corpUserDao")
-  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer"})
+  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer", TopicConventionFactory.TOPIC_CONVENTION_BEAN})
   @Nonnull
   protected EbeanLocalDAO createInstance() {
     KafkaMetadataEventProducer<CorpUserSnapshot, CorpUserAspect, CorpuserUrn> producer =
         new KafkaMetadataEventProducer(CorpUserSnapshot.class, CorpUserAspect.class,
-            applicationContext.getBean(Producer.class));
+            applicationContext.getBean(Producer.class), applicationContext.getBean(TopicConvention.class));
     return new EbeanLocalDAO<>(CorpUserAspect.class, producer, applicationContext.getBean(ServerConfig.class),
         CorpuserUrn.class);
   }
