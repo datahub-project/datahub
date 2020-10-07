d8fde5a580b7f3e340e089ccd2294154c1f9b68f
John Plaisted <jplaisted@linkedin.com>
2020-10-06 21:59:14 -0700

    Move code to linkedin/datahub-gma.
    "GMA" (Generalized Metadata Architecture) is the backend of DataHub, and has been moved to its own repository.
    
    This deletes the code that was moved and uses jars that GMA publishes to bintray to load it.
    
    Note that not all of GMA was moved, but most of it. We may still move more things to the other repository in the future.

--- metadata-testing/metadata-test-models/src/main/java/com/linkedin/testing/urn/FooUrnCoercer.java
+++
@@ -1,8 +0,0 @@
-package com.linkedin.testing.urn;
-
-import com.linkedin.data.template.Custom;
-
-
-public class FooUrnCoercer extends BaseUrnCoercer<FooUrn> {
-  private static final boolean REGISTER_COERCER = Custom.registerCoercer(new FooUrnCoercer(), FooUrn.class);
-}
