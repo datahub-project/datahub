059def7e65bc21257b44f9955207097c9991c688
RyanHolstien <rholstien@expediagroup.com>
2020-10-14 13:03:28 -0500

    fixes for IndexBuilder per review comments

--- metadata-builders/src/main/java/com/linkedin/metadata/builders/search/MLModelIndexBuilder.java
+++ metadata-builders/src/main/java/com/linkedin/metadata/builders/search/MLModelIndexBuilder.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
 import com.linkedin.common.DatasetUrnArray;
-import com.linkedin.common.InstitutionalMemory;
+import com.linkedin.common.Deprecation;
 import com.linkedin.common.Ownership;
 import com.linkedin.common.Status;
 import com.linkedin.common.urn.MLModelUrn;
@@ -17,14 +17,8 @@ import com.linkedin.data.template.StringArray;
 import com.linkedin.metadata.search.MLModelDocument;
 import com.linkedin.metadata.snapshot.MLModelSnapshot;
 import com.linkedin.ml.metadata.BaseData;
-import com.linkedin.ml.metadata.CaveatsAndRecommendations;
-import com.linkedin.ml.metadata.EthicalConsiderations;
 import com.linkedin.ml.metadata.EvaluationData;
-import com.linkedin.ml.metadata.IntendedUse;
-import com.linkedin.ml.metadata.MLModelFactorPrompts;
 import com.linkedin.ml.metadata.MLModelProperties;
-import com.linkedin.ml.metadata.Metrics;
-import com.linkedin.ml.metadata.QuantitativeAnalyses;
 import com.linkedin.ml.metadata.TrainingData;
 
 
@@ -55,51 +49,34 @@ public class MLModelIndexBuilder extends BaseIndexBuilder<MLModelDocument> {
             .setName(urn.getMlModelNameEntity())
             .setOrigin(urn.getOriginEntity())
             .setPlatform(urn.getPlatformEntity().getPlatformNameEntity())
-            .setUrn(urn)
-            .setBrowsePaths(new StringArray(Collections.singletonList(buildBrowsePath(urn))));
+            .setUrn(urn);
     }
 
     @Nonnull
     private List<MLModelDocument> getDocumentsToUpdateFromSnapshotType(@Nonnull MLModelSnapshot mlModelSnapshot) {
         final MLModelUrn urn = mlModelSnapshot.getUrn();
         return mlModelSnapshot.getAspects().stream().map(aspect -> {
-            if (aspect.isCaveatsAndRecommendations()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getCaveatsAndRecommendations());
-            } else if (aspect.isEthicalConsiderations()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getEthicalConsiderations());
+            if (aspect.isDeprecation()) {
+                return getDocumentToUpdateFromAspect(urn, aspect.getDeprecation());
             } else if (aspect.isEvaluationData()) {
                 return getDocumentToUpdateFromAspect(urn, aspect.getEvaluationData());
-            } else if (aspect.isInstitutionalMemory()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getInstitutionalMemory());
-            } else if (aspect.isIntendedUse()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getIntendedUse());
-            } else if (aspect.isMetrics()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getMetrics());
-            } else if (aspect.isMLModelFactorPrompts()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getMLModelFactorPrompts());
             } else if (aspect.isMLModelProperties()) {
                 return getDocumentToUpdateFromAspect(urn, aspect.getMLModelProperties());
             } else if (aspect.isOwnership()) {
                 return getDocumentToUpdateFromAspect(urn, aspect.getOwnership());
-            } else if (aspect.isQuantitativeAnalyses()) {
-                return getDocumentToUpdateFromAspect(urn, aspect.getQuantitativeAnalyses());
             } else if (aspect.isStatus()) {
                 return getDocumentToUpdateFromAspect(urn, aspect.getStatus());
             } else if (aspect.isTrainingData()) {
                 return getDocumentToUpdateFromAspect(urn, aspect.getTrainingData());
             }
-            return null;
-        }).filter(Objects::nonNull).collect(Collectors.toList());
+            return setUrnDerivedFields(urn);
+        }).collect(Collectors.toList());
     }
 
     @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(MLModelUrn urn, CaveatsAndRecommendations caveatsAndRecommendations) {
-        return setUrnDerivedFields(urn);
-    }
-
-    @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(MLModelUrn urn, EthicalConsiderations ethicalConsiderations) {
-        return setUrnDerivedFields(urn);
+    private MLModelDocument getDocumentToUpdateFromAspect(MLModelUrn urn, Deprecation deprecation) {
+        return setUrnDerivedFields(urn)
+            .setActive(!deprecation.isDeprecated());
     }
 
     @Nonnull
@@ -107,33 +84,13 @@ public class MLModelIndexBuilder extends BaseIndexBuilder<MLModelDocument> {
         final MLModelDocument doc = setUrnDerivedFields(urn);
 
         if (evaluationData.hasEvaluationData()) {
-            DatasetUrnArray datasetUrns = evaluationData.getEvaluationData()
+            final DatasetUrnArray datasetUrns = evaluationData.getEvaluationData()
                 .stream()
                 .map(BaseData::getDataset)
                 .collect(Collectors.toCollection(DatasetUrnArray::new));
             doc.setEvaluationDatasets(datasetUrns);
         }
         return doc;
-    }
-
-    @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(MLModelUrn urn, InstitutionalMemory institutionalMemory) {
-        return setUrnDerivedFields(urn);
-    }
-
-    @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(@Nonnull MLModelUrn urn, @Nonnull IntendedUse intendedUse) {
-        return setUrnDerivedFields(urn);
-    }
-
-    @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(@Nonnull MLModelUrn urn, @Nonnull Metrics metrics) {
-        return setUrnDerivedFields(urn);
-    }
-
-    @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(@Nonnull MLModelUrn urn, @Nonnull MLModelFactorPrompts modelFactorPrompts) {
-        return setUrnDerivedFields(urn);
     }
 
     @Nonnull
@@ -148,7 +105,6 @@ public class MLModelIndexBuilder extends BaseIndexBuilder<MLModelDocument> {
             doc.setDescription(mlModelProperties.getDescription());
         }
 
-
         return doc;
     }
 
@@ -161,15 +117,9 @@ public class MLModelIndexBuilder extends BaseIndexBuilder<MLModelDocument> {
     }
 
     @Nonnull
-    private MLModelDocument getDocumentToUpdateFromAspect(@Nonnull MLModelUrn urn, @Nonnull QuantitativeAnalyses quantitativeAnalyses) {
-        return setUrnDerivedFields(urn);
-    }
-
-    @Nonnull
     private MLModelDocument getDocumentToUpdateFromAspect(@Nonnull MLModelUrn urn, @Nonnull Status status) {
         return setUrnDerivedFields(urn)
-            .setRemoved(status.isRemoved())
-            .setActive(status.isRemoved());
+            .setRemoved(status.isRemoved());
     }
 
     @Nonnull
@@ -177,11 +127,11 @@ public class MLModelIndexBuilder extends BaseIndexBuilder<MLModelDocument> {
         final MLModelDocument doc = setUrnDerivedFields(urn);
 
         if (trainingData.hasTrainingData()) {
-            DatasetUrnArray datasetUrns = trainingData.getTrainingData()
+            final DatasetUrnArray datasetUrns = trainingData.getTrainingData()
                 .stream()
                 .map(BaseData::getDataset)
                 .collect(Collectors.toCollection(DatasetUrnArray::new));
-            doc.setEvaluationDatasets(datasetUrns);
+            doc.setTrainingDatasets(datasetUrns);
         }
 
         return doc;
