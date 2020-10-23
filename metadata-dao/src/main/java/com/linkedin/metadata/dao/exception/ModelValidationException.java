9d5efc923e13bd07e012f6aee33949cdfe74e5ba
John Plaisted <jplaisted@linkedin.com>
2020-10-23 15:14:57 -0700

    refactor: move code to linkedin/datahub-gma. (#1955)
    Move code to linkedin/datahub-gma.
    
    "GMA" (Generalized Metadata Architecture) is the backend of DataHub, and has been moved to its own repository.
    
    This deletes the code that was moved and uses jars that GMA publishes to bintray to load it.
    
    Note that not all of GMA was moved, but most of it. We may still move more things to the other repository in the future.

--- metadata-dao/src/main/java/com/linkedin/metadata/dao/exception/ModelValidationException.java
+++
@@ -1,8 +0,0 @@
-package com.linkedin.metadata.dao.exception;
-
-public class ModelValidationException extends RuntimeException {
-
-  public ModelValidationException(String message) {
-    super(message);
-  }
-}
--- metadata-dao/src/main/java/com/linkedin/metadata/dao/retention/Retention.java
+++
@@ -1,7 +0,0 @@
-package com.linkedin.metadata.dao.retention;
-
-/**
- * An interface for all metadata aspect retention policies.
- */
-public interface Retention {
-}
