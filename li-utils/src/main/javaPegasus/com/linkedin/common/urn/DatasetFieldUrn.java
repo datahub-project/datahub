7662ddfdae9e519b61b34622985afd00f099fa92
John Plaisted <jplaisted@linkedin.com>
2020-11-11 16:54:05 -0800

    fix: fix build definition of DatasetFieldUrn (#1991)

--- li-utils/src/main/javaPegasus/com/linkedin/common/urn/DatasetFieldUrn.java
+++ li-utils/src/main/javaPegasus/com/linkedin/common/urn/DatasetFieldUrn.java
@@ -17,10 +17,6 @@ public class DatasetFieldUrn extends Urn {
   // uniquely identifies urn's key type
   public static final String ENTITY_TYPE = "datasetField";
 
-  // urn pattern
-  private static final Pattern DATASET_FIELD_URN_PATTERN = Pattern.compile(
-      "urn:li:datasetField:\\(urn:li:dataset:\\(urn:li:dataPlatform:(?<dataPlatform>.+),(?<datasetName>.+),(?<fabric>.+)\\),(?<fieldPath>.+)\\)");
-
   /**
    * Dataset urn of the datasetFieldUrn
    */
@@ -44,9 +40,9 @@ public class DatasetFieldUrn extends Urn {
       public DatasetFieldUrn coerceOutput(Object object) throws TemplateOutputCastException {
         if (object instanceof String) {
           try {
-            return DatasetFieldUrn.deserialize(((String) object));
+            return DatasetFieldUrn.createFromString((String) object);
           } catch (URISyntaxException e) {
-            throw new TemplateOutputCastException((("Deserializing output '" + object) + "' failed"), e);
+            throw new TemplateOutputCastException("Invalid URN syntax: " + e.getMessage(), e);
           }
         }
         throw new TemplateOutputCastException((("Output '" + object) + ("' is not a String, and cannot be coerced to "
@@ -55,22 +51,47 @@ public class DatasetFieldUrn extends Urn {
     }, DatasetFieldUrn.class);
   }
 
-  /**
-   * Creates a new instance of a {@link DatasetFieldUrn }.
-   *
-   * @param dataset Dataset that this dataset field belongs to.
-   * @param fieldPath Dataset field path or column name
-   */
-  public DatasetFieldUrn(DatasetUrn dataset, String fieldPath) {
-    this(dataset.getPlatformEntity().getPlatformNameEntity(), dataset.getDatasetNameEntity(), dataset.getOriginEntity(),
-        fieldPath);
+  public DatasetFieldUrn(String dataPlatform, String datasetName, FabricType fabricType, String fieldPath) {
+    this(new DatasetUrn(new DataPlatformUrn(dataPlatform), datasetName, fabricType), fieldPath);
   }
 
-  public DatasetFieldUrn(String dataPlatform, String datasetName, FabricType fabricType, String fieldPath) {
-    super(ENTITY_TYPE, String.format("(urn:li:dataset:(urn:li:dataPlatform:%s,%s,%s),%s)", dataPlatform, datasetName,
-        fabricType.name(), fieldPath));
-    this._dataset = new DatasetUrn(new DataPlatformUrn(dataPlatform), datasetName, fabricType);
+  /**
+   * Creates a new instance of a {@link DatasetFieldUrn}.
+   *
+   * @param dataset dataset that this dataset field belongs to
+   * @param fieldPath dataset field path or column name
+   */
+  public DatasetFieldUrn(DatasetUrn dataset, String fieldPath) {
+    super(ENTITY_TYPE, TupleKey.create(dataset, fieldPath));
+    this._dataset = dataset;
     this._fieldPath = fieldPath;
+  }
+
+  public static DatasetFieldUrn createFromString(String rawUrn) throws URISyntaxException {
+    return createFromUrn(Urn.createFromString(rawUrn));
+  }
+
+  public static DatasetFieldUrn deserialize(String rawUrn) throws URISyntaxException {
+    return createFromString(rawUrn);
+  }
+
+  public static DatasetFieldUrn createFromUrn(Urn urn) throws URISyntaxException {
+    if (!"li".equals(urn.getNamespace())) {
+      throw new URISyntaxException(urn.toString(), "Urn namespace type should be 'li'.");
+    } else if (!ENTITY_TYPE.equals(urn.getEntityType())) {
+      throw new URISyntaxException(urn.toString(), "Urn entity type should be 'datasetField'.");
+    } else {
+      TupleKey key = urn.getEntityKey();
+      if (key.size() != 2) {
+        throw new URISyntaxException(urn.toString(), "Invalid number of keys.");
+      } else {
+        try {
+          return new DatasetFieldUrn((DatasetUrn) key.getAs(0, DatasetUrn.class), (String) key.getAs(1, String.class));
+        } catch (Exception var3) {
+          throw new URISyntaxException(urn.toString(), "Invalid URN Parameter: '" + var3.getMessage());
+        }
+      }
+    }
   }
 
   public DatasetUrn getDatasetEntity() {
@@ -79,23 +100,5 @@ public class DatasetFieldUrn extends Urn {
 
   public String getFieldPathEntity() {
     return _fieldPath;
-  }
-
-  /**
-   * Creates an instance of a DatasetFieldUrn from a raw urn string.
-   * @param rawUrn The raw urn input to convert to a full DatasetFieldUrn instance.
-   * @return {@link DatasetFieldUrn} dataset Field Urn
-   */
-  public static DatasetFieldUrn deserialize(String rawUrn) throws URISyntaxException {
-    final Matcher matcher = DATASET_FIELD_URN_PATTERN.matcher(rawUrn);
-    if (matcher.matches()) {
-      final String dataPlatform = matcher.group("dataPlatform");
-      final String datasetName = matcher.group("datasetName");
-      final String fabric = matcher.group("fabric");
-      final String fieldName = matcher.group("fieldPath");
-      return new DatasetFieldUrn(dataPlatform, datasetName, FabricType.valueOf(fabric), fieldName);
-    }
-    throw new URISyntaxException(rawUrn,
-        String.format("urn does match dataset field urn pattern %s", DATASET_FIELD_URN_PATTERN.toString()));
   }
 }
