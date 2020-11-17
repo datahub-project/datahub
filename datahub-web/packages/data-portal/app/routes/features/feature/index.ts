a67b26c8964c4a5af7b8821f4c3a33f61de1c2ca
Ignacio Bona <ibonapiedrabuena@linkedin.com>
2020-11-16 16:00:28 -0800

    OS build improvements (1minute) {COMMIT-SYNC:01040678c188091b7f5ccea77e1ec8282d4be142}

--- datahub-web/packages/data-portal/app/routes/features/feature/index.ts
+++ datahub-web/packages/data-portal/app/routes/features/feature/index.ts
@@ -1,9 +1,3 @@
 import Route from '@ember/routing/route';
 
-/**
- * Index route for a specific feature should transition to a default tab for Features
- * @export
- * @class FeaturesFeatureIndex
- * @extends {Route}
- */
-export default class FeaturesFeatureIndex extends Route { }
+export default class FeaturesFeatureIndex extends Route {}
