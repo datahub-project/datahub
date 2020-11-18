7cb62c673181d3ae3fe2e45dbe5a6461f55c89db
John Plaisted <jplaisted@linkedin.com>
2020-11-18 12:52:54 -0800

    Bump GMA to latest
    Involves fixing some types. Bright side is that ModelValidation actually checks aspects now (it did not before because aspects are not records but unions!).

--- metadata-models/src/test/java/com/linkedin/metadata/ModelValidation.java
+++ metadata-models/src/test/java/com/linkedin/metadata/ModelValidation.java
@@ -2,6 +2,7 @@ package com.linkedin.metadata;
 
 import com.google.common.reflect.ClassPath;
 import com.linkedin.data.template.RecordTemplate;
+import com.linkedin.data.template.UnionTemplate;
 import com.linkedin.metadata.validator.AspectValidator;
 import com.linkedin.metadata.validator.DeltaValidator;
 import com.linkedin.metadata.validator.DocumentValidator;
@@ -9,61 +10,92 @@ import com.linkedin.metadata.validator.EntityValidator;
 import com.linkedin.metadata.validator.RelationshipValidator;
 import com.linkedin.metadata.validator.SnapshotValidator;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import org.testng.annotations.Test;
 
 import static com.linkedin.metadata.ModelValidationConstants.*;
+import static org.testng.AssertJUnit.*;
+
 
 public class ModelValidation {
 
   @Test
   public void validateEntities() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.entity", IGNORED_ENTITY_CLASSES).forEach(
-        EntityValidator::validateEntitySchema);
+    List<? extends Class<? extends RecordTemplate>> entities =
+        getRecordTemplatesInPackage("com.linkedin.metadata.entity", IGNORED_ENTITY_CLASSES);
+
+    assertFalse("Failed to find any entities", entities.isEmpty());
+    entities.forEach(EntityValidator::validateEntitySchema);
   }
 
   @Test
   public void validateRelationships() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.relationship", IGNORED_RELATIONSHIP_CLASSES).forEach(
-        RelationshipValidator::validateRelationshipSchema);
+    List<? extends Class<? extends RecordTemplate>> relationships =
+        getRecordTemplatesInPackage("com.linkedin.metadata.relationship", IGNORED_RELATIONSHIP_CLASSES);
+
+    assertFalse("Failed to find any relationships", relationships.isEmpty());
+    relationships.forEach(RelationshipValidator::validateRelationshipSchema);
   }
 
   @Test
   public void validateDocuments() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.search", IGNORED_DOCUMENT_CLASSES).forEach(
-        DocumentValidator::validateDocumentSchema);
+    List<? extends Class<? extends RecordTemplate>> documents =
+        getRecordTemplatesInPackage("com.linkedin.metadata.search", IGNORED_DOCUMENT_CLASSES);
+
+    assertFalse("Failed to find any relationships", documents.isEmpty());
+    documents.forEach(DocumentValidator::validateDocumentSchema);
   }
 
   @Test
   public void validateAspects() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.aspect", IGNORED_ASPECT_CLASSES).forEach(
-        AspectValidator::validateAspectUnionSchema);
+    List<? extends Class<? extends UnionTemplate>> aspects =
+        getUnionTemplatesInPackage("com.linkedin.metadata.aspect", IGNORED_ASPECT_CLASSES);
+
+    assertFalse("Failed to find any aspects", aspects.isEmpty());
+    aspects.forEach(AspectValidator::validateAspectUnionSchema);
   }
 
   @Test
   public void validateSnapshots() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.snapshot", IGNORED_SNAPSHOT_CLASSES).forEach(
-        SnapshotValidator::validateSnapshotSchema);
+    List<? extends Class<? extends RecordTemplate>> snapshots =
+        getRecordTemplatesInPackage("com.linkedin.metadata.snapshot", IGNORED_SNAPSHOT_CLASSES);
 
-    SnapshotValidator.validateUniqueUrn(
-        getClassesInPackage("com.linkedin.metadata.snapshot", IGNORED_SNAPSHOT_CLASSES).collect(Collectors.toList()));
+
+    assertFalse("Failed to find any snapshots", snapshots.isEmpty());
+    snapshots.forEach(SnapshotValidator::validateSnapshotSchema);
+    SnapshotValidator.validateUniqueUrn((Collection<Class<? extends RecordTemplate>>) snapshots);
   }
 
   @Test
   public void validateDeltas() throws Exception {
-    getClassesInPackage("com.linkedin.metadata.delta", IGNORED_DELTA_CLASSES).forEach(DeltaValidator::validateDeltaSchema);
+    getRecordTemplatesInPackage("com.linkedin.metadata.delta", IGNORED_DELTA_CLASSES).forEach(
+        DeltaValidator::validateDeltaSchema);
   }
 
-  private Stream<? extends Class> getClassesInPackage(@Nonnull String packageName, @Nonnull Set<Class> ignoreClasses)
-      throws IOException {
+  private List<? extends Class<? extends UnionTemplate>> getUnionTemplatesInPackage(@Nonnull String packageName,
+      @Nonnull Set<Class<? extends UnionTemplate>> ignoreClasses) throws IOException {
+    return getClassesInPackage(packageName, UnionTemplate.class, ignoreClasses);
+  }
+
+  private List<? extends Class<? extends RecordTemplate>> getRecordTemplatesInPackage(@Nonnull String packageName,
+      @Nonnull Set<Class<? extends RecordTemplate>> ignoreClasses) throws IOException {
+    return getClassesInPackage(packageName, RecordTemplate.class, ignoreClasses);
+  }
+
+  @SuppressWarnings("unchecked")
+  private <T> List<? extends Class<? extends T>> getClassesInPackage(@Nonnull String packageName,
+      @Nonnull Class<T> parentClass, @Nonnull Set<Class<? extends T>> ignoreClasses) throws IOException {
     return ClassPath.from(ClassLoader.getSystemClassLoader())
         .getTopLevelClasses(packageName)
         .stream()
         .map(classInfo -> classInfo.load())
-        .filter(clazz -> RecordTemplate.class.isAssignableFrom(clazz))
-        .filter(clazz -> !ignoreClasses.contains(clazz));
+        .filter(clazz -> parentClass.isAssignableFrom(clazz))
+        .map(x -> (Class<? extends T>) x)
+        .filter(clazz -> !ignoreClasses.contains(clazz))
+        .collect(Collectors.toList());
   }
 }
