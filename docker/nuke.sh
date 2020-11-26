4005d5c07107aea5239693dcc8ae64a61e5d2bd2
Mars Lan <mars@trayminder.com>
2020-11-26 05:08:33 -0800

    Simplfy script & add comments

--- docker/nuke.sh
+++ docker/nuke.sh
@@ -1,28 +1,5 @@
 #!/bin/sh
 
-CONTAINERS="\
-	broker \
-	datahub-frontend \
-	datahub-gms \
-	datahub-mae-consumer \
-	datahub-mce-consumer \
-	elasticsearch \
-	elasticsearch-setup \
-	kafka-rest-proxy \
-	kafka-setup \
-	kafka-topics-ui \
-	kibana \
-	mysql \
-	neo4j \
-	schema-registry \
-	schema-registry-ui \
-	zookeeper \
-"
-
-for CONTAINER in "$CONTAINERS"; do
-	docker rm -f -v $CONTAINER
-done
-
-docker volume rm -f $(docker volume ls -f name=datahub_  -q)
-
-docker network rm datahub_network
+# Tear down and clean up all DataHub-related containers, volumes, and network
+docker-compose -p datahub down -v
+docker system prune -a -f
