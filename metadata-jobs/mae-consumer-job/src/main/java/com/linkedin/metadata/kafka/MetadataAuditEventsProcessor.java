f103998dcb1ade1a0b21fce4b1cbcb6bb17a37e9
Nagarjuna Kanamarlapudi <nkanamarlapudi@linkedin.com>
2020-12-03 11:43:48 -0800

    feat(Search): Inject restli client into index builders (#2024)
    * feat(Search): Inject restli client into index builders
    Inject restli client into index builders

--- metadata-jobs/mae-consumer-job/src/main/java/com/linkedin/metadata/kafka/MetadataAuditEventsProcessor.java
+++ metadata-jobs/mae-consumer-job/src/main/java/com/linkedin/metadata/kafka/MetadataAuditEventsProcessor.java
@@ -1,18 +1,5 @@
 package com.linkedin.metadata.kafka;
 
-import java.io.UnsupportedEncodingException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.stereotype.Component;
-import org.springframework.kafka.annotation.KafkaListener;
-
 import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.EventUtils;
@@ -20,7 +7,6 @@ import com.linkedin.metadata.builders.graph.BaseGraphBuilder;
 import com.linkedin.metadata.builders.graph.GraphBuilder;
 import com.linkedin.metadata.builders.graph.RegisteredGraphBuilders;
 import com.linkedin.metadata.builders.search.BaseIndexBuilder;
-import com.linkedin.metadata.builders.search.RegisteredIndexBuilders;
 import com.linkedin.metadata.builders.search.SnapshotProcessor;
 import com.linkedin.metadata.dao.internal.BaseGraphWriterDAO;
 import com.linkedin.metadata.dao.utils.RecordUtils;
@@ -28,10 +14,20 @@ import com.linkedin.metadata.snapshot.Snapshot;
 import com.linkedin.metadata.utils.elasticsearch.ElasticsearchConnector;
 import com.linkedin.metadata.utils.elasticsearch.MCEElasticEvent;
 import com.linkedin.mxe.MetadataAuditEvent;
-
-import lombok.extern.slf4j.Slf4j;
-
 import com.linkedin.mxe.Topics;
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 
 @Slf4j
@@ -44,12 +40,16 @@ public class MetadataAuditEventsProcessor {
   private ElasticsearchConnector elasticSearchConnector;
   private SnapshotProcessor snapshotProcessor;
   private BaseGraphWriterDAO graphWriterDAO;
+  private Set<BaseIndexBuilder<? extends RecordTemplate>> indexBuilders;
 
   public MetadataAuditEventsProcessor(ElasticsearchConnector elasticSearchConnector,
-      SnapshotProcessor snapshotProcessor, BaseGraphWriterDAO graphWriterDAO) {
+      SnapshotProcessor snapshotProcessor, BaseGraphWriterDAO graphWriterDAO,
+      Set<BaseIndexBuilder<? extends RecordTemplate>> indexBuilders) {
     this.elasticSearchConnector = elasticSearchConnector;
     this.snapshotProcessor = snapshotProcessor;
     this.graphWriterDAO = graphWriterDAO;
+    this.indexBuilders = indexBuilders;
+    log.info("registered index builders {}", indexBuilders);
   }
 
   @KafkaListener(id = "mae-consumer-job-client", topics = "${KAFKA_TOPIC_NAME:" + Topics.METADATA_AUDIT_EVENT + "}")
@@ -111,7 +111,7 @@ public class MetadataAuditEventsProcessor {
     for (RecordTemplate doc : docs) {
       MCEElasticEvent elasticEvent = new MCEElasticEvent(doc);
       BaseIndexBuilder indexBuilderForDoc = null;
-      for (BaseIndexBuilder indexBuilder : RegisteredIndexBuilders.REGISTERED_INDEX_BUILDERS) {
+      for (BaseIndexBuilder indexBuilder : indexBuilders) {
         Class docType = indexBuilder.getDocumentType();
         if (docType.isInstance(doc)) {
           indexBuilderForDoc = indexBuilder;
---
+++ metadata-jobs/mae-consumer-job/src/main/java/com/linkedin/metadata/kafka/config/IndexBuildersConfig.java
@@ -0,0 +1,59 @@
+package com.linkedin.metadata.kafka.config;
+
+import com.linkedin.data.template.RecordTemplate;
+import com.linkedin.metadata.builders.search.BaseIndexBuilder;
+import com.linkedin.metadata.builders.search.ChartIndexBuilder;
+import com.linkedin.metadata.builders.search.CorpGroupIndexBuilder;
+import com.linkedin.metadata.builders.search.DashboardIndexBuilder;
+import com.linkedin.metadata.builders.search.DataProcessIndexBuilder;
+import com.linkedin.metadata.builders.search.DatasetIndexBuilder;
+import com.linkedin.metadata.restli.DefaultRestliClientFactory;
+import com.linkedin.restli.client.Client;
+import java.util.HashSet;
+import java.util.Set;
+import javax.annotation.Nonnull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Configurations for search index builders
+ */
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class IndexBuildersConfig {
+
+  @Value("${GMS_HOST:localhost}")
+  private String gmsHost;
+  @Value("${GMS_PORT:8080}")
+  private int gmsPort;
+
+  /**
+   * Registered index builders powering GMA search
+   *
+   * @param restliClient Rest.li client to interact with GMS
+   */
+  @Bean
+  public Set<BaseIndexBuilder<? extends RecordTemplate>> indexBuilders(@Nonnull Client restliClient) {
+    log.debug("restli client {}", restliClient);
+    final Set<BaseIndexBuilder<? extends RecordTemplate>> builders = new HashSet<>();
+    builders.add(new CorpGroupIndexBuilder());
+    builders.add(new ChartIndexBuilder());
+    builders.add(new DatasetIndexBuilder());
+    builders.add(new DataProcessIndexBuilder());
+    builders.add(new DashboardIndexBuilder());
+    return builders;
+  }
+
+  /**
+   * Rest.li client to interact with GMS
+   */
+  @Bean
+  public Client restliClient() {
+    return DefaultRestliClientFactory.getRestLiClient(gmsHost, gmsPort);
+  }
+}
