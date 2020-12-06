e596456b7074f814ff8d645054a61974fc04a940
Nagarjuna Kanamarlapudi <nkanamarlapudi@linkedin.com>
2020-12-06 08:24:39 -0800

    Inject Restli Client into DatasetIndex Builders to get the delimiter of a data platform

---
+++ metadata-dao-impl/restli-dao/src/main/java/com/linkedin/metadata/dao/DataPlatformActionRequestBuilder.java
@@ -0,0 +1,17 @@
+package com.linkedin.metadata.dao;
+
+import com.linkedin.common.urn.DataPlatformUrn;
+import com.linkedin.metadata.snapshot.DataPlatformSnapshot;
+
+
+/**
+ * An action request builder for corp user info entities.
+ */
+public class DataPlatformActionRequestBuilder extends BaseActionRequestBuilder<DataPlatformSnapshot, DataPlatformUrn> {
+
+  private static final String BASE_URI_TEMPLATE = "dataPlatforms";
+
+  public DataPlatformActionRequestBuilder() {
+    super(DataPlatformSnapshot.class, DataPlatformUrn.class, BASE_URI_TEMPLATE);
+  }
+}
