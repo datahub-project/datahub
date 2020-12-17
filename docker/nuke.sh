74aca045ec78479a1f14e80e80a3b1464b820949
Mars Lan <mars.th.lan@gmail.com>
2020-12-17 13:52:31 -0800

    build(docker): add script to clean up docker environment (#2013)
    Co-authored-by: Mars Lan <mars@trayminder.com>

---
+++ docker/nuke.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+# Tear down and clean up all DataHub-related containers, volumes, and network
+docker-compose -p datahub down -v
+docker-compose rm -f -v
