148c733673eb32381a8c01d78fc575d4dca279b1
Gabe Lyons <itsgabelyons@gmail.com>
2021-02-01 11:50:10 -0800

    refactor(React Incubation): Search routing refactor (#2075)

--- datahub-web-react/src/components/search/SearchFilters.tsx
+++ datahub-web-react/src/components/search/SearchFilters.tsx
@@ -20,18 +20,14 @@ interface Props {
     onFilterSelect: (selected: boolean, field: string, value: string) => void;
 }
 
-export const SearchFilters = ({
-    facets: _facets,
-    selectedFilters: _selectedFilters,
-    onFilterSelect: _onFilterSelect,
-}: Props) => {
+export const SearchFilters = ({ facets, selectedFilters, onFilterSelect }: Props) => {
     return (
         <Card
             style={{ border: '1px solid #d2d2d2' }}
             title={<h3 style={{ marginBottom: '0px' }}>Filters</h3>}
             bodyStyle={{ padding: '24px 0px' }}
         >
-            {_facets.map((facet) => (
+            {facets.map((facet) => (
                 <div style={{ padding: '0px 25px 15px 25px' }}>
                     <div style={{ fontWeight: 'bold', marginBottom: '10px' }}>
                         {facet.field.charAt(0).toUpperCase() + facet.field.slice(1)}
@@ -40,13 +36,13 @@ export const SearchFilters = ({
                         <>
                             <Checkbox
                                 style={{ margin: '5px 0px' }}
-                                defaultChecked={
-                                    _selectedFilters.find(
+                                checked={
+                                    selectedFilters.find(
                                         (f) => f.field === facet.field && f.value === aggregation.value,
                                     ) !== undefined
                                 }
                                 onChange={(e: CheckboxChangeEvent) =>
-                                    _onFilterSelect(e.target.checked, facet.field, aggregation.value)
+                                    onFilterSelect(e.target.checked, facet.field, aggregation.value)
                                 }
                             >
                                 {aggregation.value} ({aggregation.count})
---
+++ datahub-web-react/src/components/search/utils/useFilters.ts
@@ -0,0 +1,27 @@
+import { useMemo } from 'react';
+import * as QueryString from 'query-string';
+
+import { FILTER_URL_PREFIX } from './constants';
+import { FacetFilterInput } from '../../../types.generated';
+
+export default function useFilters(params: QueryString.ParsedQuery<string>): Array<FacetFilterInput> {
+    return useMemo(
+        () =>
+            // get all query params
+            Object.entries(params)
+                // select only the ones with the `filter_` prefix
+                .filter(([key, _]) => key.indexOf(FILTER_URL_PREFIX) >= 0)
+                // transform the filters currently in format [key, [value1, value2]] to [{key: key, value: value1}, { key: key, value: value2}] format that graphql expects
+                .flatMap(([key, value]) => {
+                    // remove the `filter_` prefix
+                    const field = key.replace(FILTER_URL_PREFIX, '');
+                    if (!value) return [];
+
+                    if (Array.isArray(value)) {
+                        return value.map((distinctValue) => ({ field, value: distinctValue }));
+                    }
+                    return [{ field, value }];
+                }),
+        [params],
+    );
+}
