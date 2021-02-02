jjoyce0510/datahub-1

feat(GraphQL Query): Supporting complete Dataset, CorpUser with Search & Browse in GraphQL API

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/browse/BrowsePathsResolver.java
@@ -1,0 +1,44 @@
+package com.linkedin.datahub.graphql.resolvers.browse;
+
+import com.linkedin.datahub.graphql.types.BrowsableEntityType;
+import com.linkedin.datahub.graphql.generated.BrowsePath;
+import com.linkedin.datahub.graphql.generated.BrowsePathsInput;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+
+public class BrowsePathsResolver implements DataFetcher<CompletableFuture<List<BrowsePath>>> {
+
+    private final Map<EntityType, BrowsableEntityType<?>> _typeToEntity;
+
+    public BrowsePathsResolver(@Nonnull final List<BrowsableEntityType<?>> browsableEntities) {
+        _typeToEntity = browsableEntities.stream().collect(Collectors.toMap(
+                BrowsableEntityType::entityType,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<List<BrowsePath>> get(DataFetchingEnvironment environment) {
+        final BrowsePathsInput input = bindArgument(environment.getArgument("input"), BrowsePathsInput.class);
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).browsePaths(input.getUrn());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to retrieve browse paths: "
+                        + String.format("entity type %s, urn %s",
+                        input.getType(),
+                        input.getUrn()));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/AutoCompleteResolver.java
@@ -1,0 +1,67 @@
+package com.linkedin.datahub.graphql.resolvers.search;
+
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.exception.ValidationException;
+import com.linkedin.datahub.graphql.generated.AutoCompleteInput;
+import com.linkedin.datahub.graphql.generated.AutoCompleteResults;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.resolvers.ResolverUtils;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+/**
+ * Resolver responsible for resolving the 'autocomplete' field of the Query type
+ */
+public class AutoCompleteResolver implements DataFetcher<CompletableFuture<AutoCompleteResults>> {
+
+    private static final int DEFAULT_LIMIT = 5;
+
+    private final Map<EntityType, SearchableEntityType<?>> _typeToEntity;
+
+    public AutoCompleteResolver(@Nonnull final List<SearchableEntityType<?>> searchableEntities) {
+        _typeToEntity = searchableEntities.stream().collect(Collectors.toMap(
+                SearchableEntityType::entityType,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<AutoCompleteResults> get(DataFetchingEnvironment environment) {
+        final AutoCompleteInput input = bindArgument(environment.getArgument("input"), AutoCompleteInput.class);
+
+        // escape forward slash since it is a reserved character in Elasticsearch
+        final String sanitizedQuery = ResolverUtils.escapeForwardSlash(input.getQuery());
+        if (isBlank(sanitizedQuery)) {
+            throw new ValidationException("'query' parameter can not be null or empty");
+        }
+
+        final int limit = input.getLimit() != null ? input.getLimit() : DEFAULT_LIMIT;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).autoComplete(
+                        sanitizedQuery,
+                        input.getField(),
+                        input.getFilters(),
+                        limit
+                );
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to execute autocomplete: "
+                        + String.format("entity type %s, field %s, query %s, filters: %s, limit: %s",
+                                input.getType(),
+                                input.getField(),
+                                input.getQuery(),
+                                input.getFilters(),
+                                input.getLimit()));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchResolver.java
@@ -1,0 +1,70 @@
+package com.linkedin.datahub.graphql.resolvers.search;
+
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.exception.ValidationException;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.generated.SearchInput;
+import com.linkedin.datahub.graphql.generated.SearchResults;
+import com.linkedin.datahub.graphql.resolvers.ResolverUtils;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+/**
+ * Resolver responsible for resolving the 'search' field of the Query type
+ */
+public class SearchResolver implements DataFetcher<CompletableFuture<SearchResults>> {
+
+    private static final int DEFAULT_START = 0;
+    private static final int DEFAULT_COUNT = 10;
+
+    private final Map<EntityType, SearchableEntityType<?>> _typeToEntity;
+
+    public SearchResolver(@Nonnull final List<SearchableEntityType<?>> searchableEntities) {
+        _typeToEntity = searchableEntities.stream().collect(Collectors.toMap(
+                SearchableEntityType::entityType,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<SearchResults> get(DataFetchingEnvironment environment) {
+        final SearchInput input = bindArgument(environment.getArgument("input"), SearchInput.class);
+
+        // escape forward slash since it is a reserved character in Elasticsearch
+        final String sanitizedQuery = ResolverUtils.escapeForwardSlash(input.getQuery());
+        if (isBlank(sanitizedQuery)) {
+            throw new ValidationException("'query' parameter cannot be null or empty");
+        }
+
+        final int start = input.getStart() != null ? input.getStart() : DEFAULT_START;
+        final int count = input.getCount() != null ? input.getCount() : DEFAULT_COUNT;
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).search(
+                        sanitizedQuery,
+                        input.getFilters(),
+                        start,
+                        count
+                );
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to execute search: "
+                    + String.format("entity type %s, query %s, filters: %s, start: %s, count: %s",
+                        input.getType(),
+                        input.getQuery(),
+                        input.getFilters(),
+                        start,
+                        count));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/EntityType.java
@@ -1,0 +1,17 @@
+package com.linkedin.datahub.graphql.types;
+
+import com.linkedin.datahub.graphql.generated.Entity;
+
+/**
+ * GQL graph type representing a top-level GMS entity (eg. Dataset, User, DataPlatform, Chart, etc.).
+ *
+ * @param <T>: The GraphQL object type corresponding to the entity, must be of type {@link Entity}
+ */
+public interface EntityType<T extends Entity> extends LoadableType<T> {
+
+    /**
+     * Retrieves the {@link com.linkedin.datahub.graphql.generated.EntityType} associated with the Graph type, eg. 'DATASET'
+     */
+    com.linkedin.datahub.graphql.generated.EntityType entityType();
+
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/corpuser/CorpUserType.java
@@ -1,0 +1,93 @@
+package com.linkedin.datahub.graphql.types.corpuser;
+
+import com.linkedin.common.urn.CorpuserUrn;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.generated.AutoCompleteResults;
+import com.linkedin.datahub.graphql.generated.CorpUser;
+import com.linkedin.datahub.graphql.generated.FacetFilterInput;
+import com.linkedin.datahub.graphql.generated.SearchResults;
+import com.linkedin.datahub.graphql.types.mappers.AutoCompleteResultsMapper;
+import com.linkedin.datahub.graphql.types.mappers.CorpUserMapper;
+import com.linkedin.datahub.graphql.types.mappers.SearchResultsMapper;
+import com.linkedin.identity.client.CorpUsers;
+import com.linkedin.metadata.query.AutoCompleteResult;
+import com.linkedin.restli.common.CollectionResponse;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CorpUserType implements SearchableEntityType<CorpUser> {
+
+    private final CorpUsers _corpUsersClient;
+
+    public CorpUserType(final CorpUsers corpUsersClient) {
+        _corpUsersClient = corpUsersClient;
+    }
+
+    @Override
+    public Class<CorpUser> objectClass() {
+        return CorpUser.class;
+    }
+
+    @Override
+    public EntityType entityType() {
+        return EntityType.CORP_USER;
+    }
+
+    @Override
+    public List<CorpUser> batchLoad(List<String> urns) {
+        try {
+            final List<CorpuserUrn> corpUserUrns = urns
+                    .stream()
+                    .map(this::getCorpUserUrn)
+                    .collect(Collectors.toList());
+
+            final Map<CorpuserUrn, com.linkedin.identity.CorpUser> corpUserMap = _corpUsersClient
+                    .batchGet(new HashSet<>(corpUserUrns));
+
+            final List<com.linkedin.identity.CorpUser> results = new ArrayList<>();
+            for (CorpuserUrn urn : corpUserUrns) {
+                results.add(corpUserMap.getOrDefault(urn, null));
+            }
+            return results.stream()
+                    .map(gmsCorpUser -> gmsCorpUser == null ? null : CorpUserMapper.map(gmsCorpUser))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to batch load Datasets", e);
+        }
+    }
+
+    @Override
+    public SearchResults search(@Nonnull String query,
+                                @Nullable List<FacetFilterInput> filters,
+                                int start,
+                                int count) throws Exception {
+        final CollectionResponse<com.linkedin.identity.CorpUser> searchResult = _corpUsersClient.search(query, Collections.emptyMap(), start, count);
+        return SearchResultsMapper.map(searchResult, CorpUserMapper::map);
+    }
+
+    @Override
+    public AutoCompleteResults autoComplete(@Nonnull String query,
+                                            @Nullable String field,
+                                            @Nullable List<FacetFilterInput> filters,
+                                            int limit) throws Exception {
+        final AutoCompleteResult result = _corpUsersClient.autocomplete(query, field, Collections.emptyMap(), limit);
+        return AutoCompleteResultsMapper.map(result);
+    }
+
+    private CorpuserUrn getCorpUserUrn(final String urnStr) {
+        try {
+            return CorpuserUrn.createFromString(urnStr);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(String.format("Failed to retrieve user with urn %s, invalid urn", urnStr));
+        }
+    }
+}

datahub-project/datahub

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/browse/BrowsePathsResolver.java
@@ -1,0 +1,44 @@
+package com.linkedin.datahub.graphql.resolvers.browse;
+
+import com.linkedin.datahub.graphql.types.BrowsableEntityType;
+import com.linkedin.datahub.graphql.generated.BrowsePath;
+import com.linkedin.datahub.graphql.generated.BrowsePathsInput;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+
+public class BrowsePathsResolver implements DataFetcher<CompletableFuture<List<BrowsePath>>> {
+
+    private final Map<EntityType, BrowsableEntityType<?>> _typeToEntity;
+
+    public BrowsePathsResolver(@Nonnull final List<BrowsableEntityType<?>> browsableEntities) {
+        _typeToEntity = browsableEntities.stream().collect(Collectors.toMap(
+                BrowsableEntityType::type,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<List<BrowsePath>> get(DataFetchingEnvironment environment) {
+        final BrowsePathsInput input = bindArgument(environment.getArgument("input"), BrowsePathsInput.class);
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).browsePaths(input.getUrn());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to retrieve browse paths: "
+                        + String.format("entity type %s, urn %s",
+                        input.getType(),
+                        input.getUrn()));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/AutoCompleteResolver.java
@@ -1,0 +1,67 @@
+package com.linkedin.datahub.graphql.resolvers.search;
+
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.exception.ValidationException;
+import com.linkedin.datahub.graphql.generated.AutoCompleteInput;
+import com.linkedin.datahub.graphql.generated.AutoCompleteResults;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.resolvers.ResolverUtils;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+/**
+ * Resolver responsible for resolving the 'autocomplete' field of the Query type
+ */
+public class AutoCompleteResolver implements DataFetcher<CompletableFuture<AutoCompleteResults>> {
+
+    private static final int DEFAULT_LIMIT = 5;
+
+    private final Map<EntityType, SearchableEntityType<?>> _typeToEntity;
+
+    public AutoCompleteResolver(@Nonnull final List<SearchableEntityType<?>> searchableEntities) {
+        _typeToEntity = searchableEntities.stream().collect(Collectors.toMap(
+                SearchableEntityType::type,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<AutoCompleteResults> get(DataFetchingEnvironment environment) {
+        final AutoCompleteInput input = bindArgument(environment.getArgument("input"), AutoCompleteInput.class);
+
+        // escape forward slash since it is a reserved character in Elasticsearch
+        final String sanitizedQuery = ResolverUtils.escapeForwardSlash(input.getQuery());
+        if (isBlank(sanitizedQuery)) {
+            throw new ValidationException("'query' parameter can not be null or empty");
+        }
+
+        final int limit = input.getLimit() != null ? input.getLimit() : DEFAULT_LIMIT;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).autoComplete(
+                        sanitizedQuery,
+                        input.getField(),
+                        input.getFilters(),
+                        limit
+                );
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to execute autocomplete: "
+                        + String.format("entity type %s, field %s, query %s, filters: %s, limit: %s",
+                                input.getType(),
+                                input.getField(),
+                                input.getQuery(),
+                                input.getFilters(),
+                                input.getLimit()));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchResolver.java
@@ -1,0 +1,70 @@
+package com.linkedin.datahub.graphql.resolvers.search;
+
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.exception.ValidationException;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.generated.SearchInput;
+import com.linkedin.datahub.graphql.generated.SearchResults;
+import com.linkedin.datahub.graphql.resolvers.ResolverUtils;
+import graphql.schema.DataFetcher;
+import graphql.schema.DataFetchingEnvironment;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+/**
+ * Resolver responsible for resolving the 'search' field of the Query type
+ */
+public class SearchResolver implements DataFetcher<CompletableFuture<SearchResults>> {
+
+    private static final int DEFAULT_START = 0;
+    private static final int DEFAULT_COUNT = 10;
+
+    private final Map<EntityType, SearchableEntityType<?>> _typeToEntity;
+
+    public SearchResolver(@Nonnull final List<SearchableEntityType<?>> searchableEntities) {
+        _typeToEntity = searchableEntities.stream().collect(Collectors.toMap(
+                SearchableEntityType::type,
+                entity -> entity
+        ));
+    }
+
+    @Override
+    public CompletableFuture<SearchResults> get(DataFetchingEnvironment environment) {
+        final SearchInput input = bindArgument(environment.getArgument("input"), SearchInput.class);
+
+        // escape forward slash since it is a reserved character in Elasticsearch
+        final String sanitizedQuery = ResolverUtils.escapeForwardSlash(input.getQuery());
+        if (isBlank(sanitizedQuery)) {
+            throw new ValidationException("'query' parameter cannot be null or empty");
+        }
+
+        final int start = input.getStart() != null ? input.getStart() : DEFAULT_START;
+        final int count = input.getCount() != null ? input.getCount() : DEFAULT_COUNT;
+
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                return _typeToEntity.get(input.getType()).search(
+                        sanitizedQuery,
+                        input.getFilters(),
+                        start,
+                        count
+                );
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to execute search: "
+                    + String.format("entity type %s, query %s, filters: %s, start: %s, count: %s",
+                        input.getType(),
+                        input.getQuery(),
+                        input.getFilters(),
+                        start,
+                        count));
+            }
+        });
+    }
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/EntityType.java
@@ -1,0 +1,17 @@
+package com.linkedin.datahub.graphql.types;
+
+import com.linkedin.datahub.graphql.generated.Entity;
+
+/**
+ * GQL graph type representing a top-level GMS entity (eg. Dataset, User, DataPlatform, Chart, etc.).
+ *
+ * @param <T>: The GraphQL object type corresponding to the entity, must be of type {@link Entity}
+ */
+public interface EntityType<T extends Entity> extends LoadableType<T> {
+
+    /**
+     * Retrieves the {@link com.linkedin.datahub.graphql.generated.EntityType} associated with the Graph type, eg. 'DATASET'
+     */
+    com.linkedin.datahub.graphql.generated.EntityType type();
+
+}

datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/corpuser/CorpUserType.java
@@ -1,0 +1,96 @@
+package com.linkedin.datahub.graphql.types.corpuser;
+
+import com.linkedin.common.urn.CorpuserUrn;
+import com.linkedin.datahub.graphql.generated.EntityType;
+import com.linkedin.datahub.graphql.types.SearchableEntityType;
+import com.linkedin.datahub.graphql.generated.AutoCompleteResults;
+import com.linkedin.datahub.graphql.generated.CorpUser;
+import com.linkedin.datahub.graphql.generated.FacetFilterInput;
+import com.linkedin.datahub.graphql.generated.SearchResults;
+import com.linkedin.datahub.graphql.types.mappers.AutoCompleteResultsMapper;
+import com.linkedin.datahub.graphql.types.mappers.CorpUserMapper;
+import com.linkedin.datahub.graphql.types.mappers.SearchResultsMapper;
+import com.linkedin.identity.client.CorpUsers;
+import com.linkedin.metadata.query.AutoCompleteResult;
+import com.linkedin.restli.common.CollectionResponse;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CorpUserType implements SearchableEntityType<CorpUser> {
+
+    private static final String DEFAULT_AUTO_COMPLETE_FIELD = "ldap";
+
+    private final CorpUsers _corpUsersClient;
+
+    public CorpUserType(final CorpUsers corpUsersClient) {
+        _corpUsersClient = corpUsersClient;
+    }
+
+    @Override
+    public Class<CorpUser> objectClass() {
+        return CorpUser.class;
+    }
+
+    @Override
+    public EntityType type() {
+        return EntityType.CORP_USER;
+    }
+
+    @Override
+    public List<CorpUser> batchLoad(List<String> urns) {
+        try {
+            final List<CorpuserUrn> corpUserUrns = urns
+                    .stream()
+                    .map(this::getCorpUserUrn)
+                    .collect(Collectors.toList());
+
+            final Map<CorpuserUrn, com.linkedin.identity.CorpUser> corpUserMap = _corpUsersClient
+                    .batchGet(new HashSet<>(corpUserUrns));
+
+            final List<com.linkedin.identity.CorpUser> results = new ArrayList<>();
+            for (CorpuserUrn urn : corpUserUrns) {
+                results.add(corpUserMap.getOrDefault(urn, null));
+            }
+            return results.stream()
+                    .map(gmsCorpUser -> gmsCorpUser == null ? null : CorpUserMapper.map(gmsCorpUser))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to batch load Datasets", e);
+        }
+    }
+
+    @Override
+    public SearchResults search(@Nonnull String query,
+                                @Nullable List<FacetFilterInput> filters,
+                                int start,
+                                int count) throws Exception {
+        final CollectionResponse<com.linkedin.identity.CorpUser> searchResult = _corpUsersClient.search(query, Collections.emptyMap(), start, count);
+        return SearchResultsMapper.map(searchResult, CorpUserMapper::map);
+    }
+
+    @Override
+    public AutoCompleteResults autoComplete(@Nonnull String query,
+                                            @Nullable String field,
+                                            @Nullable List<FacetFilterInput> filters,
+                                            int limit) throws Exception {
+        field = field != null ? field : DEFAULT_AUTO_COMPLETE_FIELD;
+        final AutoCompleteResult result = _corpUsersClient.autocomplete(query, field, Collections.emptyMap(), limit);
+        return AutoCompleteResultsMapper.map(result);
+    }
+
+    private CorpuserUrn getCorpUserUrn(final String urnStr) {
+        try {
+            return CorpuserUrn.createFromString(urnStr);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(String.format("Failed to retrieve user with urn %s, invalid urn", urnStr));
+        }
+    }
+}