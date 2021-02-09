3c3b8e4137f5ea095f48c94cd9a39cc1680ed495
Harshal Sheth <hsheth2@gmail.com>
2021-02-09 15:24:33 -0800

    Add test to check that classes are not abstract

---
+++ metadata-ingestion/tests/unit/test_apis.py
@@ -0,0 +1,12 @@
+import inspect
+from gometa.ingestion.source import source_class_mapping
+from gometa.ingestion.sink import sink_class_mapping
+
+
+def test_sources_not_abstract():
+    for cls in source_class_mapping.values():
+        assert inspect.isabstract(cls)
+
+def test_sinks_not_abstract():
+    for cls in sink_class_mapping.values():
+        assert inspect.isabstract(cls)
