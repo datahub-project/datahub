4e17a24dd402ad1b960f6fc5c2cfff9948b9cfe7
Shirshanka Das <shirshanka@apache.org>
2021-02-09 01:02:05 -0800

    Adding mysql integration test + source

--- metadata-ingestion/src/gometa/ingestion/source/mssql.py
+++ metadata-ingestion/src/gometa/ingestion/source/mssql.py
@@ -1,27 +1,17 @@
-from pydantic import BaseModel
-from typing import Optional
-from .sql_common import SQLAlchemyConfig, get_sql_workunits
-from gometa.ingestion.api.source import Source
+from .sql_common import SQLAlchemySource, SQLAlchemyConfig
 
 class SQLServerConfig(SQLAlchemyConfig):
     #defaults
     host_port = "localhost:1433"
     scheme = "mssql+pytds"
 
-class SQLServerSource(Source):
+class SQLServerSource(SQLAlchemySource):
 
     def __init__(self, config, ctx):
-        super().__init__(ctx)
-        self.config = config
-
+        super().__init__(config, ctx, "mssql")
 
     @classmethod
     def create(cls, config_dict, ctx):
         config = SQLServerConfig.parse_obj(config_dict)
         return cls(config, ctx)
 
-    def get_workunits(self):
-        return get_sql_workunits(self.config, "mssql")
-        
-    def close(self):
-        pass
