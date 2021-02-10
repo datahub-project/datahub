2ccce7d0cd16ca91a5918e2605af6e986686d71f
Harshal Sheth <hsheth2@gmail.com>
2021-02-10 10:50:20 -0800

    Integrate kafka extract into source

--- metadata-ingestion/src/gometa/ingestion/source/kafka.py
+++ metadata-ingestion/src/gometa/ingestion/source/kafka.py
@@ -1,11 +1,25 @@
+import logging
 from gometa.configuration import ConfigModel, KafkaConnectionConfig
 from gometa.ingestion.api.source import Source, Extractor, SourceReport
 from gometa.ingestion.api.source import WorkUnit
-from typing import Optional, Iterable
-from dataclasses import dataclass
+from typing import Optional, Iterable, List
+from dataclasses import dataclass, field
 import confluent_kafka
+from confluent_kafka.schema_registry.schema_registry_client import SchemaRegistryClient
 import re
 from gometa.ingestion.api.closeable import Closeable
+from gometa.ingestion.source.metadata_common import MetadataWorkUnit
+
+import time
+from gometa.ingestion.api.common import PipelineContext
+import gometa.ingestion.extractor.schema_util as schema_util
+
+from gometa.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
+from gometa.metadata.com.linkedin.pegasus2avro.metadata.snapshot import DatasetSnapshot
+from gometa.metadata.com.linkedin.pegasus2avro.schema import SchemaMetadata, KafkaSchema
+from gometa.metadata.com.linkedin.pegasus2avro.common import AuditStamp
+
+logger = logging.getLogger(__name__)
 
 
 class KafkaSourceConfig(ConfigModel):
@@ -14,40 +28,104 @@ class KafkaSourceConfig(ConfigModel):
 
 
 @dataclass
-class KafkaWorkUnit(WorkUnit):
-    config: KafkaSourceConfig
+class KafkaSourceReport(SourceReport):
+    topics_scanned = 0
+    incomplete_schemas: List[str] = field(default_factory=list)
+    filtered: List[str] = field(default_factory=list)
 
-    def get_metadata(self):
-        return self.config.dict() 
+    def report_topic_scanned(self, topic: str) -> None:
+        self.topics_scanned += 1
+
+    def report_schema_incomplete(self, topic: str) -> None:
+        self.incomplete_schemas.append(topic)
+    
+    def report_dropped(self, topic: str) -> None:
+        self.filtered.append(topic)
 
 @dataclass
 class KafkaSource(Source):
     source_config: KafkaSourceConfig
     topic_pattern: re.Pattern
     consumer: confluent_kafka.Consumer
-    report: SourceReport = SourceReport()
+    report: KafkaSourceReport
 
-    def __init__(self, config, ctx):
+    def __init__(self, config: KafkaSourceConfig, ctx: PipelineContext):
         super().__init__(ctx)
         self.source_config = config
         self.topic_pattern = re.compile(self.source_config.topic)
         self.consumer = confluent_kafka.Consumer({'group.id':'test', 'bootstrap.servers':self.source_config.connection.bootstrap})
+        self.schema_registry_client = SchemaRegistryClient(
+            {"url": self.source_config.connection.schema_registry_url}
+        )
+        self.report = KafkaSourceReport()
 
     @classmethod
     def create(cls, config_dict, ctx):
         config = KafkaSourceConfig.parse_obj(config_dict)
         return cls(config, ctx)
 
-    def get_workunits(self) -> Iterable[KafkaWorkUnit]:
+    def get_workunits(self) -> Iterable[MetadataWorkUnit]:
         topics = self.consumer.list_topics().topics
         for t in topics:
-            if re.fullmatch(self.topic_pattern, t): 
-                # TODO: topics config should support allow and deny patterns
-                if not t.startswith("_"):
-                    wu = KafkaWorkUnit(id=f'kafka-{t}', config=KafkaSourceConfig(connection=self.source_config.connection, topic=t))
-                    self.report.report_workunit(wu)
-                    yield wu
+            self.report.report_topic_scanned(t)
+
+            # TODO: topics config should support allow and deny patterns
+            if re.fullmatch(self.topic_pattern, t) and not t.startswith("_"): 
+                mce = self._extract_record(t)
+                wu = MetadataWorkUnit(id=f'kafka-{t}', mce=mce)
+                self.report.report_workunit(wu)
+                yield wu
+            else:
+                self.report.report_dropped(t)
     
+    def _extract_record(self, topic: str) -> MetadataChangeEvent:
+        logger.debug(f"topic = {topic}")
+        platform = "kafka"
+        dataset_name = topic
+        env = "PROD"  # TODO: configure!
+        actor, sys_time = "urn:li:corpuser:etl", int(time.time()) * 1000
+        metadata_record = MetadataChangeEvent()
+        dataset_snapshot = DatasetSnapshot()
+        dataset_snapshot.urn = (
+            f"urn:li:dataset:(urn:li:dataPlatform:{platform},{dataset_name},{env})"
+        )
+
+        schema = None
+        try:
+            registered_schema = self.schema_registry_client.get_latest_version(
+                topic + "-value"
+            )
+            schema = registered_schema.schema
+        except Exception as e:
+            logger.debug(f"failed to get schema for {topic} with {e}")
+
+        if schema:
+            # TODO: add schema parsing capabilities
+            # canonical_schema = []
+            # if schema.schema_type == "AVRO":
+            #     canonical_schema = schema_util.avro_schema_to_mce_fields(schema.schema_str)
+
+            schema_metadata = SchemaMetadata(
+                schemaName=topic,
+                version=0,
+                hash=str(schema._hash),
+                platform=f"urn:li:dataPlatform:{platform}",
+                platformSchema = KafkaSchema(
+                    # TODO: keySchema
+                    documentSchema=schema.schema_str
+                ),
+                fields=[],
+                created=AuditStamp(time=sys_time, actor=actor),
+                lastModified=AuditStamp(time=sys_time, actor=actor),
+            )
+
+            dataset_snapshot.aspects.append(schema_metadata)
+        else:
+            self.report.report_schema_incomplete(topic)
+
+        metadata_record.proposedSnapshot = dataset_snapshot
+        return metadata_record
+
     def get_report(self):
         return self.report
         
