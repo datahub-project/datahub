36874970cef5f6f0a9466282239796247eee7ecb
Harshal Sheth <hsheth2@gmail.com>
2021-02-11 12:24:20 -0800

    Finish file sink

--- metadata-ingestion/tests/integration/mysql/test_mysql.py
+++ metadata-ingestion/tests/integration/mysql/test_mysql.py
@@ -3,11 +3,12 @@ import pytest
 import subprocess
 
 
-def test_ingest(mysql, pytestconfig):
-    config_file=os.path.join(str(pytestconfig.rootdir), "tests/integration/mysql", "mysql_to_file.yml")
-    # delete the output directory. TODO: move to a better way to create an output test fixture
-    os.system("rm -rf output")
-    ingest_command=f'gometa-ingest -c {config_file}'
+
+def test_ingest(mysql, pytestconfig, tmp_path):
+    test_resources_dir = pytestconfig.rootpath / "tests/integration/mysql"
+
+    config_file=(test_resources_dir / "mysql_to_file.yml").resolve()
+    ingest_command=f'cd {tmp_path} && gometa-ingest -c {config_file}'
     ret = os.system(ingest_command)
     assert ret == 0
 
