64d0091b14b23f1e8608d1c48803eaedcfd001de
John Joyce <john@acryl.io>
2021-02-10 16:19:34 -0800

    Fixes

--- docker/quickstart-react.sh
+++ docker/quickstart-react.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Quickstarts DataHub that serves React app. Notice that this only starts the dependencies required by datahub-frontend-react.
-# Notice that by default we do not launch datahub-frontend (Ember) app.
+# Quickstarts DataHub that serves React app
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.react.yml pull && docker-compose -p datahub \
     -f docker-compose.yml \
