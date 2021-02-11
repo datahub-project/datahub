b759205b525cfbb8bfa0e5bd39a31963428fac86
Harshal Sheth <hsheth2@gmail.com>
2021-02-11 12:53:44 -0800

    Start checking files for mysql test

--- metadata-ingestion/tests/conftest.py
+++ metadata-ingestion/tests/conftest.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+# See https://stackoverflow.com/a/33515264.
+sys.path.append(os.path.join(os.path.dirname(__file__), 'test_helpers'))
 
 pytest_plugins = [
     "tests.integration.fixtures.sql_fixtures"
