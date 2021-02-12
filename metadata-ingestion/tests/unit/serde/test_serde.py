ec84bac2dca15a23e08f29961bf13c0592bf0166
Harshal Sheth <hsheth2@gmail.com>
2021-02-11 21:17:59 -0800

    Fix serde test

--- metadata-ingestion/tests/unit/serde/test_serde.py
+++ metadata-ingestion/tests/unit/serde/test_serde.py
@@ -3,25 +3,35 @@ import pytest
 import subprocess
 import shutil
 
+from gometa.ingestion.run.pipeline import Pipeline
+
 import mce_helpers
 
 
 def test_serde_large(pytestconfig, tmp_path):
-    config_filename = "file_to_file_large.yml"
     json_filename = "test_serde_large.json"
-    output_filename = "output_large.json"
+    output_filename = "output.json"
 
     test_resources_dir = pytestconfig.rootpath / "tests/unit/serde"
 
-    config_file = test_resources_dir / config_filename
-    shutil.copy(config_file, tmp_path)
-
     golden_file = test_resources_dir / json_filename
-    shutil.copy(golden_file, tmp_path)
+    output_file = tmp_path / output_filename
 
-    ingest_command = f'cd {tmp_path} && gometa-ingest -c ./{config_filename}'
-    ret = os.system(ingest_command)
-    assert ret == 0
+    pipeline = Pipeline.create({
+        'source': {
+            'type': 'file',
+            'file': {
+                'filename': str(golden_file),
+            },
+        },
+        'sink': {
+            'type': 'file',
+            'file': {
+                'filename': str(output_file),
+            },
+        }
+    })
+    pipeline.run()
 
     output = mce_helpers.load_json_file(tmp_path / output_filename)
     golden = mce_helpers.load_json_file(golden_file)
