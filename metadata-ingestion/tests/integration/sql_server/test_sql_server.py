4d389508c6f69635559bcc2bcc446fd74b92b732
Harshal Sheth <hsheth2@gmail.com>
2021-02-12 20:17:25 -0800

    Setup CI

--- metadata-ingestion/tests/integration/sql_server/test_sql_server.py
+++ metadata-ingestion/tests/integration/sql_server/test_sql_server.py
@@ -12,8 +12,8 @@ def test_ingest(sql_server, pytestconfig):
     config_file = os.path.join(
         str(pytestconfig.rootdir), "tests/integration/sql_server", "mssql_to_file.yml"
     )
-    # delete the output directory. TODO: move to a better way to create an output test fixture
-    os.system("rm -rf output")
     ingest_command = f'gometa-ingest -c {config_file}'
     ret = os.system(ingest_command)
     assert ret == 0
+    # TODO: move to a better way to create an output test fixture
+    os.system("rm ./mssql_mces.json")
