cacdbf83c99c1a282942f059ff7704fcb8b46bbb
Harshal Sheth <hsheth2@gmail.com>
2021-02-15 11:44:29 -0800

    Remove codegen files

--- metadata-ingestion/src/gometa/metadata/com/linkedin/pegasus2avro/common/fieldtransformer/__init__.py
+++
@@ -1,6 +0,0 @@
-from ......schema_classes import TransformationTypeClass
-from ......schema_classes import UDFTransformerClass
-
-
-TransformationType = TransformationTypeClass
-UDFTransformer = UDFTransformerClass
