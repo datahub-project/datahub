95faca45e21e83ddd1fc9dff55acebf87e5a72a3
Harshal Sheth <hsheth2@gmail.com>
2021-02-15 14:44:55 -0800

    Remove old ingestion scripts

--- metadata-ingestion-old/sql-etl/mysql_etl.py
+++
@@ -1,8 +0,0 @@
-from common import run
-
-# See https://github.com/PyMySQL/PyMySQL for more details
-URL = '' # e.g. mysql+pymysql://username:password@hostname:port
-OPTIONS = {} # e.g. {"encoding": "latin1"}
-PLATFORM = 'mysql'
-
-run(URL, OPTIONS, PLATFORM)
