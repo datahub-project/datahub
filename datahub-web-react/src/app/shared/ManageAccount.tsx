65b231c45dc8bb1aec94b0e92b00bef46f4b0b4e
brendansun93 <78824220+brendansun93@users.noreply.github.com>
2021-02-18 14:16:57 -0500

    feat(React): Avatar dropdown menu and logout function (#2115)
    Co-authored-by: Brendan Sun <shihaosun@geotab.com>

--- datahub-web-react/src/app/shared/ManageAccount.tsx
+++ datahub-web-react/src/app/shared/ManageAccount.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { Avatar } from 'antd';
+import Cookies from 'js-cookie';
+import { Menu, Avatar, Dropdown } from 'antd';
 import { Link } from 'react-router-dom';
 import defaultAvatar from '../../images/default_avatar.png';
 import { EntityType } from '../../types.generated';
 import { useEntityRegistry } from '../useEntityRegistry';
+import { isLoggedInVar } from '../auth/checkAuthStatus';
 
 interface Props {
     urn: string;
@@ -16,17 +18,36 @@ const defaultProps = {
 
 export const ManageAccount = ({ urn: _urn, pictureLink: _pictureLink }: Props) => {
     const entityRegistry = useEntityRegistry();
+
+    const handleLogout = () => {
+        Cookies.remove('IS_LOGGED_IN');
+        localStorage.removeItem('userUrn');
+        isLoggedInVar(false);
+    };
+
+    const menu = (
+        <Menu>
+            <Menu.Item danger>
+                <div tabIndex={0} role="button" onClick={handleLogout} onKeyDown={handleLogout}>
+                    Log out
+                </div>
+            </Menu.Item>
+        </Menu>
+    );
+
     return (
-        <Link to={`${entityRegistry.getPathName(EntityType.CorpUser)}/${_urn}`}>
-            <Avatar
-                style={{
-                    marginRight: '15px',
-                    color: '#f56a00',
-                    backgroundColor: '#fde3cf',
-                }}
-                src={_pictureLink || defaultAvatar}
-            />
-        </Link>
+        <Dropdown overlay={menu}>
+            <Link to={`/${entityRegistry.getPathName(EntityType.CorpUser)}/${_urn}`}>
+                <Avatar
+                    style={{
+                        marginRight: '15px',
+                        color: '#f56a00',
+                        backgroundColor: '#fde3cf',
+                    }}
+                    src={_pictureLink || defaultAvatar}
+                />
+            </Link>
+        </Dropdown>
     );
 };
 
