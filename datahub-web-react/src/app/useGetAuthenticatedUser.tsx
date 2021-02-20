c6c9ecaf5053986528d0abd030af29148d6329a4
John Joyce <john@acryl.io>
2021-02-20 10:40:04 -0800

    Touch up doc string

--- datahub-web-react/src/app/useGetAuthenticatedUser.tsx
+++ datahub-web-react/src/app/useGetAuthenticatedUser.tsx
@@ -1,7 +1,7 @@
 import { useGetUserQuery } from '../graphql/user.generated';
 
 /**
- * Fetch an instance of EntityRegistry from the React context.
+ * Fetch a CorpUser object corresponding to the currently authenticated user.
  */
 export function useGetAuthenticatedUser() {
     return useGetUserQuery({ variables: { urn: localStorage.getItem('userUrn') as string } });
