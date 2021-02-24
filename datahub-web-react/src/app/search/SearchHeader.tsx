6dc250f0d31995559f3eb5798a0e7ffbcb68388c
John Joyce <john@acryl.io>
2021-02-23 18:00:37 -0800

    SearchPage updates

--- datahub-web-react/src/app/search/SearchHeader.tsx
+++ datahub-web-react/src/app/search/SearchHeader.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import 'antd/dist/antd.css';
-import { Image, Layout } from 'antd';
+import { Image, Layout, Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import { SearchBar } from './SearchBar';
 import { ManageAccount } from '../shared/ManageAccount';
@@ -9,15 +9,13 @@ import { GlobalCfg } from '../../conf';
 const { Header } = Layout;
 
 type Props = {
-    types: Array<string>;
-    selectedType: string;
     initialQuery: string;
     placeholderText: string;
     suggestions: Array<string>;
-    onSearch: (type: string, query: string) => void;
-    onQueryChange: (type: string, query: string) => void;
+    onSearch: (query: string) => void;
+    onQueryChange: (query: string) => void;
     authenticatedUserUrn: string;
-    authenticatedUserPictureLink?: string;
+    authenticatedUserPictureLink?: string | null;
 };
 
 const defaultProps = {
@@ -28,8 +26,6 @@ const defaultProps = {
  * A header containing a Logo, Search Bar view, & an account management dropdown.
  */
 export const SearchHeader = ({
-    types,
-    selectedType,
     initialQuery,
     placeholderText,
     suggestions,
@@ -44,38 +40,36 @@ export const SearchHeader = ({
                 position: 'fixed',
                 zIndex: 1,
                 width: '100%',
-                backgroundColor: 'rgb(51 62 76)',
-                fontSize: '18px',
-                height: '64px',
+                backgroundColor: GlobalCfg.THEME_COLOR_PRIMARY,
+                height: '80px',
                 lineHeight: '20px',
                 color: '#fff',
-                padding: '0px 80px',
+                padding: '0px 40px',
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
             }}
         >
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-                <Link
-                    style={{
-                        height: '64px',
-                        padding: '15px 30px',
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}
-                    to="/"
-                >
-                    <Image style={{ width: '34px', height: '30px' }} src={GlobalCfg.LOGO_IMAGE} preview={false} />
-                    <div style={{ color: 'white', fontWeight: 'bold', padding: '15px' }}>DataHub</div>
-                </Link>
-                <SearchBar
-                    types={types}
-                    initialQuery={initialQuery}
-                    selectedType={selectedType}
-                    placeholderText={placeholderText}
-                    suggestions={suggestions}
-                    onSearch={onSearch}
-                    onQueryChange={onQueryChange}
-                />
-                <ManageAccount urn={authenticatedUserUrn} pictureLink={authenticatedUserPictureLink} />
-            </div>
+            <Link
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                }}
+                to="/"
+            >
+                <Image style={{ width: '34px', height: '30px' }} src={GlobalCfg.LOGO_IMAGE} preview={false} />
+                <Typography.Title level={4} style={{ color: 'white', paddingLeft: '12px', margin: '0' }}>
+                    DataHub
+                </Typography.Title>
+            </Link>
+            <SearchBar
+                initialQuery={initialQuery}
+                placeholderText={placeholderText}
+                suggestions={suggestions}
+                onSearch={onSearch}
+                onQueryChange={onQueryChange}
+            />
+            <ManageAccount urn={authenticatedUserUrn} pictureLink={authenticatedUserPictureLink} />
         </Header>
     );
 };
