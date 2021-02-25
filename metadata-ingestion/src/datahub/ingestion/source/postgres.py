e0c8f40196a11e9aec41db5917b35017f581ddfe
Harshal Sheth <hsheth2@gmail.com>
2021-02-25 09:05:16 -0800

    feat(ingest): support Postgres PostGIS extensions (#2136)

--- metadata-ingestion/src/datahub/ingestion/source/postgres.py
+++ metadata-ingestion/src/datahub/ingestion/source/postgres.py
@@ -1,5 +1,15 @@
 from .sql_common import BasicSQLAlchemyConfig, SQLAlchemySource
 
+try:
+    # GeoAlchemy adds support for PostGIS extensions in SQLAlchemy. In order to
+    # activate it, we must import it so that it can hook into SQLAlchemy. While
+    # we don't use the Geometry type that we import, we do care about the side
+    # effects of the import. For more details, see here:
+    # https://geoalchemy-2.readthedocs.io/en/latest/core_tutorial.html#reflecting-tables.
+    from geoalchemy2 import Geometry  # noqa
+except ImportError:
+    pass
+
 
 class PostgresConfig(BasicSQLAlchemyConfig):
     # defaults
