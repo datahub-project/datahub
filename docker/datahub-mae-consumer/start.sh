4a4465ea1a46c63a5d0e03283cba84c66581e8d1
Rickard Cardell <rickard.cardell@klarna.com>
2021-02-27 06:28:22 +0100

    feat: neo4j https support (#2101) (#2144)
    * feat: neo4j https support (#2101)
    
        Ability to specify http as well as https URI schemes in the 'NEO4J_HOST' variable.

--- docker/datahub-mae-consumer/start.sh
+++ docker/datahub-mae-consumer/start.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+# Add default URI (http) scheme if needed
+if ! echo $NEO4J_HOST | grep -q "://" ; then
+    NEO4J_HOST="http://$NEO4J_HOST"
+fi
+
 dockerize \
   -wait tcp://$KAFKA_BOOTSTRAP_SERVER \
   -wait http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT \
-  -wait http://$NEO4J_HOST \
+  -wait $NEO4J_HOST \
   -timeout 240s \
   java -jar /datahub/datahub-mae-consumer/bin/mae-consumer-job.jar
