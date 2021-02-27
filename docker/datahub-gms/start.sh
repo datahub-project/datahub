4db991443280bf86c70b508574a25df14d9ee7f0
Rickard Cardell <rickard.cardell@klarna.com>
2021-02-27 06:28:22 +0100

    feat: neo4j https support (#2101) (#2144)
    * feat: neo4j https support (#2101)
    
        Ability to specify http as well as https URI schemes in the 'NEO4J_HOST' variable.

--- docker/datahub-gms/start.sh
+++ docker/datahub-gms/start.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# Add default URI (http) scheme if needed
+if ! echo $NEO4J_HOST | grep -q "://" ; then
+    NEO4J_HOST="http://$NEO4J_HOST"
+fi
+
 dockerize \
   -wait tcp://$EBEAN_DATASOURCE_HOST \
   -wait tcp://$KAFKA_BOOTSTRAP_SERVER \
   -wait http://$ELASTICSEARCH_HOST:$ELASTICSEARCH_PORT \
-  -wait http://$NEO4J_HOST \
+  -wait $NEO4J_HOST \
   -timeout 240s \
   java -jar /jetty-runner.jar /datahub/datahub-gms/bin/war.war
