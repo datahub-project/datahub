7ab76929f42021c3d10379952f2d05396fe414da
John Joyce <john@acryl.io>
2021-03-04 07:40:56 -0800

    Lower case platform

--- datahub-web-react/src/app/entity/chart/getLogoFromPlatform.tsx
+++ datahub-web-react/src/app/entity/chart/getLogoFromPlatform.tsx
@@ -4,7 +4,7 @@ import lookerLogo from '../../../images/lookerlogo.png';
  * TODO: This is a temporary solution, until the backend can push logos for all data platform types.
  */
 export function getLogoFromPlatform(platform: string) {
-    if (platform === 'Looker') {
+    if (platform.toLowerCase() === 'looker') {
         return lookerLogo;
     }
     return undefined;
