32e997e071c8aa5bae8d7408f49d92382ee495de
Gabe Lyons <itsgabelyons@gmail.com>
2021-03-09 15:43:04 -0800

    testing & debugability

--- docker/elasticsearch-setup/create-indices.sh
+++ docker/elasticsearch-setup/create-indices.sh
@@ -3,9 +3,7 @@
 set -e
 
 function create_index {
-	echo $1
-	echo $2
-	echo $3
+	echo -e '\ncreating' $1
   jq -n \
     --slurpfile settings index/$2 \
     --slurpfile mappings index/$3 \
@@ -20,4 +18,4 @@ create_index dashboarddocument dashboard/settings.json dashboard/mappings.json
 create_index dataprocessdocument data-process/settings.json data-process/mappings.json
 create_index datasetdocument dataset/settings.json dataset/mappings.json
 create_index mlmodeldocument ml-model/settings.json ml-model/mappings.json
-create_index tagdocument tag2/settings.json tag2/mappings.json
+create_index tagdocument tags/settings.json tags/mappings.json
