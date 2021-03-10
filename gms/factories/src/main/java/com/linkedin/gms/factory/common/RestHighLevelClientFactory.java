2d5679ce9f61cefef638ecda7411d147a61f0eaa
Thomas Larsson <thomas.p.larsson@gmail.com>
2021-03-10 07:42:04 +0100

    feat(mae-consumer): enable mae-consumer to use ssl when communicating with elasticsearch (#2202)
    Fixes: #2129
    
    Co-authored-by: thomas.larsson <thomas.larsson@klarna.com>

--- gms/factories/src/main/java/com/linkedin/gms/factory/common/RestHighLevelClientFactory.java
+++ gms/factories/src/main/java/com/linkedin/gms/factory/common/RestHighLevelClientFactory.java
@@ -14,10 +14,12 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 
 @Slf4j
 @Configuration
+@Import({ElasticsearchSSLContextFactory.class})
 public class RestHighLevelClientFactory {
 
   @Value("${ELASTICSEARCH_HOST:localhost}")
---
+++ metadata-jobs/mae-consumer-job/src/main/java/com/linkedin/metadata/kafka/elasticsearch/ElasticsearchConnectorFactory.java
@@ -0,0 +1,28 @@
+package com.linkedin.metadata.kafka.elasticsearch;
+
+import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+import javax.annotation.Nonnull;
+
+@Slf4j
+@Configuration
+public class ElasticsearchConnectorFactory {
+
+  @Value("${ES_BULK_REQUESTS_LIMIT:1}")
+  private Integer bulkRequestsLimit;
+
+  @Value("${ES_BULK_FLUSH_PERIOD:1}")
+  private Integer bulkFlushPeriod;
+
+  @Bean(name = "elasticsearchConnector")
+  @Nonnull
+  public ElasticsearchConnector createInstance(RestHighLevelClient elasticSearchRestHighLevelClient) {
+    return new ElasticsearchConnector(elasticSearchRestHighLevelClient, bulkRequestsLimit, bulkFlushPeriod);
+  }
+
+}
