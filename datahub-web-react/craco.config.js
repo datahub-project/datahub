ffdae85f18144f956eff4d65c224d619fbc9bae3
Gabe Lyons <itsgabelyons@gmail.com>
2021-03-11 09:18:10 -0800

    fixing theming test in react and simplifying api

--- datahub-web-react/craco.config.js
+++ datahub-web-react/craco.config.js
@@ -3,12 +3,20 @@ const CracoAntDesignPlugin = require('craco-antd');
 
 const themeConfig = require(`./src/conf/theme/${process.env.REACT_APP_THEME_CONFIG}`);
 
+function addLessPrefixToKeys(styles) {
+    const output = {};
+    Object.keys(styles).forEach((key) => {
+        output[`@${key}`] = styles[key];
+    });
+    return output;
+}
+
 module.exports = {
     plugins: [
         {
             plugin: CracoAntDesignPlugin,
             options: {
-                customizeTheme: themeConfig.styles,
+                customizeTheme: addLessPrefixToKeys(themeConfig.styles),
             },
         },
     ],
