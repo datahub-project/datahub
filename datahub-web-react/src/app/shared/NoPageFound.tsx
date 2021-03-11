d7fa03d362ed75782f2a3feb66d4aa5369d02557
John Joyce <john@acryl.io>
2021-03-10 23:39:43 -0800

    Merging

--- datahub-web-react/src/app/shared/NoPageFound.tsx
+++ datahub-web-react/src/app/shared/NoPageFound.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useReactiveVar } from '@apollo/client';
 import { Redirect } from 'react-router';
-
 import { isLoggedInVar } from '../auth/checkAuthStatus';
 import { PageRoutes } from '../../conf/Global';
 
