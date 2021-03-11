9941ee7740876244a1f4122210975e8b89d1e0ae
thomas.larsson <thomas.larsson@klarna.com>
2021-03-11 13:43:24 +0100

    feat(ingestion): add aws athena ingestion source

--- metadata-ingestion/src/datahub/entrypoints.py
+++ metadata-ingestion/src/datahub/entrypoints.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 # Set to debug on the root logger.
 logging.getLogger(None).setLevel(logging.DEBUG)
 logging.getLogger("urllib3").setLevel(logging.WARN)
+logging.getLogger("botocore").setLevel(logging.INFO)
 
 # Configure logger.
 BASE_LOGGING_FORMAT = (
