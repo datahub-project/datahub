d836ea0f8d994599184d69b46c5de89eabac320f
Fredrik Sannholm <fredrik.sannholm@wolt.com>
2021-03-12 22:38:16 +0200

    Fix some more

--- gms/factories/src/main/java/com/linkedin/gms/factory/datajob/DataFlowOrchestratorLocalDaoFactory.java
+++ gms/factories/src/main/java/com/linkedin/gms/factory/datajob/DataFlowOrchestratorLocalDaoFactory.java
@@ -3,7 +3,7 @@ package com.linkedin.gms.factory.datajob;
 import com.linkedin.common.urn.DataFlowOrchestratorUrn;
 import com.linkedin.metadata.aspect.DataFlowOrchestratorAspect;
 import com.linkedin.metadata.dao.ImmutableLocalDAO;
-import com.linkedin.metadata.resources.datajob.utils.DataFlowOrchestratorsUtil;
+import com.linkedin.metadata.resources.datajob.utils.DataFlowOrchestratorUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +17,7 @@ public class DataFlowOrchestratorLocalDaoFactory {
 
   @Bean(name = "dataFlowOrchestratorLocalDAO")
   protected ImmutableLocalDAO createInstance() {
-    return new ImmutableLocalDAO<>(DataFlowOrchestratorAspect.class, DataFlowOrchestratorsUtil.getDataFlowOrchestratorInfoMap(),
+    return new ImmutableLocalDAO<>(DataFlowOrchestratorAspect.class, DataFlowOrchestratorUtil.getDataFlowOrchestratorInfoMap(),
         DataFlowOrchestratorUrn.class);
   }
 }
