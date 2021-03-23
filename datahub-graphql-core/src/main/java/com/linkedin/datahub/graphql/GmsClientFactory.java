9c618b4835c4c1b9d49db8f137bd08f7aae73c90
Thomas Larsson <thomas.p.larsson@gmail.com>
2021-03-23 07:23:11 +0100

    feat(react): enable the react frontend to use SSL when talking with GMS (#2267)
    Fixes: #2265
    
    Co-authored-by: thomas.larsson <thomas.larsson@klarna.com>

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsClientFactory.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsClientFactory.java
@@ -24,10 +24,15 @@ public class GmsClientFactory {
      */
     private static final String GMS_HOST_ENV_VAR = "DATAHUB_GMS_HOST";
     private static final String GMS_PORT_ENV_VAR = "DATAHUB_GMS_PORT";
+    private static final String GMS_USE_SSL_ENV_VAR = "DATAHUB_GMS_USE_SSL";
+    private static final String GMS_SSL_PROTOCOL_VAR = "DATAHUB_GMS_SSL_PROTOCOL";
+
 
     private static final Client REST_CLIENT = DefaultRestliClientFactory.getRestLiClient(
             Configuration.getEnvironmentVariable(GMS_HOST_ENV_VAR),
-            Integer.valueOf(Configuration.getEnvironmentVariable(GMS_PORT_ENV_VAR)));
+            Integer.valueOf(Configuration.getEnvironmentVariable(GMS_PORT_ENV_VAR)),
+            Boolean.parseBoolean(Configuration.getEnvironmentVariable(GMS_USE_SSL_ENV_VAR, "False")),
+            Configuration.getEnvironmentVariable(GMS_SSL_PROTOCOL_VAR));
 
     private static CorpUsers _corpUsers;
     private static Datasets _datasets;
