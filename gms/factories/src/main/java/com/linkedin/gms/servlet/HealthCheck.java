59bef2fddf76023622a655f32c583561859dc16b
Dexter Lee <dexter@acryl.io>
2021-03-23 20:05:34 -0700

    Fix to John's comment

--- gms/factories/src/main/java/com/linkedin/gms/servlet/HealthCheck.java
+++ gms/factories/src/main/java/com/linkedin/gms/servlet/HealthCheck.java
@@ -4,7 +4,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// Return a 200 for kubernetes healthchecks.
+// Return a 200 for health checks
 public class HealthCheck extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
