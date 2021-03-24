ca2a7fb52760f7037a8ea0a306e1cf6554359c72
Pedro Silva <pedro.silva@definedcrowd.com>
2021-03-24 03:25:05 +0000

    fix(ingest): Add druid-specific identification logic (#2284)

--- metadata-ingestion/src/datahub/ingestion/source/druid.py
+++ metadata-ingestion/src/datahub/ingestion/source/druid.py
@@ -14,6 +14,18 @@ class DruidConfig(BasicSQLAlchemyConfig):
     def get_sql_alchemy_url(self):
         return f"{BasicSQLAlchemyConfig.get_sql_alchemy_url(self)}/druid/v2/sql/"
 
+    """
+    The pydruid library already formats the table name correctly, so we do not
+    need to use the schema name when constructing the URN. Without this override,
+    every URN would incorrectly start with "druid.
+
+    For more information, see https://druid.apache.org/docs/latest/querying/sql.html#schemata-table
+
+    """
+
+    def get_identifier(self, schema: str, table: str) -> str:
+        return f"{table}"
+
 
 class DruidSource(SQLAlchemySource):
     def __init__(self, config, ctx):
