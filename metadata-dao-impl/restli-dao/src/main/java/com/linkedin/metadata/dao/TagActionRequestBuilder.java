b710f4f9ce707e54a4d276b0cd87974652522e84
Fredrik Sannholm <fredrik.sannholm@wolt.com>
2021-03-31 14:40:06 +0300

    Add TagActionRequestBuilder to RequestBuilders

--- metadata-dao-impl/restli-dao/src/main/java/com/linkedin/metadata/dao/TagActionRequestBuilder.java
+++ metadata-dao-impl/restli-dao/src/main/java/com/linkedin/metadata/dao/TagActionRequestBuilder.java
@@ -6,11 +6,11 @@ import com.linkedin.metadata.snapshot.TagSnapshot;
 /**
  * An action request builder for tag entities.
  */
-public class TagActionRequestBuilders extends BaseActionRequestBuilder<TagSnapshot, TagUrn> {
+public class TagActionRequestBuilder extends BaseActionRequestBuilder<TagSnapshot, TagUrn> {
 
     private static final String BASE_URI_TEMPLATE = "tags";
 
-    public TagActionRequestBuilders() {
+    public TagActionRequestBuilder() {
         super(TagSnapshot.class, TagUrn.class, BASE_URI_TEMPLATE);
     }
 }
