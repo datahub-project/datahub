65ba3f4de936bfbb10008580694bc3806133900a
Harshal Sheth <hsheth2@gmail.com>
2021-03-31 23:46:24 -0700

    add a note about docker test

--- metadata-ingestion/tests/unit/test_check.py
+++ metadata-ingestion/tests/unit/test_check.py
@@ -4,6 +4,8 @@ from datahub.entrypoints import datahub
 
 def test_check_local_docker():
     # This just verifies that it runs without error.
+    # We don't actually know what environment this will be run in, so
+    # we can't depend on the output. Eventually, we should mock the docker SDK.
     runner = CliRunner()
     result = runner.invoke(datahub, ["check", "local-docker"])
     assert result.output
