c050e0a4710407e7571e0fe8fdd8011a5c182188
Gabe Lyons <itsgabelyons@gmail.com>
2021-04-05 09:15:23 -0700

    fix(lineage): fixing lineage layout bugs

--- datahub-web-react/src/app/lineage/__tests__/adjustVXTreeLayout.test.tsx
+++ datahub-web-react/src/app/lineage/__tests__/adjustVXTreeLayout.test.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { Tree, hierarchy } from '@vx/hierarchy';
 import { render } from '@testing-library/react';
 
-import { dataset3WithLineage, dataset4WithLineage, dataset5WithLineage, dataset6WithLineage } from '../../../Mocks';
+import {
+    dataset3WithLineage,
+    dataset4WithLineage,
+    dataset5WithLineage,
+    dataset6WithLineage,
+    dataset7WithLineage,
+} from '../../../Mocks';
 import constructTree from '../utils/constructTree';
 import extendAsyncEntities from '../utils/extendAsyncEntities';
 import adjustVXTreeLayout from '../utils/adjustVXTreeLayout';
@@ -34,6 +40,47 @@ describe('adjustVXTreeLayout', () => {
                     expect(adjustedTree.nodesToRender[2].y).not.toEqual(adjustedTree.nodesToRender[3].y);
                     expect(adjustedTree.nodesToRender[1].x).not.toEqual(adjustedTree.nodesToRender[3].x);
                     expect(adjustedTree.nodesToRender[0].x).not.toEqual(adjustedTree.nodesToRender[3].x);
+
+                    expect(adjustedTree.nodesToRender.length).toEqual(5);
+
+                    return <div />;
+                }}
+            </Tree>,
+        );
+    });
+
+    it('handles multiple instances of lineage jumping over layers while upstream', () => {
+        const fetchedEntities = [
+            { entity: dataset4WithLineage, direction: Direction.Upstream, fullyFetched: true },
+            { entity: dataset5WithLineage, direction: Direction.Upstream, fullyFetched: true },
+            { entity: dataset6WithLineage, direction: Direction.Upstream, fullyFetched: true },
+            { entity: dataset7WithLineage, direction: Direction.Upstream, fullyFetched: true },
+        ];
+        const mockFetchedEntities = fetchedEntities.reduce(
+            (acc, entry) => extendAsyncEntities(acc, entry.entity, entry.fullyFetched),
+            {} as FetchedEntities,
+        );
+
+        const upstreamData = hierarchy(constructTree(dataset3WithLineage, mockFetchedEntities, Direction.Upstream));
+
+        render(
+            <Tree<NodeData> root={upstreamData} size={[1000, 1000]}>
+                {(tree) => {
+                    const adjustedTree = adjustVXTreeLayout({ tree, direction: Direction.Upstream });
+
+                    expect(adjustedTree.nodesToRender[5].x).toEqual(adjustedTree.nodesToRender[6].x);
+                    expect(adjustedTree.nodesToRender[5].y).toEqual(adjustedTree.nodesToRender[6].y);
+                    expect(adjustedTree.nodesToRender[5].data.name).toEqual(adjustedTree.nodesToRender[6].data.name);
+
+                    expect(adjustedTree.nodesToRender[5].x).toEqual(adjustedTree.nodesToRender[3].x);
+                    expect(adjustedTree.nodesToRender[5].y).toEqual(adjustedTree.nodesToRender[3].y);
+                    expect(adjustedTree.nodesToRender[5].data.name).toEqual(adjustedTree.nodesToRender[3].data.name);
+
+                    expect(adjustedTree.nodesToRender.length).toEqual(7);
+
+                    adjustedTree.nodesToRender.forEach((node) => {
+                        expect(node.y).toBeLessThanOrEqual(0);
+                    });
 
                     return <div />;
                 }}
