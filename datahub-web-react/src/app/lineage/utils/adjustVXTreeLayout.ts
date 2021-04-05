17f7c9efd068c1735f37e1b2282739c265fdb187
Gabe Lyons <itsgabelyons@gmail.com>
2021-04-05 12:13:29 -0700

    fix(lineage): fixing lineage layout bugs (#2337)

--- datahub-web-react/src/app/lineage/utils/adjustVXTreeLayout.ts
+++ datahub-web-react/src/app/lineage/utils/adjustVXTreeLayout.ts
@@ -8,35 +8,36 @@ export default function adjustVXTreeLayout({
     tree: HierarchyPointNode<NodeData>;
     direction: Direction;
 }) {
-    const nodeByUrn: { [x: string]: HierarchyPointNode<NodeData> } = {};
+    const nodesByUrn: { [x: string]: HierarchyPointNode<NodeData>[] } = {};
     // adjust node's positions
     tree.descendants().forEach((descendent) => {
-        // second, duplicate nodes will be created if dependencies are in a dag rather than a tree.
-        // this is expected, however the <Tree /> component will try to lay them out independently.
-        // We need to force them to be laid out in the same place so each copy's edges begin at the same source.
-        if (descendent.data.urn && !nodeByUrn[descendent.data.urn]) {
-            nodeByUrn[descendent.data.urn] = descendent;
-        } else if (descendent.data.urn) {
-            const existing = nodeByUrn[descendent.data.urn];
-            if (descendent.height < existing.height) {
-                // eslint-disable-next-line  no-param-reassign
-                descendent.x = existing.x;
-                // eslint-disable-next-line  no-param-reassign
-                descendent.y = existing.y;
-            } else {
-                // eslint-disable-next-line  no-param-reassign
-                existing.x = descendent.x;
-                // eslint-disable-next-line  no-param-reassign
-                existing.y = descendent.y;
-                nodeByUrn[descendent.data.urn] = descendent;
-            }
-        }
-
         // first, we need to flip the position of nodes who are going upstream
         // eslint-disable-next-line  no-param-reassign
         if (direction === Direction.Upstream) {
             // eslint-disable-next-line  no-param-reassign
             descendent.y = -Math.abs(descendent.y);
+        }
+        // second, duplicate nodes will be created if dependencies are in a dag rather than a tree.
+        // this is expected, however the <Tree /> component will try to lay them out independently.
+        // We need to force them to be laid out in the same place so each copy's edges begin at the same source.
+        if (descendent.data.urn && !nodesByUrn[descendent.data.urn]) {
+            nodesByUrn[descendent.data.urn] = [descendent];
+        } else if (descendent.data.urn) {
+            const existing = nodesByUrn[descendent.data.urn];
+            if (descendent.height < Math.max(...existing.map((nodeByUrn) => nodeByUrn.height))) {
+                // eslint-disable-next-line  no-param-reassign
+                descendent.x = existing[0].x;
+                // eslint-disable-next-line  no-param-reassign
+                descendent.y = existing[0].y;
+            } else {
+                existing.forEach((nodeByUrn) => {
+                    // eslint-disable-next-line  no-param-reassign
+                    nodeByUrn.x = descendent.x;
+                    // eslint-disable-next-line  no-param-reassign
+                    nodeByUrn.y = descendent.y;
+                });
+                nodesByUrn[descendent.data.urn] = [...nodesByUrn[descendent.data.urn], descendent];
+            }
         }
     });
 
