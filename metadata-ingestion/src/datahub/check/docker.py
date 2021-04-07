7b13dc2ff50375446496ae9d5d90985b88404b33
Harshal Sheth <harshal@acryl.io>
2021-04-06 20:37:59 -0700

    feat(cli): Add support for checking docker memory usage

--- metadata-ingestion/src/datahub/check/docker.py
+++ metadata-ingestion/src/datahub/check/docker.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from typing import List
 
 import docker
@@ -26,42 +27,73 @@ ALLOW_STOPPED = [
     "elasticsearch-setup",
 ]
 
+MIN_MEMORY_NEEDED = 8  # GB
+# docker seems to under-report memory allocated, adding a bit of buffer to account for it
+MEMORY_TOLERANCE = 0.2  # GB
+
+
+@contextmanager
+def get_client_with_error():
+    try:
+        docker_cli = docker.from_env()
+    except docker.errors.DockerException as error:
+        yield None, error
+    else:
+        try:
+            yield docker_cli, None
+        finally:
+            docker_cli.close()
+
+
+def memory_in_gb(mem_bytes: int):
+    return mem_bytes / (1024 * 1024 * 1000)
+
 
 def check_local_docker_containers() -> List[str]:
     issues: List[str] = []
-    client = docker.from_env()
+    with get_client_with_error() as (client, error):
+        if error:
+            issues.append("Docker doesn't seem to be running. Did you start it?")
+            return issues
 
-    containers = client.containers.list(
-        all=True,
-        filters={
-            "label": "com.docker.compose.project=datahub",
-        },
-    )
+        # check total memory
+        total_mem_configured = int(client.info()["MemTotal"])
+        if memory_in_gb(total_mem_configured) + MEMORY_TOLERANCE < MIN_MEMORY_NEEDED:
+            issues.append(
+                f"Total Docker memory configured {memory_in_gb(total_mem_configured):.2f}GB is below the minimum threshold {MIN_MEMORY_NEEDED}GB"
+            )
 
-    # Check number of containers.
-    if len(containers) == 0:
-        issues.append("quickstart.sh or dev.sh is not running")
-    else:
-        existing_containers = set(container.name for container in containers)
-        missing_containers = set(REQUIRED_CONTAINERS) - existing_containers
-        for missing in missing_containers:
-            issues.append(f"{missing} container is not present")
+        containers = client.containers.list(
+            all=True,
+            filters={
+                "label": "com.docker.compose.project=datahub",
+            },
+        )
 
-    # Check that the containers are running and healthy.
-    for container in containers:
-        if container.name not in REQUIRED_CONTAINERS:
-            # Ignores things like "datahub-frontend" which are no longer used.
-            # This way, we only check required containers like "datahub-frontend-react"
-            # even if there are some old containers lying around.
-            continue
-        if container.name in ALLOW_STOPPED:
-            continue
-        elif container.status != "running":
-            issues.append(f"{container.name} is not running")
-        elif "Health" in container.attrs["State"]:
-            if container.attrs["State"]["Health"]["Status"] == "starting":
-                issues.append(f"{container.name} is still starting")
-            elif container.attrs["State"]["Health"]["Status"] != "healthy":
-                issues.append(f"{container.name} is running but not healthy")
+        # Check number of containers.
+        if len(containers) == 0:
+            issues.append("quickstart.sh or dev.sh is not running")
+        else:
+            existing_containers = set(container.name for container in containers)
+            missing_containers = set(REQUIRED_CONTAINERS) - existing_containers
+            for missing in missing_containers:
+                issues.append(f"{missing} container is not present")
+
+        # Check that the containers are running and healthy.
+        for container in containers:
+            if container.name not in REQUIRED_CONTAINERS:
+                # Ignores things like "datahub-frontend" which are no longer used.
+                # This way, we only check required containers like "datahub-frontend-react"
+                # even if there are some old containers lying around.
+                continue
+            if container.name in ALLOW_STOPPED:
+                continue
+            elif container.status != "running":
+                issues.append(f"{container.name} is not running")
+            elif "Health" in container.attrs["State"]:
+                if container.attrs["State"]["Health"]["Status"] == "starting":
+                    issues.append(f"{container.name} is still starting")
+                elif container.attrs["State"]["Health"]["Status"] != "healthy":
+                    issues.append(f"{container.name} is running but not healthy")
 
     return issues
