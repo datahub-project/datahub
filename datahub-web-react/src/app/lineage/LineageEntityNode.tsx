caad3f8d0efddf69260e76beb12cf24883767f1c
Brian <brianwebtek@gmail.com>
2021-04-09 01:24:33 +0800

    feat(react): add icons on entities, updated styling in LineageViz (#2366)

--- datahub-web-react/src/app/lineage/LineageEntityNode.tsx
+++ datahub-web-react/src/app/lineage/LineageEntityNode.tsx
@@ -14,10 +14,15 @@ function truncate(input, length) {
     return input;
 }
 
-export const width = 140;
+export const width = 212;
 export const height = 80;
+const iconWidth = 42;
+const iconHeight = 42;
+const iconX = -width / 2 + 8;
+const iconY = -iconHeight / 2;
 const centerX = -width / 2;
 const centerY = -height / 2;
+const textX = iconX + iconWidth + 8;
 
 const PointerGroup = styled(Group)`
     cursor: pointer;
@@ -49,7 +54,7 @@ export default function LineageEntityNode({
 
     return (
         <PointerGroup data-testid={`node-${node.data.urn}-${direction}`} top={node.x} left={node.y}>
-            {unexploredHiddenChildren && (
+            {unexploredHiddenChildren ? (
                 <Group>
                     {[...Array(unexploredHiddenChildren)].map((_, index) => {
                         const link = {
@@ -72,8 +77,8 @@ export default function LineageEntityNode({
                         );
                     })}
                 </Group>
-            )}
-            {node.data.unexploredChildren && (
+            ) : null}
+            {node.data.unexploredChildren ? (
                 <Group
                     onClick={() => {
                         onExpandClick({ urn: node.data.urn, type: EntityType.Dataset, direction });
@@ -88,13 +93,13 @@ export default function LineageEntityNode({
                     <g
                         fill="grey"
                         transform={`translate(${
-                            direction === Direction.Upstream ? -1 * width + 37 : width / 2 - 10
+                            direction === Direction.Upstream ? centerX - 32 : width / 2 - 10
                         } -21.5) scale(0.04 0.04)`}
                     >
                         <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm192 472c0 4.4-3.6 8-8 8H544v152c0 4.4-3.6 8-8 8h-48c-4.4 0-8-3.6-8-8V544H328c-4.4 0-8-3.6-8-8v-48c0-4.4 3.6-8 8-8h152V328c0-4.4 3.6-8 8-8h48c4.4 0 8 3.6 8 8v152h152c4.4 0 8 3.6 8 8v48z" />
                     </g>
                 </Group>
-            )}
+            ) : null}
             <Group
                 onClick={() => {
                     onEntityClick({ urn: node.data.urn, type: EntityType.Dataset });
@@ -118,15 +123,26 @@ export default function LineageEntityNode({
                     strokeOpacity={1}
                     rx={10}
                 />
-                <text dy=".33em" fontSize={14} fontFamily="Arial" textAnchor="middle" fill="black">
+                {node.data.icon ? (
+                    <image href={node.data.icon} height={iconHeight} width={iconWidth} x={iconX} y={iconY} />
+                ) : null}
+                <text dy=".33em" x={textX} fontSize={16} fontFamily="Arial" textAnchor="start" fill="black">
                     {truncate(node.data.name?.split('.').slice(-1)[0], 16)}
                 </text>
-                {unexploredHiddenChildren && isHovered && (
-                    <text dy=".33em" fontSize={14} fontFamily="Arial" textAnchor="middle" fill="black" y={centerY - 20}>
+                {unexploredHiddenChildren && isHovered ? (
+                    <text
+                        dy=".33em"
+                        dx={textX}
+                        fontSize={16}
+                        fontFamily="Arial"
+                        textAnchor="middle"
+                        fill="black"
+                        y={centerY - 20}
+                    >
                         {unexploredHiddenChildren} hidden {direction === Direction.Upstream ? 'downstream' : 'upstream'}{' '}
                         {unexploredHiddenChildren > 1 ? 'dependencies' : 'dependency'}
                     </text>
-                )}
+                ) : null}
             </Group>
         </PointerGroup>
     );
