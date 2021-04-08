bfe345da421b73345ad3437d8d20a101232866ab
Harshal Sheth <harshal@acryl.io>
2021-04-07 21:30:21 -0700

    fix(ingest): add test for avro serialization and deserialization (#2351)

--- metadata-ingestion/src/datahub/ingestion/source/mce_file.py
+++ metadata-ingestion/src/datahub/ingestion/source/mce_file.py
@@ -1,11 +1,22 @@
 import json
 from dataclasses import dataclass, field
-from typing import Iterable
+from typing import Iterable, Iterator
 
 from datahub.configuration.common import ConfigModel
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.source.metadata_common import MetadataWorkUnit
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
+
+
+def iterate_mce_file(path: str) -> Iterator[MetadataChangeEvent]:
+    with open(path, "r") as f:
+        mce_obj_list = json.load(f)
+    if not isinstance(mce_obj_list, list):
+        mce_obj_list = [mce_obj_list]
+
+    for obj in mce_obj_list:
+        mce: MetadataChangeEvent = MetadataChangeEvent.from_obj(obj)
+        yield mce
 
 
 class MetadataFileSourceConfig(ConfigModel):
@@ -23,15 +34,9 @@ class MetadataFileSource(Source):
         return cls(ctx, config)
 
     def get_workunits(self) -> Iterable[MetadataWorkUnit]:
-        with open(self.config.filename, "r") as f:
-            mce_obj_list = json.load(f)
-        if not isinstance(mce_obj_list, list):
-            mce_obj_list = [mce_obj_list]
-
-        for i, obj in enumerate(mce_obj_list):
-            mce: MetadataChangeEvent = MetadataChangeEvent.from_obj(obj)
+        for i, mce in enumerate(iterate_mce_file(self.config.filename)):
             if not mce.validate():
-                raise ValueError(f"failed to parse into valid MCE: {obj}")
+                raise ValueError(f"failed to parse into valid MCE: {mce} (index {i})")
             wu = MetadataWorkUnit(f"file://{self.config.filename}:{i}", mce)
             self.report.report_workunit(wu)
             yield wu
