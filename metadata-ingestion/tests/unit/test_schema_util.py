e3b4e62bfc0cae76c6fe3cb3c2a9627bda4ccc0e
Thomas Larsson <thomas.p.larsson@gmail.com>
2021-04-10 02:36:01 +0200

    fix(ingestion): dont crash on non-RecordSchema topics (#2372)
    Fixes: #2371
    
    Co-authored-by: thomas.larsson <thomas.larsson@klarna.com>

--- metadata-ingestion/tests/unit/test_schema_util.py
+++ metadata-ingestion/tests/unit/test_schema_util.py
@@ -2,7 +2,7 @@ import unittest
 
 from datahub.ingestion.extractor.schema_util import avro_schema_to_mce_fields
 
-EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE = """
+SCHEMA_WITH_OPTIONAL_FIELD_VIA_UNION_TYPE = """
 {
   "type": "record",
   "name": "some.event.name",
@@ -17,7 +17,7 @@ EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE = """
 }
 """
 
-EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE_NULL_ISNT_FIRST_IN_UNION = """
+SCHEMA_WITH_OPTIONAL_FIELD_VIA_UNION_TYPE_NULL_ISNT_FIRST_IN_UNION = """
 {
   "type": "record",
   "name": "some.event.name",
@@ -32,7 +32,7 @@ EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE_NULL_ISNT_FIRST_IN_UNION = """
 }
 """
 
-EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_PRIMITIVE_TYPE = """
+SCHEMA_WITH_OPTIONAL_FIELD_VIA_PRIMITIVE_TYPE = """
 {
   "type": "record",
   "name": "some.event.name",
@@ -64,25 +64,67 @@ SCHEMA_WITH_MAP_TYPE_FIELD = """
 }
 """
 
+SCHEMA_WITH_TOP_LEVEL_PRIMITIVE_FIELD = """
+{
+  "type": "string"
+}
+"""
+
+SCHEMA_WITH_TWO_FIELD_RECORD = """
+{
+  "type": "record",
+  "name": "some.event.name",
+  "namespace": "some.event.namespace",
+  "fields": [
+    {
+      "name": "my.field.A",
+      "type": "string",
+      "doc": "some.doc"
+    },
+    {
+      "name": "my.field.B",
+      "type": "string",
+      "doc": "some.doc"
+    }
+  ]
+}
+"""
+
 
 class SchemaUtilTest(unittest.TestCase):
     def test_avro_schema_to_mce_fields_events_with_nullable_fields(self):
 
-        events = [
-            EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE,
-            EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_UNION_TYPE_NULL_ISNT_FIRST_IN_UNION,
-            EXAMPLE_EVENT_OPTIONAL_FIELD_VIA_PRIMITIVE_TYPE,
+        examples = [
+            SCHEMA_WITH_OPTIONAL_FIELD_VIA_UNION_TYPE,
+            SCHEMA_WITH_OPTIONAL_FIELD_VIA_UNION_TYPE_NULL_ISNT_FIRST_IN_UNION,
+            SCHEMA_WITH_OPTIONAL_FIELD_VIA_PRIMITIVE_TYPE,
         ]
 
-        for event in events:
-            fields = avro_schema_to_mce_fields(event)
+        for schema in examples:
+            fields = avro_schema_to_mce_fields(schema)
             self.assertEqual(1, len(fields))
             self.assertTrue(fields[0].nullable)
 
     def test_avro_schema_to_mce_fields_sample_events_with_different_field_types(self):
 
-        EXAMPLES = [SCHEMA_WITH_MAP_TYPE_FIELD]
+        examples = [SCHEMA_WITH_MAP_TYPE_FIELD]
 
-        for schema in EXAMPLES:
+        for schema in examples:
+            fields = avro_schema_to_mce_fields(schema)
+            self.assertEqual(1, len(fields))
+
+    def test_avro_schema_to_mce_fields_record_with_two_fields(self):
+
+        examples = [SCHEMA_WITH_TWO_FIELD_RECORD]
+
+        for schema in examples:
+            fields = avro_schema_to_mce_fields(schema)
+            self.assertEqual(2, len(fields))
+
+    def test_avro_schema_to_mce_fields_toplevel_isnt_a_record(self):
+
+        examples = [SCHEMA_WITH_TOP_LEVEL_PRIMITIVE_FIELD]
+
+        for schema in examples:
             fields = avro_schema_to_mce_fields(schema)
             self.assertEqual(1, len(fields))
