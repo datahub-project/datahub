71a1f53c1255b31bd114b8999a552909db7d3476
Harshal Sheth <hsheth2@gmail.com>
2021-04-12 19:35:40 -0700

    fix(ingest): report correct version status in dev mode (#2388)

--- metadata-ingestion/src/datahub/__init__.py
+++ metadata-ingestion/src/datahub/__init__.py
@@ -1,2 +1,8 @@
 __package_name__ = "acryl-datahub"
-__version__ = "0.1.0"
+__version__ = "0.0.0.dev0"
+
+
+def nice_version_name() -> str:
+    if __version__ == "0.0.0.dev0":
+        return "unavailable (installed editable via git)"
+    return __version__
