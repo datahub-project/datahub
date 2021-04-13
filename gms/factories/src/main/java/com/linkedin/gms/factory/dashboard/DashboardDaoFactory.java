fa015c5aaab0153b81510b872915571ae154f83c
Dexter Lee <dexter@acryl.io>
2021-04-13 07:58:31 -0700

    fix(kafka-topic-convention): Fix DAOs that do not refer to TopicConvention (#2387)

--- gms/factories/src/main/java/com/linkedin/gms/factory/dashboard/DashboardDaoFactory.java
+++ gms/factories/src/main/java/com/linkedin/gms/factory/dashboard/DashboardDaoFactory.java
@@ -1,10 +1,12 @@
 package com.linkedin.gms.factory.dashboard;
 
 import com.linkedin.common.urn.DashboardUrn;
+import com.linkedin.gms.factory.common.TopicConventionFactory;
 import com.linkedin.metadata.aspect.DashboardAspect;
 import com.linkedin.metadata.dao.EbeanLocalDAO;
 import com.linkedin.metadata.dao.producer.KafkaMetadataEventProducer;
 import com.linkedin.metadata.snapshot.DashboardSnapshot;
+import com.linkedin.mxe.TopicConvention;
 import io.ebean.config.ServerConfig;
 import javax.annotation.Nonnull;
 import org.apache.kafka.clients.producer.Producer;
@@ -21,12 +23,12 @@ public class DashboardDaoFactory {
   ApplicationContext applicationContext;
 
   @Bean(name = "dashboardDAO")
-  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer"})
+  @DependsOn({"gmsEbeanServiceConfig", "kafkaEventProducer", TopicConventionFactory.TOPIC_CONVENTION_BEAN})
   @Nonnull
   protected EbeanLocalDAO createInstance() {
     KafkaMetadataEventProducer<DashboardSnapshot, DashboardAspect, DashboardUrn> producer =
         new KafkaMetadataEventProducer(DashboardSnapshot.class, DashboardAspect.class,
-            applicationContext.getBean(Producer.class));
+            applicationContext.getBean(Producer.class), applicationContext.getBean(TopicConvention.class));
     return new EbeanLocalDAO<>(DashboardAspect.class, producer, applicationContext.getBean(ServerConfig.class),
         DashboardUrn.class);
   }
