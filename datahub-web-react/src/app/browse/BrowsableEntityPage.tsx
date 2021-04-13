ddcaca124bb58247c4afde8548aa67f1ab02aee4
Brian <brianwebtek@gmail.com>
2021-04-14 05:22:43 +0800

    feat(react): fix browse link of last breadcrumb linked to unknown page (#2391)

--- datahub-web-react/src/app/browse/BrowsableEntityPage.tsx
+++ datahub-web-react/src/app/browse/BrowsableEntityPage.tsx
@@ -22,7 +22,12 @@ export const BrowsableEntityPage = ({ urn: _urn, type: _type, children: _childre
         <SearchablePage>
             {data && data.browsePaths && data.browsePaths.length > 0 && (
                 <Affix offsetTop={80}>
-                    <BrowsePath type={_type} path={data.browsePaths[0].path} lineageSupported={lineageSupported} />
+                    <BrowsePath
+                        type={_type}
+                        path={data.browsePaths[0].path}
+                        lineageSupported={lineageSupported}
+                        isProfilePage
+                    />
                 </Affix>
             )}
             {_children}
