a11329d5b8813bda30609be63f6f8904b87aa747
Harshal Sheth <hsheth2@gmail.com>
2021-04-13 17:30:24 -0700

    refactor(ingest): update test harness to use a compose file per test (#2392)

--- metadata-ingestion/tests/integration/ldap/test_ldap.py
+++ metadata-ingestion/tests/integration/ldap/test_ldap.py
@@ -1,38 +1,49 @@
-import mce_helpers
+import time
+
 import pytest
 
 from datahub.ingestion.run.pipeline import Pipeline
+from tests.test_helpers import mce_helpers
+from tests.test_helpers.docker_helpers import wait_for_port
 
 
 @pytest.mark.slow
-def test_ldap_ingest(mysql, pytestconfig, tmp_path, mock_time):
+def test_ldap_ingest(docker_compose_runner, pytestconfig, tmp_path, mock_time):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/ldap"
 
-    pipeline = Pipeline.create(
-        {
-            "run_id": "ldap-test",
-            "source": {
-                "type": "ldap",
-                "config": {
-                    "ldap_server": "ldap://localhost",
-                    "ldap_user": "cn=admin,dc=example,dc=org",
-                    "ldap_password": "admin",
-                    "base_dn": "dc=example,dc=org",
-                },
-            },
-            "sink": {
-                "type": "file",
-                "config": {
-                    "filename": f"{tmp_path}/ldap_mces.json",
-                },
-            },
-        }
-    )
-    pipeline.run()
-    pipeline.raise_from_status()
+    with docker_compose_runner(
+        test_resources_dir / "docker-compose.yml", "ldap"
+    ) as docker_services:
+        # The openldap container loads the sample data after exposing the port publicly. As such,
+        # we must wait a little bit extra to ensure that the sample data is loaded.
+        wait_for_port(docker_services, "openldap", 389)
+        time.sleep(5)
 
-    output = mce_helpers.load_json_file(str(tmp_path / "ldap_mces.json"))
-    golden = mce_helpers.load_json_file(
-        str(test_resources_dir / "ldap_mce_golden.json")
-    )
-    mce_helpers.assert_mces_equal(output, golden)
+        pipeline = Pipeline.create(
+            {
+                "run_id": "ldap-test",
+                "source": {
+                    "type": "ldap",
+                    "config": {
+                        "ldap_server": "ldap://localhost",
+                        "ldap_user": "cn=admin,dc=example,dc=org",
+                        "ldap_password": "admin",
+                        "base_dn": "dc=example,dc=org",
+                    },
+                },
+                "sink": {
+                    "type": "file",
+                    "config": {
+                        "filename": f"{tmp_path}/ldap_mces.json",
+                    },
+                },
+            }
+        )
+        pipeline.run()
+        pipeline.raise_from_status()
+
+        output = mce_helpers.load_json_file(str(tmp_path / "ldap_mces.json"))
+        golden = mce_helpers.load_json_file(
+            str(test_resources_dir / "ldap_mce_golden.json")
+        )
+        mce_helpers.assert_mces_equal(output, golden)
--- metadata-ingestion/tests/integration/sql_server/test_sql_server.py
+++ metadata-ingestion/tests/integration/sql_server/test_sql_server.py
@@ -1,36 +1,46 @@
 import subprocess
+import time
 
-import fs_helpers
-import mce_helpers
 import pytest
 from click.testing import CliRunner
 
 from datahub.entrypoints import datahub
+from tests.test_helpers import fs_helpers, mce_helpers
+from tests.test_helpers.docker_helpers import wait_for_port
 
 
 @pytest.mark.slow
-def test_mssql_ingest(sql_server, pytestconfig, tmp_path, mock_time):
+def test_mssql_ingest(docker_compose_runner, pytestconfig, tmp_path, mock_time):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/sql_server"
 
-    # Run the setup.sql file to populate the database.
-    docker = "docker"
-    command = f"{docker} exec testsqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'test!Password' -d master -i /setup/setup.sql"
-    ret = subprocess.run(
-        command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    assert ret.returncode == 0
+    with docker_compose_runner(
+        test_resources_dir / "docker-compose.yml", "sql-server"
+    ) as docker_services:
+        # Wait for SQL Server to be ready. We wait an extra couple seconds, as the port being available
+        # does not mean the server is accepting connections.
+        # TODO: find a better way to check for liveness.
+        wait_for_port(docker_services, "testsqlserver", 1433)
+        time.sleep(5)
 
-    # Run the metadata ingestion pipeline.
-    config_file = (test_resources_dir / "mssql_to_file.yml").resolve()
-    runner = CliRunner()
-    with fs_helpers.isolated_filesystem(tmp_path):
-        result = runner.invoke(datahub, ["ingest", "-c", f"{config_file}"])
-        assert result.exit_code == 0
+        # Run the setup.sql file to populate the database.
+        docker = "docker"
+        command = f"{docker} exec testsqlserver /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P 'test!Password' -d master -i /setup/setup.sql"
+        ret = subprocess.run(
+            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+        assert ret.returncode == 0
 
-        output = mce_helpers.load_json_file("mssql_mces.json")
+        # Run the metadata ingestion pipeline.
+        config_file = (test_resources_dir / "mssql_to_file.yml").resolve()
+        runner = CliRunner()
+        with fs_helpers.isolated_filesystem(tmp_path):
+            result = runner.invoke(datahub, ["ingest", "-c", f"{config_file}"])
+            assert result.exit_code == 0
 
-    # Verify the output.
-    golden = mce_helpers.load_json_file(
-        str(test_resources_dir / "mssql_mce_golden.json")
-    )
-    mce_helpers.assert_mces_equal(output, golden)
+            output = mce_helpers.load_json_file("mssql_mces.json")
+
+        # Verify the output.
+        golden = mce_helpers.load_json_file(
+            str(test_resources_dir / "mssql_mce_golden.json")
+        )
+        mce_helpers.assert_mces_equal(output, golden)
