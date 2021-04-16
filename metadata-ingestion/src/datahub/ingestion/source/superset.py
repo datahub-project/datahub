4ef269afe76d9fc52397781b2e7a95bcf373a68e
Gabe Lyons <itsgabelyons@gmail.com>
2021-04-16 09:54:16 -0700

    adding superset source

--- metadata-ingestion/src/datahub/ingestion/source/superset.py
+++ metadata-ingestion/src/datahub/ingestion/source/superset.py
@@ -3,14 +3,16 @@ from typing import Iterable, List, Optional
 
 import requests
 import json
+import dateutil.parser as dp
 
+from datahub.metadata.com.linkedin.pegasus2avro.common import AuditStamp
 from datahub.configuration.common import ConfigModel
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.source.metadata_common import MetadataWorkUnit
-from datahub.metadata.com.linkedin.pegasus2avro.metadata.snapshot import DatasetSnapshot
+from datahub.metadata.com.linkedin.pegasus2avro.metadata.snapshot import DashboardSnapshot, ChartSnapshot
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
-from datahub.metadata.schema_classes import DatasetPropertiesClass
+from datahub.metadata.schema_classes import DashboardInfoClass, ChartInfoClass
 
 PAGE_SIZE = 25
 
@@ -25,25 +27,14 @@ class SupersetConfig(ConfigModel):
 
 
 @dataclass
-class SupersetSourceReport(SourceReport):
-    def __init__(self):
-        pass
-
-    filtered: List[str] = field(default_factory=list)
-
-    def report_dropped(self, name: str) -> None:
-        self.filtered.append(name)
-
-
-@dataclass
 class SupersetSource(Source):
     config: SupersetConfig
-    report: SupersetSourceReport
+    report: SourceReport
 
     def __init__(self, ctx: PipelineContext, config: SupersetConfig):
         super().__init__(ctx)
         self.config = config
-        self.report = SupersetSourceReport()
+        self.report = SourceReport()
 
         options = {}
         if self.config.username is not None:
@@ -67,7 +58,6 @@ class SupersetSource(Source):
         })
 
         self.access_token = login_response.json()['access_token']
-        print(self.access_token)
 
         self.headers = {
             'Authorization': 'Bearer %s' % self.access_token,
@@ -86,6 +76,9 @@ class SupersetSource(Source):
         config = SupersetConfig.parse_obj(config_dict)
         return cls(ctx, config)
 
+    def get_datasource_urn_from_id(self, datasource_id):
+        return 'urn:li:datasource:id'
+
     def get_workunits(self) -> Iterable[MetadataWorkUnit]:
         env = "PROD"
         platform = "superset"
@@ -100,23 +93,85 @@ class SupersetSource(Source):
                 params={'q': '(page:%s,page_size:%s)' % (current_dashboard_page, PAGE_SIZE)},
                 headers=self.headers,
             )
+            current_dashboard_page += 1
+
             payload = dashboard_response.json()
-            print(payload)
             total_dashboards = payload['count']
-            dashboards_in_payload = len(payload['result'])
             for dashboard_data in payload['result']:
-                chart_ids = 
-                raw_position_data = dashboard_data['position_json']
+                dashboard_urn = "urn:li:dashboard:(%s,%s)" % (platform, dashboard_data['id'])
+                dashboard_snapshot = DashboardSnapshot(
+                    urn=dashboard_urn,
+                    aspects=[],
+                )
+
+                modified_actor = "urn:li:corpuser:%s" % (dashboard_data.get('changed_by') or {}).get('username', 'unknown')
+                modified_ts = int(dp.parse(dashboard_data.get('changed_on_utc', '')).timestamp())
+                title = dashboard_data.get('dashboard_title', '')
+                lastModified = AuditStamp(time=modified_ts, actor=modified_actor)
+                dashboardUrl = '%s%s' % (self.config.connect_uri, dashboard_data.get('url', ''))
+
+                chart_urns = []
+                raw_position_data = dashboard_data.get('position_json', '{}')
                 position_data = json.loads(raw_position_data)
-                for key, value in position_data:
+                for key, value in position_data.items():
                     if not key.startswith('CHART-'):
                         continue
+                    chart_urns.append('urn:li:chart:(%s,%s)' % (platform, value.get('meta', {}).get('chartId', 'unknown')))
+
+                dashboard_info = DashboardInfoClass(
+                    title=title, charts=chart_urns, lastModified=lastModified, dashboardUrl=dashboardUrl
+                )
+                dashboard_snapshot.aspects.append(dashboard_info)
+
+                mce = MetadataChangeEvent(proposedSnapshot=dashboard_snapshot)
+                wu = MetadataWorkUnit(id=dashboard_snapshot.urn, mce=mce)
+                self.report.report_workunit(wu)
+
+                yield wu
+
+        current_chart_page = 0
+        # we will set total charts to the actual number after we get the response
+        total_charts = PAGE_SIZE
+
+        while current_chart_page * PAGE_SIZE <= total_charts:
+            chart_response = requests.get(
+                '%s/api/v1/chart' % self.config.connect_uri,
+                params={'q': '(page:%s,page_size:%s)' % (current_chart_page, PAGE_SIZE)},
+                headers=self.headers,
+                )
+            current_chart_page += 1
+
+            payload = chart_response.json()
+            total_charts = payload['count']
+            for chart_data in payload['result']:
+                chart_urn = "urn:li:chart:(%s,%s)" % (platform, chart_data['id'])
+                chart_snapshot = ChartSnapshot(
+                    urn=chart_urn,
+                    aspects=[],
+                )
+
+                modified_actor = "urn:li:corpuser:%s" % (chart_data.get('changed_by') or {}).get('username', 'unknown')
+                modified_ts = int(dp.parse(chart_data.get('changed_on_utc', '')).timestamp())
+                title = chart_data.get('slice_name', '')
+                lastModified = AuditStamp(time=modified_ts, actor=modified_actor)
+                chartUrl = '%s%s' % (self.config.connect_uri, chart_data.get('url', ''))
+
+                chart_urns = []
+                raw_param_data = chart_data.get('params', '{}')
+                datasource_id = chart_data.get('datasource_id')
+                datasource_urn = self.get_datasource_urn_from_id(datasource_id)
 
 
+                chart_info = ChartInfoClass(
+                    title=title, charts=chart_urns, lastModified=lastModified, dashboardUrl=dashboardUrl
+                )
+                dashboard_snapshot.aspects.append(dashboard_info)
 
-            print(len(dashboard_response.json()['result']))
+                mce = MetadataChangeEvent(proposedSnapshot=dashboard_snapshot)
+                wu = MetadataWorkUnit(id=dashboard_snapshot.urn, mce=mce)
+                self.report.report_workunit(wu)
 
-            current_dashboard_page += 1
+                yield wu
 
         # for database_name in dashboard_names:
         #     if not self.config.database_pattern.allowed(database_name):
@@ -153,5 +208,5 @@ class SupersetSource(Source):
         #         self.report.report_workunit(wu)
         #         yield wu
 
-    def get_report(self) -> SupersetSourceReport:
+    def get_report(self) -> SourceReport:
         return self.report
