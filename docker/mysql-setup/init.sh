f6de0f1f66327be86ec4ba1c2b9b7fe81ec8177e
Dexter Lee <dexter@acryl.io>
2021-04-19 00:50:49 -0700

    Add new env

--- docker/mysql-setup/init.sh
+++ docker/mysql-setup/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" \
-  -e "s/USER_NAME/${DATAHUB_DB_NAME}/g" \
+  -e "s/USER_NAME/${MYSQL_USERNAME}/g" \
   -e "s/PASSWORD/${MYSQL_PASSWORD}/g" \
  /init.sql | tee /tmp/init-final.sql
 mysql -u $MYSQL_ROOT_USERNAME -p"$MYSQL_ROOT_PASSWORD" -h $MYSQL_HOST < /tmp/init-final.sql
