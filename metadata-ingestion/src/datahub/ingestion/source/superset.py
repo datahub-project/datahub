acbc1cad453911cfc5109499470bd7a7c0f948ab
Gabe Lyons <itsgabelyons@gmail.com>
2021-04-20 12:33:47 -0700

    almost working except for a ChartType serialization bug

--- metadata-ingestion/src/datahub/ingestion/source/superset.py
+++ metadata-ingestion/src/datahub/ingestion/source/superset.py
@@ -55,37 +55,21 @@ def get_platform_from_sqlalchemy_uri(sqlalchemy_uri: str):
     return 'external'
 
 
-def get_chart_type_from_viz_type(viz_type: str):
-    if viz_type == 'line':
-        return ChartTypeClass.LINE
-    elif viz_type == 'big_number':
-        return ChartTypeClass.LINE
-    elif viz_type == 'table':
-        return ChartTypeClass.TABLE
-    elif viz_type == 'filter_box':
-        return None
-    elif viz_type == 'dist_bar':
-        return ChartTypeClass.BAR
-    elif viz_type == 'filter_box':
-        return None
-    elif viz_type == 'area':
-        return ChartTypeClass.AREA
-    elif viz_type == 'bar':
-        return ChartTypeClass.BAR
-    elif viz_type == 'pie':
-        return ChartTypeClass.PIE
-    elif viz_type == 'histogram':
-        return ChartTypeClass.HISTOGRAM
-    elif viz_type == 'big_number_total':
-        return ChartTypeClass.LINE
-    elif viz_type == 'dual_line':
-        return ChartTypeClass.LINE
-    elif viz_type == 'line_multi':
-        return ChartTypeClass.LINE
-    elif viz_type == 'treemap':
-        return ChartTypeClass.AREA
-    elif viz_type == 'box_plot':
-        return ChartTypeClass.BAR
+chart_type_from_viz_type = {
+    'line': ChartTypeClass.LINE,
+    'big_number': ChartTypeClass.LINE,
+    'table': ChartTypeClass.TABLE,
+    'dist_bar': ChartTypeClass.BAR,
+    'area': ChartTypeClass.AREA,
+    'bar': ChartTypeClass.BAR,
+    'pie': ChartTypeClass.PIE,
+    'histogram': ChartTypeClass.HISTOGRAM,
+    'big_number_total': ChartTypeClass.LINE,
+    'dual_line': ChartTypeClass.LINE,
+    'line_multi': ChartTypeClass.LINE,
+    'treemap': ChartTypeClass.AREA,
+    'box_plot': ChartTypeClass.BAR,
+}
 
 
 @dataclass
@@ -101,37 +85,24 @@ class SupersetSource(Source):
         self.config = config
         self.report = SourceReport()
 
-        options = {}
-        if self.config.username is not None:
-            options["username"] = self.config.username
-        if self.config.password is not None:
-            options["password"] = self.config.password
-        if self.config.provider is not None:
-            options["provider"] = self.config.provider
-        if self.config.connect_uri is not None:
-            options["connect_uri"] = self.config.connect_uri
-        options = {
-            **options,
-            **self.config.options,
-        }
-
-        login_response = requests.post('%s/api/v1/security/login' % options['connect_uri'], None, {
-            'username': options['username'],
-            'password': options['password'],
+        login_response = requests.post(f"{self.config.connect_uri}/api/v1/security/login", None, {
+            'username': self.config.username,
+            'password': self.config.password,
             'refresh': True,
-            'provider': options['provider']
+            'provider': self.config.provider
         })
 
         self.access_token = login_response.json()['access_token']
 
-        self.headers = {
-            'Authorization': 'Bearer %s' % self.access_token,
+        self.session = requests.Session()
+        self.session.headers.update({
+            'Authorization': f"Bearer {self.access_token}",
             'Content-Type': 'application/json',
             'Accept': '*/*',
-        }
+        })
 
         # Test the connection
-        test_response = requests.get('%s/api/v1/database' % options['connect_uri'], None, headers=self.headers)
+        test_response = self.session.get(f"{self.config.connect_uri}/api/v1/database")
         if test_response.status_code == 200:
             pass
             # TODO(Gabe): how should we message about this error?
@@ -142,20 +113,18 @@ class SupersetSource(Source):
         config = SupersetConfig.parse_obj(config_dict)
         return cls(ctx, config)
 
-    @lru_cache(maxsize=50000)
+    @lru_cache(maxsize=None)
     def get_platform_from_database_id(self, database_id, env):
-        database_response = requests.get(
-            '%s/api/v1/database/%s' % (self.config.connect_uri, database_id),
-            headers=self.headers
+        database_response = self.session.get(
+            f"{self.config.connect_uri}/api/v1/database/{database_id}"
          ).json()
         sqlalchemy_uri = database_response.get('result', {}).get('sqlalchemy_uri')
         return get_platform_from_sqlalchemy_uri(sqlalchemy_uri)
 
-    @lru_cache(maxsize=50000)
+    @lru_cache(maxsize=None)
     def get_datasource_urn_from_id(self, datasource_id, env):
-        dataset_response = requests.get(
-            '%s/api/v1/dataset/%s' % (self.config.connect_uri, datasource_id),
-            headers=self.headers,
+        dataset_response = self.session.get(
+            f"{self.config.connect_uri}/api/v1/dataset/{datasource_id}"
             ).json()
         schema_name = dataset_response.get('result', {}).get('schema')
         table_name = dataset_response.get('result', {}).get('table_name')
@@ -181,10 +150,9 @@ class SupersetSource(Source):
         total_dashboards = PAGE_SIZE
 
         while current_dashboard_page * PAGE_SIZE <= total_dashboards:
-            dashboard_response = requests.get(
-                '%s/api/v1/dashboard' % self.config.connect_uri,
-                params={'q': '(page:%s,page_size:%s)' % (current_dashboard_page, PAGE_SIZE)},
-                headers=self.headers,
+            dashboard_response = self.session.get(
+                f"{self.config.connect_uri}/api/v1/dashboard",
+                params=f"q=(page:{current_dashboard_page},page_size:{PAGE_SIZE})"
             )
             payload = dashboard_response.json()
             total_dashboards = payload.get('count') or 0
@@ -192,22 +160,24 @@ class SupersetSource(Source):
             current_dashboard_page += 1
 
             payload = dashboard_response.json()
+            print(payload)
             for dashboard_data in payload['result']:
-                dashboard_urn = "urn:li:dashboard:(%s,%s)" % (platform, dashboard_data['id'])
+                dashboard_urn = f"urn:li:dashboard:({platform},{dashboard_data['id']})"
                 dashboard_snapshot = DashboardSnapshot(
                     urn=dashboard_urn,
                     aspects=[],
                 )
 
-                modified_actor = "urn:li:corpuser:%s" % (dashboard_data.get('changed_by') or {}).get('username', 'unknown')
-                modified_ts = int(dp.parse(dashboard_data.get('changed_on_utc', '')).timestamp())
+                modified_actor = \
+                    f"urn:li:corpuser:{(dashboard_data.get('changed_by') or {}).get('username', 'unknown')}"
+                modified_ts = int(dp.parse(dashboard_data.get('changed_on_utc', 'now')).timestamp())
                 title = dashboard_data.get('dashboard_title', '')
                 # note: the API does not currently supply created_by usernames due to a bug, but we are required to
                 # provide a created AuditStamp to comply with ChangeAuditStamp model. For now, I sub in the last
                 # modified actor urn
                 last_modified = ChangeAuditStamps(created=AuditStamp(time=modified_ts, actor=modified_actor),
                                                   lastModified=AuditStamp(time=modified_ts, actor=modified_actor))
-                dashboard_url = '%s%s' % (self.config.connect_uri[:-1], dashboard_data.get('url', ''))
+                dashboard_url = f"{self.config.connect_uri[:-1]}{dashboard_data.get('url', '')}"
 
                 chart_urns = []
                 raw_position_data = dashboard_data.get('position_json', '{}')
@@ -215,9 +185,10 @@ class SupersetSource(Source):
                 for key, value in position_data.items():
                     if not key.startswith('CHART-'):
                         continue
-                    chart_urns.append('urn:li:chart:(%s,%s)' % (platform, value.get('meta', {}).get('chartId', 'unknown')))
+                    chart_urns.append(f"urn:li:chart:({platform},{value.get('meta', {}).get('chartId', 'unknown')})")
 
                 dashboard_info = DashboardInfoClass(
+                    description='',
                     title=title, charts=chart_urns, lastModified=last_modified, dashboardUrl=dashboard_url
                 )
                 dashboard_snapshot.aspects.append(dashboard_info)
@@ -233,24 +204,23 @@ class SupersetSource(Source):
         total_charts = PAGE_SIZE
 
         while current_chart_page * PAGE_SIZE <= total_charts:
-            chart_response = requests.get(
-                '%s/api/v1/chart' % self.config.connect_uri,
-                params={'q': '(page:%s,page_size:%s)' % (current_chart_page, PAGE_SIZE)},
-                headers=self.headers,
+            chart_response = self.session.get(
+                f"{self.config.connect_uri}/api/v1/chart",
+                params=f"q=(page:{current_chart_page},page_size:{PAGE_SIZE})"
                 )
             current_chart_page += 1
 
             payload = chart_response.json()
             total_charts = payload['count']
             for chart_data in payload['result']:
-                chart_urn = "urn:li:chart:(%s,%s)" % (platform, chart_data['id'])
+                chart_urn = f"urn:li:chart:({platform},{chart_data['id']})"
                 chart_snapshot = ChartSnapshot(
                     urn=chart_urn,
                     aspects=[],
                 )
 
-                modified_actor = "urn:li:corpuser:%s" % (chart_data.get('changed_by') or {}).get('username', 'unknown')
-                modified_ts = int(dp.parse(chart_data.get('changed_on_utc', '')).timestamp())
+                modified_actor = f"urn:li:corpuser:{(chart_data.get('changed_by') or {}).get('username', 'unknown')}"
+                modified_ts = int(dp.parse(chart_data.get('changed_on_utc', 'now')).timestamp())
                 title = chart_data.get('slice_name', '')
 
                 # note: the API does not currently supply created_by usernames due to a bug, but we are required to
@@ -258,17 +228,18 @@ class SupersetSource(Source):
                 # modified actor urn
                 last_modified = ChangeAuditStamps(created=AuditStamp(time=modified_ts, actor=modified_actor),
                                                   lastModified=AuditStamp(time=modified_ts, actor=modified_actor))
-                chart_url = '%s%s' % (self.config.connect_uri[:-1], chart_data.get('url', ''))
+                chart_url = f"{self.config.connect_uri[:-1]}{chart_data.get('url', '')}"
 
                 datasource_id = chart_data.get('datasource_id')
                 datasource_urn = self.get_datasource_urn_from_id(datasource_id, env)
-                chart_type = get_chart_type_from_viz_type(
+                chart_type = chart_type_from_viz_type.get(
                     chart_data.get('viz_type')
                 )
 
                 chart_info = ChartInfoClass(
+                    description='',
                     title=title, lastModified=last_modified, chartUrl=chart_url,
-                    type=chart_type,
+                    type=ChartTypeClass.TEXT,
                     inputs=[datasource_urn] if datasource_urn else None
                 )
                 chart_snapshot.aspects.append(chart_info)
