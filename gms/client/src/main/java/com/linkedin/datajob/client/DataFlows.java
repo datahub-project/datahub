93c15350cec51b50fba6c2c03abcb71074a3c115
Dexter Lee <dexter@acryl.io>
2021-04-22 13:33:35 -0700

    Add browse types

--- gms/client/src/main/java/com/linkedin/datajob/client/DataFlows.java
+++ gms/client/src/main/java/com/linkedin/datajob/client/DataFlows.java
@@ -2,14 +2,17 @@ package com.linkedin.datajob.client;
 
 import com.linkedin.common.urn.DataFlowUrn;
 import com.linkedin.data.template.StringArray;
-import com.linkedin.metadata.query.AutoCompleteResult;
-import com.linkedin.metadata.query.SortCriterion;
-import com.linkedin.metadata.restli.BaseSearchableClient;
-import com.linkedin.datajob.DataFlow;
-import com.linkedin.datajob.DataFlowKey;
 import com.linkedin.dataflow.DataFlowsDoAutocompleteRequestBuilder;
+import com.linkedin.dataflow.DataFlowsDoBrowseRequestBuilder;
+import com.linkedin.dataflow.DataFlowsDoGetBrowsePathsRequestBuilder;
 import com.linkedin.dataflow.DataFlowsFindBySearchRequestBuilder;
 import com.linkedin.dataflow.DataFlowsRequestBuilders;
+import com.linkedin.datajob.DataFlow;
+import com.linkedin.datajob.DataFlowKey;
+import com.linkedin.metadata.query.AutoCompleteResult;
+import com.linkedin.metadata.query.BrowseResult;
+import com.linkedin.metadata.query.SortCriterion;
+import com.linkedin.metadata.restli.BaseBrowsableClient;
 import com.linkedin.r2.RemoteInvocationException;
 import com.linkedin.restli.client.BatchGetEntityRequest;
 import com.linkedin.restli.client.Client;
@@ -23,9 +26,10 @@ import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-import static com.linkedin.metadata.dao.utils.QueryUtils.*;
+import static com.linkedin.metadata.dao.utils.QueryUtils.newFilter;
 
-public class DataFlows extends BaseSearchableClient<DataFlow> {
+
+public class DataFlows extends BaseBrowsableClient<DataFlow, DataFlowUrn> {
     private static final DataFlowsRequestBuilders DATA_FLOWS_REQUEST_BUILDERS = new DataFlowsRequestBuilders();
 
     public DataFlows(@Nonnull Client restliClient) {
@@ -69,6 +73,46 @@ public class DataFlows extends BaseSearchableClient<DataFlow> {
             .build();
 
         return _client.sendRequest(getRequest).getResponse().getEntity();
+    }
+
+    /**
+     * Gets browse path(s) given dataset urn
+     *
+     * @param urn urn for the entity
+     * @return list of paths given urn
+     * @throws RemoteInvocationException
+     */
+    @Nonnull
+    @Override
+    public StringArray getBrowsePaths(@Nonnull DataFlowUrn urn) throws RemoteInvocationException {
+        DataFlowsDoGetBrowsePathsRequestBuilder requestBuilder = DATA_FLOWS_REQUEST_BUILDERS
+            .actionGetBrowsePaths()
+            .urnParam(urn);
+        return _client.sendRequest(requestBuilder.build()).getResponse().getEntity();
+    }
+
+    /**
+     * Gets browse snapshot of a given path
+     *
+     * @param path path being browsed
+     * @param requestFilters browse filters
+     * @param start start offset of first dataset
+     * @param limit max number of datasets
+     * @throws RemoteInvocationException
+     */
+    @Nonnull
+    @Override
+    public BrowseResult browse(@Nonnull String path, @Nullable Map<String, String> requestFilters,
+        int start, int limit) throws RemoteInvocationException {
+        DataFlowsDoBrowseRequestBuilder requestBuilder = DATA_FLOWS_REQUEST_BUILDERS
+            .actionBrowse()
+            .pathParam(path)
+            .startParam(start)
+            .limitParam(limit);
+        if (requestFilters != null) {
+            requestBuilder.filterParam(newFilter(requestFilters));
+        }
+        return _client.sendRequest(requestBuilder.build()).getResponse().getEntity();
     }
 
     /**
