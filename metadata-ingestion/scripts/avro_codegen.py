55aac8d7c40ded018ee1019c5866c0e6d9861720
Harshal Sheth <hsheth2@gmail.com>
2021-05-05 17:32:13 -0700

    codegen audit event schemas too

--- metadata-ingestion/scripts/avro_codegen.py
+++ metadata-ingestion/scripts/avro_codegen.py
@@ -1,7 +1,46 @@
 import json
+import types
+import unittest.mock
+from typing import List
 
+import avro.schema
 import click
 from avrogen import write_schema_files
+
+
+def load_schema_file(schema_file: str) -> str:
+    with open(schema_file) as f:
+        raw_schema_text = f.read()
+
+    redo_spaces = json.dumps(json.loads(raw_schema_text), indent=2)
+    return redo_spaces
+
+
+def merge_schemas(schemas: List[str]) -> str:
+    # Combine schemas.
+    schemas_obj = [json.loads(schema) for schema in schemas]
+    merged = ["null"] + schemas_obj
+
+    # Deduplicate repeated names.
+    def Register(self, schema):
+        if schema.fullname in self._names:
+            print(f"deduping {schema.fullname}")
+        else:
+            self._names[schema.fullname] = schema
+
+    with unittest.mock.patch("avro.schema.Names.Register", Register):
+        cleaned_schema = avro.schema.SchemaFromJSONData(merged)
+
+    # Convert back to an Avro schema JSON representation.
+    class MappingProxyEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, types.MappingProxyType):
+                return dict(obj)
+            return json.JSONEncoder.default(self, obj)
+
+    out_schema = cleaned_schema.to_json()
+    encoded = json.dumps(out_schema, cls=MappingProxyEncoder, indent=2)
+    return encoded
 
 
 def suppress_checks_in_file(filepath: str) -> None:
@@ -18,21 +57,17 @@ def suppress_checks_in_file(filepath: str) -> None:
 
 
 @click.command()
-@click.argument("schema_file", type=click.Path(exists=True))
-@click.argument("outdir", type=click.Path())
-def generate(schema_file: str, outdir: str) -> None:
-    # print(f'using {schema_file}')
-    with open(schema_file) as f:
-        raw_schema_text = f.read()
+@click.argument("schema_files", type=click.Path(exists=True), nargs=-1, required=True)
+@click.argument("outdir", type=click.Path(), required=True)
+def generate(schema_files: List[str], outdir: str) -> None:
+    schemas = []
+    for schema_file in schema_files:
+        schema = load_schema_file(schema_file)
+        schemas.append(schema)
 
-    no_spaces_schema = json.dumps(json.loads(raw_schema_text))
-    schema_json = no_spaces_schema.replace(
-        '{"type": "string", "avro.java.string": "String"}', '"string"'
-    )
+    merged_schema = merge_schemas(schemas)
 
-    redo_spaces = json.dumps(json.loads(schema_json), indent=2)
-
-    write_schema_files(redo_spaces, outdir)
+    write_schema_files(merged_schema, outdir)
     suppress_checks_in_file(f"{outdir}/schema_classes.py")
     with open(f"{outdir}/__init__.py", "w"):
         # Truncate this file.
