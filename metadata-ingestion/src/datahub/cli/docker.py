760886d376e5762b17f424612141394ebd223a67
Harshal Sheth <hsheth2@gmail.com>
2021-05-06 19:41:39 -0700

    add better print messages

--- metadata-ingestion/src/datahub/cli/docker.py
+++ metadata-ingestion/src/datahub/cli/docker.py
@@ -11,8 +11,8 @@ import requests
 
 from datahub.cli.docker_check import check_local_docker_containers
 
-# QUICKSTART_COMPOSE_URL = "https://raw.githubusercontent.com/linkedin/datahub/master/docker/cli/docker-compose.quickstart.yml"
-QUICKSTART_COMPOSE_URL = "https://raw.githubusercontent.com/linkedin/datahub/master/docker/docker-compose.override.yml"
+SIMPLE_QUICKSTART_COMPOSE_FILE = "docker/cli/docker-compose.quickstart.yml"
+QUICKSTART_COMPOSE_URL = f"https://raw.githubusercontent.com/linkedin/datahub/master/{SIMPLE_QUICKSTART_COMPOSE_FILE}"
 
 
 @click.group()
@@ -47,17 +47,22 @@ def check() -> None:
 
 
 @docker.command()
-@click.option("--use-local-quickstart", type=bool, is_flag=True, default=False)
-def quickstart(use_local_quickstart: bool) -> None:
+@click.option(
+    "--use-locally-generated-quickstart",
+    type=bool,
+    is_flag=True,
+    default=False,
+    help=f"Use the local {SIMPLE_QUICKSTART_COMPOSE_FILE} instead of pulling from GitHub",
+)
+def quickstart(use_locally_generated_quickstart: bool) -> None:
     # Run pre-flight checks.
     issues = check_local_docker_containers(preflight_only=True)
     if issues:
         _print_issue_list_and_exit(issues, "Unable to run quickstart:")
 
-    if use_local_quickstart:
+    if use_locally_generated_quickstart:
         path = (
-            pathlib.Path(__file__)
-            / "../../../../../docker/cli/docker-compose.quickstart.yml"
+            pathlib.Path(__file__) / f"../../../../../{SIMPLE_QUICKSTART_COMPOSE_FILE}"
         ).resolve()
     else:
         with tempfile.NamedTemporaryFile(suffix=".yml", delete=False) as tmp_file:
@@ -81,6 +86,7 @@ def quickstart(use_local_quickstart: bool) -> None:
     while (datetime.datetime.now() - start_time) < max_wait_time:
         # Attempt to run docker-compose up every minute.
         if (datetime.datetime.now() - start_time) > up_attempts * up_interval:
+            click.echo()
             subprocess.run(base_command + ["up", "-d"])
             up_attempts += 1
 
@@ -90,9 +96,11 @@ def quickstart(use_local_quickstart: bool) -> None:
             break
 
         # Wait until next iteration.
+        click.echo(".", nl=False)
         time.sleep(sleep_interval.total_seconds())
     else:
         # Falls through if the while loop doesn't exit via break.
+        click.echo()
         with tempfile.NamedTemporaryFile(suffix=".log", delete=False) as log_file:
             ret = subprocess.run(
                 base_command + ["logs"],
@@ -111,6 +119,7 @@ def quickstart(use_local_quickstart: bool) -> None:
         )
 
     # Handle success condition.
+    click.echo()
     click.secho("✔ DataHub is now running", fg="green")
     click.secho(
         "Ingest some demo data using `datahub docker ingest-sample-data`,\n"
@@ -119,13 +128,5 @@ def quickstart(use_local_quickstart: bool) -> None:
     )
 
 
-@docker.command()
-def clean() -> None:
-    # TODO this
-    pass
-
-
-@docker.command()
-def ingest_sample_data() -> None:
-    # TODO this
-    pass
+# TODO add a clean command
+# TODO add an ingest sample data command
