a75f4f35b0e57cad7e2f8ecc49a8179981b7d68c
Harshal Sheth <hsheth2@gmail.com>
2021-05-06 19:13:29 -0700

    add local + pull methods to docker quickstart

--- metadata-ingestion/src/datahub/cli/docker.py
+++ metadata-ingestion/src/datahub/cli/docker.py
@@ -1,4 +1,5 @@
 import datetime
+import pathlib
 import subprocess
 import sys
 import tempfile
@@ -6,8 +7,12 @@ import time
 from typing import List, NoReturn, Optional
 
 import click
+import requests
 
 from datahub.cli.docker_check import check_local_docker_containers
+
+# QUICKSTART_COMPOSE_URL = "https://raw.githubusercontent.com/linkedin/datahub/master/docker/cli/docker-compose.quickstart.yml"
+QUICKSTART_COMPOSE_URL = "https://raw.githubusercontent.com/linkedin/datahub/master/docker/docker-compose.override.yml"
 
 
 @click.group()
@@ -42,54 +47,76 @@ def check() -> None:
 
 
 @docker.command()
-def quickstart() -> None:
+@click.option("--use-local-quickstart", type=bool, is_flag=True, default=False)
+def quickstart(use_local_quickstart: bool) -> None:
     # Run pre-flight checks.
     issues = check_local_docker_containers(preflight_only=True)
     if issues:
         _print_issue_list_and_exit(issues, "Unable to run quickstart:")
 
-    with tempfile.TemporaryDirectory() as workdir:
-        # TODO pull file using requests
-        workdir = "../docker"
+    if use_local_quickstart:
+        path = (
+            pathlib.Path(__file__)
+            / "../../../../../docker/cli/docker-compose.quickstart.yml"
+        ).resolve()
+    else:
+        with tempfile.NamedTemporaryFile(suffix=".yml", delete=False) as tmp_file:
+            path = pathlib.Path(tmp_file.name)
 
-        # Pull the latest containers.
-        subprocess.run(["docker-compose", "pull"], cwd=workdir)
+            # Download the quickstart docker-compose file from GitHub.
+            quickstart_download_response = requests.get(QUICKSTART_COMPOSE_URL)
+            quickstart_download_response.raise_for_status()
+            tmp_file.write(quickstart_download_response.content)
 
-        start_time = datetime.datetime.now()
-        sleep_interval = datetime.timedelta(seconds=2)
-        up_interval = datetime.timedelta(minutes=1)
-        up_attempts = 0
-        while (datetime.datetime.now() - start_time) < datetime.timedelta(minutes=5):
-            # Attempt to run docker-compose up every minute.
-            if (datetime.datetime.now() - start_time) > up_attempts * up_interval:
-                subprocess.run(
-                    ["docker-compose", "-p", "datahub", "up", "-d"], cwd=workdir
-                )
-                up_attempts += 1
+    base_command = ["docker-compose", "-f", f"{path}", "-p", "datahub"]
 
-            # Check docker health every few seconds.
-            issues = check_local_docker_containers()
-            if not issues:
-                break
+    # Pull the latest containers.
+    subprocess.run(base_command + ["pull"], check=True)
 
-            # Wait until next iteration.
-            time.sleep(sleep_interval.total_seconds())
-        else:
-            # Falls through if the while loop doesn't exit via break.
-            # TODO: save docker-compose logs somewhere
-            _print_issue_list_and_exit(
-                issues,
-                header="Unable to run quickstart - the following issues were detected:",
-                footer="If you think something went wrong, please file an issue at TODO",
+    max_wait_time = datetime.timedelta(minutes=5)
+    start_time = datetime.datetime.now()
+    sleep_interval = datetime.timedelta(seconds=2)
+    up_interval = datetime.timedelta(minutes=1)
+    up_attempts = 0
+    while (datetime.datetime.now() - start_time) < max_wait_time:
+        # Attempt to run docker-compose up every minute.
+        if (datetime.datetime.now() - start_time) > up_attempts * up_interval:
+            subprocess.run(base_command + ["up", "-d"])
+            up_attempts += 1
+
+        # Check docker health every few seconds.
+        issues = check_local_docker_containers()
+        if not issues:
+            break
+
+        # Wait until next iteration.
+        time.sleep(sleep_interval.total_seconds())
+    else:
+        # Falls through if the while loop doesn't exit via break.
+        with tempfile.NamedTemporaryFile(suffix=".log", delete=False) as log_file:
+            ret = subprocess.run(
+                base_command + ["logs"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=True,
             )
+            log_file.write(ret.stdout)
 
-        # Handle success condition.
-        click.secho("✔ DataHub is now running", fg="green")
-        click.secho(
-            "Ingest some demo data using `datahub docker ingest-sample-data`,\n"
-            "or head to http://localhost:9002 to play around with the frontend.",
-            fg="green",
+        _print_issue_list_and_exit(
+            issues,
+            header="Unable to run quickstart - the following issues were detected:",
+            footer="If you think something went wrong, please file an issue at https://github.com/linkedin/datahub/issues\n"
+            "or send a message in our Slack https://slack.datahubproject.io/\n"
+            f"Be sure to attach the logs from {log_file.name}",
         )
+
+    # Handle success condition.
+    click.secho("✔ DataHub is now running", fg="green")
+    click.secho(
+        "Ingest some demo data using `datahub docker ingest-sample-data`,\n"
+        "or head to http://localhost:9002 to play around with the frontend.",
+        fg="green",
+    )
 
 
 @docker.command()
