3000464e2d98b12992dbec89c9a0cf4c98a02c71
Dexter Lee <dexter@acryl.io>
2021-05-10 15:16:27 -0700

    In progress

--- entity-registry/src/main/java/com/linkedin/metadata/models/annotation/SearchableAnnotation.java
+++ entity-registry/src/main/java/com/linkedin/metadata/models/annotation/SearchableAnnotation.java
@@ -1,29 +1,52 @@
 package com.linkedin.metadata.models.annotation;
 
+import java.util.List;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+
 
 /**
  * Simple object representation of the @Searchable annotation metadata.
  */
+@RequiredArgsConstructor
 public class SearchableAnnotation {
 
-    private final IndexType _indexType;
+    private final String _fieldName;
+    private final List<IndexSetting> _indexSettings;
 
-    // TODO Expand.
     public enum IndexType {
-        KEYWORD
+        KEYWORD,
+        BOOLEAN,
+        BROWSE_PATH,
+        DELIMITED,
+        PATTERN,
+        PARTIAL,
+        PARTIAL_SHORT,
+        PARTIAL_LONG,
+        PARTIAL_PATTERN
     }
+
+    @Value
+    public static class IndexSetting {
+        IndexType indexType;
+        boolean queryByDefault;
+        Optional<Double> boostScore;
+    }
+
+    private static 
 
     public static SearchableAnnotation fromPegasusAnnotationObject(@Nonnull final Object annotationObj) {
         if (Map.class.isAssignableFrom(annotationObj.getClass())) {
             Map map = (Map) annotationObj;
-            final Object indexTypeObj = map.get("indexType");
-            if (indexTypeObj == null || !String.class.isAssignableFrom(indexTypeObj.getClass())) {
+            final Object indexNameObj = map.get("indexName");
+            if (indexNameObj == null || !String.class.isAssignableFrom(indexNameObj.getClass())) {
                 throw new IllegalArgumentException("Failed to validate required Searchable field 'indexType' field of type String");
             }
             try {
-                return new SearchableAnnotation(IndexType.valueOf((String) indexTypeObj));
+                return new SearchableAnnotation((String) indexNameObj);
             } catch (IllegalArgumentException e) {
                 throw new IllegalArgumentException(String.format(
                         "Failed to validate required Searchable field 'indexType'. Invalid indexType provided. Valid types are %s",
@@ -31,10 +54,6 @@ public class SearchableAnnotation {
             }
         }
         throw new IllegalArgumentException("Failed to validate Searchable annotation object: Invalid value type provided (Expected Map)");
-    }
-
-    public SearchableAnnotation(final IndexType indexType) {
-        _indexType = indexType;
     }
 
     public IndexType getIndexType() {
