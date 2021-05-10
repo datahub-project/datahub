e9209d8d1a82779c9415f41a43ee2ad625904ea6
Kevin Hu <kevinhuwest@gmail.com>
2021-05-10 16:30:33 -0700

    Rename tests

--- metadata-ingestion/tests/integration/mongodb/setup/mongo_init.js
+++ metadata-ingestion/tests/integration/mongodb/setup/mongo_init.js
@@ -1,7 +1,8 @@
-
-db.mycollection.createIndex({ myfield: 1 }, { unique: true }),
-db.mycollection.createIndex({ thatfield: 1 }),
-db.mycollection.insert({ myfield: 'hello1', thatfield: 'testing', noindex: 8}),
-db.mycollection.insert({ myfield: 'hello2', thatfield: 'testing', noindex: 2}),
-db.mycollection.insert({ myfield: 'hello3', thatfield: 'testing', noindex: 5}),
-db.mycollection.insert({ myfield: 'hello5', thatfield: 'testing', noindex: 2})
+db.firstCollection.createIndex({ myfield: 1 }, { unique: true });
+db.firstCollection.createIndex({ thatfield: 1 });
+db.firstCollection.insertMany([
+  { myfield: "hello1", thatfield: "testing", noindex: 8 },
+  { myfield: "hello2", thatfield: "testing", noindex: 2 },
+  { myfield: "hello3", thatfield: "testing", noindex: 5 },
+  { myfield: "hello5", thatfield: "testing", noindex: 2 },
+]);
