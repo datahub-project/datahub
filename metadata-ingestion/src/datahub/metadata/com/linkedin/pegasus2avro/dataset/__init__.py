e856b113c8eb920dc0859dce35869f1bc017e7af
Harshal Sheth <hsheth2@gmail.com>
2021-05-10 19:36:23 -0700

    feat(ingest): check in generated schema files (#2503)

---
+++ metadata-ingestion/src/datahub/metadata/com/linkedin/pegasus2avro/dataset/__init__.py
@@ -0,0 +1,16 @@
+from .....schema_classes import DatasetDeprecationClass
+from .....schema_classes import DatasetFieldMappingClass
+from .....schema_classes import DatasetLineageTypeClass
+from .....schema_classes import DatasetPropertiesClass
+from .....schema_classes import DatasetUpstreamLineageClass
+from .....schema_classes import UpstreamClass
+from .....schema_classes import UpstreamLineageClass
+
+
+DatasetDeprecation = DatasetDeprecationClass
+DatasetFieldMapping = DatasetFieldMappingClass
+DatasetLineageType = DatasetLineageTypeClass
+DatasetProperties = DatasetPropertiesClass
+DatasetUpstreamLineage = DatasetUpstreamLineageClass
+Upstream = UpstreamClass
+UpstreamLineage = UpstreamLineageClass
---
+++ metadata-ingestion/src/datahub/metadata/com/linkedin/pegasus2avro/identity/__init__.py
@@ -0,0 +1,8 @@
+from .....schema_classes import CorpGroupInfoClass
+from .....schema_classes import CorpUserEditableInfoClass
+from .....schema_classes import CorpUserInfoClass
+
+
+CorpGroupInfo = CorpGroupInfoClass
+CorpUserEditableInfo = CorpUserEditableInfoClass
+CorpUserInfo = CorpUserInfoClass
