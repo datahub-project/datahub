94166df3579aa3db0a100c6fdb488f8d4cdbc4db
Kevin Hu <kevinhuwest@gmail.com>
2021-05-10 17:10:40 -0700

    Update mongo tests

--- metadata-ingestion/tests/integration/mongodb/setup/mongo_init.js
+++ metadata-ingestion/tests/integration/mongodb/setup/mongo_init.js
@@ -1,8 +1,75 @@
-db.firstCollection.createIndex({ myfield: 1 }, { unique: true });
-db.firstCollection.createIndex({ thatfield: 1 });
+db.firstCollection.createIndex({ name: 1 }, { unique: true });
+db.firstCollection.createIndex({ legs: 1 });
+db.firstCollection.createIndex({ floatField: 1 });
+db.firstCollection.createIndex({ floatField: 1 });
 db.firstCollection.insertMany([
-  { myfield: "hello1", thatfield: "testing", noindex: 8 },
-  { myfield: "hello2", thatfield: "testing", noindex: 2 },
-  { myfield: "hello3", thatfield: "testing", noindex: 5 },
-  { myfield: "hello5", thatfield: "testing", noindex: 2 },
+  {
+    name: "garfield",
+    type: "cat",
+    legs: 4,
+    age: 8.3,
+    tags: ["lasagna", "mondays", "sleep", "lazy"],
+    seen: Date.now(),
+    canSwim: false,
+    favoriteFood: {
+      name: "lasagna",
+      calories: 4000,
+      ingredients: [
+        { name: "pasta", from: "italy" },
+        { name: "cheese", from: "california" },
+        { name: "tomatoes", from: "whole foods" },
+      ],
+      servings: 43.2,
+      emptyObject: {},
+    },
+    favoriteColor: "orange",
+  },
+  {
+    name: "odie",
+    type: "dog",
+    legs: 4,
+    age: 5.2,
+    tags: ["woof"],
+    seen: Date.now(),
+    canSwim: false,
+    favoriteFood: {
+      name: "dog food",
+      calories: 1000,
+      ingredients: [{ name: "mystery meat", from: "walmart" }],
+    },
+    servings: 1,
+    emptyObject: {},
+  },
+  {
+    name: "jon",
+    type: "human",
+    legs: 2,
+    age: 32.1,
+    tags: ["boring"],
+    seen: Date.now(),
+    canSwim: false,
+    favoriteFood: {
+      name: "who knows",
+      calories: null,
+      ingredients: [],
+    },
+    servings: 1,
+  },
+  {
+    name: "george",
+    type: "octopus",
+    legs: 8,
+    age: 23,
+    tags: ["blub", "glug", "ink"],
+    seen: Date.now(),
+    canSwim: true,
+    favoriteFood: {
+      name: "crab",
+      calories: 500,
+      ingredients: [{ name: "crab", from: "the bay" }],
+    },
+    servings: 1,
+  },
 ]);
+
+db.emptyCollection.createIndex({ stringField: 1 }, { unique: true });
