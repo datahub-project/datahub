a97c237eb6acdd1fd1aa3869d31782ec158a578a
Fredrik Sannholm <fredrik.sannholm@wolt.com>
2021-05-11 10:39:32 +0300

    Remove use of mock_time

--- metadata-ingestion/tests/integration/lookml/test_lookml.py
+++ metadata-ingestion/tests/integration/lookml/test_lookml.py
@@ -7,7 +7,7 @@ from freezegun import freeze_time
 logging.getLogger("lkml").setLevel(logging.INFO)
 
 @freeze_time("2021-05-08 07:00:00")
-def test_lookml_ingest(pytestconfig, tmp_path, mock_time):
+def test_lookml_ingest(pytestconfig, tmp_path):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/lookml"
 
     print(str(test_resources_dir))
