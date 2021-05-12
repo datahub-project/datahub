1203487fe7b231a0eaf9867d784a69b89c2e1dfd
Dexter Lee <dexter@acryl.io>
2021-05-12 11:50:32 -0700

    Add transformer skeleton

--- metadata-io/src/main/java/com/linkedin/metadata/search/transformer/SearchDocumentTransformer.java
+++ metadata-io/src/main/java/com/linkedin/metadata/search/transformer/SearchDocumentTransformer.java
@@ -1,4 +1,8 @@
 package com.linkedin.metadata.search.transformer;
 
+import com.linkedin.data.template.RecordTemplate;
+
+
 public class SearchDocumentTransformer {
+  public static transform(final RecordTemplate snapshot)
 }
