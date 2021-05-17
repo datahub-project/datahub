ebe74098973296d82e2ace98b05c01cd02b52997
Harshal Sheth <hsheth2@gmail.com>
2021-05-17 11:50:38 -0700

    fix(cli): prevent click from suppressing errors (#2560)

--- metadata-ingestion/src/datahub/entrypoints.py
+++ metadata-ingestion/src/datahub/entrypoints.py
@@ -77,3 +77,12 @@ def ingest(config: str) -> None:
 
 
 datahub.add_command(check)
+
+
+def main(**kwargs):
+    # This wrapper prevents click from suppressing errors.
+    try:
+        sys.exit(datahub(standalone_mode=False, **kwargs))
+    except click.ClickException as error:
+        error.show()
+        sys.exit(1)
