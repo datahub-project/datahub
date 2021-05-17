f5aff1e0d879a615329cf60fcdd5dc566340b8ae
Harshal Sheth <hsheth2@gmail.com>
2021-05-17 11:50:38 -0700

    fix(cli): prevent click from suppressing errors (#2560)

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -146,7 +146,7 @@ dev_requirements_airflow_2 = {
 
 
 entry_points = {
-    "console_scripts": ["datahub = datahub.entrypoints:datahub"],
+    "console_scripts": ["datahub = datahub.entrypoints:main"],
     "datahub.ingestion.source.plugins": [
         "file = datahub.ingestion.source.mce_file:MetadataFileSource",
         "sqlalchemy = datahub.ingestion.source.sql_generic:SQLAlchemyGenericSource",
