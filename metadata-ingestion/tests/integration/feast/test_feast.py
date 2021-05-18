65d3d5299d50ee21c7b323a1a30e56e1bf8bdd24
Kevin Hu <kevinhuwest@gmail.com>
2021-05-18 16:56:09 -0700

    Expand Feast tests

--- metadata-ingestion/tests/integration/feast/test_feast.py
+++ metadata-ingestion/tests/integration/feast/test_feast.py
@@ -25,18 +25,25 @@ def test_feast_ingest(docker_compose_runner, pytestconfig, tmp_path):
         test_client = Client(core_url="localhost:6565")
 
         # create dummy entity since Feast demands it
-        entity = Entity(
-            name="dummy_entity",
-            description="Dummy entity",
+        entity_1 = Entity(
+            name="dummy_entity_1",
+            description="Dummy entity 1",
             value_type=ValueType.STRING,
             labels={"key": "val"},
         )
 
-        print("Breakpoint")
+        # create dummy entity since Feast demands it
+        entity_2 = Entity(
+            name="dummy_entity_2",
+            description="Dummy entity 2",
+            value_type=ValueType.INT32,
+            labels={"key": "val"},
+        )
 
-        test_client.apply(entity)
+        # commit entities
+        test_client.apply([entity_1, entity_2])
 
-        # create feature tables
+        # dummy file source
         batch_source = FileSource(
             file_format=ParquetFormat(),
             file_url="file://feast/*",
@@ -45,8 +52,8 @@ def test_feast_ingest(docker_compose_runner, pytestconfig, tmp_path):
             date_partition_column="date_partition_col",
         )
 
-        table = FeatureTable(
-            name="test_feature_table",
+        table_1 = FeatureTable(
+            name="test_feature_table_1",
             features=[
                 Feature(name="test_BYTES_feature", dtype=ValueType.BYTES),
                 Feature(name="test_STRING_feature", dtype=ValueType.STRING),
@@ -63,41 +70,64 @@ def test_feast_ingest(docker_compose_runner, pytestconfig, tmp_path):
                 Feature(name="test_FLOAT_LIST_feature", dtype=ValueType.FLOAT_LIST),
                 Feature(name="test_BOOL_LIST_feature", dtype=ValueType.BOOL_LIST),
             ],
-            entities=["dummy_entity"],
+            entities=["dummy_entity_1", "dummy_entity_2"],
             labels={"team": "matchmaking"},
             batch_source=batch_source,
         )
 
-        test_client.apply(table)
+        table_2 = FeatureTable(
+            name="test_feature_table_2",
+            features=[
+                Feature(name="test_BYTES_feature", dtype=ValueType.BYTES),
+            ],
+            entities=["dummy_entity_1"],
+            labels={"team": "matchmaking"},
+            batch_source=batch_source,
+        )
+
+        table_3 = FeatureTable(
+            name="empty_table",
+            features=[
+                Feature(name="test_BYTES_feature", dtype=ValueType.BYTES),
+            ],
+            entities=["dummy_entity_2"],
+            labels={},
+            batch_source=batch_source,
+        )
+
+        # commit the tables to the feature store
+        test_client.apply([table_1, table_2, table_3])
+
+        breakpoint()
 
         for table in test_client.list_feature_tables():
 
             print(test_client.get_feature_table(table.name))
 
-    # # Run the metadata ingestion pipeline.
-    # pipeline = Pipeline.create(
-    #     {
-    #         "run_id": "feast-test",
-    #         "source": {
-    #             "type": "feast",
-    #             "config": {
-    #                 "core_url": "feast://localhost:6565",
-    #             },
-    #         },
-    #         "sink": {
-    #             "type": "file",
-    #             "config": {
-    #                 "filename": f"{tmp_path}/feast_mces.json",
-    #             },
-    #         },
-    #     }
-    # )
-    # pipeline.run()
-    # pipeline.raise_from_status()
+        # # Run the metadata ingestion pipeline.
+        # pipeline = Pipeline.create(
+        #     {
+        #         "run_id": "feast-test",
+        #         "source": {
+        #             "type": "feast",
+        #             "config": {
+        #                 "core_url": "localhost:6565",
+        #             },
+        #         },
+        #         "sink": {
+        #             "type": "file",
+        #             "config": {
+        #                 "filename": f"{tmp_path}/feast_mces.json",
+        #             },
+        #         },
+        #     }
+        # )
+        # pipeline.run()
+        # pipeline.raise_from_status()
 
-    # # Verify the output.
-    # output = mce_helpers.load_json_file(str(tmp_path / "feast_mces.json"))
-    # golden = mce_helpers.load_json_file(
-    #     str(test_resources_dir / "feast_mce_golden.json")
-    # )
-    # mce_helpers.assert_mces_equal(output, golden)
+        # # Verify the output.
+        # output = mce_helpers.load_json_file(str(tmp_path / "feast_mces.json"))
+        # golden = mce_helpers.load_json_file(
+        #     str(test_resources_dir / "feast_mce_golden.json")
+        # )
+        # mce_helpers.assert_mces_equal(output, golden)
