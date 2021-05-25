3f5faf03a8653b112b0a4a8c10f801f445fa3f18
John Joyce <john@acryl.io>
2021-05-24 19:51:20 -0700

    Adding upgrade.sh

--- docker/datahub-upgrade/nocode/upgrade.sh
+++ docker/datahub-upgrade/nocode/upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR && docker-compose pull && docker-compose up
+cd $DIR && docker-compose -f docker-compose.nocode.yml pull && docker-compose -f docker-compose.nocode.yml up
