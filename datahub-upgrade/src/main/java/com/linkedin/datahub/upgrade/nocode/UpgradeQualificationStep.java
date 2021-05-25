1dff35742f2f6e32b57c27e07b45d44d36e2e56e
Gabe Lyons <itsgabelyons@gmail.com>
2021-05-25 08:34:01 -0700

    checking mae and gms config in migration script

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/nocode/UpgradeQualificationStep.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/nocode/UpgradeQualificationStep.java
@@ -6,9 +6,35 @@ import com.linkedin.datahub.upgrade.UpgradeStep;
 import com.linkedin.datahub.upgrade.UpgradeStepResult;
 import com.linkedin.metadata.entity.AspectStorageValidationUtil;
 import io.ebean.EbeanServer;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.function.Function;
 
 public class UpgradeQualificationStep implements UpgradeStep<Void> {
+
+  private static String convertStreamToString(InputStream is) {
+
+    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+    StringBuilder sb = new StringBuilder();
+
+    String line = null;
+    try {
+      while ((line = reader.readLine()) != null) {
+        sb.append(line + "\n");
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        is.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    return sb.toString();
+  }
 
   private final EbeanServer _server;
 
@@ -61,7 +87,11 @@ public class UpgradeQualificationStep implements UpgradeStep<Void> {
   }
 
   private boolean isQualified(EbeanServer server) {
+
     return AspectStorageValidationUtil.checkV1TableExists(server)
-        && (!AspectStorageValidationUtil.checkV2TableExists(server) || AspectStorageValidationUtil.getV2RowCount(server) == 0);
+        && (
+            !AspectStorageValidationUtil.checkV2TableExists(server)
+                || AspectStorageValidationUtil.getV2RowCount(server) == 0
+           );
   }
 }
