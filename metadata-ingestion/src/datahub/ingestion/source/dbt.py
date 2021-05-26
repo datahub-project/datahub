2ac2ee3cd725c759a5cf2ab0ef91542602fc87a6
Remi <salmon.remi@gmail.com>
2021-05-25 19:59:35 -0600

    fix(ingest): fix lineage after dbt metadata ingestion when tables name and identifier differ (#2596)

--- metadata-ingestion/src/datahub/ingestion/source/dbt.py
+++ metadata-ingestion/src/datahub/ingestion/source/dbt.py
@@ -58,7 +58,7 @@ class DBTNode:
     dbt_file_path: str
     node_type: str  # source, model
     materialization: str  # table, view, ephemeral
-    name: str
+    name: str  # name, identifier
     columns: List[DBTColumn]
     upstream_urns: List[str]
     datahub_urn: str
@@ -98,17 +98,24 @@ def extract_dbt_entities(
         dbtNode = DBTNode()
 
         dbtNode.dbt_name = key
-        dbtNode.node_type = node["resource_type"]
-        dbtNode.name = node["name"]
         dbtNode.database = node["database"]
         dbtNode.schema = node["schema"]
         dbtNode.dbt_file_path = node["original_file_path"]
+        dbtNode.node_type = node["resource_type"]
+        if "identifier" in node and load_catalog is False:
+            dbtNode.name = node["identifier"]
+        else:
+            dbtNode.name = node["name"]
 
         if "materialized" in node["config"].keys():
             # It's a model
             dbtNode.materialization = node["config"]["materialized"]
             dbtNode.upstream_urns = get_upstreams(
-                node["depends_on"]["nodes"], nodes, target_platform, environment
+                node["depends_on"]["nodes"],
+                nodes,
+                load_catalog,
+                target_platform,
+                environment,
             )
         else:
             # It's a source
@@ -190,19 +197,27 @@ def get_custom_properties(node: DBTNode) -> Dict[str, str]:
 def get_upstreams(
     upstreams: List[str],
     all_nodes: Dict[str, dict],
+    load_catalog: bool,
     target_platform: str,
     environment: str,
 ) -> List[str]:
     upstream_urns = []
 
     for upstream in upstreams:
-        upstream_node = all_nodes[upstream]
+        dbtNode_upstream = DBTNode()
+
+        dbtNode_upstream.database = all_nodes[upstream]["database"]
+        dbtNode_upstream.schema = all_nodes[upstream]["schema"]
+        if "identifier" in all_nodes[upstream] and load_catalog is False:
+            dbtNode_upstream.name = all_nodes[upstream]["identifier"]
+        else:
+            dbtNode_upstream.name = all_nodes[upstream]["name"]
 
         upstream_urns.append(
             get_urn_from_dbtNode(
-                upstream_node["database"],
-                upstream_node["schema"],
-                upstream_node["name"],
+                dbtNode_upstream.database,
+                dbtNode_upstream.schema,
+                dbtNode_upstream.name,
                 target_platform,
                 environment,
             )
