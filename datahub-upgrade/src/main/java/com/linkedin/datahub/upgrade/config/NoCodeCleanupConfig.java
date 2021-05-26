4aa9abdd2da3bca8d8a6005a79ee7633bfa9a9e4
Dexter Lee <dexter@acryl.io>
2021-05-26 00:11:40 -0700

    Add delete legacy search indices

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/config/NoCodeCleanupConfig.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/config/NoCodeCleanupConfig.java
@@ -5,13 +5,14 @@ import com.linkedin.metadata.graph.GraphService;
 import io.ebean.EbeanServerFactory;
 import io.ebean.config.ServerConfig;
 import javax.annotation.Nonnull;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
-import static com.linkedin.metadata.entity.ebean.EbeanAspectDao.*;
+import static com.linkedin.metadata.entity.ebean.EbeanAspectDao.EBEAN_MODEL_PACKAGE;
 
 
 @Configuration
@@ -21,14 +22,15 @@ public class NoCodeCleanupConfig {
   ApplicationContext applicationContext;
 
   @Bean(name = "noCodeCleanup")
-  @DependsOn({"gmsEbeanServiceConfig", "graphService"})
+  @DependsOn({"gmsEbeanServiceConfig", "graphService", "elasticSearchRestHighLevelClient"})
   @Nonnull
   public NoCodeCleanupUpgrade createInstance() {
     final ServerConfig serverConfig = applicationContext.getBean(ServerConfig.class);
     final GraphService graphClient = applicationContext.getBean(GraphService.class);
+    final RestHighLevelClient searchClient = applicationContext.getBean(RestHighLevelClient.class);
     if (!serverConfig.getPackages().contains(EBEAN_MODEL_PACKAGE)) {
       serverConfig.getPackages().add(EBEAN_MODEL_PACKAGE);
     }
-    return new NoCodeCleanupUpgrade(EbeanServerFactory.create(serverConfig), graphClient);
+    return new NoCodeCleanupUpgrade(EbeanServerFactory.create(serverConfig), graphClient, searchClient);
   }
 }
--- gms/factories/src/main/java/com/linkedin/gms/factory/common/GraphServiceFactory.java
+++ gms/factories/src/main/java/com/linkedin/gms/factory/common/GraphServiceFactory.java
@@ -2,24 +2,27 @@ package com.linkedin.gms.factory.common;
 
 import com.linkedin.metadata.graph.GraphService;
 import com.linkedin.metadata.graph.Neo4jGraphService;
+import javax.annotation.Nonnull;
 import org.neo4j.driver.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Import;
 
-import javax.annotation.Nonnull;
 
 @Configuration
+@Import({Neo4jDriverFactory.class})
 public class GraphServiceFactory {
-    @Autowired
-    ApplicationContext applicationContext;
+  @Autowired
+  @Qualifier("neo4jDriver")
+  private Driver neo4jDriver;
 
-    @Nonnull
-    @DependsOn({"neo4jDriver"})
-    @Bean(name = "graphService")
-    protected GraphService createInstance() {
-        return new Neo4jGraphService(applicationContext.getBean(Driver.class));
-    }
+  @Nonnull
+  @DependsOn({"neo4jDriver"})
+  @Bean(name = "graphService")
+  protected GraphService createInstance() {
+    return new Neo4jGraphService(neo4jDriver);
+  }
 }
