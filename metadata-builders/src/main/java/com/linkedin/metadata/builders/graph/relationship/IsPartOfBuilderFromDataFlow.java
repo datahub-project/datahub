7261eb923062813ef38ba315bf7183b177ada506
Brian <brianwebtek@gmail.com>
2021-05-29 01:25:00 +0800

    fix(react): update ispartofbuilderfromdataflow, update ui in datajob header (#2619)

--- metadata-builders/src/main/java/com/linkedin/metadata/builders/graph/relationship/IsPartOfBuilderFromDataFlow.java
+++ metadata-builders/src/main/java/com/linkedin/metadata/builders/graph/relationship/IsPartOfBuilderFromDataFlow.java
@@ -1,11 +1,13 @@
 package com.linkedin.metadata.builders.graph.relationship;
 
 import com.linkedin.common.urn.Urn;
+import com.linkedin.common.urn.DataJobUrn;
 import com.linkedin.datajob.DataJobInfo;
 import com.linkedin.metadata.builders.graph.GraphBuilder;
 import com.linkedin.metadata.relationship.IsPartOf;
 
 import javax.annotation.Nonnull;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
@@ -20,14 +22,15 @@ public class IsPartOfBuilderFromDataFlow extends BaseRelationshipBuilder<DataJob
 
   @Nonnull
   @Override
-  public List<GraphBuilder.RelationshipUpdates> buildRelationships(@Nonnull Urn urn, @Nonnull DataJobInfo dataJob) {
-    if (dataJob.getFlowUrn() == null) {
+  public List<GraphBuilder.RelationshipUpdates> buildRelationships(@Nonnull Urn sourceUrn, @Nonnull DataJobInfo dataJobInfo) {
+    final List<IsPartOf> dataJobEdges = new ArrayList<>();
+    try {
+      dataJobEdges.add(new IsPartOf().setSource(sourceUrn).setDestination(DataJobUrn.createFromUrn(sourceUrn).getFlowEntity()));
+      return Collections.singletonList(
+              new GraphBuilder.RelationshipUpdates(dataJobEdges, REMOVE_ALL_EDGES_FROM_SOURCE)
+      );
+    } catch (URISyntaxException e) {
       return Collections.emptyList();
     }
-    final List<IsPartOf> dataJobEdges = new ArrayList<>();
-    dataJobEdges.add(new IsPartOf().setSource(urn).setDestination(dataJob.getFlowUrn()));
-    return Collections.singletonList(
-        new GraphBuilder.RelationshipUpdates(dataJobEdges, REMOVE_ALL_EDGES_FROM_SOURCE)
-    );
   }
 }
