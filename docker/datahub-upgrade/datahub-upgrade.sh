b48a5a525d82fbc66f388af632495eb551a5bb2e
John Joyce <john@acryl.io>
2021-05-31 17:27:21 -0700

    Updating docs

--- docker/datahub-upgrade/datahub-upgrade.sh
+++ docker/datahub-upgrade/datahub-upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR && docker pull acryldata/datahub-upgrade:latest && docker run acryldata/datahub-upgrade:latest "$@"
+cd $DIR && docker pull acryldata/datahub-upgrade:latest && docker run --env-file env/docker.env acryldata/datahub-upgrade:latest "$@"
