96f7009e78d37b5ed82f85099e02039f8c002fba
Gabe Lyons <itsgabelyons@gmail.com>
2021-06-02 09:52:19 -0700

    handling null urns

--- datahub-web-react/src/app/entity/shared/utils.ts
+++ datahub-web-react/src/app/entity/shared/utils.ts
@@ -1,3 +1,3 @@
 export function urlEncodeUrn(urn: string) {
-    return urn.replaceAll('/', '%2F').replaceAll('?', '%3F').replaceAll('%', '%25').replaceAll('#', '%23');
+    return urn?.replaceAll('/', '%2F').replaceAll('?', '%3F').replaceAll('%', '%25').replaceAll('#', '%23');
 }
