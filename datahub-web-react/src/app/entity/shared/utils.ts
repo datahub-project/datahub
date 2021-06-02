b29663429e70ae5247458e26803e19a011880853
Gabe Lyons <itsgabelyons@gmail.com>
2021-06-02 10:14:02 -0700

    adjusting rewrite logic

--- datahub-web-react/src/app/entity/shared/utils.ts
+++ datahub-web-react/src/app/entity/shared/utils.ts
@@ -1,3 +1,3 @@
 export function urlEncodeUrn(urn: string) {
-    return urn &&  urn.replace(/\//g, '%2F').replace(/\?/g, '%3F').replace(/%/g, '%25').replace(/#/g, '%23');
+    return urn && urn.replace(/%/g, '%25').replace(/\//g, '%2F').replace(/\?/g, '%3F').replace(/#/g, '%23');
 }
