caf20be2874b8a17cca4db87c0d227a17c1ab04e
John Joyce <john@acryl.io>
2021-06-03 16:25:17 -0700

    feat(datahub-upgrade): Use the "head" image tag.  (#2641)

--- docker/datahub-upgrade/datahub-upgrade.sh
+++ docker/datahub-upgrade/datahub-upgrade.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR && docker pull acryldata/datahub-upgrade:latest && docker run --env-file ./env/docker.env --network="datahub_network" acryldata/datahub-upgrade:latest "$@"
+cd $DIR && docker pull acryldata/datahub-upgrade:head && docker run --env-file ./env/docker.env --network="datahub_network" acryldata/datahub-upgrade:latest "$@"
