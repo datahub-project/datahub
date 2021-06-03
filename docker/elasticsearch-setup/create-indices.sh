1bf3299eff4b005e736096364c9a87f0bedf56fa
Dexter Lee <dexter@acryl.io>
2021-06-03 02:21:56 -0700

    Done

--- docker/elasticsearch-setup/create-indices.sh
+++ docker/elasticsearch-setup/create-indices.sh
@@ -162,11 +162,11 @@ function create_datahub_usage_event_aws_elasticsearch() {
 
 function create_user() {
   ROLE="${INDEX_PREFIX}_access"
-  if [ $(curl -o /dev/null -s -w "%{http_code}" "$ELASTICSEARCH_PROTOCOL://$ELASTICSEARCH_HOST_URL:$ELASTICSEARCH_PORT/_opendistro/_security/api/roles//$ELASTICSEARCH_USERNAME") -eq 404 ]
+  if [ $(curl -o /dev/null -s -w "%{http_code}" "$ELASTICSEARCH_PROTOCOL://$ELASTICSEARCH_HOST_URL:$ELASTICSEARCH_PORT/_opendistro/_security/api/roles/${ROLE}") -eq 404 ]
   then
     echo -e '\ncreating role' "${ROLE}"
     curl -XPUT "$ELASTICSEARCH_PROTOCOL://$ELASTICSEARCH_HOST_URL:$ELASTICSEARCH_PORT/_opendistro/_security/api/roles/${ROLE}" -H 'Content-Type: application/json' \
-      -d "{\"index_permissions\":{\"index_patterns\":[\"${INDEX_PREFIX}_*\"], \"allowed_actions\":[\"read\", \"write\"]}}"
+      -d "{\"index_permissions\":[{\"index_patterns\":[\"${INDEX_PREFIX}_*\"], \"allowed_actions\":[\"read\", \"write\"]}]}"
   else
     echo -e "\nrole exists"
   fi
