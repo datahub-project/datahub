51980d73f65e350ecd26340cbcd2cce0b932e35d
Gabe Lyons <itsgabelyons@gmail.com>
2021-06-03 17:14:30 -0700

    reverting changes

--- metadata-ingestion/src/datahub/emitter/rest_emitter.py
+++ metadata-ingestion/src/datahub/emitter/rest_emitter.py
@@ -3,15 +3,40 @@ import json
 import logging
 import shlex
 from collections import OrderedDict
-from typing import Any, List, Optional
+from typing import Any, Dict, List, Optional, Type
 
 import requests
 from requests.exceptions import HTTPError, RequestException
 
 from datahub.configuration.common import OperationalError
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
+from datahub.metadata.schema_classes import (  # MLFeatureSnapshotClass,
+    ChartSnapshotClass,
+    CorpGroupSnapshotClass,
+    CorpUserSnapshotClass,
+    DashboardSnapshotClass,
+    DataFlowSnapshotClass,
+    DataJobSnapshotClass,
+    DataProcessSnapshotClass,
+    DatasetSnapshotClass,
+    MLModelSnapshotClass,
+    TagSnapshotClass,
+)
 
 logger = logging.getLogger(__name__)
+
+resource_locator: Dict[Type[object], str] = {
+    ChartSnapshotClass: "charts",
+    DashboardSnapshotClass: "dashboards",
+    CorpUserSnapshotClass: "corpUsers",
+    CorpGroupSnapshotClass: "corpGroups",
+    DatasetSnapshotClass: "datasets",
+    DataProcessSnapshotClass: "dataProcesses",
+    DataFlowSnapshotClass: "dataFlows",
+    DataJobSnapshotClass: "dataJobs",
+    MLModelSnapshotClass: "mlModels",
+    TagSnapshotClass: "tags",
+}
 
 
 def _rest_li_ify(obj: Any) -> Any:
@@ -76,17 +101,21 @@ class DatahubRestEmitter:
             self._session.headers.update({"Authorization": f"Bearer {token}"})
 
     def _get_ingest_endpoint(self, mce: MetadataChangeEvent) -> str:
-        return f"{self._gms_server}/entities?action=ingest"
+        snapshot_type = type(mce.proposedSnapshot)
+        snapshot_resource = resource_locator.get(snapshot_type, None)
+        if not snapshot_resource:
+            raise ValueError(
+                f"Failed to locate a snapshot resource for type {snapshot_type}"
+            )
+
+        return f"{self._gms_server}/{snapshot_resource}?action=ingest"
 
     def emit_mce(self, mce: MetadataChangeEvent) -> None:
         url = self._get_ingest_endpoint(mce)
 
         raw_mce_obj = mce.proposedSnapshot.to_obj()
         mce_obj = _rest_li_ify(raw_mce_obj)
-        snapshot_fqn = (
-            f"com.linkedin.metadata.snapshot.{mce.proposedSnapshot.RECORD_SCHEMA.name}"
-        )
-        snapshot = {"entity": {"value": {snapshot_fqn: mce_obj}}}
+        snapshot = {"snapshot": mce_obj}
         payload = json.dumps(snapshot)
 
         curl_command = _make_curl_command(self._session, "POST", url, payload)
@@ -99,14 +128,7 @@ class DatahubRestEmitter:
 
             response.raise_for_status()
         except HTTPError as e:
-            try:
-                info = response.json()
-            except Exception as parse_exception:
-                response.raise_for_status()
-                raise OperationalError(
-                    "Unable to parse response from ingestion sink",
-                    {"message": str(parse_exception)},
-                ) from e
+            info = response.json()
             raise OperationalError(
                 "Unable to emit metadata to DataHub GMS", info
             ) from e
