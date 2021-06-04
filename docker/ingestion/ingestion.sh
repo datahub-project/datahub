a483933eab6d072ba6d2a414e378eee1e80dbdc6
Harshal Sheth <hsheth2@gmail.com>
2021-06-04 12:46:57 -0700

    fix(docker): pin containers to golden hash for release (#2654)

--- docker/ingestion/ingestion.sh
+++ docker/ingestion/ingestion.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export DATAHUB_VERSION=${DATAHUB_VERSION:-head}
+export DATAHUB_VERSION=${DATAHUB_VERSION:-9829576}
 cd $DIR && docker-compose pull && docker-compose -p datahub up
