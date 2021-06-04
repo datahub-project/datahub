fade8868c6a931f79d132e30a35c99dcf279d3fe
Dexter Lee <dexter@acryl.io>
2021-06-03 21:42:23 -0700

    fix(nocode): Fix docker image tag for run_upgrade script (#2648)

--- docker/datahub-upgrade/datahub-upgrade.sh
+++ docker/datahub-upgrade/datahub-upgrade.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR && docker pull acryldata/datahub-upgrade:head && docker run --env-file ./env/docker.env --network="datahub_network" acryldata/datahub-upgrade:latest "$@"
+IMAGE=acryldata/datahub-upgrade:head
+cd $DIR && docker pull ${IMAGE} && docker run --env-file ./env/docker.env --network="datahub_network" ${IMAGE} "$@"
