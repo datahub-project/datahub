31eae2430087de0d4c9d0fda46f050e578bb4da1
Harshal Sheth <hsheth2@gmail.com>
2021-06-04 18:19:11 -0700

    fix(ingest): support mssql encryption via ODBC (#2657)

--- metadata-ingestion/src/datahub/ingestion/source/mssql.py
+++ metadata-ingestion/src/datahub/ingestion/source/mssql.py
@@ -1,3 +1,8 @@
+import urllib.parse
+from typing import Dict
+
+import pydantic
+
 # This import verifies that the dependencies are available.
 import sqlalchemy_pytds  # noqa: F401
 
@@ -8,6 +13,29 @@ class SQLServerConfig(BasicSQLAlchemyConfig):
     # defaults
     host_port = "localhost:1433"
     scheme = "mssql+pytds"
+
+    use_odbc: bool = False
+    uri_args: Dict[str, str] = {}
+
+    @pydantic.validator("uri_args")
+    def passwords_match(cls, v, values, **kwargs):
+        if values["use_odbc"] and "driver" not in v:
+            raise ValueError("uri_args must contain a 'driver' option")
+        elif not values["use_odbc"] and v:
+            raise ValueError("uri_args is not supported when ODBC is disabled")
+        return v
+
+    def get_sql_alchemy_url(self):
+        if self.use_odbc:
+            # Ensure that the import is available.
+            import pyodbc  # noqa: F401
+
+            self.scheme = "mssql+pyodbc"
+        uri = super().get_sql_alchemy_url()
+
+        if self.use_odbc:
+            uri = f"{uri}?{urllib.parse.urlencode(self.uri_args)}"
+        return uri
 
     def get_identifier(self, schema: str, table: str) -> str:
         regular = f"{schema}.{table}"
