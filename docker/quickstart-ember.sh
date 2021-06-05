e73c6525e9abaa1674f3d5bd3dee14ff97a5bff2
Harshal Sheth <hsheth2@gmail.com>
2021-06-04 18:11:36 -0700

    revert: "fix(docker): pin containers to golden hash for release (#2654)" (#2659)
    This reverts commit a483933eab6d072ba6d2a414e378eee1e80dbdc6 and moves
    us back to using HEAD in quickstart on the master branch.

--- docker/quickstart-ember.sh
+++ docker/quickstart-ember.sh
@@ -2,7 +2,7 @@
 
 # Quickstarts an Ember-serving variant of DataHub by pulling all images from dockerhub and then running the containers locally.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-export DATAHUB_VERSION=${DATAHUB_VERSION:-9829576}
+export DATAHUB_VERSION=${DATAHUB_VERSION:-head}
 cd $DIR && docker-compose -f docker-compose.yml -f docker-compose.override.yml -f docker-compose.ember.yml pull && docker-compose -p datahub \
     -f docker-compose.yml \
     -f docker-compose.override.yml \
