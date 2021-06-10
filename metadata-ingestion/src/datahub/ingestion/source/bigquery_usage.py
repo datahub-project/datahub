42aec238a3478d7e91e552e079f5ed43c088aa87
Harshal Sheth <hsheth2@gmail.com>
2021-06-10 01:05:29 -0700

    more work on usage stats pipelining

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_usage.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_usage.py
@@ -1,21 +1,21 @@
+import collections
 import dataclasses
 from datetime import datetime, timedelta, timezone
 from pprint import pprint
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Deque, Iterable, List, Optional, TypeVar, Union
 
 from google.cloud.logging_v2.client import Client
 
-# from google.cloud.logging_v2 import ProtobufEntry
 # ProtobufEntry is generated dynamically using a namedtuple, so mypy
 # can't really deal with it. As such, we short circuit mypy's typing
 # but keep the code relatively clear by retaining dummy types.
+#
+# from google.cloud.logging_v2 import ProtobufEntry
+# AuditLogEntry = ProtobufEntry
 AuditLogEntry = Any
 
 BQ_DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 INCLUDE_FULL_PAYLOADS = False
-
-# See https://github.com/googleapis/google-cloud-python/issues/2674
-client = Client(project="harshal-playground-306419", _use_grpc=False)
 GCP_LOGGING_PAGE_SIZE = 1000
 
 time_start = datetime.now(tz=timezone.utc) - timedelta(days=14)
@@ -40,6 +40,25 @@ AND
 AND
 timestamp >= "{time_start.strftime(BQ_DATETIME_FORMAT)}"
 """.strip()
+
+
+T = TypeVar("T")
+
+
+def delayed_iter(iterable: Iterable[T], delay: int) -> Iterable[T]:
+    """Waits to yield the i'th element until the (i+n)'th element has been
+    materialized by the source iterator.
+    """
+
+    cache: Deque[T] = collections.deque([], maxlen=delay)
+
+    for item in iterable:
+        if len(cache) >= delay:
+            yield cache.popleft()
+        cache.append(item)
+
+    while len(cache):
+        yield cache.popleft()
 
 
 @dataclasses.dataclass
@@ -137,34 +156,88 @@ class QueryEvent:
         return queryEvent
 
 
-entry: AuditLogEntry
-events: List[Union[ReadEvent, QueryEvent]] = []
-queries: Dict[str, QueryEvent] = {}
-for i, entry in enumerate(
-    client.list_entries(filter_=filter_rule, page_size=GCP_LOGGING_PAGE_SIZE)
-):
-    event: Union[ReadEvent, QueryEvent]
-    if ReadEvent.can_parse_entry(entry):
-        event = ReadEvent.from_entry(entry)
-        if event.jobName:
-            if event.jobName not in queries:
+class BigQueryUsageSource:
+    def __init__(self, project: str, client_options: dict = {}) -> None:
+        client_options = client_options.copy()
+        if project is not None:
+            client_options["project"] = project
+
+        # See https://github.com/googleapis/google-cloud-python/issues/2674 for
+        # why we disable gRPC here.
+        self.client = Client(**client_options, _use_grpc=False)
+
+        self.query_buffer_size = 100
+
+    def get_workunits(self) -> Iterable[ReadEvent]:
+        bigquery_log_entries = self._get_bigquery_log_entries()
+        parsed_events = self._parse_bigquery_log_entries(bigquery_log_entries)
+        hydrated_read_events = self._join_events_by_job_id(parsed_events)
+
+        # TODO: now we need to do the aggregations
+        yield from hydrated_read_events
+
+    def _get_bigquery_log_entries(self) -> Iterable[AuditLogEntry]:
+        entry: AuditLogEntry
+        for entry in self.client.list_entries(
+            filter_=filter_rule, page_size=GCP_LOGGING_PAGE_SIZE
+        ):
+            yield entry
+
+    def _parse_bigquery_log_entries(
+        self, entries: Iterable[AuditLogEntry]
+    ) -> Iterable[Union[ReadEvent, QueryEvent]]:
+        for entry in entries:
+            event: Union[ReadEvent, QueryEvent]
+            if ReadEvent.can_parse_entry(entry):
+                event = ReadEvent.from_entry(entry)
+            elif QueryEvent.can_parse_entry(entry):
+                event = QueryEvent.from_entry(entry)
+            else:
+                # TODO: add better error handling
+                print("Unable to parse:", entry)
+                exit(1)
+            yield event
+
+    def _join_events_by_job_id(
+        self, events: Iterable[Union[ReadEvent, QueryEvent]]
+    ) -> Iterable[ReadEvent]:
+        query_jobs = {}
+
+        def event_processor(
+            events: Iterable[Union[ReadEvent, QueryEvent]]
+        ) -> Iterable[ReadEvent]:
+            for event in events:
+                if isinstance(event, QueryEvent):
+                    query_jobs[event.jobName] = event
+                else:
+                    yield event
+
+        original_read_events = event_processor(events)
+        delayed_read_events = delayed_iter(original_read_events, self.query_buffer_size)
+
+        for event in delayed_read_events:
+            if event.jobName:
+                # TODO update this comment
                 # Anecdotally, the query completion event seems to come before the table read events.
                 # As such, we can simply keep a cache of the recent query events and match them to the
                 # table read events as we receive the read events.
-                breakpoint()
-            else:
-                # Join the query log event into the table read log event.
-                event.query = queries[event.jobName].query
-    elif QueryEvent.can_parse_entry(entry):
-        event = QueryEvent.from_entry(entry)
-        queries[event.jobName] = event
-    else:
-        print("Unable to parse:", entry)
-        exit(1)
-    pprint(event)
-    events.append(event)
+                if event.jobName in query_jobs:
+                    # Join the query log event into the table read log event.
+                    event.query = query_jobs[event.jobName].query
 
-pprint(events)
-print(f"Processed {i+1} entries")
+                    # TODO also join into the query itself
+                else:
+                    # TODO raise a warning and suggest increasing the buffer size option
+                    breakpoint()
+
+            yield event
+
+        print(query_jobs)
+
+
+source = BigQueryUsageSource(project="harshal-playground-306419")
+events = list(source.get_workunits())
+# pprint(events)
+pprint(f"Processed {len(events)} entries")
 breakpoint()
 exit(0)
