cdfe2837437d04ef84638a79784ac54e875b73b5
Harshal Sheth <hsheth2@gmail.com>
2021-06-11 17:26:32 -0700

    better code style in generation script

--- docker/quickstart/generate_docker_quickstart.py
+++ docker/quickstart/generate_docker_quickstart.py
@@ -33,36 +33,32 @@ def dict_merge(dct, merge_dct):
 
 def modify_docker_config(base_path, docker_yaml_config):
     # 0. Filter out services to be omitted.
-    keys_to_delete = [
-        key for key in docker_yaml_config["services"] if key in omitted_services
-    ]
-    for key in keys_to_delete:
-        del docker_yaml_config["services"][key]
+    for key in list(docker_yaml_config["services"]):
+        if key in omitted_services:
+            del docker_yaml_config["services"][key]
 
     for name, service in docker_yaml_config["services"].items():
         # 1. Extract the env file pointer
         env_file = service.get("env_file")
 
         if env_file is not None:
-          # 2. Construct full .env path
-          env_file_path = os.path.join(base_path, env_file)
+            # 2. Construct full .env path
+            env_file_path = os.path.join(base_path, env_file)
 
-          # 3. Resolve the .env values
-          env_vars = dotenv_values(env_file_path)
+            # 3. Resolve the .env values
+            env_vars = dotenv_values(env_file_path)
 
-          # 4. Add an "environment" block to YAML
-          formatted_env_pairs = map(
-              lambda tuple: "{key}={value}".format(key=tuple[0], value=tuple[1]),
-              env_vars.items(),
-          )
-          service["environment"] = list(formatted_env_pairs)
+            # 4. Add an "environment" block to YAML
+            service["environment"] = list(
+                f"{key}={value}" for key, value in env_vars.items()
+            )
 
-          # 5. Delete the "env_file" value
-          del service["env_file"]
+            # 5. Delete the "env_file" value
+            del service["env_file"]
 
         # 6. Delete build instructions
-        if "build" in service.keys():
-          del service["build"]
+        if "build" in service:
+            del service["build"]
 
         # 7. Set memory limits
         if name in mem_limits:
@@ -73,7 +69,14 @@ def modify_docker_config(base_path, docker_yaml_config):
 
 
 @click.command()
-@click.argument("compose-files", nargs=-1, type=click.Path(exists=True, dir_okay=False))
+@click.argument(
+    "compose-files",
+    nargs=-1,
+    type=click.Path(
+        exists=True,
+        dir_okay=False,
+    ),
+)
 @click.argument("output-file", type=click.Path())
 def generate(compose_files, output_file) -> None:
 
@@ -97,9 +100,13 @@ def generate(compose_files, output_file) -> None:
     if len(output_dir) and not os.path.exists(output_dir):
         os.makedirs(output_dir)
     with open(output_file, "w") as new_conf_file:
-        yaml.dump(merged_docker_config, new_conf_file, default_flow_style=False)
+        yaml.dump(
+            merged_docker_config,
+            new_conf_file,
+            default_flow_style=False,
+        )
 
-    print("Successfully generated {output_file}.".format(output_file=output_file))
+    print(f"Successfully generated {output_file}.")
 
 
 if __name__ == "__main__":
