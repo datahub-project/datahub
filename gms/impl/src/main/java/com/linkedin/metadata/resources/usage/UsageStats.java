04aea8ca359341c0493911e798a400182045dad7
Harshal Sheth <hsheth2@gmail.com>
2021-06-15 12:58:03 -0700

    add some snapshots

--- gms/impl/src/main/java/com/linkedin/metadata/resources/usage/UsageStats.java
+++ gms/impl/src/main/java/com/linkedin/metadata/resources/usage/UsageStats.java
@@ -1,12 +1,20 @@
 package com.linkedin.metadata.resources.usage;
 
 import com.linkedin.common.EntityRelationships;
+import com.linkedin.metadata.search.SearchService;
 import com.linkedin.restli.server.annotations.RestLiSimpleResource;
 import com.linkedin.restli.server.resources.SimpleResourceTemplate;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * Rest.li entry point: /usageStats
  */
 @RestLiSimpleResource(name = "usageStats", namespace = "com.linkedin.usage")
 public class UsageStats extends SimpleResourceTemplate<EntityRelationships> {
+    @Inject
+    @Named("searchService")
+    private SearchService _searchService;
+
 }
