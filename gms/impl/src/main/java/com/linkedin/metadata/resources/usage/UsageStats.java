b67fd296b306894b120f470605398cff8e1bf5e6
Harshal Sheth <hsheth2@gmail.com>
2021-06-16 11:05:35 -0700

    user in document

--- gms/impl/src/main/java/com/linkedin/metadata/resources/usage/UsageStats.java
+++ gms/impl/src/main/java/com/linkedin/metadata/resources/usage/UsageStats.java
@@ -1,5 +1,6 @@
 package com.linkedin.metadata.resources.usage;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.linkedin.common.WindowDuration;
@@ -19,6 +20,7 @@ import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.*;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -56,6 +58,17 @@ public class UsageStats extends SimpleResourceTemplate<UsageAggregation> {
         document.set("duration", JsonNodeFactory.instance.textNode(bucket.getDuration().toString()));
         document.set("bucket_end", JsonNodeFactory.instance.numberNode(bucket.getBucket() + windowDurationToMillis(bucket.getDuration())));
         document.set("resource", JsonNodeFactory.instance.textNode(bucket.getResource().toString()));
+
+        Optional.ofNullable(bucket.getMetrics().getUsers()).ifPresent(usersUsageCounts -> {
+            ArrayNode users = JsonNodeFactory.instance.arrayNode();
+            // TODO attempt to resolve users into emails
+            usersUsageCounts.forEach(userUsage -> {
+                ObjectNode userDocument = JsonNodeFactory.instance.objectNode();
+                userDocument.set("user", JsonNodeFactory.instance.textNode(userUsage.getUser().toString()));
+                userDocument.set("count", JsonNodeFactory.instance.numberNode(userUsage.getCount()));
+            });
+            document.set("metrics.users", users);
+        });
 
         // TODO: metrics here
         _usageService.upsertDocument(document.toString(), id);
