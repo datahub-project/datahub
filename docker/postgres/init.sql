109eaa8505407633881cd379ece953fd62297a73
John Joyce <john@acryl.io>
2021-06-16 21:34:31 -0700

    Remove if not exists

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -12,7 +12,7 @@ create table metadata_aspect_v2 (
 );
 
 -- create index on the "aspect" column to support aspect-oriented queries.
-CREATE INDEX IF NOT EXISTS aspectName ON metadata_aspect_v2 (aspect);
+CREATE INDEX aspectName ON metadata_aspect_v2 (aspect);
 
 insert into metadata_aspect_v2 (urn, aspect, version, metadata, createdon, createdby) values(
   'urn:li:corpuser:datahub',
