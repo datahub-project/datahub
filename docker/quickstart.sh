5b0453df6c7a11bf50d1eb58e66475821d0c76d3
Harshal Sheth <hsheth2@gmail.com>
2021-06-17 11:47:31 -0700

    feat(quickstart): use versioned docker images when on a release tag (#2715)

--- docker/quickstart.sh
+++ docker/quickstart.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 # Quickstarts DataHub by pulling all images from dockerhub and then running the containers locally. No images are
-# built locally. Note: by default this pulls the latest (head) version; you can change this to a specific version by setting
-# the DATAHUB_VERSION environment variable.
-export DATAHUB_VERSION=${DATAHUB_VERSION:-head}
+# built locally.
+# Note: by default this pulls the latest (head) version or the tagged version if you checked out a release tag.
+# You can change this to a specific version by setting the DATAHUB_VERSION environment variable.
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Detect if this is a checkout of a tagged branch.
+# If this is a tagged branch, use the tag as the default, otherwise default to head.
+# If DATAHUB_VERSION is set, it takes precedence.
+TAG_VERSION=$(cd $DIR && git name-rev --name-only --tags HEAD)
+DEFAULT_VERSION=$(echo $TAG_VERSION | sed 's/undefined/head/')
+export DATAHUB_VERSION=${DATAHUB_VERSION:-${DEFAULT_VERSION}}
+
+echo "Quickstarting DataHub: version ${DATAHUB_VERSION}"
 cd $DIR && docker-compose pull && docker-compose -p datahub up
