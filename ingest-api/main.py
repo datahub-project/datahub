e43e463169aa15deaaee7fe5518bceb65c9679f3
xiphl <xipherl9@gmail.com>
2021-06-23 21:00:38 +0700

    changes after firstround of testing

--- ingest-api/main.py
+++ ingest-api/main.py
@@ -1,43 +1,50 @@
 import uvicorn
-from fastapi import FastAPI
-from typing import Optional, List, Union
-from typing_extensions import TypedDict
-from pydantic import BaseModel, validator
+import logging
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from logging.handlers import TimedRotatingFileHandler
+
+from helper.models import FieldParam, create_dataset_params, dataset_status_params, determine_type
 from helper.mce_convenience import delete_mce, make_schema_mce, make_dataset_urn, \
                     make_user_urn, make_dataset_description_mce, recover_mce, \
                     make_browsepath_mce, make_ownership_mce, make_platform, get_sys_time 
 from datahub.emitter.rest_emitter import DatahubRestEmitter
 
+#todo - to refactor the classes here into additional modules when it gets unwieldy
+#env file, ie for pointing to the GMS rest endpoint.
+#can run from command line without using container, by using python main.py. however, the ports need changing.
+DEBUG = True
+datahub_url = "http://localhost:9002"
+api_emitting_port = 80 if not DEBUG else 8001
+rest_endpoint = "http://datahub-gms:8080" if not DEBUG else "http://localhost:8080" 
 
-import logging
-from logging.handlers import TimedRotatingFileHandler
-
+rootLogger = logging.getLogger("__name__")
 logformatter = logging.Formatter('%(asctime)s;%(levelname)s;%(message)s')
+rootLogger.setLevel(logging.DEBUG)
 
-log = TimedRotatingFileHandler('./log/debug.log', when='midnight', interval=1, backupCount=14)
-log.setLevel(logging.DEBUG)
-log.setFormatter(logformatter)
 streamLogger = logging.StreamHandler()
 streamLogger.setFormatter(logformatter)
 streamLogger.setLevel(logging.DEBUG)
-
-rootLogger = logging.getLogger("__name__")
-rootLogger.addHandler(log)    
 rootLogger.addHandler(streamLogger)
-rootLogger.setLevel(logging.DEBUG)
+
+if not DEBUG:
+    log = TimedRotatingFileHandler('./log/api.log', when='midnight', interval=1, backupCount=14)
+    log.setLevel(logging.DEBUG)
+    log.setFormatter(logformatter)
+    rootLogger.addHandler(log)    
 
 rootLogger.info("started!")
 
-#todo - to refactor the classes here into additional modules when it gets unwieldy
-#env file, ie for pointing to the GMS rest endpoint.
-
-rest_endpoint = "http://datahub-gms:8080"
-datahub_url = "http://localhost:9002"
-api_emitting_port = 80
-
 app = FastAPI(title="Datahub secret API",
     description="For generating datasets",
-    version="0.0.1",)
+    version="0.0.2",)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:9002"],
+    allow_credentials=False,
+    allow_methods=["POST", "GET"],
+)
 
 @app.get("/hello")
 async def hello_world() -> None:
@@ -45,60 +52,9 @@ async def hello_world() -> None:
     Just a hello world endpoint to ensure that the api is running. 
     """
     ## how to check that this dataset exist? - curl to GMS? 
-    rootLogger.info("hello world called")
-    return  "hello world"
+    rootLogger.info("hello world is called")
+    return  "API is alive"
 
-
-class FieldParam(TypedDict):
-    field_name: str
-    field_type: str
-    field_description: Optional[str]
-
-class create_dataset_params(BaseModel):
-    dataset_name: str 
-    dataset_type: str 
-    dataset_fields: List[FieldParam] 
-    dataset_owner: str
-    dataset_description: Union[None,str]     
-    dataset_location: Union[None,str]
-    dataset_origin: Union[None,str]
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "dataset_name": "name of dataset",
-                "dataset_type": "text/csv",
-                "dataset_description": "What this dataset is about...",
-                "dataset_owner": "12345",
-                "dataset_location": "the file can be found here @...",
-                "dataset_origin": "this dataset found came from... ie internet",
-                "dataset_fields": [{
-                    "field_name": "columnA",
-                    "field_type": "string",
-                    "field_description": "what is column A about"
-                },
-                {
-                    "field_name": "columnB",
-                    "field_type": "num",
-                    "field_description": "what is column B about"
-                }
-                ]
-            }
-        }
-    # @validator('dataset_name')
-    # def dataset_name_alphanumeric(cls, v):
-    #     assert len(set(v).difference(ascii_letters+digits+' -_/\\'))==0, 'dataset_name must be alphanumeric/space character only'
-    #     return v
-    # @validator('dataset_type')
-    # def dataset_type_alphanumeric(cls, v):
-    #     assert v.isalpha(), 'dataset_type must be alphabetical string only'
-    #     return v
-
-def determine_type(type_string:str) -> str:
-    if type_string.lower()=='text/csv':
-        return 'csv'
-    else:
-        return 'new_undefined_type'
 
 @app.post("/make_dataset")
 async def create_item(item: create_dataset_params) -> None:
@@ -107,35 +63,42 @@ async def create_item(item: create_dataset_params) -> None:
     """
     rootLogger.info("make_dataset_request_received {}".format(item))
     item.dataset_type = determine_type(item.dataset_type)
-    rootLogger.debug("type ok {}".format(item.dataset_type))
+    
     item.dataset_name = "{}_{}".format(item.dataset_name,str(get_sys_time()))
     datasetName = make_dataset_urn(item.dataset_type, item.dataset_name)
     platformName = make_platform(item.dataset_type)        
     browsePath = "/{}/{}".format(item.dataset_type, item.dataset_name) 
     #not sure what happens when multiple different datasets all lead to the same browsepath, probably disaster.    
-    requestor = make_user_urn(item.dataset_owner)
-    rootLogger.debug("defined")
+    requestor = make_user_urn(item.dataset_owner)    
+    
+    properties = {"dataset_origin": item.dict().get("dataset_origin", ""), 
+                    "dataset_location": item.dict().get("dataset_location", "")}
+    
+    all_mce=[]
+    dataset_description = item.dataset_description if item.dataset_description else ""
+    all_mce.append(make_dataset_description_mce(dataset_name = datasetName,
+                                                description = dataset_description, 
+                                                customProperties=properties))
+
+    all_mce.append(make_ownership_mce(actor = requestor, 
+                                        dataset_urn = datasetName))
+    all_mce.append(make_browsepath_mce(dataset_urn=datasetName, 
+                                        path=[browsePath]))
+    field_params = []
+    for existing_field in item.fields:
+        current_field={}
+        current_field.update(existing_field.dict())          
+        current_field["FieldParam"] = current_field.pop("field_name")
+        if "field_description" not in current_field:
+            current_field["field_description"] = ""
+        field_params.append(current_field)
+    all_mce.append(make_schema_mce(dataset_urn = datasetName,
+                                    platformName = platformName,
+                                    actor = requestor,
+                                    fields = field_params,
+                                    ))
+    
     try:
-        properties = {"dataset_origin": item.dict().get("dataset_origin", ""), "dataset_location": item.dict().get("dataset_location", "")}
-        
-        all_mce=[]
-        dataset_description = item.dataset_description if item.dataset_description else ""
-        all_mce.append(make_dataset_description_mce(dataset_name = datasetName,
-                                                    description = item.dataset_description, 
-                                                    customProperties=properties))
-        rootLogger.debug("properties ok")
-        all_mce.append(make_ownership_mce(actor = requestor, 
-                                            dataset_urn = datasetName))
-        all_mce.append(make_browsepath_mce(dataset_urn=datasetName, 
-                                            path=[browsePath]))
-        for field in item.dataset_fields:
-            field["fieldPath"] = field.pop("field_name")
-        all_mce.append(make_schema_mce(dataset_urn = datasetName,
-                                        platformName = platformName,
-                                        actor = requestor,
-                                        fields = item.dataset_fields,
-                                        ))
-        rootLogger.debug("all mce ok")
         emitter = DatahubRestEmitter(rest_endpoint)
 
         for mce in all_mce:
@@ -143,17 +106,14 @@ async def create_item(item: create_dataset_params) -> None:
         emitter._session.close()
     except Exception as e:
         rootLogger.debug(e)
-    rootLogger.info("make_dataset_request_completed_for {} requested_by {}".format(item.dataset_name, item.dataset_owner))      
-    return "dataset can be found at {}/dataset/{}".format(datahub_url, make_dataset_urn(item.dataset_type, item.dataset_name))
+        return Response("Dataset was not created because upstream has encountered an error {}".format(e), status_code=502)
+    rootLogger.info("Make_dataset_request_completed_for {} requested_by {}".format(item.dataset_name, item.dataset_owner))      
+    return Response(content = "dataset can be found at {}/dataset/{}".format(datahub_url, make_dataset_urn(item.dataset_type, item.dataset_name)),
+                        status_code = 205) 
 
-
-class delete_dataset_params(BaseModel):
-    dataset_name: str
-    requestor: str
-    platform: str
 
 @app.post("/delete_dataset")
-async def delete_item(item: delete_dataset_params) -> None:
+async def delete_item(item: dataset_status_params) -> None:
     """
     This endpoint is to support soft delete of datasets. Still require a database/ES chron job to remove the entries though, it only suppresses it from search and UI
     """
@@ -161,14 +121,18 @@ async def delete_item(item: delete_dataset_params) -> None:
     rootLogger.info("remove_dataset_request_received {}".format(item))
     datasetName = make_dataset_urn(item.platform, item.dataset_name)
     mce = delete_mce(dataset_name = datasetName)
-    emitter = DatahubRestEmitter(rest_endpoint)
-    emitter.emit_mce(mce)
-    emitter._session.close() 
+    try:
+        emitter = DatahubRestEmitter(rest_endpoint)
+        emitter.emit_mce(mce)
+        emitter._session.close()
+    except Exception as e:
+        rootLogger.debug(e)
+        return Response("Request was not fulfilled because upstream has encountered an error {}".format(e), status_code=502) 
     rootLogger.info("remove_dataset_request_completed_for {} requested_by {}".format(item.dataset_name, item.requestor))      
-    return "dataset has been removed from search and UI. please refresh the webpage."
+    return Response("dataset has been removed from search and UI. please refresh the webpage.", status_code=205)
 
 @app.post("/recover_dataset")
-async def recover_item(item: delete_dataset_params) -> None:
+async def recover_item(item: dataset_status_params) -> None:
     """
     This endpoint is meant for undoing soft deletes. 
     """
@@ -176,11 +140,15 @@ async def recover_item(item: delete_dataset_params) -> None:
     rootLogger.info("recover_dataset_request_received {}".format(item))
     datasetName = make_dataset_urn(item.platform, item.dataset_name)
     mce = recover_mce(dataset_name = datasetName)
-    emitter = DatahubRestEmitter(rest_endpoint)
-    emitter.emit_mce(mce)
-    emitter._session.close() 
+    try:
+        emitter = DatahubRestEmitter(rest_endpoint)
+        emitter.emit_mce(mce)
+        emitter._session.close() 
+    except Exception as e:
+        rootLogger.debug(e)
+        return Response("Request was not fulfilled because upstream has encountered an error {}".format(e), status_code=502) 
     rootLogger.info("recover_dataset_request_completed_for {} requested_by {}".format(item.dataset_name, item.requestor))      
-    return "dataset has been restored"
+    return Response("dataset has been restored", status_code=205)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=api_emitting_port)
