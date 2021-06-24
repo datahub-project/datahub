5535fb5c2e8392d3210c2944fc23199c2c304a47
Harshal Sheth <hsheth2@gmail.com>
2021-06-24 11:12:30 -0700

    issue queries better

--- metadata-ingestion/src/datahub/ingestion/source/snowflake_usage.py
+++ metadata-ingestion/src/datahub/ingestion/source/snowflake_usage.py
@@ -1,9 +1,11 @@
+import dataclasses
+import json
 import logging
-from dataclasses import dataclass, field
-from datetime import datetime
-from typing import Iterable
+from datetime import datetime, timezone
+from typing import Iterable, List, Optional
 
 import pydantic
+import pydantic.dataclasses
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 
@@ -18,33 +20,60 @@ SNOWFLAKE_USAGE_SQL_TEMPLATE = """
 SELECT
     -- access_history.query_id, -- only for debugging purposes
     access_history.query_start_time,
-    access_history.user_name,
-    -- access_history.direct_objects_accessed, -- might be useful in the future
-    access_history.base_objects_accessed,
     query_history.query_text,
     query_history.query_type,
+    access_history.base_objects_accessed,
+    -- access_history.direct_objects_accessed, -- might be useful in the future
     -- query_history.execution_status, -- not really necessary, but should equal "SUCCESS"
     -- query_history.warehouse_name,
+    access_history.user_name,
+    users.first_name,
+    users.last_name,
+    users.display_name,
+    users.email,
     query_history.role_name
 FROM
     snowflake.account_usage.access_history access_history
 LEFT JOIN
     snowflake.account_usage.query_history query_history
     ON access_history.query_id = query_history.query_id
-WHERE   query_start_time >= to_timestamp_ltz({start_time_millis}, 3)
+LEFT JOIN
+    snowflake.account_usage.users users
+    ON access_history.user_name = users.name
+WHERE   ARRAY_SIZE(base_objects_accessed) > 0
+    AND query_start_time >= to_timestamp_ltz({start_time_millis}, 3)
     AND query_start_time < to_timestamp_ltz({end_time_millis}, 3)
 ORDER BY query_start_time DESC
 ;
 """.strip()
 
 
-@dataclass
+@pydantic.dataclasses.dataclass
+class SnowflakeColumnReference:
+    columnId: int
+    columnName: str
+
+
+@pydantic.dataclasses.dataclass
+class SnowflakeObjectAccessEntry:
+    columns: List[SnowflakeColumnReference]
+    objectDomain: str
+    objectId: int
+    objectName: str
+
+
+@pydantic.dataclasses.dataclass
 class SnowflakeJoinedAccessEvent:
     query_start_time: datetime
-    user_name: str
-    base_objects_accessed: list
     query_text: str
     query_type: str
+    base_objects_accessed: List[SnowflakeObjectAccessEntry]
+
+    user_name: str
+    first_name: Optional[str]
+    last_name: Optional[str]
+    display_name: Optional[str]
+    email: str
     role_name: str
 
 
@@ -61,10 +90,10 @@ class SnowflakeUsageConfig(SnowflakeConfig, BaseUsageConfig):
         return v
 
 
-@dataclass
+@dataclasses.dataclass
 class SnowflakeUsageSource(Source):
     config: SnowflakeUsageConfig
-    report: SourceReport = field(default_factory=SourceReport)
+    report: SourceReport = dataclasses.field(default_factory=SourceReport)
 
     @classmethod
     def create(cls, config_dict, ctx):
@@ -73,7 +102,10 @@ class SnowflakeUsageSource(Source):
 
     def get_workunits(self) -> Iterable[UsageStatsWorkUnit]:
         access_events = self._get_snowflake_history()
+
+        access_events = list(access_events)
         breakpoint()
+        yield from []
 
     def _make_usage_query(self) -> str:
         return SNOWFLAKE_USAGE_SQL_TEMPLATE.format(
@@ -93,7 +125,16 @@ class SnowflakeUsageSource(Source):
 
         results = engine.execute(query)
         for row in results:
-            event = SnowflakeJoinedAccessEvent()
+            # Make some minor type conversions.
+            event_dict = row._asdict()
+            event_dict["base_objects_accessed"] = json.loads(
+                event_dict["base_objects_accessed"]
+            )
+            event_dict["query_start_time"] = (
+                event_dict["query_start_time"]
+            ).astimezone(tz=timezone.utc)
+
+            event = SnowflakeJoinedAccessEvent(**event_dict)
             yield event
 
     def get_report(self):
