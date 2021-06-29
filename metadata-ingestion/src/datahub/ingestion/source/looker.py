5e69a4355ea1d4cb1daa4fe1250131f10db05e1d
Harshal Sheth <hsheth2@gmail.com>
2021-06-28 20:40:10 -0700

    refactor(ingest): use common get_sys_time method (#2782)

--- metadata-ingestion/src/datahub/ingestion/source/looker.py
+++ metadata-ingestion/src/datahub/ingestion/source/looker.py
@@ -2,7 +2,6 @@ import datetime
 import json
 import logging
 import os
-import time
 from dataclasses import dataclass
 from dataclasses import field as dataclass_field
 from typing import Any, Iterable, List, MutableMapping, Optional, Sequence
@@ -18,6 +17,7 @@ from looker_sdk.sdk.api31.models import (
 
 from datahub.configuration import ConfigModel
 from datahub.configuration.common import AllowDenyPattern
+from datahub.emitter.mce_builder import get_sys_time
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.source.metadata_common import MetadataWorkUnit
@@ -304,7 +304,7 @@ class LookerDashboardSource(Source):
         self, dashboard_element: LookerDashboardElement
     ) -> MetadataChangeEvent:
         actor = self.source_config.actor
-        sys_time = int(time.time()) * 1000
+        sys_time = get_sys_time()
         chart_urn = f"urn:li:chart:({self.source_config.platform_name},{dashboard_element.get_urn_element_id()})"
         chart_snapshot = ChartSnapshot(
             urn=chart_urn,
@@ -338,7 +338,7 @@ class LookerDashboardSource(Source):
         self, looker_dashboard: LookerDashboard
     ) -> List[MetadataChangeEvent]:
         actor = self.source_config.actor
-        sys_time = int(time.time()) * 1000
+        sys_time = get_sys_time()
 
         chart_mces = [
             self._make_chart_mce(element)
