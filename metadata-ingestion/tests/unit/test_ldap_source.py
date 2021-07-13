98767fd695ca8fbd076509942aadfb0ef6d3a6d0
Harshal Sheth <hsheth2@gmail.com>
2021-07-13 13:18:48 -0700

    protect imports

--- metadata-ingestion/tests/unit/test_ldap_source.py
+++ metadata-ingestion/tests/unit/test_ldap_source.py
@@ -1,9 +1,9 @@
 import pytest
 
-from src.datahub.ingestion.source.ldap import parse_from_attrs, strip_ldap_info
-
 
 def test_strip_ldap_info():
+    from datahub.ingestion.source.ldap import strip_ldap_info
+
     assert (
         strip_ldap_info(b"uid=firstname.surname,ou=People,dc=internal,dc=machines")
         == "firstname.surname"
@@ -34,6 +34,8 @@ def test_strip_ldap_info():
     ],
 )
 def test_parse_from_attrs(input, expected):
+    from datahub.ingestion.source.ldap import parse_from_attrs
+
     assert (
         parse_from_attrs(
             input,
