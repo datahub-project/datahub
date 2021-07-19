dfacc6c14fb040569ca361c3550b6c0d7e5794ec
Kevin Hu <6051736+kevinhu@users.noreply.github.com>
2021-07-19 11:30:43 -0700

    feat(ingest): lineage for SageMaker model endpoints and groups (#2894)

--- metadata-ingestion/src/datahub/ingestion/source/sagemaker_processors/common.py
+++ metadata-ingestion/src/datahub/ingestion/source/sagemaker_processors/common.py
@@ -20,6 +20,8 @@ class SagemakerSourceConfig(AwsSourceConfig):
 class SagemakerSourceReport(SourceReport):
     feature_groups_scanned = 0
     features_scanned = 0
+    endpoints_scanned = 0
+    groups_scanned = 0
     models_scanned = 0
     jobs_scanned = 0
     datasets_scanned = 0
@@ -29,6 +31,12 @@ class SagemakerSourceReport(SourceReport):
 
     def report_feature_scanned(self) -> None:
         self.features_scanned += 1
+
+    def report_endpoint_scanned(self) -> None:
+        self.endpoints_scanned += 1
+
+    def report_group_scanned(self) -> None:
+        self.groups_scanned += 1
 
     def report_model_scanned(self) -> None:
         self.models_scanned += 1
