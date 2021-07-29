c7dddc20259e07c3c6b955e610d19f9576e0f6bb
Gabe Lyons <itsgabelyons@gmail.com>
2021-07-28 20:39:05 -0700

    feat(ml-model): adding ml models and ml model groups (#2961)

---
+++ datahub-web-react/src/app/entity/mlModel/MLModelEntity.tsx
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import { CodeSandboxOutlined } from '@ant-design/icons';
+import { MlModel, EntityType, SearchResult } from '../../../types.generated';
+import { Preview } from './preview/Preview';
+import { MLModelProfile } from './profile/MLModelProfile';
+import { Entity, IconStyleType, PreviewType } from '../Entity';
+import getChildren from '../../lineage/utils/getChildren';
+import { Direction } from '../../lineage/types';
+
+/**
+ * Definition of the DataHub MlModel entity.
+ */
+export class MLModelEntity implements Entity<MlModel> {
+    type: EntityType = EntityType.Mlmodel;
+
+    icon = (fontSize: number, styleType: IconStyleType) => {
+        if (styleType === IconStyleType.TAB_VIEW) {
+            return <CodeSandboxOutlined style={{ fontSize }} />;
+        }
+
+        if (styleType === IconStyleType.HIGHLIGHT) {
+            return <CodeSandboxOutlined style={{ fontSize, color: '#9633b9' }} />;
+        }
+
+        return (
+            <CodeSandboxOutlined
+                style={{
+                    fontSize,
+                    color: '#BFBFBF',
+                }}
+            />
+        );
+    };
+
+    isSearchEnabled = () => true;
+
+    isBrowseEnabled = () => true;
+
+    isLineageEnabled = () => true;
+
+    getAutoCompleteFieldName = () => 'name';
+
+    getPathName = () => 'mlModels';
+
+    getCollectionName = () => 'ML Models';
+
+    renderProfile = (urn: string) => <MLModelProfile urn={urn} />;
+
+    renderPreview = (_: PreviewType, data: MlModel) => {
+        return <Preview model={data} />;
+    };
+
+    renderSearch = (result: SearchResult) => {
+        const data = result.entity as MlModel;
+        return <Preview model={data} />;
+    };
+
+    getLineageVizConfig = (entity: MlModel) => {
+        return {
+            urn: entity.urn,
+            name: entity.name,
+            type: EntityType.Mlmodel,
+            upstreamChildren: getChildren({ entity, type: EntityType.Mlmodel }, Direction.Upstream).map(
+                (child) => child.entity.urn,
+            ),
+            downstreamChildren: getChildren({ entity, type: EntityType.Mlmodel }, Direction.Downstream).map(
+                (child) => child.entity.urn,
+            ),
+            icon: entity.platform.info?.logoUrl || undefined,
+            platform: entity.platform.name,
+        };
+    };
+}
