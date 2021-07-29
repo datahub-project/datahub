acffd8fb4d3699a5c33afa7d49e02fc96be0a299
James Lamb <jaylamb20@gmail.com>
2021-07-29 07:20:49 +0100

    fix(ingest): ensure that LookML files are always parsed in the same order (#2966)

--- metadata-ingestion/src/datahub/ingestion/source/lookml.py
+++ metadata-ingestion/src/datahub/ingestion/source/lookml.py
@@ -139,8 +139,9 @@ class LookerModel:
                 # Need to handle a relative path.
                 glob_expr = str(pathlib.Path(path).parent / inc)
             # "**" matches an arbitrary number of directories in LookML
-            outputs = glob.glob(glob_expr, recursive=True) + glob.glob(
-                f"{glob_expr}.lkml", recursive=True
+            outputs = sorted(
+                glob.glob(glob_expr, recursive=True)
+                + glob.glob(f"{glob_expr}.lkml", recursive=True)
             )
             if "*" not in inc and not outputs:
                 reporter.report_failure(path, f"cannot resolve include {inc}")
