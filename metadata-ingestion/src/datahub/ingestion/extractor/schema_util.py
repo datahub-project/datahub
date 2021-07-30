e85207675aeeaa1c28c55a9f9f6c76d7e5e3f60f
Ravindra Lanka <rlanka@acryl.io>
2021-07-29 21:01:22 -0700

    Added support for converting nested and recursive AVRO schemas to MCE fields

--- metadata-ingestion/src/datahub/ingestion/extractor/schema_util.py
+++ metadata-ingestion/src/datahub/ingestion/extractor/schema_util.py
@@ -1,8 +1,6 @@
-import logging
-from typing import Any, List, Optional, Union
-
 import avro.schema
-
+import logging
+from avrogen.dict_wrapper import DictWrapper
 from datahub.metadata.com.linkedin.pegasus2avro.schema import (
     ArrayTypeClass,
     BooleanTypeClass,
@@ -18,109 +16,162 @@ from datahub.metadata.com.linkedin.pegasus2avro.schema import (
     StringTypeClass,
     UnionTypeClass,
 )
+from typing import Any, List, Optional, Union, Dict, Callable
 
 """A helper file for Avro schema -> MCE schema transformations"""
 
 logger = logging.getLogger(__name__)
 
-_field_type_mapping = {
-    "null": NullTypeClass,
-    "bool": BooleanTypeClass,
-    "boolean": BooleanTypeClass,
-    "int": NumberTypeClass,
-    "long": NumberTypeClass,
-    "float": NumberTypeClass,
-    "double": NumberTypeClass,
-    "bytes": BytesTypeClass,
-    "string": StringTypeClass,
-    "record": RecordTypeClass,
-    "map": MapTypeClass,
-    "enum": EnumTypeClass,
-    "array": ArrayTypeClass,
-    "union": UnionTypeClass,
-    "fixed": FixedTypeClass,
-}
 
+class AvroToMceSchemaConverter:
+    field_type_mapping: Dict[str, DictWrapper] = {
+        "null": NullTypeClass,
+        "bool": BooleanTypeClass,
+        "boolean": BooleanTypeClass,
+        "int": NumberTypeClass,
+        "long": NumberTypeClass,
+        "float": NumberTypeClass,
+        "double": NumberTypeClass,
+        "bytes": BytesTypeClass,
+        "string": StringTypeClass,
+        "record": RecordTypeClass,
+        "map": MapTypeClass,
+        "enum": EnumTypeClass,
+        "array": ArrayTypeClass,
+        "union": UnionTypeClass,
+        "fixed": FixedTypeClass,
+    }
 
-def _get_column_type(field_type: Union[str, dict]) -> SchemaFieldDataType:
-    tp = field_type
-    if hasattr(tp, "type"):
-        tp = tp.type  # type: ignore
-    tp = str(tp)
-    TypeClass: Any = _field_type_mapping.get(tp)
-    # Note: we could populate the nestedTypes field for unions and similar fields
-    # for the other types as well. However, since we already populate the nativeDataType
-    # field below, it is mostly ok to leave this as not fully initialized.
-    dt = SchemaFieldDataType(type=TypeClass())
-    return dt
+    def __init__(self):
+        # Initialize the prefix name stack for nested name generation.
+        self._prefix_name_stack: List[str] = []
+        self._record_types_seen: List[str] = []
 
+    def _should_recurse(self, schema: avro.schema.Schema) -> bool:
+        return isinstance(schema, (
+            avro.schema.RecordSchema, avro.schema.UnionSchema, avro.schema.ArraySchema, avro.schema.MapSchema)) and (
+                       not isinstance(schema,
+                                      avro.schema.RecordSchema) or (schema.fullname not in self._record_types_seen))
 
-def _is_nullable(schema: avro.schema.Schema) -> bool:
-    if isinstance(schema, avro.schema.UnionSchema):
-        return any(_is_nullable(sub_schema) for sub_schema in schema.schemas)
-    elif isinstance(schema, avro.schema.PrimitiveSchema):
-        return schema.name == "null"
-    else:
-        return False
+    def _get_column_type(self, field_type: Union[str, dict]) -> SchemaFieldDataType:
+        tp = field_type
+        if hasattr(tp, "type"):
+            tp = tp.type  # type: ignore
+        tp = str(tp)
+        TypeClass: Any = self.field_type_mapping.get(tp)
+        # Note: we could populate the nestedTypes field for unions and similar fields
+        # for the other types as well. However, since we already populate the nativeDataType
+        # field below, it is mostly ok to leave this as not fully initialized.
+        dt = SchemaFieldDataType(type=TypeClass())
+        return dt
 
+    def _is_nullable(self, schema: avro.schema.Schema) -> bool:
+        if isinstance(schema, avro.schema.UnionSchema):
+            return any(self._is_nullable(sub_schema) for sub_schema in schema.schemas)
+        elif isinstance(schema, avro.schema.PrimitiveSchema):
+            return schema.name == "null"
+        else:
+            return False
 
-def _recordschema_to_mce_fields(schema: avro.schema.RecordSchema) -> List[SchemaField]:
-    fields: List[SchemaField] = []
+    def _get_cur_field_path(self) -> str:
+        return ".".join(self._prefix_name_stack)
 
-    for parsed_field in schema.fields:
-        description: Optional[str] = parsed_field.doc
-        if parsed_field.has_default:
-            description = description if description else "No description available."
-            description = f"{description}\nField default value: {parsed_field.default}"
+    def _recordschema_to_mce_fields(self, schema: avro.schema.RecordSchema) -> List[SchemaField]:
+        fields: List[SchemaField] = []
+        # Add the full name of this schema to the list of record types seen so far.
+        self._record_types_seen.append(schema.fullname)
+
+        # Generate the fields from the record schema's fields
+        for parsed_field in schema.fields:
+            # Generate the description.
+            description: Optional[str] = parsed_field.doc if parsed_field.doc else "No description available."
+            if parsed_field.has_default:
+                description = f"{description}\nField default value: {parsed_field.default}"
+            # Add the field name to the prefix stack.
+            self._prefix_name_stack.append(parsed_field.name)
+            cur_sub_fields: List[SchemaField] = []
+            if self._should_recurse(parsed_field.type):
+                # Recursively explore sub-types.
+                cur_sub_fields = self._to_mce_fields(parsed_field.type)
+                fields.extend(cur_sub_fields)
+            if not cur_sub_fields:
+                # No subfields exist. So, this is the leaf field. Go ahead and construct the field and append it to the output.
+                fields.append(SchemaField(
+                    fieldPath=self._get_cur_field_path(),
+                    nativeDataType=str(parsed_field.type),
+                    type=self._get_column_type(parsed_field.type),
+                    description=description,
+                    recursive=False,
+                    nullable=self._is_nullable(parsed_field.type),
+                ))
+            # Remove the name from prefix stack.
+            self._prefix_name_stack.pop()
+        return fields
+
+    def _arrayschema_to_mce_fields(self, schema: avro.schema.ArraySchema) -> List[SchemaField]:
+        fields: List[SchemaField] = []
+        # Recurse if needed.
+        if self._should_recurse(schema.items):
+            # Recursively explore sub-types
+            fields.extend(self._to_mce_fields(schema.items))
+        return fields
+
+    def _mapschema_to_mce_fields(self, schema: avro.schema.MapSchema) -> List[SchemaField]:
+        fields: List[SchemaField] = []
+        # Process the map schema
+        if self._should_recurse(schema.values):
+            fields.extend(self._to_mce_fields(schema.values))
+        return fields
+
+    def _unionschema_to_mce_fields(self, schema: avro.schema.UnionSchema) -> List[SchemaField]:
+        fields: List[SchemaField] = []
+        # Process the union schemas.
+        for sub_schema in schema.schemas:
+            # Recursively explore sub-types
+            if self._should_recurse(sub_schema):
+                fields.extend(self._to_mce_fields(sub_schema))
+        return fields
+
+    def _non_recursive_to_mce_fields(self, schema: avro.schema.Schema) -> List[SchemaField]:
+        fields: List[SchemaField] = []
+        # In the non-recursive case, only a single SchemaField will be returned
+        # and the fieldPath will be set to empty to signal that the type refers to the
+        # the whole object.
         field = SchemaField(
-            fieldPath=parsed_field.name,
-            nativeDataType=str(parsed_field.type),
-            type=_get_column_type(parsed_field.type),
-            description=description,
+            fieldPath="",
+            nativeDataType=str(schema.type),
+            type=self._get_column_type(schema.type),
+            description=schema.props.get("doc", None),
             recursive=False,
-            nullable=_is_nullable(parsed_field.type),
+            nullable=self._is_nullable(schema),
         )
-
         fields.append(field)
+        return fields
 
-    return fields
+    def _to_mce_fields(self, avro_schema: avro.schema.Schema) -> List[SchemaField]:
+        # Map of avro schema type to the conversion handler
+        avro_type_to_mce_converter_map: Dict[
+            avro.schema.RecordSchema, Callable[[avro.schema.Schema], List[SchemaField]]] = {
+            avro.schema.RecordSchema: self._recordschema_to_mce_fields,
+            avro.schema.UnionSchema: self._unionschema_to_mce_fields,
+            avro.schema.ArraySchema: self._arrayschema_to_mce_fields,
+            avro.schema.MapSchema: self._mapschema_to_mce_fields,
+            avro.schema.PrimitiveSchema: self._non_recursive_to_mce_fields,
+            avro.schema.FixedSchema: self._non_recursive_to_mce_fields,
+            avro.schema.EnumSchema: self._non_recursive_to_mce_fields,
+        }
+        # Invoke the relevant conversion handler for the schema element type.
+        return avro_type_to_mce_converter_map[type(avro_schema)](avro_schema)
 
-
-def _genericschema_to_mce_fields(schema: avro.schema.Schema) -> List[SchemaField]:
-    fields: List[SchemaField] = []
-
-    # In the generic (non-RecordSchema) case, only a single SchemaField will be returned
-    # and the fieldPath will be set to empty to signal that the type refers to the
-    # the whole object.
-    field = SchemaField(
-        fieldPath="",
-        nativeDataType=str(schema.type),
-        type=_get_column_type(schema.type),
-        description=schema.props.get("doc", None),
-        recursive=False,
-        nullable=_is_nullable(schema),
-    )
-    fields.append(field)
-
-    return fields
+    @classmethod
+    def to_mce_fields(cls, avro_schema_string: str) -> List[SchemaField]:
+        # Prefer the `parse` function over the deprecated `Parse` function.
+        avro_schema_parse_fn = getattr(avro.schema, "parse", "Parse")
+        avro_schema = avro_schema_parse_fn(avro_schema_string)
+        converter = cls()
+        return converter._to_mce_fields(avro_schema)
 
 
 def avro_schema_to_mce_fields(avro_schema_string: str) -> List[SchemaField]:
     """Converts an avro schema into a schema compatible with MCE"""
-
-    # Handle some library compatability issues.
-    if hasattr(avro.schema, "parse"):
-        schema_parse_fn = avro.schema.parse
-    else:
-        schema_parse_fn = avro.schema.Parse
-
-    parsed_schema: avro.schema.Schema = schema_parse_fn(avro_schema_string)
-
-    if isinstance(parsed_schema, avro.schema.RecordSchema):
-        schema_convert_fn = _recordschema_to_mce_fields
-    else:
-        schema_convert_fn = _genericschema_to_mce_fields
-
-    fields = schema_convert_fn(parsed_schema)
-
-    return fields
+    return AvroToMceSchemaConverter.to_mce_fields(avro_schema_string)
--- metadata-ingestion/tests/unit/test_schema_util.py
+++ metadata-ingestion/tests/unit/test_schema_util.py
@@ -1,6 +1,7 @@
+import os
 import pytest
-
 from datahub.ingestion.extractor.schema_util import avro_schema_to_mce_fields
+from pathlib import Path
 
 SCHEMA_WITH_OPTIONAL_FIELD_VIA_UNION_TYPE = """
 {
@@ -47,65 +48,6 @@ SCHEMA_WITH_OPTIONAL_FIELD_VIA_PRIMITIVE_TYPE = """
 }
 """
 
-SCHEMA_WITH_MAP_TYPE_FIELD = """
-{
-  "type": "record",
-  "name": "some.event.name",
-  "namespace": "some.namespace",
-  "fields": [
-    {
-      "name": "some.field.name",
-      "type": {
-        "type": "map",
-        "values": "long"
-      }
-    }
-  ]
-}
-"""
-
-SCHEMA_WITH_TOP_LEVEL_PRIMITIVE_FIELD = """
-{
-  "type": "string"
-}
-"""
-
-SCHEMA_WITH_TWO_FIELD_RECORD = """
-{
-  "type": "record",
-  "name": "some.event.name",
-  "namespace": "some.event.namespace",
-  "fields": [
-    {
-      "name": "my.field.A",
-      "type": "string",
-      "doc": "some.doc"
-    },
-    {
-      "name": "my.field.B",
-      "type": "string",
-      "doc": "some.doc"
-    }
-  ]
-}
-"""
-
-SCHEMA_WITH_DEFAULT_VALUE = """
-{
-  "type": "record",
-  "name": "some.event.name",
-  "namespace": "some.event.namespace",
-  "fields": [
-    {
-      "name": "my.field",
-      "type": "string",
-      "doc": "some docs",
-      "default": "this is custom, default value"
-    }
-  ]
-}
-"""
-
 
 @pytest.mark.parametrize(
     "schema",
@@ -127,28 +69,362 @@ def test_avro_schema_to_mce_fields_events_with_nullable_fields(schema):
 
 
 def test_avro_schema_to_mce_fields_sample_events_with_different_field_types():
-    schema = SCHEMA_WITH_MAP_TYPE_FIELD
+    schema = """
+{
+  "type": "record",
+  "name": "some.event.name",
+  "namespace": "some.namespace",
+  "fields": [
+    {
+      "name": "some.field.name",
+      "type": {
+        "type": "map",
+        "values": "long"
+      }
+    }
+  ]
+}
+"""
     fields = avro_schema_to_mce_fields(schema)
     assert 1 == len(fields)
+    assert fields[0].fieldPath == "some.field.name"
 
 
 def test_avro_schema_to_mce_fields_record_with_two_fields():
-    schema = SCHEMA_WITH_TWO_FIELD_RECORD
-
+    schema = """
+{
+  "type": "record",
+  "name": "some.event.name",
+  "namespace": "some.event.namespace",
+  "fields": [
+    {
+      "name": "my.field.A",
+      "type": "string",
+      "doc": "some.doc"
+    },
+    {
+      "name": "my.field.B",
+      "type": "string",
+      "doc": "some.doc"
+    }
+  ]
+}
+"""
     fields = avro_schema_to_mce_fields(schema)
     assert len(fields) == 2
+    assert fields[0].fieldPath == "my.field.A"
+    assert fields[1].fieldPath == "my.field.B"
 
 
 def test_avro_schema_to_mce_fields_toplevel_isnt_a_record():
-    schema = SCHEMA_WITH_TOP_LEVEL_PRIMITIVE_FIELD
-
+    schema = """
+{
+  "type": "string"
+}
+"""
     fields = avro_schema_to_mce_fields(schema)
     assert len(fields) == 1
+    assert fields[0].fieldPath == ""
 
 
 def test_avro_schema_to_mce_fields_with_default():
-    schema = SCHEMA_WITH_DEFAULT_VALUE
-
+    schema = """
+{
+  "type": "record",
+  "name": "some.event.name",
+  "namespace": "some.event.namespace",
+  "fields": [
+    {
+      "name": "my.field",
+      "type": "string",
+      "doc": "some docs",
+      "default": "this is custom, default value"
+    }
+  ]
+}
+"""
     fields = avro_schema_to_mce_fields(schema)
     assert len(fields) == 1
+    assert fields[0].fieldPath == "my.field"
     assert fields[0].description and "custom, default value" in fields[0].description
+
+
+def test_avro_schema_with_recursion():
+    schema = """
+{
+    "type": "record",
+    "name": "TreeNode",
+    "fields": [
+        {
+            "name": "value",
+            "type": "long"
+        },
+        {
+            "name": "children",
+            "type": { "type": "array", "items": "TreeNode" }
+        }
+    ]
+}
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 2
+    assert fields[0].fieldPath == "value"
+    assert fields[1].fieldPath == "children"
+
+
+def test_avro_sample_payment_schema_to_mce_fields_with_nesting():
+    schema = """
+{
+  "type": "record",
+  "name": "Payment",
+  "namespace": "some.event.namespace",
+  "fields": [
+    {"name": "id", "type": "string"},
+    {"name": "amount", "type": "double"},
+    {"name": "name","type": "string","default": ""},
+    {"name": "phoneNumber",
+     "type": [{
+         "type": "record",
+         "name": "PhoneNumber",
+         "fields": [{
+             "name": "areaCode",
+             "type": "string",
+             "default": ""
+             }, {
+             "name": "countryCode",
+             "type": "string",
+             "default": ""
+             }, {
+             "name": "prefix",
+             "type": "string",
+             "default": ""
+             }, {
+             "name": "number",
+             "type": "string",
+             "default": ""
+             }]
+         },
+         "null"
+     ],
+     "default": "null"
+    }
+  ]
+}
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 7
+    field_paths = ["id", "amount", "name", "phoneNumber.areaCode", "phoneNumber.countryCode", "phoneNumber.prefix",
+                   "phoneNumber.number"]
+    for i, f in enumerate(fields):
+        assert f.fieldPath == field_paths[i]
+
+
+def test_avro_schema_to_mce_fields_with_nesting_across_records():
+    schema = """
+[
+    {
+        "type": "record",
+        "name": "Address",
+        "fields": [
+            {"name": "streetaddress", "type": "string"},
+            {"name": "city", "type": "string"}
+        ]
+    },
+    {
+        "type": "record",
+        "name": "person",
+        "fields": [
+            {"name": "firstname", "type": "string"},
+            {"name": "lastname", "type": "string" },
+            {"name": "address", "type": "Address"}
+        ]
+    }
+]
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 5
+    expected_field_paths = ["streetaddress", "city", "firstname", "lastname", "address"]
+    for i, f in enumerate(fields):
+        assert f.fieldPath == expected_field_paths[i]
+
+
+def test_simple_record_with_primitive_types():
+    schema = """
+    {
+        "type": "record",
+        "name": "Simple",
+        "namespace": "com.linkedin",
+        "fields": [
+            {"name": "stringField", "type": "string", "doc": "string field"},
+            {"name": "booleanField", "type": "boolean" },
+            {"name": "intField", "type": "int" },
+            {
+                "name": "enumField",
+                "type": {
+                    "type": "enum",
+                    "name": "EnumField",
+                    "symbols": [ "TEST", "TEST1" ],
+                    "symbolDoc": {
+                        "TEST": "test enum",
+                        "TEST1": "test1 enum"
+                    }
+                }
+            }
+        ]
+    }
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 4
+    field_paths = ["stringField", "booleanField", "intField", "enumField"]
+    for i, f in enumerate(fields):
+        assert f.fieldPath == field_paths[i]
+
+
+def test_simple_nested_record_with_a_string_field():
+    schema = """
+    {
+        "type": "record",
+        "name": "Simple",
+        "namespace": "com.linkedin",
+        "fields": [{
+            "name": "nestedRcd",
+            "type": {
+                "type": "record",
+                "name": "NestedRcd",
+                "fields": [{
+                    "name": "aStringField",
+                     "type": "string"
+                } ]
+            }
+        }]
+    }
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 1
+    assert fields[0].fieldPath == "nestedRcd.aStringField"
+
+
+def test_union_with_nested_record_of_union():
+    schema = """
+    {
+        "type": "record",
+        "name": "UnionSample",
+        "namespace": "com.linkedin",
+        "fields": [
+            {
+                "name": "aUnion",
+                "type": [
+                    "boolean",
+                    {
+                        "type": "record",
+                        "name": "Rcd",
+                        "fields": [
+                            {
+                                "name": "aNullableStringField",
+                                "type": ["null", "string"]
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 1
+    assert fields[0].fieldPath == "aUnion.aNullableStringField"
+
+
+def test_nested_arrays():
+    schema = """
+{
+    "type": "record",
+    "name": "nestedArrays",
+    "namespace": "com.linkedin",
+    "fields": [{
+        "name": "ar",
+        "type": {
+            "type": "array",
+            "items": {
+                "type": "array",
+                "items": [
+                    "null",
+                    {
+                        "type": "record",
+                        "name": "Foo",
+                        "fields": [ {
+                            "name": "a",
+                            "type": "long"
+                        } ]
+                    }
+                ]
+            }
+        }
+    } ]
+}
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 1
+    assert fields[0].fieldPath == "ar.a"
+
+
+def test_map_of_union_of_int_and_record_of_union():
+    schema = """
+    {
+        "type": "record",
+        "name": "MapSample",
+        "namespace": "com.linkedin",
+        "fields": [{
+            "name": "aMap",
+            "type": {
+                "type": "map",
+                "values": [
+                    "int",
+                    {
+                        "type": "record",
+                        "name": "Rcd",
+                        "fields": [{
+                            "name": "aUnion",
+                            "type": ["null", "string"]
+                        }]
+                    }
+               ]
+            }
+        }]
+    }
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 1
+    assert fields[0].fieldPath == "aMap.aUnion"
+
+
+def test_recursive_avro():
+    schema = """
+    {
+        "type": "record",
+        "name": "Recursive",
+        "namespace": "com.linkedin",
+        "fields": [{
+            "name": "r",
+            "type": {
+                "type": "record",
+                "name": "R",
+                "fields": [
+                    { "name" : "anIntegerField", "type" : "int" },
+                    { "name": "aRecursiveField", "type": "com.linkedin.R"}
+                ]
+            }
+        }]
+    }
+"""
+    fields = avro_schema_to_mce_fields(schema)
+    assert len(fields) == 2
+    assert fields[0].fieldPath == "r.anIntegerField"
+    assert fields[1].fieldPath == "r.aRecursiveField"
+
+
+def test_mce_avro_parses_okay():
+    # This helps to exercise the complexity in parsing and catch unexpected regressions.
+    schema = Path(
+        os.path.join(os.path.dirname(__file__), "..", "..", "src", "datahub", "metadata", "schema.avsc")).read_text()
+    fields = avro_schema_to_mce_fields(schema)
