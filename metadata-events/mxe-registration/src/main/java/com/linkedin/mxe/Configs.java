3ab1373965e3c5e6babe13c93fe5e5d89effdeab
John Joyce <john@acryl.io>
2021-08-10 22:21:45 -0700

    checkstyle

--- metadata-events/mxe-registration/src/main/java/com/linkedin/mxe/Configs.java
+++ metadata-events/mxe-registration/src/main/java/com/linkedin/mxe/Configs.java
@@ -9,7 +9,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.avro.Schema;
-import org.apache.avro.specific.SpecificRecord;
 
 
 public class Configs {
