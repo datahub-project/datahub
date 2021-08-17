c16cc0d9018bde9e00e50b2c159dc00d2e94165a
Enrico Minack <github@enrico.minack.dev>
2021-08-17 09:42:39 +0200

    Add Dgraph to docker, as an alternative to Neo4J
    Elasticsearch backend becomes the default, Neo4J and Dgraph an equivalent alternative to elasticsearch.

--- docker/datahub-upgrade/datahub-upgrade.sh
+++ docker/datahub-upgrade/datahub-upgrade.sh
@@ -2,4 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IMAGE=acryldata/datahub-upgrade:head
-cd $DIR && docker pull ${IMAGE} && docker run --env-file ./env/docker.env --network="datahub_network" ${IMAGE} "$@"
+cd $DIR && docker pull ${IMAGE} && docker run --env-file ./env/docker-with-neo4j.env --network="datahub_network" ${IMAGE} "$@"
