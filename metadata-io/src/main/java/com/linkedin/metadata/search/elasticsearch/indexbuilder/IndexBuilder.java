059c00cf2a744ed8a9b4936b50a115799b2682f2
Dexter Lee <dexter@acryl.io>
2021-08-19 12:18:42 -0700

    feat(ingest): Add custom browse paths for kafka sources and remove browse lowercase filter (#3113)

--- metadata-io/src/main/java/com/linkedin/metadata/search/elasticsearch/indexbuilder/IndexBuilder.java
+++ metadata-io/src/main/java/com/linkedin/metadata/search/elasticsearch/indexbuilder/IndexBuilder.java
@@ -2,12 +2,18 @@ package com.linkedin.metadata.search.elasticsearch.indexbuilder;
 
 import com.google.common.collect.MapDifference;
 import com.google.common.collect.Maps;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Nonnull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest;
 import org.elasticsearch.action.admin.indices.alias.IndicesAliasesRequest.AliasActions;
 import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.settings.get.GetSettingsRequest;
 import org.elasticsearch.client.GetAliasesResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -15,13 +21,9 @@ import org.elasticsearch.client.core.CountRequest;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.client.indices.GetMappingsRequest;
+import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.reindex.ReindexRequest;
-
-import javax.annotation.Nonnull;
-import java.io.IOException;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 
 @Slf4j
@@ -55,13 +57,25 @@ public class IndexBuilder {
         .getSourceAsMap();
 
     MapDifference<String, Object> mappingsDiff = Maps.difference(mappings, oldMappings);
+
+    Settings oldSettings = searchClient.indices()
+        .getSettings(new GetSettingsRequest().indices(indexName), RequestOptions.DEFAULT)
+        .getIndexToSettings()
+        .valuesIt()
+        .next();
+
     // If there are no updates to mappings, return
-    if (mappingsDiff.areEqual()) {
+    if (mappingsDiff.areEqual() && equals(settings, oldSettings)) {
       log.info("No updates to index {}", indexName);
       return;
     }
 
-    log.info("There's diff between new mappings (left) and old mappings (right): {}", mappingsDiff.toString());
+    if (!mappingsDiff.areEqual()) {
+      log.info("There's diff between new mappings (left) and old mappings (right): {}", mappingsDiff.toString());
+    } else {
+      log.info("There's an update to settings");
+    }
+
     String tempIndexName = indexName + "_" + System.currentTimeMillis();
     createIndex(tempIndexName, mappings, settings);
     try {
@@ -101,8 +115,8 @@ public class IndexBuilder {
     log.info("Reindex from {} to {} succeeded", indexName, tempIndexName);
     String indexNamePattern = indexName + "_*";
     // Check if the original index is aliased or not
-    GetAliasesResponse aliasesResponse =
-        searchClient.indices().getAlias(new GetAliasesRequest(indexName).indices(indexNamePattern), RequestOptions.DEFAULT);
+    GetAliasesResponse aliasesResponse = searchClient.indices()
+        .getAlias(new GetAliasesRequest(indexName).indices(indexNamePattern), RequestOptions.DEFAULT);
     // If not aliased, delete the original index
     if (aliasesResponse.getAliases().isEmpty()) {
       searchClient.indices().delete(new DeleteIndexRequest().indices(indexName), RequestOptions.DEFAULT);
@@ -134,5 +148,39 @@ public class IndexBuilder {
     createIndexRequest.settings(settings);
     searchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
     log.info("Created index {}", indexName);
+  }
+
+  private boolean equals(Map<String, Object> newSettings, Settings oldSettings) {
+    if (!newSettings.containsKey("index") || !((Map<String, Object>) newSettings.get("index")).containsKey(
+        "analysis")) {
+      return true;
+    }
+    Map<String, Object> newAnalysis =
+        (Map<String, Object>) ((Map<String, Object>) newSettings.get("index")).get("analysis");
+    Settings oldAnalysis = oldSettings.getByPrefix("index.analysis.");
+    return equalsGroup(newAnalysis, oldAnalysis);
+  }
+
+  private boolean equalsGroup(Map<String, Object> newSettings, Settings oldSettings) {
+    if (!newSettings.keySet().equals(oldSettings.names())) {
+      return false;
+    }
+
+    for (String key : newSettings.keySet()) {
+      if (newSettings.get(key) instanceof Map) {
+        if (!equalsGroup((Map<String, Object>) newSettings.get(key), oldSettings.getByPrefix(key + "."))) {
+          return false;
+        }
+      } else if (newSettings.get(key) instanceof List) {
+        if (!newSettings.get(key).equals(oldSettings.getAsList(key))) {
+          return false;
+        }
+      } else {
+        if (!newSettings.get(key).toString().equals(oldSettings.get(key))) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 }
