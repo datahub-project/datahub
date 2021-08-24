f73725fdf6974dd96044cdabcb00e47aeeb1d4ae
Fredrik Sannholm <fredrik.sannholm@wolt.com>
2021-08-24 06:21:48 +0300

    feat(ingestion): bring your own SQL parser (#3110)

---
+++ metadata-ingestion/src/datahub/utilities/sql_parser.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+try:
+    from sql_metadata import Parser as MetadataSQLParser
+except ImportError:
+    pass
+
+
+class SQLParser(metaclass=ABCMeta):
+    def __init__(self, sql_query: str) -> None:
+        self._sql_query = sql_query
+
+    @abstractmethod
+    def get_tables(self) -> List[str]:
+        pass
+
+
+class DefaultSQLParser(SQLParser):
+    def __init__(self, sql_query: str) -> None:
+        self._parser = MetadataSQLParser(sql_query)
+
+    def get_tables(self) -> List[str]:
+        return self._parser.tables
