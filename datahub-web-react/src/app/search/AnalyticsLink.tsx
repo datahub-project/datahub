11c273474389c1f8af864df97400495669390e7e
Dexter Lee <dexter@acryl.io>
2021-09-02 11:23:25 -0700

    Fix build

--- datahub-web-react/src/app/search/AnalyticsLink.tsx
+++ datahub-web-react/src/app/search/AnalyticsLink.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Tag } from 'antd';
+import { Typography } from 'antd';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
