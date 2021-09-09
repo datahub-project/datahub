c31e4086778b4de2361b65b9e902f1e8193a93ae
Chinmay Bhat <chinmaya.bhat1@gmail.com>
2021-09-09 14:19:56 -0400

    updated data_platforms aspect jsons

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/steps/IngestDataPlatformsStep.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/steps/IngestDataPlatformsStep.java
@@ -6,6 +6,7 @@ import com.linkedin.common.AuditStamp;
 import com.linkedin.common.BrowsePaths;
 import com.linkedin.common.urn.Urn;
 import com.linkedin.dataplatform.DataPlatformInfo;
+import com.linkedin.metadata.Constants;
 import com.linkedin.metadata.boot.BootstrapStep;
 import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.dao.utils.RecordUtils;
@@ -21,57 +22,57 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Iterator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.xml.crypto.Data;
 
+
+@Slf4j
 public class IngestDataPlatformsStep implements BootstrapStep {
 
-    private static final String PLATFORM_ASPECT_NAME = "dataPlatformInfo";
+  private static final String PLATFORM_ASPECT_NAME = "dataPlatformInfo";
 
-    private final EntityService _entityService;
+  private final EntityService _entityService;
 
-    public IngestDataPlatformsStep(final EntityService entityService) {
-        _entityService = entityService;
+  public IngestDataPlatformsStep(final EntityService entityService) {
+    _entityService = entityService;
+  }
+
+  @Override
+  public String name() {
+    return "IngestDataPlatformsStep";
+  }
+
+  @Override
+  public void execute() throws IOException, URISyntaxException {
+
+    final ObjectMapper mapper = new ObjectMapper();
+
+    // 1. Read from the file into JSON.
+    final JsonNode dataPlatforms = mapper.readTree(new ClassPathResource("./boot/data_platforms.json").getFile());
+
+    if (!dataPlatforms.isArray()) {
+      throw new RuntimeException(String.format("Found malformed data platforms file, expected an Array but found %s",
+          dataPlatforms.getNodeType()));
     }
 
-    @Override
-    public String name() {
-        return "IngestDataPlatformsStep";
+    // 2. For each JSON object, cast into a DataPlatformSnapshot object.
+    for (final JsonNode dataPlatform : dataPlatforms) {
+      final Urn urn;
+      try {
+        urn = Urn.createFromString(dataPlatform.get("urn").asText());
+      } catch (URISyntaxException e) {
+        log.error("Malformed urn: {}", dataPlatform.get("urn").asText());
+        throw new RuntimeException("Malformed urn", e);
+      }
+      final DataPlatformInfo info =
+          RecordUtils.toRecordTemplate(DataPlatformInfo.class, dataPlatform.get("aspect").toString());
+
+      final AuditStamp aspectAuditStamp =
+          new AuditStamp().setActor(Urn.createFromString(Constants.UNKNOWN_ACTOR)).setTime(System.currentTimeMillis());
+
+      _entityService.ingestAspect(urn, PLATFORM_ASPECT_NAME, info, aspectAuditStamp);
     }
-
-    @Override
-    public void execute() throws IOException, URISyntaxException {
-
-        final ObjectMapper mapper = new ObjectMapper();
-
-        // 1. Read from the file into JSON.
-        final JsonNode dataPlatformsObj = mapper.readTree(new ClassPathResource("./boot/data_platforms.json").getFile());
-
-        if (!dataPlatformsObj.isArray()) {
-            throw new RuntimeException(String.format("Found malformed data platforms file, expected an Array but found %s", dataPlatformsObj.getNodeType()));
-        }
-
-        // 2. For each JSON object, cast into a DataPlatformSnapshot object.
-        for (Iterator<JsonNode> it = dataPlatformsObj.iterator(); it.hasNext(); ) {
-            final JsonNode dataPlatformObj = it.next();
-            final String dataPlatformSnapshotStr = "com.linkedin.pegasus2avro.metadata.snapshot.DataPlatformSnapshot";
-            final String dataPlatformInfoStr = "com.linkedin.pegasus2avro.dataplatform.DataPlatformInfo";
-            final Urn urn = Urn.createFromString(dataPlatformObj.get("proposedSnapshot").get(dataPlatformSnapshotStr).get("urn").asText());
-            final DataPlatformInfo info = RecordUtils.toRecordTemplate(DataPlatformInfo.class,
-                    dataPlatformObj.get("proposedSnapshot").get(dataPlatformSnapshotStr).get("aspects").get(0).get(dataPlatformInfoStr).toString()
-            );
-
-            try {
-                final AuditStamp aspectAuditStamp = new AuditStamp();
-                aspectAuditStamp.setActor(Urn.createFromString("urn:li:dataPlatform"));
-                aspectAuditStamp.setTime(System.currentTimeMillis());
-
-                _entityService.ingestAspect(urn, PLATFORM_ASPECT_NAME, info, aspectAuditStamp);
-
-            } catch (URISyntaxException e) {
-                throw new RuntimeException("Failed to ingest aspect", e);
-            }
-
-        }
-    }
+  }
 }
