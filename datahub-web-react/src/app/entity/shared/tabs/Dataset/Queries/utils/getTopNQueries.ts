3d25826d78b179e990b5c43dc7eccdbefb71a641
Ravindra Lanka <rlanka@acryl.io>
2021-09-13 12:05:27 -0700

    Show only unique top queries for the usage stats

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Queries/utils/getTopNQueries.ts
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Queries/utils/getTopNQueries.ts
@@ -6,12 +6,16 @@ export default function getTopNQueries(responseSize: number, buckets?: Maybe<Usa
         return response;
     }
 
+    const unique = (value: string | null, index: number, self): boolean => {
+        return self.indexOf(value) === index;
+    };
+
     for (let i = 0; i < buckets.length; i++) {
         const bucket = buckets[i];
 
         if (bucket?.metrics?.topSqlQueries && bucket?.metrics?.topSqlQueries !== null) {
             response = [...response, ...bucket?.metrics?.topSqlQueries];
-            response = response.filter(Boolean);
+            response = response.filter(Boolean).filter(unique);
             if (response.length >= responseSize) {
                 return response.slice(0, responseSize);
             }
