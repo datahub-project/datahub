b749e9f993dc2bfe9574881e925077845e26e1f9
Gabe Lyons <itsgabelyons@gmail.com>
2021-09-13 14:35:06 -0700

    fix(react): fix jitter on schema when adding description (#3230)

--- datahub-web-react/src/app/entity/dataset/profile/schema/components/SchemaDescriptionField.tsx
+++ datahub-web-react/src/app/entity/dataset/profile/schema/components/SchemaDescriptionField.tsx
@@ -1,4 +1,4 @@
-import { Typography, message, Tag } from 'antd';
+import { Typography, message, Button } from 'antd';
 import { EditOutlined } from '@ant-design/icons';
 import React, { useState } from 'react';
 import styled from 'styled-components';
@@ -14,9 +14,10 @@ const EditIcon = styled(EditOutlined)`
     display: none;
 `;
 
-const AddNewDescription = styled(Tag)`
-    cursor: pointer;
+const AddNewDescription = styled(Button)`
     display: none;
+    margin: -4px;
+    width: 140px;
 `;
 
 const ExpandedActions = styled.div`
@@ -112,6 +113,8 @@ export default function DescriptionField({
         (editable && description && <EditIcon twoToneColor="#52c41a" onClick={() => setShowAddModal(true)} />) ||
         undefined;
 
+    const showAddDescription = editable && !description;
+
     return (
         <DescriptionContainer
             onClick={(e) => {
@@ -121,19 +124,21 @@ export default function DescriptionField({
         >
             {expanded ? (
                 <>
-                    <DescriptionText source={description} />
-                    <ExpandedActions>
-                        {overLimit && (
-                            <ReadLessText
-                                onClick={() => {
-                                    setExpanded(false);
-                                }}
-                            >
-                                Read Less
-                            </ReadLessText>
-                        )}
-                        {EditButton}
-                    </ExpandedActions>
+                    {!!description && <DescriptionText source={description} />}
+                    {!!description && (
+                        <ExpandedActions>
+                            {overLimit && (
+                                <ReadLessText
+                                    onClick={() => {
+                                        setExpanded(false);
+                                    }}
+                                >
+                                    Read Less
+                                </ReadLessText>
+                            )}
+                            {EditButton}
+                        </ExpandedActions>
+                    )}
                 </>
             ) : (
                 <>
@@ -169,8 +174,8 @@ export default function DescriptionField({
                     />
                 </div>
             )}
-            {editable && !description && (
-                <AddNewDescription color="success" onClick={() => setShowAddModal(true)}>
+            {showAddDescription && (
+                <AddNewDescription type="text" onClick={() => setShowAddModal(true)}>
                     + Add Description
                 </AddNewDescription>
             )}
