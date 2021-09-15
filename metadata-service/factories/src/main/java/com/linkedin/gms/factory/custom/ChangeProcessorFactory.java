90584a01d0ebab03b5ac06e8ec6db34b5f95e89c
matthewc-gr <matthew.cannon@gresearch.co.uk>
2021-09-15 17:45:45 +0100

    Change processor factory

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/custom/ChangeProcessorFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/custom/ChangeProcessorFactory.java
@@ -3,6 +3,7 @@ package com.linkedin.gms.factory.custom;
 import com.linkedin.metadata.changeprocessor.AspectScope;
 import com.linkedin.metadata.changeprocessor.ChangeProcessor;
 import com.linkedin.metadata.changeprocessor.ChangeStreamProcessor;
+import com.linkedin.util.Pair;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
@@ -11,11 +12,9 @@ import javax.annotation.Nullable;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ServiceLoader;
-import java.util.SortedSet;
 
 @Configuration
 @Slf4j
@@ -24,46 +23,43 @@ public class ChangeProcessorFactory {
   @Value("${CUSTOM_JAR_PATH:#{null}}")
   private String customJarPath;
 
-  @Value("${CUSTOM_CLASS_NAME:#{null}}")
-  private String customClassName;
-
   @Nullable
   public ChangeStreamProcessor createInstance() {
     try {
-      if (customJarPath == null || customClassName == null) {
-        log.debug("Not loading custom defined behaviour");
-        return null;
-      }
-      log.debug("Loading custom defined behaviour {} from {}", customClassName, customJarPath);
-//      Class<?> clazz = getMethodInstance(customJarPath, customClassName);
-
       ChangeStreamProcessor changeStreamProcessor = new ChangeStreamProcessor();
 
-//      changeStreamProcessor.registerProcessor();
+      if (customJarPath == null ) {
+        log.debug("Not loading custom defined behaviour");
+        return changeStreamProcessor;
+      }
+
+      List<Pair<String, ChangeProcessor>> changeProcessors = buildChangeProcessors(customJarPath);
+
+      for (Pair<String, ChangeProcessor> processor : changeProcessors) {
+        changeStreamProcessor.registerProcessor(processor.getKey(), processor.getValue());
+      }
 
       return changeStreamProcessor;
     } catch (Exception ex) {
-      log.error("Failed to load custom defined behaviour {} from {}", customClassName, customJarPath);
+      log.error("Failed to load custom defined behaviour {}", customJarPath);
       return null;
     }
   }
 
-  private Map<String, SortedSet<ChangeProcessor>> getMethodInstance(String jarUrl, String className) throws Exception {
+  private List<Pair<String, ChangeProcessor>> buildChangeProcessors(String jarUrl) throws Exception {
     URL url = (new File(jarUrl).toURI().toURL());
     URLClassLoader classLoader = new URLClassLoader(new URL[]{url});
-
     ServiceLoader<ChangeProcessor> serviceLoader = ServiceLoader.load(ChangeProcessor.class, classLoader);
 
-    Map<String, SortedSet<ChangeProcessor>> changeProcessorMap = new HashMap<>();
-    Iterator<ChangeProcessor> iterator = serviceLoader.iterator();
+    List<Pair<String, ChangeProcessor>> changeProcessors = new ArrayList<>();
 
-    while (iterator.hasNext()){
-      ChangeProcessor changeProcessor = iterator.next();
-
-
-      AspectScope annotation = changeProcessor.getClass().getAnnotation(AspectScope.class);
+    for (ChangeProcessor changeProcessor : serviceLoader) {
+      String[] aspectNames = changeProcessor.getClass().getAnnotation(AspectScope.class).aspectNames();
+      for (String aspectName : aspectNames) {
+        changeProcessors.add(new Pair(aspectName, changeProcessor));
+      }
     }
 
-    return changeProcessorMap;
+    return changeProcessors;
   }
 }
