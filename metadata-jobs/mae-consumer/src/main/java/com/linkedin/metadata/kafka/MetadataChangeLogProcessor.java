645e7ceb78cd70257ff776461c9a002bfb04df27
rslanka <rlanka@acryl.io>
2021-09-14 18:35:10 -0700

    feat(Analytics): Support for Timeseries Aggregated Statistics (#3207)
    Co-authored-by: Gabe Lyons <itsgabelyons@gmail.com>
    Co-authored-by: Dexter Lee <dexter@acryl.io>

--- metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MetadataChangeLogProcessor.java
+++ metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MetadataChangeLogProcessor.java
@@ -52,7 +52,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import static com.linkedin.metadata.dao.Neo4jUtil.createRelationshipFilter;
+import static com.linkedin.metadata.dao.Neo4jUtil.*;
 
 
 @Slf4j
@@ -126,7 +126,8 @@ public class MetadataChangeLogProcessor {
           GenericAspectUtils.deserializeAspect(event.getAspect().getValue(), event.getAspect().getContentType(),
               aspectSpec);
       if (aspectSpec.isTimeseries()) {
-        updateTemporalStats(event.getEntityType(), event.getAspectName(), urn, aspect, event.getSystemMetadata());
+        updateTimeseriesFields(event.getEntityType(), event.getAspectName(), urn, aspect, aspectSpec,
+            event.getSystemMetadata());
       } else {
         updateSearchService(entitySpec.getName(), urn, aspectSpec, aspect);
         updateGraphService(urn, aspectSpec, aspect);
@@ -194,15 +195,17 @@ public class MetadataChangeLogProcessor {
   /**
    * Process snapshot and update timseries index
    */
-  private void updateTemporalStats(String entityType, String aspectName, Urn urn, RecordTemplate aspect,
-      SystemMetadata systemMetadata) {
-    JsonNode document;
+  private void updateTimeseriesFields(String entityType, String aspectName, Urn urn, RecordTemplate aspect,
+      AspectSpec aspectSpec, SystemMetadata systemMetadata) {
+    Map<String, JsonNode> documents;
     try {
-      document = TimeseriesAspectTransformer.transform(urn, aspect, systemMetadata);
+      documents = TimeseriesAspectTransformer.transform(urn, aspect, aspectSpec, systemMetadata);
     } catch (JsonProcessingException e) {
       log.error("Failed to generate timeseries document from aspect: {}", e.toString());
       return;
     }
-    _timeseriesAspectService.upsertDocument(entityType, aspectName, document);
+    documents.entrySet().forEach(document -> {
+      _timeseriesAspectService.upsertDocument(entityType, aspectName, document.getKey(), document.getValue());
+    });
   }
 }
