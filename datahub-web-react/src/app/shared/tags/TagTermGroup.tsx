8d3c4641f199946de9d5b2e84d02533841828a54
Gabe Lyons <itsgabelyons@gmail.com>
2021-09-16 12:33:24 -0700

    feat(glossary): splitting apart tags & terms into their own visual sections (#3250)

--- datahub-web-react/src/app/shared/tags/TagTermGroup.tsx
+++ datahub-web-react/src/app/shared/tags/TagTermGroup.tsx
@@ -64,7 +64,11 @@ export default function TagTermGroup({
     const entityRegistry = useEntityRegistry();
     const [showAddModal, setShowAddModal] = useState(false);
     const [addModalType, setAddModalType] = useState(EntityType.Tag);
-    const tagsEmpty = !editableTags?.tags?.length;
+    const tagsEmpty =
+        !editableTags?.tags?.length &&
+        !uneditableTags?.tags?.length &&
+        !editableGlossaryTerms?.terms?.length &&
+        !uneditableGlossaryTerms?.terms?.length;
     const [removeTagMutation] = useRemoveTagMutation();
     const [removeTermMutation] = useRemoveTermMutation();
 
@@ -203,9 +207,14 @@ export default function TagTermGroup({
                     </TagLink>
                 );
             })}
-            {showEmptyMessage && (canAddTag || canAddTerm) && tagsEmpty && (
+            {showEmptyMessage && canAddTag && tagsEmpty && (
                 <Typography.Paragraph type="secondary">
                     {EMPTY_MESSAGES.tags.title}. {EMPTY_MESSAGES.tags.description}
+                </Typography.Paragraph>
+            )}
+            {showEmptyMessage && canAddTerm && tagsEmpty && (
+                <Typography.Paragraph type="secondary">
+                    {EMPTY_MESSAGES.terms.title}. {EMPTY_MESSAGES.terms.description}
                 </Typography.Paragraph>
             )}
             {canAddTag && (uneditableTags?.tags?.length || 0) + (editableTags?.tags?.length || 0) < 10 && (
