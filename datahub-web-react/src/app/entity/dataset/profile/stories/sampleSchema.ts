57d5a43e60dc88b39b505f401901f3910ab8e4cf
Gabe Lyons <itsgabelyons@gmail.com>
2021-09-24 16:54:17 -0700

    starting tests

--- datahub-web-react/src/app/entity/dataset/profile/stories/sampleSchema.ts
+++ datahub-web-react/src/app/entity/dataset/profile/stories/sampleSchema.ts
@@ -190,3 +190,104 @@ export const sampleSchemaWithTags: Schema = {
         } as SchemaField,
     ],
 };
+
+export const sampleSchemaWithPkFk: Schema = {
+    name: 'MockSchema',
+    platformUrn: 'mock:urn',
+    version: 1,
+    hash: '',
+    fields: [
+        {
+            fieldPath: 'id',
+            nullable: false,
+            description: 'order id',
+            type: SchemaFieldDataType.Number,
+            nativeDataType: 'number',
+            recursive: false,
+            globalTags: {
+                tags: [
+                    {
+                        tag: {
+                            urn: 'urn:li:tag:Legacy',
+                            name: 'Legacy',
+                            description: 'this is a legacy dataset',
+                            type: EntityType.Tag,
+                        },
+                    },
+                ],
+            },
+            glossaryTerms: {
+                terms: [
+                    {
+                        term: {
+                            type: EntityType.GlossaryTerm,
+                            urn: 'urn:li:glossaryTerm:sample-glossary-term',
+                            name: 'sample-glossary-term',
+                            hierarchicalName: 'example.sample-glossary-term',
+                            glossaryTermInfo: {
+                                definition: 'sample definition',
+                                termSource: 'sample term source',
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            fieldPath: 'name',
+            nullable: true,
+            description: 'the name of the order',
+            type: SchemaFieldDataType.String,
+            nativeDataType: 'string',
+            recursive: false,
+        } as SchemaField,
+        {
+            fieldPath: 'shipping_address',
+            nullable: true,
+            description: 'the address the order ships to',
+            type: SchemaFieldDataType.String,
+            nativeDataType: 'string',
+            recursive: false,
+        } as SchemaField,
+        {
+            fieldPath: 'count',
+            nullable: true,
+            description: 'the number of items in the order',
+            type: SchemaFieldDataType.Number,
+            nativeDataType: 'number',
+            recursive: false,
+        },
+        {
+            fieldPath: 'cost',
+            nullable: true,
+            description: 'the dollar value of the order',
+            type: SchemaFieldDataType.Number,
+            nativeDataType: 'number',
+            recursive: false,
+        } as SchemaField,
+        {
+            fieldPath: 'was_returned',
+            nullable: true,
+            description: 'if the order was sent back',
+            type: SchemaFieldDataType.Boolean,
+            nativeDataType: 'boolean',
+            recursive: false,
+        },
+        {
+            fieldPath: 'payload',
+            nullable: true,
+            description: 'payload attached to the order',
+            type: SchemaFieldDataType.Bytes,
+            nativeDataType: 'bytes',
+            recursive: false,
+        },
+        {
+            fieldPath: 'payment_information',
+            nullable: true,
+            description: 'struct representing the payment information',
+            type: SchemaFieldDataType.Struct,
+            nativeDataType: 'struct',
+            recursive: false,
+        } as SchemaField,
+    ],
+};
