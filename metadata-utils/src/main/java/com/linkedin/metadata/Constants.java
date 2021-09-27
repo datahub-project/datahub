d9ad618e5dc07979e06b3754d8aa9a8074666493
John Joyce <john@acryl.io>
2021-09-27 10:13:44 -0700

    Improving NoCodeUpgrade logic

--- metadata-utils/src/main/java/com/linkedin/metadata/Constants.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/Constants.java
@@ -6,7 +6,7 @@ package com.linkedin.metadata;
 public class Constants {
   public static final String ACTOR_HEADER_NAME = "X-DataHub-Actor";
   public static final String DATAHUB_ACTOR = "urn:li:corpuser:datahub"; // Super user.
-  public static final String SYSTEM_ACTOR = "urn:li:principal:datahub"; // DataHub internal service principal.
+  public static final String SYSTEM_ACTOR = "urn:li:corpuser:__datahub_system"; // DataHub internal service principal.
   public static final String UNKNOWN_ACTOR = "urn:li:corpuser:UNKNOWN"; // Unknown principal.
   public static final Long ASPECT_LATEST_VERSION = 0L;
 
