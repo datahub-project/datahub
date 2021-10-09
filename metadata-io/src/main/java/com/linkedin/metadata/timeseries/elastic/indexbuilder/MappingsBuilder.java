01a3845ad263e375c46cb492a44bb5a846903bfb
Ravindra Lanka <rlanka@acryl.io>
2021-10-08 18:59:54 -0700

    Add custom elastic field mappings for all timeseries fields instead of using the dynamic elastic field mappings.

--- metadata-io/src/main/java/com/linkedin/metadata/timeseries/elastic/indexbuilder/MappingsBuilder.java
+++ metadata-io/src/main/java/com/linkedin/metadata/timeseries/elastic/indexbuilder/MappingsBuilder.java
@@ -1,7 +1,10 @@
 package com.linkedin.metadata.timeseries.elastic.indexbuilder;
 
 import com.google.common.collect.ImmutableMap;
+import com.linkedin.data.schema.DataSchema;
 import com.linkedin.metadata.models.AspectSpec;
+import com.linkedin.metadata.models.TimeseriesFieldCollectionSpec;
+import com.linkedin.metadata.models.TimeseriesFieldSpec;
 import java.util.HashMap;
 import java.util.Map;
 import javax.annotation.Nonnull;
@@ -35,7 +38,46 @@ public class MappingsBuilder {
     mappings.put(EVENT_FIELD, ImmutableMap.of("type", "object", "enabled", false));
     mappings.put(SYSTEM_METADATA_FIELD, ImmutableMap.of("type", "object", "enabled", false));
     mappings.put(IS_EXPLODED_FIELD, ImmutableMap.of("type", "boolean"));
+    aspectSpec.getTimeseriesFieldSpecs().stream().forEach(x -> setTimeseriesFieldSpecMapping(x, mappings));
+    aspectSpec.getTimeseriesFieldCollectionSpecs()
+        .stream()
+        .forEach(x -> setTimeseriesFieldCollectionSpecMapping(x, mappings));
 
     return ImmutableMap.of("properties", mappings);
+  }
+
+  private static <K, V> void setTimeseriesFieldSpecMapping(TimeseriesFieldSpec timeseriesFieldSpec,
+      Map<String, Object> mappings) {
+    mappings.put(timeseriesFieldSpec.getName(), getFieldMapping(timeseriesFieldSpec.getPegasusSchema().getType()));
+  }
+
+  private static <K, V> void setTimeseriesFieldCollectionSpecMapping(
+      TimeseriesFieldCollectionSpec timeseriesFieldCollectionSpec, Map<String, Object> mappings) {
+    String collectionFieldName = timeseriesFieldCollectionSpec.getName();
+    timeseriesFieldCollectionSpec.getTimeseriesFieldSpecMap()
+        .values()
+        .forEach(x -> mappings.put(collectionFieldName + "." + x.getName(),
+            getFieldMapping(x.getPegasusSchema().getType())));
+    mappings.put(
+        collectionFieldName + "." + timeseriesFieldCollectionSpec.getTimeseriesFieldCollectionAnnotation().getKey(),
+        getFieldMapping(DataSchema.Type.STRING));
+  }
+
+  private static <K, V> ImmutableMap<K, V> getFieldMapping(DataSchema.Type dataSchemaType) {
+    switch (dataSchemaType) {
+      case INT:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "integer");
+      case LONG:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "long");
+      case FLOAT:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "float");
+      case DOUBLE:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "double");
+      case RECORD:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "object", "enabled", false);
+      case STRING:
+      default:
+        return (ImmutableMap<K, V>) ImmutableMap.of("type", "keyword");
+    }
   }
 }
