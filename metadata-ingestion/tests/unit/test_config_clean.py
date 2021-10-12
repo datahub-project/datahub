002c11de834ad4750459ce013eb0df2f3ae5a0d1
Aseem Bansal <aseem.bansal@gogox.com>
2021-10-12 21:49:16 +0530

    feat(ingest): fix formatting

--- metadata-ingestion/tests/unit/test_config_clean.py
+++ metadata-ingestion/tests/unit/test_config_clean.py
@@ -3,7 +3,8 @@ from datahub.utilities import config_clean
 
 def test_url_without_slash_suffix():
     assert (
-        config_clean.remove_trailing_slashes("http://example.com") == "http://example.com"
+        config_clean.remove_trailing_slashes("http://example.com")
+        == "http://example.com"
     )
 
 
