a33df143b36e41d0884e9ba23ba2777841b380b2
RyanHolstien <rholstien@yahoo.com>
2021-10-13 21:46:33 -0500

    test(modelValidation): enhance error message

--- entity-registry/src/main/java/com/linkedin/metadata/models/SearchableFieldSpecExtractor.java
+++ entity-registry/src/main/java/com/linkedin/metadata/models/SearchableFieldSpecExtractor.java
@@ -88,14 +88,15 @@ public class SearchableFieldSpecExtractor implements SchemaVisitor {
   private void extractSearchableAnnotation(final Object annotationObj, final DataSchema currentSchema,
       final TraverserContext context) {
     final PathSpec path = new PathSpec(context.getSchemaPathSpec());
+    final Optional<PathSpec> fullPath = FieldSpecUtils.getPathSpecWithAspectName(context);
     final SearchableAnnotation annotation =
         SearchableAnnotation.fromPegasusAnnotationObject(annotationObj, FieldSpecUtils.getSchemaFieldName(path),
             currentSchema.getDereferencedType(), path.toString());
     if (_searchFieldNamesToPatch.containsKey(annotation.getFieldName()) && !_searchFieldNamesToPatch.get(
         annotation.getFieldName()).equals(context.getSchemaPathSpec().toString())) {
       throw new ModelValidationException(
-          String.format("Entity has multiple searchable fields with the same field name %s",
-              annotation.getFieldName()));
+          String.format("Entity has multiple searchable fields with the same field name %s, path: %s",
+              annotation.getFieldName(), fullPath.orElse(path)));
     }
     final SearchableFieldSpec fieldSpec = new SearchableFieldSpec(path, annotation, currentSchema);
     _specs.add(fieldSpec);
