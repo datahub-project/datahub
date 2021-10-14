c4aa314f0e26d4cce09782dc0abf1775c8c11348
John Joyce <john@acryl.io>
2021-10-13 17:45:56 -0700

    Fixing checkstyle

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/util/SearchInsightsUtil.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/util/SearchInsightsUtil.java
@@ -5,13 +5,13 @@ import com.linkedin.datahub.graphql.generated.SearchInsight;
 import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nonnull;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class SearchInsightsUtil {
 
   public static List<SearchInsight> getInsightsFromFeatures(@Nonnull final DoubleMap features) {
     // Currently no features are extracted.
     return Collections.emptyList();
   }
+
+  private SearchInsightsUtil() { }
 }
