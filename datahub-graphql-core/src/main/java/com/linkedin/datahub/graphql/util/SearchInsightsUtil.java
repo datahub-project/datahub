278cac255f25f5b631d48f6ba2356035c61e341f
John Joyce <john@acryl.io>
2021-10-15 12:51:34 -0700

    Adding back into acryl-main

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/util/SearchInsightsUtil.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/util/SearchInsightsUtil.java
@@ -2,15 +2,35 @@ package com.linkedin.datahub.graphql.util;
 
 import com.linkedin.data.template.DoubleMap;
 import com.linkedin.datahub.graphql.generated.SearchInsight;
-import java.util.Collections;
+import com.linkedin.metadata.search.features.Features;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 
 public class SearchInsightsUtil {
 
+  @Nonnull
   public static List<SearchInsight> getInsightsFromFeatures(@Nonnull final DoubleMap features) {
     // Currently no features are extracted.
-    return Collections.emptyList();
+    final List<SearchInsight> insights = new ArrayList<>();
+    final Optional<SearchInsight> queryCountInsight = extractQueryCountInsight(features);
+    queryCountInsight.ifPresent(insights::add);
+    return insights;
+  }
+
+  @Nonnull
+  private static Optional<SearchInsight> extractQueryCountInsight(final DoubleMap features) {
+    if (features.containsKey(Features.Name.QUERY_COUNT.toString())) {
+      // Search result has query count.
+      Double queryCount = features.get(Features.Name.QUERY_COUNT.toString());
+      if (queryCount > 20) {
+        // Show the query count insight.
+        String queryCountStr = queryCount > 1000 ? "1000+" : String.valueOf((int) queryCount.doubleValue());
+        return Optional.of(new SearchInsight(String.format("Queried %s times in the past month", queryCountStr), "\uD83D\uDE80"));
+      }
+    }
+    return Optional.empty();
   }
 
   private SearchInsightsUtil() { }
