53cbe4e41362a561eac67732d4f769a1c660dc69
Andreas Törnström Andersson <andreas.tornstrom-andersson@forefront.se>
2021-10-19 09:25:47 +0200

    The linting...

--- metadata-ingestion/tests/integration/dbt/test_dbt.py
+++ metadata-ingestion/tests/integration/dbt/test_dbt.py
@@ -83,7 +83,9 @@ def test_dbt_ingest(pytestconfig, tmp_path, mock_time):
             "dbt_without_schemas_with_filter_mces_golden.json",
             source_config_modifiers={
                 "load_schemas": False,
-                "node_name_pattern": {"deny": ["source.sample_dbt.pagila.payment_p2020_06"]},
+                "node_name_pattern": {
+                    "deny": ["source.sample_dbt.pagila.payment_p2020_06"]
+                },
             },
         ),
     ]
