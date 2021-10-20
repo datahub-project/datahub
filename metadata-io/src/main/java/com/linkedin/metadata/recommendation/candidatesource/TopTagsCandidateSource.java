bfa154a700615428476223cfafea5f0091f9912a
Dexter Lee <dexter@acryl.io>
2021-10-20 15:02:17 -0700

    Add top tags

--- metadata-io/src/main/java/com/linkedin/metadata/recommendation/candidatesource/TopTagsCandidateSource.java
+++ metadata-io/src/main/java/com/linkedin/metadata/recommendation/candidatesource/TopTagsCandidateSource.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import com.linkedin.common.urn.Urn;
 import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.dataplatform.DataPlatformInfo;
-import com.linkedin.metadata.entity.EntityService;
 import com.linkedin.metadata.models.registry.EntityRegistry;
 import com.linkedin.metadata.query.filter.Criterion;
 import com.linkedin.metadata.query.filter.CriterionArray;
@@ -24,6 +23,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
@@ -34,38 +34,38 @@ import org.springframework.cache.CacheManager;
 
 
 @Slf4j
-public class TopPlatformsCandidateSource implements RecommendationCandidateSource {
-  private final EntityService _entityService;
+public class TopTagsCandidateSource implements RecommendationCandidateSource {
   private final EntitySearchService _entitySearchService;
   private final NonEmptyEntitiesCache _nonEmptyEntitiesCache;
 
-  private static final String PLATFORM = "platform";
+  private static final String TAGS = "tags";
+  private static final int MAX_CONTENT = 10;
 
-  public TopPlatformsCandidateSource(EntityService entityService, EntitySearchService entitySearchService,
-      EntityRegistry entityRegistry, CacheManager cacheManager) {
-    _entityService = entityService;
+  public TopTagsCandidateSource(EntitySearchService entitySearchService, EntityRegistry entityRegistry,
+      CacheManager cacheManager) {
     _entitySearchService = entitySearchService;
     _nonEmptyEntitiesCache = new NonEmptyEntitiesCache(entityRegistry, entitySearchService, cacheManager);
   }
 
   @Override
   public String getTitle() {
-    return "Platforms";
+    return "Top Tags";
   }
 
   @Override
   public String getModuleId() {
-    return "Platforms";
+    return "TopTags";
   }
 
   @Override
   public RecommendationRenderType getRenderType() {
-    return RecommendationRenderType.PLATFORM_SEARCH_LIST;
+    return RecommendationRenderType.TAG_SEARCH_LIST;
   }
 
   @Override
   public boolean isEligible(@Nonnull Urn userUrn, @Nonnull RecommendationRequestContext requestContext) {
-    return requestContext.getScenario() == ScenarioType.HOME;
+    return requestContext.getScenario() == ScenarioType.HOME
+        || requestContext.getScenario() == ScenarioType.SEARCH_RESULTS;
   }
 
   @Override
@@ -74,41 +74,43 @@ public class TopPlatformsCandidateSource implements RecommendationCandidateSourc
     // Fetch number of documents per platform for each entity type
     List<Map<String, Long>> resultPerEntity =
         ConcurrencyUtils.transformAndCollectAsync(_nonEmptyEntitiesCache.getNonEmptyEntities(),
-            entity -> _entitySearchService.aggregateByValue(entity, PLATFORM, null, 100));
+            entity -> _entitySearchService.aggregateByValue(entity, TAGS, null, 100));
 
     // Merge the aggregated result into one
     Map<String, Long> mergedResult =
         resultPerEntity.stream().reduce(this::mergeAggregation).orElse(Collections.emptyMap());
 
     // Convert key into urns
-    Map<Urn, Long> platformCounts = mergedResult.entrySet().stream().map(entry -> {
+    Map<Urn, Long> tagCounts = mergedResult.entrySet().stream().map(entry -> {
       try {
-        Urn platformUrn = Urn.createFromString(entry.getKey());
-        return Optional.of(Pair.of(platformUrn, entry.getValue()));
+        Urn tagUrn = Urn.createFromString(entry.getKey());
+        return Optional.of(Pair.of(tagUrn, entry.getValue()));
       } catch (URISyntaxException e) {
-        log.error("Invalid platform urn {}", entry.getKey(), e);
+        log.error("Invalid tag urn {}", entry.getKey(), e);
         return Optional.<Pair<Urn, Long>>empty();
       }
     }).filter(Optional::isPresent).map(Optional::get).collect(Collectors.toMap(Pair::getKey, Pair::getValue));
 
-    if (platformCounts.isEmpty()) {
+    if (tagCounts.isEmpty()) {
       return Collections.emptyList();
     }
 
-    return platformCounts.entrySet()
-        .stream()
-        .filter(entry -> isValidPlatform(entry.getKey()))
+    // Get the top X valid platforms (ones with logo) with the most number of documents
+    PriorityQueue<Map.Entry<Urn, Long>> queue =
+        new PriorityQueue<>(MAX_CONTENT, Map.Entry.comparingByValue(Comparator.reverseOrder()));
+    for (Map.Entry<Urn, Long> entry : tagCounts.entrySet()) {
+      if (queue.size() < MAX_CONTENT) {
+        queue.add(entry);
+      } else if (queue.peek().getValue() < entry.getValue()) {
+        queue.poll();
+        queue.add(entry);
+      }
+    }
+
+    return queue.stream()
         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
         .map(entry -> buildRecommendationContent(entry.getKey(), entry.getValue()))
         .collect(Collectors.toList());
-  }
-
-  private boolean isValidPlatform(Urn platformUrn) {
-    RecordTemplate dataPlatformInfo = _entityService.getLatestAspect(platformUrn, "dataPlatformInfo");
-    if (dataPlatformInfo == null) {
-      return false;
-    }
-    return ((DataPlatformInfo) dataPlatformInfo).hasLogoUrl();
   }
 
   private Map<String, Long> mergeAggregation(Map<String, Long> first, Map<String, Long> second) {
@@ -116,14 +118,14 @@ public class TopPlatformsCandidateSource implements RecommendationCandidateSourc
         .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Long::sum));
   }
 
-  private RecommendationContent buildRecommendationContent(Urn platformUrn, long count) {
+  private RecommendationContent buildRecommendationContent(Urn tagUrn, long count) {
     // Set filters for platform
     SearchParams searchParams = new SearchParams().setQuery("")
         .setFilters(
-            new CriterionArray(ImmutableList.of(new Criterion().setField(PLATFORM).setValue(platformUrn.toString()))));
+            new CriterionArray(ImmutableList.of(new Criterion().setField(TAGS).setValue(tagUrn.toString()))));
     ContentParams contentParams = new ContentParams().setCount(count);
-    return new RecommendationContent().setValue(platformUrn.toString())
-        .setEntity(platformUrn)
+    return new RecommendationContent().setValue(tagUrn.toString())
+        .setEntity(tagUrn)
         .setParams(new RecommendationParams().setSearchParams(searchParams).setContentParams(contentParams));
   }
 }
