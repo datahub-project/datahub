d5908e71ce3f71d37f9b3e89eeab3140924a67fe
Dexter Lee <dexter@acryl.io>
2021-10-19 22:30:31 -0700

    Fix checkstyle

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/ranker/UsageBasedRankerFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/ranker/UsageBasedRankerFactory.java
@@ -2,7 +2,6 @@ package com.linkedin.gms.factory.search.ranker;
 
 import com.linkedin.gms.factory.search.features.GraphBasedFeatureFactory;
 import com.linkedin.gms.factory.search.features.UsageFeatureFactory;
-import com.linkedin.metadata.search.features.GraphBasedFeature;
 import com.linkedin.metadata.search.features.UsageFeature;
 import com.linkedin.metadata.search.ranker.UsageBasedRanker;
 import javax.annotation.Nonnull;
