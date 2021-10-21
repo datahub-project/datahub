e44a4606a15ec17d9bf64c44439932752b96ba0b
Dexter Lee <dexter@acryl.io>
2021-10-20 18:52:32 -0700

    Fix

--- metadata-io/src/main/java/com/linkedin/metadata/recommendation/RecommendationService.java
+++ metadata-io/src/main/java/com/linkedin/metadata/recommendation/RecommendationService.java
@@ -14,7 +14,6 @@ import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-@RequiredArgsConstructor
 public class RecommendationService {
 
   private final List<RecommendationCandidateSource> _candidateSources;
