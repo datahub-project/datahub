a305f84a4514dae3d8584750ec949504b6926181
Harshal Sheth <hsheth2@gmail.com>
2021-10-20 23:56:18 -0400

    perf(ingest): lazy load ingestion plugins

--- metadata-ingestion/tests/unit/test_plugin_system.py
+++ metadata-ingestion/tests/unit/test_plugin_system.py
@@ -3,7 +3,7 @@ from click.testing import CliRunner
 
 from datahub.configuration.common import ConfigurationError
 from datahub.entrypoints import datahub
-from datahub.ingestion.api.registry import Registry
+from datahub.ingestion.api.registry import PluginRegistry
 from datahub.ingestion.api.sink import Sink
 from datahub.ingestion.extractor.extractor_registry import extractor_registry
 from datahub.ingestion.sink.console import ConsoleSink
@@ -34,7 +34,7 @@ def test_list_all() -> None:
 
 def test_registry():
     # Make a mini sink registry.
-    fake_registry = Registry[Sink]()
+    fake_registry = PluginRegistry[Sink]()
     fake_registry.register("console", ConsoleSink)
     fake_registry.register_disabled("disabled", ModuleNotFoundError("disabled sink"))
 
@@ -48,7 +48,18 @@ def test_registry():
         fake_registry.get("datahub.ingestion.sink.console.ConsoleSink") == ConsoleSink
     )
 
-    with pytest.raises(KeyError, match="key cannot contain '.'"):
+    # Test lazy-loading capabilities.
+    fake_registry.register_lazy(
+        "lazy-console", "datahub.ingestion.sink.console:ConsoleSink"
+    )
+    assert fake_registry.get("lazy-console") == ConsoleSink
+
+    fake_registry.register_lazy("lazy-error", "thisdoesnot.exist")
+    with pytest.raises(ConfigurationError, match="disabled"):
+        fake_registry.get("lazy-error")
+
+    # Test error-checking.
+    with pytest.raises(KeyError, match="special characters"):
         fake_registry.register("thisdoesnotexist.otherthing", ConsoleSink)
     with pytest.raises(KeyError, match="in use"):
         fake_registry.register("console", ConsoleSink)
