aa6b458e861855b9f53a403a27e3f00f855f2d53
Enrico Minack <github@enrico.minack.dev>
2021-10-21 22:39:31 +0200

    Reduce code duplication in docker/quickstart/generate_and_compare.sh

--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -10,30 +10,16 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
-
 python generate_docker_quickstart.py ../docker-compose-without-neo4j.yml ../docker-compose-without-neo4j.override.yml temp-without-neo4j.quickstart.yml
+python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.monitoring.quickstart.yml
 
-python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.quickstart.monitoring.yml
-
-if cmp docker-compose.quickstart.yml temp.quickstart.yml; then
-    printf 'docker-compose.quickstart.yml is up to date.'
-else
-    printf 'docker-compose.quickstart.yml is out of date.'
+for flavour in "" "-without-neo4j" ".monitoring"
+do
+  if cmp docker-compose$flavour.quickstart.yml temp$flavour.quickstart.yml; then
+    printf 'docker-compose$flavour.quickstart.yml is up to date.'
+  else
+    printf 'docker-compose$flavour.quickstart.yml is out of date.'
     exit 1
-fi
+  fi
+done
 
-if cmp docker-compose-without-neo4j.quickstart.yml temp-without-neo4j.quickstart.yml; then
-    printf 'docker-compose-without-neo4j.quickstart.yml is up to date.'
-    exit 0
-else
-    printf 'docker-compose-without-neo4j.quickstart.yml is out of date.'
-    exit 1
-fi
-
-if cmp docker-compose.quickstart.monitoring.yml temp.quickstart.monitoring.yml; then
-    printf 'docker-compose.quickstart.monitoring.yml is up to date.'
-    exit 0
-else
-    printf 'docker-compose.quickstart.monitoring.yml is out of date.'
-    exit 1
-fi
