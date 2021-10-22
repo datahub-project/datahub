deedc120e2437f643271195c0e998f2aa2e3f66f
Gabe Lyons <itsgabelyons@gmail.com>
2021-10-21 19:35:57 -0700

    feat(docs-site): remove ga and add peleton logo (#3437)

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -157,9 +157,6 @@ module.exports = {
       //   darkTheme: require('prism-react-renderer/themes/dracula'),
       additionalLanguages: ["ini"],
     },
-    gtag: {
-      trackingID: "G-2G54RXWD4D",
-    },
     algolia: {
       apiKey: "26a4b687e96e7476b5a6f11365a83336",
       indexName: "datahubproject",
