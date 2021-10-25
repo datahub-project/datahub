ee25612f4f546d207736faabece71c40cd8ba95b
Remi Salmon <salmon.remi@gmail.com>
2021-10-25 09:06:52 -0600

    fix(ingest): fix lookml derived tables parsing

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -99,7 +99,7 @@ plugins: Dict[str, Set[str]] = {
     "kafka-connect": sql_common | {"requests"},
     "ldap": {"python-ldap>=2.4"},
     "looker": looker_common,
-    "lookml": looker_common | {"lkml>=1.1.0", "sql-metadata==2.2.1"},
+    "lookml": looker_common | {"lkml>=1.1.0", "sql-metadata==2.2.2"},
     "mongodb": {"pymongo>=3.11"},
     "mssql": sql_common | {"sqlalchemy-pytds>=0.3"},
     "mssql-odbc": sql_common | {"pyodbc"},
