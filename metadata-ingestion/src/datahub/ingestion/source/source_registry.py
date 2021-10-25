dccef30dafd945f44142ad05ab9b73d149fb5430
Harshal Sheth <hsheth2@gmail.com>
2021-10-25 17:10:24 -0400

    perf(ingest): lazy load ingestion plugins (#3430)

--- metadata-ingestion/src/datahub/ingestion/source/source_registry.py
+++ metadata-ingestion/src/datahub/ingestion/source/source_registry.py
@@ -1,8 +1,8 @@
-from datahub.ingestion.api.registry import Registry
+from datahub.ingestion.api.registry import PluginRegistry
 from datahub.ingestion.api.source import Source
 
-source_registry = Registry[Source]()
-source_registry.load("datahub.ingestion.source.plugins")
+source_registry = PluginRegistry[Source]()
+source_registry.register_from_entrypoint("datahub.ingestion.source.plugins")
 
 # This source is always enabled
 assert source_registry.get("file")
