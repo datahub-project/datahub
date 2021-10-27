13ed6a5eb2bab7462e45248e48eea3d4423a5f74
John Joyce <john@acryl.io>
2021-10-27 12:49:23 -0700

    actually fix

--- metadata-service/graphql-api/src/main/java/com/datahub/metadata/graphql/GraphQLEngineFactory.java
+++ metadata-service/graphql-api/src/main/java/com/datahub/metadata/graphql/GraphQLEngineFactory.java
@@ -41,12 +41,11 @@ public class GraphQLEngineFactory {
   private EntityService _entityService;
 
   @Autowired
-  private RecommendationsService _recommendationsService;
-
-  @Autowired
   @Qualifier("graphClient")
   private GraphClient _graphClient;
 
+  @Autowired
+  private RecommendationsService _recommendationsService;
 
   @Value("${ANALYTICS_ENABLED:true}") // TODO: Migrate to DATAHUB_ANALYTICS_ENABLED
   private Boolean isAnalyticsEnabled;
