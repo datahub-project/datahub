f3b931740d9e2628dd5d4b7cba554309f81f0a84
Gabe Lyons <itsgabelyons@gmail.com>
2021-11-04 19:30:27 -0700

    lint fix

--- metadata-io/src/main/java/com/linkedin/metadata/entity/RollbackResult.java
+++ metadata-io/src/main/java/com/linkedin/metadata/entity/RollbackResult.java
@@ -1,9 +1,9 @@
 package com.linkedin.metadata.entity;
 
 import com.linkedin.common.urn.Urn;
+
 import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.events.metadata.ChangeType;
-import com.linkedin.mxe.MetadataAuditOperation;
 import com.linkedin.mxe.SystemMetadata;
 import lombok.Value;
 
