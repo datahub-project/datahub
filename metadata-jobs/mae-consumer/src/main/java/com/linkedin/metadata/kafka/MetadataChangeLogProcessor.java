de156f1b89cd0c46dc0383bf4a669d5407e165d3
Shirshanka Das <shirshanka@apache.org>
2021-11-06 15:15:25 -0700

    feat(metadata-service): adding support for custom metadata models

--- metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MetadataChangeLogProcessor.java
+++ metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MetadataChangeLogProcessor.java
@@ -1,7 +1,6 @@
 package com.linkedin.metadata.kafka;
 
 import com.codahale.metrics.Histogram;
-
 import com.codahale.metrics.MetricRegistry;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
