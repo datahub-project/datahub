d7e9233bc730989517839b329e090e40ee3bce38
Danny Lee <me@danielhlee.com>
2021-11-05 21:56:33 -0400

    - Move env vars containing dots(.), from ES env file to ES entry in docker-compose.yml - add double quotes around env vars with whitespace

--- docker/dev.sh
+++ docker/dev.sh
@@ -17,12 +17,6 @@ fi
 # YOU MUST BUILD VIA GRADLE BEFORE RUNNING THIS.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && \
-  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose \
-    -f docker-compose.yml \
-    -f docker-compose.override.yml \
-    -f docker-compose.dev.yml \
-    $CONSUMERS_COMPOSE $MONITORING_COMPOSE pull \
-&& \
   COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -p datahub \
     -f docker-compose.yml \
     -f docker-compose.override.yml \
