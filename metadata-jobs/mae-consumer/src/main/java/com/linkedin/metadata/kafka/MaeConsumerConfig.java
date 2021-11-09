7c4d63400596a147ba539523663116860def9170
Dexter Lee <dexter@acryl.io>
2021-11-09 11:07:13 -0800

    Revert "feat(git-version): Encode the latest release included in the build (#3535)" (#3539)
    This reverts commit e01be7bb3d760c03883c9f18700b99b82375bb6f.

--- metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MaeConsumerConfig.java
+++ metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MaeConsumerConfig.java
@@ -1,31 +1,20 @@
 package com.linkedin.metadata.kafka;
 
-import com.linkedin.gms.factory.common.GitVersionFactory;
-import com.linkedin.metadata.version.GitVersion;
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 @Controller
-@Import(GitVersionFactory.class)
 public class MaeConsumerConfig {
-  private final Map<String, String> config;
+    Map<String, String> config = new HashMap<String, String>() {{
+        put("noCode", "true");
+    }};
 
-  public MaeConsumerConfig(GitVersion gitVersion) {
-    config = new HashMap<>();
-    config.put("noCode", "true");
-
-    config.put("version", gitVersion.getVersion());
-    config.put("commit", gitVersion.getCommitId());
-  }
-
-  @GetMapping("/config")
-  @ResponseBody
-  public Map<String, String> getConfig() {
-    return config;
-  }
+    @GetMapping("/config")
+    @ResponseBody
+    public Map<String, String> sayHello() {
+      return config;
+    }
 }
