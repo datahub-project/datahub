a54084cf3bd12563263fe69d310ec00af7d5ceb4
John Joyce <john@acryl.io>
2021-11-11 13:18:29 -0800

    spacing

--- metadata-ingestion/src/datahub/ingestion/source/usage/bigquery_usage.py
+++ metadata-ingestion/src/datahub/ingestion/source/usage/bigquery_usage.py
@@ -255,8 +255,8 @@ class BigQueryUsageConfig(BaseUsageConfig):
     project_id: Optional[str] = None  # deprecated in favor of `projects`
     extra_client_options: dict = {}
     env: str = builder.DEFAULT_ENV
-    log_page_size: pydantic.PositiveInt = 1000
 
+    log_page_size: pydantic.PositiveInt = 1000
     query_log_delay: Optional[pydantic.PositiveInt] = None
     max_query_duration: timedelta = timedelta(minutes=15)
 
