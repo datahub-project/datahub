5071f65a72fa7c983323ed583c114537aa751a69
Kevin Hu <kevinhuwest@gmail.com>
2021-11-16 16:44:34 -0500

    Scaffold analyzers

--- metadata-ingestion/src/datahub/ingestion/source/data_lake.py
+++ metadata-ingestion/src/datahub/ingestion/source/data_lake.py
@@ -1,4 +1,5 @@
 import dataclasses
+import logging
 import os
 from dataclasses import field as dataclass_field
 from enum import Enum
@@ -7,32 +8,38 @@ from typing import Any, Dict, Iterable, List, Optional
 import pydantic
 import pydeequ
 from pydeequ.analyzers import (
+    AnalysisRunBuilder,
     AnalysisRunner,
     ApproxCountDistinct,
+    ApproxQuantile,
     ApproxQuantiles,
-    Completeness,
-    CountDistinct,
-    Distinctness,
     Histogram,
     Maximum,
     Mean,
     Minimum,
     Size,
     StandardDeviation,
-    UniqueValueRatio,
 )
 from pyspark.sql import Row, SparkSession
+from pyspark.sql.functions import col, count, isnan, when
 
 from datahub.configuration.common import AllowDenyPattern, ConfigModel
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.api.workunit import MetadataWorkUnit
 
+logging.getLogger("py4j").setLevel(logging.ERROR)
+
+
+class FileType(Enum):
+    PARQUET = "parquet"
+    CSV = "csv"
+
 
 class DataLakeSourceConfig(ConfigModel):
 
     file: str
-    file_type: Optional[str] = None
+    file_type: Optional[FileType] = None
 
     limit: Optional[int] = None
     offset: Optional[int] = None
@@ -74,9 +81,16 @@ class DataLakeSourceReport(SourceReport):
         self.filtered.append(file)
 
 
-class FileType(Enum):
-    PARQUET = "parquet"
-    CSV = "csv"
+class TableWrapper:
+    table: pydeequ.Table
+    analyzer: AnalysisRunBuilder
+    columns: List[str]
+
+    def __init__(self, table):
+        self.table = table
+        self.analyzer = AnalysisRunner(self.spark).onData(table).addAnalyzer(Size())
+        self.columns = table.columns
+
 
 class DataLakeSource(Source):
     source_config: DataLakeSourceConfig
@@ -85,44 +99,107 @@ class DataLakeSource(Source):
     def __init__(self, config: DataLakeSourceConfig, ctx: PipelineContext):
         super().__init__(ctx)
         self.source_config = config
-
-        self.spark = (SparkSession
-            .builder
-            .config("spark.jars.packages", pydeequ.deequ_maven_coord)
+        self.spark = (
+            SparkSession.builder.config(
+                "spark.jars.packages", pydeequ.deequ_maven_coord
+            )
             .config("spark.jars.excludes", pydeequ.f2j_maven_coord)
-            .getOrCreate())
-        
+            .getOrCreate()
+        )
+
     @classmethod
     def create(cls, config_dict, ctx):
         config = DataLakeSourceConfig.parse_obj(config_dict)
         return cls(config, ctx)
 
-    def read_file(self, file: str, file_type:FileType):
-        
-        if file_type == FileType.PARQUET:
+    def calculate_null_count(self, table: TableWrapper):
+        if self.config.include_field_null_count:
+            # Deequ only has methods for getting null fraction, so we use Spark to get null count
+            null_counts = table.table.select(
+                [count(when(isnan(c), c)).alias(c) for c in table.columns]
+            ).show()
+
+        return
+
+    def prep_min_value(self, table: TableWrapper):
+        if self.config.include_field_min_value:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(Minimum(column))
+        return
+
+    def prep_max_value(self, table: TableWrapper):
+        if self.config.include_field_max_value:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(Maximum(column))
+        return
+
+    def prep_mean_value(self, table: TableWrapper):
+        if self.config.include_field_mean_value:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(Mean(column))
+        return
+
+    def prep_median_value(self, table: TableWrapper):
+        if self.config.include_field_median_value:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(ApproxQuantile(column, 0.5))
+        return
+
+    def prep_stdev_value(self, table: TableWrapper):
+        if self.config.include_field_stddev_value:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(StandardDeviation(column))
+        return
+
+    def prep_quantiles(self, table: TableWrapper):
+        if self.config.include_field_quantiles:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(
+                    ApproxQuantiles(column, [0.05, 0.25, 0.5, 0.75, 0.95])
+                )
+        return
+
+    def prep_distinct_value_frequencies(self, table: TableWrapper):
+        if self.config.include_field_distinct_value_frequencies:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(ApproxCountDistinct(column))
+        return
+
+    def prep_field_histogram(self, table: TableWrapper):
+        if self.config.include_field_histogram:
+            for column in table.columns:
+                table.analyzer.addAnalyzer(Histogram(column))
+        return
+
+    def calculate_sample_values(self, table: TableWrapper):
+        num_rows_to_sample = 3
+
+        if self.config.include_field_sample_values:
+            rdd_sample = table.table.rdd.takeSample(False, num_rows_to_sample, seed=0)
+
+        return
+
+    def read_file(self, file: str, file_type: FileType):
+
+        if file_type is FileType.PARQUET:
             df = self.spark.read.parquet(file)
-        elif file_type == FileType.CSV:
+        elif file_type is FileType.CSV:
             df = self.spark.read.csv(file)
+        else:
+            raise ValueError("File type not found")
 
         return df
 
     def generate_profiles(self):
-        
-        df = self.read_file(self.source_config.file, self.source_config.file_type)
 
-        columns = df.columns
-            
+        table = self.read_file(self.source_config.file, self.source_config.file_type)
 
-        analyzer = AnalysisRunner(self.spark) \
-            .onData(df) \
-            .addAnalyzer(Size())
+        analysis_table = TableWrapper(table)
 
-        for column in columns:
-            analyzer.addAnalyzer(Completeness(column))
-
-        analyzer_result = analyzer.run()
+        analyzer_result = analysis_table.analyer.run()
 
     def get_workunits(self) -> Iterable[MetadataWorkUnit]:
+        self.generate_profiles()
         return []
 
     def get_report(self):
--- metadata-ingestion/tests/integration/data_lake/test_data_lake.py
+++ metadata-ingestion/tests/integration/data_lake/test_data_lake.py
@@ -3,6 +3,7 @@ from click.testing import CliRunner
 from freezegun import freeze_time
 
 from datahub.entrypoints import datahub
+from datahub.ingestion.run.pipeline import Pipeline
 from tests.test_helpers import fs_helpers, mce_helpers
 from tests.test_helpers.click_helpers import assert_result_ok
 from tests.test_helpers.docker_helpers import wait_for_port
@@ -11,19 +12,48 @@ FROZEN_TIME = "2020-04-14 07:00:00"
 
 
 @pytest.mark.integration
-def test_data_lake_inggest(pytestconfig, tmp_path, mock_time):
+def test_data_lake_ingest(pytestconfig, tmp_path, mock_time):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/data_lake"
-    
-    # Run the metadata ingestion pipeline.
-    runner = CliRunner()
-    with fs_helpers.isolated_filesystem(tmp_path):
-        config_file = (test_resources_dir / "data_lake_to_file.yml").resolve()
-        result = runner.invoke(datahub, ["ingest", "-c", f"{config_file}"])
-        assert_result_ok(result)
 
-        # Verify the output.
-        mce_helpers.check_golden_file(
-            pytestconfig,
-            output_path="data_lake_mces.json",
-            golden_path=test_resources_dir / "data_lake_mces_golden.json",
-        )
+    # Run the metadata ingestion pipeline.
+    pipeline = Pipeline.create(
+        {
+            "run_id": "data-lake-test",
+            "source": {
+                "type": "data-lake",
+                "config": {
+                    "file": str(test_resources_dir / "test.csv"),
+                    "file_type": "csv",
+                },
+            },
+            "sink": {
+                "type": "file",
+                "config": {
+                    "filename": f"{tmp_path}/data_lake_mces.json",
+                },
+            },
+        }
+    )
+    pipeline.run()
+    pipeline.raise_from_status()
+
+    # Verify the output.
+    mce_helpers.check_golden_file(
+        pytestconfig,
+        output_path=tmp_path / "data_lake_mces.json",
+        golden_path=test_resources_dir / "data_lake_mces_golden.json",
+    )
+
+    # # Run the metadata ingestion pipeline.
+    # runner = CliRunner()
+    # with fs_helpers.isolated_filesystem(tmp_path):
+    #     config_file = (test_resources_dir / "data_lake_to_file.yml").resolve()
+    #     result = runner.invoke(datahub, ["ingest", "-c", f"{config_file}"])
+    #     assert_result_ok(result)
+
+    #     # Verify the output.
+    #     mce_helpers.check_golden_file(
+    #         pytestconfig,
+    #         output_path="data_lake_mces.json",
+    #         golden_path=test_resources_dir / "data_lake_mces_golden.json",
+    #     )
