b0f652156fb1ab3881824aea31a91bbf730a6590
xiphl <xipherl9@gmail.com>
2021-11-30 15:30:57 +0800

    banner added only

--- datahub-web-react/src/app/home/HomePage.tsx
+++ datahub-web-react/src/app/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { HomePageHeader } from './HomePageHeader';
 import { HomePageBody } from './HomePageBody';
+import { BannerSplash } from '../announce/BannerSplash';
 
 export const HomePage = () => {
     return (
         <>
+            <BannerSplash />
             <HomePageHeader />
             <HomePageBody />
         </>
