ed828fdb3274a871a0d8eccfc96576c3703f6d79
John Joyce <john@acryl.io>
2021-11-30 19:59:39 -0800

    adding gql files to generateGraphQLSchema

--- docs-website/graphql/generateGraphQLSchema.sh
+++ docs-website/graphql/generateGraphQLSchema.sh
@@ -6,3 +6,8 @@ echo "# Auto Generated During Docs Build" >> combined.graphql
 cat ../../datahub-graphql-core/src/main/resources/entity.graphql >> combined.graphql
 cat ../../datahub-graphql-core/src/main/resources/search.graphql >> combined.graphql
 cat ../../datahub-graphql-core/src/main/resources/app.graphql >> combined.graphql
+cat ../../datahub-graphql-core/src/main/resources/recommendation.graphql >> combined.graphql
+cat ../../datahub-graphql-core/src/main/resources/actions.graphql >> combined.graphql
+cat ../../datahub-graphql-core/src/main/resources/constraints.graphql >> combined.graphql
+cat ../../datahub-graphql-core/src/main/resources/auth.graphql >> combined.graphql
+
