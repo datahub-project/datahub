e289719089eddce6d2584a6c8e966e6e0077d011
Pedro Silva <pedro@Pedros-MacBook-Pro.local>
2021-12-01 20:21:21 +0000

    Fix formatting & incorrect convertion issues

--- metadata-io/src/test/java/com/linkedin/metadata/DockerTestUtils.java
+++ metadata-io/src/test/java/com/linkedin/metadata/DockerTestUtils.java
@@ -7,10 +7,14 @@ public class DockerTestUtils {
     final private static int MIN_MEMORY_NEEDED_GB = 8;
 
     public static void checkContainerEngine(DockerClient dockerClient) {
-        final long dockerEngineMemory = dockerClient.infoCmd().exec().getMemTotal() /1024/1024/1024;
-        if ( dockerEngineMemory < MIN_MEMORY_NEEDED_GB) {
-            final String error = String.format("Total Docker memory configured: %s GB is below the minimum threshold of %d GB", dockerEngineMemory, MIN_MEMORY_NEEDED_GB);
+        final long dockerEngineMemory = dockerClient.infoCmd().exec().getMemTotal() / 1000 / 1000 / 1000;
+        if (dockerEngineMemory < MIN_MEMORY_NEEDED_GB) {
+            final String error = String.format("Total Docker memory configured: %s GB is below the minimum threshold "
+                    + "of %d GB", dockerEngineMemory, MIN_MEMORY_NEEDED_GB);
             throw new IllegalStateException(error);
         }
+    }
+
+    private DockerTestUtils() {
     }
 }
