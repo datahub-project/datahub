4e472a7e0591f68f42164c8024b77eb0c335da77
Pedro Silva <pedro.cls93@gmail.com>
2021-12-02 12:08:02 +0000

    Improve message by showing docker engine memory config

--- metadata-io/src/test/java/com/linkedin/metadata/DockerTestUtils.java
+++ metadata-io/src/test/java/com/linkedin/metadata/DockerTestUtils.java
@@ -7,10 +7,11 @@ public class DockerTestUtils {
     final private static int MIN_MEMORY_NEEDED_GB = 8;
 
     public static void checkContainerEngine(DockerClient dockerClient) {
-        final long dockerEngineMemory = dockerClient.infoCmd().exec().getMemTotal() / 1000 / 1000 / 1000;
-        if (dockerEngineMemory < MIN_MEMORY_NEEDED_GB) {
-            final String error = String.format("Total Docker memory configured: %s GB is below the minimum threshold "
-                    + "of %d GB", dockerEngineMemory, MIN_MEMORY_NEEDED_GB);
+        final long dockerEngineMemoryBytes = dockerClient.infoCmd().exec().getMemTotal();
+        final long dockerEngineMemoryGB = dockerEngineMemoryBytes / 1000 / 1000 / 1000;
+        if (dockerEngineMemoryGB < MIN_MEMORY_NEEDED_GB) {
+            final String error = String.format("Total Docker memory configured: %s GB (%d bytes) is below the minimum threshold "
+                    + "of %d GB", dockerEngineMemoryGB, dockerEngineMemoryBytes, MIN_MEMORY_NEEDED_GB);
             throw new IllegalStateException(error);
         }
     }
