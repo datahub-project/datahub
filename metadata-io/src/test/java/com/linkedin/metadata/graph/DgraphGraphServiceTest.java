d96e36ae4e98c4cad40f1dd2dbadb37256ec20b1
Enrico Minack <github@enrico.minack.dev>
2021-12-02 22:37:39 +0100

    Use latest Dgraph release v21.12.0

--- metadata-io/src/test/java/com/linkedin/metadata/graph/DgraphGraphServiceTest.java
+++ metadata-io/src/test/java/com/linkedin/metadata/graph/DgraphGraphServiceTest.java
@@ -52,7 +52,7 @@ public class DgraphGraphServiceTest extends GraphServiceTestBase {
 
     @BeforeTest
     public void setup() {
-        _container = new DgraphContainer(DgraphContainer.DEFAULT_IMAGE_NAME.withTag("v21.03.0"))
+        _container = new DgraphContainer(DgraphContainer.DEFAULT_IMAGE_NAME.withTag("v21.12.0"))
                 .withTmpFs(Collections.singletonMap("/dgraph", "rw,noexec,nosuid,size=1g"))
                 .withStartupTimeout(Duration.ofMinutes(1))
                 .withStartupAttempts(3);
