fc62a1fe18f5d695a063d6aae6de83303c9873ca
Gabe Lyons <itsgabelyons@gmail.com>
2021-12-02 17:30:31 -0800

    fix login test

--- smoke-test/tests/cypress/cypress/integration/login.js
+++ smoke-test/tests/cypress/cypress/integration/login.js
@@ -2,7 +2,7 @@ describe('homepage', () => {
   it('logs in', () => {
     cy.visit('/');
 		cy.get('input[placeholder=Username]').type('datahub');
-		cy.get('input[placeholder=Password]').type('wrong');
+		cy.get('input[placeholder=Password]').type('datahub');
 		cy.contains('Log in').click();
 		cy.contains('Welcome back, datahub');
   });
