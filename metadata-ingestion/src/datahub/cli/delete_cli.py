708c09d81ad359d90e038d318bad872ed67629b0
Kevin Hu <6051736+kevinhu@users.noreply.github.com>
2021-12-07 08:57:12 -0800

    feat(ingestion): anonymous usage stats (#3668)

--- metadata-ingestion/src/datahub/cli/delete_cli.py
+++ metadata-ingestion/src/datahub/cli/delete_cli.py
@@ -14,6 +14,7 @@ from datahub.cli.cli_utils import guess_entity_type
 from datahub.emitter import rest_emitter
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.metadata.schema_classes import ChangeTypeClass, StatusClass
+from datahub.telemetry import telemetry
 
 logger = logging.getLogger(__name__)
 
@@ -87,6 +88,7 @@ def delete_for_registry(
 @click.option("--query", required=False, type=str)
 @click.option("--registry-id", required=False, type=str)
 @click.option("-n", "--dry-run", required=False, is_flag=True)
+@telemetry.with_telemetry
 def delete(
     urn: str,
     force: bool,
@@ -172,6 +174,7 @@ def delete(
         )
 
 
+@telemetry.with_telemetry
 def delete_with_filters(
     dry_run: bool,
     soft: bool,
@@ -181,6 +184,7 @@ def delete_with_filters(
     env: Optional[str] = None,
     platform: Optional[str] = None,
 ) -> DeletionResult:
+
     session, gms_host = cli_utils.get_session_and_host()
     logger.info(f"datahub configured with {gms_host}")
     emitter = rest_emitter.DatahubRestEmitter(gms_server=gms_host)
@@ -216,6 +220,7 @@ def delete_with_filters(
     return batch_deletion_result
 
 
+@telemetry.with_telemetry
 def delete_one_urn(
     urn: str,
     soft: bool = False,
@@ -224,6 +229,7 @@ def delete_one_urn(
     cached_session_host: Optional[Tuple[sessions.Session, str]] = None,
     cached_emitter: Optional[rest_emitter.DatahubRestEmitter] = None,
 ) -> DeletionResult:
+
     deletion_result = DeletionResult()
     deletion_result.num_entities = 1
     deletion_result.num_records = UNKNOWN_NUM_RECORDS  # Default is unknown
