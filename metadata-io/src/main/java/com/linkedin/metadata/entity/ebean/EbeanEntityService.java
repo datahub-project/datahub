2f963557ce0760b9ed22f96dd0a84274e2141405
Dexter Lee <dexter@acryl.io>
2021-12-09 00:04:59 +0900

    Fix

--- metadata-io/src/main/java/com/linkedin/metadata/entity/ebean/EbeanEntityService.java
+++ metadata-io/src/main/java/com/linkedin/metadata/entity/ebean/EbeanEntityService.java
@@ -11,7 +11,6 @@ import com.linkedin.data.schema.RecordDataSchema;
 import com.linkedin.data.template.DataTemplateUtil;
 import com.linkedin.data.template.JacksonDataTemplateCodec;
 import com.linkedin.data.template.RecordTemplate;
-import com.linkedin.entity.EntityResponse;
 import com.linkedin.entity.EnvelopedAspect;
 import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.aspect.Aspect;
