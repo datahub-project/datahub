7e61f9cfb5da0d55bcf5de437c3aaef5452a3546
Gabe Lyons <itsgabelyons@gmail.com>
2021-12-08 23:41:18 -0800

    feat(ingestion): Add lineage support for Redshift source (#3697)
    Co-authored-by: treff7es <treff7es@gmail.com>

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -122,7 +122,7 @@ plugins: Dict[str, Set[str]] = {
     "oracle": sql_common | {"cx_Oracle"},
     "postgres": sql_common | {"psycopg2-binary", "GeoAlchemy2"},
     "redash": {"redash-toolbelt", "sql-metadata"},
-    "redshift": sql_common | {"sqlalchemy-redshift", "psycopg2-binary", "GeoAlchemy2"},
+    "redshift": sql_common | {"sqlalchemy-redshift", "psycopg2-binary", "GeoAlchemy2", "sqllineage"},
     "redshift-usage": sql_common
     | {"sqlalchemy-redshift", "psycopg2-binary", "GeoAlchemy2"},
     "sagemaker": aws_common,
