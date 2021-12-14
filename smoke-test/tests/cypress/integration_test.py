e9c87bc0dfd2934a59dcc571a1c7723225398a4d
Gabe Lyons <itsgabelyons@gmail.com>
2021-12-13 23:08:30 -0800

    ci(cypress): adding the foundation for cypress integration tests & some starter coverage for login, search & updates (#3672)

---
+++ smoke-test/tests/cypress/integration_test.py
@@ -0,0 +1,28 @@
+import pytest
+import subprocess
+
+from tests.utils import ingest_file_via_rest
+from tests.utils import delete_urns_from_file
+
+
+@pytest.fixture(scope="module", autouse=True)
+def ingest_cleanup_data():
+    print("ingesting test data")
+    ingest_file_via_rest("tests/cypress/data.json")
+    yield
+    print("removing test data")
+    delete_urns_from_file("tests/cypress/data.json")
+
+
+def test_run_cypress(frontend_session, wait_for_healthchecks):
+    command = f"npx cypress run"
+    print('starting?')
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd="tests/cypress")
+    stdout = proc.stdout.read()
+    stderr = proc.stderr.read()
+    return_code = proc.wait()
+    print(stdout.decode("utf-8"))
+    print('stderr output:')
+    print(stderr.decode("utf-8"))
+    print('return code', return_code)
+    assert(return_code == 0)
