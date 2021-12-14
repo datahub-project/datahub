af8df21c26c2e06d0b4570ab0e538bc49fa10a9c
ksrinath <k.srinath@gmail.com>
2021-12-14 15:13:36 +0530

    feat(model): model for column-level lineage Add aspect to DataSet and modify DataJobInputOutput to capture field-level lineages. Add example of emitting column-level lineage in lineage_emitter_view_job_mcp.py . Deprecated DatasetUpstreamLineage aspect, and DatasetFieldUrn typeref. Pending: The parentFields in FieldLineageDetails is not yet Searchable.

--- metadata-ingestion/examples/library/lineage_emitter_view_job_mcp.py
+++ metadata-ingestion/examples/library/lineage_emitter_view_job_mcp.py
@@ -1,15 +1,19 @@
 import datahub.emitter.mce_builder as builder
+import json
+
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.emitter.rest_emitter import DatahubRestEmitter
 from datahub.metadata.com.linkedin.pegasus2avro.dataset import (
     FieldLineageDetailsClass,
-    ViewPropertiesClass
+    DatasetFieldsLineages
 )
 from datahub.metadata.schema_classes import ChangeTypeClass, DataJobInputOutputClass
 
+def datasetUrn(tbl):
+    return builder.make_dataset_urn("postgres", tbl)
+
 def fldUrn(tbl, fld):
-    datasetUrn = builder.make_dataset_urn("foo", tbl)
-    return f"urn:li:datasetField:({datasetUrn}, {fld})"
+    return f"urn:li:schemaField:({datasetUrn(tbl)}, {fld})"
 
 # Lineage of fields in a view
 # view col c1 <-- unknownFunc(foo.bar2.c9, foo.bar4.c2)
@@ -18,18 +22,17 @@ def fldUrn(tbl, fld):
 # note that the semantic of the "transform" value is contextual.
 # In above example, it is regarded as some kind of UDF; but it could also be an expression etc.
 
-viewProps = ViewPropertiesClass(False, "someLogic", "someLang", {
-        fldUrn("bar", "c1"): FieldLineageDetailsClass(parentFields=[fldUrn("bar2", "c9"), fldUrn("bar4", "c2")]),
-        fldUrn("bar", "c2"): FieldLineageDetailsClass(parentFields=[fldUrn("bar3","c5")], transform="myfunc")
-    })
+fieldsLineages = DatasetFieldsLineages({
+    fldUrn("bar", "c1"): FieldLineageDetailsClass(parentFields=[fldUrn("bar2", "c9"), fldUrn("bar4", "c2")]),
+    fldUrn("bar", "c2"): FieldLineageDetailsClass(parentFields=[fldUrn("bar3","c5")], transform="myfunc")
+})
 
-# Construct a MetadataChangeProposalWrapper object.
 viewLineageMcp = MetadataChangeProposalWrapper(
     entityType="dataset",
     changeType=ChangeTypeClass.UPSERT,
-    entityUrn=builder.make_dataset_urn("foo", "bar"),
-    aspectName="viewProperties",
-    aspect=viewProps
+    entityUrn=datasetUrn("bar"),
+    aspectName="datasetFieldsLineages",
+    aspect=fieldsLineages
 )
 
 # Lineage of fields output by a job
@@ -40,20 +43,15 @@ viewLineageMcp = MetadataChangeProposalWrapper(
 # output col bar.c2 is known to not have any parents i.e. its values are somehow created independently within this job.
 
 dataJobInputOutput = DataJobInputOutputClass(
-    inputDatasets=[
-        builder.make_dataset_urn("foo", "bar2"),
-        builder.make_dataset_urn("foo", "bar3"),
-        builder.make_dataset_urn("foo", "bar4")], 
-    outputDatasets=[
-        builder.make_dataset_urn("foo", "bar"),
-        builder.make_dataset_urn("foo", "bar2")], 
+    inputDatasets=[datasetUrn("bar2"), datasetUrn("bar3"), datasetUrn("bar4")], 
+    outputDatasets=[datasetUrn("bar"), datasetUrn("bar2")], 
     inputDatajobs=None,
     inputDatasetFields=[fldUrn("bar2", "c9"), fldUrn("bar4", "c2"),fldUrn("bar3","c5"), fldUrn("bar3","c6")],
     outputDatasetFields=[fldUrn("bar", "c1"), fldUrn("bar", "c2"),fldUrn("bar2", "c1"), fldUrn("bar2", "c2")],
     fieldsLineages={
         fldUrn("bar", "c1"): FieldLineageDetailsClass([fldUrn("bar2", "c9"), fldUrn("bar4", "c2")]),
         fldUrn("bar2", "c2"): FieldLineageDetailsClass([fldUrn("bar3","c5")], transform="myfunc"),
-        fldUrn("bar2", "c1"): FieldLineageDetailsClass([fldUrn("unknown","unknown")])
+        fldUrn("bar2", "c1"): FieldLineageDetailsClass(["urn:li:schemaField:(urn:li:dataset:(urn:li:dataPlatform:unknown,unknown,PROD), unknown)"])
     })
 
 dataJobLineageMcp = MetadataChangeProposalWrapper(
