fdaa1fadf9531224dc9f5343f566890f3be40429
Gabe Lyons <itsgabelyons@gmail.com>
2021-12-14 10:49:03 -0800

    Revert "feat(graph): Make Dgraph a proper Neo4j alternative (#3578)" (#3740)

--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this scripts checks if docker-compose$flavour.quickstart.yml is up to date for these 'flavours':
-FLAVOURS=("-with-elasticsearch" "-with-neo4j" "-with-dgraph" ".monitoring")
+FLAVOURS=("" "-without-neo4j" ".monitoring")
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
@@ -12,10 +12,8 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp-with-elasticsearch.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose-with-neo4j.yml ../docker-compose-with-neo4j.override.yml temp-with-neo4j.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose-with-dgraph.yml ../docker-compose-with-dgraph.override.yml temp-with-dgraph.quickstart.yml
-
+python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose-without-neo4j.yml ../docker-compose-without-neo4j.override.yml temp-without-neo4j.quickstart.yml
 python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.monitoring.quickstart.yml
 
 for flavour in "${FLAVOURS[@]}"
