4c1bb9bfc97a918594f7f155f1aa512c1d11fd50
Enrico Minack <github@enrico.minack.dev>
2021-12-15 10:49:17 +0100

    Fetching symlinks through http does not work, copying those files

--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # this scripts checks if docker-compose$flavour.quickstart.yml is up to date for these 'flavours':
-FLAVOURS=("-with-elasticsearch" "-with-neo4j" "-with-dgraph" ".monitoring")
+FLAVOURS=("" "-without-neo4j" "-with-neo4j" "-with-dgraph" ".monitoring")
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
@@ -12,7 +12,8 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp-with-elasticsearch.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
+python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp-without-neo4j.quickstart.yml
 python generate_docker_quickstart.py ../docker-compose-with-neo4j.yml ../docker-compose-with-neo4j.override.yml temp-with-neo4j.quickstart.yml
 python generate_docker_quickstart.py ../docker-compose-with-dgraph.yml ../docker-compose-with-dgraph.override.yml temp-with-dgraph.quickstart.yml
 
@@ -20,7 +21,8 @@ python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml
 
 for flavour in "${FLAVOURS[@]}"
 do
-  if cmp docker-compose$flavour.quickstart.yml temp$flavour.quickstart.yml; then
+  # compare actual quickstart file (while ignoring comments) with temporarily generated one
+  if cmp <(grep -v -E -e "^#" docker-compose$flavour.quickstart.yml) temp$flavour.quickstart.yml; then
     echo "docker-compose$flavour.quickstart.yml is up to date."
   else
     echo "docker-compose$flavour.quickstart.yml is out of date."
