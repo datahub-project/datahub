0a4b981170411eaf47670d3e901a0a49521e594a
MugdhaHardikar-GSLab <mugdha.hardikar@gslab.com>
2021-12-21 21:16:26 +0530

    fix(datahub-client): remove unused imports causing check fail

--- metadata-integration/java/datahub-client/src/main/java/datahub/client/RESTEmitter.java
+++ metadata-integration/java/datahub-client/src/main/java/datahub/client/RESTEmitter.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
