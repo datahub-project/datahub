410338393aea3083920a2c0311a10cedcdc88303
Kevin Hu <kevinhuwest@gmail.com>
2022-01-05 13:29:03 -0500

    Remove missing imports

--- metadata-ingestion/src/datahub/ingestion/source/data_lake.py
+++ metadata-ingestion/src/datahub/ingestion/source/data_lake.py
@@ -3,7 +3,6 @@ import logging
 import os
 from dataclasses import field as dataclass_field
 from datetime import datetime
-from enum import Enum
 from typing import Any, Dict, Iterable, List, Optional
 
 import pydantic
