db9cf06410d3217a040a0e8cb5ed32163fb97cfc
Dexter Lee <dexter@acryl.io>
2022-01-10 20:55:47 -0800

    Add git version to the UI and config endpoint

--- metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MclConsumerConfig.java
+++ metadata-jobs/mae-consumer/src/main/java/com/linkedin/metadata/kafka/MclConsumerConfig.java
@@ -1,20 +1,31 @@
 package com.linkedin.metadata.kafka;
 
+import com.linkedin.gms.factory.common.GitVersionFactory;
+import com.linkedin.metadata.version.GitVersion;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
-public class MclConsumerConfig {
-    Map<String, String> config = new HashMap<String, String>() {{
-        put("noCode", "true");
-    }};
 
-    @GetMapping("/config")
-    @ResponseBody
-    public Map<String, String> sayHello() {
-      return config;
-    }
+@Controller
+@Import(GitVersionFactory.class)
+public class MclConsumerConfig {
+  private final Map<String, String> config;
+
+  public MclConsumerConfig(GitVersion gitVersion) {
+    config = new HashMap<>();
+    config.put("noCode", "true");
+
+    config.put("version", gitVersion.getVersion());
+    config.put("commit", gitVersion.getCommitId());
+  }
+
+  @GetMapping("/config")
+  @ResponseBody
+  public Map<String, String> getConfig() {
+    return config;
+  }
 }
