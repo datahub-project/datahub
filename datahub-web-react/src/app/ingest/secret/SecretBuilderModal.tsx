afd758b1fd0e109c0389ce3cbea1974d8824e773
John Joyce <john@acryl.io>
2022-01-12 14:41:48 -0800

    Addressing PR review comments

--- datahub-web-react/src/app/ingest/secret/SecretBuilderModal.tsx
+++ datahub-web-react/src/app/ingest/secret/SecretBuilderModal.tsx
@@ -1,5 +1,5 @@
 import { Button, Form, Input, Modal, Typography } from 'antd';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { SecretBuilderState } from './types';
 
 type Props = {
--- ingestion-scheduler/src/test/java/com/datahub/metadata/ingestion/IngestionSchedulerTest.java
+++ ingestion-scheduler/src/test/java/com/datahub/metadata/ingestion/IngestionSchedulerTest.java
@@ -1,4 +1,4 @@
-package com.linkedin.metadata.kafka.hook;
+package com.datahub.metadata.ingestion;
 
 import com.datahub.authentication.Authentication;
 import com.google.common.collect.ImmutableMap;
@@ -10,33 +10,25 @@ import com.linkedin.entity.EntityResponse;
 import com.linkedin.entity.EnvelopedAspect;
 import com.linkedin.entity.EnvelopedAspectMap;
 import com.linkedin.entity.client.JavaEntityClient;
-import com.linkedin.events.metadata.ChangeType;
-import com.linkedin.gms.factory.config.ConfigurationProvider;
+import com.linkedin.metadata.config.IngestionConfiguration;
 import com.linkedin.ingestion.DataHubIngestionSourceConfig;
 import com.linkedin.ingestion.DataHubIngestionSourceInfo;
 import com.linkedin.ingestion.DataHubIngestionSourceSchedule;
 import com.linkedin.metadata.Constants;
-import com.linkedin.metadata.config.IngestionConfiguration;
-import com.linkedin.metadata.models.registry.ConfigEntityRegistry;
-import com.linkedin.metadata.models.registry.EntityRegistry;
 import com.linkedin.metadata.query.ListResult;
-import com.linkedin.metadata.utils.GenericAspectUtils;
-import com.linkedin.mxe.MetadataChangeLog;
 import java.util.Collections;
 import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import org.mockito.Mockito;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-
 import org.testng.annotations.Test;
-
-import static com.linkedin.metadata.Constants.*;
 import static org.testng.Assert.*;
 
 
-public class IngestionSchedulerHookTest {
-  private IngestionSchedulerHook _ingestionSchedulerHook;
+public class IngestionSchedulerTest {
+  private IngestionScheduler _ingestionScheduler;
 
   @BeforeMethod
   public void setupTest() throws Exception {
@@ -54,15 +46,15 @@ public class IngestionSchedulerHookTest {
     );
 
     final EnvelopedAspect envelopedAspect1 = new EnvelopedAspect();
-    envelopedAspect1.setName(INGESTION_INFO_ASPECT_NAME);
+    envelopedAspect1.setName(Constants.INGESTION_INFO_ASPECT_NAME);
     envelopedAspect1.setValue(new Aspect(info1.data()));
 
     final EnvelopedAspectMap map1 = new EnvelopedAspectMap();
-    map1.put(INGESTION_INFO_ASPECT_NAME, envelopedAspect1);
+    map1.put(Constants.INGESTION_INFO_ASPECT_NAME, envelopedAspect1);
 
     final EntityResponse entityResponse1 = Mockito.mock(EntityResponse.class);
     Mockito.when(entityResponse1.getUrn()).thenReturn(ingestionSourceUrn1);
-    Mockito.when(entityResponse1.getEntityName()).thenReturn(INGESTION_SOURCE_ENTITY_NAME);
+    Mockito.when(entityResponse1.getEntityName()).thenReturn(Constants.INGESTION_SOURCE_ENTITY_NAME);
     Mockito.when(entityResponse1.getAspects()).thenReturn(map1);
 
     final Urn ingestionSourceUrn2 = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:1");
@@ -77,19 +69,16 @@ public class IngestionSchedulerHookTest {
     );
 
     final EnvelopedAspect envelopedAspect2 = new EnvelopedAspect();
-    envelopedAspect2.setName(INGESTION_INFO_ASPECT_NAME);
+    envelopedAspect2.setName(Constants.INGESTION_INFO_ASPECT_NAME);
     envelopedAspect2.setValue(new Aspect(info2.data()));
 
     final EnvelopedAspectMap map2 = new EnvelopedAspectMap();
-    map2.put(INGESTION_INFO_ASPECT_NAME, envelopedAspect2);
+    map2.put(Constants.INGESTION_INFO_ASPECT_NAME, envelopedAspect2);
 
     final EntityResponse entityResponse2 = Mockito.mock(EntityResponse.class);
     Mockito.when(entityResponse2.getUrn()).thenReturn(ingestionSourceUrn2);
-    Mockito.when(entityResponse2.getEntityName()).thenReturn(INGESTION_SOURCE_ENTITY_NAME);
+    Mockito.when(entityResponse2.getEntityName()).thenReturn(Constants.INGESTION_SOURCE_ENTITY_NAME);
     Mockito.when(entityResponse2.getAspects()).thenReturn(map2);
-
-    EntityRegistry registry = new ConfigEntityRegistry(
-        IngestionSchedulerHookTest.class.getClassLoader().getResourceAsStream("test-entity-registry.yml"));
 
     JavaEntityClient mockClient = Mockito.mock(JavaEntityClient.class);
 
@@ -111,14 +100,10 @@ public class IngestionSchedulerHookTest {
         ingestionSourceUrn1, entityResponse1,
         ingestionSourceUrn2, entityResponse2));
 
-    ConfigurationProvider configurationProvider = new ConfigurationProvider();
-    configurationProvider.setIngestion(new IngestionConfiguration(true, "0.1.1"));
-
-    _ingestionSchedulerHook = new IngestionSchedulerHook(
-        registry,
+    _ingestionScheduler = new IngestionScheduler(
         Mockito.mock(Authentication.class),
         mockClient,
-        configurationProvider,
+        Mockito.mock(IngestionConfiguration.class),
         1,
         1200);
     Thread.sleep(2000); // Sleep so the runnable can execute. (not ideal)
@@ -126,17 +111,10 @@ public class IngestionSchedulerHookTest {
 
   @Test
   public void testInvokeUpdateExistingSchedule() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
     Urn ingestionSourceUrn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0");
-
-    Future<?> beforeFuture = _ingestionSchedulerHook._nextIngestionSourceExecutionCache.get(ingestionSourceUrn);
-
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    event.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0"));
+    Future<?> beforeFuture = _ingestionScheduler._nextIngestionSourceExecutionCache.get(ingestionSourceUrn);
 
     final DataHubIngestionSourceInfo newInfo = new DataHubIngestionSourceInfo();
     newInfo.setSchedule(new DataHubIngestionSourceSchedule().setInterval("0 1 1 * *").setTimezone("UTC")); // Run every monday
@@ -147,27 +125,21 @@ public class IngestionSchedulerHookTest {
         .setRecipe("{ type }")
         .setVersion("0.8.18")
     );
-    event.setAspect(GenericAspectUtils.serializeAspect(newInfo));
 
     // Assert that the new source has been scheduled successfully.
-    _ingestionSchedulerHook.invoke(event);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
-    Future<?> newFuture = _ingestionSchedulerHook._nextIngestionSourceExecutionCache.get(ingestionSourceUrn);
+    _ingestionScheduler.scheduleNextIngestionSourceExecution(ingestionSourceUrn, newInfo);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
+    Future<?> newFuture = _ingestionScheduler._nextIngestionSourceExecutionCache.get(ingestionSourceUrn);
 
     // Ensure that there is an overwritten future.
-    assertNotSame(beforeFuture, newFuture);
+    Assert.assertNotSame(beforeFuture, newFuture);
   }
 
   @Test
   public void testInvokeNewSchedule() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    event.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSourceUrn:2"));
-
+    final Urn urn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:2");
     final DataHubIngestionSourceInfo newInfo = new DataHubIngestionSourceInfo();
     newInfo.setSchedule(new DataHubIngestionSourceSchedule().setInterval("0 1 1 * *").setTimezone("UTC")); // Run every monday
     newInfo.setType("redshift");
@@ -177,23 +149,17 @@ public class IngestionSchedulerHookTest {
         .setRecipe("{ type }")
         .setVersion("0.8.18")
     );
-    event.setAspect(GenericAspectUtils.serializeAspect(newInfo));
 
     // Assert that the new source has been scheduled successfully.
-    _ingestionSchedulerHook.invoke(event);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 2);
+    _ingestionScheduler.scheduleNextIngestionSourceExecution(urn, newInfo);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 2);
   }
 
   @Test
   public void testInvokeInvalidSchedule() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    event.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSourceUrn:2"));
-
+    final Urn urn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:2");
     final DataHubIngestionSourceInfo newInfo = new DataHubIngestionSourceInfo();
     // Invalid schedule set.
     newInfo.setSchedule(new DataHubIngestionSourceSchedule().setInterval("NOT A SCHEDULE").setTimezone("America/Los Angeles")); // Run every monday
@@ -204,23 +170,17 @@ public class IngestionSchedulerHookTest {
         .setRecipe("{ type }")
         .setVersion("0.8.18")
     );
-    event.setAspect(GenericAspectUtils.serializeAspect(newInfo));
 
     // Assert that no changes have been made to next execution cache.
-    _ingestionSchedulerHook.invoke(event);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    _ingestionScheduler.scheduleNextIngestionSourceExecution(urn, newInfo);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
   }
 
   @Test
   public void testInvokeMissingSchedule() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    event.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0"));
-
+    final Urn urn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0");
     final DataHubIngestionSourceInfo newInfo = new DataHubIngestionSourceInfo();
     // No schedule set.
     newInfo.setType("mysql");
@@ -230,60 +190,32 @@ public class IngestionSchedulerHookTest {
         .setRecipe("{ type }")
         .setVersion("0.8.18")
     );
-    event.setAspect(GenericAspectUtils.serializeAspect(newInfo));
 
     // Assert that the schedule has been removed.
-    _ingestionSchedulerHook.invoke(event);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 0);
+    _ingestionScheduler.scheduleNextIngestionSourceExecution(urn, newInfo);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 0);
   }
 
   @Test
   public void testInvokeDelete() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
     // Attempt to delete an unscheduled urn
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.DELETE);
-    event.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSource:not-scheduled"));
-    _ingestionSchedulerHook.invoke(event);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    final Urn urn1 = Urn.createFromString("urn:li:dataHubIngestionSource:not-scheduled");
+    _ingestionScheduler.unscheduleNextIngestionSourceExecution(urn1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
-    // Attempt to delete the scheduled urn
-    MetadataChangeLog event2 = new MetadataChangeLog();
-    event2.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event2.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event2.setChangeType(ChangeType.DELETE);
-    event2.setEntityUrn(Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0"));
-    _ingestionSchedulerHook.invoke(event2);
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 0);
-  }
-
-  @Test
-  public void testInvokeWrongAspect() throws Exception {
-    int nextExecCacheSizeBefore = _ingestionSchedulerHook._nextIngestionSourceExecutionCache.size();
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setAspectName(SECRET_VALUE_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    _ingestionSchedulerHook.invoke(event);
-    int nextExecCacheSizeAfter = _ingestionSchedulerHook._nextIngestionSourceExecutionCache.size();
-
-    // Assert that nothing changed.
-    assertEquals(nextExecCacheSizeAfter, nextExecCacheSizeBefore);
+    // Attempt to delete a scheduled urn
+    final Urn urn2 = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0");
+    _ingestionScheduler.unscheduleNextIngestionSourceExecution(urn2);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 0);
   }
 
   @Test
   public void testSchedule() throws Exception {
-    assertEquals(_ingestionSchedulerHook._nextIngestionSourceExecutionCache.size(), 1);
+    assertEquals(_ingestionScheduler._nextIngestionSourceExecutionCache.size(), 1);
 
-    Urn ingestionSourceUrn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0");
-    MetadataChangeLog event = new MetadataChangeLog();
-    event.setEntityType(INGESTION_SOURCE_ENTITY_NAME);
-    event.setAspectName(INGESTION_INFO_ASPECT_NAME);
-    event.setChangeType(ChangeType.UPSERT);
-    event.setEntityUrn(ingestionSourceUrn);
-
+    final Urn urn = Urn.createFromString("urn:li:dataHubIngestionSourceUrn:0");
     final DataHubIngestionSourceInfo newInfo = new DataHubIngestionSourceInfo();
     newInfo.setSchedule(new DataHubIngestionSourceSchedule().setInterval("* * * * *").setTimezone("UTC")); // Run every monday
     newInfo.setType("redshift");
@@ -293,13 +225,10 @@ public class IngestionSchedulerHookTest {
         .setRecipe("{ type }")
         .setVersion("0.8.18")
     );
-    event.setAspect(GenericAspectUtils.serializeAspect(newInfo));
 
-    _ingestionSchedulerHook.invoke(event);
+    _ingestionScheduler.scheduleNextIngestionSourceExecution(urn, newInfo);
 
-    ScheduledFuture<?> future = _ingestionSchedulerHook._nextIngestionSourceExecutionCache.get(ingestionSourceUrn);
-    assertTrue(future.getDelay(TimeUnit.SECONDS) < 60); // Next execution must always be less than a minute away.
+    ScheduledFuture<?> future = _ingestionScheduler._nextIngestionSourceExecutionCache.get(urn);
+    Assert.assertTrue(future.getDelay(TimeUnit.SECONDS) < 60); // Next execution must always be less than a minute away.
   }
 }
-
-
