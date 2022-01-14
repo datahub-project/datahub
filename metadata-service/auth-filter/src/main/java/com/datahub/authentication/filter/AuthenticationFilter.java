2f524be73de9d2b42611861a4eef4d900210bba6
Gabe Lyons <itsgabelyons@gmail.com>
2022-01-14 11:31:36 -0800

    starting work

--- metadata-service/auth-filter/src/main/java/com/datahub/authentication/filter/AuthenticationFilter.java
+++ metadata-service/auth-filter/src/main/java/com/datahub/authentication/filter/AuthenticationFilter.java
@@ -3,6 +3,8 @@ package com.datahub.authentication.filter;
 import com.datahub.authentication.Authentication;
 import com.datahub.authentication.AuthenticationConfiguration;
 import com.datahub.authentication.AuthenticationContext;
+import com.datahub.authentication.AuthenticationException;
+import com.datahub.authentication.AuthenticationExpiredException;
 import com.datahub.authentication.Authenticator;
 import com.datahub.authentication.AuthenticatorConfiguration;
 import com.datahub.authentication.authenticator.AuthenticatorChain;
@@ -56,7 +58,19 @@ public class AuthenticationFilter implements Filter {
       FilterChain chain)
       throws IOException, ServletException {
     AuthenticatorContext context = buildAuthContext((HttpServletRequest) request);
-    Authentication authentication = this.authenticatorChain.authenticate(context);
+    Authentication authentication = null;
+    try {
+      authentication = this.authenticatorChain.authenticate(context);
+    } catch (AuthenticationExpiredException e) {
+      // For AuthenticationExpiredExceptions, terminate and provide that feedback to the user
+      log.debug("Failed to authenticate request. Received an AuthenticationExpiredException from authenticator chain.", e);
+      ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+      return;
+    } catch (AuthenticationException e) {
+      // For other AuthenticationExceptions, log & continue
+      log.debug("Failed to authenticate request. Received an AuthenticationExpiredException from authenticator chain.", e);
+    }
+
     if (authentication != null) {
       // Successfully authenticated.
       log.debug(String.format("Successfully authenticated request for Actor with type: %s, id: %s",
