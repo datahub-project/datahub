f27b743a05a13895058ae10cfed3cc856f27c8be
John Joyce <john@acryl.io>
2022-01-24 17:55:27 -0800

    Remove unused import

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/domain/DomainType.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/domain/DomainType.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
