5c5dd5bfee3cf81a84c768f9f652296aaf166d92
RyanHolstien <RyanHolstien@users.noreply.github.com>
2022-01-25 21:03:31 -0600

    Feature/dynamic platform icons (#3968)

--- datahub-web-react/src/app/entity/chart/preview/ChartPreview.tsx
+++ datahub-web-react/src/app/entity/chart/preview/ChartPreview.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AccessLevel, EntityType, GlobalTags, GlossaryTerms, Owner, SearchInsight } from '../../../../types.generated';
 import DefaultPreviewCard from '../../../preview/DefaultPreviewCard';
 import { useEntityRegistry } from '../../../useEntityRegistry';
-import { getLogoFromPlatform } from '../../../shared/getLogoFromPlatform';
 import { capitalizeFirstLetter } from '../../../shared/capitalizeFirstLetter';
 
 export const ChartPreview = ({
@@ -15,6 +14,7 @@ export const ChartPreview = ({
     tags,
     glossaryTerms,
     insights,
+    logoUrl,
 }: {
     urn: string;
     platform: string;
@@ -25,6 +25,7 @@ export const ChartPreview = ({
     tags?: GlobalTags;
     glossaryTerms?: GlossaryTerms | null;
     insights?: Array<SearchInsight> | null;
+    logoUrl?: string | null;
 }): JSX.Element => {
     const entityRegistry = useEntityRegistry();
     const capitalizedPlatform = capitalizeFirstLetter(platform);
@@ -35,7 +36,7 @@ export const ChartPreview = ({
             name={name || ''}
             description={description || ''}
             type="Chart"
-            logoUrl={getLogoFromPlatform(platform) || ''}
+            logoUrl={logoUrl || ''}
             platform={capitalizedPlatform}
             qualifier={access}
             tags={tags}
