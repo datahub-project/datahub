d7222193c86a3477836f143623c9dcfd68f903c8
xiphl <xipherl9@gmail.com>
2022-01-28 23:00:35 +0700

    disable trino by impossible constraints

--- metadata-ingestion/tests/integration/trino/test_trino.py
+++ metadata-ingestion/tests/integration/trino/test_trino.py
@@ -13,8 +13,8 @@ FROZEN_TIME = "2021-09-23 12:00:00"
 
 
 @freeze_time(FROZEN_TIME)
-@pytest.mark.skipif(sys.version_info < (3, 7), reason="trino requires Python 3.7+")
-@pytest.mark.slow_integration
+@pytest.mark.skipif(sys.version_info < (3, 7) and sys.version_info > (3.9), reason="trino requires Python 3.7+ and less than 3.9")
+@pytest.mark.integration
 def test_trino_ingest(docker_compose_runner, pytestconfig, tmp_path, mock_time):
     test_resources_dir = pytestconfig.rootpath / "tests/integration/trino"
 
