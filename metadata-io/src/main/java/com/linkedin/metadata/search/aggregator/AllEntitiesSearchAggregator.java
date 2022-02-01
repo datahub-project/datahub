a4c3718d2bc5ff82d653497804618149417c9c74
John Joyce <john@acryl.io>
2022-01-31 18:23:48 -0800

    Updating filter ranking

--- metadata-io/src/main/java/com/linkedin/metadata/search/aggregator/AllEntitiesSearchAggregator.java
+++ metadata-io/src/main/java/com/linkedin/metadata/search/aggregator/AllEntitiesSearchAggregator.java
@@ -48,7 +48,15 @@ public class AllEntitiesSearchAggregator {
 
   private final EntitySearchServiceCache _entitySearchServiceCache;
 
-  private static final List<String> FILTER_RANKING = ImmutableList.of("entity", "typeNames", "platform", "domain", "tags", "glossaryTerms", "container", "origin");
+  private static final List<String> FILTER_RANKING = ImmutableList.of(
+      "entity",
+      "typeNames",
+      "platform",
+      "domain",
+      "tags",
+      "glossaryTerms",
+      "container",
+      "origin");
 
   public AllEntitiesSearchAggregator(EntityRegistry entityRegistry, EntitySearchService entitySearchService,
       SearchRanker searchRanker, CacheManager cacheManager, int batchSize) {
