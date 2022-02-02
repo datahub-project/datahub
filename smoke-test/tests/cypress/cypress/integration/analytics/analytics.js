f61012c0e67b025d9791f39c36221ca06097506b
Aseem Bansal <asmbansal2@gmail.com>
2022-02-02 07:40:26 +0530

    fix cypress test

--- smoke-test/tests/cypress/cypress/integration/analytics/analytics.js
+++ smoke-test/tests/cypress/cypress/integration/analytics/analytics.js
@@ -1,19 +1,14 @@
 describe('analytics', () => {
-  it('can go to a dataset and see analytics', () => {
+  it('can go to a dataset and see analytics in Section Views', () => {
     cy.login();
 
     cy.visit("/analytics");
     cy.contains("documentation").should('not.exist');
-    cy.contains("dashboards").should('not.exist');
-
-    cy.visit('/dataset/urn:li:dataset:(urn:li:dataPlatform:hive,cypress_logging_events,PROD)');
-    cy.get("#rc-tabs-0-tab-Documentation").click();
 
     cy.visit("/chart/urn:li:chart:(looker,baz1)");
     cy.get("#rc-tabs-0-panel-Dashboards").click({ force: true });
 
     cy.visit("/analytics");
     cy.contains("documentation");
-    cy.contains("dashboards");
   });
 })
