c8922b39d62f232f9915094c5e1e9ea31b6738fe
John Joyce <john@acryl.io>
2022-02-04 10:18:53 -0800

    feat(container): Add domains aspect to container.  (#4059)

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/container/ContainerType.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/container/ContainerType.java
@@ -32,7 +32,8 @@ public class ContainerType implements com.linkedin.datahub.graphql.types.EntityT
       Constants.SUB_TYPES_ASPECT_NAME,
       Constants.GLOBAL_TAGS_ASPECT_NAME,
       Constants.GLOSSARY_TERMS_ASPECT_NAME,
-      Constants.CONTAINER_ASPECT_NAME
+      Constants.CONTAINER_ASPECT_NAME,
+      Constants.DOMAINS_ASPECT_NAME
   );
   private final EntityClient _entityClient;
 
