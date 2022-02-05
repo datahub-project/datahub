61db4ed1526542d74b1019dd3f9c8215309ae081
Aditya Radhakrishnan <aditya.radhakrish@gmail.com>
2022-02-04 23:24:20 -0800

    refactor(ingest) - remove snowflake_common dependency on aws_common (#4054)
    Co-authored-by: Shirshanka Das <shirshanka@apache.org>

--- metadata-ingestion/src/datahub/ingestion/source/aws/glue.py
+++ metadata-ingestion/src/datahub/ingestion/source/aws/glue.py
@@ -15,7 +15,8 @@ from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.api.workunit import MetadataWorkUnit
-from datahub.ingestion.source.aws.aws_common import AwsSourceConfig, make_s3_urn
+from datahub.ingestion.source.aws.aws_common import AwsSourceConfig
+from datahub.ingestion.source.aws.s3_util import make_s3_urn
 from datahub.metadata.com.linkedin.pegasus2avro.common import Status
 from datahub.metadata.com.linkedin.pegasus2avro.metadata.snapshot import DatasetSnapshot
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
