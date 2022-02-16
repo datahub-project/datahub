e6c168e87c7213ab7416c74ea6cf0b2d2508d09c
MohdSiddiqueBagwan <mohdsiddique.bagwan@gslab.com>
2022-02-16 11:51:41 +0530

    Fix the lint issue

--- metadata-ingestion/tests/integration/powerbi/test_powerbi.py
+++ metadata-ingestion/tests/integration/powerbi/test_powerbi.py
@@ -112,7 +112,6 @@ def test_powerbi_ingest(pytestconfig, tmp_path, mock_time):
                     "type": "file",
                     "config": {
                         "filename": f"{tmp_path}/powerbi_mces.json",
-                        #"filename": "/tmp/powerbi_mces5.json",
                     },
                 },
             }
