c77ecc24519df077fc6190fca989511d5ea45a4e
MugdhaHardikar-GSLab <mugdha.hardikar@gslab.com>
2022-02-16 17:01:15 +0530

    Update smoke.sh

--- smoke-test/spark-smoke-test/smoke.sh
+++ smoke-test/spark-smoke-test/smoke.sh
@@ -27,31 +27,11 @@ datahub docker quickstart \
 	--quickstart-compose-file ../../docker/docker-compose.dev.yml \
 	--dump-logs-on-failure
 
-
-#datahub docker quickstart
-
 echo "--------------------------------------------------------------------"
-echo "Building spark images"
+echo "Setup environment for pytest"
 echo "--------------------------------------------------------------------"
 
-cd docker
-
-#build spark cluster images
-./build_images.sh 
-
-echo "--------------------------------------------------------------------"
-echo "Bringing up spark cluster"
-echo "--------------------------------------------------------------------"
-
-#bring up spark cluster
-docker-compose -f spark-docker-compose.yml up -d
-
-echo "--------------------------------------------------------------------"
-echo "Executing spark-submit jobs"
-echo "--------------------------------------------------------------------"
-
-#Execute spark-submit jobs
-docker run --network datahub_network spark-submit
+./setup_spark_smoke_test.sh
 
 echo "--------------------------------------------------------------------"
 echo "Starting pytest"
