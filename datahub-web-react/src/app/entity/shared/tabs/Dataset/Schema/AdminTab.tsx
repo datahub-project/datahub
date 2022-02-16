6f3ab99bca8a4689a88438024b63a3da1993dc68
neojunjie87 <neojunjie87@gmail.com>
2022-02-16 15:47:39 +0800

    create function checkownership in whoami.tsx for reuse and remove duplicate codes from test suite and edit tabs

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/AdminTab.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/AdminTab.tsx
@@ -1,21 +1,14 @@
-// import { Empty } from 'antd';
 import { Divider } from 'antd';
 import React from 'react';
 import { GetDatasetQuery } from '../../../../../../graphql/dataset.generated';
-import { FindWhoAmI } from '../../../../dataset/whoAmI';
+import { checkOwnerShip } from '../../../../dataset/whoAmI';
 import { useBaseEntity } from '../../../EntityContext';
 import { EditBrowsePathTable } from '../BrowsePath/EditBrowsePathTable';
 import { DeleteSchemaTabv2 } from '../Delete/DeleteSchemaTabv2';
 
 export const AdminTab = () => {
-    const queryBase = useBaseEntity<GetDatasetQuery>()?.dataset?.ownership?.owners;
-    const currUser = FindWhoAmI();
-    const ownersArray =
-        queryBase?.map((x) =>
-            x?.type === 'DATAOWNER' && x?.owner?.__typename === 'CorpUser' ? x?.owner?.username : '',
-        ) || [];
-    // console.log(`ownersArray is ${ownersArray} and I am ${currUser}`);
-    if (ownersArray.includes(currUser)) {
+    const dataset = useBaseEntity<GetDatasetQuery>();
+    if (checkOwnerShip(dataset)) {
         return (
             <>
                 <DeleteSchemaTabv2 />
--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditPropertiesTab.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/EditPropertiesTab.tsx
@@ -1,20 +1,12 @@
-// import { Empty } from 'antd';
 import React from 'react';
 import { GetDatasetQuery } from '../../../../../../graphql/dataset.generated';
-// import { useGetAuthenticatedUser } from '../../../../../useGetAuthenticatedUser';
-import { FindWhoAmI } from '../../../../dataset/whoAmI';
+import { checkOwnerShip } from '../../../../dataset/whoAmI';
 import { useBaseEntity } from '../../../EntityContext';
 import { EditPropertiesTableEditable } from './EditPropertiesTableEditable';
 
 export const EditPropertiesTab = () => {
-    const queryBase = useBaseEntity<GetDatasetQuery>()?.dataset?.ownership?.owners;
-    const currUser = FindWhoAmI();
-    const ownersArray =
-        queryBase?.map((x) =>
-            x?.type === 'DATAOWNER' && x?.owner?.__typename === 'CorpUser' ? x?.owner?.username : '',
-        ) || [];
-    // console.log(`ownersArray is ${ownersArray} and I am ${currUser}`);
-    if (ownersArray.includes(currUser)) {
+    const dataset = useBaseEntity<GetDatasetQuery>();
+    if (checkOwnerShip(dataset)) {
         return (
             <>
                 <EditPropertiesTableEditable />
