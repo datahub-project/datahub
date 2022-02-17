ca7b05d227f95ace91a669a9a919f32a68587a33
Claudio Benfatto <claudio.benfatto@schibsted.com>
2022-02-17 13:19:25 +0100

    test: add tests for kafka schema warnings

--- metadata-ingestion/tests/unit/test_kafka_source.py
+++ metadata-ingestion/tests/unit/test_kafka_source.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import MagicMock, patch
 
 import pytest
+from confluent_kafka.schema_registry.error import SchemaRegistryError
 from confluent_kafka.schema_registry.schema_registry_client import (
     RegisteredSchema,
     Schema,
@@ -18,7 +19,7 @@ from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
 from datahub.metadata.schema_classes import BrowsePathsClass, DataPlatformInstanceClass
 
 
-class KafkaSourceTest(unittest.TestCase):
+class TestKafkaSource(object):
     def test_get_schema_str_replace_confluent_ref_avro(self):
 
         # References external schema 'TestTopic1' in the definition of 'my_field1' field.
@@ -127,6 +128,109 @@ class KafkaSourceTest(unittest.TestCase):
         )
         kafka_source.close()
         assert mock_kafka.call_count == 1
+
+    @pytest.mark.parametrize(
+        "ignore_warnings_on_schema_type",
+        [
+            pytest.param(
+                False,
+                id="ignore_warnings_on_schema_type-FALSE",
+            ),
+            pytest.param(
+                True,
+                id="ignore_warnings_on_schema_type-TRUE",
+            ),
+        ],
+    )
+    @patch("datahub.ingestion.source.kafka.confluent_kafka.Consumer", autospec=True)
+    def test_kafka_unsupported_schema_type(
+        self, mock_kafka, ignore_warnings_on_schema_type
+    ):
+        mock_kafka_instance = mock_kafka.return_value
+        mock_cluster_metadata = MagicMock()
+        mock_cluster_metadata.topics = ["foobar"]
+        mock_kafka_instance.list_topics.return_value = mock_cluster_metadata
+
+        ctx = PipelineContext(run_id="test")
+        kafka_source = KafkaSource.create(
+            {
+                "connection": {"bootstrap": "localhost:9092"},
+                "ignore_warnings_on_schema_type": f"{ignore_warnings_on_schema_type}",
+            },
+            ctx,
+        )
+        with patch.object(
+            kafka_source.schema_registry_client,
+            "get_latest_version",
+        ) as mock_get_latest_version:
+            mock_get_latest_version.return_value = RegisteredSchema(
+                schema_id="schema_id_1",
+                schema=Schema(schema_str="{}", schema_type="JSON"),
+                subject="foobar-value",
+                version=1,
+            )
+            # we use list() to force the generation of the workunits
+            list(kafka_source.get_workunits())
+            if ignore_warnings_on_schema_type:
+                assert not kafka_source.report.warnings
+            else:
+                assert kafka_source.report.warnings
+
+    @pytest.mark.parametrize(
+        "ignore_warnings_on_missing_schema,error,expect_warnings",
+        [
+            pytest.param(
+                False,
+                SchemaRegistryError(404, 40401, "Subject not found."),
+                True,
+                id="SUBJECT_NOT_FOUND-DO_NOT_IGNORE_WARNINGS",
+            ),
+            pytest.param(
+                True,
+                SchemaRegistryError(404, 40401, "Subject not found."),
+                False,
+                id="SUBJECT_NOT_FOUND-IGNORE_WARNINGS",
+            ),
+            pytest.param(
+                True,
+                SchemaRegistryError(400, -1, "Unknown error."),
+                True,
+                id="UNKNOWN_SR_ERROR-IGNORE_WARNINGS",
+            ),
+            pytest.param(
+                True,
+                Exception("Unknown exception."),
+                True,
+                id="UNKNOWN_EXCEPTION-IGNORE_WARNINGS",
+            ),
+        ],
+    )
+    @patch("datahub.ingestion.source.kafka.confluent_kafka.Consumer", autospec=True)
+    def test_kafka_warnings_on_missing_schema(
+        self, mock_kafka, ignore_warnings_on_missing_schema, error, expect_warnings
+    ):
+        mock_kafka_instance = mock_kafka.return_value
+        mock_cluster_metadata = MagicMock()
+        mock_cluster_metadata.topics = ["foobar"]
+        mock_kafka_instance.list_topics.return_value = mock_cluster_metadata
+
+        ctx = PipelineContext(run_id="test")
+        kafka_source = KafkaSource.create(
+            {
+                "connection": {"bootstrap": "localhost:9092"},
+                "ignore_warnings_on_missing_schema": f"{ignore_warnings_on_missing_schema}",
+            },
+            ctx,
+        )
+
+        with patch.object(
+            kafka_source.schema_registry_client,
+            "get_latest_version",
+        ) as mock_get_latest_version:
+            mock_get_latest_version.side_effect = [error]
+            # we use list() to force the generation of the workunits
+            list(kafka_source.get_workunits())
+            assert (len(kafka_source.report.warnings) > 0) == expect_warnings
 
     @patch("datahub.ingestion.source.kafka.confluent_kafka.Consumer", autospec=True)
     def test_kafka_source_workunits_wildcard_topic(self, mock_kafka):
