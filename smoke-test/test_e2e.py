d0b4a27ebbf6473874e8818a379d4549ff930713
John Joyce <john@acryl.io>
2022-02-17 21:56:17 -0800

    Update urn fix

--- smoke-test/test_e2e.py
+++ smoke-test/test_e2e.py
@@ -1083,7 +1083,7 @@ def test_update_corp_group_properties(frontend_session):
     # Update Corp Group Description
     json = {
         "query": """mutation updateCorpGroupProperties($urn: String!, $input: CorpGroupUpdateInput!) {\n
-            updateCorpGroupProperties(urn: $urn, input: $input) }""",
+            updateCorpGroupProperties(urn: $urn, input: $input) { urn } }""",
         "variables": {
           "urn": group_urn,
           "input": {
