27276869e52e724ea8024be9052dea04af110164
Ravindra Lanka <rlanka@acryl.io>
2022-02-24 14:21:07 -0800

    Move differes from timeline/ebean/differ to timeline/differ.

--- metadata-io/src/main/java/com/linkedin/metadata/timeline/differ/BasicDiffer.java
+++ metadata-io/src/main/java/com/linkedin/metadata/timeline/differ/BasicDiffer.java
@@ -1,4 +1,4 @@
-package com.linkedin.metadata.timeline.ebean.differ;
+package com.linkedin.metadata.timeline.differ;
 
 import com.github.fge.jsonpatch.JsonPatch;
 import com.linkedin.metadata.entity.ebean.EbeanAspectV2;
