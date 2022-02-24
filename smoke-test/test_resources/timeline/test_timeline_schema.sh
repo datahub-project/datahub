03f27fb4a6c801fccab278e9e96ae74300546fa6
Ryan Holstien <ryan@acryl.io>
2022-02-23 19:32:32 -0600

    modify scripts to hit bug

--- smoke-test/test_resources/timeline/test_timeline_schema.sh
+++ smoke-test/test_resources/timeline/test_timeline_schema.sh
@@ -5,7 +5,4 @@ datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,
 datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" -a schemaMetadata -d newschemav2.json
 datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" -a schemaMetadata -d newschemav3.json
 datahub timeline --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" --category technical_schema --start 10daysago --end 2682397800000 $1
-datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" -a ownership -d ../smoke-test/test_resources/ownership_v1.json
-datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" -a ownership -d ../smoke-test/test_resources/ownership_v2.json
-datahub put --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" -a ownership -d ../smoke-test/test_resources/ownership_v3.json
-datahub timeline --urn "urn:li:dataset:(urn:li:dataPlatform:hive,testTimelineDataset,PROD)" --category ownership --start 10daysago --end 2682397800000 $1
+
