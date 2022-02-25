3a0fe44344be5607fb10394f461caa3000e2354f
HuYueeer <40758588+Huyueeer@users.noreply.github.com>
2022-02-25 09:11:43 +0800

    feat(search): supporting chinese glossaryterm full text retrieval(#3914) (#3956)
    * feat(search): supporting chinese glossaryterm full text retrieval(#3914)
    
    * refactor(search): modify mainTokenizer to appropriate position(#3914)
    
    Co-authored-by: Shirshanka Das <shirshanka@apache.org>

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/SettingsBuilderFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/SettingsBuilderFactory.java
@@ -2,21 +2,28 @@ package com.linkedin.gms.factory.search;
 
 import com.google.common.collect.ImmutableList;
 import com.linkedin.gms.factory.entityregistry.EntityRegistryFactory;
+import com.linkedin.gms.factory.spring.YamlPropertySourceFactory;
 import com.linkedin.metadata.models.registry.EntityRegistry;
 import com.linkedin.metadata.search.elasticsearch.indexbuilder.SettingsBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 
 
 @Configuration
 @Import(EntityRegistryFactory.class)
+@PropertySource(value = "classpath:/application.yml", factory = YamlPropertySourceFactory.class)
 public class SettingsBuilderFactory {
   @Autowired
   @Qualifier("entityRegistry")
   private EntityRegistry entityRegistry;
+
+  @Value("${elasticsearch.index.mainTokenizer}")
+  private String mainTokenizer;
 
   @Bean("settingsBuilder")
   protected SettingsBuilder getInstance() {
@@ -24,6 +31,6 @@ public class SettingsBuilderFactory {
     ImmutableList.Builder<String> stopWords = ImmutableList.<String>builder().add("urn").add("li");
     // Add all entity names to stop word list
     entityRegistry.getEntitySpecs().values().forEach(entitySpec -> stopWords.add(entitySpec.getName().toLowerCase()));
-    return new SettingsBuilder(stopWords.build());
+    return new SettingsBuilder(stopWords.build(), mainTokenizer);
   }
 }
