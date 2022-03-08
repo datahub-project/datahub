b1eae57a3dbb5cbe07fcb6a02299e554de0f7731
Mayuri N <mayuri.nehate@gslab.com>
2022-03-08 20:22:08 +0530

    fix(GE): fix dependencies for GE DatahubValidationAction, logic for sqlalchemy url for snowflake

--- metadata-ingestion/src/datahub/integrations/great_expectations/action.py
+++ metadata-ingestion/src/datahub/integrations/great_expectations/action.py
@@ -25,6 +25,7 @@ from great_expectations.execution_engine.sqlalchemy_execution_engine import (
     SqlAlchemyExecutionEngine,
 )
 from great_expectations.validator.validator import Validator
+from sqlalchemy.engine.base import Connection, Engine
 from sqlalchemy.engine.url import make_url
 
 import datahub.emitter.mce_builder as builder
@@ -550,13 +551,20 @@ class DataHubValidationAction(ValidationAction):
                     data_asset.active_batch_definition.datasource_name
                 ),
             }
+            sqlalchemy_uri = None
+            if isinstance(data_asset.execution_engine.engine, Engine):
+                sqlalchemy_uri = data_asset.execution_engine.engine.url
+            # For snowflake sqlalchemy_execution_engine.engine is actually instance of Connection
+            elif isinstance(data_asset.execution_engine.engine, Connection):
+                sqlalchemy_uri = data_asset.execution_engine.engine.engine.url
+
             if isinstance(ge_batch_spec, SqlAlchemyDatasourceBatchSpec):
                 # e.g. ConfiguredAssetSqlDataConnector with splitter_method or sampling_method
                 schema_name = ge_batch_spec.get("schema_name")
                 table_name = ge_batch_spec.get("table_name")
 
                 dataset_urn = make_dataset_urn_from_sqlalchemy_uri(
-                    data_asset.execution_engine.engine.url,
+                    sqlalchemy_uri,
                     schema_name,
                     table_name,
                     self.env,
@@ -609,7 +617,7 @@ class DataHubValidationAction(ValidationAction):
                     )
                 for table in tables:
                     dataset_urn = make_dataset_urn_from_sqlalchemy_uri(
-                        data_asset.execution_engine.engine.url,
+                        sqlalchemy_uri,
                         None,
                         table,
                         self.env,
@@ -640,7 +648,7 @@ class DataHubValidationAction(ValidationAction):
     def get_platform_instance(self, datasource_name):
         if self.platform_instance_map and datasource_name in self.platform_instance_map:
             return self.platform_instance_map[datasource_name]
-        if datasource_name:
+        if self.platform_instance_map:
             warn(
                 f"Datasource {datasource_name} is not present in platform_instance_map"
             )
@@ -705,6 +713,7 @@ def make_dataset_urn_from_sqlalchemy_uri(
         platform_instance=platform_instance,
         env=env,
     )
+
     return dataset_urn
 
 
