7eec30b2ec57dbd174ddabb3e4217c010d90b3f3
Aseem Bansal <asmbansal2@gmail.com>
2022-03-09 01:27:26 +0530

    fix(hive): clean protocol for hive source (#4330)

--- metadata-ingestion/src/datahub/utilities/config_clean.py
+++ metadata-ingestion/src/datahub/utilities/config_clean.py
@@ -1,4 +1,16 @@
+import re
+
+
 def remove_trailing_slashes(url: str) -> str:
     while url.endswith("/"):
         url = url[:-1]
     return url
+
+
+def remove_protocol(url: str) -> str:
+    pattern = re.compile(r"https?://")
+
+    return pattern.sub(
+        "",
+        url,
+    )
