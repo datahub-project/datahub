899c96c7531129937cc40a2a1e97bccdefb163ae
John Joyce <john@acryl.io>
2022-03-09 20:06:02 -0800

    fix(ui): Supporting unknown data platform type (#4361)

--- metadata-utils/src/main/java/com/linkedin/metadata/Constants.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/Constants.java
@@ -11,6 +11,7 @@ public class Constants {
   public static final String SYSTEM_ACTOR = "urn:li:corpuser:__datahub_system"; // DataHub internal service principal.
   public static final String UNKNOWN_ACTOR = "urn:li:corpuser:UNKNOWN"; // Unknown principal.
   public static final Long ASPECT_LATEST_VERSION = 0L;
+  public static final String UNKNOWN_DATA_PLATFORM = "urn:li:dataPlatform:unknown";
 
   /**
    * Entities
