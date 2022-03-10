9801cdfb5f1f98c3fe0ba1a9336c5464bad1749e
treff7es <treff7es@gmail.com>
2022-03-10 15:15:36 +0100

    Fixing container id tests

--- metadata-ingestion/tests/unit/test_mcp_builder.py
+++ metadata-ingestion/tests/unit/test_mcp_builder.py
@@ -17,28 +17,19 @@ def test_guid_generator_with_empty_instance():
         schema="Test",
         platform="mysql",
         instance=None,
-        environment="PROD",
     )
 
     guid = key.guid()
-    keys = key.dict()
-    # If instance is not set we should use environment as instance for backward compability
-    assert "PROD" == keys.get("instance")
-    assert guid == "f5268c71373b9100d50c1299861cfb3f"
+    assert guid == "693ed953c7192bcf46f8b9db36d71c2b"
 
 
-def test_guid_generator_with_instance_and_env():
+def test_guid_generator_with_instance():
     key = builder.SchemaKey(
         database="test",
         schema="Test",
         platform="mysql",
         instance="TestInstance",
-        environment="PROD",
     )
-    keys = key.dict()
-    # If instance is set we should not have environment property
-    assert not keys.get("environment")
-
     guid = key.guid()
     assert guid == "f096b3799fc86a3e5d5d0c083eb1f2a4"
 
