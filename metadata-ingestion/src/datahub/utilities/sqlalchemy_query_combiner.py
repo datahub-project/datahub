950909be5c4336cda5a267fbf2d0660566ce6233
Wagner Steuer Costa <wagner.steuercosta@outlook.com>
2022-03-12 17:17:58 +0100

    Added try block to handly queries of type str
    Previous code had an early return in case that query did not contain attribute columns. With the new assertion, there is no fallback in the new get_query_columns() function. Hence, some queries fail without a functional fallback. Current work around is to disable the query combiner via: profiling.query_combiner_enabled.

--- metadata-ingestion/src/datahub/utilities/sqlalchemy_query_combiner.py
+++ metadata-ingestion/src/datahub/utilities/sqlalchemy_query_combiner.py
@@ -213,7 +213,16 @@ class SQLAlchemyQueryCombiner:
 
         # Figure out how many columns this query returns.
         # This also implicitly ensures that the typing is generally correct.
-        assert len(get_query_columns(query)) > 0
+        try:
+            assert len(get_query_columns(query)) > 0
+        except Exception as e:
+            logger.exception(
+                f"Failed to assert count of columns returned by query: {str(query)}"
+            )
+            logger.debug(
+                f"Query of type: '{type(query)}' does not contain attributes required by 'get_query_columns()'."
+            )
+            return False, None
 
         # Add query to the queue.
         queue = self._get_queue(main_greenlet)
