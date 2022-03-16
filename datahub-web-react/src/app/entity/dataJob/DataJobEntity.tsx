1ab3ad3986e00108a1fc30efb59e62c911b48346
Gabe Lyons <itsgabelyons@gmail.com>
2022-03-16 14:19:10 -0700

    feat(gql): make gql layer resistant to unresolvable relationships (#4424)
    * query for custom properties on containers
    
    * dont break gql if fine grained lineage is present

--- datahub-web-react/src/app/entity/dataJob/DataJobEntity.tsx
+++ datahub-web-react/src/app/entity/dataJob/DataJobEntity.tsx
@@ -16,7 +16,6 @@ import { DataJobFlowTab } from '../shared/tabs/Entity/DataJobFlowTab';
 import { getDataForEntityType } from '../shared/containers/profile/utils';
 import { capitalizeFirstLetter } from '../../shared/textUtil';
 import { SidebarDomainSection } from '../shared/containers/profile/sidebar/Domain/SidebarDomainSection';
-import { EntityAndType } from '../../lineage/types';
 
 /**
  * Definition of the DataHub DataJob entity.
@@ -178,14 +177,6 @@ export class DataJobEntity implements Entity<DataJob> {
             name: entity?.properties?.name || '',
             type: EntityType.DataJob,
             icon: entity?.dataFlow?.platform?.properties?.logoUrl || '',
-            // eslint-disable-next-line @typescript-eslint/dot-notation
-            downstreamChildren: entity?.['downstream']?.relationships?.map(
-                (relationship) => ({ entity: relationship.entity, type: relationship.entity.type } as EntityAndType),
-            ),
-            // eslint-disable-next-line @typescript-eslint/dot-notation
-            upstreamChildren: entity?.['upstream']?.relationships?.map(
-                (relationship) => ({ entity: relationship.entity, type: relationship.entity.type } as EntityAndType),
-            ),
             platform: entity?.dataFlow?.orchestrator || '',
         };
     };
