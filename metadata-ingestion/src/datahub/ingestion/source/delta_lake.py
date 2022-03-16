cade22d715368e92aec43b25b67dc70a486b9aa3
buggythepirate <bugmenotspamalot@yahoo.com>
2022-03-16 14:16:34 +0100

    first draft of interface call

---
+++ metadata-ingestion/src/datahub/ingestion/source/delta_lake.py
@@ -0,0 +1,114 @@
+from re import A
+from attr import frozen
+from dataclasses import dataclass, field
+from typing import Sequence, Optional, Iterable, Union
+import pydantic
+
+from delta_sharing.delta_sharing import SharingClient
+from delta_sharing.protocol import Table
+from delta_sharing.rest_client import QueryTableMetadataResponse
+
+from datahub.configuration.common import AllowDenyPattern, ConfigModel
+from datahub.configuration.common import ConfigModel
+from datahub.ingestion.api.source import Source, SourceReport
+from datahub.ingestion.api.workunit import MetadataWorkUnit, UsageStatsWorkUnit
+from datahub.metadata.com.linkedin.pegasus2avro.mxe import (
+    MetadataChangeEvent,
+    MetadataChangeProposal,
+)
+
+@dataclass(frozen=True)
+class QueryTableMetadataResponse_extended(QueryTableMetadataResponse):
+    table: Table
+
+
+class SharingClient_extended(SharingClient):
+    """
+    An extension of the delta sharing class SharingClient in order to query metadata including the table origin.
+    This is done in order to ingest more easily.
+    """
+
+    def query_table_metadata(self, table: Table) -> QueryTableMetadataResponse_extended:
+        """
+        List all metadata for a specified table in a Delta Sharing Server.
+        :return: all metadata in a specified table.
+        """
+
+        response = self._rest_client.query_table_metadata(table=table)
+
+        return QueryTableMetadataResponse_extended(protocol=response.protocol, metadata=response.metadata, table=table)
+
+    def query_all_table_metadata(self) -> Sequence[QueryTableMetadataResponse_extended]:
+        """
+        List all metadata in all tables that can be accessed by you in a Delta Sharing Server.
+        :return: all metadata that can be accessed.
+        """
+        tables = self.list_all_tables()
+        querytablesmetadata = [self.query_table_metadata(table=table) for table in tables]
+
+        return querytablesmetadata
+
+class DeltaLakeSourceConfig(ConfigModel):
+
+    url: str
+    token: str
+    shareCredentialsVersion: str ="1"
+    
+    schema_patterns: AllowDenyPattern = AllowDenyPattern.allow_all()
+    table_patterns: AllowDenyPattern = AllowDenyPattern.allow_all()
+
+
+@dataclass
+class GenericFileSource(Source):
+    config: DeltaLakeSourceConfig
+    report: SourceReport = field(default_factory=SourceReport)
+
+    # @classmethod
+    # def create(cls, config_dict, ctx):
+    #     config = DeltaLakeSourceConfig.parse_obj(config_dict)
+    #     return cls(ctx, config)
+
+    # def get_workunits(self) -> Iterable[Union[MetadataWorkUnit, UsageStatsWorkUnit]]:
+    #     for i, obj in enumerate(iterate_generic_file(self.config.filename)):
+    #         wu: Union[MetadataWorkUnit, UsageStatsWorkUnit]
+    #         if isinstance(obj, UsageAggregationClass):
+    #             wu = UsageStatsWorkUnit(f"file://{self.config.filename}:{i}", obj)
+    #         elif isinstance(obj, MetadataChangeProposal):
+    #             wu = MetadataWorkUnit(f"file://{self.config.filename}:{i}", mcp_raw=obj)
+    #         else:
+    #             wu = MetadataWorkUnit(f"file://{self.config.filename}:{i}", mce=obj)
+    #         self.report.report_workunit(wu)
+    #         yield wu
+
+    # def get_report(self):
+    #     return self.report
+
+    # def close(self):
+    #     pass
+
+
+if __name__ == "__main__":
+
+    import delta_sharing
+
+    #TODO: get key from recipe
+
+    # Get the access keys for delta-sharing & start the client
+    profile = delta_sharing.protocol.DeltaSharingProfile(share_credentials_version=1, endpoint="https://sharing.delta.io/delta-sharing/", bearer_token="faaie590d541265bcab1f2de9813274bf233")
+    client = SharingClient_extended(profile)
+
+    # get all shared metadata
+    metadata_list=client.query_all_table_metadata()
+
+    #Filter tables and schemas
+    filter_table=AllowDenyPattern(allow=["COVID_19_NYT"], deny=["LA"])
+    metadata_list=[metadata for metadata in metadata_list if filter_table.allowed(metadata.table.name)]
+
+    filter_schema=AllowDenyPattern(allow=["default"], deny=["LA"])
+    metadata_list=[metadata for metadata in metadata_list if filter_schema.allowed(metadata.table.schema)]
+
+    #TODO: transform to datahub dataset entity
+
+    #TODO: publish data
+
+
---
+++ metadata-ingestion/tests/integration/deltalake/test_deltalake.py
@@ -0,0 +1,24 @@
+import pytest
+from freezegun import freeze_time
+
+from tests.test_helpers import mce_helpers
+from tests.test_helpers.click_helpers import run_datahub_cmd
+
+FROZEN_TIME = "2020-04-14 07:00:00"
+
+
+@freeze_time(FROZEN_TIME)
+@pytest.mark.integration
+def test_deltalake_ingest(pytestconfig, tmp_path):
+    test_resources_dir = pytestconfig.rootpath / "tests/integration/deltalake"
+
+    # Run the metadata ingestion pipeline.
+    config_file = (test_resources_dir / "deltalake_to_file.yml").resolve()
+    run_datahub_cmd(["ingest", "-c", f"{config_file}"], tmp_path=tmp_path)
+
+    # Verify the output.
+    mce_helpers.check_golden_file(
+        pytestconfig,
+        output_path="/tmp/deltalake_mces.json",
+        golden_path=test_resources_dir / "deltalake_mces_golden.json",
+    )
