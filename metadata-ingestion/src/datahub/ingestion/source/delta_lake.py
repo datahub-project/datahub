e10fdbd9fe984666a61760435790d303cc77351c
buggythepirate <bugmenotspamalot@yahoo.com>
2022-03-18 15:49:36 +0100

    added ingestion of metadata. still some small pieces missing

--- metadata-ingestion/src/datahub/ingestion/source/delta_lake.py
+++ metadata-ingestion/src/datahub/ingestion/source/delta_lake.py
@@ -1,5 +1,5 @@
-from curses import meta
 import logging
+import json
 
 from dataclasses import dataclass, field
 from typing import Sequence, List, Optional, Iterable, Union, Dict,Type
@@ -7,19 +7,32 @@ import pydantic
 
 from delta_sharing.delta_sharing import SharingClient
 from delta_sharing.protocol import Table
-from delta_sharing.rest_client import QueryTableMetadataResponse
+from delta_sharing.rest_client import QueryTableMetadataResponse, Metadata
 
-from datahub.configuration.common import AllowDenyPattern, ConfigModel
+from datahub.configuration.common import AllowDenyPattern
+from datahub.configuration.source_common import DatasetSourceConfigBase
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.source import Source, SourceReport
 from datahub.ingestion.api.workunit import MetadataWorkUnit, UsageStatsWorkUnit
+from datahub.metadata.com.linkedin.pegasus2avro.metadata.snapshot import DatasetSnapshot
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import (
     MetadataChangeEvent,
     MetadataChangeProposal,
 )
+
+from datahub.metadata.com.linkedin.pegasus2avro.schema import (
+    SchemaField,
+    OtherSchemaClass,
+    SchemaMetadata,
+    SchemaFieldDataType
+)
+
+from datahub.metadata.schema_classes import (
+    DatasetPropertiesClass,
+)
+
 from datahub.metadata.com.linkedin.pegasus2avro.schema import (
     ArrayTypeClass,
-    BinaryJsonSchema,
     BooleanTypeClass,
     BytesTypeClass,
     DateTypeClass,
@@ -27,22 +40,25 @@ from datahub.metadata.com.linkedin.pegasus2avro.schema import (
     NullTypeClass,
     NumberTypeClass,
     RecordTypeClass,
-    SchemaField,
-    SchemaFieldDataType,
-    SchemalessClass,
-    SchemaMetadata,
     StringTypeClass,
     TimeTypeClass,
     UnionTypeClass,
 )
+from datahub.emitter.mce_builder import (DEFAULT_ENV,    make_data_platform_urn,
+    make_dataset_urn_with_platform_instance,
+    make_domain_urn,
+)
+#TODO: how do we support domains
 
 LOGGER=logging.getLogger(__name__)
 
+#TODO: can be removed if delta-sharing accepts pull request and new version is released
 @dataclass(frozen=True)
 class QueryTableMetadataResponse_extended(QueryTableMetadataResponse):
     table: Table
 
 
+#TODO: can be removed if delta-sharing accepts pull request and new version is released
 class SharingClient_extended(SharingClient):
     """
     An extension of the delta sharing class SharingClient in order to query metadata including the table origin.
@@ -69,15 +85,16 @@ class SharingClient_extended(SharingClient):
 
         return querytablesmetadata
 
-class DeltaLakeSourceConfig(ConfigModel):
+class DeltaLakeSourceConfig(DatasetSourceConfigBase):
 
     url: str
     token: str
     shareCredentialsVersion: str ="1"
-    
+
     share_patterns: AllowDenyPattern = AllowDenyPattern.allow_all()
     schema_patterns: AllowDenyPattern = AllowDenyPattern.allow_all()
     table_patterns: AllowDenyPattern = AllowDenyPattern.allow_all()
+    
 
 # map delta-sharing types to DataHub classes
 _field_type_mapping: Dict[Union[Type, str], Type] = {
@@ -101,6 +118,7 @@ _field_type_mapping: Dict[Union[Type, str], Type] = {
 class DeltaLakeSourceReport(SourceReport):
     filtered: List[str] = field(default_factory=list)
 
+    #TODO: this needs to be fixed i think
     def report_dropped(self, name: str) -> None:
         self.filtered.append(name)
 
@@ -109,6 +127,7 @@ class DeltaLakeSource(Source):
     config: DeltaLakeSourceConfig
     report: DeltaLakeSourceReport
     platform: str = "deltalake"
+    
 
     def __init__(self, ctx: PipelineContext, config: DeltaLakeSourceConfig):
         super().__init__(ctx)
@@ -121,19 +140,6 @@ class DeltaLakeSource(Source):
         config = DeltaLakeSourceConfig.parse_obj(config_dict)
         return cls(ctx, config)
 
-    #TODO: test connection / proper error handling
-    # def get_workunits(self) -> Iterable[Union[MetadataWorkUnit, UsageStatsWorkUnit]]:
-    #     for i, obj in enumerate(iterate_generic_file(self.config.filename)):
-    #         wu: Union[MetadataWorkUnit, UsageStatsWorkUnit]
-    #         if isinstance(obj, UsageAggregationClass):
-    #             wu = UsageStatsWorkUnit(f"file://{self.config.filename}:{i}", obj)
-    #         elif isinstance(obj, MetadataChangeProposal):
-    #             wu = MetadataWorkUnit(f"file://{self.config.filename}:{i}", mcp_raw=obj)
-    #         else:
-    #             wu = MetadataWorkUnit(f"file://{self.config.filename}:{i}", mce=obj)
-    #         self.report.report_workunit(wu)
-    #         yield wu
-
     def get_metadata(self,config: DeltaLakeSourceConfig)-> List[QueryTableMetadataResponse_extended]:
         # Get the access keys for delta-sharing & start the client
         profile = delta_sharing.protocol.DeltaSharingProfile(share_credentials_version=config.share_credentials_version, endpoint=config.url, bearer_token=config.token)
@@ -142,50 +148,142 @@ class DeltaLakeSource(Source):
         # get all shared metadata
         metadata_list=client.query_all_table_metadata()
 
-        #filter data and TODO report dropped items
-        metadata_list=[metadata for metadata in metadata_list if config.share_patterns.allowed(metadata.table.share)]
-        metadata_list=[metadata for metadata in metadata_list if config.schema_patterns.allowed(metadata.table.schema)]
-        metadata_list=[metadata for metadata in metadata_list if config.table_patterns.allowed(metadata.table.name)]
-
         return metadata_list
-    
-    def get_field_type(
-        self, field_type: Union[Type, str], collection_name: str
-    ) -> SchemaFieldDataType:
-        """
-        Maps types encountered in delta-sharing to corresponding schema types.
 
-        Parameters
-        ----------
-            field_type:
-                type of a Python object
-            collection_name:
-                name of collection (for logging)
-        """
-        TypeClass: Optional[Type] = _field_type_mapping.get(field_type)
-
-        if TypeClass is None:
-            self.report.report_warning(
-                collection_name, f"unable to map type {field_type} to metadata schema"
-            )
-            TypeClass = NullTypeClass
+    def get_workunits(self) -> List[MetadataWorkUnit]:
         
-        #TODO: this should be removed if struct nested fields are parsed properly
-        if TypeClass is UnionTypeClass:
-            self.report.report_warning(
-                collection_name, f"Warning {field_type} is a nested field this will not be processed properly and it will displayed poorly in UI."
-            )
+        #get all metadata from API
+        metadata_list = self.get_metadata(config=self.config)
+        
+        wu_list=[]
+        for metadata in metadata_list:
 
-        return SchemaFieldDataType(type=TypeClass())
+            #filter data based on share
+            if not self.config.share_pattern.allowed(metadata.table.share):
+                self.report.report_dropped(metadata.table.share)
+                continue
 
+            #filter data based on schema
+            if not self.config.schema_pattern.allowed(metadata.table.schema):
+                self.report.report_dropped(metadata.table.schema)
+                continue
+
+            #filter data based on table
+            if not self.config.table_pattern.allowed(metadata.table.name):
+                self.report.report_dropped(metadata.table.name)
+                continue
+
+            #collect results
+            wu_list.append(self._create_delta_workunit(metadata))
+        
+        return wu_list
+
+    #TODO: create_workunit report scanned, create data set urn -> datasetsnapshot, add properties, ownership, schema + fields, then turn snapshot into MCE and emit workunit and report
+    # should we rewrite the metadata get to an iterator instead? less mem consumption
+
+    def _create_delta_workunit(
+        self, metadata:QueryTableMetadataResponse_extended,
+    ) -> Iterable[MetadataWorkUnit]:
+        self.report.report_table_scanned(metadata.table.name)
+        
+        dataset_name = f"{metadata.table.schema}.{metadata.table.name}"
+        dataset_urn = make_dataset_urn_with_platform_instance(
+            platform=self.platform,
+            name=dataset_name,
+            platform_instance=metadata.table.share,
+            env=self.config.env,
+        )
+        dataset_snapshot = DatasetSnapshot(
+            urn=dataset_urn,
+            aspects=[],  # we append to this list later on
+        )
+
+        #from metadata top level: get md.description, md.format, md.partitioncolumns
+        custom_properties = {"Id": metadata.metadata.id,
+                            "Format": metadata.metadata.format,
+                            "PartitionColumns":metadata.metadata.partition_columns}
+        
+
+        dataset_properties = DatasetPropertiesClass(
+            tags=[],
+            description=metadata.metadata.description,
+            customProperties=custom_properties,
+        )
+        dataset_snapshot.aspects.append(dataset_properties)
+        
+        
+        #TODO: add documentation that ownership not implemented because not available in API
+        
+        #TODO: add docu & message that stats are not implemented yet.
+        # this has to be collected from file object!
+        
+        #build schema
+        #from md.schemaObject = struct(type, fields). N.B. ignore top-level struct (just container!)
+        #from md.schemaObject  -> md.so.name, md.so.type (struct or atomic), md.so.nullable, md.so.metadata.comment (if exists)
+        schema_metadata = self._create_schema_metadata(dataset_name, metadata)
+        dataset_snapshot.aspects.append(schema_metadata)
+
+        #emit workunit        
+        mce = MetadataChangeEvent(proposedSnapshot=dataset_snapshot)
+        wu = MetadataWorkUnit(id=dataset_name, mce=mce)
+        self.report.report_workunit(wu)
+
+        return wu
+
+    def _create_schema_metadata(
+        self, dataset_name: str, metadata:QueryTableMetadataResponse_extended
+    ) -> SchemaMetadata:
+        schema_fields = self._get_schema_fields(metadata.metadata)
+
+        #TODO: add documentation that version read.out not implemented in pypi version of API yet
+
+        schema_metadata = SchemaMetadata(
+            schemaName=dataset_name,
+            platform=make_data_platform_urn(self.platform),
+            version=0,
+            hash="",
+            platformSchema=OtherSchemaClass(rawSchema=""),
+            fields=schema_fields,
+        )
+        return schema_metadata
+
+    def _get_schema_fields(self, metadata: Metadata) -> List[SchemaField]:
+        canonical_schema = []
+        
+        #get schema strings
+        columns=json.loads(metadata.schema_string)
+        columns=columns["fields"]#get rid of other "hull"
+
+        #TODO: where do we add the name of the field???
+
+        for column in columns:
+            if isinstance(column["type"], dict):    
+                #nested type
+                #TODO: this needs to be fixed
+                self.report.report_warning("Warning {} is a nested field this will not be processed properly and it will displayed poorly in UI.".format(column["name"]))
+                nativeType=column["type"].get("type")
+                datahubType=_field_type_mapping.get(nativeType)
+                datahubDescription=column["metadata"].get("comment")
+                datahubJsonProps=json.dumps(column["type"])
+
+                datahubField=SchemaField(type=datahubType, nativeDataType=nativeType, nullable=column["nullable"], description=datahubDescription, jsonProps=datahubJsonProps)
+            else:
+                #primitive type
+                nativeType=column["type"]
+                datahubType=_field_type_mapping.get(nativeType)
+                datahubType=_field_type_mapping.get(column["type"],NullTypeClass) #NullTypeClass if we cannot map
+                datahubDescription=column["metadata"].get("comment")
+                
+                datahubField=SchemaField(type=datahubType, nativeDataType=nativeType, nullable=column["nullable"], description=datahubDescription)
+    
+            canonical_schema.append(datahubField)
+        return canonical_schema
+    
     def get_report(self) -> SourceReport:
         return self.report
 
     def close(self):
         pass
-
-#TODO: report dropped and filtered in WU
-#TODO: add message that stats are not implemented yet.
 
 
 if __name__ == "__main__":
@@ -211,10 +309,5 @@ if __name__ == "__main__":
     filter_share=AllowDenyPattern(allow=["delta_sharing"], deny=["LA"])
     metadata_list=[metadata for metadata in metadata_list if filter_share.allowed(metadata.table.share)]
 
-    #TODO: catch if no data left to process
-
-    #TODO: transform to datahub dataset entity
-
-    #TODO: publish data
 
 
