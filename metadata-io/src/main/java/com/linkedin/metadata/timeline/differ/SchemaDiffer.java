63f4f9605f6a3c8e8c3370d2f1cc9f3daf0a4915
Ryan Holstien <ryan@acryl.io>
2022-04-06 13:39:55 -0500

    fix(timeline): remove unused import

--- metadata-io/src/main/java/com/linkedin/metadata/timeline/differ/SchemaDiffer.java
+++ metadata-io/src/main/java/com/linkedin/metadata/timeline/differ/SchemaDiffer.java
@@ -20,7 +20,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 
