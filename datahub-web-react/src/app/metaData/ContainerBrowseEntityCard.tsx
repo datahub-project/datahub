5b38db5a0bb71ae995712f6ce747cd561b9e6bf1
Ankit Keshari <ankit.keshari@gslab.com>
2022-04-06 12:51:10 +0530

    Changes the URL path and change the browser for glossaly tern

--- datahub-web-react/src/app/metaData/ContainerBrowseEntityCard.tsx
+++ datahub-web-react/src/app/metaData/ContainerBrowseEntityCard.tsx
@@ -8,6 +8,17 @@ import { LogoCountCard } from '../shared/LogoCountCard';
 
 export const ContainerBrowseEntityCard = ({ entityType, count }: { entityType: EntityType; count: number }) => {
     const entityRegistry = useEntityRegistry();
+    if (entityType === EntityType.GlossaryTerm) {
+        return (
+            <Link to={`${PageRoutes.BROWSE}/${entityRegistry.getPathName(entityType)}`}>
+                <LogoCountCard
+                    logoComponent={entityRegistry.getIcon(entityType, 18, IconStyleType.HIGHLIGHT)}
+                    name={entityRegistry.getCollectionName(entityType)}
+                    count={count}
+                />
+            </Link>
+        );
+    }
     return (
         <Link to={`${PageRoutes.METADATA}/${entityRegistry.getPathName(entityType)}`}>
             <LogoCountCard
