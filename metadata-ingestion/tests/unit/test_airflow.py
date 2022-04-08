3dd8b9f657e8f29740ada81f3b2dcbfaa0787553
treff7es <treff7es@gmail.com>
2022-04-08 21:13:33 +0200

    Fixing airflow test

--- metadata-ingestion/tests/unit/test_airflow.py
+++ metadata-ingestion/tests/unit/test_airflow.py
@@ -210,7 +210,7 @@ def test_lineage_backend(mock_emit, inlets, outlets):
             "AIRFLOW__LINEAGE__BACKEND": "datahub_provider.lineage.datahub.DatahubLineageBackend",
             "AIRFLOW__LINEAGE__DATAHUB_CONN_ID": datahub_rest_connection_config.conn_id,
             "AIRFLOW__LINEAGE__DATAHUB_KWARGS": json.dumps(
-                {"graceful_exceptions": False, "capture_execution": False}
+                {"graceful_exceptions": False, "capture_executions": False}
             ),
         },
     ), mock.patch("airflow.models.BaseOperator.xcom_pull"), mock.patch(
