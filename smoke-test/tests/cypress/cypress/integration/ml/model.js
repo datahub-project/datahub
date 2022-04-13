c92990d32ba1405dfd712fed372a020d4ffae81c
Gabe Lyons <itsgabelyons@gmail.com>
2022-04-12 22:42:12 -0700

    feat(ml): bringing ml screens up to date w/ the modern ui layout & improving ml lineage (#4651)
    * backend ml changes
    
    * updating ml model UI
    
    * more work on the UI
    
    * ml primary key joining the party
    
    * more progress on UI
    
    * making progress on lineage
    
    * finalizing UI experience
    
    * remove irrelevant test
    
    * fixing lint
    
    * fixups
    
    * add tests and fix what the issues they discovered
    
    * internal > core

---
+++ smoke-test/tests/cypress/cypress/integration/ml/model.js
@@ -0,0 +1,30 @@
+describe('models', () => {
+    it('can visit models and groups', () => {
+      cy.visit('/')
+      cy.login();
+      cy.visit('/mlModels/urn:li:mlModel:(urn:li:dataPlatform:sagemaker,cypress-model,PROD)/Summary?is_lineage_mode=false');
+
+      cy.contains('ml model description');
+
+      // the model has metrics & hyper params
+      cy.contains('another-metric');
+      cy.contains('parameter-1');
+
+      // the model has features
+      cy.contains('Features').click();
+      cy.contains('some-cypress-feature-1');
+
+      // the model has a group
+      cy.visit('/mlModels/urn:li:mlModel:(urn:li:dataPlatform:sagemaker,cypress-model,PROD)/Group?is_lineage_mode=false');
+      cy.contains('cypress-model-package-group');
+    });
+
+    it('can visit models and groups', () => {
+      cy.visit('/')
+      cy.login();
+      cy.visit('/mlModelGroup/urn:li:mlModelGroup:(urn:li:dataPlatform:sagemaker,cypress-model-package-group,PROD)');
+      // the model group has its model
+      cy.contains('cypress-model');
+      cy.contains('Just a model package group.');
+    });
+})
