228434328ff18e2dcc02d9acdf14d898ec40b0e6
Aseem Bansal <asmbansal2@gmail.com>
2022-04-13 18:59:04 +0530

    fix(sqlparser): fix sqlparser breaking due to # sign

---
+++ metadata-ingestion/tests/unit/test_sql_lineage_parser_impl.py
@@ -0,0 +1,20 @@
+from datahub.utilities.sql_lineage_parser_impl import SqlLineageSQLParserImpl
+
+
+def test_hash_in_sql_query_with_no_space():
+    parser = SqlLineageSQLParserImpl(
+        sql_query="""
+/*
+HERE IS A STANDARD COMMENT BLOCK
+THIS WILL NOT BREAK sqllineage
+*/
+CREATE OR REPLACE TABLE `foo.bar.trg_tbl`AS
+#This, comment will break sqllineage
+SELECT foo
+-- this comment will not break sqllineage
+# this comment will not break sqllineage either
+FROM `foo.bar.src_tbl`
+        """
+    )
+
+    assert parser.get_tables() == ["foo.bar.src_tbl"]
