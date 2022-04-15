fa9b1a340a167de96c9a15aa66b089cd9b94ba3f
Gabe Lyons <itsgabelyons@gmail.com>
2022-04-15 14:56:18 -0700

    feat(analytics): enable on off mixpanel via env vars (#543)
    * turn mixpanel into an env var thing
    
    * whoops

--- datahub-web-react/src/conf/analytics.ts
+++ datahub-web-react/src/conf/analytics.ts
@@ -3,9 +3,12 @@ const config: any = {
     // googleAnalytics: {
     //    trackingId: 'UA-24123123-01',
     // },
-    mixpanel: {
-        token: '7cee38380de7a8469069c040a1fee320',
-    },
+    mixpanel:
+        (process.env.REACT_APP_MIXPANEL_EVENTS_ENABLED || 'false').toLowerCase() === 'true'
+            ? {
+                  token: process.env.REACT_APP_MIXPANEL_TOKEN,
+              }
+            : null,
     // amplitude: {
     //    apiKey: 'c5c212632315d19c752ab083bc7c92ff',
     // },
