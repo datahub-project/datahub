d4c6d5a1c1a9be64af5c7efc7899f68a2d9ca15b
ShubhamThakre <shhubhamthackeray@gmail.com>
2022-04-15 21:31:54 +0200

    Container side bar section-> scroll added

--- datahub-web-react/src/app/entity/shared/containers/profile/sidebar/EntitySidebar.tsx
+++ datahub-web-react/src/app/entity/shared/containers/profile/sidebar/EntitySidebar.tsx
@@ -6,12 +6,24 @@ import { useBaseEntity, useEntityData } from '../../../EntityContext';
 import { EntitySidebarSection } from '../../../types';
 
 const ContentContainer = styled.div`
+    height: calc(100vh - 61px);
+    overflow-y: auto;
     & > div {
         &:not(:first-child) {
             border-top: 1px solid ${ANTD_GRAY[4]};
         }
         padding-top: 20px;
         margin-bottom: 20px;
+    }
+    &::-webkit-scrollbar {
+        height: 12px;
+        width: 2px;
+        background: #f2f2f2;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: #cccccc;
+        -webkit-border-radius: 1ex;
+        -webkit-box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.75);
     }
 `;
 
