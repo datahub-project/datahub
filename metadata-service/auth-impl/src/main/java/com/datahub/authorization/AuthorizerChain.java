4dd37ef733358048031611a258a87e1275ffa464
Dexter Lee <dexter@acryl.io>
2022-04-15 09:41:47 -0700

    Fix checkstyle

--- metadata-service/auth-impl/src/main/java/com/datahub/authorization/AuthorizerChain.java
+++ metadata-service/auth-impl/src/main/java/com/datahub/authorization/AuthorizerChain.java
@@ -9,7 +9,6 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import lombok.extern.slf4j.Slf4j;
