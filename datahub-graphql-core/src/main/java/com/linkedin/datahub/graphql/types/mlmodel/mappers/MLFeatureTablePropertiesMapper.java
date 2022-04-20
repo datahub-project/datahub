c0f38972988494a680454abc2b7c41a84bca00f3
david.sanchez <david.sanchez@cabify.com>
2022-04-20 10:58:06 +0200

    feat(ml): show custom properties for MLFeatureTable in UI

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/mlmodel/mappers/MLFeatureTablePropertiesMapper.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/mlmodel/mappers/MLFeatureTablePropertiesMapper.java
@@ -3,6 +3,7 @@ package com.linkedin.datahub.graphql.types.mlmodel.mappers;
 import com.linkedin.datahub.graphql.generated.MLFeature;
 import com.linkedin.datahub.graphql.generated.MLFeatureTableProperties;
 import com.linkedin.datahub.graphql.generated.MLPrimaryKey;
+import com.linkedin.datahub.graphql.types.common.mappers.StringMapMapper;
 import com.linkedin.datahub.graphql.types.mappers.ModelMapper;
 import lombok.NonNull;
 
@@ -40,6 +41,8 @@ public class MLFeatureTablePropertiesMapper implements ModelMapper<com.linkedin.
                 })
                 .collect(Collectors.toList()));
         }
+
+        result.setCustomProperties(StringMapMapper.map(mlFeatureTableProperties.getCustomProperties()));
 
         return result;
     }
