980c0550de7b93f1f02f357a19c690b869536351
Dexter Lee <dexter@acryl.io>
2022-04-25 18:45:01 -0700

    fix(policies): Re-revert the policies fix + ingest documents directly to search (#4733)

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/factories/BootstrapManagerFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/factories/BootstrapManagerFactory.java
@@ -2,6 +2,9 @@ package com.linkedin.metadata.boot.factories;
 
 import com.google.common.collect.ImmutableList;
 import com.linkedin.gms.factory.entity.EntityServiceFactory;
+import com.linkedin.gms.factory.entityregistry.EntityRegistryFactory;
+import com.linkedin.gms.factory.search.EntitySearchServiceFactory;
+import com.linkedin.gms.factory.search.SearchDocumentTransformerFactory;
 import com.linkedin.metadata.boot.BootstrapManager;
 import com.linkedin.metadata.boot.steps.IngestDataPlatformInstancesStep;
 import com.linkedin.metadata.boot.steps.IngestDataPlatformsStep;
@@ -9,6 +12,9 @@ import com.linkedin.metadata.boot.steps.IngestPoliciesStep;
 import com.linkedin.metadata.boot.steps.IngestRetentionPoliciesStep;
 import com.linkedin.metadata.boot.steps.IngestRootUserStep;
 import com.linkedin.metadata.entity.EntityService;
+import com.linkedin.metadata.models.registry.EntityRegistry;
+import com.linkedin.metadata.search.EntitySearchService;
+import com.linkedin.metadata.search.transformer.SearchDocumentTransformer;
 import io.ebean.EbeanServer;
 import javax.annotation.Nonnull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +26,25 @@ import org.springframework.context.annotation.Scope;
 
 
 @Configuration
-@Import({EntityServiceFactory.class})
+@Import({EntityServiceFactory.class, EntityRegistryFactory.class, EntitySearchServiceFactory.class,
+    SearchDocumentTransformerFactory.class})
 public class BootstrapManagerFactory {
 
   @Autowired
   @Qualifier("entityService")
   private EntityService _entityService;
+
+  @Autowired
+  @Qualifier("entityRegistry")
+  private EntityRegistry _entityRegistry;
+
+  @Autowired
+  @Qualifier("entitySearchService")
+  private EntitySearchService _entitySearchService;
+
+  @Autowired
+  @Qualifier("searchDocumentTransformer")
+  private SearchDocumentTransformer _searchDocumentTransformer;
 
   @Autowired(required = false)
   @Qualifier("ebeanServer")
@@ -40,7 +59,8 @@ public class BootstrapManagerFactory {
   @Nonnull
   protected BootstrapManager createInstance() {
     final IngestRootUserStep ingestRootUserStep = new IngestRootUserStep(_entityService);
-    final IngestPoliciesStep ingestPoliciesStep = new IngestPoliciesStep(_entityService);
+    final IngestPoliciesStep ingestPoliciesStep =
+        new IngestPoliciesStep(_entityRegistry, _entityService, _entitySearchService, _searchDocumentTransformer);
     final IngestDataPlatformsStep ingestDataPlatformsStep = new IngestDataPlatformsStep(_entityService);
     final IngestDataPlatformInstancesStep ingestDataPlatformInstancesStep =
         new IngestDataPlatformInstancesStep(_entityService, _server);
