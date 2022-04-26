01a5b13a7a012c06eef8880b5079f41b4d931d70
Eddie (Xiao Di) Li <xdl@users.noreply.github.com>
2022-04-26 01:29:24 +0100

    feat(Cassandra): Add Cassandra implementation of EntityService (#3286)

--- docker/dev-with-cassandra.sh
+++ docker/dev-with-cassandra.sh
@@ -8,7 +8,7 @@ fi
 
 CONSUMERS_COMPOSE=""
 if [[ $SEPARATE_CONSUMERS == true ]]; then
-  CONSUMERS_COMPOSE="-f docker-compose.consumers.yml -f docker-compose.consumers.dev.yml"
+  CONSUMERS_COMPOSE="-f docker-compose.consumers-without-neo4j.yml -f docker-compose.consumers.dev.yml"
   if [[ $MONITORING == true ]]; then
     MONITORING_COMPOSE="-f monitoring/docker-compose.monitoring.yml -f monitoring/docker-compose.consumers.monitoring.yml"
   fi
@@ -24,13 +24,13 @@ fi
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR && \
   COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose \
-    -f docker-compose.yml \
-    -f docker-compose.override.yml \
+    -f docker-compose-with-cassandra.yml \
     -f docker-compose.dev.yml \
-    $CONSUMERS_COMPOSE $MONITORING_COMPOSE $M1_COMPOSE pull \
+    $CONSUMERS_COMPOSE $MONITORING_COMPOSE $M1_COMPOSE \
+    pull \
 && \
   COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -p datahub \
-    -f docker-compose.yml \
-    -f docker-compose.override.yml \
+    -f docker-compose-with-cassandra.yml \
     -f docker-compose.dev.yml \
-    $CONSUMERS_COMPOSE $MONITORING_COMPOSE $M1_COMPOSE up --build $@
+    $CONSUMERS_COMPOSE $MONITORING_COMPOSE $M1_COMPOSE \
+    up --build $@
