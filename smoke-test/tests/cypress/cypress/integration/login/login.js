05d6d62cde52363bf99d131a46e599dce3e9c120
xiphl <xipherl9@gmail.com>
2022-04-26 10:20:22 +0800

    .

--- smoke-test/tests/cypress/cypress/integration/login/login.js
+++ smoke-test/tests/cypress/cypress/integration/login/login.js
@@ -3,8 +3,8 @@ describe('login', () => {
     cy.visit('/');
     cy.get('input[placeholder=Username]').type('datahub');
     cy.get('input[placeholder=Password]').type('datahub');
-    cy.wait(5000);
     cy.contains('Log in').should('be.visible').click();
+    cy.wait(5000);
     cy.contains('Welcome back, datahub');
     // cy.contains('Log in').click();
     // cy.contains('Welcome back, Data Hub');
