9c91147cbd36bdea888214a9e5067eb202b137d2
Ryan Holstien <ryan@acryl.io>
2022-04-27 15:05:38 -0500

    feat(graphql): versioned dataset query - fix import

--- metadata-io/src/main/java/com/linkedin/metadata/entity/ebean/EbeanEntityService.java
+++ metadata-io/src/main/java/com/linkedin/metadata/entity/ebean/EbeanEntityService.java
@@ -9,6 +9,7 @@ import com.linkedin.common.AuditStamp;
 import com.linkedin.common.Status;
 import com.linkedin.common.UrnArray;
 import com.linkedin.common.urn.Urn;
+import com.linkedin.common.urn.UrnUtils;
 import com.linkedin.data.schema.RecordDataSchema;
 import com.linkedin.data.template.DataTemplateUtil;
 import com.linkedin.data.template.JacksonDataTemplateCodec;
