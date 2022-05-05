ceb5c284f101466b7b9dc755e38764f717657c1b
Lal Rishav <80503855+saxo-lalrishav@users.noreply.github.com>
2022-05-05 09:13:16 +0530

    Update oauth_generator.py

--- metadata-ingestion/src/datahub/ingestion/source/sql/oauth_generator.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/oauth_generator.py
@@ -20,30 +20,21 @@ class OauthTokenGenerator:
         scopes: List[str],
         check_cache: bool,
     ) -> str:
-        logger.info("hola")
         token = getattr(self, "_get_{}_token".format(self.provider))(
             scopes, check_cache, credentials
         )
         return token
 
     def _get_microsoft_token(self, scopes, check_cache, credentials):
-        logger.info("i am here")
         app = msal.ConfidentialClientApplication(
             self.client_id, authority=self.authority_url, client_credential=credentials
         )
         _token = None
-        logger.info("i am here 1")
         if check_cache:
-            logger.info("i am here 2")
             _token = app.acquire_token_silent(scopes=scopes, account=None)
-            logger.info("i am here 3")
 
         if not _token:
-            logger.info("No token available in cache. Getting a new one from AAD.")
-            logger.info("scope is " + str(scopes))
             _token = app.acquire_token_for_client(scopes=scopes)
-            logger.info("token is " + str(_token))
-            logger.info("i am here 4")
 
         return _token
 
@@ -74,5 +65,4 @@ class OauthTokenGenerator:
     def get_token_with_secret(
         self, secret: str, scopes: List[str], check_cache: bool = False
     ) -> Any:
-        #         logger.info("in get secrets")
         return self._get_token(secret, scopes, check_cache)
