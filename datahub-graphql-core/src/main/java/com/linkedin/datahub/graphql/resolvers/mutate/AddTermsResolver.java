8e07d4fecc941a3126a3e2c160fd7283c5900af0
John Joyce <john@acryl.io>
2022-05-13 12:17:56 -0700

    Finalizing tests

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/mutate/AddTermsResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/mutate/AddTermsResolver.java
@@ -1,59 +1,57 @@
 package com.linkedin.datahub.graphql.resolvers.mutate;
 
-import com.google.common.collect.ImmutableList;
 import com.linkedin.common.urn.CorpuserUrn;
-
 import com.linkedin.common.urn.Urn;
+import com.linkedin.common.urn.UrnUtils;
 import com.linkedin.datahub.graphql.QueryContext;
 import com.linkedin.datahub.graphql.exception.AuthorizationException;
-import com.linkedin.datahub.graphql.generated.TagAssociationInput;
+import com.linkedin.datahub.graphql.generated.AddTermsInput;
 import com.linkedin.datahub.graphql.resolvers.mutate.util.LabelUtils;
 import com.linkedin.metadata.entity.EntityService;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.*;
 
-
 @Slf4j
 @RequiredArgsConstructor
-public class AddTagResolver implements DataFetcher<CompletableFuture<Boolean>> {
+public class AddTermsResolver implements DataFetcher<CompletableFuture<Boolean>> {
   private final EntityService _entityService;
 
   @Override
   public CompletableFuture<Boolean> get(DataFetchingEnvironment environment) throws Exception {
-    final TagAssociationInput input = bindArgument(environment.getArgument("input"), TagAssociationInput.class);
-    Urn tagUrn = Urn.createFromString(input.getTagUrn());
+    final AddTermsInput input = bindArgument(environment.getArgument("input"), AddTermsInput.class);
+    List<Urn> termUrns = input.getTermUrns().stream()
+        .map(UrnUtils::getUrn)
+        .collect(Collectors.toList());
     Urn targetUrn = Urn.createFromString(input.getResourceUrn());
 
-    if (!LabelUtils.isAuthorizedToUpdateTags(environment.getContext(), targetUrn, input.getSubResource())) {
-      throw new AuthorizationException("Unauthorized to perform this action. Please contact your DataHub administrator.");
-    }
-
     return CompletableFuture.supplyAsync(() -> {
+
+      if (!LabelUtils.isAuthorizedToUpdateTerms(environment.getContext(), targetUrn, input.getSubResource())) {
+        throw new AuthorizationException("Unauthorized to perform this action. Please contact your DataHub administrator.");
+      }
+
       LabelUtils.validateInput(
-          tagUrn,
+          termUrns,
           targetUrn,
           input.getSubResource(),
           input.getSubResourceType(),
-          "tag",
+          "glossaryTerm",
           _entityService,
           false
       );
+
       try {
-
-        if (!tagUrn.getEntityType().equals("tag")) {
-          log.error("Failed to add {}. It is not a tag urn.", tagUrn.toString());
-          return false;
-        }
-
-        log.info("Adding Tag. input: {}", input.toString());
+        log.info("Adding Term. input: {}", input);
         Urn actor = CorpuserUrn.createFromString(((QueryContext) environment.getContext()).getActorUrn());
-        LabelUtils.addTagsToTarget(
-            ImmutableList.of(tagUrn),
+        LabelUtils.addTermsToTarget(
+            termUrns,
             targetUrn,
             input.getSubResource(),
             actor,
