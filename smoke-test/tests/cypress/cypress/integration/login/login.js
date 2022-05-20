c2863c1e21b29e2f14bd33f2315d78fe1d4ed942
Gabe Lyons <itsgabelyons@gmail.com>
2022-05-20 16:20:21 -0700

    trying another thing

--- smoke-test/tests/cypress/cypress/integration/login/login.js
+++ smoke-test/tests/cypress/cypress/integration/login/login.js
@@ -1,6 +1,7 @@
 describe('login', () => {
   it('logs in', () => {
     cy.visit('/');
+		cy.wait(10000)
     cy.get('input[data-testid=username]').type('datahub');
     cy.get('input[data-testid=password]').type('datahub');
     cy.contains('Sign In').click();
