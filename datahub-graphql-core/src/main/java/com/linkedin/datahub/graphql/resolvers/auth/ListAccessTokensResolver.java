820f2c4d9029fddc5a1e00a7b9927812903345b2
Pedro Silva <pedro.cls93@gmail.com>
2022-05-21 21:53:06 +0100

    Adds smoke test & minor bug fixes

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/auth/ListAccessTokensResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/auth/ListAccessTokensResolver.java
@@ -5,11 +5,11 @@ import com.google.common.collect.ImmutableSet;
 import com.linkedin.datahub.graphql.QueryContext;
 import com.linkedin.datahub.graphql.authorization.AuthorizationUtils;
 import com.linkedin.datahub.graphql.exception.AuthorizationException;
-import com.linkedin.datahub.graphql.generated.AccessTokenMetadata;
 import com.linkedin.datahub.graphql.generated.EntityType;
 import com.linkedin.datahub.graphql.generated.FacetFilterInput;
 import com.linkedin.datahub.graphql.generated.ListAccessTokenInput;
 import com.linkedin.datahub.graphql.generated.ListAccessTokenResult;
+import com.linkedin.datahub.graphql.generated.AccessTokenMetadata;
 import com.linkedin.entity.client.EntityClient;
 import com.linkedin.metadata.Constants;
 import com.linkedin.metadata.query.filter.SortCriterion;
@@ -18,82 +18,86 @@ import com.linkedin.metadata.search.SearchResult;
 import com.linkedin.metadata.search.utils.QueryUtils;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.*;
+
 
 @Slf4j
 public class ListAccessTokensResolver implements DataFetcher<CompletableFuture<ListAccessTokenResult>> {
 
-  private static final String EXPIRES_AT_FIELD_NAME = "expiredAt";
+    private static final String EXPIRES_AT_FIELD_NAME = "expiredAt";
+    private static final Set<String> FACET_FIELDS = ImmutableSet.of("ownerUrn", "actorUrn", "name", "createdAt",
+            "expiredAt", "description");
 
-  private final EntityClient _entityClient;
+    private final EntityClient _entityClient;
 
-  public ListAccessTokensResolver(final EntityClient entityClient) {
-    this._entityClient = entityClient;
-  }
+    public ListAccessTokensResolver(final EntityClient entityClient) {
+        this._entityClient = entityClient;
+    }
 
-  @Override
-  public CompletableFuture<ListAccessTokenResult> get(DataFetchingEnvironment environment) throws Exception {
-    return CompletableFuture.supplyAsync(() -> {
-      final QueryContext context = environment.getContext();
-      final ListAccessTokenInput input = bindArgument(environment.getArgument("input"), ListAccessTokenInput.class);
-      final Integer start = input.getStart();
-      final Integer count = input.getCount();
-      final List<FacetFilterInput> filters = input.getFilters() == null ? ImmutableList.of() : input.getFilters();
+    @Override
+    public CompletableFuture<ListAccessTokenResult> get(DataFetchingEnvironment environment) throws Exception {
+        return CompletableFuture.supplyAsync(() -> {
+            final QueryContext context = environment.getContext();
+            final ListAccessTokenInput input = bindArgument(environment.getArgument("input"), ListAccessTokenInput.class);
+            final Integer start = input.getStart();
+            final Integer count = input.getCount();
+            final List<FacetFilterInput> filters = input.getFilters() == null ? ImmutableList.of() : input.getFilters();
 
-      log.info("User {} listing access tokens with filters {}", context.getActorUrn(), filters.toString());
+            log.info("User {} listing access tokens with filters {}", context.getActorUrn(), filters.toString());
 
-      if (AuthorizationUtils.canManageAllPersonalTokens(context) || isListingSelfTokens(filters, context)) {
-        try {
-          final SortCriterion sortCriterion =
-              new SortCriterion().setField(EXPIRES_AT_FIELD_NAME).setOrder(SortOrder.DESCENDING);
+            if (AuthorizationUtils.canManageAllPersonalTokens(context) || isListingSelfTokens(filters, context)) {
+                try {
+                    final SortCriterion sortCriterion =
+                            new SortCriterion().setField(EXPIRES_AT_FIELD_NAME).setOrder(SortOrder.DESCENDING);
 
-          final SearchResult searchResult = _entityClient.search(Constants.ACCESS_TOKEN_ENTITY_NAME, "",
-              QueryUtils.newFilter(buildFacetFilters(filters, ImmutableSet.of("actorUrn"))), sortCriterion, start, count,
-              getAuthentication(environment));
+                    final SearchResult searchResult = _entityClient.search(Constants.ACCESS_TOKEN_ENTITY_NAME, "",
+                            QueryUtils.newFilter(buildFacetFilters(filters, FACET_FIELDS)), sortCriterion, start, count,
+                            getAuthentication(environment));
 
-          final List<AccessTokenMetadata> tokens = searchResult.getEntities().stream().map(entity -> {
-            final AccessTokenMetadata metadata = new AccessTokenMetadata();
-            metadata.setUrn(entity.getEntity().toString());
-            metadata.setType(EntityType.ACCESS_TOKEN);
-            return metadata;
-          }).collect(Collectors.toList());
+                    final List<AccessTokenMetadata> tokens = searchResult.getEntities().stream().map(entity -> {
+                        final AccessTokenMetadata metadata = new AccessTokenMetadata();
+                        metadata.setUrn(entity.getEntity().toString());
+                        metadata.setType(EntityType.ACCESS_TOKEN);
+                        return metadata;
+                    }).collect(Collectors.toList());
 
-          final ListAccessTokenResult result = new ListAccessTokenResult();
-          result.setTokens(tokens);
-          result.setStart(searchResult.getFrom());
-          result.setCount(searchResult.getPageSize());
-          result.setTotal(searchResult.getNumEntities());
+                    final ListAccessTokenResult result = new ListAccessTokenResult();
+                    result.setTokens(tokens);
+                    result.setStart(searchResult.getFrom());
+                    result.setCount(searchResult.getPageSize());
+                    result.setTotal(searchResult.getNumEntities());
 
-          return result;
-        } catch (Exception e) {
-          throw new RuntimeException("Failed to list access tokens", e);
-        }
-      }
+                    return result;
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to list access tokens", e);
+                }
+            }
+            throw new AuthorizationException(
+                    "Unauthorized to perform this action. Please contact your DataHub administrator.");
+        });
+    }
 
-      throw new AuthorizationException(
-          "Unauthorized to perform this action. Please contact your DataHub administrator.");
-    });
-  }
-
-  /**
-   * Utility method to answer: Does the existing security context have permissions to generate their personal tokens
-   * AND is the request coming in requesting those personal tokens?
-   *
-   * Note: We look for the actorUrn field because a token generated by someone else means that the generator actor has
-   * manage all access token privileges which means that he/she will be bound to just listing their own tokens.
-   *
-   * @param filters The filters being used in the request.
-   * @param context Current security context.
-   * @return A boolean stating if the current user can list its personal tokens.
-   */
-  private boolean isListingSelfTokens(final List<FacetFilterInput> filters, final QueryContext context) {
-    return AuthorizationUtils.canGeneratePersonalAccessToken(context) && filters.stream()
-        .anyMatch(filter -> filter.getField().equals("actorUrn") && filter.getValue().equals(context.getActorUrn()));
-  }
+    /**
+     * Utility method to answer: Does the existing security context have permissions to generate their personal tokens
+     * AND is the request coming in requesting those personal tokens?
+     * <p>
+     * Note: We look for the actorUrn field because a token generated by someone else means that the generator actor has
+     * manage all access token privileges which means that he/she will be bound to just listing their own tokens.
+     *
+     * @param filters The filters being used in the request.
+     * @param context Current security context.
+     * @return A boolean stating if the current user can list its personal tokens.
+     */
+    private boolean isListingSelfTokens(final List<FacetFilterInput> filters, final QueryContext context) {
+        return AuthorizationUtils.canGeneratePersonalAccessToken(context) && filters.stream()
+                .anyMatch(filter -> filter.getField().equals("actorUrn") && filter.getValue().equals(context.getActorUrn()));
+    }
 }
--- metadata-service/auth-impl/src/main/java/com/datahub/authentication/token/StatefulTokenService.java
+++ metadata-service/auth-impl/src/main/java/com/datahub/authentication/token/StatefulTokenService.java
@@ -12,20 +12,24 @@ import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.Constants;
 import com.linkedin.metadata.entity.EntityService;
 import com.linkedin.metadata.key.DataHubAccessTokenKey;
+import com.linkedin.metadata.resources.entity.AspectUtils;
 import com.linkedin.metadata.utils.AuditStampUtils;
 import com.linkedin.metadata.utils.GenericRecordUtils;
 import com.linkedin.mxe.MetadataChangeProposal;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.ArrayUtils;
 
@@ -39,129 +43,120 @@ import static com.datahub.authentication.token.TokenClaims.*;
 @Slf4j
 public class StatefulTokenService extends StatelessTokenService {
 
-  private final EntityService _entityService;
-  private final LoadingCache<String, Boolean> _revokedTokenCache;
-  private final String saltingKey;
-  private final MessageDigest sha256;
+    private final EntityService _entityService;
+    private final LoadingCache<String, Boolean> _revokedTokenCache;
+    private final String saltingKey;
+    private final MessageDigest sha256;
 
-  public StatefulTokenService(@Nonnull final String signingKey, @Nonnull final String signingAlgorithm,
-      @Nullable final String iss, @Nonnull final EntityService entityService, @Nonnull final String saltingKey) {
-    super(signingKey, signingAlgorithm, iss);
-    this._entityService = entityService;
-    this._revokedTokenCache = CacheBuilder.newBuilder()
-        .maximumSize(10000)
-        .expireAfterWrite(6, TimeUnit.HOURS)
-        .build(new CacheLoader<String, Boolean>() {
-          @Override
-          public Boolean load(final String key) {
-            final Urn accessUrn = Urn.createFromTuple(Constants.ACCESS_TOKEN_ENTITY_NAME, key);
-            return !_entityService.exists(accessUrn);
-          }
-        });
-    this.saltingKey = saltingKey;
-    try {
-      this.sha256 = MessageDigest.getInstance("SHA-256");
-    } catch (NoSuchAlgorithmException e) {
-      throw new RuntimeException("Unable to get SHA-256 algorithm.");
-    }
-  }
-
-  @Nonnull
-  public String generateAccessToken(@Nonnull TokenType type, @Nonnull Actor actor, @Nonnull String tokenName,
-      String tokenDescription, String actorUrn) {
-    Date date = new Date();
-    long timeMilli = date.getTime();
-    return generateAccessToken(type, actor, DEFAULT_EXPIRES_IN_MS, timeMilli, tokenName, tokenDescription, actorUrn);
-  }
-
-  @Nonnull
-  public String generateAccessToken(@Nonnull TokenType type, @Nonnull Actor actor, long expiresInMs, long createdAtInMs,
-      String tokenName, String tokenDescription, String actorUrn) {
-    Objects.requireNonNull(type);
-    Objects.requireNonNull(actor);
-    Map<String, Object> claims = new HashMap<>();
-    // Only stateful token service generates v2 tokens.
-    claims.put(TOKEN_VERSION_CLAIM_NAME, String.valueOf(TokenVersion.TWO.numericValue));
-    claims.put(TOKEN_TYPE_CLAIM_NAME, type.toString());
-    claims.put(ACTOR_TYPE_CLAIM_NAME, actor.getType());
-    claims.put(ACTOR_ID_CLAIM_NAME, actor.getId());
-    final String accessToken = super.generateAccessToken(actor.getId(), claims, expiresInMs);
-    final String tokenHash = this.hash(accessToken);
-
-    final MetadataChangeProposal proposal = new MetadataChangeProposal();
-
-    // Create the access token key --> use a hashed access token value as a unique id to ensure it's not duplicated.
-    final DataHubAccessTokenKey key = new DataHubAccessTokenKey();
-    key.setId(tokenHash);
-    proposal.setEntityKeyAspect(GenericRecordUtils.serializeAspect(key));
-
-    // Create the secret value.
-    final DataHubAccessTokenInfo value = new DataHubAccessTokenInfo();
-    value.setName(tokenName);
-    value.setDescription(tokenDescription);
-    value.setActorUrn(UrnUtils.getUrn(actor.toUrnStr()));
-    value.setOwnerUrn(UrnUtils.getUrn(actorUrn));
-    value.setCreatedAt(createdAtInMs);
-    value.setExpiredAt(createdAtInMs + expiresInMs);
-
-    proposal.setEntityType(Constants.ACCESS_TOKEN_ENTITY_NAME);
-    proposal.setAspectName(Constants.ACCESS_TOKEN_INFO_NAME);
-    proposal.setAspect(GenericRecordUtils.serializeAspect(value));
-    proposal.setChangeType(ChangeType.UPSERT);
-
-    log.info("About to ingest access token metadata {}", proposal);
-    AuditStamp auditStamp = AuditStampUtils.createDefaultAuditStamp().setActor(UrnUtils.getUrn(actorUrn));
-    _entityService.ingestProposal(proposal, auditStamp);
-
-    return accessToken;
-  }
-
-  @Nonnull
-  @Override
-  public TokenClaims validateAccessToken(@Nonnull String accessToken) throws TokenException {
-    try {
-      final TokenClaims tokenClaims = super.validateAccessToken(accessToken);
-      if (tokenClaims.getTokenVersion().equals(TokenVersion.TWO)) {
-        final String hash = hash(accessToken);
-          if (_revokedTokenCache.get(hash)) {
-            throw new TokenException("Failed to validate DataHub token: Token has been revoked");
-          }
+    public StatefulTokenService(@Nonnull final String signingKey, @Nonnull final String signingAlgorithm,
+                                @Nullable final String iss, @Nonnull final EntityService entityService, @Nonnull final String saltingKey) {
+        super(signingKey, signingAlgorithm, iss);
+        this._entityService = entityService;
+        this._revokedTokenCache = CacheBuilder.newBuilder()
+                .maximumSize(10000)
+                .expireAfterWrite(6, TimeUnit.HOURS)
+                .build(new CacheLoader<String, Boolean>() {
+                    @Override
+                    public Boolean load(final String key) {
+                        final Urn accessUrn = Urn.createFromTuple(Constants.ACCESS_TOKEN_ENTITY_NAME, key);
+                        return !_entityService.exists(accessUrn);
+                    }
+                });
+        this.saltingKey = saltingKey;
+        try {
+            this.sha256 = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Unable to get SHA-256 algorithm.");
         }
-        return tokenClaims;
-      } catch (final TokenExpiredException e) {
-        // delete entity
-        this.revokeAccessToken(hash(accessToken));
-        throw e;
-      } catch (final ExecutionException e) {
-        throw new TokenException("Failed to validate DataHub token: Unable to load token information from store");
-      }
-  }
-
-  public void revokeAccessToken(@Nonnull String hashedToken) throws TokenException {
-    final Urn tokenUrn = Urn.createFromTuple(Constants.ACCESS_TOKEN_ENTITY_NAME, hashedToken);
-    if (_entityService.exists(tokenUrn)) {
-      _entityService.deleteUrn(tokenUrn);
-      _revokedTokenCache.put(salt(hashedToken), true);
-      return;
     }
-    throw new TokenException("Access token no longer exists");
-  }
 
-  /**
-   * Hashes the input after salting it.
-   */
-  public String hash(String input) {
-    final byte[] saltingKeyBytes = this.saltingKey.getBytes();
-    final byte[] inputBytes = input.getBytes();
-    final byte[] concatBytes = ArrayUtils.addAll(inputBytes, saltingKeyBytes);
-    final byte[] bytes = sha256.digest(concatBytes);
-    return Base64.getEncoder().encodeToString(bytes);
-  }
+    @Nonnull
+    public String generateAccessToken(@Nonnull TokenType type, @Nonnull Actor actor, long expiresInMs, long createdAtInMs,
+                                      String tokenName, String tokenDescription, String actorUrn) {
+        Objects.requireNonNull(type);
+        Objects.requireNonNull(actor);
+        Map<String, Object> claims = new HashMap<>();
+        // Only stateful token service generates v2 tokens.
+        claims.put(TOKEN_VERSION_CLAIM_NAME, String.valueOf(TokenVersion.TWO.numericValue));
+        claims.put(TOKEN_TYPE_CLAIM_NAME, type.toString());
+        claims.put(ACTOR_TYPE_CLAIM_NAME, actor.getType());
+        claims.put(ACTOR_ID_CLAIM_NAME, actor.getId());
+        final String accessToken = super.generateAccessToken(actor.getId(), claims, expiresInMs);
+        final String tokenHash = this.hash(accessToken);
 
-  private String salt(String input) {
-    byte[] saltingKeyBytes = this.saltingKey.getBytes();
-    final byte[] inputBytes = input.getBytes();
-    byte[] concatBytes = ArrayUtils.addAll(saltingKeyBytes, inputBytes);
-    return Base64.getEncoder().encodeToString(concatBytes);
-  }
+        final MetadataChangeProposal proposal = new MetadataChangeProposal();
+
+        // Create the access token key --> use a hashed access token value as a unique id to ensure it's not duplicated.
+        final DataHubAccessTokenKey key = new DataHubAccessTokenKey();
+        key.setId(tokenHash);
+        proposal.setEntityKeyAspect(GenericRecordUtils.serializeAspect(key));
+
+        // Create the secret value.
+        final DataHubAccessTokenInfo value = new DataHubAccessTokenInfo();
+        value.setName(tokenName);
+        value.setDescription(tokenDescription);
+        value.setActorUrn(UrnUtils.getUrn(actor.toUrnStr()));
+        value.setOwnerUrn(UrnUtils.getUrn(actorUrn));
+        value.setCreatedAt(createdAtInMs);
+        value.setExpiredAt(createdAtInMs + expiresInMs);
+
+        proposal.setEntityType(Constants.ACCESS_TOKEN_ENTITY_NAME);
+        proposal.setAspectName(Constants.ACCESS_TOKEN_INFO_NAME);
+        proposal.setAspect(GenericRecordUtils.serializeAspect(value));
+        proposal.setChangeType(ChangeType.UPSERT);
+
+        log.info("About to ingest access token metadata {}", proposal);
+        final AuditStamp auditStamp = AuditStampUtils.createDefaultAuditStamp().setActor(UrnUtils.getUrn(actorUrn));
+
+        // Need this to write key aspect
+        final List<MetadataChangeProposal> additionalChanges =
+                AspectUtils.getAdditionalChanges(proposal, _entityService);
+
+        _entityService.ingestProposal(proposal, auditStamp);
+        additionalChanges.forEach(mcp -> _entityService.ingestProposal(mcp, auditStamp));
+
+        return accessToken;
+    }
+
+    @Nonnull
+    @Override
+    public TokenClaims validateAccessToken(@Nonnull String accessToken) throws TokenException {
+        try {
+            final TokenClaims tokenClaims = super.validateAccessToken(accessToken);
+            if (tokenClaims.getTokenVersion().equals(TokenVersion.TWO)) {
+                final String hash = hash(accessToken);
+                if (_revokedTokenCache.get(hash)) {
+                    throw new TokenException("Failed to validate DataHub token: Token has been revoked");
+                }
+            }
+            return tokenClaims;
+        } catch (final TokenExpiredException e) {
+            // delete entity
+            this.revokeAccessToken(hash(accessToken));
+            throw e;
+        } catch (final ExecutionException e) {
+            throw new TokenException("Failed to validate DataHub token: Unable to load token information from store");
+        }
+    }
+
+    public void revokeAccessToken(@Nonnull String hashedToken) throws TokenException {
+        final Urn tokenUrn = Urn.createFromTuple(Constants.ACCESS_TOKEN_ENTITY_NAME, hashedToken);
+        if (_entityService.exists(tokenUrn)) {
+            _entityService.deleteUrn(tokenUrn);
+            _revokedTokenCache.put(hashedToken, true);
+            return;
+        }
+        throw new TokenException("Access token no longer exists");
+    }
+
+    /**
+     * Hashes the input after salting it.
+     */
+    public String hash(String input) {
+        final byte[] saltingKeyBytes = this.saltingKey.getBytes();
+        final byte[] inputBytes = input.getBytes();
+        final byte[] concatBytes = ArrayUtils.addAll(inputBytes, saltingKeyBytes);
+        final byte[] bytes = sha256.digest(concatBytes);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 }
