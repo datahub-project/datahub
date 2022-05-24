b77f381429d13864290ab8f12632407e8b1673a0
Aseem Bansal <asmbansal2@gmail.com>
2022-05-24 23:17:27 +0530

    doc(ingestion): default boolean fix, broken bigquery docgen (#4984)

--- metadata-ingestion/src/datahub/ingestion/source_config/usage/bigquery_usage.py
+++ metadata-ingestion/src/datahub/ingestion/source_config/usage/bigquery_usage.py
@@ -20,7 +20,7 @@ class BigQueryCredential(ConfigModel):
     project_id: str = pydantic.Field(description="Project id to set the credentials")
     private_key_id: str = pydantic.Field(description="Private key id")
     private_key: str = pydantic.Field(
-        description="Private key in a form of '-----BEGIN PRIVATE KEY-----\nprivate-key\n-----END PRIVATE KEY-----\n'"
+        description="Private key in a form of '-----BEGIN PRIVATE KEY-----\\nprivate-key\\n-----END PRIVATE KEY-----\\n'"
     )
     client_email: str = pydantic.Field(description="Client email")
     client_id: str = pydantic.Field(description="Client Id")
