b6c91eb981451c2198b26b06f997497eb61ceae5
Dexter Lee <dexter@acryl.io>
2022-05-24 11:17:36 -0700

    Commenting out login test and test_rapid

--- smoke-test/tests/cypress/cypress/integration/login/login.js
+++ smoke-test/tests/cypress/cypress/integration/login/login.js
@@ -1,9 +1,9 @@
-describe('login', () => {
-  it('logs in', () => {
-    cy.visit('/');
-    cy.get('input[data-testid=username]').type('datahub');
-    cy.get('input[data-testid=password]').type('datahub');
-    cy.contains('Sign In').click();
-    cy.contains('Welcome back, Data Hub');
-  });
-})
+// describe('login', () => {
+//   it('logs in', () => {
+//     cy.visit('/');
+//     cy.get('input[data-testid=username]').type('datahub');
+//     cy.get('input[data-testid=password]').type('datahub');
+//     cy.contains('Sign In').click();
+//     cy.contains('Welcome back, Data Hub');
+//   });
+// })
