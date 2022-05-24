8b0973e277cca9619fa89e87e7a44e71ed788fbc
Ravindra Lanka <rlanka@acryl.io>
2022-05-24 15:42:02 -0700

    Minor cleanup + annotations.

--- metadata-ingestion/src/datahub/ingestion/source/sql/hana.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/hana.py
@@ -1,6 +1,8 @@
-# These imports verify that the dependencies are available.
-import hdbcli  # noqa: F401
+from typing import Dict
 
+import pydantic
+
+from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.source.sql.sql_common import (
     BasicSQLAlchemyConfig,
     SQLAlchemySource,
@@ -8,19 +10,16 @@ from datahub.ingestion.source.sql.sql_common import (
 
 
 class HanaConfig(BasicSQLAlchemyConfig):
-    # defaults
-    host_port = "localhost:39041"
-    scheme = "hana+hdbcli"
+    # Override defaults
+    host_port: str = pydantic.Field(default="localhost:39041")
+    scheme: str = pydantic.Field(default="hana+hdbcli")
 
 
 class HanaSource(SQLAlchemySource):
-    def __init__(self, config, ctx):
-        super().__init__(config, ctx, self.get_platform())
-
-    def get_platform(self):
-        return "hana"
+    def __init__(self, config: HanaConfig, ctx: PipelineContext):
+        super().__init__(config, ctx, "hana")
 
     @classmethod
-    def create(cls, config_dict, ctx):
+    def create(cls, config_dict: Dict, ctx: PipelineContext) -> "HanaSource":
         config = HanaConfig.parse_obj(config_dict)
         return cls(config, ctx)
