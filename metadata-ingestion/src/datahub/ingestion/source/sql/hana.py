24d8f91f85f55cf98a850c5a4033f8cb3ab0dead
Ravindra Lanka <rlanka@acryl.io>
2022-05-25 14:33:19 -0700

    Fix documentation & capability decorators.

--- metadata-ingestion/src/datahub/ingestion/source/sql/hana.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/hana.py
@@ -3,6 +3,14 @@ from typing import Dict
 import pydantic
 
 from datahub.ingestion.api.common import PipelineContext
+from datahub.ingestion.api.decorators import (
+    SourceCapability,
+    SupportStatus,
+    capability,
+    config_class,
+    platform_name,
+    support_status,
+)
 from datahub.ingestion.source.sql.sql_common import (
     BasicSQLAlchemyConfig,
     SQLAlchemySource,
@@ -15,6 +23,13 @@ class HanaConfig(BasicSQLAlchemyConfig):
     scheme: str = pydantic.Field(default="hana+hdbcli")
 
 
+@platform_name("Hana")
+@config_class(HanaConfig)
+@support_status(SupportStatus.TESTING)
+@capability(SourceCapability.PLATFORM_INSTANCE, "Enabled by default")
+@capability(SourceCapability.DOMAINS, "Supported via the `domain` config field")
+@capability(SourceCapability.DATA_PROFILING, "Optionally enabled via configuration")
+@capability(SourceCapability.DELETION_DETECTION, "Enabled via stateful ingestion")
 class HanaSource(SQLAlchemySource):
     def __init__(self, config: HanaConfig, ctx: PipelineContext):
         super().__init__(config, ctx, "hana")
