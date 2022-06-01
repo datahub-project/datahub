ff04919042c0d3c6f15717cd53c2892cbc1b5868
Mert Tunç <mert.tunc@udemy.com>
2022-06-01 21:46:53 +0300

    fix(mysql-setup-job): add mysql default port override support (#5036)

--- docker/mysql-setup/init.sh
+++ docker/mysql-setup/init.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+: ${MYSQL_PORT:=3306}
 
 sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" /init.sql | tee -a /tmp/init-final.sql
-mysql -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST < /tmp/init-final.sql
+mysql -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT < /tmp/init-final.sql
