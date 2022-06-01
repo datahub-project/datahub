a10e60a0d5b5dfb66f8b4e6abe41c11d4250dd52
Chris Collins <chriscollins@Chriss-MBP.lan>
2022-06-01 10:31:08 -0400

    feat(glossary) Display Incoming 'IsA' Glossary related entities

--- datahub-web-react/src/app/entity/shared/types.ts
+++ datahub-web-react/src/app/entity/shared/types.ts
@@ -84,6 +84,7 @@ export type GenericEntityProperties = {
     parentContainers?: Maybe<ParentContainersResult>;
     children?: Maybe<EntityRelationshipsResult>;
     parentNodes?: Maybe<ParentNodesResult>;
+    childTerms?: Maybe<EntityRelationshipsResult>;
 };
 
 export type GenericEntityUpdate = {
