c5d4aa58a0931a609140017d6a7718d50013e603
xiphl <xipherl9@gmail.com>
2022-06-02 18:40:45 +0800

    initial commit

--- datahub-web-react/src/conf/Adhoc.ts
+++ datahub-web-react/src/conf/Adhoc.ts
@@ -1,6 +1,6 @@
 /*
     url to call the rest api
 */
-const config = 'http://localhost:8001/custom/make_dataset';
+export const config = 'http://localhost:8001/custom/make_dataset';
 
-export default config;
+export const helpRefLink = 'http://www.google.com';
