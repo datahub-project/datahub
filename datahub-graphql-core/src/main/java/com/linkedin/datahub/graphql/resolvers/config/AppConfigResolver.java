e6915a652cc6f398cc38ad0f810843f4f26c45ff
John Joyce <john@acryl.io>
2022-06-07 14:32:37 -0700

    Revert unnecessary changes

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/config/AppConfigResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/config/AppConfigResolver.java
@@ -15,12 +15,11 @@ import com.linkedin.datahub.graphql.generated.Privilege;
 import com.linkedin.datahub.graphql.generated.ResourcePrivileges;
 import com.linkedin.datahub.graphql.generated.TelemetryConfig;
 import com.linkedin.datahub.graphql.generated.TestsConfig;
-import com.linkedin.datahub.graphql.generated.VisualConfig;
+import com.linkedin.datahub.graphql.generated.VisualConfiguration;
 import com.linkedin.metadata.config.DatahubConfiguration;
 import com.linkedin.metadata.config.IngestionConfiguration;
 import com.linkedin.metadata.config.TestsConfiguration;
 import com.linkedin.metadata.telemetry.TelemetryConfiguration;
-import com.linkedin.metadata.config.VisualConfiguration;
 import com.linkedin.metadata.version.GitVersion;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
@@ -111,12 +110,7 @@ public class AppConfigResolver implements DataFetcher<CompletableFuture<AppConfi
     appConfig.setIdentityManagementConfig(identityManagementConfig);
     appConfig.setManagedIngestionConfig(ingestionConfig);
     appConfig.setAuthConfig(authConfig);
-
-    final VisualConfig visualConfig = new VisualConfig();
-    if (_visualConfiguration != null && _visualConfiguration.getAssets() != null && _visualConfiguration.getAssets().getLogoUrl() != null) {
-      visualConfig.setLogoUrl(_visualConfiguration.getAssets().getLogoUrl());
-    }
-    appConfig.setVisualConfig(visualConfig);
+    appConfig.setVisualConfig(_visualConfiguration);
 
     final TelemetryConfig telemetryConfig = new TelemetryConfig();
     telemetryConfig.setEnableThirdPartyLogging(_telemetryConfiguration.isEnableThirdPartyLogging());
