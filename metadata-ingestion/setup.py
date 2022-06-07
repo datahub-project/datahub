51854a768dfb3629561919865618f0177bab2081
Shirshanka Das <shirshanka@apache.org>
2022-06-07 10:44:00 -0700

    fix(ci): pin google-cloud-logging to avoid pip backtracking (#5109)

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -114,9 +114,11 @@ looker_common = {
 
 bigquery_common = {
     # Google cloud logging library
-    "google-cloud-logging",
+    "google-cloud-logging<3.1.2",
     "google-cloud-bigquery",
     "more-itertools>=8.12.0",
+    # we do not use protobuf directly but newer version caused bigquery connector to fail
+    "protobuf<=3.20.1",
 }
 
 snowflake_common = {
