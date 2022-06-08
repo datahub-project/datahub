b3d3e7edd831cb78c4e6665460b6b4fcf7f250c3
Chris Collins <chriscollins@Chriss-MBP-2-373.lan>
2022-06-08 14:22:06 -0400

    fix broken cypress test

--- smoke-test/tests/cypress/cypress/integration/lineage/impact_analysis.js
+++ smoke-test/tests/cypress/cypress/integration/lineage/impact_analysis.js
@@ -2,7 +2,8 @@ describe('mutations', () => {
   it('can create and add a tag to dataset and visit new tag page', () => {
     cy.login();
     cy.visit('/dataset/urn:li:dataset:(urn:li:dataPlatform:kafka,SampleCypressKafkaDataset,PROD)/Lineage?is_lineage_mode=false');
-    cy.contains('Impact Analysis').click({ force: true });
+    // click to show more relationships now that we default to 1 degree of dependency
+    cy.contains('3+').click({ force: true });
 
     // impact analysis can take a beat- don't want to time out here
     cy.wait(5000);
