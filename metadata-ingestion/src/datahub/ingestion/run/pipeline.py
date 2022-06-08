2b06cb19fd4168b6c42614d354c4a3f55079c7a5
Aseem Bansal <asmbansal2@gmail.com>
2022-06-08 23:03:32 +0530

    fix(bigquery): handling of empty partitioned tables, improve report message (#5122)

--- metadata-ingestion/src/datahub/ingestion/run/pipeline.py
+++ metadata-ingestion/src/datahub/ingestion/run/pipeline.py
@@ -324,6 +324,12 @@ class Pipeline:
             self.ctx.graph,
         )
 
+    def _count_all_vals(self, d: Dict[str, List]) -> int:
+        result = 0
+        for val in d.values():
+            result += len(val)
+        return result
+
     def pretty_print_summary(self, warnings_as_failure: bool = False) -> int:
         click.echo()
         click.secho(f"Source ({self.config.source.type}) report:", bold=True)
@@ -331,12 +337,29 @@ class Pipeline:
         click.secho(f"Sink ({self.config.sink.type}) report:", bold=True)
         click.echo(self.sink.get_report().as_string())
         click.echo()
+        workunits_produced = self.source.get_report().workunits_produced
         if self.source.get_report().failures or self.sink.get_report().failures:
-            click.secho("Pipeline finished with failures", fg="bright_red", bold=True)
+            num_failures_source = self._count_all_vals(
+                self.source.get_report().failures
+            )
+            click.secho(
+                f"Pipeline finished with {num_failures_source} failures in source producing {workunits_produced} workunits",
+                fg="bright_red",
+                bold=True,
+            )
             return 1
         elif self.source.get_report().warnings or self.sink.get_report().warnings:
-            click.secho("Pipeline finished with warnings", fg="yellow", bold=True)
+            num_warn_source = self._count_all_vals(self.source.get_report().warnings)
+            click.secho(
+                f"Pipeline finished with {num_warn_source} warnings in source producing {workunits_produced} workunits",
+                fg="yellow",
+                bold=True,
+            )
             return 1 if warnings_as_failure else 0
         else:
-            click.secho("Pipeline finished successfully", fg="green", bold=True)
+            click.secho(
+                f"Pipeline finished successfully producing {workunits_produced} workunits",
+                fg="green",
+                bold=True,
+            )
             return 0
