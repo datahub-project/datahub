a29a0286d55c26fe6f05337291bfbe781cddcfad
Aditya Radhakrishnan <aditya@acryl.io>
2022-06-09 17:38:53 -0400

    fix(ui-ingestion): show warning banner when configuring looker ui-ingestion for the first time (#5139)

--- datahub-web-react/src/app/ingest/source/builder/DefineRecipeStep.tsx
+++ datahub-web-react/src/app/ingest/source/builder/DefineRecipeStep.tsx
@@ -1,4 +1,4 @@
-import { Button, message, Typography } from 'antd';
+import { Alert, Button, message, Typography } from 'antd';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { StepProps } from './types';
@@ -6,6 +6,8 @@ import { getSourceConfigs, jsonToYaml, yamlToJson } from '../utils';
 import { YamlEditor } from './YamlEditor';
 import { ANTD_GRAY } from '../../../entity/shared/constants';
 import { IngestionSourceBuilderStep } from './steps';
+
+const LOOKML_DOC_LINK = 'https://datahubproject.io/docs/generated/ingestion/sources/looker#module-lookml';
 
 const Section = styled.div`
     display: flex;
@@ -46,15 +48,20 @@ export const DefineRecipeStep = ({ state, updateState, goTo, prev }: StepProps) 
 
     const { type } = state;
     const sourceConfigs = getSourceConfigs(type as string);
+    const isEditing: boolean = prev === undefined;
     const displayRecipe = stagedRecipeYml || sourceConfigs.placeholderRecipe;
     const sourceDisplayName = sourceConfigs.displayName;
     const sourceDocumentationUrl = sourceConfigs.docsUrl; // Maybe undefined (in case of "custom")
 
+    // LookML banner specific code
+    const isSourceLooker: boolean = sourceConfigs.type === 'looker';
+    const [showLookerBanner, setShowLookerBanner] = useState(isSourceLooker && !isEditing);
+
     useEffect(() => {
-        if (stagedRecipeYml && stagedRecipeYml.length > 0) {
+        if (stagedRecipeYml && stagedRecipeYml.length > 0 && !showLookerBanner) {
             setStepComplete(true);
         }
-    }, [stagedRecipeYml]);
+    }, [stagedRecipeYml, showLookerBanner]);
 
     const onClickNext = () => {
         // Convert the recipe into it's json representation, and catch + report exceptions while we do it.
@@ -82,7 +89,37 @@ export const DefineRecipeStep = ({ state, updateState, goTo, prev }: StepProps) 
         <>
             <Section>
                 <SelectTemplateHeader level={5}>Configure {sourceDisplayName} Recipe</SelectTemplateHeader>
+                {showLookerBanner && (
+                    <Alert
+                        type="warning"
+                        banner
+                        closable
+                        message={
+                            <>
+                                <big>
+                                    <i>
+                                        <b>You must close this banner to proceed!</b>
+                                    </i>
+                                </big>
+                                <br />
+                                <br />
+                                To get complete Looker metadata integration (including Looker views and lineage to the
+                                underlying warehouse tables), you must <b>also</b> use the{' '}
+                                <a href={LOOKML_DOC_LINK} target="_blank" rel="noopener noreferrer">
+                                    DataHub lookml module
+                                </a>
+                                .
+                                <br />
+                                <br />
+                                LookML ingestion <b>cannot</b> currently be performed via UI-based ingestion. This is a
+                                known problem the DataHub team is working to solve!
+                            </>
+                        }
+                        afterClose={() => setShowLookerBanner(false)}
+                    />
+                )}
                 <Typography.Text>
+                    {showLookerBanner && <br />}
                     For more information about how to configure a recipe, see the{' '}
                     <a href={sourceDocumentationUrl} target="_blank" rel="noopener noreferrer">
                         {sourceDisplayName} source docs.
@@ -93,7 +130,7 @@ export const DefineRecipeStep = ({ state, updateState, goTo, prev }: StepProps) 
                 <YamlEditor initialText={displayRecipe} onChange={setStagedRecipeYml} />
             </BorderedSection>
             <ControlsContainer>
-                <Button disabled={prev === undefined} onClick={prev}>
+                <Button disabled={isEditing} onClick={prev}>
                     Previous
                 </Button>
                 <Button disabled={!stepComplete} onClick={onClickNext}>
