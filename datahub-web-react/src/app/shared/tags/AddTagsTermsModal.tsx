4be8eb77bba6397f0d6a4252126f7395d25d68bc
John Joyce <john@acryl.io>
2022-06-10 09:30:31 -0700

    Only show proposals for Dataset

--- datahub-web-react/src/app/shared/tags/AddTagsTermsModal.tsx
+++ datahub-web-react/src/app/shared/tags/AddTagsTermsModal.tsx
@@ -368,13 +368,15 @@ export default function AddTagsTermsModal({
                     <Button onClick={onCloseModal} type="text">
                         Cancel
                     </Button>
-                    <Button
-                        onClick={() => onOkProposal()}
-                        disabled={urns.length === 0 || urns.length > 1 || disableAdd}
-                        data-testid="create-proposal-btn"
-                    >
-                        Propose
-                    </Button>
+                    {entityType === EntityType.Dataset && (
+                        <Button
+                            onClick={() => onOkProposal()}
+                            disabled={urns.length === 0 || urns.length > 1 || disableAdd}
+                            data-testid="create-proposal-btn"
+                        >
+                            Propose
+                        </Button>
+                    )}
                     <Button
                         id="addTagButton"
                         data-testid="add-tag-term-from-modal-btn"
