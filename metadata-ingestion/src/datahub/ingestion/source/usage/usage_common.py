b9259637473aa5267654cec256542e17f30aaab8
treff7es <treff7es@gmail.com>
2022-06-13 17:31:56 +0200

    Black linting

--- metadata-ingestion/src/datahub/ingestion/source/usage/usage_common.py
+++ metadata-ingestion/src/datahub/ingestion/source/usage/usage_common.py
@@ -150,7 +150,8 @@ class BaseUsageConfig(BaseTimeWindowConfig):
     )
 
     include_read_operational_stats: bool = Field(
-        default=False, description="Whether to report read operational stats. Experimental."
+        default=False,
+        description="Whether to report read operational stats. Experimental.",
     )
 
     format_sql_queries: bool = Field(
