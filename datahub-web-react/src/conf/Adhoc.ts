b8c81b2337f1573222f2d9eb99595eca306b0597
xiphl <xipherl9@gmail.com>
2022-06-23 11:32:25 +0800

    Revert "initial commit"
    This reverts commit c5d4aa58a0931a609140017d6a7718d50013e603.
    
    Conflicts:
    	datahub-web-react/src/app/search/SearchBar.tsx
    	datahub-web-react/src/conf/Adhoc.ts

--- datahub-web-react/src/conf/Adhoc.ts
+++ datahub-web-react/src/conf/Adhoc.ts
@@ -3,4 +3,3 @@
 */
 export const config = 'http://localhost:8001/custom/make_dataset';
 export const TemplateDescriptionString = `*About Dataset:*  \n*For Data Access, Contact:*  \n`;
-export const helpRefLink = 'http://www.google.com';
