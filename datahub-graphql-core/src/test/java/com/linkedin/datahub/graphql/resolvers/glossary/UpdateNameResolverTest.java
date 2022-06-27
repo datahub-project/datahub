f9aca8aca226b300d58cc7034f70656d8e81c6b5
Chris Collins <chriscollins3456@gmail.com>
2022-06-27 19:09:36 -0400

    feat(domain) Add ability to edit a Domain name from the UI (#5266)

--- datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/glossary/UpdateNameResolverTest.java
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/glossary/UpdateNameResolverTest.java
@@ -7,6 +7,7 @@ import com.linkedin.common.urn.Urn;
 import com.linkedin.datahub.graphql.QueryContext;
 import com.linkedin.datahub.graphql.generated.UpdateNameInput;
 import com.linkedin.datahub.graphql.resolvers.mutate.UpdateNameResolver;
+import com.linkedin.domain.DomainProperties;
 import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.glossary.GlossaryNodeInfo;
 import com.linkedin.glossary.GlossaryTermInfo;
@@ -29,8 +30,10 @@ public class UpdateNameResolverTest {
   private static final String NEW_NAME = "New Name";
   private static final String TERM_URN = "urn:li:glossaryTerm:11115397daf94708a8822b8106cfd451";
   private static final String NODE_URN = "urn:li:glossaryNode:22225397daf94708a8822b8106cfd451";
+  private static final String DOMAIN_URN = "urn:li:domain:22225397daf94708a8822b8106cfd451";
   private static final UpdateNameInput INPUT = new UpdateNameInput(NEW_NAME, TERM_URN);
   private static final UpdateNameInput INPUT_FOR_NODE = new UpdateNameInput(NEW_NAME, NODE_URN);
+  private static final UpdateNameInput INPUT_FOR_DOMAIN = new UpdateNameInput(NEW_NAME, DOMAIN_URN);
   private static final CorpuserUrn TEST_ACTOR_URN = new CorpuserUrn("test");
 
   private MetadataChangeProposal setupTests(DataFetchingEnvironment mockEnv, EntityService mockService) throws Exception {
@@ -100,6 +103,43 @@ public class UpdateNameResolverTest {
     info.setName(NEW_NAME);
     proposal.setAspectName(Constants.GLOSSARY_NODE_INFO_ASPECT_NAME);
     proposal.setAspect(GenericRecordUtils.serializeAspect(info));
+    proposal.setChangeType(ChangeType.UPSERT);
+
+    UpdateNameResolver resolver = new UpdateNameResolver(mockService);
+
+    assertTrue(resolver.get(mockEnv).get());
+    Mockito.verify(mockService, Mockito.times(1)).ingestProposal(
+        Mockito.eq(proposal),
+        Mockito.any()
+    );
+  }
+
+  @Test
+  public void testGetSuccessForDomain() throws Exception {
+    EntityService mockService = Mockito.mock(EntityService.class);
+    Mockito.when(mockService.exists(Urn.createFromString(DOMAIN_URN))).thenReturn(true);
+    DataFetchingEnvironment mockEnv = Mockito.mock(DataFetchingEnvironment.class);
+    Mockito.when(mockEnv.getArgument("input")).thenReturn(INPUT_FOR_DOMAIN);
+
+    QueryContext mockContext = getMockAllowContext();
+    Mockito.when(mockContext.getAuthentication()).thenReturn(Mockito.mock(Authentication.class));
+    Mockito.when(mockContext.getActorUrn()).thenReturn(TEST_ACTOR_URN.toString());
+    Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
+
+    final String name = "test name";
+    Mockito.when(mockService.getAspect(
+            Urn.createFromString(DOMAIN_URN),
+            Constants.DOMAIN_PROPERTIES_ASPECT_NAME,
+            0))
+        .thenReturn(new DomainProperties().setName(name));
+
+    final MetadataChangeProposal proposal = new MetadataChangeProposal();
+    proposal.setEntityUrn(Urn.createFromString(DOMAIN_URN));
+    proposal.setEntityType(Constants.DOMAIN_ENTITY_NAME);
+    DomainProperties properties = new DomainProperties();
+    properties.setName(NEW_NAME);
+    proposal.setAspectName(Constants.DOMAIN_PROPERTIES_ASPECT_NAME);
+    proposal.setAspect(GenericRecordUtils.serializeAspect(properties));
     proposal.setChangeType(ChangeType.UPSERT);
 
     UpdateNameResolver resolver = new UpdateNameResolver(mockService);
