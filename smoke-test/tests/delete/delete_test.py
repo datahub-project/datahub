62cb181f792bbd8457c3f6b50a04697675330fa3
Pedro Silva <pedro.cls93@gmail.com>
2022-07-01 23:41:34 +0100

    Increase sleeps for references test

--- smoke-test/tests/delete/delete_test.py
+++ smoke-test/tests/delete/delete_test.py
@@ -40,7 +40,7 @@ def test_setup():
 
     ingested_dataset_run_id = ingest_file_via_rest("tests/delete/cli_test_data.json").config.run_id
 
-    sleep(2)
+    sleep(3)
 
     assert "browsePaths" in get_aspects_for_entity(entity_urn=dataset_urn, aspects=["browsePaths"], typed=False)
 
@@ -48,7 +48,7 @@ def test_setup():
     rollback_url = f"{gms_host}/runs?action=rollback"
     session.post(rollback_url, data=json.dumps({"runId": ingested_dataset_run_id, "dryRun": False, "hardDelete": True, "safe": False}))
 
-    sleep(2)
+    sleep(3)
 
     assert "browsePaths" not in get_aspects_for_entity(entity_urn=dataset_urn, aspects=["browsePaths"], typed=False)
     assert "editableDatasetProperties" not in get_aspects_for_entity(entity_urn=dataset_urn, aspects=["editableDatasetProperties"], typed=False)
@@ -74,7 +74,7 @@ def test_delete_reference(depends=["test_healthchecks"]):
     # Delete references to the tag
     delete_references(tag_urn, dry_run=False, cached_session_host=(session, gms_host))
 
-    sleep(2)
+    sleep(3)
 
     # Validate that references no longer exist
     references_count, related_aspects = delete_references(tag_urn, dry_run=True, cached_session_host=(session, gms_host))
