5aaebbbf086793a25effa0eabc029f54cb96f641
Aseem Bansal <asmbansal2@gmail.com>
2022-07-12 22:51:19 +0530

    fix login test

--- smoke-test/tests/cypress/cypress/integration/login/login.js
+++ smoke-test/tests/cypress/cypress/integration/login/login.js
@@ -4,6 +4,6 @@ describe('login', () => {
     cy.get('input[data-testid=username]').type('datahub');
     cy.get('input[data-testid=password]').type('datahub');
     cy.contains('Sign In').click();
-    cy.contains('Welcome back, Data Hub');
+    cy.contains('Welcome back, DataHub');
   });
 })
