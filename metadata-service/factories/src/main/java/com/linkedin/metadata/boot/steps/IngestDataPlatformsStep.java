3a2fec341aa2dac2d59eb51290a4f3e5756ca092
Aditya Radhakrishnan <aditya@acryl.io>
2022-07-12 17:23:41 -0700

    fix(bootstrap): skip ingesting data platforms that already exist (#5382)

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/steps/IngestDataPlatformsStep.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/steps/IngestDataPlatformsStep.java
@@ -1,5 +1,6 @@
 package com.linkedin.metadata.boot.steps;
 
+import com.datahub.util.RecordUtils;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.linkedin.common.AuditStamp;
@@ -7,12 +8,9 @@ import com.linkedin.common.urn.Urn;
 import com.linkedin.dataplatform.DataPlatformInfo;
 import com.linkedin.metadata.Constants;
 import com.linkedin.metadata.boot.BootstrapStep;
-import com.datahub.util.RecordUtils;
 import com.linkedin.metadata.entity.EntityService;
-
 import java.io.IOException;
 import java.net.URISyntaxException;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
@@ -46,13 +44,24 @@ public class IngestDataPlatformsStep implements BootstrapStep {
 
     // 2. For each JSON object, cast into a DataPlatformSnapshot object.
     for (final JsonNode dataPlatform : dataPlatforms) {
+      final String urnString;
       final Urn urn;
       try {
-        urn = Urn.createFromString(dataPlatform.get("urn").asText());
+        urnString = dataPlatform.get("urn").asText();
+        urn = Urn.createFromString(urnString);
       } catch (URISyntaxException e) {
         log.error("Malformed urn: {}", dataPlatform.get("urn").asText());
         throw new RuntimeException("Malformed urn", e);
       }
+
+      final DataPlatformInfo existingInfo =
+          (DataPlatformInfo) _entityService.getLatestAspect(urn, PLATFORM_ASPECT_NAME);
+      // Skip ingesting for this JSON object if info already exists.
+      if (existingInfo != null) {
+        log.debug(String.format("%s already exists for %s. Skipping...", PLATFORM_ASPECT_NAME, urnString));
+        continue;
+      }
+
       final DataPlatformInfo info =
           RecordUtils.toRecordTemplate(DataPlatformInfo.class, dataPlatform.get("aspect").toString());
 
