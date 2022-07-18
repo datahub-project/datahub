1a3c40d4b92168a6e96ff9fd5c9c8d1a482ae2db
John Joyce <john@acryl.io>
2022-07-18 09:29:19 -0700

    Set a maximum on number of top users / owners displayed in search preview

--- datahub-web-react/src/app/entity/shared/components/styled/ExpandedActorGroup.tsx
+++ datahub-web-react/src/app/entity/shared/components/styled/ExpandedActorGroup.tsx
@@ -1,18 +1,43 @@
+import { Popover, Typography } from 'antd';
 import React from 'react';
+import styled from 'styled-components';
 import { CorpGroup, CorpUser } from '../../../../../types.generated';
 import { ExpandedActor } from './ExpandedActor';
 
+const PopoverActors = styled.div`
+    max-width: 260px;
+`;
+
 type Props = {
     actors: Array<CorpUser | CorpGroup>;
+    max?: number | null;
     onClose?: (actor: CorpUser | CorpGroup) => void;
 };
 
-export const ExpandedActorGroup = ({ actors, onClose }: Props) => {
+const DEFAULT_MAX = 10;
+
+export const ExpandedActorGroup = ({ actors, max, onClose }: Props) => {
+    const finalMax = max || DEFAULT_MAX;
+    const finalActors = actors.length > finalMax ? actors.slice(0, finalMax) : actors;
+    const remainder = actors.length > finalMax ? actors.length - finalMax : undefined;
+
     return (
         <>
-            {actors.map((actor) => (
-                <ExpandedActor key={actor.urn} actor={actor} onClose={() => onClose?.(actor)} />
-            ))}
+            <Popover
+                placement="left"
+                content={
+                    <PopoverActors>
+                        {actors.map((actor) => (
+                            <ExpandedActor key={actor.urn} actor={actor} onClose={() => onClose?.(actor)} />
+                        ))}
+                    </PopoverActors>
+                }
+            >
+                {finalActors.map((actor) => (
+                    <ExpandedActor key={actor.urn} actor={actor} onClose={() => onClose?.(actor)} />
+                ))}
+                {remainder && <Typography.Text type="secondary">+ {remainder} more</Typography.Text>}
+            </Popover>
         </>
     );
 };
