f18313ec4ba1a07d65d2ffeff754ff7dac5e95ab
Chris Collins <chriscollins@Chriss-MBP-2.lan>
2022-07-19 16:41:38 -0400

    PR edits

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/ingest/execution/CreateTestConnectionRequestResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/ingest/execution/CreateTestConnectionRequestResolver.java
@@ -16,6 +16,7 @@ import com.linkedin.metadata.utils.GenericRecordUtils;
 import com.linkedin.mxe.MetadataChangeProposal;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -49,42 +50,42 @@ public class CreateTestConnectionRequestResolver implements DataFetcher<Completa
 
     return CompletableFuture.supplyAsync(() -> {
 
-      if (IngestionAuthUtils.canManageIngestion(context)) {
-
-        final CreateTestConnectionRequestInput input =
-            bindArgument(environment.getArgument("input"), CreateTestConnectionRequestInput.class);
-
-        try {
-
-          final MetadataChangeProposal proposal = new MetadataChangeProposal();
-          final ExecutionRequestKey key = new ExecutionRequestKey();
-          final UUID uuid = UUID.randomUUID();
-          final String uuidStr = uuid.toString();
-          key.setId(uuidStr);
-          proposal.setEntityKeyAspect(GenericRecordUtils.serializeAspect(key));
-
-          final ExecutionRequestInput execInput = new ExecutionRequestInput();
-          execInput.setTask(TEST_CONNECTION_TASK_NAME);
-          execInput.setSource(new ExecutionRequestSource().setType(TEST_CONNECTION_SOURCE_NAME));
-          execInput.setExecutorId(DEFAULT_EXECUTOR_ID);
-          execInput.setRequestedAt(System.currentTimeMillis());
-
-          Map<String, String> arguments = new HashMap<>();
-          arguments.put(RECIPE_ARG_NAME, input.getRecipe());
-          arguments.put(VERSION_ARG_NAME, _ingestionConfiguration.getDefaultCliVersion());
-          execInput.setArgs(new StringMap(arguments));
-
-          proposal.setEntityType(Constants.EXECUTION_REQUEST_ENTITY_NAME);
-          proposal.setAspectName(Constants.EXECUTION_REQUEST_INPUT_ASPECT_NAME);
-          proposal.setAspect(GenericRecordUtils.serializeAspect(execInput));
-          proposal.setChangeType(ChangeType.UPSERT);
-
-          return _entityClient.ingestProposal(proposal, context.getAuthentication());
-        } catch (Exception e) {
-          throw new RuntimeException(String.format("Failed to create new test ingestion connection request %s", input.toString()), e);
-        }
+      if (!IngestionAuthUtils.canManageIngestion(context)) {
+        throw new AuthorizationException("Unauthorized to perform this action. Please contact your DataHub administrator.");
       }
-      throw new AuthorizationException("Unauthorized to perform this action. Please contact your DataHub administrator.");
+
+      final CreateTestConnectionRequestInput input =
+          bindArgument(environment.getArgument("input"), CreateTestConnectionRequestInput.class);
+
+      try {
+
+        final MetadataChangeProposal proposal = new MetadataChangeProposal();
+        final ExecutionRequestKey key = new ExecutionRequestKey();
+        final UUID uuid = UUID.randomUUID();
+        final String uuidStr = uuid.toString();
+        key.setId(uuidStr);
+        proposal.setEntityKeyAspect(GenericRecordUtils.serializeAspect(key));
+
+        final ExecutionRequestInput execInput = new ExecutionRequestInput();
+        execInput.setTask(TEST_CONNECTION_TASK_NAME);
+        execInput.setSource(new ExecutionRequestSource().setType(TEST_CONNECTION_SOURCE_NAME));
+        execInput.setExecutorId(DEFAULT_EXECUTOR_ID);
+        execInput.setRequestedAt(System.currentTimeMillis());
+
+        Map<String, String> arguments = new HashMap<>();
+        arguments.put(RECIPE_ARG_NAME, input.getRecipe());
+        arguments.put(VERSION_ARG_NAME, _ingestionConfiguration.getDefaultCliVersion());
+        execInput.setArgs(new StringMap(arguments));
+
+        proposal.setEntityType(Constants.EXECUTION_REQUEST_ENTITY_NAME);
+        proposal.setAspectName(Constants.EXECUTION_REQUEST_INPUT_ASPECT_NAME);
+        proposal.setAspect(GenericRecordUtils.serializeAspect(execInput));
+        proposal.setChangeType(ChangeType.UPSERT);
+
+        return _entityClient.ingestProposal(proposal, context.getAuthentication());
+      } catch (Exception e) {
+        throw new RuntimeException(String.format("Failed to create new test ingestion connection request %s", input.toString()), e);
+      }
     });
   }
 }
