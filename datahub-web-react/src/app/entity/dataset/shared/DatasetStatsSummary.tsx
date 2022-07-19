41d2fcffdd4151b5dadda0573810e69df46fa46c
John Joyce <john@acryl.io>
2022-07-19 14:48:31 -0700

    Adding untrackd files

---
+++ datahub-web-react/src/app/entity/dataset/shared/DatasetStatsSummary.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Popover } from 'antd';
+import { ClockCircleOutlined, ConsoleSqlOutlined, TableOutlined, TeamOutlined } from '@ant-design/icons';
+import { formatNumberWithoutAbbreviation } from '../../../shared/formatNumber';
+import { ANTD_GRAY } from '../../shared/constants';
+import { toLocalDateTimeString, toRelativeTimeString } from '../../../shared/time/timeUtils';
+import { StatsSummary } from '../../shared/components/styled/StatsSummary';
+
+const StatText = styled.span`
+    color: ${ANTD_GRAY[8]};
+`;
+
+type Props = {
+    rowCount?: number | null;
+    queryCountLast30Days?: number | null;
+    uniqueUserCountLast30Days?: number | null;
+    lastUpdatedMs?: number | null;
+};
+
+export const DatasetStatsSummary = ({
+    rowCount,
+    queryCountLast30Days,
+    uniqueUserCountLast30Days,
+    lastUpdatedMs,
+}: Props) => {
+    const statsViews = [
+        (!!rowCount && (
+            <StatText>
+                <TableOutlined style={{ marginRight: 8, color: ANTD_GRAY[7] }} />
+                <b>{formatNumberWithoutAbbreviation(rowCount)}</b> rows
+            </StatText>
+        )) ||
+            undefined,
+        (!!queryCountLast30Days && (
+            <StatText>
+                <ConsoleSqlOutlined style={{ marginRight: 8, color: ANTD_GRAY[7] }} />
+                <b>{formatNumberWithoutAbbreviation(queryCountLast30Days)}</b> queries last month
+            </StatText>
+        )) ||
+            undefined,
+        (!!uniqueUserCountLast30Days && (
+            <StatText>
+                <TeamOutlined style={{ marginRight: 8, color: ANTD_GRAY[7] }} />
+                <b>{formatNumberWithoutAbbreviation(uniqueUserCountLast30Days)}</b> unique users
+            </StatText>
+        )) ||
+            undefined,
+        (!!lastUpdatedMs && (
+            <Popover content={<div>Changed on {toLocalDateTimeString(lastUpdatedMs)}.</div>}>
+                <StatText>
+                    <ClockCircleOutlined style={{ marginRight: 8, color: ANTD_GRAY[7] }} />
+                    Changed {toRelativeTimeString(lastUpdatedMs)}
+                </StatText>
+            </Popover>
+        )) ||
+            undefined,
+    ].filter((stat) => stat !== undefined);
+
+    return <>{statsViews.length > 0 && <StatsSummary stats={statsViews} />}</>;
+};
