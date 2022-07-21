d28e18147c7604491080fbb5dd8c6b9a3e6f5205
Aditya Radhakrishnan <aditya@acryl.io>
2022-07-21 08:58:48 -0700

    feat(app): schema-history - remove blame language for the schema history feature (#5457)

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/timeline/GetSchemaVersionListResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/timeline/GetSchemaVersionListResolver.java
@@ -1,9 +1,9 @@
 package com.linkedin.datahub.graphql.resolvers.timeline;
 
 import com.linkedin.common.urn.Urn;
-import com.linkedin.datahub.graphql.generated.GetSchemaBlameInput;
-import com.linkedin.datahub.graphql.generated.GetSchemaBlameResult;
-import com.linkedin.datahub.graphql.types.timeline.mappers.SchemaBlameMapper;
+import com.linkedin.datahub.graphql.generated.GetSchemaVersionListInput;
+import com.linkedin.datahub.graphql.generated.GetSchemaVersionListResult;
+import com.linkedin.datahub.graphql.types.timeline.mappers.SchemaVersionListMapper;
 import com.linkedin.metadata.timeline.TimelineService;
 import com.linkedin.metadata.timeline.data.ChangeCategory;
 import com.linkedin.metadata.timeline.data.ChangeTransaction;
@@ -23,21 +23,21 @@ import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.*;
 Returns the most recent changes made to each column in a dataset at each dataset version.
  */
 @Slf4j
-public class GetSchemaBlameResolver implements DataFetcher<CompletableFuture<GetSchemaBlameResult>> {
+public class GetSchemaVersionListResolver implements DataFetcher<CompletableFuture<GetSchemaVersionListResult>> {
   private final TimelineService _timelineService;
 
-  public GetSchemaBlameResolver(TimelineService timelineService) {
+  public GetSchemaVersionListResolver(TimelineService timelineService) {
     _timelineService = timelineService;
   }
 
   @Override
-  public CompletableFuture<GetSchemaBlameResult> get(final DataFetchingEnvironment environment) throws Exception {
-    final GetSchemaBlameInput input = bindArgument(environment.getArgument("input"), GetSchemaBlameInput.class);
+  public CompletableFuture<GetSchemaVersionListResult> get(final DataFetchingEnvironment environment) throws Exception {
+    final GetSchemaVersionListInput input =
+        bindArgument(environment.getArgument("input"), GetSchemaVersionListInput.class);
 
     final String datasetUrnString = input.getDatasetUrn();
     final long startTime = 0;
     final long endTime = 0;
-    final String version = input.getVersion() == null ? null : input.getVersion();
 
     return CompletableFuture.supplyAsync(() -> {
       try {
@@ -46,7 +46,7 @@ public class GetSchemaBlameResolver implements DataFetcher<CompletableFuture<Get
         Urn datasetUrn = Urn.createFromString(datasetUrnString);
         List<ChangeTransaction> changeTransactionList =
             _timelineService.getTimeline(datasetUrn, changeCategorySet, startTime, endTime, null, null, false);
-        return SchemaBlameMapper.map(changeTransactionList, version);
+        return SchemaVersionListMapper.map(changeTransactionList);
       } catch (URISyntaxException u) {
         log.error(
             String.format("Failed to list schema blame data, likely due to the Urn %s being invalid", datasetUrnString),
