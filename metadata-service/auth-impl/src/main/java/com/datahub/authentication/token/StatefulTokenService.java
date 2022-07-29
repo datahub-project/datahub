bbf5a796a08114a4d1baba4d9b1c48958429f217
ksrinath <k.srinath@gmail.com>
2022-07-29 16:46:36 +0530

    feat(metadata-service-auth): remove unused imports

--- metadata-service/auth-impl/src/main/java/com/datahub/authentication/token/StatefulTokenService.java
+++ metadata-service/auth-impl/src/main/java/com/datahub/authentication/token/StatefulTokenService.java
@@ -22,7 +22,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
