0690f521e8e6805080a8f237985857023e8e4c50
oleks.lytvynov <oleks.lytvynov@temabit.com>
2022-08-09 12:09:26 +0300

    feat: change imports

--- metadata-ingestion/tests/integration/powerbi_report_server/test_powerbi_report_server.py
+++ metadata-ingestion/tests/integration/powerbi_report_server/test_powerbi_report_server.py
@@ -1,102 +1,203 @@
 from datetime import datetime
 from unittest import mock
-from unittest.mock import patch
 
 from freezegun import freeze_time
 
 from datahub.ingestion.run.pipeline import Pipeline
-from datahub.ingestion.source.powerbi_report_server import (
-    CorpUser,
-    PowerBiReport,
-)
+from datahub.ingestion.source.powerbi_report_server import Constant, CorpUser
 from tests.test_helpers import mce_helpers
 
-FROZEN_TIME = "2022-07-08 07:00:00"
-
-POWERBI_REPORT_SERVER_API = "datahub.ingestion.source.powerbi_report_server.report_server.PowerBiReportServerAPI"
+FROZEN_TIME = "2022-02-03 07:00:00"
 POWERBI_REPORT_SERVER_USER_DAO_GET_OWNER = "datahub.ingestion.source.powerbi_report_server.report_server.UserDao.get_owner_by_name"
 
 
+def mock_user(*args, **kwargs):
+    user_data = dict(
+        urn=f"urn:li:corpuser:User1",
+        type=Constant.CORP_USER,
+        username="User1",
+        properties=dict(active=True, displayName="User1", email="User1@foo.com"),
+    )
+    return CorpUser.parse_obj(user_data)
+
+
+def register_mock_api(request_mock):
+    api_vs_response = {
+        "https://host_port/Reports/api/v2.0/Reports": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "Id": "ee56dc21-248a-4138-a446-ee5ab1fc938a",
+                        "Name": "Testa",
+                        "Description": None,
+                        "Path": "/path/to/Testa",
+                        "Type": "Report",
+                        "Hidden": False,
+                        "Size": 1010101,
+                        "ModifiedBy": "TEST_USER",
+                        "ModifiedDate": str(datetime.now()),
+                        "CreatedBy": "TEST_USER",
+                        "CreatedDate": str(datetime.now()),
+                        "ParentFolderId": "47495172-89ab-455f-a446-fffd3cf239ca",
+                        "IsFavorite": False,
+                        "ContentType": None,
+                        "Content": "",
+                        "HasDataSources": True,
+                        "Roles": [],
+                        "HasSharedDataSets": True,
+                        "HasParameters": True,
+                    },
+                ]
+            },
+        },
+        "https://host_port/Reports/api/v2.0/MobileReports": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "Id": "ee56dc21-248a-4138-a446-ee5ab1fc938b",
+                        "Name": "Testb",
+                        "Description": None,
+                        "Path": "/path/to/Testb",
+                        "Type": "MobileReport",
+                        "Hidden": False,
+                        "Size": 1010101,
+                        "ModifiedBy": "TEST_USER",
+                        "ModifiedDate": str(datetime.now()),
+                        "CreatedBy": "TEST_USER",
+                        "CreatedDate": str(datetime.now()),
+                        "ParentFolderId": "47495172-89ab-455f-a446-fffd3cf239cb",
+                        "IsFavorite": False,
+                        "ContentType": None,
+                        "Content": "",
+                        "HasDataSources": True,
+                        "Roles": [],
+                        "HasSharedDataSets": True,
+                        "HasParameters": True,
+                        "AllowCaching": True,
+                        "Manifest": {"Resources": []},
+                    },
+                ]
+            },
+        },
+        "https://host_port/Reports/api/v2.0/LinkedReports": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "Id": "ee56dc21-248a-4138-a446-ee5ab1fc938c",
+                        "Name": "Testc",
+                        "Description": None,
+                        "Path": "/path/to/Testc",
+                        "Type": "LinkedReport",
+                        "Hidden": False,
+                        "Size": 1010101,
+                        "ModifiedBy": "TEST_USER",
+                        "ModifiedDate": str(datetime.now()),
+                        "CreatedBy": "TEST_USER",
+                        "CreatedDate": str(datetime.now()),
+                        "ParentFolderId": "47495172-89ab-455f-a446-fffd3cf239cc",
+                        "IsFavorite": False,
+                        "ContentType": None,
+                        "Content": "",
+                        "HasDataSources": True,
+                        "Roles": [],
+                        "HasParameters": True,
+                        "Link": "sjfgnk-7134-1234-abcd-ee5axvcv938b",
+                    },
+                ]
+            },
+        },
+        "https://host_port/Reports/api/v2.0/PowerBIReports": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "Id": "ee56dc21-248a-4138-a446-ee5ab1fc938d",
+                        "Name": "Testd",
+                        "Description": None,
+                        "Path": "/path/to/Testd",
+                        "Type": "PowerBIReport",
+                        "Hidden": False,
+                        "Size": 1010101,
+                        "ModifiedBy": "TEST_USER",
+                        "ModifiedDate": str(datetime.now()),
+                        "CreatedBy": "TEST_USER",
+                        "CreatedDate": str(datetime.now()),
+                        "ParentFolderId": "47495172-89ab-455f-a446-fffd3cf239cd",
+                        "IsFavorite": False,
+                        "ContentType": None,
+                        "Content": "",
+                        "HasDataSources": True,
+                        "Roles": [],
+                    },
+                ]
+            },
+        },
+    }
+
+    for url in api_vs_response.keys():
+        request_mock.register_uri(
+            api_vs_response[url]["method"],
+            url,
+            json=api_vs_response[url]["json"],
+            status_code=api_vs_response[url]["status_code"],
+        )
+
+
+def default_source_config():
+    return {
+        "username": "foo",
+        "password": "bar",
+        "workstation_name": "workstation",
+        "host_port": "host_port",
+        "server_alias": "server_alias",
+        "graphql_url": "http://localhost:8080/api/graphql",
+        "report_virtual_directory_name": "Reports",
+        "report_server_virtual_directory_name": "ReportServer",
+        "env": "DEV",
+    }
+
+
 @freeze_time(FROZEN_TIME)
-@patch(POWERBI_REPORT_SERVER_USER_DAO_GET_OWNER)
-def test_powerbi_report_server_ingest(pytestconfig, tmp_path, mock_time):
-    mocked_client = mock.MagicMock()
-    # Set the datasource mapping dictionary
-    with mock.patch(POWERBI_REPORT_SERVER_API) as mock_sdk:
-        mock_sdk.return_value = mocked_client
-        # Mock the PowerBi Dashboard API response
-        mocked_client.get_all_reports.return_value = [
-            PowerBiReport(
-                Id="ee56dc21-248a-4138-a446-ee5ab1fc938b",
-                Name="Test",
-                Description=None,
-                Path="/path/to/Test",
-                Type="PowerBIReport",
-                Hidden=False,
-                Size=1010101,
-                ModifiedBy="TEST_USER",
-                ModifiedDate=datetime.now(),
-                CreatedBy="TEST_USER",
-                CreatedDate=datetime.now(),
-                ParentFolderId="47495172-89ab-455f-a446-fffd3cf239cb",
-                ContentType=None,
-                Content="",
-                IsFavorite=False,
-                UserInfo=CorpUser(
-                    active=True,
-                    display_name="User1",
-                    email="User1@foo.com",
-                    title=None,
-                    manager=None,
-                    department_id=None,
-                    department_name=None,
-                    first_name=None,
-                    last_name=None,
-                    full_name=None,
-                    country_code=None,
-                ),
-                DisplayName="TEST_USER",
-                HasDataSources=True,
-                DataSources=[],
-            ),
-        ]
+@mock.patch(POWERBI_REPORT_SERVER_USER_DAO_GET_OWNER, side_effect=mock_user)
+@mock.patch("requests_ntlm.HttpNtlmAuth")
+def test_powerbi_ingest(
+    mock_msal, user_mock, pytestconfig, tmp_path, mock_time, requests_mock
+):
+    test_resources_dir = pytestconfig.rootpath / "tests/integration/powerbi"
 
-        test_resources_dir = (
-            pytestconfig.rootpath / "tests/integration/powerbi_report_server"
-        )
+    register_mock_api(request_mock=requests_mock)
 
-        pipeline = Pipeline.create(
-            {
-                "run_id": "powerbi-report-server-test",
-                "source": {
-                    "type": "powerbi-report-server",
-                    "config": {
-                        "username": "foo",
-                        "password": "bar",
-                        "workstation_name": "workstation",
-                        "host_port": "host_port",
-                        "server_alias": "server_alias",
-                        "graphql_url": "http://localhost:8080/api/graphql",
-                        "report_virtual_directory_name": "Reports",
-                        "report_server_virtual_directory_name": "ReportServer",
-                        "env": "DEV",
-                    },
+    pipeline = Pipeline.create(
+        {
+            "run_id": "powerbi-report-server-test",
+            "source": {
+                "type": "powerbi-report-server",
+                "config": {
+                    **default_source_config(),
                 },
-                "sink": {
-                    "type": "file",
-                    "config": {
-                        "filename": f"{tmp_path}/powerbi_report_server_mces.json",
-                    },
+            },
+            "sink": {
+                "type": "file",
+                "config": {
+                    "filename": f"{tmp_path}/powerbi_report_server_mces.json",
                 },
-            }
-        )
+            },
+        }
+    )
 
-        pipeline.run()
-        pipeline.raise_from_status()
-        mce_out_file = "golden_test_ingest.json"
+    pipeline.run()
+    pipeline.raise_from_status()
+    mce_out_file = "../powerbi/golden_test_ingest.json"
 
-        mce_helpers.check_golden_file(
-            pytestconfig,
-            output_path=tmp_path / "powerbi_report_server_mces.json",
-            golden_path=f"{test_resources_dir}/{mce_out_file}",
-        )
+    mce_helpers.check_golden_file(
+        pytestconfig,
+        output_path=tmp_path / "powerbi_report_server_mces.json",
+        golden_path=f"{test_resources_dir}/{mce_out_file}",
+    )
