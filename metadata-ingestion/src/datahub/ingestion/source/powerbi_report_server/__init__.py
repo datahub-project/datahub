74fca17bcc291ede44314e62985f57122ef0c839
oleks.lytvynov <oleks.lytvynov@temabit.com>
2022-08-09 11:00:26 +0300

    feat: change improts

--- metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/__init__.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/__init__.py
@@ -2,7 +2,8 @@ from datahub.ingestion.source.powerbi_report_server.report_server import (
     PowerBiReportServerDashboardSource,
 )
 from datahub.ingestion.source.powerbi_report_server.report_server_domain import (
-    CorpUser,
     PowerBiReport,
     Report,
 )
+from datahub.ingestion.source.powerbi_report_server.graphql_domain import CorpUser
+from datahub.ingestion.source.powerbi_report_server.constants import Constant
