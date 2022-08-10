be3d34012277ebfda1b3de08d915df5e114574c1
oleks.lytvynov <oleks.lytvynov@temabit.com>
2022-08-10 14:35:26 +0300

    fix: remove duplicate doc

--- metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/report_server.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/report_server.py
@@ -520,12 +520,6 @@ class PowerBiReportServerDashboardSource(Source):
        - Paginated report(.rdl)
        - Mobile report
        - Linked report
-
-    ## Configuration Notes
-
-    See the
-    1. [Microsoft Grant user access to a Report Server doc](https://docs.microsoft.com/en-us/sql/reporting-services/security/grant-user-access-to-a-report-server?view=sql-server-ver16)
-    2. Use your user credentials from previous step in yaml file
     """
 
     source_config: PowerBiReportServerDashboardSourceConfig
