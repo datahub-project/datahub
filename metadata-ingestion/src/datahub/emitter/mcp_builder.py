8fb14b403911af9bb11751572e4ae9dad0a5a119
treff7es <treff7es@gmail.com>
2022-08-25 17:37:07 +0200

    Removing unused import

--- metadata-ingestion/src/datahub/emitter/mcp_builder.py
+++ metadata-ingestion/src/datahub/emitter/mcp_builder.py
@@ -7,7 +7,6 @@ from pydantic.main import BaseModel
 
 from datahub.emitter.mce_builder import make_container_urn, make_data_platform_urn
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
-from datahub.ingestion.api.source import SourceReport
 from datahub.ingestion.api.workunit import MetadataWorkUnit
 from datahub.metadata.com.linkedin.pegasus2avro.common import DataPlatformInstance
 from datahub.metadata.com.linkedin.pegasus2avro.container import ContainerProperties
