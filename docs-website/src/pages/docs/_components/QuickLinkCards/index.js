f19de96e89439b100df494d366e814b10b698820
Maggie Hays <maggiem.hays@gmail.com>
2022-08-26 11:02:26 -0500

    lint cleanup

--- docs-website/src/pages/docs/_components/QuickLinkCards/index.js
+++ docs-website/src/pages/docs/_components/QuickLinkCards/index.js
@@ -20,7 +20,7 @@ const quickLinkContent = [
     title: "Get Started",
     icon: <ThunderboltTwoTone />,
     description: "Details on how to get DataHub up and running",
-    to: "/docs/quickstart",
+    to: "/docs/wip/get-started",
   },
   {
     title: "Ingest Metadata",
