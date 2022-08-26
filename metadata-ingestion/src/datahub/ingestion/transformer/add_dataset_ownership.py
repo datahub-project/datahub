93b4391eabb0d24d84528b853430e1da72c7ab3f
Harshal Sheth <hsheth2@gmail.com>
2022-08-25 23:11:10 -0700

    remove import

--- metadata-ingestion/src/datahub/ingestion/transformer/add_dataset_ownership.py
+++ metadata-ingestion/src/datahub/ingestion/transformer/add_dataset_ownership.py
@@ -1,7 +1,5 @@
 from typing import Callable, List, Optional, Union
 
-from pydantic import validator
-
 import datahub.emitter.mce_builder as builder
 from datahub.configuration.common import (
     ConfigModel,
