3eb0a201bbf0d0d3ae0b71501b93eb2336d925ea
Harshal Sheth <hsheth2@gmail.com>
2022-08-25 22:56:42 -0700

    add test for config enum

---
+++ metadata-ingestion/tests/unit/config/test_config_enum.py
@@ -0,0 +1,29 @@
+import enum
+
+import pydantic
+import pytest
+
+from datahub.configuration.common import ConfigEnum, ConfigModel
+
+
+def test_config_enum():
+    class Fruit(ConfigEnum):
+        APPLE = enum.auto()
+        PEAR = enum.auto()
+        ORANGE = "ORANGE"
+
+    class FruitConfig(ConfigModel):
+        fruit: Fruit = Fruit.APPLE
+
+    # Test auto-generated and manual enum values.
+    assert Fruit.APPLE.value == "APPLE"
+    assert Fruit.ORANGE.value == "ORANGE"
+
+    # Check that config loading works.
+    assert FruitConfig.parse_obj({"fruit": "APPLE"}).fruit == Fruit.APPLE
+    assert FruitConfig.parse_obj({"fruit": "apple"}).fruit == Fruit.APPLE
+    assert FruitConfig.parse_obj({"fruit": "Orange"}).fruit == Fruit.ORANGE
+
+    # Check that errors are thrown.
+    with pytest.raises(pydantic.ValidationError):
+        FruitConfig.parse_obj({"fruit": "banana"})
