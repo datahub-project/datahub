1eca8a4f5859b8cda7584c05cc85fef7afc9e484
Aseem Bansal <asmbansal2@gmail.com>
2022-08-28 20:25:45 +0530

    revert change on local

--- docker/datahub-upgrade/datahub-upgrade.sh
+++ docker/datahub-upgrade/datahub-upgrade.sh
@@ -2,5 +2,4 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 IMAGE=acryldata/datahub-upgrade:head
-docker build -t ${IMAGE} -f docker/datahub-upgrade/Dockerfile .
-cd $DIR && docker run --env-file ./env/docker.env --network="datahub_network" ${IMAGE} "$@"
+cd $DIR && docker pull ${IMAGE} && docker run --env-file ./env/docker.env --network="datahub_network" ${IMAGE} "$@"
