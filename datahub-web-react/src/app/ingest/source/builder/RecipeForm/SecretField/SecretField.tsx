907ff9985e37b081e365d57a4ac869b379bc525c
Ankit Keshari <ankit.keshari@gslab.com>
2022-08-29 14:39:40 +0530

    Worked on the CSS issue and remove react Fragment

--- datahub-web-react/src/app/ingest/source/builder/RecipeForm/SecretField/SecretField.tsx
+++ datahub-web-react/src/app/ingest/source/builder/RecipeForm/SecretField/SecretField.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import { Secret } from '../../../../../../types.generated';
 import CreateSecretButton from './CreateSecretButton';
 import { RecipeField } from '../common';
+import { ANTD_GRAY } from '../../../../../entity/shared/constants';
 
 const StyledDivider = styled(Divider)`
     margin: 0;
@@ -17,7 +18,6 @@ export const StyledFormItem = styled(Form.Item)<{ alignLeft?: boolean; removeMar
         `
         .ant-form-item {
             flex-direction: row;
-
         }
 
         .ant-form-item-label {
@@ -34,32 +34,48 @@ interface SecretFieldProps {
     refetchSecrets: () => void;
 }
 
+function getTooltip() {
+    return (
+        <div>
+            <p>
+                This field requires you to use a DataHub Secret. For more information on Secrets in DataHub, please
+                review{' '}
+                <a
+                    href="https://datahubproject.io/docs/ui-ingestion/#creating-a-secret"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    the docs
+                </a>
+            </p>
+        </div>
+    );
+}
+
 function SecretField({ field, secrets, removeMargin, refetchSecrets }: SecretFieldProps) {
     return (
-        <>
-            <StyledFormItem name={field.name} label={field.label} tooltip={field.tooltip} removeMargin={!!removeMargin}>
-                <Tooltip title="This field requires you to use a DataHub Secret. For more information on Secrets in DataHub, please review the docs.">
-                    Secret Field
-                </Tooltip>
-                <Select
-                    showSearch
-                    filterOption={(input, option) => !!option?.children.toLowerCase().includes(input.toLowerCase())}
-                    dropdownRender={(menu) => (
-                        <>
-                            {menu}
-                            <StyledDivider />
-                            <CreateSecretButton refetchSecrets={refetchSecrets} />
-                        </>
-                    )}
-                >
-                    {secrets.map((secret) => (
-                        <Select.Option key={secret.urn} value={`\${${secret.name}}`}>
-                            {secret.name}
-                        </Select.Option>
-                    ))}
-                </Select>
-            </StyledFormItem>
-        </>
+        <StyledFormItem name={field.name} label={field.label} tooltip={field.tooltip} removeMargin={!!removeMargin}>
+            <Tooltip title={getTooltip}>
+                <span style={{ color: ANTD_GRAY[7] }}>Secret Field</span>
+            </Tooltip>
+            <Select
+                showSearch
+                filterOption={(input, option) => !!option?.children.toLowerCase().includes(input.toLowerCase())}
+                dropdownRender={(menu) => (
+                    <>
+                        {menu}
+                        <StyledDivider />
+                        <CreateSecretButton refetchSecrets={refetchSecrets} />
+                    </>
+                )}
+            >
+                {secrets.map((secret) => (
+                    <Select.Option key={secret.urn} value={`\${${secret.name}}`}>
+                        {secret.name}
+                    </Select.Option>
+                ))}
+            </Select>
+        </StyledFormItem>
     );
 }
 
