4a66ce0486689329a7147aeffb840aac074f8f61
Benjamin Marte <benmarte@bmv-interactive.com>
2022-09-01 12:44:28 -0400

    fixed logout for OIDC and java

--- datahub-web-react/src/app/auth/LogOut.tsx
+++ datahub-web-react/src/app/auth/LogOut.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Redirect } from 'react-router';
+import { useHistory } from 'react-router';
 import Cookies from 'js-cookie';
 import analytics, { EventType } from '../analytics';
 import { isLoggedInVar } from './checkAuthStatus';
 import { GlobalCfg } from '../../conf';
 
 export const LogOut = () => {
-    const handleLogout = () => {
+    const history = useHistory();
+    const handleLogout = async () => {
         analytics.event({ type: EventType.LogOutEvent });
         isLoggedInVar(false);
         Cookies.remove(GlobalCfg.CLIENT_AUTH_COOKIE);
+        const logoutBackend = await fetch('/logOut');
+        if (logoutBackend?.status === 200) {
+            history.push('/');
+        }
     };
 
     handleLogout();
 
-    return <Redirect to="/" />;
+    return <></>;
 };
