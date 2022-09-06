e7140fd3fe240186bdc7a5c0772d942e8959b5bf
szalai1 <szalaipeti.vagyok@gmail.com>
2022-09-06 08:55:21 +0200

    fix(frontend): docker indents
    (cherry picked from commit 9a46be46680cc98f43561dc2eab172e50b01b6b1)

--- docker/datahub-frontend/start.sh
+++ docker/datahub-frontend/start.sh
@@ -3,22 +3,25 @@ set -u
 
 PROMETHEUS_AGENT=""
 if [[ ${ENABLE_PROMETHEUS:-false} == true ]]; then
-  PROMETHEUS_AGENT="-javaagent:/jmx_prometheus_javaagent.jar=4318:/datahub-frontend/scripts/prometheus-config.yaml "
+  PROMETHEUS_AGENT="-javaagent:jmx_prometheus_javaagent.jar=4318:/datahub-frontend/client-prometheus-config.yaml"
 fi
 
 OTEL_AGENT=""
 if [[ ${ENABLE_OTEL:-false} == true ]]; then
-  OTEL_AGENT="-javaagent:/opentelemetry-javaagent-all.jar "
+  OTEL_AGENT="-javaagent:/opentelemetry-javaagent-all.jar"
 fi
 
-export JAVA_OPTS=" -Xms512m \
+# make sure there is no whitespace at the beginning and the end of 
+# this string
+export JAVA_OPTS="-Xms512m \
    -Xmx1024m \
    -Dhttp.port=$SERVER_PORT \
    -Dconfig.file=datahub-frontend/conf/application.conf \
    -Djava.security.auth.login.config=datahub-frontend/conf/jaas.conf \
    -Dlogback.configurationFile=datahub-frontend/conf/logback.xml \
    -Dlogback.debug=false \
-   -Dpidfile.path=/dev/null \ ${PROMETHEUS_AGENT:-} ${OTEL_AGENT:-} "
+   ${PROMETHEUS_AGENT:-} ${OTEL_AGENT:-}
+   -Dpidfile.path=/dev/null"
 
 exec ./datahub-frontend/bin/datahub-frontend
 
