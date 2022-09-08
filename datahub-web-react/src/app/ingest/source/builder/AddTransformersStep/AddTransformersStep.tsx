0e5e918aca71cca9c3d90b9510dc71de43b04290
Chris Collins <chriscollins@Chriss-MBP-2-1153.lan>
2022-09-08 12:18:51 -0400

    set recipe state and get initial state properly

--- datahub-web-react/src/app/ingest/source/builder/AddTransformersStep/AddTransformersStep.tsx
+++ datahub-web-react/src/app/ingest/source/builder/AddTransformersStep/AddTransformersStep.tsx
@@ -1,11 +1,14 @@
 import { PlusOutlined } from '@ant-design/icons';
 import { Button, Typography } from 'antd';
 import React, { useState } from 'react';
+import { set, get } from 'lodash';
+import YAML from 'yamljs';
 import styled from 'styled-components/macro';
 import StepButtons from '../StepButtons';
 import { IngestionSourceBuilderStep } from '../steps';
-import { StepProps, Transformer } from '../types';
+import { SourceBuilderState, StepProps, Transformer } from '../types';
 import TransformerInput from './TransformerInput';
+// import { jsonToYaml } from '../../utils';
 
 const Section = styled.div`
     display: flex;
@@ -23,11 +26,54 @@ const AddTransformerButton = styled(Button)`
     margin: 16px 0;
 `;
 
-export default function AddTransformersStep({ goTo, prev }: StepProps) {
-    const [transformers, setTransformers] = useState<Transformer[]>([]);
+function updateRecipeInState(
+    state: SourceBuilderState,
+    transformers: Transformer[],
+    updateState: (newState: SourceBuilderState) => void,
+) {
+    const recipe = state.config?.recipe || '';
+    const jsonRecipe = YAML.parse(recipe);
+    const jsonTransformers = transformers
+        .filter((t) => t.type)
+        .map((transformer) => {
+            return {
+                type: transformer.type,
+                config: {
+                    urns: transformer.urns,
+                },
+            };
+        });
+    set(jsonRecipe, 'transformers', jsonTransformers);
+    const jsonRecipeString = JSON.stringify(jsonRecipe);
+    const newState = {
+        ...state,
+        config: {
+            ...state.config,
+            recipe: jsonRecipeString,
+        },
+    };
+    updateState(newState);
+}
+
+function getInitialState(state: SourceBuilderState) {
+    const jsonState = YAML.parse(state.config?.recipe || '');
+    const jsonTransformers = get(jsonState, 'transformers') || [];
+    return jsonTransformers.map((t) => {
+        return { type: t.type, urns: t.config.urns || [] };
+    });
+}
+
+export default function AddTransformersStep({ goTo, prev, state, updateState }: StepProps) {
+    const [transformers, setTransformers] = useState<Transformer[]>(getInitialState(state));
 
     function onClickNext() {
+        updateRecipeInState(state, transformers, updateState);
         goTo(IngestionSourceBuilderStep.CREATE_SCHEDULE);
+    }
+
+    function onClickPrevious() {
+        updateRecipeInState(state, transformers, updateState);
+        prev?.();
     }
 
     function addNewTransformer() {
@@ -58,7 +104,7 @@ export default function AddTransformersStep({ goTo, prev }: StepProps) {
             </AddTransformerButton>
             <StepButtons
                 onClickNext={onClickNext}
-                onClickPrevious={prev}
+                onClickPrevious={onClickPrevious}
                 onClickSkip={onClickNext}
                 isNextDisabled={false}
             />
