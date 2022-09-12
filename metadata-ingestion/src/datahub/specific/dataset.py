e5eb98db8ea431268a25dc5bc5a44ac22520520b
Shirshanka Das <shirshanka@apache.org>
2022-09-12 10:28:36 -0700

    fix ownership patch construction

--- metadata-ingestion/src/datahub/specific/dataset.py
+++ metadata-ingestion/src/datahub/specific/dataset.py
@@ -1,10 +1,12 @@
-from typing import List
+from typing import List, Optional
 
 from datahub.emitter.mcp_patch_builder import MetadataPatchProposal
 from datahub.metadata.schema_classes import (
     OwnerClass as Owner,
+    OwnershipTypeClass,
     UpstreamClass as Upstream,
 )
+from datahub.utilities.urns.urn import Urn
 
 
 class DatasetPatchBuilder(MetadataPatchProposal):
@@ -12,12 +14,22 @@ class DatasetPatchBuilder(MetadataPatchProposal):
         super().__init__(urn, "dataset")
 
     def add_owner(self, owner: Owner) -> "DatasetPatchBuilder":
-        self._add_patch("ownership", "add", path=f"/owners/{owner.owner}", value=owner)
+        self._add_patch(
+            "ownership", "add", path=f"/owners/{owner.owner}/{owner.type}", value=owner
+        )
         return self
 
-    def remove_owner(self, owner: Owner) -> "DatasetPatchBuilder":
+    def remove_owner(
+        self, owner: Urn, owner_type: Optional[OwnershipTypeClass] = None
+    ) -> "DatasetPatchBuilder":
+        """
+        param: owner_type is optional
+        """
         self._add_patch(
-            "ownership", "remove", path=f"/owners/{owner.owner}", value=owner
+            "ownership",
+            "remove",
+            path=f"/owners/{owner}" + (f"/{owner_type}" if owner_type else ""),
+            value=owner,
         )
         return self
 
