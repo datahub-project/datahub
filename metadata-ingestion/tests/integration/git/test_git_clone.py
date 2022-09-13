73926fb31a0f60b93ef5639e8a0d1caac8aecf57
Shirshanka Das <shirshanka@apache.org>
2022-09-13 16:18:48 -0700

    fix lint issues

--- metadata-ingestion/tests/integration/git/test_git_clone.py
+++ metadata-ingestion/tests/integration/git/test_git_clone.py
@@ -10,7 +10,7 @@ def test_git_clone(pytestconfig, tmp_path):
     git_clone = GitClone(tmp_path)
     secret_env_variable = "DATAHUB_LOOKML_GIT_TEST_SSH_KEY"
     if os.environ.get(secret_env_variable) is not None:
-        secret_key = SecretStr(os.environ.get(secret_env_variable))
+        secret_key = SecretStr(os.environ.get(secret_env_variable))  # type: ignore
         checkout_dir = git_clone.clone(
             ssh_key=secret_key,
             repo_url="git@github.com:acryldata/long-tail-companions-looker",
@@ -27,4 +27,6 @@ def test_git_clone(pytestconfig, tmp_path):
             ]
         )
     else:
-        print("Skipping test as env variable DATAHUB_LOOKML_GIT_TEST_SSH_KEY is not configured")
+        print(
+            "Skipping test as env variable DATAHUB_LOOKML_GIT_TEST_SSH_KEY is not configured"
+        )
