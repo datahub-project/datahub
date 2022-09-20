cde460ba2a2c15f1d17271f28468c66ea7f1d514
MohdSiddique Bagwan <mohdsiddique.bagwan@gslab.com>
2022-09-20 19:50:10 +0530

    test with stat golden file

--- metadata-ingestion/tests/integration/tableau/test_tableau.py
+++ metadata-ingestion/tests/integration/tableau/test_tableau.py
@@ -4,6 +4,7 @@ from unittest import mock
 
 import pytest
 from freezegun import freeze_time
+from tableauserverclient.models import ViewItem
 
 from datahub.configuration.source_common import DEFAULT_ENV
 from datahub.ingestion.run.pipeline import Pipeline
@@ -25,36 +26,56 @@ def _read_response(file_name):
         return data
 
 
-def side_effect_query_metadata(query):
-    if "workbooksConnection (first:0" in query:
-        return _read_response("workbooksConnection_0.json")
+def define_side_effect_query_func(workbooks_conn_0: str = "workbooksConnection_0.json",
+                                  workbooks_conn_all: str = "workbooksConnection_all.json"):
+    # side_effect_query_metadata can be reused in different test-cases where workbooks files are different
+    def side_effect_query_metadata(query):
+        if "workbooksConnection (first:0" in query:
+            return _read_response(workbooks_conn_0)
 
-    if "workbooksConnection (first:3" in query:
-        return _read_response("workbooksConnection_all.json")
+        if "workbooksConnection (first:3" in query:
+            return _read_response(workbooks_conn_all)
 
-    if "embeddedDatasourcesConnection (first:0" in query:
-        return _read_response("embeddedDatasourcesConnection_0.json")
+        if "embeddedDatasourcesConnection (first:0" in query:
+            return _read_response("embeddedDatasourcesConnection_0.json")
 
-    if "embeddedDatasourcesConnection (first:8" in query:
-        return _read_response("embeddedDatasourcesConnection_all.json")
+        if "embeddedDatasourcesConnection (first:8" in query:
+            return _read_response("embeddedDatasourcesConnection_all.json")
 
-    if "publishedDatasourcesConnection (first:0" in query:
-        return _read_response("publishedDatasourcesConnection_0.json")
+        if "publishedDatasourcesConnection (first:0" in query:
+            return _read_response("publishedDatasourcesConnection_0.json")
 
-    if "publishedDatasourcesConnection (first:2" in query:
-        return _read_response("publishedDatasourcesConnection_all.json")
+        if "publishedDatasourcesConnection (first:2" in query:
+            return _read_response("publishedDatasourcesConnection_all.json")
 
-    if "customSQLTablesConnection (first:0" in query:
-        return _read_response("customSQLTablesConnection_0.json")
+        if "customSQLTablesConnection (first:0" in query:
+            return _read_response("customSQLTablesConnection_0.json")
 
-    if "customSQLTablesConnection (first:2" in query:
-        return _read_response("customSQLTablesConnection_all.json")
+        if "customSQLTablesConnection (first:2" in query:
+            return _read_response("customSQLTablesConnection_all.json")
+
+    return side_effect_query_metadata
 
 
-@freeze_time(FROZEN_TIME)
-@pytest.mark.slow_unit
-def test_tableau_ingest(pytestconfig, tmp_path):
+def side_effect_usage_stat(*arg, **kwargs):
+    mock_pagination = mock.MagicMock()
+    mock_pagination.total_available = None
 
+    dashboard_stat: ViewItem = ViewItem()
+
+    # Added as luid of Dashboard in workbooksConnection_state_all.json
+    dashboard_stat._id = "fc9ea488-f810-4fa8-ac19-aa96018b5d66"
+    dashboard_stat._total_views = 3
+
+    # Added as luid of Sheet in workbooksConnection_state_all.json
+    sheet_stat: ViewItem = ViewItem()
+    sheet_stat._id = "f0779f9d-6765-47a9-a8f6-c740cfd27783"
+    sheet_stat._total_views = 5
+
+    return [dashboard_stat, sheet_stat], mock_pagination
+
+
+def tableau_ingest_common(pytestconfig, tmp_path, side_effect_query_metadata_func, golden_file_name, output_file_name):
     global test_resources_dir
     test_resources_dir = pathlib.Path(
         pytestconfig.rootpath / "tests/integration/tableau"
@@ -63,9 +84,11 @@ def test_tableau_ingest(pytestconfig, tmp_path):
     with mock.patch("tableauserverclient.Server") as mock_sdk:
         mock_client = mock.Mock()
         mocked_metadata = mock.Mock()
-        mocked_metadata.query.side_effect = side_effect_query_metadata
+        mocked_metadata.query.side_effect = side_effect_query_metadata_func
         mock_client.metadata = mocked_metadata
         mock_client.auth = mock.Mock()
+        mock_client.views = mock.Mock()
+        mock_client.views.get.side_effect = side_effect_usage_stat
         mock_client.auth.sign_in.return_value = None
         mock_client.auth.sign_out.return_value = None
         mock_sdk.return_value = mock_client
@@ -96,7 +119,7 @@ def test_tableau_ingest(pytestconfig, tmp_path):
                 "sink": {
                     "type": "file",
                     "config": {
-                        "filename": f"{tmp_path}/tableau_mces.json",
+                        "filename": f"{tmp_path}/{output_file_name}",
                     },
                 },
             }
@@ -106,56 +129,71 @@ def test_tableau_ingest(pytestconfig, tmp_path):
 
         mce_helpers.check_golden_file(
             pytestconfig,
-            output_path=f"{tmp_path}/tableau_mces.json",
-            golden_path=test_resources_dir / "tableau_mces_golden.json",
+            output_path=f"{tmp_path}/{output_file_name}",
+            golden_path=test_resources_dir / golden_file_name,
             ignore_paths=mce_helpers.IGNORE_PATH_TIMESTAMPS,
         )
 
 
-def test_lineage_overrides():
+@freeze_time(FROZEN_TIME)
+@pytest.mark.slow_unit
+def test_tableau_ingest(pytestconfig, tmp_path):
+    output_file_name: str = "tableau_mces.json"
+    golden_file_name: str = "tableau_mces_golden.json"
+    tableau_ingest_common(pytestconfig, tmp_path, define_side_effect_query_func(), golden_file_name, output_file_name)
 
+
+@freeze_time(FROZEN_TIME)
+@pytest.mark.slow_unit
+def test_tableau_ingest_stat(pytestconfig, tmp_path):
+    output_file_name: str = "tableau_stat_mces.json"
+    golden_file_name: str = "tableau_state_mces_golden.json"
+    tableau_ingest_common(pytestconfig, tmp_path, define_side_effect_query_func(workbooks_conn_all="workbooksConnection_state_all.json"), golden_file_name, output_file_name)
+
+
+def test_lineage_overrides():
     # Simple - specify platform instance to presto table
     assert (
-        make_table_urn(
-            DEFAULT_ENV,
-            "presto_catalog",
-            "presto",
-            "test-schema",
-            "presto_catalog.test-schema.test-table",
-            platform_instance_map={"presto": "my_presto_instance"},
-        )
-        == "urn:li:dataset:(urn:li:dataPlatform:presto,my_presto_instance.presto_catalog.test-schema.test-table,PROD)"
+            make_table_urn(
+                DEFAULT_ENV,
+                "presto_catalog",
+                "presto",
+                "test-schema",
+                "presto_catalog.test-schema.test-table",
+                platform_instance_map={"presto": "my_presto_instance"},
+            )
+            == "urn:li:dataset:(urn:li:dataPlatform:presto,my_presto_instance.presto_catalog.test-schema.test-table,PROD)"
     )
 
     # Transform presto urn to hive urn
     # resulting platform instance for hive = mapped platform instance + presto_catalog
     assert (
-        make_table_urn(
-            DEFAULT_ENV,
-            "presto_catalog",
-            "presto",
-            "test-schema",
-            "presto_catalog.test-schema.test-table",
-            platform_instance_map={"presto": "my_instance"},
-            lineage_overrides=TableauLineageOverrides(
-                platform_override_map={"presto": "hive"},
-            ),
-        )
-        == "urn:li:dataset:(urn:li:dataPlatform:hive,my_instance.presto_catalog.test-schema.test-table,PROD)"
+            make_table_urn(
+                DEFAULT_ENV,
+                "presto_catalog",
+                "presto",
+                "test-schema",
+                "presto_catalog.test-schema.test-table",
+                platform_instance_map={"presto": "my_instance"},
+                lineage_overrides=TableauLineageOverrides(
+                    platform_override_map={"presto": "hive"},
+                ),
+            )
+            == "urn:li:dataset:(urn:li:dataPlatform:hive,my_instance.presto_catalog.test-schema.test-table,PROD)"
     )
 
     # tranform hive urn to presto urn
     assert (
-        make_table_urn(
-            DEFAULT_ENV,
-            "",
-            "hive",
-            "test-schema",
-            "test-schema.test-table",
-            platform_instance_map={"hive": "my_presto_instance.presto_catalog"},
-            lineage_overrides=TableauLineageOverrides(
-                platform_override_map={"hive": "presto"},
-            ),
-        )
-        == "urn:li:dataset:(urn:li:dataPlatform:presto,my_presto_instance.presto_catalog.test-schema.test-table,PROD)"
+            make_table_urn(
+                DEFAULT_ENV,
+                "",
+                "hive",
+                "test-schema",
+                "test-schema.test-table",
+                platform_instance_map={"hive": "my_presto_instance.presto_catalog"},
+                lineage_overrides=TableauLineageOverrides(
+                    platform_override_map={"hive": "presto"},
+                ),
+            )
+            == "urn:li:dataset:(urn:li:dataPlatform:presto,my_presto_instance.presto_catalog.test-schema.test-table,PROD)"
     )
