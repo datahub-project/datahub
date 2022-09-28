a8f8d6bcde20e9810f93cae3e20b92a2524bbe0f
Harshal Sheth <hsheth2@gmail.com>
2022-09-27 21:49:26 -0700

    refactor(ingest): remove typing workarounds
    Possible now that we're on mypy 0.980.

--- metadata-ingestion/src/datahub/ingestion/api/committable.py
+++ metadata-ingestion/src/datahub/ingestion/api/committable.py
@@ -11,17 +11,10 @@ class CommitPolicy(Enum):
 
 
 @dataclass
-class _CommittableConcrete:
+class Committable(ABC):
     name: str
     commit_policy: CommitPolicy
-    committed: bool
-
-
-# The concrete portion Committable is separated from the abstract portion due to
-# https://github.com/python/mypy/issues/5374#issuecomment-568335302.
-class Committable(_CommittableConcrete, ABC):
-    def __init__(self, name: str, commit_policy: CommitPolicy):
-        super(Committable, self).__init__(name, commit_policy, committed=False)
+    committed: bool = False
 
     @abstractmethod
     def commit(self) -> None:
@@ -34,25 +27,15 @@ StateType = TypeVar("StateType")
 FilterType = TypeVar("FilterType")
 
 
-class _StatefulCommittableConcrete(Generic[StateType]):
-    def __init__(self, state_to_commit: StateType):
-        self.state_to_commit: StateType = state_to_commit
-
-
 class StatefulCommittable(
     Committable,
-    _StatefulCommittableConcrete[StateType],
     Generic[StateKeyType, StateType, FilterType],
 ):
     def __init__(
         self, name: str, commit_policy: CommitPolicy, state_to_commit: StateType
     ):
-        # _ConcreteCommittable will be the first from this class.
-        super(StatefulCommittable, self).__init__(
-            name=name, commit_policy=commit_policy
-        )
-        # _StatefulCommittableConcrete will be after _CommittableConcrete in the __mro__.
-        super(_CommittableConcrete, self).__init__(state_to_commit=state_to_commit)
+        super().__init__(name=name, commit_policy=commit_policy)
+        self.state_to_commit: StateType = state_to_commit
 
     def has_successfully_committed(self) -> bool:
         return bool(not self.state_to_commit or self.committed)
