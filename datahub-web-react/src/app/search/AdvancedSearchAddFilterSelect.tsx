df815b8bd8302b3126d727758d58045b064f7edc
Gabe Lyons <itsgabelyons@gmail.com>
2022-10-03 15:03:31 -0700

    fix smoke tests

--- datahub-web-react/src/app/search/AdvancedSearchAddFilterSelect.tsx
+++ datahub-web-react/src/app/search/AdvancedSearchAddFilterSelect.tsx
@@ -43,6 +43,7 @@ export const AdvancedSearchAddFilterSelect = ({ selectedFilters, onFilterFieldSe
                         // disable the `entity` option if they already have an entity filter selected
                         disabled={key === 'entity' && !!selectedFilters.find((filter) => filter.field === 'entity')}
                         value={key}
+                        data-testid={`adv-search-add-filter-${key}`}
                     >
                         {FIELD_TO_LABEL[key]}
                     </Option>
