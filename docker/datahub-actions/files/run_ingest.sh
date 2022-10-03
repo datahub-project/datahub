a8cf4e2f951dfaeec1679ff4c45238a6b9e4efae
Mide Ojikutu <mide.ojikutu@gmail.com>
2022-10-03 15:56:36 +0100

    [CDBD-125] - use default kafka consumer timeouts in actions framework consumer config

--- docker/datahub-actions/files/run_ingest.sh
+++ docker/datahub-actions/files/run_ingest.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
-# usage: ./run_ingest.sh <recipe-id> <datahub-version> <plugins-required> <tmp-dir>
+# usage: ./run_ingest.sh <task-id> <datahub-version> <plugins-required> <tmp-dir> <recipe_file> <report_file>
 
+set -euo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd "$DIR"
+cd "$DIR" || exit
+
+task_id="$1"
+datahub_version="$2"
+plugins="$3"
+tmp_dir="$4"
+recipe_file="$5"
+report_file="$6"
+debug_mode="$7"
 
 source $VENV_NAME/bin/activate
 
-if (python3 -m datahub ingest -c "$4/$1.yml"); then
+if (datahub ingest run --help | grep -q report-to); then
+  echo "This version of datahub supports report-to functionality"
+  rm -f "$report_file"
+  report_option="--report-to ${report_file}"
+else
+  report_option=""
+fi
+
+if [ "$debug_mode" == "true" ]; then
+  debug_option="--debug"
+else
+  debug_option=""
+fi;
+
+# Execute DataHub recipe, based on the recipe id.
+echo "datahub ${debug_option} ingest run -c ${recipe_file} ${report_option}"
+if (datahub ${debug_option} ingest run -c "${recipe_file}" ${report_option}); then
   exit 0
 else
   exit 1
