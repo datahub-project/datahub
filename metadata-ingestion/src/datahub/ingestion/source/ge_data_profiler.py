9a71c6d26cf6e52f7a8035f0e435fd433782917c
Harshal Sheth <hsheth2@gmail.com>
2022-10-03 14:30:33 -0700

    fix(ingest): add column name quoting for approx count distinct

--- metadata-ingestion/src/datahub/ingestion/source/ge_data_profiler.py
+++ metadata-ingestion/src/datahub/ingestion/source/ge_data_profiler.py
@@ -100,21 +100,14 @@ def get_column_unique_count_patch(self, column):
     if self.engine.dialect.name.lower() == "redshift":
         element_values = self.engine.execute(
             sa.select(
-                [sa.text(f"APPROXIMATE count(distinct {column})")]  # type:ignore
+                [sa.text(f'APPROXIMATE count(distinct "{column}")')]  # type:ignore
             ).select_from(self._table)
         )
         return convert_to_json_serializable(element_values.fetchone()[0])
-    elif self.engine.dialect.name.lower() == "bigquery":
+    elif self.engine.dialect.name.lower() in {"bigquery", "snowflake"}:
         element_values = self.engine.execute(
             sa.select(
-                [sa.text(f"APPROX_COUNT_DISTINCT ({column})")]  # type:ignore
-            ).select_from(self._table)
-        )
-        return convert_to_json_serializable(element_values.fetchone()[0])
-    elif self.engine.dialect.name.lower() == "snowflake":
-        element_values = self.engine.execute(
-            sa.select(
-                [sa.text(f"APPROX_COUNT_DISTINCT({column})")]  # type:ignore
+                [sa.text(f'APPROX_COUNT_DISTINCT ("{column}")')]  # type:ignore
             ).select_from(self._table)
         )
         return convert_to_json_serializable(element_values.fetchone()[0])
