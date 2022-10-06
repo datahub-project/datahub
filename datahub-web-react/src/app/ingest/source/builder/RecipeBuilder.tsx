20080a671c1589db825e58efe5c507dd1821d67a
Ankit Keshari <ankit.keshari@gslab.com>
2022-10-06 16:12:30 +0530

    Worked on the PR comments

--- datahub-web-react/src/app/ingest/source/builder/RecipeBuilder.tsx
+++ datahub-web-react/src/app/ingest/source/builder/RecipeBuilder.tsx
@@ -1,5 +1,5 @@
-import { Button, Row, Col, message, Typography, Alert } from 'antd';
 import React, { useState } from 'react';
+import { Button, message, Typography } from 'antd';
 import YAML from 'yamljs';
 import { CodeOutlined, FormOutlined } from '@ant-design/icons';
 import styled from 'styled-components/macro';
@@ -7,9 +7,8 @@ import { ANTD_GRAY } from '../../../entity/shared/constants';
 import { YamlEditor } from './YamlEditor';
 import RecipeForm from './RecipeForm/RecipeForm';
 import { SourceBuilderState, SourceConfig } from './types';
-
-const LOOKML_DOC_LINK = 'https://datahubproject.io/docs/generated/ingestion/sources/looker#module-lookml';
-const LOOKER_DOC_LINK = 'https://datahubproject.io/docs/generated/ingestion/sources/looker#module-looker';
+import { LOOKER, LOOK_ML } from './constants';
+import { LookerWarning } from './LookerWarning';
 
 export const ControlsContainer = styled.div`
     display: flex;
@@ -46,6 +45,12 @@ const ButtonsWrapper = styled.div`
     justify-content: flex-end;
 `;
 
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 10px;
+`;
+
 interface Props {
     state: SourceBuilderState;
     isEditing: boolean;
@@ -73,57 +78,20 @@ function RecipeBuilder(props: Props) {
         }
     }
 
-    let link: React.ReactNode;
-    if (type === 'looker') {
-        link = (
-            <a href={LOOKER_DOC_LINK} target="_blank" rel="noopener noreferrer">
-                DataHub looker module
-            </a>
-        );
-    } else if (type === 'lookml') {
-        link = (
-            <a href={LOOKML_DOC_LINK} target="_blank" rel="noopener noreferrer">
-                DataHub lookml module
-            </a>
-        );
-    }
-
     return (
         <div>
-            {(type === 'looker' || type === 'lookml') && (
-                <Alert
-                    type="warning"
-                    banner
-                    message={
-                        <>
-                            <big>
-                                <i>
-                                    <b>You must acknowledge this message to proceed!</b>
-                                </i>
-                            </big>
-                            <br />
-                            <br />
-                            To get complete Looker metadata integration (including Looker views and lineage to the
-                            underlying warehouse tables), you must <b>also</b> use the {link}.
-                        </>
-                    }
-                />
-            )}
-            <Row style={{ marginBottom: '10px' }}>
-                <Col xl={8} lg={8} md={8} sm={24} xs={24}>
-                    <Title level={5}>{sourceConfigs?.displayName} Recipe</Title>
-                </Col>
-                <Col xl={16} lg={16} md={16} sm={24} xs={24}>
-                    <ButtonsWrapper>
-                        <StyledButton type="text" isSelected={isViewingForm} onClick={() => switchViews(true)}>
-                            <FormOutlined /> Form
-                        </StyledButton>
-                        <StyledButton type="text" isSelected={!isViewingForm} onClick={() => switchViews(false)}>
-                            <CodeOutlined /> YAML
-                        </StyledButton>
-                    </ButtonsWrapper>
-                </Col>
-            </Row>
+            {(type === LOOKER || type === LOOK_ML) && <LookerWarning type={type} />}
+            <HeaderContainer>
+                <Title level={5}>{sourceConfigs?.displayName} Recipe</Title>
+                <ButtonsWrapper>
+                    <StyledButton type="text" isSelected={isViewingForm} onClick={() => switchViews(true)}>
+                        <FormOutlined /> Form
+                    </StyledButton>
+                    <StyledButton type="text" isSelected={!isViewingForm} onClick={() => switchViews(false)}>
+                        <CodeOutlined /> YAML
+                    </StyledButton>
+                </ButtonsWrapper>
+            </HeaderContainer>
             {isViewingForm && (
                 <RecipeForm
                     state={state}
