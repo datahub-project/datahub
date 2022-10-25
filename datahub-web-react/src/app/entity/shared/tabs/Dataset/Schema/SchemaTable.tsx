94e52c5f81e867ff8a7a16cd41b76b1b60964172
stanbaker <sbaker@manifold.ai>
2022-10-25 19:17:17 -0400

    perf(ui): virtualized schema table for better performance when rendering wide table definitions with many columns.

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/SchemaTable.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/SchemaTable.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { ColumnsType } from 'antd/es/table';
+import { VList } from 'virtuallist-antd';
 import styled from 'styled-components';
-import {} from 'antd';
 import {
     EditableSchemaMetadata,
     ForeignKeyConstraint,
@@ -17,7 +17,6 @@ import useUsageStatsRenderer from './utils/useUsageStatsRenderer';
 import useTagsAndTermsRenderer from './utils/useTagsAndTermsRenderer';
 import ExpandIcon from './components/ExpandIcon';
 import { StyledTable } from '../../../components/styled/StyledTable';
-import { SchemaRow } from './components/SchemaRow';
 import { FkContext } from './utils/selectedFkContext';
 import useSchemaBlameRenderer from './utils/useSchemaBlameRenderer';
 import { ANTD_GRAY } from '../../../constants';
@@ -194,11 +193,8 @@ export default function SchemaTable({
                     columns={allColumns}
                     dataSource={rows}
                     rowKey="fieldPath"
-                    components={{
-                        body: {
-                            row: SchemaRow,
-                        },
-                    }}
+                    scroll={{ y: '100vh', x: '100%' }}
+                    components={VList({ height: 'auto' })}
                     expandable={{
                         expandedRowKeys: [...Array.from(expandedRows)],
                         defaultExpandAllRows: false,
