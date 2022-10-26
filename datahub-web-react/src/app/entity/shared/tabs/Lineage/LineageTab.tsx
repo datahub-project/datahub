cd1331fffe52c3c155f349569835be51c7e626c5
Chris Collins <chriscollins3456@gmail.com>
2022-10-26 16:43:39 -0400

    feat(lineage) Add column-level impact analysis feature (#6272)

--- datahub-web-react/src/app/entity/shared/tabs/Lineage/LineageTab.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Lineage/LineageTab.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import { Button } from 'antd';
-import { useHistory } from 'react-router';
+import * as QueryString from 'query-string';
+import { useHistory, useLocation } from 'react-router';
 import { ArrowDownOutlined, ArrowUpOutlined, PartitionOutlined } from '@ant-design/icons';
 import styled from 'styled-components/macro';
 
@@ -10,6 +11,10 @@ import { getEntityPath } from '../../containers/profile/utils';
 import { useEntityRegistry } from '../../../../useEntityRegistry';
 import { ImpactAnalysis } from './ImpactAnalysis';
 import { LineageDirection } from '../../../../../types.generated';
+import { generateSchemaFieldUrn } from './utils';
+import { downgradeV2FieldPath } from '../../../dataset/profile/schema/utils/utils';
+import ColumnsLineageSelect from './ColumnLineageSelect';
+import { LineageTabContext } from './LineageTabContext';
 
 const StyledTabToolbar = styled(TabToolbar)`
     justify-content: space-between;
@@ -26,6 +31,11 @@ const StyledButton = styled(Button)<{ isSelected: boolean }>`
     `}
 `;
 
+const RightButtonsWrapper = styled.div`
+    align-items: center;
+    display: flex;
+`;
+
 export const LineageTab = ({
     properties = { defaultDirection: LineageDirection.Downstream },
 }: {
@@ -34,11 +44,19 @@ export const LineageTab = ({
     const { urn, entityType } = useEntityData();
     const history = useHistory();
     const entityRegistry = useEntityRegistry();
-    const [lineageDirection, setLineageDirection] = useState<string>(properties.defaultDirection);
+    const location = useLocation();
+    const params = QueryString.parse(location.search, { arrayFormat: 'comma' });
+    const [lineageDirection, setLineageDirection] = useState<LineageDirection>(properties.defaultDirection);
+    const [selectedColumn, setSelectedColumn] = useState<string | undefined>(params?.column as string);
+    const [isColumnLevelLineage, setIsColumnLevelLineage] = useState(!!params?.column);
 
     const routeToLineage = useCallback(() => {
         history.push(getEntityPath(entityType, urn, entityRegistry, true, false));
     }, [history, entityType, urn, entityRegistry]);
+
+    const selectedV1FieldPath = downgradeV2FieldPath(selectedColumn) || '';
+    const selectedColumnUrn = generateSchemaFieldUrn(selectedV1FieldPath, urn);
+    const impactAnalysisUrn = isColumnLevelLineage && selectedColumnUrn ? selectedColumnUrn : urn;
 
     return (
         <>
@@ -59,12 +77,22 @@ export const LineageTab = ({
                         <ArrowUpOutlined /> Upstream
                     </StyledButton>
                 </div>
-                <Button type="text" onClick={routeToLineage}>
-                    <PartitionOutlined />
-                    Visualize Lineage
-                </Button>
+                <RightButtonsWrapper>
+                    <ColumnsLineageSelect
+                        selectedColumn={selectedColumn}
+                        isColumnLevelLineage={isColumnLevelLineage}
+                        setSelectedColumn={setSelectedColumn}
+                        setIsColumnLevelLineage={setIsColumnLevelLineage}
+                    />
+                    <Button type="text" onClick={routeToLineage}>
+                        <PartitionOutlined />
+                        Visualize Lineage
+                    </Button>
+                </RightButtonsWrapper>
             </StyledTabToolbar>
-            <ImpactAnalysis urn={urn} direction={lineageDirection as LineageDirection} />
+            <LineageTabContext.Provider value={{ isColumnLevelLineage, selectedColumn, lineageDirection }}>
+                <ImpactAnalysis urn={impactAnalysisUrn} direction={lineageDirection as LineageDirection} />
+            </LineageTabContext.Provider>
         </>
     );
 };
---
+++ datahub-web-react/src/app/preview/EntityPaths/ColumnsRelationshipText.tsx
@@ -0,0 +1,39 @@
+import { Maybe } from 'graphql/jsutils/Maybe';
+import React, { useContext } from 'react';
+import styled from 'styled-components/macro';
+import { Entity, LineageDirection } from '../../../types.generated';
+import { downgradeV2FieldPath } from '../../entity/dataset/profile/schema/utils/utils';
+import { LineageTabContext } from '../../entity/shared/tabs/Lineage/LineageTabContext';
+import DisplayedColumns from './DisplayedColumns';
+
+const ColumnNameWrapper = styled.span<{ isBlack?: boolean }>`
+    font-family: 'Roboto Mono', monospace;
+    font-weight: bold;
+    ${(props) => props.isBlack && 'color: black;'}
+`;
+
+interface Props {
+    displayedColumns: (Maybe<Entity> | undefined)[];
+}
+
+export default function ColumnsRelationshipText({ displayedColumns }: Props) {
+    const { selectedColumn, lineageDirection } = useContext(LineageTabContext);
+
+    const displayedFieldPath = downgradeV2FieldPath(selectedColumn);
+
+    return (
+        <>
+            {lineageDirection === LineageDirection.Downstream ? (
+                <span>
+                    <ColumnNameWrapper>{displayedFieldPath}</ColumnNameWrapper> to&nbsp;
+                    <DisplayedColumns displayedColumns={displayedColumns} />
+                </span>
+            ) : (
+                <span>
+                    <DisplayedColumns displayedColumns={displayedColumns} /> to{' '}
+                    <ColumnNameWrapper>{displayedFieldPath}</ColumnNameWrapper>
+                </span>
+            )}
+        </>
+    );
+}
