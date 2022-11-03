184e846ef60a3d305ccd23d0d71c922a31b54d52
Harshal Sheth <hsheth2@gmail.com>
2022-11-02 23:20:56 -0700

    fix plugin

--- metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/datahub_plugin.py
+++ metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/datahub_plugin.py
@@ -1,2 +1,2 @@
 # This package serves as a shim, but the actual implementation lives in datahub_provider from the acryl-datahub package.
-from datahub_provider._plugin import DatahubPlugin
+from datahub_provider._plugin import DatahubPlugin  # noqa: F401
