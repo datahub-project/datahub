acf2f0d225fe048843f155a9461dd7da737c707f
MohdSiddique Bagwan <mohdsiddique.bagwan@gslab.com>
2022-11-03 14:31:12 +0530

    metadata-service/auth-api

---
+++ metadata-service/auth-api/src/main/java/com/datahub/auth/authorization/DefaultAuthorizerConfiguration.java
@@ -0,0 +1,16 @@
+package com.datahub.auth.authorization;
+
+import lombok.Data;
+
+
+@Data
+public class DefaultAuthorizerConfiguration {
+  /**
+   * Whether authorization via DataHub policies is enabled.
+   */
+  private boolean enabled;
+  /**
+   * The duration between policies cache refreshes.
+   */
+  private int cacheRefreshIntervalSecs;
+}
