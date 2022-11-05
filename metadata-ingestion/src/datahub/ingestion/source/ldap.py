6bcd109c8f7f650200369ab71a784462dede8310
Dmitry Bryazgin <58312247+bda618@users.noreply.github.com>
2022-11-05 11:59:25 -0400

    rolled back changes

--- metadata-ingestion/src/datahub/ingestion/source/ldap.py
+++ metadata-ingestion/src/datahub/ingestion/source/ldap.py
@@ -28,7 +28,6 @@ from datahub.ingestion.source.state.stateful_ingestion_base import (
     StatefulIngestionConfigBase,
     StatefulIngestionSourceBase,
 )
-from datahub.metadata.com.linkedin.pegasus2avro.common import StatusClass
 from datahub.metadata.com.linkedin.pegasus2avro.mxe import MetadataChangeEvent
 from datahub.metadata.schema_classes import (
     CorpGroupInfoClass,
@@ -400,7 +399,6 @@ class LDAPSource(StatefulIngestionSourceBase):
                     title=title,
                     managerUrn=manager_urn,
                 ),
-                StatusClass(removed=False),
             ],
         )
 
