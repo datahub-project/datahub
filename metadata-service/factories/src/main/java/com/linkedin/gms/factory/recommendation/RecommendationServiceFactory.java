76aa7266fbc917863ccb466971fa4017951537e2
John Joyce <john@acryl.io>
2022-11-14 16:58:10 -0800

    Fixing factory

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/recommendation/RecommendationServiceFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/recommendation/RecommendationServiceFactory.java
@@ -3,7 +3,6 @@ package com.linkedin.gms.factory.recommendation;
 import com.google.common.collect.ImmutableList;
 import com.linkedin.gms.factory.recommendation.candidatesource.DomainsCandidateSourceFactory;
 import com.linkedin.gms.factory.recommendation.candidatesource.MostPopularCandidateSourceFactory;
-import com.linkedin.gms.factory.recommendation.candidatesource.RecentlyViewedCandidateSourceFactory;
 import com.linkedin.gms.factory.recommendation.candidatesource.RecentlyEditedCandidateSourceFactory;
 import com.linkedin.gms.factory.recommendation.candidatesource.TopPlatformsCandidateSourceFactory;
 import com.linkedin.gms.factory.recommendation.candidatesource.TopTagsCandidateSourceFactory;
@@ -29,7 +28,7 @@ import org.springframework.context.annotation.Import;
 
 
 @Configuration
-@Import({TopPlatformsCandidateSourceFactory.class, RecentlyViewedCandidateSourceFactory.class,
+@Import({TopPlatformsCandidateSourceFactory.class, RecentlyEditedCandidateSourceFactory.class,
     MostPopularCandidateSourceFactory.class, TopTagsCandidateSourceFactory.class, TopTermsCandidateSourceFactory.class, DomainsCandidateSourceFactory.class})
 public class RecommendationServiceFactory {
 
