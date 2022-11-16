7c496d0f7fe58e0bceda727008ad70f0dda7a310
Ankit Keshari <ankit.keshari@gslab.com>
2022-11-16 21:48:26 +0530

    Optimize the code

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/filterSchemaRows.ts
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/filterSchemaRows.ts
@@ -1,7 +1,7 @@
 import { EntityType, SchemaField } from '../../../../../../../types.generated';
 import EntityRegistry from '../../../../../EntityRegistry';
 
-function matchesTagsOrTerms(field: SchemaField, filterText: string, entityRegistry: EntityRegistry) {
+function matchesTagsOrTermsOrDescription(field: SchemaField, filterText: string, entityRegistry: EntityRegistry) {
     return (
         field.globalTags?.tags?.find((tagAssociation) =>
             entityRegistry.getDisplayName(EntityType.Tag, tagAssociation.tag).toLocaleLowerCase().includes(filterText),
@@ -11,31 +11,22 @@ function matchesTagsOrTerms(field: SchemaField, filterText: string, entityRegist
                 .getDisplayName(EntityType.GlossaryTerm, termAssociation.term)
                 .toLocaleLowerCase()
                 .includes(filterText),
-        )
+        ) ||
+        field.description?.toLocaleLowerCase().includes(filterText)
     );
 }
 
-// returns list of fieldPaths for fields that have Terms or Tags matching the filterText
+// returns list of fieldPaths for fields that have Terms or Tags or Descriptions matching the filterText
 function getFilteredFieldPathsByMetadata(editableSchemaMetadata: any, entityRegistry, filterText) {
     return (
         editableSchemaMetadata?.editableSchemaFieldInfo
-            .filter((fieldInfo) => matchesTagsOrTerms(fieldInfo, filterText, entityRegistry))
+            .filter((fieldInfo) => matchesTagsOrTermsOrDescription(fieldInfo, filterText, entityRegistry))
             .map((fieldInfo) => fieldInfo.fieldPath) || []
     );
 }
 
 function matchesDescription(fieldDescription: any, filterText: string) {
     return fieldDescription?.toLocaleLowerCase().includes(filterText);
-}
-
-// returns list of fieldPaths for fields that have description matching the filterText
-function getDescriptionFieldPathsByMetadata(editableSchemaMetadata: any, filterText: string) {
-    return (
-        editableSchemaMetadata?.editableSchemaFieldInfo
-            .filter((fieldInfo) => fieldInfo.description !== null)
-            .filter((fieldInfo) => matchesDescription(fieldInfo.description, filterText))
-            .map((fieldInfo) => fieldInfo.fieldPath) || []
-    );
 }
 
 function matchesEditableTagsOrTerms(field: SchemaField, filteredFieldPathsByEditableMetadata: any) {
@@ -66,8 +57,6 @@ export function filterSchemaRows(
         formattedFilterText,
     );
 
-    const filteredEditableDescription = getDescriptionFieldPathsByMetadata(editableSchemaMetadata, formattedFilterText);
-
     const finalFieldPaths = new Set();
     const expandedRowsFromFilter = new Set();
 
@@ -75,8 +64,8 @@ export function filterSchemaRows(
         if (
             matchesFieldName(row.fieldPath, formattedFilterText) ||
             matchesEditableTagsOrTerms(row, filteredFieldPathsByEditableMetadata) ||
-            matchesEditableDescription(row, filteredEditableDescription) ||
-            matchesTagsOrTerms(row, formattedFilterText, entityRegistry) || // non-editable tags and terms
+            matchesEditableDescription(row, filteredFieldPathsByEditableMetadata) ||
+            matchesTagsOrTermsOrDescription(row, formattedFilterText, entityRegistry) || // non-editable tags and terms
             matchesDescription(row.description, formattedFilterText) // non-editable description
         ) {
             finalFieldPaths.add(row.fieldPath);
@@ -86,8 +75,8 @@ export function filterSchemaRows(
         if (
             matchesFieldName(fieldName, formattedFilterText) ||
             matchesEditableTagsOrTerms(row, filteredFieldPathsByEditableMetadata) ||
-            matchesEditableDescription(row, filteredEditableDescription) ||
-            matchesTagsOrTerms(row, formattedFilterText, entityRegistry) || // non-editable tags and terms
+            matchesEditableDescription(row, filteredFieldPathsByEditableMetadata) ||
+            matchesTagsOrTermsOrDescription(row, formattedFilterText, entityRegistry) || // non-editable tags and terms
             matchesDescription(row.description, formattedFilterText) // non-editable description
         ) {
             // if we match specifically on this field (not just its parent), add and expand all parents
