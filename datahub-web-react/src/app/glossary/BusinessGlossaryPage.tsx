9b5188ebc578d7db26072c781a1d3697e839ded5
Chris Collins <chriscollins3456@gmail.com>
2022-11-16 12:26:51 -0500

    fix(ui) Fix glossary side browser width fluctuating (#6457)
    Co-authored-by: Chris Collins <chriscollins@Chriss-MBP-2-753.lan>

--- datahub-web-react/src/app/glossary/BusinessGlossaryPage.tsx
+++ datahub-web-react/src/app/glossary/BusinessGlossaryPage.tsx
@@ -37,6 +37,7 @@ const MainContentWrapper = styled.div`
 
 export const BrowserWrapper = styled.div<{ width: number }>`
     max-height: 100%;
+    width: ${(props) => props.width}px;
     min-width: ${(props) => props.width}px;
 `;
 
