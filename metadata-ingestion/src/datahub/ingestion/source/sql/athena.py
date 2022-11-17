7b9df1bf01942b237cde80f15adbca01c80cea97
Harshal Sheth <hsheth2@gmail.com>
2022-11-17 14:36:32 -0500

    fix athena cursor

--- metadata-ingestion/src/datahub/ingestion/source/sql/athena.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/athena.py
@@ -109,9 +109,9 @@ class AthenaSource(SQLAlchemySource):
         self, inspector: Inspector, schema: str, table: str
     ) -> Tuple[Optional[str], Dict[str, str], Optional[str]]:
         if not self.cursor:
-            self.cursor = inspector.engine.raw_connection().cursor()
+            self.cursor: BaseCursor = inspector.engine.raw_connection().cursor()
+            assert self.cursor
 
-        assert self.cursor
         # Unfortunately properties can be only get through private methods as those are not exposed
         # https://github.com/laughingman7743/PyAthena/blob/9e42752b0cc7145a87c3a743bb2634fe125adfa7/pyathena/model.py#L201
         metadata: AthenaTableMetadata = self.cursor._get_table_metadata(
