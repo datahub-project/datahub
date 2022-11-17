f470d988567d3bac108e5c64a81bd361ce27ee96
Harshal Sheth <hsheth2@gmail.com>
2022-11-17 13:44:08 -0500

    add dbt cloud deps

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -258,6 +258,7 @@ plugins: Dict[str, Set[str]] = {
     "datahub-business-glossary": set(),
     "delta-lake": {*data_lake_profiling, *delta_lake},
     "dbt": {"requests"} | aws_common,
+    "dbt-cloud": {"requests"},
     "druid": sql_common | {"pydruid>=0.6.2"},
     # Starting with 7.14.0 python client is checking if it is connected to elasticsearch client. If its not it throws
     # UnsupportedProductError
