844a95ae76600c4dc8f00241c99bd166f2456105
Teppo Naakka <teppo.naakka@gmail.com>
2022-11-18 12:50:29 +0200

    fix: merge error, reports should be extracted from all workspaces

--- metadata-ingestion/src/datahub/ingestion/source/powerbi.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi.py
@@ -1793,13 +1793,13 @@ class PowerBiDashboardSource(Source):
                     # Return workunit to Datahub Ingestion framework
                     yield workunit
 
-        if self.source_config.extract_reports:
-            for report in self.powerbi_client.get_reports(workspace=workspace):
-                for work_unit in self.mapper.report_to_datahub_work_units(
-                    report, workspace
-                ):
-                    self.reporter.report_workunit(work_unit)
-                    yield work_unit
+            if self.source_config.extract_reports:
+                for report in self.powerbi_client.get_reports(workspace=workspace):
+                    for work_unit in self.mapper.report_to_datahub_work_units(
+                        report, workspace
+                    ):
+                        self.reporter.report_workunit(work_unit)
+                        yield work_unit
 
     def get_report(self) -> SourceReport:
         return self.reporter
