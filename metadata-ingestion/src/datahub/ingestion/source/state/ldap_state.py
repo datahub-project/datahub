24ca7665f3a94afbde8def427155bb973e3e480d
Dmitry Bryazgin <58312247+bda618@users.noreply.github.com>
2022-11-23 15:39:14 -0500

    Update ldap_state.py

--- metadata-ingestion/src/datahub/ingestion/source/state/ldap_state.py
+++ metadata-ingestion/src/datahub/ingestion/source/state/ldap_state.py
@@ -5,44 +5,35 @@ import pydantic
 from datahub.ingestion.source.state.stale_entity_removal_handler import (
     StaleEntityCheckpointStateBase,
 )
+from datahub.utilities.urns.urn import guess_entity_type
 
 
 class LdapCheckpointState(StaleEntityCheckpointStateBase["LdapCheckpointState"]):
     """
     Base class for representing the checkpoint state for all LDAP based sources.
-    Stores all ldap_users being ingested and is used to remove any stale entities.
+    Stores all corpuser and corpGroup and being ingested and is used to remove any stale entities.
     """
 
-    encoded_ldap_users: List[str] = pydantic.Field(default_factory=list)
+    urns: List[str] = pydantic.Field(default_factory=list)
 
     @classmethod
     def get_supported_types(cls) -> List[str]:
-        return ["corpuser"]
+        return ["corpuser", "corpGroup"]
 
-    @staticmethod
-    def _get_urns_not_in(
-        encoded_ldap_users_1: List[str], encoded_ldap_users_2: List[str]
-    ) -> Iterable[str]:
-        difference = set(encoded_ldap_users_1) - set(encoded_ldap_users_2)
-        for ldap_user in difference:
-            yield ldap_user
+    def add_checkpoint_urn(self, type: str, urn: str) -> None:
+        assert type in self.get_supported_types()
+        self.urns.append(urn)
 
     def get_urns_not_in(
         self, type: str, other_checkpoint_state: "LdapCheckpointState"
     ) -> Iterable[str]:
         assert type in self.get_supported_types()
-        if type == "corpuser":
-            yield from self._get_urns_not_in(
-                self.encoded_ldap_users, other_checkpoint_state.encoded_ldap_users
-            )
-
-    def add_checkpoint_urn(self, type: str, urn: str) -> None:
-        assert type in self.get_supported_types()
-        self.encoded_ldap_users.append(urn)
+        diff = set(self.urns) - set(other_checkpoint_state.urns)
+        yield from (urn for urn in diff if guess_entity_type(urn) == type)
 
     def get_percent_entities_changed(
         self, old_checkpoint_state: "LdapCheckpointState"
     ) -> float:
         return StaleEntityCheckpointStateBase.compute_percent_entities_changed(
-            [(self.encoded_ldap_users, old_checkpoint_state.encoded_ldap_users)]
+            [(self.urns, old_checkpoint_state.urns)]
         )
