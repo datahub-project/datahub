1e4d012df600c7bef0c3055148362c24ce21d759
John Joyce <john@acryl.io>
2022-11-23 12:09:56 -0800

    Fix checkstyle

--- metadata-io/src/main/java/com/linkedin/metadata/recommendation/candidatesource/RecommendationUtils.java
+++ metadata-io/src/main/java/com/linkedin/metadata/recommendation/candidatesource/RecommendationUtils.java
@@ -18,4 +18,6 @@ public class RecommendationUtils {
     final String entityType = urn.getEntityType();
     return entityTypes.contains(entityType);
   }
+  
+  private RecommendationUtils() { }
 }
