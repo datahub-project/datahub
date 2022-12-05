9fc1e4d6f30ebb64ddb816b285d24f4d20362c24
Teppo Naakka <teppo.naakka@gmail.com>
2022-12-05 15:24:19 +0200

    fix: add documentation to the new configuration switch

--- metadata-ingestion/src/datahub/ingestion/source/powerbi.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi.py
@@ -142,6 +142,7 @@ class PowerBiAPIConfig(EnvBasedSourceConfigBase):
     extract_reports: bool = pydantic.Field(
         default=True, description="Whether reports should be ingested"
     )
+    # Enable/Disable ingestion of orphan datasets
     extract_orphan_datasets: bool = pydantic.Field(
         default=False, description="Whether orphan datasets should be ingested"
     )
