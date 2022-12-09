bdbbe1dd61fa5b762ba91a3d99a725c9774fb794
Maggie Hays <maggiem.hays@gmail.com>
2022-12-08 21:01:23 -0600

    lowcase bigquery ingestion guide filenames

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -109,9 +109,9 @@ module.exports = {
     "Integration Guides": [
       {
         BigQuery: [
-          "docs/quick-ingestion-guides/BigQuery/Overview",
-          "docs/quick-ingestion-guides/BigQuery/Setup",
-          "docs/quick-ingestion-guides/BigQuery/Configuration",
+          "docs/quick-ingestion-guides/bigquery/overview",
+          "docs/quick-ingestion-guides/bigquery/setup",
+          "docs/quick-ingestion-guides/bigquery/configuration",
         ],
       },
     ],
