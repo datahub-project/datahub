b3d423b9e4138ec2a68f65c361b654d165f95805
PatrickfBraz <patrickfbraz@poli.ufrj.br>
2022-12-13 10:04:22 -0300

    refactor(ingest): bigquery-lineage - creates a parameter to allow tables and datasets in uppercase

--- metadata-ingestion/src/datahub/utilities/sql_lineage_parser_impl.py
+++ metadata-ingestion/src/datahub/utilities/sql_lineage_parser_impl.py
@@ -28,9 +28,10 @@ class SqlLineageSQLParserImpl(SQLParser):
     _MYVIEW_SQL_TABLE_NAME_TOKEN = "__my_view__.__sql_table_name__"
     _MYVIEW_LOOKER_TOKEN = "my_view.SQL_TABLE_NAME"
 
-    def __init__(self, sql_query: str) -> None:
+    def __init__(self, sql_query: str, use_raw_names: bool = False) -> None:
         super().__init__(sql_query)
         original_sql_query = sql_query
+        self._use_raw_names = use_raw_names
 
         # SqlLineageParser makes mistakes on lateral flatten queries, use the prefix
         if "lateral flatten" in sql_query:
@@ -88,12 +89,12 @@ class SqlLineageSQLParserImpl(SQLParser):
             ]
 
             with unittest.mock.patch(
-                "sqllineage.core.handlers.source.SourceHandler.end_of_query_cleanup",
-                datahub.utilities.sqllineage_patch.end_of_query_cleanup_patch,
+                    "sqllineage.core.handlers.source.SourceHandler.end_of_query_cleanup",
+                    datahub.utilities.sqllineage_patch.end_of_query_cleanup_patch,
             ):
                 with unittest.mock.patch(
-                    "sqllineage.core.holders.SubQueryLineageHolder.add_column_lineage",
-                    datahub.utilities.sqllineage_patch.add_column_lineage_patch,
+                        "sqllineage.core.holders.SubQueryLineageHolder.add_column_lineage",
+                        datahub.utilities.sqllineage_patch.add_column_lineage_patch,
                 ):
                     self._stmt_holders = [
                         LineageAnalyzer().analyze(stmt) for stmt in self._stmt
@@ -110,7 +111,9 @@ class SqlLineageSQLParserImpl(SQLParser):
             logger.error("sql holder not present so cannot get tables")
             return result
         for table in self._sql_holder.source_tables:
-            table_normalized = re.sub(r"^<default>.", "", table.raw_name)
+            table_normalized = re.sub(
+                r"^<default>.", "", str(table) if not self._use_raw_names else f"{table.schema.raw_name}.{table.raw_name}"
+            )
             result.append(str(table_normalized))
 
         # We need to revert TOKEN replacements
