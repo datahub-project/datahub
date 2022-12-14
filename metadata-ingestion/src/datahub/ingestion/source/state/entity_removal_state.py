8af6d4b9cc0e2f57f1360b20be07c60de9836881
Harshal Sheth <hsheth2@gmail.com>
2022-12-14 15:35:02 -0500

    add state inspect command

--- metadata-ingestion/src/datahub/ingestion/source/state/entity_removal_state.py
+++ metadata-ingestion/src/datahub/ingestion/source/state/entity_removal_state.py
@@ -11,50 +11,6 @@ from datahub.utilities.dedup_list import deduplicate_list
 from datahub.utilities.urns.urn import guess_entity_type
 
 
-class GenericCheckpointState(StaleEntityCheckpointStateBase["GenericCheckpointState"]):
-    urns: List[str] = pydantic.Field(default_factory=list)
-
-    # We store a bit of extra internal-only state so that we can keep the urns list deduplicated.
-    # However, we still want `urns` to be a list so that it maintains its order.
-    # We can't used OrderedSet here because pydantic doesn't recognize it and
-    # it isn't JSON serializable.
-    _urns_set: set = pydantic.PrivateAttr(default_factory=set)
-
-    def __init__(self, **data: Any):  # type: ignore
-        super().__init__(**data)
-        self.urns = deduplicate_list(self.urns)
-        self._urns_set = set(self.urns)
-
-    @classmethod
-    def get_supported_types(cls) -> List[str]:
-        return ["*"]
-
-    def add_checkpoint_urn(self, type: str, urn: str) -> None:
-        if urn not in self._urns_set:
-            self.urns.append(urn)
-            self._urns_set.add(urn)
-
-    def get_urns_not_in(
-        self, type: str, other_checkpoint_state: "GenericCheckpointState"
-    ) -> Iterable[str]:
-        diff = set(self.urns) - set(other_checkpoint_state.urns)
-
-        # To maintain backwards compatibility, we provide this filtering mechanism.
-        if type == "*":
-            yield from diff
-        elif type == "topic":
-            yield from (urn for urn in diff if guess_entity_type(urn) == "dataset")
-        else:
-            yield from (urn for urn in diff if guess_entity_type(urn) == type)
-
-    def get_percent_entities_changed(
-        self, old_checkpoint_state: "GenericCheckpointState"
-    ) -> float:
-        return StaleEntityCheckpointStateBase.compute_percent_entities_changed(
-            [(self.urns, old_checkpoint_state.urns)]
-        )
-
-
 def pydantic_state_migrator(mapping: Dict[str, str]) -> classmethod:
     # mapping would be something like:
     # {
@@ -98,3 +54,62 @@ def pydantic_state_migrator(mapping: Dict[str, str]) -> classmethod:
         return values
 
     return pydantic.root_validator(pre=True, allow_reuse=True)(_validate_field_rename)
+
+
+class GenericCheckpointState(StaleEntityCheckpointStateBase["GenericCheckpointState"]):
+    urns: List[str] = pydantic.Field(default_factory=list)
+
+    # We store a bit of extra internal-only state so that we can keep the urns list deduplicated.
+    # However, we still want `urns` to be a list so that it maintains its order.
+    # We can't used OrderedSet here because pydantic doesn't recognize it and
+    # it isn't JSON serializable.
+    _urns_set: set = pydantic.PrivateAttr(default_factory=set)
+
+    _migration = pydantic_state_migrator(
+        {
+            # From SQL:
+            "encoded_table_urns": "dataset",
+            "encoded_view_urns": "dataset",
+            "encoded_container_urns": "container",
+            "encoded_assertion_urns": "assertion",
+            # From kafka:
+            "encoded_topic_urns": "topic",
+            # From dbt:
+            "encoded_node_urns": "dataset",
+            # "encoded_assertion_urns": "assertion",  # already handled from SQL
+        }
+    )
+
+    def __init__(self, **data: Any):  # type: ignore
+        super().__init__(**data)
+        self.urns = deduplicate_list(self.urns)
+        self._urns_set = set(self.urns)
+
+    @classmethod
+    def get_supported_types(cls) -> List[str]:
+        return ["*"]
+
+    def add_checkpoint_urn(self, type: str, urn: str) -> None:
+        if urn not in self._urns_set:
+            self.urns.append(urn)
+            self._urns_set.add(urn)
+
+    def get_urns_not_in(
+        self, type: str, other_checkpoint_state: "GenericCheckpointState"
+    ) -> Iterable[str]:
+        diff = set(self.urns) - set(other_checkpoint_state.urns)
+
+        # To maintain backwards compatibility, we provide this filtering mechanism.
+        if type == "*":
+            yield from diff
+        elif type == "topic":
+            yield from (urn for urn in diff if guess_entity_type(urn) == "dataset")
+        else:
+            yield from (urn for urn in diff if guess_entity_type(urn) == type)
+
+    def get_percent_entities_changed(
+        self, old_checkpoint_state: "GenericCheckpointState"
+    ) -> float:
+        return StaleEntityCheckpointStateBase.compute_percent_entities_changed(
+            [(self.urns, old_checkpoint_state.urns)]
+        )
