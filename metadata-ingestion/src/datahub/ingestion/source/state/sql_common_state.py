4920873e807adcda14d35dc1ba61de19ef401842
Harshal Sheth <hsheth2@gmail.com>
2022-12-19 13:40:48 -0500

    refactor(ingest/stateful): remove most remaining state classes (#6791)

--- metadata-ingestion/src/datahub/ingestion/source/state/sql_common_state.py
+++ metadata-ingestion/src/datahub/ingestion/source/state/sql_common_state.py
@@ -1,9 +1,3 @@
 from datahub.ingestion.source.state.entity_removal_state import GenericCheckpointState
 
-
-class BaseSQLAlchemyCheckpointState(GenericCheckpointState):
-    """
-    Base class for representing the checkpoint state for all SQLAlchemy based sources.
-    Stores all tables and views being ingested and is used to remove any stale entities.
-    Subclasses can define additional state as appropriate.
-    """
+BaseSQLAlchemyCheckpointState = GenericCheckpointState
