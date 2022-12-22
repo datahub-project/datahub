964bf536351bcf7b5676fa6a3e1ac0d6d86f2258
Chris Collins <chriscollins3456@gmail.com>
2022-12-22 08:44:30 -0500

    format fix

--- metadata-utils/src/test/java/com/linkedin/metadata/utils/IngestionUtilsTest.java
+++ metadata-utils/src/test/java/com/linkedin/metadata/utils/IngestionUtilsTest.java
@@ -21,6 +21,9 @@ public class IngestionUtilsTest {
     String recipe = "{\"source\":{\"type\":\"snowflake\",\"config\":{\"stateful_ingestion\":{\"enabled\":true}}}}";
     recipe = IngestionUtils.injectPipelineName(recipe, ingestionSourceUrn);
 
-    assertEquals(recipe, "{\"source\":{\"type\":\"snowflake\",\"config\":{\"stateful_ingestion\":{\"enabled\":true}}},\"pipeline_name\":\"urn:li:ingestionSource:12345\"}");
+    assertEquals(
+        recipe,
+        "{\"source\":{\"type\":\"snowflake\",\"config\":{\"stateful_ingestion\":{\"enabled\":true}}},\"pipeline_name\":\"urn:li:ingestionSource:12345\"}"
+    );
   }
 }
