774cef8675eadbdf6a12c17ed2535bc2abdad03c
miliv <milivac@ymail.com>
2022-12-22 15:55:11 +0100

    feat(ingest): adding JavaDoc for condUpdate

--- metadata-io/src/main/java/com/linkedin/metadata/params/ExtraIngestParamsHelper.java
+++ metadata-io/src/main/java/com/linkedin/metadata/params/ExtraIngestParamsHelper.java
@@ -11,6 +11,11 @@ public class ExtraIngestParamsHelper {
 
     // condUpdate String format: "urn1+AspectName1=createdOn1;urn2+AspectName2=createdOn2;...;urnN+AspectNameN=createdOnN"
 
+    /**
+     * createdOn is the UNIX epoch in milliseconds when the Urn+aspectName combination is edited for the last time.
+     * For simplicity, we do not support the default If-Unmodified-Since date time format.
+     * We only support the UNIX timestamp.
+     */
     public static Map<String, Long> extractCondUpdate(String condUpdate) {
         Map<String, Long> createdOnMap = new HashMap<>();
         if (condUpdate != null) {
