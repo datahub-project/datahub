2a182f484677d056730d6b4e9f0143e67368359f
david-leifker <114954101+david-leifker@users.noreply.github.com>
2022-12-21 20:11:11 -0600

    fix(pac4j-oidc): add verifier parameter (#6835)
    * fix(pac4j-oidc): add verifier parameter

--- datahub-frontend/app/auth/sso/oidc/custom/CustomOidcAuthenticator.java
+++ datahub-frontend/app/auth/sso/oidc/custom/CustomOidcAuthenticator.java
@@ -15,6 +15,7 @@ import com.nimbusds.oauth2.sdk.auth.Secret;
 import com.nimbusds.oauth2.sdk.http.HTTPRequest;
 import com.nimbusds.oauth2.sdk.http.HTTPResponse;
 import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.pkce.CodeVerifier;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponse;
 import com.nimbusds.openid.connect.sdk.OIDCTokenResponseParser;
 import com.nimbusds.openid.connect.sdk.token.OIDCTokens;
@@ -144,8 +145,10 @@ public class CustomOidcAuthenticator implements Authenticator<OidcCredentials> {
     if (code != null) {
       try {
         final String computedCallbackUrl = client.computeFinalCallbackUrl(context);
+        CodeVerifier verifier = (CodeVerifier) configuration.getValueRetriever()
+                .retrieve(client.getCodeVerifierSessionAttributeName(), client, context).orElse(null);
         // Token request
-        final TokenRequest request = createTokenRequest(new AuthorizationCodeGrant(code, new URI(computedCallbackUrl)));
+        final TokenRequest request = createTokenRequest(new AuthorizationCodeGrant(code, new URI(computedCallbackUrl), verifier));
         HTTPRequest tokenHttpRequest = request.toHTTPRequest();
         tokenHttpRequest.setConnectTimeout(configuration.getConnectTimeout());
         tokenHttpRequest.setReadTimeout(configuration.getReadTimeout());
