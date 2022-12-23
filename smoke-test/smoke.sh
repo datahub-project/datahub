ad78de83d1b1100ce18f86a48d9fc45bc35fb3e4
Aseem Bansal <asmbansal2@gmail.com>
2022-12-23 15:59:12 +0530

    change to fix quickstart run problem

--- smoke-test/smoke.sh
+++ smoke-test/smoke.sh
@@ -14,21 +14,11 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-python3 -m venv venv
-source venv/bin/activate
-pip install --upgrade pip wheel setuptools
-pip install -r requirements.txt
-
-mkdir -p ~/.datahub/plugins/frontend/auth/
-echo "test_user:test_pass" >> ~/.datahub/plugins/frontend/auth/user.props
-
-echo "DATAHUB_VERSION = $DATAHUB_VERSION"
-DATAHUB_TELEMETRY_ENABLED=false  \
-DOCKER_COMPOSE_BASE="file://$( dirname "$DIR" )" \
-
-if [ "${SKIP_QUICKSTART:-false}" == "false" ]; then
-    datahub docker quickstart --standalone_consumers --dump-logs-on-failure
+if [ "${RUN_QUICKSTART:-true}" == "true" ]; then
+    source ./run-quickstart.sh
 fi
+
+source venv/bin/activate
 
 (cd ..; ./gradlew :smoke-test:yarnInstall)
 
