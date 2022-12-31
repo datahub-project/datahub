e4725d5d2b8427559906e6f3d36d02004badac89
David Leifker <david.leifker@acryl.io>
2022-12-31 13:41:38 -0600

    Support aliases during clone Add default clone setting

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/buildindices/PreConfigureESStep.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/buildindices/PreConfigureESStep.java
@@ -7,8 +7,11 @@ import com.linkedin.datahub.upgrade.UpgradeStepResult;
 import com.linkedin.datahub.upgrade.impl.DefaultUpgradeStepResult;
 import com.linkedin.gms.factory.config.ConfigurationProvider;
 import com.linkedin.gms.factory.search.BaseElasticSearchComponentsFactory;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -16,12 +19,15 @@ import com.linkedin.metadata.search.elasticsearch.indexbuilder.ReindexConfig;
 import com.linkedin.metadata.shared.ElasticSearchIndexed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
 import org.elasticsearch.ElasticsearchStatusException;
+import org.elasticsearch.action.admin.indices.alias.get.GetAliasesRequest;
 import org.elasticsearch.action.admin.indices.settings.put.UpdateSettingsRequest;
+import org.elasticsearch.client.GetAliasesResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.indices.ResizeRequest;
 
-import static com.linkedin.datahub.upgrade.buildindices.IndexUtils.*;
+import static com.linkedin.datahub.upgrade.buildindices.IndexUtils.getAllReindexConfigs;
 
 
 @RequiredArgsConstructor
@@ -52,7 +58,23 @@ public class PreConfigureESStep implements UpgradeStep {
                 .collect(Collectors.toList());
 
         for (ReindexConfig indexConfig : indexConfigs) {
-          UpdateSettingsRequest request = new UpdateSettingsRequest(indexConfig.name());
+          String indexName = indexConfig.name();
+
+          GetAliasesResponse aliasResponse = getAlias(indexConfig.name());
+          if (!aliasResponse.getAliases().isEmpty()) {
+            Set<String> indices = aliasResponse.getAliases().keySet();
+            if (indices.size() != 1) {
+              throw new NotImplementedException(
+                      String.format("Clone not supported for %s indices in alias %s. Indicies: %s",
+                              indices.size(),
+                              indexConfig.name(),
+                              String.join(",", indices)));
+            }
+            indexName = indices.stream().findFirst().get();
+            log.info("Alias {} resolved to index {}", indexConfig.name(), indexName);
+          }
+
+          UpdateSettingsRequest request = new UpdateSettingsRequest(indexName);
           Map<String, Object> indexSettings = ImmutableMap.of("index.blocks.write", "true");
 
           request.settings(indexSettings);
@@ -60,6 +82,7 @@ public class PreConfigureESStep implements UpgradeStep {
           try {
             ack =
                 _esComponents.getSearchClient().indices().putSettings(request, RequestOptions.DEFAULT).isAcknowledged();
+            log.info("Updated index {} with new settings. Settings: {}, Acknowledged: {}", indexName, indexSettings, ack);
           } catch (ElasticsearchStatusException ese) {
             // Cover first run case, indices won't exist so settings updates won't work nor will the rest of the preConfigure steps.
             // Since no data are in there they are skippable.
@@ -69,7 +92,7 @@ public class PreConfigureESStep implements UpgradeStep {
             }
             throw ese;
           }
-          log.info("Updated index {} with new settings. Settings: {}, Acknowledged: {}", indexConfig.name(), indexSettings, ack);
+
           if (!ack) {
             log.error("Partial index settings update, some indices may still be blocking writes."
                 + " Please fix the error and re-run the BuildIndices upgrade job.");
@@ -79,10 +102,10 @@ public class PreConfigureESStep implements UpgradeStep {
           // Clone indices
           if (_configurationProvider.getElasticSearch().getBuildIndices().isCloneIndices()) {
             String clonedName = indexConfig.name() + "_clone_" + System.currentTimeMillis();
-            ResizeRequest resizeRequest = new ResizeRequest(clonedName, indexConfig.name());
+            ResizeRequest resizeRequest = new ResizeRequest(clonedName, indexName);
             boolean cloneAck =
                     _esComponents.getSearchClient().indices().clone(resizeRequest, RequestOptions.DEFAULT).isAcknowledged();
-            log.info("Cloned index {} into {}, Acknowledged: {}", indexConfig.name(), clonedName, cloneAck);
+            log.info("Cloned index {} into {}, Acknowledged: {}", indexName, clonedName, cloneAck);
             if (!cloneAck) {
               log.error("Partial index settings update, cloned indices may need to be cleaned up: {}", clonedName);
               return new DefaultUpgradeStepResult(id(), UpgradeStepResult.Result.FAILED);
@@ -95,5 +118,10 @@ public class PreConfigureESStep implements UpgradeStep {
       }
       return new DefaultUpgradeStepResult(id(), UpgradeStepResult.Result.SUCCEEDED);
     };
+  }
+
+  private GetAliasesResponse getAlias(String name) throws IOException {
+    return _esComponents.getSearchClient().indices()
+            .getAlias(new GetAliasesRequest(name), RequestOptions.DEFAULT);
   }
 }
