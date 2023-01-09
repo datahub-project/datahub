5f29cdbd2f2ac8af94a5b41aadfb39dfc3ced21a
Teppo Naakka <teppo.naakka@gmail.com>
2023-01-09 10:44:50 +0200

    fix: logger usage should match convention

--- metadata-ingestion/src/datahub/ingestion/source/powerbi/m_query/parser.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi/m_query/parser.py
@@ -48,7 +48,7 @@ def get_upstream_tables(
     native_query_enabled: bool = True,
 ) -> List[resolver.DataPlatformTable]:
     if table.expression is None:
-        logger.debug("Expression is none: {table.full_name}")
+        logger.debug("Expression is none %s", table.full_name)
         return []
 
     try:
