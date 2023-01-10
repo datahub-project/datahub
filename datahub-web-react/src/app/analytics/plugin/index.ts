6e3a63b717045055b228c72a7f5b67c26aa6acf3
Oliver Sterzel <959372+osterzel@users.noreply.github.com>
2023-01-10 15:48:15 +0000

    CDBD-213: Remove google analytics

--- datahub-web-react/src/app/analytics/plugin/index.ts
+++ datahub-web-react/src/app/analytics/plugin/index.ts
@@ -1,8 +1,7 @@
 import googleAnalytics from './googleAnalytics';
-import googleTagManager from './googleTagManager';
 import mixpanel from './mixpanel';
 import amplitude from './amplitude';
 import datahub from './datahub';
 import logger from './logger';
 
-export default [googleAnalytics, googleTagManager, mixpanel, amplitude, datahub, logger];
+export default [googleAnalytics, mixpanel, amplitude, datahub, logger];
