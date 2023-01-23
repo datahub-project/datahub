26b29b3fe6075b7b92398c4f195e212d92dabd2c
MohdSiddique Bagwan <mohdsiddique.bagwan@gslab.com>
2023-01-23 09:30:44 +0530

    stateful test file

---
+++ metadata-ingestion/tests/integration/powerbi/test_stateful_ingestion.py
@@ -0,0 +1,142 @@
+
+def register_mock_api_state1(request_mock):
+    api_vs_response = {
+        "https://api.powerbi.com/v1.0/myorg/groups": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "id": "64ED5CAD-7C10-4684-8180-826122881108",
+                        "isReadOnly": True,
+                        "name": "Workspace 1",
+                        "type": "Workspace",
+                    },
+                ]
+            },
+        },
+        "https://api.powerbi.com/v1.0/myorg/groups/64ED5CAD-7C10-4684-8180-826122881108/dashboards": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "id": "7D668CAD-7FFC-4505-9215-655BCA5BEBAE",
+                        "isReadOnly": True,
+                        "displayName": "marketing",
+                        "embedUrl": "https://localhost/dashboards/embed/1",
+                        "webUrl": "https://localhost/dashboards/web/1",
+                    },
+                    {
+                        "id": "e41cbfe7-9f54-40ad-8d6a-043ab97cf303",
+                        "isReadOnly": True,
+                        "displayName": "sales",
+                        "embedUrl": "https://localhost/dashboards/embed/1",
+                        "webUrl": "https://localhost/dashboards/web/1",
+                    }
+
+                ]
+            },
+        },
+        "https://api.powerbi.com/v1.0/myorg/groups/64ED5CAD-7C10-4684-8180-826122881108/dashboards/7D668CAD-7FFC-4505-9215-655BCA5BEBAE/tiles": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                ]
+            },
+        },
+        "https://api.powerbi.com/v1.0/myorg/groups/64ED5CAD-7C10-4684-8180-826122881108/dashboards/e41cbfe7-9f54-40ad-8d6a-043ab97cf303/tiles": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                ]
+            },
+        },
+
+    }
+
+    for url in api_vs_response.keys():
+        request_mock.register_uri(
+            api_vs_response[url]["method"],
+            url,
+            json=api_vs_response[url]["json"],
+            status_code=api_vs_response[url]["status_code"],
+        )
+
+
+def register_mock_api_state2(request_mock):
+    api_vs_response = {
+        "https://api.powerbi.com/v1.0/myorg/groups": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "id": "64ED5CAD-7C10-4684-8180-826122881108",
+                        "isReadOnly": True,
+                        "name": "Workspace 1",
+                        "type": "Workspace",
+                    },
+                ]
+            },
+        },
+        "https://api.powerbi.com/v1.0/myorg/groups/64ED5CAD-7C10-4684-8180-826122881108/dashboards": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                    {
+                        "id": "7D668CAD-7FFC-4505-9215-655BCA5BEBAE",
+                        "isReadOnly": True,
+                        "displayName": "marketing",
+                        "embedUrl": "https://localhost/dashboards/embed/1",
+                        "webUrl": "https://localhost/dashboards/web/1",
+                    },
+                    {
+                        "id": "e41cbfe7-9f54-40ad-8d6a-043ab97cf303",
+                        "isReadOnly": True,
+                        "displayName": "sales",
+                        "embedUrl": "https://localhost/dashboards/embed/1",
+                        "webUrl": "https://localhost/dashboards/web/1",
+                    }
+
+                ]
+            },
+        },
+        "https://api.powerbi.com/v1.0/myorg/groups/64ED5CAD-7C10-4684-8180-826122881108/dashboards/7D668CAD-7FFC-4505-9215-655BCA5BEBAE/tiles": {
+            "method": "GET",
+            "status_code": 200,
+            "json": {
+                "value": [
+                ]
+            },
+        },
+    }
+
+    for url in api_vs_response.keys():
+        request_mock.register_uri(
+            api_vs_response[url]["method"],
+            url,
+            json=api_vs_response[url]["json"],
+            status_code=api_vs_response[url]["status_code"],
+        )
+
+
+def default_source_config():
+    return {
+        "client_id": "foo",
+        "client_secret": "bar",
+        "tenant_id": "0B0C960B-FCDF-4D0F-8C45-2E03BB59DDEB",
+        "workspace_id": "64ED5CAD-7C10-4684-8180-826122881108",
+        "extract_lineage": False,
+        "extract_reports": False,
+        "convert_lineage_urns_to_lowercase": False,
+        "workspace_id_pattern": {"allow": ["64ED5CAD-7C10-4684-8180-826122881108"]},
+        "dataset_type_mapping": {
+            "PostgreSql": "postgres",
+            "Oracle": "oracle",
+        },
+        "env": "DEV",
+    }
