e92ce161f917b81897b842c98ff34bf07d3bd48d
Tamas Nemeth <treff7es@gmail.com>
2023-01-24 03:07:14 +0100

    fix(ingest/profile): properly quoting approx_count_distinct (#7101)

--- metadata-ingestion/src/datahub/ingestion/source/ge_data_profiler.py
+++ metadata-ingestion/src/datahub/ingestion/source/ge_data_profiler.py
@@ -119,7 +119,11 @@ def get_column_unique_count_patch(self, column):
     elif self.engine.dialect.name.lower() in {"bigquery", "snowflake"}:
         element_values = self.engine.execute(
             sa.select(
-                [sa.text(f"APPROX_COUNT_DISTINCT({sa.column(column)})")]  # type:ignore
+                [
+                    sa.text(  # type:ignore
+                        f'APPROX_COUNT_DISTINCT("{sa.column(column)}")'
+                    )
+                ]
             ).select_from(self._table)
         )
         return convert_to_json_serializable(element_values.fetchone()[0])
