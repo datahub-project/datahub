8ee9fa19308599841dcb8d917cd408fb8419c463
Patrick Franco Braz <patrickfbraz@poli.ufrj.br>
2023-01-31 11:02:30 -0300

    feat(ingest): bigquery - extracts lineage metadata from catalog api (#7137)

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_config.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_config.py
@@ -112,6 +112,11 @@ class BigQueryV2Config(
         description="This parameter ignores the lowercase pattern stipulated in the SQLParser. NOTE: Ignored if lineage_use_sql_parser is False.",
     )
 
+    extract_lineage_from_catalog: bool = Field(
+        default=False,
+        description="This flag enables the data lineage extraction from Data Lineage API exposed by Google Data Catalog. NOTE: This extractor can't build views lineage. It's recommended to enable the view's DDL parsing. Read the docs to have more information about: https://cloud.google.com/data-catalog/docs/reference/data-lineage/rest",
+    )
+
     convert_urns_to_lowercase: bool = Field(
         default=False,
         description="Convert urns to lowercase.",
