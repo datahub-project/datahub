793e1ca35411f9486f2cda01b2378f2c696c5c7b
Aseem Bansal <asmbansal2@gmail.com>
2023-01-31 22:25:36 +0530

    docs(managed datahub): release notes for v0.1.73

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -463,6 +463,7 @@ module.exports = {
 
     "Release History": ["releases"],
     "Managed DataHub Release History": [
+      "docs/managed-datahub/release-notes/v_0_1_73",
       "docs/managed-datahub/release-notes/v_0_1_72",
       "docs/managed-datahub/release-notes/v_0_1_70",
       "docs/managed-datahub/release-notes/v_0_1_69",
