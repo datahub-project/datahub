906dfa947cfdb875b130cea16e2f6c1d0d61a023
Harshal Sheth <hsheth2@gmail.com>
2023-02-01 18:36:50 -0800

    refactor test markers

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -470,6 +470,7 @@ base_dev_requirements = {
     *list(
         dependency
         for plugin in [
+            "athena",
             "bigquery",
             "clickhouse",
             "clickhouse-usage",
@@ -492,6 +493,7 @@ base_dev_requirements = {
             "kafka",
             "datahub-rest",
             "datahub-lite",
+            "great-expectations",
             "presto",
             "redash",
             "redshift",
@@ -530,6 +532,7 @@ full_test_dev_requirements = {
             "clickhouse",
             "delta-lake",
             "druid",
+            "feast" if sys.version_info >= (3, 8) else None,
             "hana",
             "hive",
             "iceberg" if sys.version_info >= (3, 8) else None,
