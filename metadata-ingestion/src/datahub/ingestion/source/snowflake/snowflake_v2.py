32ab7949ded5bc9fa8ec037336d704903256b722
Tamas Nemeth <treff7es@gmail.com>
2023-02-17 22:23:16 +0100

    fix(ingestion/snowflake): Fixing stateful ingestion commit at Snowflake source (#7363)

--- metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_v2.py
+++ metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_v2.py
@@ -1554,6 +1554,7 @@ class SnowflakeV2Source(
 
     def close(self) -> None:
         super().close()
+        super(StatefulIngestionSourceBase, self).close()
         if hasattr(self, "lineage_extractor"):
             self.lineage_extractor.close()
         if hasattr(self, "usage_extractor"):
