e7f6ca5de776d0e5c79550180007890c895af84c
Aseem Bansal <asmbansal2@gmail.com>
2023-02-20 16:37:17 +0530

    fix(ingest): add missing package, add workflow to detect problem

---
+++ .github/scripts/check_packages.py
@@ -0,0 +1,15 @@
+import setuptools
+import pprint
+
+where = "./src"
+package_list = set(setuptools.find_packages(where))
+namespace_packages = set(setuptools.find_namespace_packages(where))
+diff1 = package_list.difference(namespace_packages)
+if len(diff1) > 0:
+    pprint.pprint(diff1)
+diff2 = namespace_packages.difference(package_list)
+if len(diff2) > 0:
+    pprint.pprint(diff2)
+
+assert len(diff1) == 0
+assert len(diff2) == 0
