84f945f4c442795f7b63b8efd2da9d7d52f0b076
Aseem Bansal <asmbansal2@gmail.com>
2023-02-20 16:55:01 +0530

    improve pytest

--- metadata-ingestion/tests/unit/test_packages.py
+++ metadata-ingestion/tests/unit/test_packages.py
@@ -4,7 +4,4 @@ def test_package_list_match_inits():
     where = "./src"
     package_list = set(setuptools.find_packages(where))
     namespace_packages = set(setuptools.find_namespace_packages(where))
-    diff1 = package_list.difference(namespace_packages)
-    diff2 = namespace_packages.difference(package_list)
-    assert len(diff1) == 0
-    assert len(diff2) == 0
+    assert package_list == namespace_packages
