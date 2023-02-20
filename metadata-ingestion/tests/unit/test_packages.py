181a895a63a43bd51c79e93caa937dd254e41b12
Aseem Bansal <asmbansal2@gmail.com>
2023-02-20 16:51:48 +0530

    fix test name

--- metadata-ingestion/tests/unit/test_packages.py
+++ metadata-ingestion/tests/unit/test_packages.py
@@ -1,6 +1,6 @@
 import setuptools
 
-def test_oracle_config():
+def test_package_list_match_inits():
     where = "./src"
     package_list = set(setuptools.find_packages(where))
     namespace_packages = set(setuptools.find_namespace_packages(where))
