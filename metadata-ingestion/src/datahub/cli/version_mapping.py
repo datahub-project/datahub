815a1b0f970ed06218f77c48ce71c52f50d22e13
Peter Szalai <szalaipeti.vagyok@gmail.com>
2023-02-23 10:46:47 +0100

    Use pydantic instead of dataclass

--- metadata-ingestion/src/datahub/cli/version_mapping.py
+++ metadata-ingestion/src/datahub/cli/version_mapping.py
@@ -1,29 +1,42 @@
 
 import json
 from dataclasses import dataclass
+from pydantic import BaseModel
 from typing import Any, Dict, List
 
 import yaml
 
-@dataclass
-class QuickstartVersionMappings:
-    composefileGitRef: str
-    dockerTag: str
+class QuickstartVersionMapping(BaseModel):
+    composefile_git_ref: str
+    docker_tag: str
 
-@dataclass
-class StableVersions:
+class StableVersions(BaseModel):
     force: bool
-    composefileGitRef: str
-    dockerTag: str
+    composefile_git_ref: str
+    docker_tag: str
 
-@dataclass
-class QuickstartChecks:
-    validUntilGitRef: str
-    requieredContainers: List[str]
-    ensureExistSuccess: List[str]
+class QuickstartChecks(BaseModel):
+    valid_until_git_ref: str
+    required_containers: List[str]
+    ensure_exit_success: List[str]
 
-@dataclass
-class QuickstartVersionMappingCongif:
-    quickstartVersionMappings: Dict[str, QuickstartVersionMappings]
-    stableVersions: StableVersions
-    quickstartChecks: List[QuickstartChecks]
+class QuickstartVersionMappingConfig(BaseModel):
+    quickstart_version_mappings: Dict[str, QuickstartVersionMapping]
+    stable_versions: StableVersions
+    quickstart_checks: List[QuickstartChecks]
+
+@click.group()
+def test():
+    pass
+
+@test.command()
+def qs_test():
+    config: QuickstartVersionMappingConfig
+    
+    with open("/Users/szalai1/workspace/datahub/docker/quickstart/quickstart_version_mapping.yaml") as f:
+        config_raw = yaml.safe_load(f)
+        click.echo(config_raw)
+        config = QuickstartVersionMappingConfig.parse_obj(config_raw)
+
+    print(config)
+
