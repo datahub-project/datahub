99b55b2708c447f0e48b4d7b3c134c0bce5b620b
Harshal Sheth <hsheth2@gmail.com>
2023-02-28 15:56:50 -0800

    add pragmas for perf

--- metadata-ingestion/src/datahub/utilities/file_backed_collections.py
+++ metadata-ingestion/src/datahub/utilities/file_backed_collections.py
@@ -15,10 +15,20 @@ class FileBackedDict(MutableMapping[str, _VT], Generic[_VT]):
         self._filename = filename or tempfile.mktemp()
 
         self._conn = sqlite3.connect(self._filename, isolation_level=None)
+
+        # These settings are optimized for performance.
+        # See https://www.sqlite.org/pragma.html for more information.
+        # Because we're only using this file to offload data from memory, we don't need
+        # to worry about data integrity too much.
+        self._conn.execute('PRAGMA locking_mode = "EXCLUSIVE"')
+        self._conn.execute('PRAGMA synchronous = "OFF"')
+        self._conn.execute('PRAGMA journal_mode = "MEMORY"')
+        self._conn.execute(f"PRAGMA journal_size_limit = {100 * 1024 * 1024}")  # 100MB
+
+        # The key will automatically be indexed.
         self._conn.execute(
             "CREATE TABLE IF NOT EXISTS data (key TEXT PRIMARY KEY, value BLOB)"
         )
-        self._conn.execute('PRAGMA synchronous = "OFF"')
 
     def __getitem__(self, key: str) -> _VT:
         cursor = self._conn.execute("SELECT value FROM data WHERE key = ?", (key,))
