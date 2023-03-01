be49f14af44af40c55a54953988bea51a4e59c36
Harshal Sheth <hsheth2@gmail.com>
2023-02-28 18:12:22 -0800

    finish basic cache for the cache

--- metadata-ingestion/src/datahub/utilities/file_backed_collections.py
+++ metadata-ingestion/src/datahub/utilities/file_backed_collections.py
@@ -1,8 +1,11 @@
+import collections
 import sqlite3
 import tempfile
-from typing import Generic, Iterator, MutableMapping, Optional, TypeVar
+from typing import Generic, Iterator, MutableMapping, Optional, OrderedDict, TypeVar
 
 _VT = TypeVar("_VT")
+
+_CACHE_EVICTION_ALLOWED_OVERAGE = 10
 
 
 class FileBackedDict(MutableMapping[str, _VT], Generic[_VT]):
@@ -11,10 +14,22 @@ class FileBackedDict(MutableMapping[str, _VT], Generic[_VT]):
     This is useful for storing large amounts of data that don't fit in memory.
     """
 
-    def __init__(self, filename: Optional[str] = None):
+    _cache_max_size: int
+    _filename: str
+    _conn: sqlite3.Connection
+
+    _active_object_cache: OrderedDict[str, _VT]
+
+    def __init__(self, filename: Optional[str] = None, cache_max_size: int = 2000):
+        self._cache_max_size = cache_max_size
         self._filename = filename or tempfile.mktemp()
 
         self._conn = sqlite3.connect(self._filename, isolation_level=None)
+
+        # We keep a small cache in memory to avoid having to serialize/deserialize
+        # data from the database too often. We use an OrderedDict to build
+        # a poor-man's LRU cache.
+        self._active_object_cache = collections.OrderedDict()
 
         # These settings are optimized for performance.
         # See https://www.sqlite.org/pragma.html for more information.
@@ -30,31 +45,71 @@ class FileBackedDict(MutableMapping[str, _VT], Generic[_VT]):
             "CREATE TABLE IF NOT EXISTS data (key TEXT PRIMARY KEY, value BLOB)"
         )
 
+    def _add_to_cache(self, key: str, value: _VT) -> None:
+        self._active_object_cache[key] = value
+
+        if (
+            len(self._active_object_cache)
+            > self._cache_max_size + _CACHE_EVICTION_ALLOWED_OVERAGE
+        ):
+            num_items_to_prune = len(self._active_object_cache) - self._cache_max_size
+            self._prune_cache(num_items_to_prune)
+
+    def _prune_cache(self, num_items_to_prune: int) -> None:
+        items_to_write = []
+        for _ in range(num_items_to_prune):
+            key, value = self._active_object_cache.popitem(last=False)
+            items_to_write.append((key, value))
+
+        self._conn.executemany(
+            "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)", items_to_write
+        )
+
     def __getitem__(self, key: str) -> _VT:
+        if key in self._active_object_cache:
+            self._active_object_cache.move_to_end(key)
+            return self._active_object_cache[key]
+
         cursor = self._conn.execute("SELECT value FROM data WHERE key = ?", (key,))
         result = cursor.fetchone()
         if result is None:
             raise KeyError(key)
+
+        self._add_to_cache(key, result[0])
         return result[0]
 
     def __setitem__(self, key: str, value: _VT) -> None:
-        self._conn.execute(
-            "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)", (key, value)
-        )
-        self._conn.commit()
+        self._add_to_cache(key, value)
 
     def __delitem__(self, key: str) -> None:
+        self[key]  # raise KeyError if key doesn't exist
+
+        if key in self._active_object_cache:
+            del self._active_object_cache[key]
+
         self._conn.execute("DELETE FROM data WHERE key = ?", (key,))
-        self._conn.commit()
 
     def __iter__(self) -> Iterator[str]:
         cursor = self._conn.execute("SELECT key FROM data")
         for row in cursor:
+            if row[0] in self._active_object_cache:
+                # If the key is in the active object cache, then SQL isn't the source of truth.
+                continue
+
             yield row[0]
 
+        for key in self._active_object_cache:
+            yield key
+
     def __len__(self) -> int:
-        cursor = self._conn.execute("SELECT COUNT(*) FROM data")
-        return cursor.fetchone()[0]
+        cursor = self._conn.execute(
+            # Binding a list of values in SQLite: https://stackoverflow.com/a/1310001/5004662.
+            f"SELECT COUNT(*) FROM data WHERE key NOT IN ({','.join('?' * len(self._active_object_cache))})",
+            (*self._active_object_cache.keys(),),
+        )
+        row = cursor.fetchone()
+
+        return row[0] + len(self._active_object_cache)
 
     def __repr__(self) -> str:
         return f"FileBackedDict({self._filename})"
--- metadata-ingestion/tests/unit/utilities/test_file_backed_collections.py
+++ metadata-ingestion/tests/unit/utilities/test_file_backed_collections.py
@@ -4,21 +4,54 @@ from datahub.utilities.file_backed_collections import FileBackedDict
 
 
 def test_file_dict():
-    cache = FileBackedDict[int]()
+    cache = FileBackedDict[int](cache_max_size=10)
 
     for i in range(100):
         cache[f"key-{i}"] = i
 
     assert len(cache) == 100
+    assert sorted(cache) == sorted([f"key-{i}" for i in range(100)])
 
+    # Force eviction of everything.
+    cache._prune_cache(num_items_to_prune=len(cache._active_object_cache))
+
+    # Test getting a key.
+    # This implicitly also tests that cache eviction happens.
     for i in range(100):
         assert cache[f"key-{i}"] == i
 
+    # Make sure that the cache is being automatically evicted.
+    assert len(cache._active_object_cache) < 50
+
+    # Test overwriting a key.
+    cache["key-3"] = 3000
+    assert cache["key-3"] == 3000
+
+    # Test repeated overwrites.
+    for i in range(100):
+        cache["key-3"] = i
+    assert cache["key-3"] == 99
+    cache["key-3"] = 3
+
+    # Test deleting a key.
     del cache["key-0"]
     assert len(cache) == 99
     with pytest.raises(KeyError):
         cache["key-0"]
 
+    # Test deleting a key that doesn't exist.
+    with pytest.raises(KeyError):
+        del cache["key-0"]
+
+    # Test adding another key.
     cache["a"] = 1
     assert cache["a"] == 1
     assert len(cache) == 100
+    assert sorted(cache) == sorted(["a"] + [f"key-{i}" for i in range(1, 100)])
+
+    # Test deleting most things.
+    for i in range(1, 100):
+        assert cache[f"key-{i}"] == i
+        del cache[f"key-{i}"]
+    assert len(cache) == 1
+    assert cache["a"] == 1
