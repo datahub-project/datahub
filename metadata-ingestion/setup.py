aa4228734cc386560b5604a8c8af4aaa47fed690
J Feldman <8128360+feljen@users.noreply.github.com>
2023-03-07 19:49:37 -0500

    feat(ingest/looker): upgrade to Looker API from 3.1 to 4.0 (#7411)
    Co-authored-by: Tamas Nemeth <treff7es@gmail.com>
    Co-authored-by: Shirshanka Das <shirshanka@apache.org>

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -138,7 +138,7 @@ path_spec_common = {
 
 looker_common = {
     # Looker Python SDK
-    "looker-sdk==22.2.1",
+    "looker-sdk==23.0.0",
     # This version of lkml contains a fix for parsing lists in
     # LookML files with spaces between an item and the following comma.
     # See https://github.com/joshtemple/lkml/issues/73.
