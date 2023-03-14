510d3aab342645cf1987253f0a3a756f9a2f3a86
Aseem Bansal <asmbansal2@gmail.com>
2023-03-14 19:34:37 +0530

    docs(managed ingestion): add release notes for v0.2.3 (#7578)

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -577,6 +577,7 @@ module.exports = {
 
         {
           "Managed DataHub Release History": [
+            "docs/managed-datahub/release-notes/v_0_2_3",
             "docs/managed-datahub/release-notes/v_0_2_2",
             "docs/managed-datahub/release-notes/v_0_2_1",
             "docs/managed-datahub/release-notes/v_0_2_0",
