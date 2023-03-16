f10b8cc623f0e1d5d3eac7586f158607c85a503e
Aseem Bansal <asmbansal2@gmail.com>
2023-03-16 12:24:58 +0530

    try to run through with python instead of python3

--- smoke-test/run-quickstart.sh
+++ smoke-test/run-quickstart.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-python3 -m venv venv
+python -m venv venv
 source venv/bin/activate
 pip install --upgrade pip wheel setuptools
 pip install -r requirements.txt
