cb62994678e51d37ff98336d11b3a37e55b147da
Aseem Bansal <asmbansal2@gmail.com>
2023-03-16 15:34:19 +0530

    add python change

--- smoke-test/run-quickstart.sh
+++ smoke-test/run-quickstart.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip wheel setuptools
 pip install -r requirements.txt
