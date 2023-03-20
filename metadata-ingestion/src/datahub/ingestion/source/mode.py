7ca79ad4038189bad66b125b6b41a928d3559f3e
rtol5 <110069999+rtol5@users.noreply.github.com>
2023-03-20 06:52:21 -0700

    Update mode.py
    Applied black formatting to the lines i added

--- metadata-ingestion/src/datahub/ingestion/source/mode.py
+++ metadata-ingestion/src/datahub/ingestion/source/mode.py
@@ -216,7 +216,7 @@ class ModeSource(Source):
             modified_actor = builder.make_user_urn(creator)
             if report_info.get('last_saved_at') is None:
                 report_info['last_saved_at'] = report_info.get('created_at')
-                
+
             modified_ts = int(
                 dp.parse(f"{report_info.get('last_saved_at', 'now')}").timestamp()
                 * 1000
