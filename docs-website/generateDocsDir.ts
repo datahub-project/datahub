a9de060bbfab53c9f0efefd8b31cf497a601a4d5
Harshal Sheth <hsheth2@gmail.com>
2023-03-20 13:53:38 -0700

    remove deployment stuff

--- docs-website/generateDocsDir.ts
+++ docs-website/generateDocsDir.ts
@@ -1,5 +1,5 @@
 import { execSync } from "child_process";
-import * as matter from "gray-matter";
+import matter from "gray-matter";
 import * as fs from "fs";
 import * as path from "path";
 import { Octokit } from "@octokit/rest";
