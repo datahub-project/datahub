d6a011e9af0c83ed59924f5e16b77edcbc7fde16
Hyejin Yoon <0327jane@gmail.com>
2023-03-24 08:49:34 +0900

    docs: add guides on deleting entities (#7636)
    Co-authored-by: Hyejin Yoon <yoonhyejin@Hyejins-MacBook-Pro.local>
    Co-authored-by: Hyejin Yoon <yoonhyejin@ip-192-168-0-10.us-west-2.compute.internal>

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -391,6 +391,11 @@ module.exports = {
               ],
             },
             {
+              "Deleting Entities": [
+                "docs/api/tutorials/deleting-entities-by-urn",
+              ],
+            },
+            {
               Reference: [
                 "docs/api/tutorials/references/generate-access-token",
                 "docs/api/tutorials/references/prepare-datahub",
