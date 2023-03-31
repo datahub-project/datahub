83267a72e29c7a3513502c7e65a02f38bc293b5b
Hyejin Yoon <yoonhyejin@ip-192-168-0-10.us-west-2.compute.internal>
2023-03-31 10:49:24 +0900

    style: fix indentation

--- docs-website/src/pages/_components/Hero/index.js
+++ docs-website/src/pages/_components/Hero/index.js
@@ -42,21 +42,21 @@ const Hero = ({}) => {
         </div>
         <Image className="hero__image" img={require(`/img/diagrams/datahub-flow-diagram-${colorMode}.png`)} alt="DataHub Flow Diagram" />
         <div className="quickstart__content">
-            <h1 className="quickstart__title">Start DataHub Now</h1>
-            <p className="quickstart__subtitle">Run the following command to get started with DataHub.</p>
-            <div className="quickstart__codeblock">
-              <CodeBlock className={"language-shell"}>
-                python3 -m pip install --upgrade pip wheel setuptools <br />
-                python3 -m pip install --upgrade acryl-datahub <br />
-                datahub docker quickstart
-              </CodeBlock>
-            </div>
-              <Link className="button button--primary button--md" to={useBaseUrl("docs/quickstart")}>
-              DataHub Quickstart Guide
-              </Link>
-              <Link className="button button--secondary button--outline button--md" to={useBaseUrl("docs/deploy/kubernetes")}>
-              Deploying With Kubernetes
-              </Link>
+          <h1 className="quickstart__title">Start DataHub Now</h1>
+          <p className="quickstart__subtitle">Run the following command to get started with DataHub.</p>
+          <div className="quickstart__codeblock">
+            <CodeBlock className={"language-shell"}>
+              python3 -m pip install --upgrade pip wheel setuptools <br />
+              python3 -m pip install --upgrade acryl-datahub <br />
+              datahub docker quickstart
+            </CodeBlock>
+          </div>
+          <Link className="button button--primary button--md" to={useBaseUrl("docs/quickstart")}>
+            DataHub Quickstart Guide
+          </Link>
+          <Link className="button button--secondary button--outline button--md" to={useBaseUrl("docs/deploy/kubernetes")}>
+            Deploying With Kubernetes
+          </Link>
         </div>
         <div className={clsx("card", styles.quickLinks)}>
           <div className={styles.quickLinksLabel}>
