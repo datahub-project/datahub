0cc12bcce784d3334c36c05995317157420df9e6
Tamas Nemeth <treff7es@gmail.com>
2023-04-12 19:15:43 +0200

    feat(ingest): redshift - Redshift rework (#6906)

--- metadata-ingestion/src/datahub/ingestion/source/source_registry.py
+++ metadata-ingestion/src/datahub/ingestion/source/source_registry.py
@@ -27,6 +27,16 @@ source_registry.register_alias(
     ),
 )
 
+source_registry.register_alias(
+    "redshift-usage",
+    "redshift-usage-legacy",
+    lambda: warnings.warn(
+        "source type redshift-usage is deprecated, use redshift source instead as usage was merged into the main source",
+        ConfigurationWarning,
+        stacklevel=3,
+    ),
+)
+
 # The MSSQL source has two possible sets of dependencies. We alias
 # the second to the first so that we maintain the 1:1 mapping between
 # source type and pip extra.
