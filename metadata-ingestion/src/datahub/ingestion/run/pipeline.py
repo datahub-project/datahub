092026f79ff211cb7095d03c5c1e6be7e0a90d54
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-04-13 13:25:42 -0700

    add try catch

--- metadata-ingestion/src/datahub/ingestion/run/pipeline.py
+++ metadata-ingestion/src/datahub/ingestion/run/pipeline.py
@@ -132,21 +132,26 @@ class CliReport(Report):
     _peak_disk_usage: int = 0
 
     def compute_stats(self) -> None:
-        mem_usage = psutil.Process(os.getpid()).memory_info().rss
-        if self._peak_memory_usage < mem_usage:
-            self._peak_memory_usage = mem_usage
-            self.peak_memory_usage = humanfriendly.format_size(self._peak_memory_usage)
-        self.mem_info = humanfriendly.format_size(mem_usage)
+        try:
+            mem_usage = psutil.Process(os.getpid()).memory_info().rss
+            if self._peak_memory_usage < mem_usage:
+                self._peak_memory_usage = mem_usage
+                self.peak_memory_usage = humanfriendly.format_size(
+                    self._peak_memory_usage
+                )
+            self.mem_info = humanfriendly.format_size(mem_usage)
 
-        disk_usage = shutil.disk_usage("/")
-        if self._peak_disk_usage < disk_usage.used:
-            self._peak_disk_usage = disk_usage.used
-            self.peak_disk_usage = humanfriendly.format_size(self._peak_disk_usage)
-        self.disk_info = {
-            "total": humanfriendly.format_size(disk_usage.total),
-            "used": humanfriendly.format_size(disk_usage.used),
-            "free": humanfriendly.format_size(disk_usage.free),
-        }
+            disk_usage = shutil.disk_usage("/")
+            if self._peak_disk_usage < disk_usage.used:
+                self._peak_disk_usage = disk_usage.used
+                self.peak_disk_usage = humanfriendly.format_size(self._peak_disk_usage)
+            self.disk_info = {
+                "total": humanfriendly.format_size(disk_usage.total),
+                "used": humanfriendly.format_size(disk_usage.used),
+                "free": humanfriendly.format_size(disk_usage.free),
+            }
+        except Exception as e:
+            logger.warning(f"Failed to compute report memory usage: {e}")
 
         return super().compute_stats()
 
