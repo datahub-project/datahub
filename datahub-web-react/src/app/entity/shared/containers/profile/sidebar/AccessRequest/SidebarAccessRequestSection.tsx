9a7782959422b62f1f748fb9794da464b0e74604
rmd9481 <rmd9481@g.rit.edu>
2023-04-14 10:46:48 -0400

    refactor

--- datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SidebarAccessRequestSection.tsx
+++ datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SidebarAccessRequestSection.tsx
@@ -10,8 +10,7 @@ interface Props {
 
 export const SidebarAccessRequestSection = ({ readOnly }: Props) => {
     useEffect(() => {
-        const jiraScriptUrl =
-            'https://sharphealthcare.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/1jmxwi/b/8/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=2dd71ebc';
+        const jiraScriptUrl = "jira_url";
 
         const script = document.createElement('script');
         script.src = jiraScriptUrl;
