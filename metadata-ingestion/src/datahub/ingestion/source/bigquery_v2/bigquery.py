e49bef5875cbadeafac5dfac6c04d02e34dc0ec2
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-04-13 23:56:53 -0700

    fix(ingest/bigquery): Enable lineage and usage ingestion without tables

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery.py
@@ -748,6 +748,17 @@ class BigqueryV2Source(StatefulIngestionSourceBase, TestableSource):
                     project_id=project_id,
                     dataset_name=dataset_name,
                 )
+        elif self.config.include_table_lineage or self.config.include_usage_statistics:
+            # Need table_refs to calculate lineage and usage
+            for table in self.get_core_table_details(conn, dataset_name, project_id):
+                table_identifier = BigqueryTableIdentifier(
+                    project_id=project_id,
+                    dataset=dataset_name,
+                    table=table,
+                )
+                self.table_refs.add(
+                    str(BigQueryTableRef(table_identifier).get_sanitized_table_ref())
+                )
 
         if self.config.include_views:
             db_views[dataset_name] = list(
