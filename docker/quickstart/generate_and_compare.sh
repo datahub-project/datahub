625221a30bb53fd449de96a600c1bf60dda86273
Harshal Sheth <hsheth2@gmail.com>
2023-04-21 15:02:59 -0700

    fix(cli): remove duplicate labels from quickstart files
    Initially reported in https://github.com/datahub-project/datahub/pull/7878. I'm also refactoring some of our code to be a bit more maintainable / readable moving forwards.

--- docker/quickstart/generate_and_compare.sh
+++ docker/quickstart/generate_and_compare.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# this scripts checks if docker-compose$flavour.quickstart.yml is up to date for these 'flavours':
-FLAVOURS=("" "-without-neo4j" ".monitoring")
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
@@ -12,21 +9,4 @@ python3 -m venv venv
 source venv/bin/activate
 
 pip install -r requirements.txt
-python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml temp.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose-without-neo4j.yml ../docker-compose-without-neo4j.override.yml temp-without-neo4j.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose.yml ../docker-compose.override.yml ../docker-compose.m1.yml temp-m1.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose-without-neo4j.yml ../docker-compose-without-neo4j.override.yml ../docker-compose-without-neo4j.m1.yml temp-without-neo4j-m1.quickstart.yml
-python generate_docker_quickstart.py ../monitoring/docker-compose.monitoring.yml temp.monitoring.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose.consumers.yml temp.consumers.quickstart.yml
-python generate_docker_quickstart.py ../docker-compose.consumers-without-neo4j.yml temp.consumers-without-neo4j.quickstart.yml
-
-for flavour in "${FLAVOURS[@]}"
-do
-
-  if cmp <(yq -i -P 'sort_keys(..)' docker-compose$flavour.quickstart.yml) <(yq -i -P 'sort_keys(..)' temp$flavour.quickstart.yml); then
-    echo "docker-compose$flavour.quickstart.yml is up to date."
-  else
-    echo "docker-compose$flavour.quickstart.yml is out of date."
-    exit 1
-  fi
-done
+python generate_docker_quickstart.py check-all
