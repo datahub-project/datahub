00ca6ca9aa34a1b7580f21b8ef938a1999318d28
rmd9481 <rmd9481@g.rit.edu>
2023-04-24 12:19:17 -0400

    changes

--- datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SetupJiraIssueCollector.tsx
+++ datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SetupJiraIssueCollector.tsx
@@ -38,4 +38,3 @@ export const setupJiraIssueCollector = (customButtonId: string) => {
         };
     }
 };
-
--- datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SidebarAccessRequestSection.tsx
+++ datahub-web-react/src/app/entity/shared/containers/profile/sidebar/AccessRequest/SidebarAccessRequestSection.tsx
@@ -10,7 +10,8 @@ interface Props {
 
 export const SidebarAccessRequestSection = ({ readOnly }: Props) => {
     useEffect(() => {
-        const jiraScriptUrl = "jira_url";
+        const jiraScriptUrl =
+            'https://sharphealthcare.atlassian.net/s/d41d8cd98f00b204e9800998ecf8427e-T/1jmxwi/b/8/c95134bc67d3a521bb3f4331beb9b804/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en-US&collectorId=2dd71ebc';
 
         const script = document.createElement('script');
         script.src = jiraScriptUrl;
@@ -36,4 +37,5 @@ export const SidebarAccessRequestSection = ({ readOnly }: Props) => {
         </div>
     );
 };
+
 export default SidebarAccessRequestSection;
