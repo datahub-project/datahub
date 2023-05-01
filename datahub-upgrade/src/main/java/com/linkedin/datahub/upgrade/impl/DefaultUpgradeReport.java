2d88f20e930ba90548799ba146d2fb0f96043ecf
Aseem Bansal <asmbansal2@gmail.com>
2023-05-01 17:12:20 +0530

    add missing import

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/impl/DefaultUpgradeReport.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/impl/DefaultUpgradeReport.java
@@ -1,6 +1,7 @@
 package com.linkedin.datahub.upgrade.impl;
 
 import com.linkedin.datahub.upgrade.UpgradeReport;
+import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 
