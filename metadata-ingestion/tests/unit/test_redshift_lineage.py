f04362c91bc9de926d4c864ba034fa4c3668651a
treff7es <treff7es@gmail.com>
2023-05-08 09:35:58 +0200

    Adding another test

--- metadata-ingestion/tests/unit/test_redshift_lineage.py
+++ metadata-ingestion/tests/unit/test_redshift_lineage.py
@@ -54,6 +54,23 @@ def test_get_sources_from_query_with_database():
     assert lineage.path == "test.my_schema.my_table"
 
 
+def test_get_sources_from_query_with_non_default_database():
+    config = RedshiftConfig(host_port="localhost:5439", database="test")
+    report = RedshiftReport()
+
+    test_query = """
+        select * from test2.my_schema.my_table
+    """
+    lineage_extractor = RedshiftLineageExtractor(config, report)
+    lineage_datasets = lineage_extractor._get_sources_from_query(
+        db_name="test", query=test_query
+    )
+    assert len(lineage_datasets) == 1
+
+    lineage = lineage_datasets[0]
+    assert lineage.path == "test2.my_schema.my_table"
+
+
 def test_get_sources_from_query_with_only_table():
     config = RedshiftConfig(host_port="localhost:5439", database="test")
     report = RedshiftReport()
