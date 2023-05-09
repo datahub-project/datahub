a58453fdf80b1c67302bad258eecb81f9ba3dbac
amanda-her <912amandahernando@gmail.com>
2023-05-09 09:50:35 +0200

    feat(ingest): tests added

--- metadata-ingestion/tests/unit/test_generic_aspect_transformer.py
+++ metadata-ingestion/tests/unit/test_generic_aspect_transformer.py
@@ -1,6 +1,6 @@
 import json
 import unittest
-from typing import Any, List, Optional, cast
+from typing import Any, List, Optional
 
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.ingestion.api.common import EndOfStream, PipelineContext, RecordEnvelope
@@ -78,7 +78,7 @@ class DummyAddGenericAspectTransformer(GenericAspectTransformer):
     @classmethod
     def create(
         cls, config_dict: dict, ctx: PipelineContext
-    ) -> "GenericAspectTransformer":
+    ) -> "DummyAddGenericAspectTransformer":
         return cls()
 
     def entity_types(self) -> List[str]:
@@ -115,10 +115,7 @@ class AddGenericAspectTransformer(unittest.TestCase):
         assert inputs[1] == outputs[1].record
         # Check the first entry generates generic aspect.
         last_event = outputs[2].record
-        assert (
-            last_event.entityUrn
-            == cast(MetadataChangeEventClass, inputs[0]).proposedSnapshot.urn
-        )
+        assert last_event.entityUrn == mce_dataset.proposedSnapshot.urn
         assert isinstance(last_event, MetadataChangeProposalClass)
         assert isinstance(last_event.aspect, GenericAspectClass)
         assert (json.loads(last_event.aspect.value))["customAspect"] == 10
@@ -144,10 +141,7 @@ class AddGenericAspectTransformer(unittest.TestCase):
         assert inputs[1] == outputs[1].record
         # Check the first entry generates generic aspect.
         last_event = outputs[2].record
-        assert (
-            last_event.entityUrn
-            == cast(MetadataChangeProposalWrapper, inputs[0]).entityUrn
-        )
+        assert last_event.entityUrn == mcpw_dataset.entityUrn
         assert isinstance(last_event, MetadataChangeProposalClass)
         assert isinstance(last_event.aspect, GenericAspectClass)
         assert (json.loads(last_event.aspect.value))["customAspect"] == 10
@@ -173,15 +167,145 @@ class AddGenericAspectTransformer(unittest.TestCase):
         assert inputs[1] == outputs[1].record
         # Check the first entry generates generic aspect.
         last_event = outputs[2].record
-        assert (
-            last_event.entityUrn
-            == cast(MetadataChangeProposalClass, inputs[0]).entityUrn
-        )
+        assert last_event.entityUrn == mcpc_dataset.entityUrn
         assert isinstance(last_event, MetadataChangeProposalClass)
         assert isinstance(last_event.aspect, GenericAspectClass)
         assert (json.loads(last_event.aspect.value))["customAspect"] == 10
         # Verify that the last entry is EndOfStream
         assert inputs[2] == outputs[3].record
+
+
+class DummyModifyGenericAspectTransformer(GenericAspectTransformer):
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def create(
+        cls, config_dict: dict, ctx: PipelineContext
+    ) -> "DummyModifyGenericAspectTransformer":
+        return cls()
+
+    def entity_types(self) -> List[str]:
+        return ["dataset"]
+
+    def aspect_name(self) -> str:
+        return "customAspect"
+
+    def transform_generic_aspect(
+        self, entity_urn: str, aspect_name: str, aspect: Optional[GenericAspectClass]
+    ) -> Optional[GenericAspectClass]:
+        value = (
+            aspect.value if aspect else json.dumps({"customAspect": 10}).encode("utf-8")
+        )
+        result_aspect = GenericAspectClass(
+            contentType="application/json",
+            value=value,
+        )
+        return result_aspect
+
+
+class ModifyGenericAspectTransformer(unittest.TestCase):
+    def test_modify_generic_aspect_when_mcpc_received(self):
+        mcpc_dataset_without_custom_aspect = make_mcpc()
+        mcpc_dataset_with_custom_aspect = make_mcpc(
+            entity_urn="urn:li:dataset:(urn:li:dataPlatform:snowflake,example1,PROD)",
+            aspect_name="customAspect",
+            aspect=GenericAspectClass(
+                contentType="application/json",
+                value=json.dumps({"customAspect": 5}).encode("utf-8"),
+            ),
+        )
+        mcpc_datajob = make_mcpc(
+            entity_urn="urn:li:dataJob:(urn:li:dataFlow:(airflow,dag_abc,PROD),task_456)"
+        )
+        inputs = [
+            mcpc_dataset_without_custom_aspect,
+            mcpc_dataset_with_custom_aspect,
+            mcpc_datajob,
+            EndOfStream(),
+        ]
+        outputs = list(
+            DummyModifyGenericAspectTransformer().transform(
+                [RecordEnvelope(input, metadata={}) for input in inputs]
+            )
+        )
+
+        assert len(outputs) == len(inputs) + 1
+        # Verify that the first entry is unchanged.
+        assert inputs[0] == outputs[0].record
+        # Check the second entry has original generic aspect.
+        assert outputs[1].record.entityUrn == mcpc_dataset_with_custom_aspect.entityUrn
+        assert isinstance(outputs[1].record, MetadataChangeProposalClass)
+        assert isinstance(outputs[1].record.aspect, GenericAspectClass)
+        assert (json.loads(outputs[1].record.aspect.value))["customAspect"] == 5
+        # Verify that the third entry is unchanged.
+        assert inputs[2] == outputs[2].record
+        # Check the first entry generates generic aspect.
+        last_event = outputs[3].record
+        assert last_event.entityUrn == mcpc_dataset_without_custom_aspect.entityUrn
+        assert isinstance(last_event, MetadataChangeProposalClass)
+        assert isinstance(last_event.aspect, GenericAspectClass)
+        assert (json.loads(last_event.aspect.value))["customAspect"] == 10
+        # Verify that the last entry is EndOfStream
+        assert inputs[3] == outputs[4].record
+
+
+class DummyRemoveGenericAspectTransformer(GenericAspectTransformer):
+    def __init__(self):
+        super().__init__()
+
+    @classmethod
+    def create(
+        cls, config_dict: dict, ctx: PipelineContext
+    ) -> "DummyRemoveGenericAspectTransformer":
+        return cls()
+
+    def entity_types(self) -> List[str]:
+        return ["dataset"]
+
+    def aspect_name(self) -> str:
+        return "customAspect"
+
+    def transform_generic_aspect(
+        self, entity_urn: str, aspect_name: str, aspect: Optional[GenericAspectClass]
+    ) -> Optional[GenericAspectClass]:
+        return None
+
+
+class RemoveGenericAspectTransformer(unittest.TestCase):
+    def test_remove_generic_aspect_when_mcpc_received(self):
+        mcpc_dataset_without_custom_aspect = make_mcpc()
+        mcpc_dataset_with_custom_aspect = make_mcpc(
+            entity_urn="urn:li:dataset:(urn:li:dataPlatform:snowflake,example1,PROD)",
+            aspect_name="customAspect",
+            aspect=GenericAspectClass(
+                contentType="application/json",
+                value=json.dumps({"customAspect": 5}).encode("utf-8"),
+            ),
+        )
+        mcpc_datajob = make_mcpc(
+            entity_urn="urn:li:dataJob:(urn:li:dataFlow:(airflow,dag_abc,PROD),task_456)"
+        )
+        inputs = [
+            mcpc_dataset_without_custom_aspect,
+            mcpc_dataset_with_custom_aspect,
+            mcpc_datajob,
+            EndOfStream(),
+        ]
+        outputs = list(
+            DummyRemoveGenericAspectTransformer().transform(
+                [RecordEnvelope(input, metadata={}) for input in inputs]
+            )
+        )
+
+        # Check that the second entry is removed.
+        assert len(outputs) == len(inputs) - 1
+        # Verify that the first entry is unchanged.
+        assert inputs[0] == outputs[0].record
+        # Verify that the third entry is unchanged.
+        assert inputs[2] == outputs[1].record
+        # Verify that the last entry is EndOfStream
+        assert inputs[3] == outputs[2].record
 
 
 if __name__ == "__main__":
