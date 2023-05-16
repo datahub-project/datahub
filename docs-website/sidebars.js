004f2b29ab9e2cef289c6fce54048bd64126f47a
yangjd33 <yangjd33@chinaunicom.cn>
2023-05-16 10:55:21 +0800

    fix error of building docs

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -188,6 +188,7 @@ module.exports = {
             "docs/how/kafka-config",
             "docs/deploy/confluent-cloud",
             "docs/advanced/no-code-upgrade",
+            " docs/how/jattach-guide.md",
           ],
         },
         "docs/how/updating-datahub",
