24cc73041b49063105d70179ee73d3bfbb12308a
Pedro Silva <pedro@acryl.io>
2023-05-17 11:49:58 +0100

    Rebase with master after data products PR

--- datahub-web-react/src/app/entity/shared/components/styled/ExpandedOwner/OwnerContent.tsx
+++ datahub-web-react/src/app/entity/shared/components/styled/ExpandedOwner/OwnerContent.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Popover, Typography } from 'antd';
 import styled from 'styled-components/macro';
 import { Owner } from '../../../../../../types.generated';
