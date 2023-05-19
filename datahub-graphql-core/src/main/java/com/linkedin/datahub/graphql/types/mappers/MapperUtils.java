3d7ff86e7cd1667ac08097eecc2fd6e8ff34bc41
Indy Prentice <indy@Indys-MacBook-Pro.local>
2023-05-19 15:14:17 -0500

    Translate entity type to format expected by frontend

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/mappers/MapperUtils.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/mappers/MapperUtils.java
@@ -7,9 +7,11 @@ import com.linkedin.datahub.graphql.generated.SearchResult;
 import com.linkedin.datahub.graphql.resolvers.EntityTypeMapper;
 import com.linkedin.datahub.graphql.types.common.mappers.UrnToEntityMapper;
 import com.linkedin.metadata.search.SearchEntity;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static com.linkedin.datahub.graphql.util.SearchInsightsUtil.*;
 import static com.linkedin.metadata.utils.SearchUtil.*;
@@ -30,7 +32,8 @@ public class MapperUtils {
   public static FacetMetadata mapFacet(com.linkedin.metadata.search.AggregationMetadata aggregationMetadata) {
     final FacetMetadata facetMetadata = new FacetMetadata();
     List<String> aggregationFacets = List.of(aggregationMetadata.getName().split(AGGREGATION_SEPARATOR_CHAR));
-    boolean isEntityTypeFilter = aggregationFacets.contains("entity");
+    List<Boolean> isEntityTypeFilter = aggregationFacets.stream().map(
+        facet -> facet.equals("entity") || facet.contains("_entityType")).collect(Collectors.toList());
     facetMetadata.setField(aggregationMetadata.getName());
     facetMetadata.setDisplayName(
         Optional.ofNullable(aggregationMetadata.getDisplayName()).orElse(aggregationMetadata.getName()));
@@ -43,11 +46,11 @@ public class MapperUtils {
     return facetMetadata;
   }
 
-  public static String convertFilterValue(String filterValue, boolean isEntityType) {
-    if (isEntityType) {
-      return EntityTypeMapper.getType(filterValue).toString();
-    }
-    return filterValue;
+  public static String convertFilterValue(String filterValue, List<Boolean> isEntityTypeFilter) {
+    String[] aggregations = filterValue.split(AGGREGATION_SEPARATOR_CHAR);
+    return IntStream.range(0, aggregations.length).mapToObj(
+        idx -> idx < isEntityTypeFilter.size() && isEntityTypeFilter.get(idx) ? EntityTypeMapper.getType(aggregations[idx]).toString() : aggregations[idx])
+        .collect(Collectors.joining(AGGREGATION_SEPARATOR_CHAR));
   }
 
   public static List<MatchedField> getMatchedFieldEntry(List<com.linkedin.metadata.search.MatchedField> highlightMetadata) {
