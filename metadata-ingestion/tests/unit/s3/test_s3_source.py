14fbf20376d4d6a353f84ac712da4a70ce98c2d4
treff7es <treff7es@gmail.com>
2023-05-19 16:07:12 +0200

    Adding comparator to dir sorting which can sort dirs as numbers if it is number

---
+++ metadata-ingestion/tests/unit/s3/test_s3_source.py
@@ -0,0 +1,43 @@
+from datahub.ingestion.source.s3.source import partitioned_folder_comparator
+
+
+def test_partition_comparator_numeric_folder_name():
+    folder1 = "3"
+    folder2 = "12"
+    assert partitioned_folder_comparator(folder1, folder2) == -1
+
+
+def test_partition_comparator_numeric_folder_name2():
+    folder1 = "12"
+    folder2 = "3"
+    assert partitioned_folder_comparator(folder1, folder2) == 1
+
+
+def test_partition_comparator_string_folder():
+    folder1 = "myfolderB"
+    folder2 = "myFolderA"
+    assert partitioned_folder_comparator(folder1, folder2) == 1
+
+
+def test_partition_comparator_string_same_folder():
+    folder1 = "myFolderA"
+    folder2 = "myFolderA"
+    assert partitioned_folder_comparator(folder1, folder2) == 0
+
+
+def test_partition_comparator_with_numeric_partition():
+    folder1 = "year=3"
+    folder2 = "year=12"
+    assert partitioned_folder_comparator(folder1, folder2) == -1
+
+
+def test_partition_comparator_with_padded_numeric_partition():
+    folder1 = "year=03"
+    folder2 = "year=12"
+    assert partitioned_folder_comparator(folder1, folder2) == -1
+
+
+def test_partition_comparator_with_string_partition():
+    folder1 = "year=year2020"
+    folder2 = "year=year2021"
+    assert partitioned_folder_comparator(folder1, folder2) == -1
