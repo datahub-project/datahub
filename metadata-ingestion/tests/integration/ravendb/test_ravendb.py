5f79ea8c09d5ecbb5eecd878b553566e599adb0c
Verena Barth <verena.barth@viadee.de>
2023-05-23 15:27:23 +0200

    fix: ravendb connection issues

--- metadata-ingestion/tests/integration/ravendb/test_ravendb.py
+++ metadata-ingestion/tests/integration/ravendb/test_ravendb.py
@@ -1,26 +1,46 @@
+import json
+import logging
+import random
+import re
+
+import numpy as np
 import pytest
+from ravendb import DocumentStore
+
 from tests.test_helpers import mce_helpers
 from tests.test_helpers.click_helpers import run_datahub_cmd
 from tests.test_helpers.docker_helpers import wait_for_port
-
-import numpy as np
-import random
-import logging
-import json
-import re
-
-from ravendb import DocumentStore
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 
+DB_RECIPE_FILE = "ravendb_to_file_db.yml"
 RAVENDB_PORT = 8080
 TESTDB_NAME = "testdb"
 CONTAINER_NAME = "testravendb"
+CONTAINER_IP = None
+
 
 # ignore timestamps and random changing values triggered by container start
-IGNORE_KEYS = ["runId", "lastCollectionIndexingTime", "lastDatabaseEtag", "lastDocEtag", "lastObserved", "databaseChangeVector"]
+IGNORE_KEYS = [
+    "runId",
+    "lastCollectionIndexingTime",
+    "lastDatabaseEtag",
+    "lastDocEtag",
+    "lastObserved",
+    "databaseChangeVector",
+]
+
+
+def get_container_ip():
+    return CONTAINER_IP
+
+
+def set_container_ip(value):
+    global CONTAINER_IP
+    logging.debug(f"Setting container ip to: {value}")
+    CONTAINER_IP = value
 
 
 @pytest.fixture(scope="module")
@@ -30,80 +50,24 @@ def test_resources_dir(pytestconfig):
 
 def is_container_running(container_name: str) -> bool:
     """Returns true if the status of the container with the given name is 'Running'"""
-    import docker
     import time
+
+    import docker
+
     client = docker.from_env()
     try:
         container = client.containers.get(container_name)
-        return container.status == 'running'
+        logging.debug(
+            f"Container with name {container_name} found. Status: {container.status}"
+        )
+        time.sleep(60)
+        set_container_ip(container.attrs["NetworkSettings"]["IPAddress"])
+        return container.status == "running"
     except docker.errors.NotFound:
-        print(f"Container with name '{container_name}' not found.")
-        return False  
-
-def load_document_store():
-    logging.debug(f"Loading document store of database '{TESTDB_NAME}'")
-    store = DocumentStore('http://localhost:8080', TESTDB_NAME)  # RAVEN_DATABASE)
-    store.initialize()
-    return store
+        logging.debug(f"Container with name '{container_name}' not found.")
+        return False
 
 
-def remove_database():
-    from ravendb.serverwide.operations.common import DeleteDatabaseOperation
-    logging.debug("Deleting databases")
-    store = load_document_store()
-    store.maintenance.server.send(DeleteDatabaseOperation(database_name=TESTDB_NAME, hard_delete=True))
-
-def prepare_database():
-    logging.debug("Preparing databases")
-    print("loading store")
-    store=load_document_store()
-    print(store)
-    request_executor=store.get_request_executor()
-
-    # inserts
-    from ravendb.documents.commands.crud import PutDocumentCommand, GetDocumentsCommand
-    for i in range(5):
-        put_command1=PutDocumentCommand(key = f"testing/toy{i}", change_vector = None,  # f"test_change_vector",
-                                    document = {"Name": f"test_toy_{i}",
-                                    "Price": str(np.around(random.uniform(1, 100), 2)),
-                                    "Category": "Toy",
-                                    "Brand": "Fisher Price",
-                                    "@metadata": {"Raven-Python-Type": "Products", "@collection": "Products"}})
-        request_executor.execute_command(put_command1)
-        put_command2=PutDocumentCommand(key = f"testing/art{i}", change_vector = None,
-                                    document = {"Name": f"test_art_{i}",
-                                    "Price": str(np.around(random.uniform(1, 100), 2)),
-                                    "Category": "Image",
-                                    "Size": "A4",
-                                    "Shipping": True,
-                                    "@metadata": {"Raven-Python-Type": "Products", "@collection": "Products"}})
-        request_executor.execute_command(put_command2)
-
-    # create index
-    from ravendb.documents.indexes.definitions import IndexDefinition
-    from ravendb.documents.operations.indexes import PutIndexesOperation, GetIndexOperation
-    index=IndexDefinition()
-    index.name="Products/Search"
-
-    map_=(
-        "from p in docs.Products "
-        + "select new { "
-        + "   Name = p.Name, "
-        + "   Category = p.Category,"
-        + "   Id = p.DocumentId "
-        + "}"
-    )
-    index.maps = map_
-    request_executor.execute_command(PutIndexesOperation(index).get_command(store.conventions))
-
-    # assert entries set
-    command = GetDocumentsCommand.from_single_id("testing/art1")
-    request_executor.execute_command(command)
-    assert command.result.results[0]["@metadata"]["@id"] == "testing/art1"
-    command = GetIndexOperation(index.name).get_command(store.conventions)
-    request_executor.execute_command(command)
-    assert command.result != None
-    
 @pytest.fixture(scope="module")
 def ravendb_runner(docker_compose_runner, pytestconfig, test_resources_dir):
     logging.debug("Start RavenDB runner")
@@ -120,41 +84,166 @@ def ravendb_runner(docker_compose_runner, pytestconfig, test_resources_dir):
         yield docker_services
 
 
+@pytest.fixture(scope="module")
+def prepare_config_file(test_resources_dir):
+    import yaml
 
-def test_ravendb_ingest_with_db(ravendb_runner, test_resources_dir, tmp_path, pytestconfig):
+    file_path = (test_resources_dir / DB_RECIPE_FILE).resolve()
+    # Load the YAML file
+    with open(file_path, "r") as file:
+        data = yaml.safe_load(file)
+    # Modify the attribute value
+    data["source"]["config"]["connect_uri"] = data["source"]["config"][
+        "connect_uri"
+    ].replace("ravendb", get_container_ip())
+    logging.debug(f"Replacing connect_uri of config file with '{get_container_ip()}'")
+    logging.debug(data)
+
+    # Save the modified data back to the YAML file
+    with open(file_path, "w") as file:
+        yaml.safe_dump(data, file)
+
+
+def load_document_store():
+    logging.debug(f"Loading document store of database '{TESTDB_NAME}'")
+    container_ip = get_container_ip()
+    print(container_ip)
+    store = DocumentStore(
+        f"http://{container_ip}:{RAVENDB_PORT}", TESTDB_NAME
+    )  # RAVEN_DATABASE)
+    store.initialize()
+    logging.debug(store)
+    return store
+
+
+def remove_database():
+    from ravendb.serverwide.operations.common import DeleteDatabaseOperation
+
+    logging.debug("Deleting databases")
+    store = load_document_store()
+    store.maintenance.server.send(
+        DeleteDatabaseOperation(database_name=TESTDB_NAME, hard_delete=True)
+    )
+
+
+def prepare_database():
+    logging.debug("Preparing databases")
+    store = load_document_store()
+    request_executor = store.get_request_executor()
+
+    # inserts
+    from ravendb.documents.commands.crud import GetDocumentsCommand, PutDocumentCommand
+
+    for i in range(5):
+        put_command1 = PutDocumentCommand(
+            key=f"testing/toy{i}",
+            change_vector=None,
+            document={
+                "Name": f"test_toy_{i}",
+                "Price": str(np.around(random.uniform(1, 100), 2)),
+                "Category": "Toy",
+                "Brand": "Fisher Price",
+                "@metadata": {
+                    "Raven-Python-Type": "Products",
+                    "@collection": "Products",
+                },
+            },
+        )
+        request_executor.execute_command(put_command1)
+        put_command2 = PutDocumentCommand(
+            key=f"testing/art{i}",
+            change_vector=None,
+            document={
+                "Name": f"test_art_{i}",
+                "Price": str(np.around(random.uniform(1, 100), 2)),
+                "Category": "Image",
+                "Size": "A4",
+                "Shipping": True,
+                "@metadata": {
+                    "Raven-Python-Type": "Products",
+                    "@collection": "Products",
+                },
+            },
+        )
+        request_executor.execute_command(put_command2)
+        logging.debug(f"Successfull iteration {i} of inserts.")
+
+    # create index
+    from ravendb.documents.indexes.definitions import IndexDefinition
+    from ravendb.documents.operations.indexes import (
+        GetIndexOperation,
+        PutIndexesOperation,
+    )
+
+    index = IndexDefinition()
+    index.name = "Products/Search"
+
+    index.maps = (
+        "from p in docs.Products "
+        + "select new { "
+        + "   Name = p.Name, "
+        + "   Category = p.Category,"
+        + "   Id = p.DocumentId "
+        + "}"
+    )
+    request_executor.execute_command(
+        PutIndexesOperation(index).get_command(store.conventions)
+    )
+
+    # assert entries set
+    command = GetDocumentsCommand.from_single_id("testing/art1")
+    request_executor.execute_command(command)
+    assert command.result.results[0]["@metadata"]["@id"] == "testing/art1"
+    command = GetIndexOperation(index.name).get_command(store.conventions)
+    request_executor.execute_command(command)
+    assert command.result != None
+
+
+def test_ravendb_ingest_with_db(
+    ravendb_runner, prepare_config_file, test_resources_dir, tmp_path, pytestconfig
+):
     # Set up database
-    prepare_database()  
+    prepare_database()
     # Run the metadata ingestion pipeline
-    config_file = (test_resources_dir / "ravendb_to_file_db.yml").resolve()
+    config_file = (test_resources_dir / DB_RECIPE_FILE).resolve()
     run_datahub_cmd(["ingest", "-c", f"{config_file}"], tmp_path=tmp_path)
 
     golden_path = test_resources_dir / "ravendb_mces_with_db_golden.json"
     output_path = test_resources_dir / "ravendb_mces.json"
     check_golden_file(output_path, golden_path, pytestconfig)
 
-def test_ravendb_ingest_without_collections(ravendb_runner,test_resources_dir, tmp_path, pytestconfig):
+
+def test_ravendb_ingest_without_collections(
+    ravendb_runner, prepare_config_file, test_resources_dir, tmp_path, pytestconfig
+):
     # Run the metadata ingestion pipeline.
-    config_file = (test_resources_dir / "ravendb_to_file_db.yml").resolve()
+    config_file = (test_resources_dir / DB_RECIPE_FILE).resolve()
     run_datahub_cmd(["ingest", "-c", f"{config_file}"], tmp_path=tmp_path)
 
     golden_path = test_resources_dir / "ravendb_mces_no_collections_golden.json"
     output_path = test_resources_dir / "ravendb_mces.json"
     check_golden_file(output_path, golden_path, pytestconfig)
 
-def test_ravendb_ingest_without_collections(ravendb_runner, test_resources_dir, tmp_path, pytestconfig):
+
+def test_ravendb_ingest_without_documentstore(
+    ravendb_runner, prepare_config_file, test_resources_dir, tmp_path, pytestconfig
+):
+    remove_database()
     # Run the metadata ingestion pipeline.
-    config_file = (test_resources_dir / "ravendb_to_file_db.yml").resolve()
+    config_file = (test_resources_dir / DB_RECIPE_FILE).resolve()
     run_datahub_cmd(["ingest", "-c", f"{config_file}"], tmp_path=tmp_path)
 
     golden_path = test_resources_dir / "ravendb_mces_no_documentstore_golden.json"
     output_path = test_resources_dir / "ravendb_mces.json"
     check_golden_file(output_path, golden_path, pytestconfig)
 
+
 def check_golden_file(output_file_path, golden_file_path, pytestconfig):
     """
-    Check mce output file against golden file ignoring the keys in IGNORE_KEYS array 
-    since they are run dependent. 
+    Check mce output file against golden file ignoring the keys in IGNORE_KEYS array
+    since they are run dependent.
     """
+
     def find_key_structure(d, target_key):
         if isinstance(d, dict):
             for k, v in d.items():
@@ -179,11 +268,11 @@ def check_golden_file(output_file_path, golden_file_path, pytestconfig):
             else:
                 replace = f"['{str(key)}'][XX]"
             s = s.replace("[XX]", replace)
-        s =  s.replace("[XX]", "")
+        s = s.replace("[XX]", "")
         # Escape special characters
         regex_string = re.escape(s)
         # Replace numbers with '\d+'
-        regex_string = re.sub(r'\d+', r'\\d+', regex_string)
+        regex_string = re.sub(r"\d+", r"\\d+", regex_string)
         return regex_string
 
     # ignore timestamps and random changing values triggered by container start
@@ -202,5 +291,5 @@ def check_golden_file(output_file_path, golden_file_path, pytestconfig):
         pytestconfig,
         output_path=output_file_path,
         golden_path=golden_file_path,
-        ignore_paths=ignore_key_paths
+        ignore_paths=ignore_key_paths,
     )
