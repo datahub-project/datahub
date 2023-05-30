6a9cc28de6eba4c7afff06bcc9cebefeb46c24e0
junjie <neojunjie87@gmail.com>
2023-05-30 10:42:28 +0700

    removed fieldKey (deprecated prop)

--- datahub-web-react/src/app/create/Components/CsvForm.tsx
+++ datahub-web-react/src/app/create/Components/CsvForm.tsx
@@ -225,7 +225,6 @@ export const CsvForm = () => {
                                             <Form.Item
                                                 {...restField}
                                                 name={[name, 'field_name']}
-                                                fieldKey={[fieldKey, 'field_name']}
                                                 rules={[{ required: true, message: 'Missing field name' }]}
                                             >
                                                 <Input placeholder="Field Name" />
@@ -240,7 +239,6 @@ export const CsvForm = () => {
                                                 <Form.Item
                                                     {...restField}
                                                     name={[name, 'field_type']}
-                                                    fieldKey={[fieldKey, 'field_type']}
                                                     rules={[{ required: true, message: 'Missing field type' }]}
                                                 >
                                                     <Select showSearch placeholder="Select field type">
@@ -262,7 +260,6 @@ export const CsvForm = () => {
                                             <Form.Item
                                                 {...restField}
                                                 name={[name, 'field_description']}
-                                                fieldKey={[fieldKey, 'field_description']}
                                                 rules={[
                                                     {
                                                         required: false,
