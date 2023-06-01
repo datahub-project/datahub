798935e38afc2aade7f37cfbed63bbcce59f2276
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-06-01 06:38:28 -0700

    flags as configmodel

--- metadata-ingestion/src/datahub/ingestion/run/pipeline_config.py
+++ metadata-ingestion/src/datahub/ingestion/run/pipeline_config.py
@@ -37,6 +37,19 @@ class FailureLoggingConfig(ConfigModel):
     log_config: Optional[FileSinkConfig] = None
 
 
+class FlagsConfig(ConfigModel):
+    """Experimental flags for the ingestion pipeline.
+
+    As this is an experimental feature, we do not guarantee backwards compatibility.
+    Use at your own risk!
+    """
+
+    generate_browse_path_v2: bool = Field(
+        default=False,
+        description="Generate BrowsePathsV2 aspects from container hierarchy and existing BrowsePaths aspects.",
+    )
+
+
 class PipelineConfig(ConfigModel):
     # Once support for discriminated unions gets merged into Pydantic, we can
     # simplify this configuration and validation.
@@ -45,7 +58,7 @@ class PipelineConfig(ConfigModel):
     source: SourceConfig
     sink: DynamicTypedConfig
     transformers: Optional[List[DynamicTypedConfig]]
-    flags: Optional[Dict[str, bool]] = None
+    flags: FlagsConfig = None
     reporting: List[ReporterConfig] = []
     run_id: str = DEFAULT_RUN_ID
     datahub_api: Optional[DatahubClientConfig] = None
