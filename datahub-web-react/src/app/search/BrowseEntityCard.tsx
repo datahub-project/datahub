293b7be401a7e9e523bfc2a7bda332681dd6e63b
Chris Collins <chriscollins3456@gmail.com>
2023-06-05 15:29:28 -0400

    Take users to new browse from home page

--- datahub-web-react/src/app/search/BrowseEntityCard.tsx
+++ datahub-web-react/src/app/search/BrowseEntityCard.tsx
@@ -1,5 +1,6 @@
+import styled from 'styled-components';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { useEntityRegistry } from '../useEntityRegistry';
 import { PageRoutes } from '../../conf/Global';
 import { IconStyleType } from '../entity/Entity';
@@ -7,9 +8,17 @@ import { EntityType } from '../../types.generated';
 import { LogoCountCard } from '../shared/LogoCountCard';
 import { EventType } from '../analytics/event';
 import analytics from '../analytics';
+import { useAppConfig } from '../useAppConfig';
+import { navigateToSearchUrl } from './utils/navigateToSearchUrl';
+import { ENTITY_FILTER_NAME } from './utils/constants';
+
+const BrowseEntityCardWrapper = styled.div``;
 
 export const BrowseEntityCard = ({ entityType, count }: { entityType: EntityType; count: number }) => {
+    const appConfig = useAppConfig();
+    const history = useHistory();
     const entityRegistry = useEntityRegistry();
+    const { showBrowseV2 } = appConfig.config.featureFlags;
     const isGlossaryEntityCard = entityType === EntityType.GlossaryTerm;
     const entityPathName = entityRegistry.getPathName(entityType);
     const url = isGlossaryEntityCard ? PageRoutes.GLOSSARY : `${PageRoutes.BROWSE}/${entityPathName}`;
@@ -20,14 +29,26 @@ export const BrowseEntityCard = ({ entityType, count }: { entityType: EntityType
         });
     };
 
+    function browse() {
+        if (showBrowseV2 && !isGlossaryEntityCard) {
+            navigateToSearchUrl({
+                query: '*',
+                filters: [{ field: ENTITY_FILTER_NAME, values: [entityType] }],
+                history,
+            });
+        } else {
+            history.push(url);
+        }
+    }
+
     return (
-        <Link to={url} data-testid={`entity-type-browse-card-${entityType}`}>
+        <BrowseEntityCardWrapper onClick={browse} data-testid={`entity-type-browse-card-${entityType}`}>
             <LogoCountCard
                 logoComponent={entityRegistry.getIcon(entityType, 18, IconStyleType.HIGHLIGHT)}
                 name={entityRegistry.getCollectionName(entityType)}
                 count={count}
                 onClick={onBrowseEntityCardClick}
             />
-        </Link>
+        </BrowseEntityCardWrapper>
     );
 };
