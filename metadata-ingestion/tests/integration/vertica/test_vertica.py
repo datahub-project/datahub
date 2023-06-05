714d1e5bc82094fede5f7c1395d73acdcf81479d
vishalksimplify <vishal.k@simplify3x.com>
2023-06-05 05:26:13 -0400

    updated the golden files

--- metadata-ingestion/tests/integration/vertica/test_vertica.py
+++ metadata-ingestion/tests/integration/vertica/test_vertica.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 import time
 from typing import Optional
@@ -16,6 +17,34 @@ FROZEN_TIME = "2020-04-14 07:00:00"
 # )
 
 
+def generalization(tmp_path):
+    with open(tmp_path / "vertica.json", "r+") as file:
+        jsonObject = json.load(file)
+        file.close()
+
+    with open(tmp_path / "vertica.json", "w+") as file:
+        for object in jsonObject:
+            recursionObj(object)
+        json.dump(jsonObject, file, indent=4)
+        file.close()
+
+
+def recursionObj(object):
+    for key, value in object.items():
+        if key == "create_time":
+            object[key] = "0"
+        elif type(value) == dict:
+            recursionObj(value)
+        elif type(value) == list:
+            recursionList(value)
+
+
+def recursionList(arr):
+    for i in arr:
+        if type(i) == dict:
+            recursionObj(i)
+
+
 @pytest.fixture(scope="module")
 def test_resources_dir(pytestconfig):
     return pytestconfig.rootpath / "tests/integration/vertica"
@@ -26,9 +55,7 @@ def is_vertica_responsive(
 ) -> bool:
     if hostname:
         cmd = f"docker logs {container_name} 2>&1 | grep 'Vertica is now running' "
-    ret = subprocess.run(
-        cmd, shell=True, stdout=subprocess.DEVNULL
-    )
+    ret = subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
 
     return ret.returncode == 0
 
@@ -60,12 +87,9 @@ def vertica_runner(docker_compose_runner, test_resources_dir):
                     /opt/vertica/bin/vsql -w abc123 -f naive_bayes/naivebayes_examples.sql &&
                 """
 
-        ret = subprocess.run(
-            commands, shell=True, stdout=subprocess.DEVNULL
-        )
+        ret = subprocess.run(commands, shell=True, stdout=subprocess.DEVNULL)
         # waiting for vertica to create default table and system table and ml models
         time.sleep(60)
-
 
         assert ret.returncode == 1
 
@@ -75,9 +99,7 @@ def vertica_runner(docker_compose_runner, test_resources_dir):
 # Test needs more work to be done , currently it is working fine.
 @freeze_time(FROZEN_TIME)
 @pytest.mark.integration
-
 def test_vertica_ingest_with_db(vertica_runner, pytestconfig, tmp_path):
-    
     test_resources_dir = pytestconfig.rootpath / "tests/integration/vertica"
 
     # Run the metadata ingestion pipeline.
@@ -85,6 +107,10 @@ def test_vertica_ingest_with_db(vertica_runner, pytestconfig, tmp_path):
     run_datahub_cmd(
         ["ingest", "--strict-warnings", "-c", f"{config_file}"], tmp_path=tmp_path
     )
+
+    # using recursion to read vertica.json and replace value of create_time to 0
+    # while using --update-golden-files please change value of create_time to 0 in golden file .
+    generalization(tmp_path)
 
     # Verify the output.
     mce_helpers.check_golden_file(
