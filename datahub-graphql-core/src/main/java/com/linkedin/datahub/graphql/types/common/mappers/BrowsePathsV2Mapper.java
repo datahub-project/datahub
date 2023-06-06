7da8258810a55d1c7a68af5a3a4f7d5c3f4de3b0
Chris Collins <chriscollins3456@gmail.com>
2023-06-06 10:03:09 -0400

    map instead of loop

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/common/mappers/BrowsePathsV2Mapper.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/common/mappers/BrowsePathsV2Mapper.java
@@ -3,12 +3,11 @@ package com.linkedin.datahub.graphql.types.common.mappers;
 import com.linkedin.common.BrowsePathsV2;
 import com.linkedin.datahub.graphql.generated.BrowsePathEntry;
 import com.linkedin.datahub.graphql.generated.BrowsePathV2;
-import com.linkedin.datahub.graphql.generated.Embed;
 import com.linkedin.datahub.graphql.types.mappers.ModelMapper;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BrowsePathsV2Mapper implements ModelMapper<BrowsePathsV2, BrowsePathV2> {
 
@@ -21,16 +20,17 @@ public class BrowsePathsV2Mapper implements ModelMapper<BrowsePathsV2, BrowsePat
   @Override
   public BrowsePathV2 apply(@Nonnull final BrowsePathsV2 input) {
     final BrowsePathV2 result = new BrowsePathV2();
-    final List<BrowsePathEntry> path = new ArrayList<>();
-    input.getPath().forEach(pathEntry -> {
-      final BrowsePathEntry entry = new BrowsePathEntry();
-      entry.setName(pathEntry.getId());
-      if (pathEntry.hasUrn() && pathEntry.getUrn() != null) {
-        entry.setEntity(UrnToEntityMapper.map(pathEntry.getUrn()));
-      }
-      path.add(entry);
-    });
+    final List<BrowsePathEntry> path = input.getPath().stream().map(this::mapBrowsePathEntry).collect(Collectors.toList());
     result.setPath(path);
     return result;
+  }
+
+  private BrowsePathEntry mapBrowsePathEntry(com.linkedin.common.BrowsePathEntry pathEntry) {
+    final BrowsePathEntry entry = new BrowsePathEntry();
+    entry.setName(pathEntry.getId());
+    if (pathEntry.hasUrn() && pathEntry.getUrn() != null) {
+      entry.setEntity(UrnToEntityMapper.map(pathEntry.getUrn()));
+    }
+    return entry;
   }
 }
