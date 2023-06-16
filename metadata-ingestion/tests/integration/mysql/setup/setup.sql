106498890d9071c592c32c6e257f68fd486c741e
David Leifker <david.leifker@acryl.io>
2023-06-16 09:09:02 -0500

    chore(deprecate): remove legacy sql table

--- metadata-ingestion/tests/integration/mysql/setup/setup.sql
+++ metadata-ingestion/tests/integration/mysql/setup/setup.sql
@@ -29,24 +29,6 @@ insert into metadata_aspect (urn, aspect, version, metadata, createdon, createdb
   'urn:li:corpuser:__datahub_system'
 );
 
--- create metadata index table
-CREATE TABLE metadata_index (
- `id` BIGINT NOT NULL AUTO_INCREMENT,
- `urn` VARCHAR(200) NOT NULL COMMENT "This is a column comment about URNs",
- `aspect` VARCHAR(150) NOT NULL,
- `path` VARCHAR(150) NOT NULL,
- `longVal` BIGINT,
- `stringVal` VARCHAR(200),
- `doubleVal` DOUBLE,
- CONSTRAINT id_pk PRIMARY KEY (id),
- INDEX longIndex (`urn`,`aspect`,`path`,`longVal`),
- INDEX stringIndex (`urn`,`aspect`,`path`,`stringVal`),
- INDEX doubleIndex (`urn`,`aspect`,`path`,`doubleVal`)
-) COMMENT="This is a table comment";
-
--- create view for testing
-CREATE VIEW metadata_index_view AS SELECT id, urn, path, doubleVal FROM metadata_index;
-
 -- -----------------------------------------------------
 -- Some sample data, from https://github.com/dataCharmer/test_db.
 -- -----------------------------------------------------
