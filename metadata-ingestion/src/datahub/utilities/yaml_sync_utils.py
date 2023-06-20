4d17a18b241e8c960289b5945c99ff8f1f00003d
Harshal Sheth <hsheth2@gmail.com>
2023-06-20 12:36:32 -0700

    feat(ingest): add YamlFileUpdater utility

---
+++ metadata-ingestion/src/datahub/utilities/yaml_sync_utils.py
@@ -0,0 +1,30 @@
+import contextlib
+import pathlib
+from typing import Any, Iterator
+
+import ruamel.yaml.util
+from ruamel.yaml import YAML
+
+
+@contextlib.contextmanager
+def YamlFileUpdater(file: pathlib.Path) -> Iterator[Any]:
+    yaml = YAML()
+    yaml.preserve_quotes = True  # type: ignore[assignment]
+
+    doc = yaml.load(file)
+
+    # All the user to make changes to the doc.
+    # TODO: Enable replacing the doc entirely.
+    yield doc
+
+    # Guess existing indentation in the file so that we can preserve it.
+    _, ind, bsi = ruamel.yaml.util.load_yaml_guess_indent(file.read_text())
+    yaml.width = 2**20  # type: ignore[assignment]
+
+    # TODO: Some folks use a different mapping indent than sequence indent.
+    # We should support that, but for now, we just use the sequence indent.
+    yaml.map_indent = ind
+    yaml.sequence_indent = ind
+    yaml.block_seq_indent = bsi
+
+    yaml.dump(doc, file)
