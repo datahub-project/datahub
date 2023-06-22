54073c362165e047f2c5e92e1964fec7fbdf3b41
ndespouy <ndespouy@gmail.com>
2023-06-22 12:20:25 +0400

    Preparation i18n

--- datahub-web-react/src/setupProxy.js
+++ datahub-web-react/src/setupProxy.js
@@ -1,33 +1,37 @@
 const logInFilter = function (pathname, req) {
-  return pathname.match('^/logIn') && req.method === 'POST';
+    return pathname.match('^/logIn') && req.method === 'POST';
 };
 
 if (process.env.REACT_APP_MOCK === 'true' || process.env.REACT_APP_MOCK === 'cy') {
     // no proxy needed, MirageJS will intercept all http requests
-    module.exports = function () {};
+    module.exports = function () {
+    };
 } else {
     // create a proxy to the graphql server running in docker container
-    const { createProxyMiddleware } = require('http-proxy-middleware');
+    const {createProxyMiddleware} = require('http-proxy-middleware');
+
+    const proxyTarget = 'http://localhost:9002';
+    // const proxyTarget = 'https://demo.datahubproject.io/api/v2/graphql';
 
     module.exports = function (app) {
         app.use(
             '/logIn',
             createProxyMiddleware(logInFilter, {
-                target: 'http://localhost:9002',
+                target: proxyTarget,
                 changeOrigin: true,
             }),
         );
         app.use(
             '/authenticate',
             createProxyMiddleware({
-                target: 'http://localhost:9002',
+                target: proxyTarget,
                 changeOrigin: true,
             }),
         );
         app.use(
             '/api/v2/graphql',
             createProxyMiddleware({
-                target: 'http://localhost:9002',
+                target: proxyTarget,
                 changeOrigin: true,
             }),
         );
