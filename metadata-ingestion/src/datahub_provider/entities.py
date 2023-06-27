6133433a0eeeee0d8e751b8bf2a0c7e567e7505d
Đặng Minh Dũng <dungdm93@live.com>
2023-06-27 17:23:57 +0700

    feat(ingest/airflow): able to set `platform_instance` in `Dataset`
    Signed-off-by: Đặng Minh Dũng <dungdm93@live.com>

--- metadata-ingestion/src/datahub_provider/entities.py
+++ metadata-ingestion/src/datahub_provider/entities.py
@@ -18,10 +18,16 @@ class Dataset(_Entity):
     platform: str
     name: str
     env: str = builder.DEFAULT_ENV
+    platform_instance: str = None
 
     @property
     def urn(self):
-        return builder.make_dataset_urn(self.platform, self.name, self.env)
+        return builder.make_dataset_urn_with_platform_instance(
+            platform=self.platform,
+            name=self.name,
+            platform_instance=self.platform_instance,
+            env=self.env
+        )
 
 
 @attr.s(str=True)
