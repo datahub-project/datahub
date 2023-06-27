55e2cf54b756b6d623e15d05145d9f8a38e84fca
Mayuri Nehate <33225191+mayurinehate@users.noreply.github.com>
2023-06-27 09:29:20 +0530

    Update metadata-ingestion/src/datahub/configuration/validate_field_rename.py
    Co-authored-by: Harshal Sheth <hsheth2@gmail.com>

--- metadata-ingestion/src/datahub/configuration/validate_field_rename.py
+++ metadata-ingestion/src/datahub/configuration/validate_field_rename.py
@@ -40,7 +40,7 @@ def pydantic_renamed_field(
     # Hack: Pydantic maintains unique list of validators by referring its __name__.
     # https://github.com/pydantic/pydantic/blob/v1.10.9/pydantic/main.py#L264
     # This hack ensures that multiple field renames do not overwrite each other.
-    _validate_field_rename.__name__ = f"{_validate_field_rename.__name__}_{new_name}"
+    _validate_field_rename.__name__ = f"{_validate_field_rename.__name__}_{old_name}"
 
     # Why aren't we using pydantic.validator here?
     # The `values` argument that is passed to field validators only contains items
