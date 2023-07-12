2c435ad80cffbbe11548622b50a3a2c0d697f96a
Indy Prentice <indy@Indys-MacBook-Pro.local>
2023-07-12 14:26:22 -0300

    fix checkstyle

--- metadata-service/openapi-servlet/src/main/java/io/datahubproject/openapi/util/ElasticsearchUtils.java
+++ metadata-service/openapi-servlet/src/main/java/io/datahubproject/openapi/util/ElasticsearchUtils.java
@@ -1,6 +1,7 @@
 package io.datahubproject.openapi.util;
 
 public class ElasticsearchUtils {
+  private ElasticsearchUtils() { }
   public static boolean isTaskIdValid(String task) {
     if (task.matches("^[a-zA-Z0-9-_]+:[0-9]+$")) {
       try {
