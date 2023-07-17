31657a97d4b1c28cfd415c4fe437a2eb21acb2f7
Aseem Bansal <asmbansal2@gmail.com>
2023-07-17 14:32:42 +0530

    fix lint

--- metadata-ingestion/src/datahub/ingestion/source/elastic_search.py
+++ metadata-ingestion/src/datahub/ingestion/source/elastic_search.py
@@ -10,10 +10,7 @@ from elasticsearch import Elasticsearch
 from pydantic import validator
 from pydantic.fields import Field
 
-from datahub.configuration.common import (
-    AllowDenyPattern,
-    ConfigModel,
-)
+from datahub.configuration.common import AllowDenyPattern, ConfigModel
 from datahub.configuration.source_common import (
     EnvConfigMixin,
     PlatformInstanceConfigMixin,
