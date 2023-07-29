3872c206bd7d507a5a2d23c764ee766d04d61e57
david-leifker <114954101+david-leifker@users.noreply.github.com>
2023-07-28 21:16:41 -0500

    Update DataHubUpgradeKafkaListener.java

--- metadata-service/factories/src/main/java/com/linkedin/metadata/boot/kafka/DataHubUpgradeKafkaListener.java
+++ metadata-service/factories/src/main/java/com/linkedin/metadata/boot/kafka/DataHubUpgradeKafkaListener.java
@@ -110,7 +110,7 @@ public class DataHubUpgradeKafkaListener implements ConsumerSeekAware, Bootstrap
   public void waitForUpdate() {
     if (!_configurationProvider.getSystemUpdate().isWaitForSystemUpdate()) {
       log.warn("Wait for system update is disabled. Proceeding with startup.");
-      return;
+      IS_UPDATED.getAndSet(true);
     }
     int maxBackOffs = Integer.parseInt(_configurationProvider.getSystemUpdate().getMaxBackOffs());
     long initialBackOffMs = Long.parseLong(_configurationProvider.getSystemUpdate().getInitialBackOffMs());
