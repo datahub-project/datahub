8071cd4e3b4832c4b237ad1856455775ce499ff7
John Joyce <john@acryl.io>
2023-08-02 14:12:15 -0700

    Correctly add to sidebar

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -419,7 +419,7 @@ module.exports = {
         },
         "docs/act-on-metadata/impact-analysis",
         {
-          Observability: ["docs/observe/freshness-assertions"],
+          Observability: ["docs/observe/freshness-assertions.md"],
         },
       ],
     },
