5b4a0b3e1962c3b09706d7196d72db72754568e0
Mayuri N <mayuri.nehate@gslab.com>
2023-08-04 14:14:08 +0530

    docs(ingest): update s3 ang gcs doc with concept mapping

--- metadata-ingestion/src/datahub/ingestion/source/s3/source.py
+++ metadata-ingestion/src/datahub/ingestion/source/s3/source.py
@@ -219,37 +219,6 @@ class TableData:
     supported=True,
 )
 class S3Source(StatefulIngestionSourceBase):
-    """
-    This plugin extracts:
-
-    - Row and column counts for each table
-    - For each column, if profiling is enabled:
-      - null counts and proportions
-      - distinct counts and proportions
-      - minimum, maximum, mean, median, standard deviation, some quantile values
-      - histograms or frequencies of unique values
-
-    This connector supports both local files as well as those stored on AWS S3 (which must be identified using the prefix `s3://`). Supported file types are as follows:
-
-    - CSV
-    - TSV
-    - JSON
-    - Parquet
-    - Apache Avro
-
-    Schemas for Parquet and Avro files are extracted as provided.
-
-    Schemas for schemaless formats (CSV, TSV, JSON) are inferred. For CSV and TSV files, we consider the first 100 rows by default, which can be controlled via the `max_rows` recipe parameter (see [below](#config-details))
-    JSON file schemas are inferred on the basis of the entire file (given the difficulty in extracting only the first few objects of the file), which may impact performance.
-    We are working on using iterator-based JSON parsers to avoid reading in the entire JSON object.
-
-    To ingest datasets from your data lake, you need to provide the dataset path format specifications using `path_specs` configuration in ingestion recipe.
-    Refer section [Path Specs](https://datahubproject.io/docs/generated/ingestion/sources/s3/#path-specs) for examples.
-
-    Note that because the profiling is run with PySpark, we require Spark 3.0.3 with Hadoop 3.2 to be installed (see [compatibility](#compatibility) for more details). If profiling, make sure that permissions for **s3a://** access are set because Spark and Hadoop use the s3a:// protocol to interface with AWS (schema inference outside of profiling requires s3:// access).
-    Enabling profiling will slow down ingestion runs.
-    """
-
     source_config: DataLakeSourceConfig
     report: DataLakeSourceReport
     profiling_times_taken: List[float]
