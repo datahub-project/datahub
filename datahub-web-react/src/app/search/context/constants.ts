cda22a4cee9d204d75f19504d203c8a23b609e1c
Chris Collins <chriscollins3456@gmail.com>
2023-08-08 15:09:11 -0400

    add last modified in source platform as filter

--- datahub-web-react/src/app/search/context/constants.ts
+++ datahub-web-react/src/app/search/context/constants.ts
@@ -2,6 +2,7 @@ import { SortOrder } from '../../../types.generated';
 
 export const RECOMMENDED = 'recommended';
 export const NAME_FIELD = 'name';
+export const LAST_OPERATION_TIME_FIELD = 'lastOperationTime';
 
 export const DEFAULT_SORT_OPTION = RECOMMENDED;
 
@@ -9,4 +10,9 @@ export const SORT_OPTIONS = {
     [RECOMMENDED]: { label: 'Recommended', field: RECOMMENDED, sortOrder: SortOrder.Descending },
     [`${NAME_FIELD}_${SortOrder.Ascending}`]: { label: 'A to Z', field: NAME_FIELD, sortOrder: SortOrder.Ascending },
     [`${NAME_FIELD}_${SortOrder.Descending}`]: { label: 'Z to A', field: NAME_FIELD, sortOrder: SortOrder.Descending },
+    [`${LAST_OPERATION_TIME_FIELD}_${SortOrder.Descending}`]: {
+        label: 'Last Modified in Platform',
+        field: LAST_OPERATION_TIME_FIELD,
+        sortOrder: SortOrder.Descending,
+    },
 };
