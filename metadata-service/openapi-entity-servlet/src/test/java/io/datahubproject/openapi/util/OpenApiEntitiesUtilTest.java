93fa6e44aee75029e21b04cd5d7dc55e91d0b905
David Leifker <david.leifker@acryl.io>
2023-08-08 06:57:58 -0500

    fix test

--- metadata-service/openapi-entity-servlet/src/test/java/io/datahubproject/openapi/util/OpenApiEntitiesUtilTest.java
+++ metadata-service/openapi-entity-servlet/src/test/java/io/datahubproject/openapi/util/OpenApiEntitiesUtilTest.java
@@ -1,16 +1,16 @@
 package io.datahubproject.openapi.util;
 
+import com.linkedin.data.schema.annotation.PathSpecBasedSchemaAnnotationVisitor;
 import com.linkedin.gms.factory.spring.YamlPropertySourceFactory;
 import com.linkedin.metadata.models.registry.EntityRegistry;
 import io.datahubproject.openapi.config.OpenAPIEntityTestConfiguration;
 import io.datahubproject.openapi.dto.UpsertAspectRequest;
-import io.datahubproject.openapi.generated.ContainerEntityRequestV2;
+import io.datahubproject.openapi.generated.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-import org.testng.annotations.Ignore;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -25,6 +25,12 @@ public class OpenApiEntitiesUtilTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private EntityRegistry entityRegistry;
 
+    @BeforeTest
+    public void disableAssert() {
+        PathSpecBasedSchemaAnnotationVisitor.class.getClassLoader()
+                .setClassAssertionStatus(PathSpecBasedSchemaAnnotationVisitor.class.getName(), false);
+    }
+
     @Test
     public void testInitialization() {
         assertNotNull(entityRegistry);
@@ -34,10 +40,12 @@ public class OpenApiEntitiesUtilTest extends AbstractTestNGSpringContextTests {
     public void containerConversionTest() {
         ContainerEntityRequestV2 test = ContainerEntityRequestV2.builder()
                 .urn("urn:li:container:123")
+                .containerKey(ContainerKeyAspectRequestV2.builder().value(ContainerKey.builder().guid("123").build()).build())
                 .build();
         List<UpsertAspectRequest> expected = List.of(UpsertAspectRequest.builder()
                 .entityType("container")
                 .entityUrn("urn:li:container:123")
+                .aspect(ContainerKey.builder().guid("123").build())
                 .build());
 
         assertEquals(expected, OpenApiEntitiesUtil.convertEntityToUpsert(test, ContainerEntityRequestV2.class, entityRegistry));
