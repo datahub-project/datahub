52cf2aa49d3595c96252aecd981ca8040f40399d
wuteng <online_tw5@hotmail.com>
2023-08-09 12:24:04 +0800

    fix(metadata-ingestion) Fix format issue in setup.py for building locally

--- metadata-ingestion/examples/transforms/setup.py
+++ metadata-ingestion/examples/transforms/setup.py
@@ -4,7 +4,6 @@ setup(
     name="custom_transform_example",
     version="1.0",
     packages=find_packages(),
-
     entry_points={
         "datahub.ingestion.transformer.plugins": [
             "custom_transform_example_alias = custom_transform_example:AddCustomOwnership",
