1c09763516e623060f4425e95bab447729f276f6
alplatonov <al.al.platonov@gmail.com>
2023-08-10 22:02:40 +0400

    fix(ingest/ldap)_added unittest for depricated attributes

--- metadata-ingestion/tests/unit/test_clickhouse_source.py
+++ metadata-ingestion/tests/unit/test_clickhouse_source.py
@@ -58,3 +58,36 @@ def test_clickhouse_uri_default_password():
         }
     )
     assert config.get_sql_alchemy_url() == "clickhouse+native://user@host:1111/db"
+
+
+def test_clickhouse_uri_native_secure_deprication():
+    config = ClickHouseConfig.parse_obj(
+        {
+            "username": "user",
+            "password": "password",
+            "host_port": "host:1111",
+            "database": "db",
+            "scheme": "clickhouse+native",
+            "secure": True,
+        }
+    )
+    assert (
+        config.get_sql_alchemy_url()
+        == "clickhouse+native://user:password@host:1111/db?secure=True"
+    )
+
+
+def test_clickhouse_uri_https_deprication():
+    config = ClickHouseConfig.parse_obj(
+        {
+            "username": "user",
+            "password": "password",
+            "host_port": "host:1111",
+            "database": "db",
+            "protocol": "https",
+        }
+    )
+    assert (
+        config.get_sql_alchemy_url()
+        == "clickhouse://user:password@host:1111/db?protocol=https"
+    )
