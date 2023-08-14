datahub-project/datahub

commit 1a47a51f1b32b769336207563610535afccd8d36

fix(ingest/build): Fix sagemaker mypy and flake8 issues (#8530)

metadata-ingestion/src/datahub_provider/_plugin.py
@@ -107,7 +107,7 @@ def get_inlets_from_task(task: BaseOperator, context: Any) -> Iterable[Any]:
         ]
 
         for inlet in task_inlets:
-            if type(inlet) != str:
+            if isinstance(inlet, str):
                 inlets.append(inlet)
 
     return inlets

metadata-ingestion/src/datahub/ingestion/source/aws/sagemaker_processors/feature_groups.py
@@ -24,7 +24,7 @@ if TYPE_CHECKING:
     from mypy_boto3_sagemaker import SageMakerClient
     from mypy_boto3_sagemaker.type_defs import (
         DescribeFeatureGroupResponseTypeDef,
-        FeatureDefinitionOutputTypeDef,
+        FeatureDefinitionTypeDef,
         FeatureGroupSummaryTypeDef,
     )
 
@@ -147,7 +147,7 @@ class FeatureGroupProcessor:
     def get_feature_wu(
         self,
         feature_group_details: "DescribeFeatureGroupResponseTypeDef",
-        feature: "FeatureDefinitionOutputTypeDef",
+        feature: "FeatureDefinitionTypeDef",
     ) -> MetadataWorkUnit:
         """
         Generate an MLFeature workunit for a SageMaker feature.

metadata-ingestion/src/datahub/utilities/mapping.py
@@ -252,11 +252,11 @@ class OperationProcessor:
 
     def get_match(self, match_clause: Any, raw_props_value: Any) -> Optional[Match]:
         # function to check if a match clause is satisfied to a value.
-        if type(raw_props_value) not in Constants.OPERAND_DATATYPE_SUPPORTED or type(
-            raw_props_value
-        ) != type(match_clause):
+        if not any(
+            isinstance(raw_props_value, t) for t in Constants.OPERAND_DATATYPE_SUPPORTED
+        ) or not isinstance(raw_props_value, type(match_clause)):
             return None
-        elif type(raw_props_value) == str:
+        elif isinstance(raw_props_value, str):
             return re.match(match_clause, raw_props_value)
         else:
             return re.match(str(match_clause), str(raw_props_value))

metadata-ingestion/src/datahub_provider/operators/datahub_assertion_operator.py
@@ -62,9 +62,9 @@ class DataHubAssertionOperator(BaseOperator):
             return True
 
         self.log.info(f"Checking if dataset {self.urn} is ready to be consumed")
-        if type(self.urn) == str:
+        if isinstance(self.urn, str):
             urns = [self.urn]
-        elif type(self.urn) == list:
+        elif isinstance(self.urn, list):
             urns = self.urn
         else:
             raise Exception(f"urn parameter has invalid type {type(self.urn)}")

metadata-ingestion/src/datahub_provider/operators/datahub_assertion_sensor.py
@@ -61,9 +61,9 @@ class DataHubAssertionSensor(BaseSensorOperator):
             return True
 
         self.log.info(f"Checking if dataset {self.urn} is ready to be consumed")
-        if type(self.urn) == str:
+        if isinstance(self.urn, str):
             urns = [self.urn]
-        elif type(self.urn) == list:
+        elif isinstance(self.urn, list):
             urns = self.urn
         else:
             raise Exception(f"urn parameter has invalid type {type(self.urn)}")

metadata-ingestion/src/datahub_provider/operators/datahub_operation_operator.py
@@ -76,9 +76,9 @@ class DataHubOperationCircuitBreakerOperator(BaseSensorOperator):
             return True
 
         self.log.info(f"Checking if dataset {self.urn} is ready to be consumed")
-        if type(self.urn) == str:
+        if isinstance(self.urn, str):
             urns = [self.urn]
-        elif type(self.urn) == list:
+        elif isinstance(self.urn, list):
             urns = self.urn
         else:
             raise Exception(f"urn parameter has invalid type {type(self.urn)}")

metadata-ingestion/src/datahub_provider/operators/datahub_operation_sensor.py
@@ -78,9 +78,9 @@ class DataHubOperationCircuitBreakerSensor(BaseSensorOperator):
             return True
 
         self.log.info(f"Checking if dataset {self.urn} is ready to be consumed")
-        if type(self.urn) == str:
+        if isinstance(self.urn, str):
             urns = [self.urn]
-        elif type(self.urn) == list:
+        elif isinstance(self.urn, list):
             urns = self.urn
         else:
             raise Exception(f"urn parameter has invalid type {type(self.urn)}")

metadata-ingestion/tests/unit/test_glue_source.py
@@ -89,7 +89,7 @@ def test_column_type(hive_column_type: str, expected_type: Type) -> None:
     )
     schema_fields = avro_schema_to_mce_fields(json.dumps(avro_schema))
     actual_schema_field_type = schema_fields[0].type
-    assert type(actual_schema_field_type.type) == expected_type
+    assert isinstance(actual_schema_field_type.type, expected_type)
 
 
 @pytest.mark.parametrize(

commit 7799bdd3c08f023132027a860186293a1f679471

fix(ingest/airflow): make inlets work again
Fixes a regression from 1a47a51f1b32b769336207563610535afccd8d36.

metadata-ingestion/src/datahub_provider/_plugin.py
@@ -107,7 +107,7 @@ def get_inlets_from_task(task: BaseOperator, context: Any) -> Iterable[Any]:
         ]
 
         for inlet in task_inlets:
-            if isinstance(inlet, str):
+            if not isinstance(inlet, str):
                 inlets.append(inlet)
 
     return inlets