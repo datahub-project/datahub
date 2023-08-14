3bfaf55c80b6134b32912a9a92894e696593ac48
Mayuri Nehate <33225191+mayurinehate@users.noreply.github.com>
2023-08-14 14:51:39 +0530

    Update metadata-ingestion/src/datahub/ingestion/source/usage/usage_common.py
    Co-authored-by: Harshal Sheth <hsheth2@gmail.com>

--- metadata-ingestion/src/datahub/ingestion/source/usage/usage_common.py
+++ metadata-ingestion/src/datahub/ingestion/source/usage/usage_common.py
@@ -220,7 +220,7 @@ class BaseUsageConfig(BaseTimeWindowConfig):
         if get_time_bucket(v, values["bucket_duration"]) != v:
             new_start_time = get_time_bucket(v, values["bucket_duration"])
             logger.warning(
-                f"`start_time` will be considered as {new_start_time}, although input `start_time` is {v}."
+                f"`start_time` will be changed to {new_start_time}, although the input `start_time` is {v}."
                 "This is necessary to record correct usage for the configured bucket duration."
             )
             return new_start_time
