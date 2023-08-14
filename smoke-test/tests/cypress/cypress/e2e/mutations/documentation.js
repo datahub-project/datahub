9f81b06bcad6168a1a50f6bcc92907fd61e798dc
Kos Korchak <kostia.korchak@squadformers.com>
2023-08-14 15:02:58 -0400

    Update for documentation.js

--- smoke-test/tests/cypress/cypress/e2e/mutations/documentation.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/documentation.js
@@ -1,71 +1,69 @@
 const test_id = Math.floor(Math.random() * 100000);
-const documentation = `This is test${test_id} documentation`;
 const documentation_edited = `This is test${test_id} documentation EDITED`;
-const wrong_url = "https://datahubio";
-const correct_url = "https://datahub.io";
+const wrong_url = "https://www.linkedincom";
+const correct_url = "https://www.linkedin.com";
 
 describe("add, remove documentation and link to dataset", () => {
 
-    it("open test dataset page, add and remove dataset documentation", () => {
-        //add documentation, and propose, decline proposal
+    it("open test dataset page, edit and remove dataset documentation", () => {
+        //edit documentation, and propose, decline proposal
         cy.loginWithCredentials();
-        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressDocumentationDataset,PROD)/Schema");
-        cy.waitTextVisible("No documentation yet. Share your knowledge by adding documentation and links to helpful resources.");
-        cy.waitTextVisible("Add Documentation");
+        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hive,SampleCypressHiveDataset,PROD)/Schema");
         cy.get("[role='tab']").contains("Documentation").click();
-        cy.waitTextVisible("No documentation yet");
-        cy.waitTextVisible("Share your knowledge by adding documentation and links to helpful resources.");
-        cy.clickOptionWithText("Add Documentation");
-        cy.focused().type(documentation);
+        cy.waitTextVisible("my hive dataset");
+        cy.waitTextVisible("Sample doc");
+        cy.clickOptionWithText("Edit");
+        cy.focused().clear();
+        cy.focused().type(documentation_edited);
         cy.get("button").contains("Propose").click();
         cy.waitTextVisible("Proposed description update!").wait(3000);
-        cy.waitTextVisible("No documentation yet");
+        //cy.waitTextVisible("No documentation yet");
         cy.clickOptionWithText("Inbox");
         cy.waitTextVisible("Update Description Proposal");
         cy.contains("View difference").first().click();
-        cy.waitTextVisible(documentation);
+        cy.waitTextVisible(documentation_edited);
         cy.get("[data-icon='close'").click();
         cy.get("button").contains("Decline").click();
         cy.waitTextVisible("Are you sure you want to reject this proposal?");
         cy.get("button").contains("Yes").click();
         cy.waitTextVisible("Proposal declined.");
-        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressDocumentationDataset,PROD)/Schema");
+        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hive,SampleCypressHiveDataset,PROD)/Schema");
         cy.get("[role='tab']").contains("Documentation").click();
-        cy.waitTextVisible("No documentation yet");
-        //add documentation, and propose, approve proposal
-        cy.clickOptionWithText("Add Documentation");
-        cy.focused().type(documentation);
+        cy.waitTextVisible("my hive dataset");
+        //edit documentation, and propose, approve proposal
+        cy.clickOptionWithText("Edit");
+        cy.focused().clear().wait(1000);
+        cy.focused().type(documentation_edited);
         cy.get("button").contains("Propose").click();
         cy.waitTextVisible("Proposed description update!").wait(3000);
-        cy.waitTextVisible("No documentation yet");
+        cy.waitTextVisible("my hive dataset");
         cy.clickOptionWithText("Inbox");
         cy.waitTextVisible("Update Description Proposal");
         cy.get("button").contains("Approve").click();
         cy.waitTextVisible("Are you sure you want to accept this proposal?");
         cy.get("button").contains("Yes").click();
         cy.waitTextVisible("Successfully accepted the proposal!");
-        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressDocumentationDataset,PROD)/Schema");
+        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hive,SampleCypressHiveDataset,PROD)/Schema");
         cy.get("[role='tab']").contains("Documentation").click();
-        cy.ensureTextNotPresent("No documentation yet");
-        cy.waitTextVisible(documentation);
-        cy.ensureTextNotPresent("Add Documentation");
-        //edit and save documentation
-        cy.clickOptionWithTestId("edit-description");
-        cy.focused().clear().type(documentation_edited);
-        cy.clickOptionWithTestId("save-description");
-        cy.waitTextVisible("Description Updated");
+        cy.ensureTextNotPresent("my hive dataset");
         cy.waitTextVisible(documentation_edited);
-        //remove documentation
-        cy.clickOptionWithTestId("edit-description");
-        cy.focused().clear();
-        cy.clickOptionWithTestId("save-description");
+        cy.ensureTextNotPresent("Add Documentation");
+        //return documentation to original state
+        cy.clickOptionWithText("Edit");
+        cy.focused().clear().wait(1000);
+        cy.focused().type("my hive dataset");
+        cy.get("button").contains("Save").click();
         cy.waitTextVisible("Description Updated");
-        cy.ensureTextNotPresent(documentation_edited);
+        cy.waitTextVisible("my hive dataset");
     });
 
-    it("open test dataset page, add and remove dataset link", () => {
+    it("open test dataset page, remove and add dataset link", () => {
         cy.loginWithCredentials();
-        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressDocumentationDataset,PROD)/Schema");
+        cy.visit("/dataset/urn:li:dataset:(urn:li:dataPlatform:hive,SampleCypressHiveDataset,PROD)/Schema");
+        cy.get("[role='tab']").contains("Documentation").click();
+        cy.contains("Sample doc").trigger("mouseover", { force: true });
+        cy.get('[data-icon="delete"]').click();
+        cy.waitTextVisible("Link Removed");
         cy.get("button").contains("Add Link").click();
         cy.get("#addLinkForm_url").type(wrong_url);
         cy.waitTextVisible("This field must be a valid url.");
@@ -73,16 +71,10 @@ describe("add, remove documentation and link to dataset", () => {
         cy.waitTextVisible("A URL is required.");
         cy.focused().type(correct_url);
         cy.ensureTextNotPresent("This field must be a valid url.");
-        cy.get("#addLinkForm_label").type(test_id);
+        cy.get("#addLinkForm_label").type("Sample doc");
         cy.get('[role="dialog"] button').contains("Add").click();
         cy.waitTextVisible("Link Added");
         cy.get("[role='tab']").contains("Documentation").click();
         cy.get(`[href='${correct_url}']`).should("be.visible");
-        cy.get(`[href='${correct_url}']`).eq(1).trigger("mouseover", { force: true });
-        cy.get('[data-icon="delete"]').click();
-        cy.waitTextVisible("Link Removed");
-        cy.get(`[href='${correct_url}']`).should("not.exist");
-        cy.waitTextVisible("Add Documentation");
-        cy.waitTextVisible("No documentation yet");
     });
 });
