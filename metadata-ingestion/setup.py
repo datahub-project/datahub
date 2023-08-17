e1445331c03c30ed337503f9040f80427f0ff6c0
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-08-17 11:47:27 -0400

    build(ingest): Bump pydantic pin

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -454,7 +454,7 @@ base_dev_requirements = {
     "mypy==1.0.0",
     # pydantic 1.8.2 is incompatible with mypy 0.910.
     # See https://github.com/samuelcolvin/pydantic/pull/3175#issuecomment-995382910.
-    "pydantic>=1.9.0",
+    "pydantic>=1.10.0",
     *test_api_requirements,
     pytest_dep,
     "pytest-asyncio>=0.16.0",
