026cb1d4dfb7764a70fb020f8cc5d9140b1eb256
Maiara Reinaldo <maiara.reinaldo@fundingcircle.com>
2023-08-22 10:01:38 +0100

    Revert "feat: add dbt-athena adapter support for column types mapping (#8116)"
    This reverts commit 6adb496581cce5472a0f096cba63cc35155682b5.

--- metadata-ingestion/src/datahub/ingestion/source/sql/sql_types.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/sql_types.py
@@ -233,23 +233,8 @@ def resolve_trino_modified_type(type_string: str) -> Any:
     if match:
         modified_type_base: str = match.group(1)
         return TRINO_SQL_TYPES_MAP[modified_type_base]
-    return TRINO_SQL_TYPES_MAP[type_string]
-
-
-def resolve_athena_modified_type(type_string: str) -> Any:
-    # for cases like struct<...>, array<...>, map<...>
-    match_complex = re.match(r"([a-zA-Z]+)<.+>", type_string)
-    # for cases like timestamp(3), decimal(10,0)
-    match_simple = re.match(r"([a-zA-Z]+)\(.+\)", type_string)
-
-    modified_type_base = ""
-    if match_complex:
-        modified_type_base = match_complex.group(1)
-    elif match_simple:
-        modified_type_base = match_simple.group(1)
-    if modified_type_base:
-        return ATHENA_SQL_TYPES_MAP[modified_type_base]
-    return ATHENA_SQL_TYPES_MAP[type_string]
+    else:
+        return TRINO_SQL_TYPES_MAP[type_string]
 
 
 def resolve_vertica_modified_type(type_string: str) -> Any:
@@ -258,7 +243,8 @@ def resolve_vertica_modified_type(type_string: str) -> Any:
     if match:
         modified_type_base: str = match.group(1)
         return VERTICA_SQL_TYPES_MAP[modified_type_base]
-    return VERTICA_SQL_TYPES_MAP[type_string]
+    else:
+        return VERTICA_SQL_TYPES_MAP[type_string]
 
 
 # see https://docs.snowflake.com/en/sql-reference/intro-summary-data-types.html
@@ -363,28 +349,6 @@ TRINO_SQL_TYPES_MAP: Dict[str, Any] = {
     "time": TimeType,
     "timestamp": TimeType,
     "row": RecordType,
-    "map": MapType,
-    "array": ArrayType,
-}
-
-# https://docs.aws.amazon.com/athena/latest/ug/data-types.html
-# https://github.com/dbt-athena/dbt-athena/tree/main
-ATHENA_SQL_TYPES_MAP: Dict[str, Any] = {
-    "boolean": BooleanType,
-    "tinyint": NumberType,
-    "smallint": NumberType,
-    "int": NumberType,
-    "integer": NumberType,
-    "bigint": NumberType,
-    "float": NumberType,
-    "double": NumberType,
-    "decimal": NumberType,
-    "varchar": StringType,
-    "char": StringType,
-    "binary": BytesType,
-    "date": DateType,
-    "timestamp": TimeType,
-    "struct": RecordType,
     "map": MapType,
     "array": ArrayType,
 }
