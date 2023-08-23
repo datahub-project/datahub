a7a2c1c53e5fbe3428f2ac2b7eadaea222679107
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-08-23 11:57:08 -0400

    Update metadata-ingestion/src/datahub/utilities/time.py

--- metadata-ingestion/src/datahub/utilities/time.py
+++ metadata-ingestion/src/datahub/utilities/time.py
@@ -36,7 +36,7 @@ class TimeWindow:
 
     def starts_after(self, other: "TimeWindow") -> bool:
         """Whether current window starts after other window ends"""
-        return other.start_time < other.end_time < self.start_time
+        return other.start_time <= other.end_time < self.start_time
 
     def ends_after(self, other: "TimeWindow") -> bool:
         """Whether current window ends after other window ends."""
