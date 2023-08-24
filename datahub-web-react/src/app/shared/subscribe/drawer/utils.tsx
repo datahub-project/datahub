86593094f20a87570cf78718d42b22f1bfc07903
John Joyce <john@acryl.io>
2023-08-24 08:34:06 -0700

    refactor(subscriptions): Tiny confirmation copy update (#1669)

--- datahub-web-react/src/app/shared/subscribe/drawer/utils.tsx
+++ datahub-web-react/src/app/shared/subscribe/drawer/utils.tsx
@@ -339,8 +339,8 @@ export const createSubscriptionFunction = ({
                 actorType: isPersonal ? ActorTypes.PERSONAL : ActorTypes.GROUP,
             });
             const description = isPersonal
-                ? 'You are now following changes on this entity.'
-                : 'Your group is now following changes on this entity.';
+                ? 'You are now subscribed to this entity.'
+                : 'Your group is now subcribed to this entity.';
             notification.success({
                 message: 'Success',
                 description,
