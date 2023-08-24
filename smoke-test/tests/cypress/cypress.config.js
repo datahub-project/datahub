203de65ed13f37f46a631141374d1ffccad4ba4f
Josh Eilers <josh.eilers@acryl.io>
2023-08-24 11:14:48 -0700

    fix(documentation): non siblings should use primary entity urn

--- smoke-test/tests/cypress/cypress.config.js
+++ smoke-test/tests/cypress/cypress.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
   viewportHeight: 960,
   viewportWidth: 1536,
-  projectId: 'hkrxk5',
+  projectId: "hkrxk5",
   defaultCommandTimeout: 10000,
   retries: {
     runMode: 2,
@@ -15,10 +15,10 @@ module.exports = defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'http://localhost:9002/',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    baseUrl: "http://localhost:3000/",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     experimentalStudio: true,
   },
-})
+});
