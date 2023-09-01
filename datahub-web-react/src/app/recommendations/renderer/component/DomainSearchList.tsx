3d64d335605d3b86a8ec2027eb007cbc0cead7e3
Chris Collins <chriscollins3456@gmail.com>
2023-09-01 10:41:45 -0400

    fix weird filter logic

--- datahub-web-react/src/app/recommendations/renderer/component/DomainSearchList.tsx
+++ datahub-web-react/src/app/recommendations/renderer/component/DomainSearchList.tsx
@@ -38,7 +38,7 @@ export const DomainSearchList = ({ content, onClick }: Props) => {
 
     const domainsWithCounts: Array<{ domain: Domain; count?: number }> = content
         .map((cnt) => ({ domain: cnt.entity, count: cnt.params?.contentParams?.count }))
-        .filter((domainWithCount) => domainWithCount.domain !== null && domainWithCount !== undefined)
+        .filter((domainWithCount) => domainWithCount?.domain !== null)
         .slice(0, NUM_DOMAIN_CARDS) as Array<{
         domain: Domain;
         count?: number;
