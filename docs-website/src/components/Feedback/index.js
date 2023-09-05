99113f093eb62f325f5383498f7a6c46c94f718a
Jeff Merrick <jeff@wireform.io>
2023-09-05 14:31:43 -0500

    docs: style docs feedback widget

--- docs-website/src/components/Feedback/index.js
+++ docs-website/src/components/Feedback/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { supabase } from "./supabase";
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
+import { LikeOutlined, DislikeOutlined, CheckCircleOutlined } from "@ant-design/icons";
 
 const Feedback = ({ page }) => {
   const [reaction, setReaction] = useState(null);
@@ -27,18 +28,27 @@ const Feedback = ({ page }) => {
   };
 
   return (
-    <div className={styles.feedbackWidget}>
-      {reaction === null ? (
-        <div className={styles.reactionButtons}>
-          <h3>Is this page helpful?</h3>
-          <button className={styles.reaction} onClick={() => handleReaction("thumbs_up")}>👍</button>
-          <button className={styles.reaction} onClick={() => handleReaction("thumbs_down")}>👎</button>
-        </div>
-      ) : (
-        <div className={styles.feedbackMessage}>
-          Thanks for your feedback!
-        </div>
-      )}
+    <div className={styles.feedbackWrapper}>
+      <div className={styles.feedbackWidget}>
+        {reaction === null ? (
+          <div className={styles.feedbackButtons}>
+            <strong>Is this page helpful?</strong>
+            <div>
+              <button onClick={() => handleReaction("thumbs_up")}>
+                <LikeOutlined />
+              </button>
+              <button onClick={() => handleReaction("thumbs_down")}>
+                <DislikeOutlined />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div className={styles.feedbackMessage}>
+            <CheckCircleOutlined />
+            <strong>Thanks for your feedback!</strong>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
