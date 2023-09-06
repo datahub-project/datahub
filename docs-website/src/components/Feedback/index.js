ac87ce86def5dadd843e73d57899f24b51e84fd8
Jeff Merrick <jeff@wireform.io>
2023-09-06 13:50:00 -0500

    docs: collect text feedback from users

--- docs-website/src/components/Feedback/index.js
+++ docs-website/src/components/Feedback/index.js
@@ -1,27 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
+import clsx from "clsx";
 import { supabase } from "./supabase";
 import styles from "./styles.module.scss";
 import { LikeOutlined, DislikeOutlined, CheckCircleOutlined } from "@ant-design/icons";
+import { v4 as uuidv4 } from "uuid";
 
-const Feedback = ({ page }) => {
+const Feedback = () => {
   const [reaction, setReaction] = useState(null);
+  const [feedback, setFeedback] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  const [reactionId, setReactionId] = useState(null);
 
   const handleReaction = async (selectedReaction) => {
-    console.log("Button clicked:", selectedReaction);
-    try {
-      const { data, error } = await supabase.from("feedback").insert([
-        {
-          page: window.location.href,
-          reaction: selectedReaction,
-        },
-      ]);
+    if (reaction !== selectedReaction) {
+      const uuid = uuidv4();
+      try {
+        const { error } = await supabase.from("reaction_feedback").insert([
+          {
+            id: uuid,
+            page: window.location.href,
+            reaction: selectedReaction,
+          },
+        ]);
 
-      if (error) {
+        if (error) {
+          console.error("Error submitting feedback:", error);
+          return;
+        }
+        setReactionId(uuid);
+        setReaction(selectedReaction);
+      } catch (error) {
         console.error("Error submitting feedback:", error);
-        return;
       }
+    } else {
+      setReaction(null);
+    }
+  };
 
-      setReaction(selectedReaction);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (feedback !== "" && reactionId !== null) {
+        const { error } = await supabase.from("written_feedback").insert([
+          {
+            feedback: feedback,
+            reaction_feedback_id: reactionId,
+          },
+        ]);
+
+        if (error) {
+          console.error("Error submitting feedback:", error);
+          return;
+        }
+        setSubmitted(true);
+      }
     } catch (error) {
       console.error("Error submitting feedback:", error);
     }
@@ -30,18 +62,32 @@ const Feedback = ({ page }) => {
   return (
     <div className={styles.feedbackWrapper}>
       <div className={styles.feedbackWidget}>
-        {reaction === null ? (
-          <div className={styles.feedbackButtons}>
-            <strong>Is this page helpful?</strong>
-            <div>
-              <button onClick={() => handleReaction("thumbs_up")}>
-                <LikeOutlined />
-              </button>
-              <button onClick={() => handleReaction("thumbs_down")}>
-                <DislikeOutlined />
-              </button>
+        {!submitted ? (
+          <>
+            <div className={styles.feedbackButtons}>
+              <strong>Is this page helpful?</strong>
+              <div>
+                <button className={clsx(styles.feedbackButton, reaction === "positive" && styles.active)} onClick={() => handleReaction("positive")}>
+                  <LikeOutlined />
+                </button>
+                <button className={clsx(styles.feedbackButton, reaction === "negative" && styles.active)} onClick={() => handleReaction("negative")}>
+                  <DislikeOutlined />
+                </button>
+              </div>
             </div>
-          </div>
+            {reaction !== null && (
+              <form className={styles.feedbackForm} onSubmit={handleSubmit}>
+                <textarea
+                  className={styles.feedbackText}
+                  value={feedback}
+                  rows="4"
+                  onChange={(e) => setFeedback(e.target.value)}
+                  placeholder="Your feedback (optional)"
+                ></textarea>
+                <button className="button button--secondary">Send</button>
+              </form>
+            )}
+          </>
         ) : (
           <div className={styles.feedbackMessage}>
             <CheckCircleOutlined />
