a17cb7b63df3ea7c75f55d197053378a0b83b252
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-09-18 11:06:33 -0700

    build(ingest/databricks): Relax databricks-sdk pin

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -259,7 +259,7 @@ usage_common = {
 
 databricks = {
     # 0.1.11 appears to have authentication issues with azure databricks
-    "databricks-sdk>=0.1.1, <0.1.11",
+    "databricks-sdk>=0.1.1, != 0.1.11",
     "pyspark",
     "requests",
 }
