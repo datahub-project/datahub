85fa5a1c4fdf2b4c4439558fa3a4cbbfd3491fbf
Chris Collins <chriscollins3456@gmail.com>
2023-09-18 16:14:33 -0400

    feat(ui/java) Update domains to be nested (#8841)
    Allow the ability to now nest domains underneath other domains. This should work much like the business glossary where you can add domains underneath other domains, move domains underneath other domains or at the root, and navigate domains using a nice new navigator.

--- metadata-auth/auth-api/src/main/java/com/datahub/authorization/ResolvedResourceSpec.java
+++ metadata-auth/auth-api/src/main/java/com/datahub/authorization/ResolvedResourceSpec.java
@@ -3,7 +3,6 @@ package com.datahub.authorization;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
-import javax.annotation.Nullable;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -27,21 +26,6 @@ public class ResolvedResourceSpec {
   }
 
   /**
-   * Fetch the entity-registry type for a resource. ('dataset', 'dashboard', 'chart').
-   * @return the entity type.
-   */
-  public String getType() {
-    if (!fieldResolvers.containsKey(ResourceFieldType.RESOURCE_TYPE)) {
-      throw new UnsupportedOperationException(
-          "Failed to resolve resource type! No field resolver for RESOURCE_TYPE provided.");
-    }
-    Set<String> resourceTypes =
-        fieldResolvers.get(ResourceFieldType.RESOURCE_TYPE).getFieldValuesFuture().join().getValues();
-    assert resourceTypes.size() == 1; // There should always be a single resource type.
-    return resourceTypes.stream().findFirst().get();
-  }
-
-  /**
    * Fetch the owners for a resource.
    * @return a set of owner urns, or empty set if none exist.
    */
@@ -50,21 +34,5 @@ public class ResolvedResourceSpec {
       return Collections.emptySet();
     }
     return fieldResolvers.get(ResourceFieldType.OWNER).getFieldValuesFuture().join().getValues();
-  }
-
-  /**
-   * Fetch the domain for a Resolved Resource Spec
-   * @return a Domain or null if one does not exist.
-   */
-  @Nullable
-  public String getDomain() {
-    if (!fieldResolvers.containsKey(ResourceFieldType.DOMAIN)) {
-      return null;
-    }
-    Set<String> domains = fieldResolvers.get(ResourceFieldType.DOMAIN).getFieldValuesFuture().join().getValues();
-    if (domains.size() > 0) {
-      return domains.stream().findFirst().get();
-    }
-    return null;
   }
 }
