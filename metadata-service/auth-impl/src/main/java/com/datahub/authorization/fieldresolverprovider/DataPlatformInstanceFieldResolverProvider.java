32b3d91b403517c04f3aaa1d59bf33cd00a42cff
amanda-her <912amandahernando@gmail.com>
2023-09-22 14:30:17 +0200

    feat(auth): add DataPlatformInstanceFieldResolverProvider tests

--- metadata-service/auth-impl/src/main/java/com/datahub/authorization/fieldresolverprovider/DataPlatformInstanceFieldResolverProvider.java
+++ metadata-service/auth-impl/src/main/java/com/datahub/authorization/fieldresolverprovider/DataPlatformInstanceFieldResolverProvider.java
@@ -25,46 +25,46 @@ import static com.linkedin.metadata.Constants.*;
 @RequiredArgsConstructor
 public class DataPlatformInstanceFieldResolverProvider implements ResourceFieldResolverProvider {
 
-    private final EntityClient _entityClient;
-    private final Authentication _systemAuthentication;
+  private final EntityClient _entityClient;
+  private final Authentication _systemAuthentication;
 
-    @Override
-    public ResourceFieldType getFieldType() {
-        return ResourceFieldType.DATA_PLATFORM_INSTANCE;
+  @Override
+  public ResourceFieldType getFieldType() {
+    return ResourceFieldType.DATA_PLATFORM_INSTANCE;
+  }
+
+  @Override
+  public FieldResolver getFieldResolver(ResourceSpec resourceSpec) {
+    return FieldResolver.getResolverFromFunction(resourceSpec, this::getDataPlatformInstance);
+  }
+
+  private FieldResolver.FieldValue getDataPlatformInstance(ResourceSpec resourceSpec) {
+    Urn entityUrn = UrnUtils.getUrn(resourceSpec.getResource());
+    // In the case that the entity is a platform instance, the associated platform instance entity is the instance itself
+    if (entityUrn.getEntityType().equals(DATA_PLATFORM_INSTANCE_ENTITY_NAME)) {
+      return FieldResolver.FieldValue.builder()
+          .values(Collections.singleton(entityUrn.toString()))
+          .build();
     }
 
-    @Override
-    public FieldResolver getFieldResolver(ResourceSpec resourceSpec) {
-        return FieldResolver.getResolverFromFunction(resourceSpec, this::getDataPlatformInstance);
+    EnvelopedAspect dataPlatformInstanceAspect;
+    try {
+      EntityResponse response = _entityClient.getV2(entityUrn.getEntityType(), entityUrn,
+          Collections.singleton(DATA_PLATFORM_INSTANCE_ASPECT_NAME), _systemAuthentication);
+      if (response == null || !response.getAspects().containsKey(DATA_PLATFORM_INSTANCE_ASPECT_NAME)) {
+        return FieldResolver.emptyFieldValue();
+      }
+      dataPlatformInstanceAspect = response.getAspects().get(DATA_PLATFORM_INSTANCE_ASPECT_NAME);
+    } catch (Exception e) {
+      log.error("Error while retrieving platform instance aspect for urn {}", entityUrn, e);
+      return FieldResolver.emptyFieldValue();
     }
-
-    private FieldResolver.FieldValue getDataPlatformInstance(ResourceSpec resourceSpec) {
-        Urn entityUrn = UrnUtils.getUrn(resourceSpec.getResource());
-        // In the case that the entity is a platform instance, the associated platform instance entity is the instance itself
-        if (entityUrn.getEntityType().equals(DATA_PLATFORM_INSTANCE_ENTITY_NAME)) {
-            return FieldResolver.FieldValue.builder()
-                .values(Collections.singleton(entityUrn.toString()))
-                .build();
-        }
-
-        EnvelopedAspect dataPlatformInstanceAspect;
-        try {
-            EntityResponse response = _entityClient.getV2(entityUrn.getEntityType(), entityUrn,
-                    Collections.singleton(DATA_PLATFORM_INSTANCE_ASPECT_NAME), _systemAuthentication);
-            if (response == null || !response.getAspects().containsKey(DATA_PLATFORM_INSTANCE_ASPECT_NAME)) {
-                return FieldResolver.emptyFieldValue();
-            }
-            dataPlatformInstanceAspect = response.getAspects().get(DATA_PLATFORM_INSTANCE_ASPECT_NAME);
-        } catch (Exception e) {
-            log.error("Error while retrieving platform instance aspect for urn {}", entityUrn, e);
-            return FieldResolver.emptyFieldValue();
-        }
-        DataPlatformInstance dataPlatformInstance = new DataPlatformInstance(dataPlatformInstanceAspect.getValue().data());
-        if (dataPlatformInstance.getInstance() == null) {
-            return FieldResolver.emptyFieldValue();
-        }
-        return FieldResolver.FieldValue.builder()
-             .values(Collections.singleton(Objects.requireNonNull(dataPlatformInstance.getInstance()).toString()))
-            .build();
+    DataPlatformInstance dataPlatformInstance = new DataPlatformInstance(dataPlatformInstanceAspect.getValue().data());
+    if (dataPlatformInstance.getInstance() == null) {
+      return FieldResolver.emptyFieldValue();
     }
+    return FieldResolver.FieldValue.builder()
+        .values(Collections.singleton(Objects.requireNonNull(dataPlatformInstance.getInstance()).toString()))
+        .build();
+  }
 }
