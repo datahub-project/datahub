4fd20110d8231894ea0c0a5c169545133c05ff85
treff7es <treff7es@gmail.com>
2023-10-03 12:01:38 +0200

    Adding some comments

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_audit.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_audit.py
@@ -20,6 +20,10 @@ BigQueryAuditMetadata = Any
 
 logger: logging.Logger = logging.getLogger(__name__)
 
+# Regexp for sharded tables.
+# A sharded table is a table that has a suffix of the form _yyyymmdd or yyyymmdd, where yyyymmdd is a date.
+# The regexp checks for valid dates in the suffix (e.g. 20200101, 20200229, 20201231) and if the date is not valid
+# then it is not a sharded table.
 _BIGQUERY_DEFAULT_SHARDED_TABLE_REGEX = (
     "(.+?)_?(\\d\\d\\d\\d(?:0[1-9]|1[0-2])(?:0[1-9]|[12][0-9]|3[01]))$"
 )
