c4248d117dcfbd41e49995d62edb190c212c7a78
Kos Korchak <kostia.korchak@squadformers.com>
2023-10-04 18:21:28 -0400

    Added more usage of data-testids and other small improvements

--- smoke-test/tests/cypress/cypress/e2e/domains/nested_domains.js
+++ smoke-test/tests/cypress/cypress/e2e/domains/nested_domains.js
@@ -4,44 +4,48 @@ const domainDescription = "CypressNestedDomainDescription";
 describe("nested domains test", () => {
 
     it("create a domain, move under parent, remove domain", () => {
-        //create a new domain without a parent
+        // Create a new domain without a parent
         cy.loginWithCredentials();
         cy.goToDomainList();
-        cy.clickOptionWithText("New Domain");
-        cy.waitTextVisible("Create New Domain");
+        cy.clickOptionWithTestId("domains-new-domain-button");
         cy.get('[data-testid="create-domain-name"]').click().type(domainName);
-        cy.get('#description').click().type(domainDescription);
-        cy.get('[data-testid="create-domain-button"]').click()
+        cy.get('[data-testid="create-domain-description"]').click().type(domainDescription);
+        cy.clickOptionWithTestId("create-domain-button");
         cy.waitTextVisible(domainName);
-        //ensure the new domain has no parent in the navigation sidebar
+
+        // Ensure the new domain has no parent in the navigation sidebar
         cy.waitTextVisible(domainDescription);
-        //move a domain from the root level to be under a parent domain
+
+        // Move a domain from the root level to be under a parent domain
         cy.clickOptionWithText(domainName);
         cy.openThreeDotDropdown();
-        cy.clickOptionWithText("Move");
-        cy.get('[role="dialog"]').contains("Marketing").click({force: true});
-        cy.get('[role="dialog"]').contains("Marketing").should("be.visible");
-        cy.get("button").contains("Move").click().wait(5000);
-        //ensure domain is no longer on the sidebar navigator at the top level but shows up under the parent
+        cy.clickOptionWithTestId("entity-menu-move-button");
+        cy.get('[data-testid="move-domain-modal"]').contains("Marketing").click({force: true});
+        cy.get('[data-testid="move-domain-modal"]').contains("Marketing").should("be.visible");
+        cy.clickOptionWithTestId("move-domain-modal-move-button").wait(5000);
+
+        // Wnsure domain is no longer on the sidebar navigator at the top level but shows up under the parent
         cy.goToDomainList();
         cy.ensureTextNotPresent(domainName);
         cy.ensureTextNotPresent(domainDescription);
         cy.waitTextVisible("1 sub-domain");
-        //move a domain from under a parent domain to the root level
-        cy.contains("Marketing").prev().click();
+
+        // Move a domain from under a parent domain to the root level
+        cy.get('[data-testid="domain-list-item"]').contains("Marketing").prev().click();
         cy.clickOptionWithText(domainName);
         cy.openThreeDotDropdown();
-        cy.clickOptionWithText("Move");
-        cy.get("button").contains("Move").click().wait(5000);
+        cy.clickOptionWithTestId("entity-menu-move-button");
+        cy.clickOptionWithTestId("move-domain-modal-move-button").wait(5000);
         cy.goToDomainList();
         cy.waitTextVisible(domainName);
         cy.waitTextVisible(domainDescription);
-        //delete a domain
+
+        // Delete a domain
         cy.clickOptionWithText(domainName).wait(3000);
         cy.openThreeDotDropdown();
-        cy.clickOptionWithText("Delete");
+        cy.clickOptionWithTestId("entity-menu-delete-button");
         cy.waitTextVisible("Are you sure you want to remove this Domain?");
-        cy.get("button").contains("Yes").click();
+        cy.clickOptionWithText("Yes");
         cy.waitTextVisible("Deleted Domain!");
         cy.ensureTextNotPresent(domainName);
         cy.ensureTextNotPresent(domainDescription);
