7640da066cf188b7b82c7f579f7a81019fd84107
treff7es <treff7es@gmail.com>
2023-10-19 11:28:19 +0200

    Updating Snowflake cloud region map

--- metadata-ingestion/src/datahub/ingestion/source/snowflake/constants.py
+++ metadata-ingestion/src/datahub/ingestion/source/snowflake/constants.py
@@ -21,6 +21,25 @@ class SnowflakeEdition(str, Enum):
 # Includes only exceptions to format <provider>_<cloud region with hyphen replaced by _>
 SNOWFLAKE_REGION_CLOUD_REGION_MAPPING = {
     "aws_us_east_1_gov": (SnowflakeCloudProvider.AWS, "us-east-1"),
+    "aws_us_gov_west_1": (SnowflakeCloudProvider.AWS, "us-west-1"),
+    "aws_us_gov_west_1_fhplus": (SnowflakeCloudProvider.AWS, "us-west-1"),
+    "aws_us_east_2": (SnowflakeCloudProvider.AWS, "us-east-2"),
+    "aws_us_east_1": (SnowflakeCloudProvider.AWS, "us-east-1"),
+    "aws_us_gov_east_1": (SnowflakeCloudProvider.AWS, "us-east-1"),
+    "aws_ca_central_1": (SnowflakeCloudProvider.AWS, "ca-central-1"),
+    "aws_sa_east_1": (SnowflakeCloudProvider.AWS, "sa-east-1"),
+    "aws_eu_west_1": (SnowflakeCloudProvider.AWS, "eu-west-1"),
+    "aws_eu_west_2": (SnowflakeCloudProvider.AWS, "eu-west-2"),
+    "aws_eu_west_3": (SnowflakeCloudProvider.AWS, "eu-west-3"),
+    "aws_eu_central_1": (SnowflakeCloudProvider.AWS, "eu-central-1"),
+    "aws_eu_north_1": (SnowflakeCloudProvider.AWS, "eu-north-1"),
+    "aws_ap_northeast_1": (SnowflakeCloudProvider.AWS, "ap-northeast-1"),
+    "aws_ap_northeast_2": (SnowflakeCloudProvider.AWS, "ap-northeast-2"),
+    "aws_ap_northeast_3": (SnowflakeCloudProvider.AWS, "ap-northeast-3"),
+    "aws_ap_south_1": (SnowflakeCloudProvider.AWS, "ap-south-1"),
+    "aws_ap_southeast_1": (SnowflakeCloudProvider.AWS, "ap-southeast-1"),
+    "aws_ap_southeast_2": (SnowflakeCloudProvider.AWS, "ap-southeast-2"),
+    "aws_ap_southeast_3": (SnowflakeCloudProvider.AWS, "ap-southeast-3"),
     "azure_westus2": (SnowflakeCloudProvider.AZURE, "west-us-2"),
     "azure_centralus": (SnowflakeCloudProvider.AZURE, "central-us"),
     "azure_southcentralus": (SnowflakeCloudProvider.AZURE, "south-central-us"),
@@ -36,6 +55,10 @@ SNOWFLAKE_REGION_CLOUD_REGION_MAPPING = {
     "azure_japaneast": (SnowflakeCloudProvider.AZURE, "japan-east"),
     "azure_southeastasia": (SnowflakeCloudProvider.AZURE, "southeast-asia"),
     "azure_australiaeast": (SnowflakeCloudProvider.AZURE, "australia-east"),
+    "gcp_us_central1": (SnowflakeCloudProvider.GCP, "us-central1"),
+    "gcp_us_east4": (SnowflakeCloudProvider.GCP, "us-east4"),
+    "gcp_europe_west2": (SnowflakeCloudProvider.GCP, "europe-west2"),
+    "gcp_europe_west4": (SnowflakeCloudProvider.GCP, "europe-west4"),
 }
 
 # https://docs.snowflake.com/en/sql-reference/snowflake-db.html
