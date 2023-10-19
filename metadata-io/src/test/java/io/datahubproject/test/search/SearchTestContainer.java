c368bba54cfb4107f450e01e2a1667b8daf852bc
Pedro Silva <pedro@acryl.io>
2023-10-19 19:22:01 +0100

    Bump memory usage for test containers

--- metadata-io/src/test/java/io/datahubproject/test/search/SearchTestContainer.java
+++ metadata-io/src/test/java/io/datahubproject/test/search/SearchTestContainer.java
@@ -5,7 +5,7 @@ import org.testcontainers.containers.GenericContainer;
 import java.time.Duration;
 
 public interface SearchTestContainer {
-    String SEARCH_JAVA_OPTS = "-Xms64m -Xmx384m -XX:MaxDirectMemorySize=368435456";
+    String SEARCH_JAVA_OPTS = "-Xms64m -Xmx400m -XX:MaxDirectMemorySize=368435456";
     Duration STARTUP_TIMEOUT = Duration.ofMinutes(5); // usually < 1min
 
     GenericContainer<?> startContainer();
