e64bfd858854d7f8534de1875bd57c203908023b
treff7es <treff7es@gmail.com>
2023-10-20 14:22:40 +0200

    Initial tearadata speed up hacks

--- metadata-ingestion/src/datahub/ingestion/source/sql/teradata.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/teradata.py
@@ -1,14 +1,26 @@
 import logging
+from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Iterable, Optional, Union
+from typing import (
+    Dict,
+    Iterable,
+    List,
+    MutableMapping,
+    Optional,
+    Tuple,
+    Union,
+)
 
 # This import verifies that the dependencies are available.
 import teradatasqlalchemy  # noqa: F401
 import teradatasqlalchemy.types as custom_types
 from pydantic.fields import Field
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 from sqlalchemy.engine import Engine
+from sqlalchemy.engine.reflection import Inspector
+
+# from teradatasqlalchemy.dialect import TeradataDialect
 
 from datahub.configuration.common import AllowDenyPattern
 from datahub.configuration.time_window_config import BaseTimeWindowConfig
@@ -25,6 +37,7 @@ from datahub.ingestion.api.decorators import (
 from datahub.ingestion.api.workunit import MetadataWorkUnit
 from datahub.ingestion.graph.client import DataHubGraph
 from datahub.ingestion.source.sql.sql_common import SqlWorkUnit, register_custom_type
+from datahub.ingestion.source.sql.sql_config import SQLCommonConfig
 from datahub.ingestion.source.sql.sql_generic_profiler import ProfilingSqlReport
 from datahub.ingestion.source.sql.two_tier_sql_source import (
     TwoTierSQLAlchemyConfig,
@@ -33,11 +46,14 @@ from datahub.ingestion.source.sql.two_tier_sql_source import (
 from datahub.ingestion.source.usage.usage_common import BaseUsageConfig
 from datahub.ingestion.source_report.ingestion_stage import IngestionStageReport
 from datahub.ingestion.source_report.time_window import BaseTimeWindowReport
+from datahub.metadata._schema_classes import SchemaMetadataClass, ViewPropertiesClass
 from datahub.metadata.com.linkedin.pegasus2avro.schema import (
     BytesTypeClass,
     TimeTypeClass,
 )
+from datahub.utilities.file_backed_collections import FileBackedDict
 from datahub.utilities.sqlglot_lineage import SchemaResolver, sqlglot_lineage
+from datahub.utilities.urns.dataset_urn import DatasetUrn
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -75,7 +91,39 @@ class BaseTeradataConfig(TwoTierSQLAlchemyConfig):
 
 class TeradataConfig(BaseTeradataConfig, BaseTimeWindowConfig):
     database_pattern = Field(
-        default=AllowDenyPattern(deny=["dbc"]),
+        default=AllowDenyPattern(
+            deny=[
+                "dbc",
+                "All",
+                "Crashdumps",
+                "DBC",
+                "dbcmngr",
+                "Default",
+                "External_AP",
+                "EXTUSER",
+                "LockLogShredder",
+                "PUBLIC",
+                "Sys_Calendar",
+                "SysAdmin",
+                "SYSBAR",
+                "SYSJDBC",
+                "SYSLIB",
+                "SystemFe",
+                "SYSUDTLIB",
+                "SYSUIF",
+                "TD_SERVER_DB",
+                "TDStats",
+                "TD_SYSGPL",
+                "TD_SYSXML",
+                "TDMaps",
+                "TDPUSER",
+                "TDQCD",
+                "tdwm",
+                "SQLJ",
+                "TD_SYSFNLIB",
+                "SYSSPATIAL",
+            ]
+        ),
         description="Regex patterns for databases to filter in ingestion.",
     )
     include_table_lineage = Field(
@@ -84,6 +132,11 @@ class TeradataConfig(BaseTeradataConfig, BaseTimeWindowConfig):
         "This requires to have the table lineage feature enabled.",
     )
 
+    include_view_lineage = Field(
+        default=True,
+        description="Whether to include view lineage in the ingestion. "
+        "This requires to have the view lineage feature enabled.",
+    )
     usage: BaseUsageConfig = Field(
         description="The usage config to use when generating usage statistics",
         default=BaseUsageConfig(),
@@ -98,6 +151,33 @@ class TeradataConfig(BaseTeradataConfig, BaseTimeWindowConfig):
         default=False,
         description="Generate usage statistic.",
     )
+
+    use_file_backed_cache: bool = Field(
+        default=True,
+        description="Whether to use a file backed cache for the view definitions.",
+    )
+
+    disable_schema_metadata: bool = Field(
+        default=False,
+        description="Whether to disable schema metadata ingestion. Only table names and database names will be ingested.",
+    )
+
+    use_cached_metadata: bool = Field(
+        default=True,
+        description="Whether to use cached metadata. This reduce the number of queries to the database but requires to have cached metadata.",
+    )
+
+
+@dataclass
+class TeradataTable:
+    database: str
+    name: str
+    description: Optional[str]
+    object_type: str
+    create_timestamp: datetime
+    last_alter_name: Optional[str]
+    last_alter_timestamp: Optional[datetime]
+    request_text: Optional[str]
 
 
 @platform_name("Teradata")
@@ -125,10 +205,41 @@ class TeradataSource(TwoTierSQLAlchemySource):
     LINEAGE_QUERY: str = """SELECT ProcID, UserName as "user", StartTime AT TIME ZONE 'GMT' as "timestamp", DefaultDatabase as default_database, QueryText as query
      FROM "DBC".DBQLogTbl
      where ErrorCode = 0
-     and QueryText like 'create table demo_user.test_lineage%'
      and "timestamp" >= TIMESTAMP '{start_time}'
      and "timestamp" < TIMESTAMP '{end_time}'
      """
+
+    TABLES_AND_VIEWS_QIERY: str = """
+SELECT
+    t.DatabaseName,
+    t.TableName as name,
+    t.CommentString as description,
+    CASE t.TableKind
+         WHEN 'I' THEN 'Join index'
+         WHEN 'N' THEN 'Hash index'
+         WHEN 'T' THEN 'Table'
+         WHEN 'V' THEN 'View'
+         WHEN 'O' THEN 'NoPI Table'
+         WHEN 'Q' THEN 'Queue table'
+    END AS object_type,
+    t.CreateTimeStamp,
+    t.LastAlterName,
+    t.LastAlterTimeStamp,
+    t.RequestText
+FROM dbc.Tables t
+WHERE DatabaseName NOT IN ('All', 'Crashdumps', 'DBC', 'dbcmngr',
+        'Default', 'External_AP', 'EXTUSER', 'LockLogShredder', 'PUBLIC',
+        'Sys_Calendar', 'SysAdmin', 'SYSBAR', 'SYSJDBC', 'SYSLIB',
+        'SystemFe', 'SYSUDTLIB', 'SYSUIF', 'TD_SERVER_DB', 'TDStats',
+        'TD_SYSGPL', 'TD_SYSXML', 'TDMaps', 'TDPUSER', 'TDQCD',
+        'tdwm', 'SQLJ', 'TD_SYSFNLIB', 'SYSSPATIAL')
+and t.TableKind in ('T', 'V', 'Q', 'O')
+ORDER by DatabaseName, TableName;
+     """
+
+    _tables_cache: MutableMapping[str, List[TeradataTable]] = defaultdict(list)
+
+    _view_definition_cache: MutableMapping[str, str]
 
     def __init__(self, config: TeradataConfig, ctx: PipelineContext):
         super().__init__(config, ctx, "teradata")
@@ -152,10 +263,159 @@ class TeradataSource(TwoTierSQLAlchemySource):
             env=self.config.env,
         )
 
+        if self.config.use_file_backed_cache:
+            self._view_definition_cache = FileBackedDict[str]()
+        else:
+            self._view_definition_cache = {}
+
+        if self.config.use_cached_metadata:
+            self.cache_tables_and_views()
+            logger.info(f"Found {len(self._tables_cache)} tables and views")
+            setattr(self, "loop_tables", self.cached_loop_tables)
+            setattr(self, "loop_views", self.cached_loop_views)
+            setattr(self, "get_table_properties", self.cached_get_table_properties)
+            # self.loop_tables = self.cached_loop_tables
+            # self.loop_views = self.cached_loop_views
+            # self.get_table_properties = self.cached_get_table_properties
+        if self.config.disable_schema_metadata:
+            # self._get_columns = lambda dataset_name, inspector, schema, table: []
+            # self._get_foreign_keys = lambda dataset_name, inspector, schema, table: []
+            setattr(
+                self, "_get_columns", lambda dataset_name, inspector, schema, table: []
+            )
+            setattr(
+                self,
+                "_get_foreign_keys",
+                lambda dataset_name, inspector, schema, table: [],
+            )
+
     @classmethod
     def create(cls, config_dict, ctx):
         config = TeradataConfig.parse_obj(config_dict)
         return cls(config, ctx)
+
+    def get_inspectors(self):
+        # This method can be overridden in the case that you want to dynamically
+        # run on multiple databases.
+        url = self.config.get_sql_alchemy_url()
+        logger.debug(f"sql_alchemy_url={url}")
+        engine = create_engine(url, **self.config.options)
+        with engine.connect() as conn:
+            inspector = inspect(conn)
+            if self.config.database and self.config.database != "":
+                databases = [self.config.database]
+            else:
+                databases = inspector.get_schema_names()
+            for db in databases:
+                if self.config.database_pattern.allowed(db):
+                    # url = self.config.get_sql_alchemy_url(current_db=db)
+                    # with create_engine(url, **self.config.options).connect() as conn:
+                    #    inspector = inspect(conn)
+                    inspector._datahub_database = db
+                    yield inspector
+
+    def get_db_name(self, inspector: Inspector) -> str:
+        if hasattr(inspector, "_datahub_database"):
+            return inspector._datahub_database
+
+        engine = inspector.engine
+
+        if engine and hasattr(engine, "url") and hasattr(engine.url, "database"):
+            return str(engine.url.database).strip('"')
+        else:
+            raise Exception("Unable to get database name from Sqlalchemy inspector")
+
+    def cached_loop_tables(  # noqa: C901
+        self,
+        inspector: Inspector,
+        schema: str,
+        sql_config: SQLCommonConfig,
+    ) -> Iterable[Union[SqlWorkUnit, MetadataWorkUnit]]:
+        setattr(
+            inspector,
+            "get_table_names",
+            lambda schema: [
+                i.name
+                for i in filter(
+                    lambda t: t.object_type != "View", self._tables_cache[schema]
+                )
+            ],
+        )
+        yield from super().loop_tables(inspector, schema, sql_config)
+
+    def cached_get_table_properties(
+        self, inspector: Inspector, schema: str, table: str
+    ) -> Tuple[Optional[str], Dict[str, str], Optional[str]]:
+        description: Optional[str] = None
+        properties: Dict[str, str] = {}
+
+        # The location cannot be fetched generically, but subclasses may override
+        # this method and provide a location.
+        location: Optional[str] = None
+
+        for entry in self._tables_cache[schema]:
+            if entry.name == table:
+                description = entry.description
+                if entry.object_type == "View" and entry.request_text:
+                    properties["view_definition"] = entry.request_text
+                break
+        return description, properties, location
+
+    def cached_loop_views(  # noqa: C901
+        self,
+        inspector: Inspector,
+        schema: str,
+        sql_config: SQLCommonConfig,
+    ) -> Iterable[Union[SqlWorkUnit, MetadataWorkUnit]]:
+        setattr(
+            inspector,
+            "get_view_names",
+            lambda schema: [
+                i.name
+                for i in filter(
+                    lambda t: t.object_type == "View", self._tables_cache[schema]
+                )
+            ],
+        )
+        yield from super().loop_views(inspector, schema, sql_config)
+
+    def get_view_lineage(self) -> Iterable[MetadataWorkUnit]:
+        for key in self._view_definition_cache.keys():
+            view_definition = self._view_definition_cache[key]
+            dataset_urn = DatasetUrn.create_from_string(key)
+
+            db_name: Optional[str] = None
+            # We need to get the default db from the dataset urn otherwise the builder generates the wrong urns
+            if "." in dataset_urn.get_dataset_name():
+                db_name = dataset_urn.get_dataset_name().split(".", 1)[0]
+
+            self.report.num_view_ddl_parsed += 1
+            if self.report.num_view_ddl_parsed % 1000 == 0:
+                logger.info(f"Parsed {self.report.num_queries_parsed} view ddl")
+
+            yield from self.gen_lineage_from_query(
+                query=view_definition, default_database=db_name, is_view_ddl=True
+            )
+
+    def cache_tables_and_views(self) -> None:
+        engine = self.get_metadata_engine()
+        for entry in engine.execute(self.TABLES_AND_VIEWS_QIERY):
+            table = TeradataTable(
+                database=entry.DatabaseName.strip(),
+                name=entry.name.strip(),
+                description=entry.description.strip() if entry.description else None,
+                object_type=entry.object_type,
+                create_timestamp=entry.CreateTimeStamp,
+                last_alter_name=entry.LastAlterName,
+                last_alter_timestamp=entry.LastAlterTimeStamp,
+                request_text=entry.RequestText.strip()
+                if entry.object_type == "View" and entry.RequestText
+                else None,
+            )
+            if table.database not in self._tables_cache:
+                self._tables_cache[table.database] = []
+
+            self._tables_cache[table.database].append(table)
 
     def get_audit_log_mcps(self) -> Iterable[MetadataWorkUnit]:
         engine = self.get_metadata_engine()
@@ -214,7 +474,19 @@ class TeradataSource(TwoTierSQLAlchemySource):
 
     def get_workunits_internal(self) -> Iterable[Union[MetadataWorkUnit, SqlWorkUnit]]:
         # Add all schemas to the schema resolver
-        yield from super().get_workunits_internal()
+        for wu in super().get_workunits_internal():
+            urn = wu.get_urn()
+            schema_metadata = wu.get_aspect_of_type(SchemaMetadataClass)
+            if schema_metadata:
+                self.schema_resolver.add_schema_metadata(urn, schema_metadata)
+            view_properties = wu.get_aspect_of_type(ViewPropertiesClass)
+            if view_properties and self.config.include_view_lineage:
+                self._view_definition_cache[urn] = view_properties.viewLogic
+            yield wu
+
+        if self.config.include_view_lineage:
+            self.report.report_ingestion_stage_start("view lineage extraction")
+            yield from self.get_view_lineage()
 
         if self.config.include_table_lineage or self.config.include_usage_statistics:
             self.report.report_ingestion_stage_start("audit log extraction")
