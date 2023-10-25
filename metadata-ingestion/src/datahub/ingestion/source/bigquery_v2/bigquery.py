28b29f7ca8a1635f4d88f2959089d0984120cab0
David Sanchez <david.sanchez@cabify.com>
2023-10-25 15:09:55 +0200

    Update metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery.py
    add suggestion
    
    Co-authored-by: Mayuri Nehate <33225191+mayurinehate@users.noreply.github.com>

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery.py
@@ -1042,7 +1042,7 @@ class BigqueryV2Source(StatefulIngestionSourceBase, TestableSource):
                     for idx, field in enumerate(schema_fields):
                         # Remove all the [version=2.0].[type=struct]. tags to get the field path
                         if (
-                            re.sub(r"\[.*?\]\.", "", field.fieldPath, 0, re.MULTILINE)
+                            re.sub(r"\[.*?\]\.", "", field.fieldPath.lower(), 0, re.MULTILINE)
                             == col.field_path.lower()
                         ):
                             field.description = col.comment
