69231d2130c15055bc45301a252bd44fe9f1e603
Harshal Sheth <hsheth2@gmail.com>
2023-10-26 16:43:47 -0700

    feat(sqlparser): performance improvements
    In particular, this will improve performance for queries with a large
    number of output columns. Simpler queries will also see modest speedups.
    
    In my tests, a simple `SELECT` with ~6000 columns saw a ~15-20x speedup.
    
    Should have no behavioral impact.

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -108,7 +108,7 @@ usage_common = {
 sqlglot_lib = {
     # Using an Acryl fork of sqlglot.
     # https://github.com/tobymao/sqlglot/compare/main...hsheth2:sqlglot:hsheth?expand=1
-    "acryl-sqlglot==18.5.2.dev45",
+    "acryl-sqlglot==18.17.1.dev12",
 }
 
 sql_common = (
