a8ad8191c4024e1e4c06647a8ed9a1d0c7fd14b8
gaurav2733 <gaurav.wagh@apptware.com>
2023-11-10 12:13:52 +0530

    feat: add empty state to govern domains page

---
+++ datahub-web-react/src/app/domain/EmptyDomainsSection.tsx
@@ -0,0 +1,89 @@
+import { PlusOutlined, ReadOutlined } from '@ant-design/icons';
+import { Button, Empty, Typography } from 'antd';
+import React from 'react';
+import styled from 'styled-components/macro';
+import { ANTD_GRAY } from '../entity/shared/constants';
+
+const EmptyDomainContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const StyledEmpty = styled(Empty)`
+    width: 35vw;
+    @media screen and (max-width: 1300px) {
+        width: 50vw;
+    }
+    @media screen and (max-width: 896px) {
+        overflow-y: auto;
+        max-height: 75vh;
+        &::-webkit-scrollbar {
+            width: 5px;
+            background: #d6d6d6;
+        }
+    }
+    padding: 60px 40px;
+    .ant-empty-image {
+        display: none;
+    }
+`;
+
+const StyledButton = styled(Button)`
+    margin: 18px 8px 0 0;
+`;
+
+const StyledParagraph = styled(Typography.Paragraph)<{ alignLeft?: string }>`
+    text-align: justify;
+    text-justify: inter-word;
+    margin: 40px 0;
+    font-size: 15px;
+`;
+
+interface Props {
+    title?: string;
+    setIsCreatingDomain: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+function EmptyDomainsSection(props: Props) {
+    const { title, setIsCreatingDomain } = props;
+
+    return (
+        <EmptyDomainContainer>
+            <StyledEmpty
+                description={
+                    <>
+                        <ReadOutlined style={{ fontSize: 40, marginBottom: 10, color: ANTD_GRAY[7] }} />
+                        <Typography.Title level={4}>{title}</Typography.Title>
+                        <StyledParagraph type="secondary">
+                            <strong style={{ color: ANTD_GRAY[8] }}>Welcome to your Data Domains!</strong> It looks like
+                            this space is ready to be transformed into a well-organized data universe. Start by creating
+                            your first domain - a high-level category for your data assets.
+                        </StyledParagraph>
+                        <StyledParagraph type="secondary">
+                            <strong style={{ color: ANTD_GRAY[8] }}> Create Nested Domains:</strong> Want to dive
+                            deeper? You can also create nested domains to add granularity and structure. Just like
+                            nesting Russian dolls, its all about refining your organization.
+                        </StyledParagraph>
+                        <StyledParagraph type="secondary">
+                            <strong style={{ color: ANTD_GRAY[8] }}>Build Data Products</strong>: Once your domains are
+                            set, go a step further! Organize your data assets into data products to realize a data mesh
+                            architecture. Data products empower you to treat data as a product, making it more
+                            accessible and manageable.
+                        </StyledParagraph>
+                        <StyledParagraph type="secondary">
+                            Ready to embark on this data adventure? Click the Create Domain button to begin shaping your
+                            data landscape!
+                        </StyledParagraph>
+                    </>
+                }
+            >
+                <StyledButton onClick={() => setIsCreatingDomain(true)}>
+                    <PlusOutlined /> Create Domain
+                </StyledButton>
+            </StyledEmpty>
+        </EmptyDomainContainer>
+    );
+}
+
+export default EmptyDomainsSection;
--- datahub-web-react/src/app/domain/nestedDomains/RootDomains.tsx
+++ datahub-web-react/src/app/domain/nestedDomains/RootDomains.tsx
@@ -5,13 +5,17 @@ import { ResultWrapper } from '../../search/SearchResultList';
 import { useEntityRegistry } from '../../useEntityRegistry';
 import { EntityType } from '../../../types.generated';
 import useListDomains from '../useListDomains';
+import EmptyDomainsSection from '../EmptyDomainsSection';
 
 const DomainsWrapper = styled.div`
     overflow: auto;
     padding: 0 28px 16px 28px;
 `;
 
-export default function RootDomains() {
+interface Props {
+    setIsCreatingDomain: React.Dispatch<React.SetStateAction<boolean>>;
+}
+export default function RootDomains({ setIsCreatingDomain }: Props) {
     const entityRegistry = useEntityRegistry();
     const { loading, error, data, sortedDomains } = useListDomains({});
 
@@ -19,6 +23,9 @@ export default function RootDomains() {
         <>
             {!data && loading && <Message type="loading" content="Loading domains..." />}
             {error && <Message type="error" content="Failed to load domains. An unexpected error occurred." />}
+            {!loading && (!data || !data?.listDomains?.domains?.length) && (
+                <EmptyDomainsSection title="Organize your data" setIsCreatingDomain={setIsCreatingDomain} />
+            )}
             <DomainsWrapper>
                 {sortedDomains?.map((domain) => (
                     <ResultWrapper showUpdatedStyles>
