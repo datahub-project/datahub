d735228c29cfe5804dfc1064a7a0dea13d0e86ff
John Joyce <john@acryl.io>
2023-11-13 16:38:15 -0800

    refactor(ui): Nitpick: Remove primary color for sort selector + add tooltips (#2048)

--- datahub-web-react/src/app/search/context/constants.ts
+++ datahub-web-react/src/app/search/context/constants.ts
@@ -9,17 +9,17 @@ export const DEFAULT_SORT_OPTION = RELEVANCE;
 export const SORT_OPTIONS = {
     [RELEVANCE]: { label: 'Relevance', field: RELEVANCE, sortOrder: SortOrder.Descending },
     [`${ENTITY_NAME_FIELD}_${SortOrder.Ascending}`]: {
-        label: 'A to Z',
+        label: 'Name A to Z',
         field: ENTITY_NAME_FIELD,
         sortOrder: SortOrder.Ascending,
     },
     [`${ENTITY_NAME_FIELD}_${SortOrder.Descending}`]: {
-        label: 'Z to A',
+        label: 'Name Z to A',
         field: ENTITY_NAME_FIELD,
         sortOrder: SortOrder.Descending,
     },
     [`${LAST_OPERATION_TIME_FIELD}_${SortOrder.Descending}`]: {
-        label: 'Last Modified in Platform',
+        label: 'Last Modified (In Source)',
         field: LAST_OPERATION_TIME_FIELD,
         sortOrder: SortOrder.Descending,
     },
