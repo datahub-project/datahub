0b68115499c89b545c20a96149b3bf832bed15fe
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2023-11-20 05:13:02 -0500

    pr feedback

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/dataset/mappers/DatasetMapper.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/dataset/mappers/DatasetMapper.java
@@ -160,6 +160,8 @@ public class DatasetMapper implements ModelMapper<EntityResponse, Dataset> {
         if (lastModified != null) {
             Urn actor = lastModified.getActor();
             properties.setLastModified(new AuditStamp(lastModified.getTime(), actor == null ? null : actor.toString()));
+        } else {
+            properties.setLastModified(new AuditStamp(0L, null));
         }
     }
 
