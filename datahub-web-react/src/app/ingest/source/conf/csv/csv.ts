ed937a14bd8878660f8bf4bb32b8cbf8da3f032d
purnimagarg1 <purnima.garg@apptware.com>
2023-11-21 19:03:40 +0530

    Update csv config file

--- datahub-web-react/src/app/ingest/source/conf/csv/csv.ts
+++ datahub-web-react/src/app/ingest/source/conf/csv/csv.ts
@@ -5,14 +5,14 @@ const placeholderRecipe = `\
 source:
     type: csv-enricher
     config:
-    filename: # relative path to your csv file to ingest, e.g. ./path/to/your/file.csv
-    array_delimiter: |
-    delimiter: ,
-    write_semantics: PATCH
+        filename: # relative path to your csv file to ingest, e.g. ./path/to/your/file.csv
+        array_delimiter: |
+        delimiter: ,
+        write_semantics: PATCH
 `;
 
 const csvConfig: SourceConfig = {
-    type: 'csv',
+    type: 'csv-enricher',
     placeholderRecipe,
     displayName: 'CSV',
     docsUrl: 'https://datahubproject.io/docs/generated/ingestion/sources/csv',
