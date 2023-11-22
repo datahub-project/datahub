4e19c6b3cfeea33a1c995f149b4358104e8e69eb
Ethan Cartwright <ethan.cartwright@acryl.io>
2023-11-22 17:38:16 -0500

    add unit test

---
+++ metadata-ingestion/tests/unit/test_redshift_source.py
@@ -0,0 +1,35 @@
+from datahub.ingestion.source.redshift.config import RedshiftConfig
+from datahub.ingestion.source.redshift.redshift import RedshiftSource
+from datahub.ingestion.api.common import PipelineContext
+from datahub.ingestion.source.redshift.redshift_schema import (
+    RedshiftTable,
+    RedshiftColumn,
+)
+from datahub.metadata.schema_classes import MetadataChangeProposalClass
+from typing import cast
+
+
+def test_gen_dataset_workunits_patch_custom_properties():
+    config = RedshiftConfig(host_port="localhost:5439", database="test")
+    source: RedshiftSource = RedshiftSource(config, ctx=PipelineContext(run_id="test"))
+    gen = source.gen_dataset_workunits(
+        table=RedshiftTable(
+            name="category",
+            columns=[],
+            created=None,
+            comment="",
+        ),
+        database="dev",
+        schema="public",
+        sub_type="test_sub_type",
+        custom_properties={"my_key": "my_value"},
+    )
+
+    custom_props_exist = False
+    for item in gen:
+        mcp = cast(MetadataChangeProposalClass, item.metadata)
+        if mcp.aspectName == "datasetProperties":
+            assert mcp.changeType == "PATCH"
+            custom_props_exist = True
+
+    assert custom_props_exist
