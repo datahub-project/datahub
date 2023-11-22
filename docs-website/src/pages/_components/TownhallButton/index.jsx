059b2a7f846f59d8b57988d12bd2c9014999e469
socar-dini <0327jane@gmail.com>
2023-11-22 18:57:13 +0900

    fix: remove useEffect & useState

--- docs-website/src/pages/_components/TownhallButton/index.jsx
+++ docs-website/src/pages/_components/TownhallButton/index.jsx
@@ -1,39 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styles from "./townhallbutton.module.scss";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 
 const TownhallButton = () => {
-  const [showButton, setShowButton] = useState(false);
-  const [currentMonth, setCurrentMonth] = useState('');
+  const today = new Date();
+  const currentDay = today.getDate();
+  const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+  const lastThursday = lastDayOfMonth.getDate() - ((lastDayOfMonth.getDay() + 7 - 4) % 7);
 
-  useEffect(() => {
-    const today = new Date();
-    const currentDay = today.getDate();
-    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-    const lastThursday = lastDayOfMonth.getDate() - ((lastDayOfMonth.getDay() + 7 - 4) % 7);
+  const daysUntilLastThursday = lastThursday - currentDay;
 
-    const daysUntilLastThursday = lastThursday - currentDay;
+  let showButton = false;
+  let currentMonth = '';
 
-    let shouldShowButton = false;
-    let monthText = '';
-
-    if (daysUntilLastThursday > 0 && daysUntilLastThursday <= 14) {
-      shouldShowButton = true;
-      monthText = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(today);
-    }
-
-    setShowButton(shouldShowButton);
-    setCurrentMonth(monthText);
-  }); 
+  if (daysUntilLastThursday > 0 && daysUntilLastThursday <= 14) {
+    showButton = true;
+    currentMonth = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(today);
+  }
 
   return (
-      showButton && (
-        <Link to="http://rsvp.datahubproject.io" className={clsx("button button--primary button--md", styles.feature)} >
-            Join {currentMonth} Townhall!&nbsp;✨
-        </Link>
-      )
-  ); 
+    showButton && (
+      <Link to="http://rsvp.datahubproject.io" className={clsx('button button--primary button--md', styles.feature)}>
+        Join {currentMonth} Townhall!&nbsp;✨
+      </Link>
+    )
+  );
 };
 
 export default TownhallButton;
