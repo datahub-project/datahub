7d3d4c7870f419b7741d9c075ea0b0c15a1fe0d7
Aseem Bansal <asmbansal2@gmail.com>
2023-11-28 11:36:03 +0530

    fix(migrate): connect with token without dry-run

--- metadata-ingestion/src/datahub/cli/migrate.py
+++ metadata-ingestion/src/datahub/cli/migrate.py
@@ -23,7 +23,11 @@ from datahub.emitter.mcp_builder import (
     SchemaKey,
 )
 from datahub.emitter.rest_emitter import DatahubRestEmitter
-from datahub.ingestion.graph.client import DataHubGraph, DataHubGraphConfig
+from datahub.ingestion.graph.client import (
+    DataHubGraph,
+    DataHubGraphConfig,
+    get_default_graph,
+)
 from datahub.metadata.schema_classes import (
     ContainerKeyClass,
     ContainerPropertiesClass,
@@ -141,13 +145,10 @@ def dataplatform2instance_func(
     migration_report = MigrationReport(run_id, dry_run, keep)
     system_metadata = SystemMetadataClass(runId=run_id)
 
-    # initialize for dry-run
-    graph = DataHubGraph(config=DataHubGraphConfig(server="127.0.0.1"))
-
-    if not dry_run:
-        graph = DataHubGraph(
-            config=DataHubGraphConfig(server=cli_utils.get_session_and_host()[1])
-        )
+    if dry_run:
+        graph = DataHubGraph(config=DataHubGraphConfig(server="127.0.0.1"))
+    else:
+        graph = get_default_graph()
 
     urns_to_migrate = []
 
