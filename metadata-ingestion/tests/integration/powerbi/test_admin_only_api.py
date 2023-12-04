108be5ed7dda0a01d3fd025e2ee6c13f7e950317
Aseem Bansal <asmbansal2@gmail.com>
2023-12-04 18:08:14 +0530

    move to new batch instead

--- metadata-ingestion/tests/integration/powerbi/test_admin_only_api.py
+++ metadata-ingestion/tests/integration/powerbi/test_admin_only_api.py
@@ -9,7 +9,7 @@ from freezegun import freeze_time
 from datahub.ingestion.run.pipeline import Pipeline
 from tests.test_helpers import mce_helpers
 
-pytestmark = pytest.mark.integration_batch_1
+pytestmark = pytest.mark.integration_batch_3
 
 FROZEN_TIME = "2022-02-03 07:00:00"
 
