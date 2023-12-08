84c1031d995144cf67958b53f9ea21582f032839
salman <salman.sayyad@apptware.com>
2023-12-08 10:15:32 +0530

    feat: remove react-reflex and reuse ProfileResizer

--- datahub-web-react/src/app/search/SearchResults.tsx
+++ datahub-web-react/src/app/search/SearchResults.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Pagination, Typography } from 'antd';
 import styled from 'styled-components/macro';
-import {
-    ReflexContainer,
-    ReflexSplitter,
-    ReflexElement
-  } from 'react-reflex'
 import { Entity, FacetFilterInput, FacetMetadata, MatchedField, SearchSuggestion } from '../../types.generated';
 import { SearchCfg } from '../../conf';
 import { SearchResultsRecommendations } from './SearchResultsRecommendations';
@@ -34,8 +29,6 @@ import SearchQuerySuggester from './suggestions/SearchQuerySugggester';
 import { ANTD_GRAY_V2 } from '../entity/shared/constants';
 import { formatNumberWithoutAbbreviation } from '../shared/formatNumber';
 import SearchResultsLoadingSection from './SearchResultsLoadingSection';
-  
-import 'react-reflex/styles.css'
 
 const SearchResultsWrapper = styled.div<{ v2Styles: boolean }>`
     display: flex;
@@ -201,105 +194,98 @@ export const SearchResults = ({
                             />
                         </div>
                     )}
-                    <ReflexContainer orientation="vertical">
-                        {showBrowseV2 && isSidebarOpen && (
-                            <ReflexElement className="left-pane" flex={0.5}>
-                            <SidebarProvider selectedFilters={selectedFilters} onChangeFilters={onChangeFilters}>
-                                <BrowseProvider>
-                                    <BrowseSidebar visible={isSidebarOpen} width='100%' />
-                                </BrowseProvider>
-                            </SidebarProvider>
-                            </ReflexElement>
+                    {showBrowseV2 && (
+                        <SidebarProvider selectedFilters={selectedFilters} onChangeFilters={onChangeFilters}>
+                            <BrowseProvider>
+                                <BrowseSidebar visible={isSidebarOpen} />
+                            </BrowseProvider>
+                        </SidebarProvider>
+                    )}
+                    <ResultContainer v2Styles={showSearchFiltersV2}>
+                        <PaginationInfoContainer v2Styles={showSearchFiltersV2}>
+                            <LeftControlsContainer>
+                                {showBrowseV2 && <ToggleSidebarButton isOpen={isSidebarOpen} onClick={toggleSidebar} />}
+                                <Typography.Text>
+                                    Showing{' '}
+                                    <b>
+                                        {lastResultIndex > 0 ? (page - 1) * pageSize + 1 : 0} - {lastResultIndex}
+                                    </b>{' '}
+                                    of{' '}
+                                    <b>
+                                        {totalResults >= 10000
+                                            ? `${formatNumberWithoutAbbreviation(10000)}+`
+                                            : formatNumberWithoutAbbreviation(totalResults)}
+                                    </b>{' '}
+                                    results
+                                </Typography.Text>
+                            </LeftControlsContainer>
+                            <SearchMenuContainer>
+                                <SearchSortSelect />
+                                <SearchExtendedMenu
+                                    downloadSearchResults={downloadSearchResults}
+                                    filters={generateOrFilters(unionType, selectedFilters)}
+                                    query={query}
+                                    viewUrn={viewUrn}
+                                    setShowSelectMode={setIsSelectMode}
+                                    totalResults={totalResults}
+                                />
+                            </SearchMenuContainer>
+                        </PaginationInfoContainer>
+                        {isSelectMode && (
+                            <StyledTabToolbar>
+                                <SearchSelectBar
+                                    isSelectAll={
+                                        selectedEntities.length > 0 &&
+                                        isListSubset(searchResultUrns, selectedEntityUrns)
+                                    }
+                                    selectedEntities={selectedEntities}
+                                    onChangeSelectAll={onChangeSelectAll}
+                                    onCancel={() => setIsSelectMode(false)}
+                                    refetch={refetch}
+                                />
+                            </StyledTabToolbar>
                         )}
-                        {isSidebarOpen && <ReflexSplitter/>}
-                        <ReflexElement className="right-pane"flex={2} style={{ display: 'flex'}}>
-                            <ResultContainer v2Styles={showSearchFiltersV2}>
-                                <PaginationInfoContainer v2Styles={showSearchFiltersV2}>
-                                    <LeftControlsContainer>
-                                        {showBrowseV2 && <ToggleSidebarButton isOpen={isSidebarOpen} onClick={toggleSidebar} />}
-                                        <Typography.Text>
-                                            Showing{' '}
-                                            <b>
-                                                {lastResultIndex > 0 ? (page - 1) * pageSize + 1 : 0} - {lastResultIndex}
-                                            </b>{' '}
-                                            of{' '}
-                                            <b>
-                                                {totalResults >= 10000
-                                                    ? `${formatNumberWithoutAbbreviation(10000)}+`
-                                                    : formatNumberWithoutAbbreviation(totalResults)}
-                                            </b>{' '}
-                                            results
-                                        </Typography.Text>
-                                    </LeftControlsContainer>
-                                    <SearchMenuContainer>
-                                        <SearchSortSelect />
-                                        <SearchExtendedMenu
-                                            downloadSearchResults={downloadSearchResults}
-                                            filters={generateOrFilters(unionType, selectedFilters)}
-                                            query={query}
-                                            viewUrn={viewUrn}
-                                            setShowSelectMode={setIsSelectMode}
-                                            totalResults={totalResults}
-                                        />
-                                    </SearchMenuContainer>
-                                </PaginationInfoContainer>
-                                {isSelectMode && (
-                                    <StyledTabToolbar>
-                                        <SearchSelectBar
-                                            isSelectAll={
-                                                selectedEntities.length > 0 &&
-                                                isListSubset(searchResultUrns, selectedEntityUrns)
-                                            }
-                                            selectedEntities={selectedEntities}
-                                            onChangeSelectAll={onChangeSelectAll}
-                                            onCancel={() => setIsSelectMode(false)}
-                                            refetch={refetch}
-                                        />
-                                    </StyledTabToolbar>
-                                )}
-                                {(error && <ErrorSection />) ||
-                                    (loading && !combinedSiblingSearchResults.length && <SearchResultsLoadingSection />) ||
-                                    (combinedSiblingSearchResults && (
-                                        <SearchResultListContainer v2Styles={showSearchFiltersV2}>
-                                            {totalResults > 0 && <SearchQuerySuggester suggestions={suggestions} />}
-                                            <SearchResultList
-                                                loading={loading}
-                                                query={query}
-                                                searchResults={combinedSiblingSearchResults}
-                                                totalResultCount={totalResults}
-                                                isSelectMode={isSelectMode}
-                                                selectedEntities={selectedEntities}
-                                                setSelectedEntities={setSelectedEntities}
-                                                suggestions={suggestions}
+                        {(error && <ErrorSection />) ||
+                            (loading && !combinedSiblingSearchResults.length && <SearchResultsLoadingSection />) ||
+                            (combinedSiblingSearchResults && (
+                                <SearchResultListContainer v2Styles={showSearchFiltersV2}>
+                                    {totalResults > 0 && <SearchQuerySuggester suggestions={suggestions} />}
+                                    <SearchResultList
+                                        loading={loading}
+                                        query={query}
+                                        searchResults={combinedSiblingSearchResults}
+                                        totalResultCount={totalResults}
+                                        isSelectMode={isSelectMode}
+                                        selectedEntities={selectedEntities}
+                                        setSelectedEntities={setSelectedEntities}
+                                        suggestions={suggestions}
+                                    />
+                                    {totalResults > 0 && (
+                                        <PaginationControlContainer id="search-pagination">
+                                            <Pagination
+                                                current={page}
+                                                pageSize={numResultsPerPage}
+                                                total={totalResults}
+                                                showLessItems
+                                                onChange={onChangePage}
+                                                showSizeChanger={totalResults > SearchCfg.RESULTS_PER_PAGE}
+                                                onShowSizeChange={(_currNum, newNum) => setNumResultsPerPage(newNum)}
+                                                pageSizeOptions={['10', '20', '50', '100']}
                                             />
-                                            {totalResults > 0 && (
-                                                <PaginationControlContainer id="search-pagination">
-                                                    <Pagination
-                                                        current={page}
-                                                        pageSize={numResultsPerPage}
-                                                        total={totalResults}
-                                                        showLessItems
-                                                        onChange={onChangePage}
-                                                        showSizeChanger={totalResults > SearchCfg.RESULTS_PER_PAGE}
-                                                        onShowSizeChange={(_currNum, newNum) => setNumResultsPerPage(newNum)}
-                                                        pageSizeOptions={['10', '20', '50', '100']}
-                                                    />
-                                                </PaginationControlContainer>
-                                            )}
-                                            {authenticatedUserUrn && (
-                                                <SearchResultsRecommendationsContainer>
-                                                    <SearchResultsRecommendations
-                                                        userUrn={authenticatedUserUrn}
-                                                        query={query}
-                                                        filters={selectedFilters}
-                                                    />
-                                                </SearchResultsRecommendationsContainer>
-                                            )}
-                                        </SearchResultListContainer>
-                                    ))}
-                            </ResultContainer>
-                        </ReflexElement>
-                    </ReflexContainer>
+                                        </PaginationControlContainer>
+                                    )}
+                                    {authenticatedUserUrn && (
+                                        <SearchResultsRecommendationsContainer>
+                                            <SearchResultsRecommendations
+                                                userUrn={authenticatedUserUrn}
+                                                query={query}
+                                                filters={selectedFilters}
+                                            />
+                                        </SearchResultsRecommendationsContainer>
+                                    )}
+                                </SearchResultListContainer>
+                            ))}
+                    </ResultContainer>
                 </SearchBody>
             </SearchResultsWrapper>
         </>
--- datahub-web-react/src/app/search/sidebar/BrowseSidebar.tsx
+++ datahub-web-react/src/app/search/sidebar/BrowseSidebar.tsx
@@ -8,11 +8,16 @@ import SidebarLoadingError from './SidebarLoadingError';
 import { SEARCH_RESULTS_BROWSE_SIDEBAR_ID } from '../../onboarding/config/SearchOnboardingConfig';
 import useSidebarEntities from './useSidebarEntities';
 import { ANTD_GRAY_V2 } from '../../entity/shared/constants';
+import { ProfileSidebarResizer } from '../../entity/shared/containers/profile/sidebar/ProfileSidebarResizer';
 
 
-const Sidebar = styled.div<{ visible: boolean; width: string }>`
+export const MAX_BROWSER_WIDTH = 500;
+export const MIN_BROWSWER_WIDTH = 200;
+
+export const SidebarWrapper = styled.div<{ visible: boolean; width: number }>`
     height: 100%;
-    width: ${(props) => (`${props.width}px`)};
+    width: ${(props) => (props.visible ? `${props.width}px` : '0')};
+    min-width: ${(props) => (props.visible ? `${props.width}px` : '0')};
     transition: width 250ms ease-in-out;
     border-right: 1px solid ${(props) => props.theme.styles['border-color-base']};
     background-color: ${ANTD_GRAY_V2[1]};
@@ -39,7 +44,6 @@ const SidebarBody = styled.div<{ visible: boolean }>`
 
 type Props = {
     visible: boolean;
-    width: string;
 };
 
 const EntityAggregationsDraggableList = ({entityAggregations}: any) =>{
@@ -88,22 +92,32 @@ const EntityAggregationsDraggableList = ({entityAggregations}: any) =>{
     )
 }
 
-const BrowseSidebar = ({ visible, width }: Props) => {
+const BrowseSidebar = ({ visible }: Props) => {
     const { error, entityAggregations, retry } = useSidebarEntities({
         skip: !visible,
     });
+    const [browserWidth, setBrowserWith] = useState(window.innerWidth * 0.2);
 
     return (
-        <Sidebar visible={visible} width={width} id={SEARCH_RESULTS_BROWSE_SIDEBAR_ID} data-testid="browse-v2">
-            <SidebarHeader>
-                <Typography.Text strong>Navigate</Typography.Text>
-            </SidebarHeader>
-            <SidebarBody visible={visible}>
-                {entityAggregations && !entityAggregations.length && <div>No results found</div>}
-                <EntityAggregationsDraggableList entityAggregations={entityAggregations}/>
-                {error && <SidebarLoadingError onClickRetry={retry} />}
-            </SidebarBody>
-        </Sidebar>
+        <>
+            <SidebarWrapper visible={visible} width={browserWidth} id={SEARCH_RESULTS_BROWSE_SIDEBAR_ID} data-testid="browse-v2">
+                <SidebarHeader>
+                    <Typography.Text strong>Navigate</Typography.Text>
+                </SidebarHeader>
+                <SidebarBody visible={visible}>
+                    {entityAggregations && !entityAggregations.length && <div>No results found</div>}
+                    <EntityAggregationsDraggableList entityAggregations={entityAggregations}/>
+                    {error && <SidebarLoadingError onClickRetry={retry} />}
+                </SidebarBody>
+            </SidebarWrapper>
+            <ProfileSidebarResizer
+                setSidePanelWidth={(widthProp) =>
+                    setBrowserWith(Math.min(Math.max(widthProp, MIN_BROWSWER_WIDTH), MAX_BROWSER_WIDTH))
+                }
+                initialSize={browserWidth}
+                isSidebarOnLeft
+            />
+        </>
     );
 };
 
