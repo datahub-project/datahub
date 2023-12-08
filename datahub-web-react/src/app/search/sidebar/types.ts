455fe8e65dc8e8aa40aebc0186292820e0508c38
John Joyce <john@acryl.io>
2023-12-08 12:07:41 -0800

    Adding support for platform browse UI

--- datahub-web-react/src/app/search/sidebar/types.ts
+++ datahub-web-react/src/app/search/sidebar/types.ts
@@ -4,3 +4,14 @@ export type SidebarFilters = Pick<
     ReturnType<typeof useGetSearchQueryInputs>,
     'entityFilters' | 'query' | 'orFilters' | 'viewUrn'
 >;
+
+export enum BrowseMode {
+    /**
+     * Browse by entity type
+     */
+    ENTITY_TYPE = 'ENTITY_TYPE',
+    /**
+     * Browse by platform
+     */
+    PLATFORM = 'PLATFORM',
+}
