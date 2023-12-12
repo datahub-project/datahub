a865b4a60ac1cbf3f8f6e1899f060c08232fe5bd
arunvasudevan <arun.vasudevan@onepeloton.com>
2023-12-12 17:01:15 -0600

    Fix formatting

--- metadata-ingestion/src/datahub/api/circuit_breaker/__init__.py
+++ metadata-ingestion/src/datahub/api/circuit_breaker/__init__.py
@@ -2,6 +2,7 @@ import logging
 from gql.transport.requests import log as requests_logger
 
 requests_logger.setLevel(logging.WARNING)
+
 from datahub.api.circuit_breaker.assertion_circuit_breaker import (
     AssertionCircuitBreaker,
     AssertionCircuitBreakerConfig,
