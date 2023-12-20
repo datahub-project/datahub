29e8089eb9b58b3adaba8501c760e0d1c6f3f32d
Fernando Marino <f.marino@rheagroup.com>
2023-12-20 17:18:38 +0100

    fix for https://github.com/datahub-project/datahub/issues/7914 adding http proxies support for authentication scheme other than bearer

--- metadata-ingestion/src/datahub/ingestion/source/openapi.py
+++ metadata-ingestion/src/datahub/ingestion/source/openapi.py
@@ -52,6 +52,7 @@ class OpenApiConfig(ConfigModel):
     ignore_endpoints: list = Field(default=[], description="")
     username: str = Field(default="", description="")
     password: str = Field(default="", description="")
+    proxies: dict = Field(default={"http": None, "https": None}, description="")
     forced_examples: dict = Field(default={}, description="")
     token: Optional[str] = Field(default=None, description="")
     get_token: dict = Field(default={}, description="")
@@ -87,9 +88,10 @@ class OpenApiConfig(ConfigModel):
                     password=self.password,
                     tok_url=url4req,
                     method=self.get_token["request_type"],
+                    proxies=self.proxies
                 )
             sw_dict = get_swag_json(
-                self.url, token=self.token, swagger_file=self.swagger_file
+                self.url, token=self.token, swagger_file=self.swagger_file, proxies=self.proxies
             )  # load the swagger file
 
         else:  # using basic auth for accessing endpoints
@@ -98,6 +100,7 @@ class OpenApiConfig(ConfigModel):
                 username=self.username,
                 password=self.password,
                 swagger_file=self.swagger_file,
+                proxies=self.proxies
             )
         return sw_dict
 
@@ -258,10 +261,10 @@ class APISource(Source, ABC):
                 tot_url = clean_url(config.url + self.url_basepath + endpoint_k)
 
                 if config.token:
-                    response = request_call(tot_url, token=config.token)
+                    response = request_call(tot_url, token=config.token, proxies=config.proxies)
                 else:
                     response = request_call(
-                        tot_url, username=config.username, password=config.password
+                        tot_url, username=config.username, password=config.password, proxies=config.proxies
                     )
                 if response.status_code == 200:
                     fields2add, root_dataset_samples[dataset_name] = extract_fields(
@@ -281,10 +284,10 @@ class APISource(Source, ABC):
                     url_guess = try_guessing(endpoint_k, root_dataset_samples)
                     tot_url = clean_url(config.url + self.url_basepath + url_guess)
                     if config.token:
-                        response = request_call(tot_url, token=config.token)
+                        response = request_call(tot_url, token=config.token, proxies=config.proxies)
                     else:
                         response = request_call(
-                            tot_url, username=config.username, password=config.password
+                            tot_url, username=config.username, password=config.password, proxies=config.proxies
                         )
                     if response.status_code == 200:
                         fields2add, _ = extract_fields(response, dataset_name)
@@ -304,10 +307,10 @@ class APISource(Source, ABC):
                     )
                     tot_url = clean_url(config.url + self.url_basepath + composed_url)
                     if config.token:
-                        response = request_call(tot_url, token=config.token)
+                        response = request_call(tot_url, token=config.token, proxies=config.proxies)
                     else:
                         response = request_call(
-                            tot_url, username=config.username, password=config.password
+                            tot_url, username=config.username, password=config.password, proxies=config.proxies
                         )
                     if response.status_code == 200:
                         fields2add, _ = extract_fields(response, dataset_name)
