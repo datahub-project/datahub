abb7b7a724a619dbb9893a6fa7d7e3023d6dffc1
gaurav2733 <gaurav.wagh@apptware.com>
2023-12-25 17:29:36 +0530

    updated cypress test

--- smoke-test/tests/cypress/cypress/e2e/mutations/add_users.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/add_users.js
@@ -86,16 +86,16 @@ describe("Reset Password Functionality", () => {
         cy.enterTextInTestId("password", password);
         cy.enterTextInTestId("confirmPassword", password);
         cy.get("[type=submit]").click();
+        cy.wait(2000);
+        cy.logout();
+        cy.visit("/reset?reset_token=bad_token");
+        cy.enterTextInTestId("email", email);
+        cy.enterTextInTestId("password", password);
+        cy.enterTextInTestId("confirmPassword", password);
+        cy.get("[type=submit]").click();
+        cy.waitTextVisible("Failed to log in!");
       })
-      // .then(() => {
-      //   cy.logout();
-      //   cy.visit("/reset?reset_token=bad_token");
-      //   cy.enterTextInTestId("email", email);
-      //   cy.enterTextInTestId("password", password);
-      //   cy.enterTextInTestId("confirmPassword", password);
-      //   cy.get("[type=submit]").click();
-      //   cy.waitTextVisible("Failed to log in!");
-      // });
+     
   });
 });
 // Verify you can’t generate a reset password link for a non-native user (root, for example)
