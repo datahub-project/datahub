44bd61ffda93102c2aab135b4fb482dacefd276b
sumitappt <sumit.patil@apptware.com>
2024-01-02 22:48:25 +0530

    refactor: PRD-900 | Review glossary related cypress tests and extract out/simplify code

--- smoke-test/tests/cypress/cypress/e2e/glossary/glossaryTerm.js
+++ smoke-test/tests/cypress/cypress/e2e/glossary/glossaryTerm.js
@@ -1,107 +1,56 @@
+
 describe("glossaryTerm", () => {
   it("can visit related entities", () => {
-    cy.login();
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
-    cy.wait(5000);
-    cy.contains("of 0").should("not.exist");
-    cy.contains(/of [0-9]+/);
+      cy.loginAndVisitRelatedEntities();
+      cy.contains("of 0").should("not.exist");
+      cy.contains(/of [0-9]+/);
   });
 
   it("can search related entities by query", () => {
-    cy.login();
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
-    cy.get('[placeholder="Filter entities..."]').click().type(
-      "logging{enter}"
-    );
-    cy.wait(5000);
-    cy.contains("of 0").should("not.exist");
-    cy.contains(/of 1/);
-    cy.contains("cypress_logging_events");
-    cy.contains("SampleCypressHdfsDataset").should("not.exist");
+      cy.loginAndVisitRelatedEntities();
+      cy.get('[placeholder="Filter entities..."]').click().type("logging{enter}");
+      cy.contains("of 0").should("not.exist");
+      cy.contains(/of 1/);
+      cy.contains("cypress_logging_events");
+      cy.contains("SampleCypressHdfsDataset").should("not.exist");
   });
 
   it("can apply filters on related entities", () => {
-    cy.login();
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
-    cy.wait(5000);
-    // Select tags filter
-    cy.contains("Filters").click();
-
-    cy.get('[data-testid="facet-tags-urn:li:tag:Cypress2"]').click({force: true})
-    cy.wait(3000);
-
-    // Unselect tags filter
-    cy.contains("cypress_logging_events").should("not.exist");
-    cy.contains("SampleCypressHdfsDataset");
-    cy.get('[data-testid="facet-tags-urn:li:tag:Cypress2"]').click({force: true})
-
-    cy.wait(3000);
-    cy.contains("cypress_logging_events");
-    cy.contains("SampleCypressHdfsDataset");
+      cy.loginAndVisitRelatedEntities();
+      cy.applyTagFilter("urn:li:tag:Cypress2");
+      cy.contains("cypress_logging_events").should("not.exist");
+      cy.contains("SampleCypressHdfsDataset");
   });
 
   it("can search related entities by a specific tag using advanced search", () => {
-    cy.login();
-
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
-    cy.wait(3000);
-    cy.contains("Filters").click();
-    cy.contains("Advanced").click();
-    cy.contains("Add Filter").click();
-    cy.contains(/^Tag$/).click({ force: true });
-
-    cy.selectOptionInTagTermModal("Cypress2");
-
-    cy.contains("SampleCypressHdfsDataset");
-    // Only 1 result.
-    cy.contains("of 1");
+      cy.loginAndVisitRelatedEntities();
+      cy.applyAdvancedSearchFilter("Tag", "Cypress2");
+      cy.contains("SampleCypressHdfsDataset");
+      cy.contains("of 1");
   });
 
-  it("can search related entities by AND-ing two concepts using advanced search", () => {
-    cy.login();
-
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
+  it("can search related entities by AND-ing two concepts using search", () => {
+    cy.loginAndVisitRelatedEntities();
     cy.wait(3000);
-    cy.contains("Filters").click();
-    cy.contains("Advanced").click();
-    cy.contains("Add Filter").click();
-    cy.contains(/^Tag$/).click({ force: true });
+    // Apply tags filter using advanced search
+    cy.applyAdvancedSearchFilter();
 
-    cy.selectOptionInTagTermModal("Cypress2")
-
-    cy.contains("Add Filter").click();
+    cy.contains("Add Filter").click()
     cy.get('[data-testid="adv-search-add-filter-description"]').click({
       force: true,
     });
     cy.get('[data-testid="edit-text-input"]').type("my hdfs");
     cy.get('[data-testid="edit-text-done-btn"]').click({ force: true });
-    cy.contains("SampleCypressHdfsDataset");
-    cy.contains("of 1");
+    cy.contains('SampleCypressHdfsDataset');
+    cy.contains("of 1");  
   });
 
-  it("can search related entities by OR-ing two concepts using advanced search", () => {
-    cy.login();
-
-    cy.visit("/glossaryTerm/urn:li:glossaryTerm:CypressNode.CypressColumnInfoType/Related%20Entities");
-    cy.wait(3000);
-    cy.contains("Filters").click();
-    cy.contains("Advanced").click();
-    cy.contains("Add Filter").click();
-    cy.get('[data-testid="adv-search-add-filter-description"]').click({
-      force: true,
-    });
-    cy.get('[data-testid="edit-text-input"]').type("single log event");
-    cy.get('[data-testid="edit-text-done-btn"]').click({ force: true });
-    cy.wait(2000);
-    cy.contains("Add Filter").click();
-    cy.contains(/^Tag$/).click({ force: true });
-
-    cy.selectOptionInTagTermModal("Cypress2");
-
-    cy.contains("all filters").click();
-    cy.contains("any filter").click({ force: true });
-
-    cy.contains("SampleCypressHdfsDataset");
-    cy.contains("cypress_logging_events");
+  it("can search related entities by OR-ing two concepts using search", () => {
+    cy.loginAndVisitRelatedEntities();
+      cy.applyAdvancedSearchFilter("Description", "single log event");
+      cy.applyBasicSearchFilter("Tag", "Cypress2");
+      cy.searchByConceptsWithLogicalOperator("Cypress", "Tag", "any filter");
+      cy.contains("SampleCypressHdfsDataset");
+      cy.contains("cypress_logging_events");
   });
 });
--- smoke-test/tests/cypress/cypress/e2e/glossary/glossary_navigation.js
+++ smoke-test/tests/cypress/cypress/e2e/glossary/glossary_navigation.js
@@ -5,57 +5,26 @@ const glossaryParentGroup = "CypressNode";
 
 describe("glossary sidebar navigation test", () => {
     it("create term and term parent group, move and delete term group", () => {
-
-        // Create a new term group and term, move term to the group
         cy.loginWithCredentials();
-        cy.goToGlossaryList();
-        cy.clickOptionWithTestId("add-term-group-button");
-        cy.waitTextVisible("Create Term Group");
-        cy.enterTextInTestId("create-glossary-entity-modal-name", glossaryTermGroup);
-        cy.clickOptionWithTestId("glossary-entity-modal-create-button");
-        cy.get('[data-testid="glossary-browser-sidebar"]').contains(glossaryTermGroup).should("be.visible");
-        cy.clickOptionWithTestId("add-term-button"); 
-        cy.waitTextVisible("Created Term Group!");
-        cy.waitTextVisible("Create Glossary Term");
-        cy.enterTextInTestId("create-glossary-entity-modal-name", glossaryTerm);
-        cy.clickOptionWithTestId("glossary-entity-modal-create-button").wait(3000);
-        cy.get('[data-testid="glossary-browser-sidebar"]').contains(glossaryTerm).click().wait(3000);
-        cy.openThreeDotDropdown();
-        cy.clickOptionWithTestId("entity-menu-move-button")
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryTermGroup).click({force: true});
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryTermGroup).should("be.visible");
-        cy.clickOptionWithTestId("glossary-entity-modal-move-button");
-        cy.waitTextVisible("Moved Glossary Term!");
+
+        // Create term group
+        cy.createGlossaryTermGroup(glossaryTermGroup);
+        cy.clickOptionWithTestId("add-term-button");
+        cy.createTerm(glossaryTerm);
+        cy.moveGlossaryEntityToGroup(glossaryTerm, glossaryTermGroup, `Moved Glossary Term!`);
 
         // Ensure the new term is under the parent term group in the navigation sidebar
-        cy.get('[data-testid="glossary-browser-sidebar"]').contains(glossaryTermGroup).click().wait(3000);
-        cy.get('*[class^="GlossaryEntitiesList"]').contains(glossaryTerm).should("be.visible");
+        cy.navigateToParentAndCheckTermGroup(glossaryTermGroup, glossaryTerm);
 
         // Create another term and move it to the same term group
         cy.clickOptionWithText(glossaryTermGroup);
         cy.openThreeDotDropdown();
         cy.clickOptionWithTestId("entity-menu-add-term-button");
+        cy.createTerm(glossarySecondTerm)
+        cy.moveGlossaryEntityToGroup(glossarySecondTerm, glossaryTermGroup, `Moved Glossary Term!`);
 
-        // Wait for the create term modal to be visible
-        cy.waitTextVisible("Create Glossary Term");
-        cy.enterTextInTestId("create-glossary-entity-modal-name", glossarySecondTerm);
-        cy.clickOptionWithTestId("glossary-entity-modal-create-button");
-
-        // Wait for the new term to be visible in the sidebar
-        cy.clickOptionWithText(glossarySecondTerm).wait(3000);
-
-        // Move the term to the created term group
-        cy.openThreeDotDropdown();
-        cy.clickOptionWithTestId("entity-menu-move-button");
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryTermGroup).click({ force: true });
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryTermGroup).should("be.visible");
-        cy.clickOptionWithTestId("glossary-entity-modal-move-button");
-        cy.waitTextVisible("Moved Glossary Term!");
-
-        // Ensure the new term is under the parent term group in the navigation sidebar
-        cy.get('[data-testid="glossary-browser-sidebar"]').contains(glossaryTermGroup).click();
-        cy.get('*[class^="GlossaryEntitiesList"]').contains(glossarySecondTerm).should("be.visible");
-
+         // Navigate to parent group and check term group visibility
+         cy.navigateToParentAndCheckTermGroup(glossaryTermGroup, glossarySecondTerm);
 
         // Switch between terms and ensure the "Properties" tab is active
         cy.clickOptionWithText(glossaryTerm);
@@ -66,34 +35,15 @@ describe("glossary sidebar navigation test", () => {
 
         // Move a term group from the root level to be under a parent term group
         cy.goToGlossaryList();
-        cy.clickOptionWithText(glossaryTermGroup);
-        cy.wait(3000)
-        cy.openThreeDotDropdown();
-        cy.clickOptionWithText("Move");
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryParentGroup).click({force: true});
-        cy.get('[data-testid="move-glossary-entity-modal"]').contains(glossaryParentGroup).should("be.visible");
-        cy.clickOptionWithTestId("glossary-entity-modal-move-button");
-        cy.waitTextVisible("Moved Term Group!");
+        cy.moveGlossaryEntityToGroup(glossaryTermGroup, glossaryParentGroup,'Moved Term Group!');
 
-        // Ensure it is no longer on the sidebar navigator at the top level but shows up under the new parent
-        cy.get('[data-testid="glossary-browser-sidebar"]').contains(glossaryParentGroup).click().wait(3000);
-        cy.get('*[class^="GlossaryEntitiesList"]').contains(glossaryTermGroup).should("be.visible");
+        // Navigate to parent group and check term group visibility
+        cy.navigateToParentAndCheckTermGroup(glossaryParentGroup, glossaryTermGroup);
 
-        // Delete a term group
-        cy.goToGlossaryList();
-        cy.clickOptionWithText(glossaryParentGroup);
-        cy.clickOptionWithText(glossaryTermGroup);
-        cy.clickOptionWithText(glossaryTerm).wait(3000);
-        cy.deleteFromDropdown();
-        cy.waitTextVisible("Deleted Glossary Term!");
-        cy.clickOptionWithText(glossaryTermGroup);
-        cy.clickOptionWithText(glossarySecondTerm).wait(3000);
-        cy.deleteFromDropdown();
-        cy.waitTextVisible("Deleted Glossary Term!");
-        cy.clickOptionWithText(glossaryParentGroup);
-        cy.clickOptionWithText(glossaryTermGroup).wait(3000);
-        cy.deleteFromDropdown();
-        cy.waitTextVisible("Deleted Term Group!");
+        // Delete glossary terms and term group
+        cy.deleteGlossaryTerm(glossaryParentGroup, glossaryTermGroup, glossaryTerm);
+        cy.deleteGlossaryTerm(glossaryParentGroup, glossaryTermGroup, glossarySecondTerm);
+        cy.deleteTermGroup(glossaryParentGroup, glossaryTermGroup);
 
         // Ensure it is no longer in the sidebar navigator
         cy.ensureTextNotPresent(glossaryTerm);
