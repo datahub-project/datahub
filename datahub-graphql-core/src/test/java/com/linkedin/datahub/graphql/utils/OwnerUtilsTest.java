7457c27feb561a173f851507b576b5b68c47c27a
Aseem Bansal <asmbansal2@gmail.com>
2024-01-09 18:37:08 +0530

    update unit tests

--- datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/utils/OwnerUtilsTest.java
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/utils/OwnerUtilsTest.java
@@ -13,7 +13,7 @@ public class OwnerUtilsTest {
 
   public static String TECHNICAL_OWNER_OWNERSHIP_TYPE_URN =
       "urn:li:ownershipType:__system__technical_owner";
-
+  public static String BUSINESS_OWNER_OWNERSHIP_TYPE_URN = "urn:li:ownershipType:__system__business_owner";
   @Test
   public void testMapOwnershipType() {
     assertEquals(
@@ -32,38 +32,64 @@ public class OwnerUtilsTest {
   }
 
   @Test
-  public void testIsOwnerEqualWithLegacyTypeIsNotConsidered() throws URISyntaxException {
+  public void testIsOwnerEqualWithLegacyTypeOnly() throws URISyntaxException {
 
     Urn technicalOwnershipTypeUrn = new Urn(TECHNICAL_OWNER_OWNERSHIP_TYPE_URN);
     Urn ownerUrn1 = new Urn("urn:li:corpuser:foo");
-    Owner owner1 = new Owner();
-    owner1.setOwner(ownerUrn1);
-    owner1.setType(OwnershipType.TECHNICAL_OWNER);
+    Owner ownerWithTechnicalOwnership = new Owner();
+    ownerWithTechnicalOwnership.setOwner(ownerUrn1);
+    ownerWithTechnicalOwnership.setType(OwnershipType.TECHNICAL_OWNER);
 
-    assertTrue(OwnerUtils.isOwnerEqual(owner1, ownerUrn1, technicalOwnershipTypeUrn));
+    assertTrue(OwnerUtils.isOwnerEqual(ownerWithTechnicalOwnership, ownerUrn1, technicalOwnershipTypeUrn));
 
-    Owner owner2 = new Owner();
-    owner2.setOwner(ownerUrn1);
-    owner2.setType(OwnershipType.BUSINESS_OWNER);
-    assertTrue(
-        OwnerUtils.isOwnerEqual(owner1, ownerUrn1, new Urn(TECHNICAL_OWNER_OWNERSHIP_TYPE_URN)));
+    Owner ownerWithBusinessOwnership = new Owner();
+    ownerWithBusinessOwnership.setOwner(ownerUrn1);
+    ownerWithBusinessOwnership.setType(OwnershipType.BUSINESS_OWNER);
+    assertFalse(
+        OwnerUtils.isOwnerEqual(ownerWithBusinessOwnership, ownerUrn1, new Urn(TECHNICAL_OWNER_OWNERSHIP_TYPE_URN)));
   }
 
   @Test
-  public void testIsOwnerEqualOwnershipTypeUrnIsConsidered() throws URISyntaxException {
+  public void testIsOwnerEqualOnlyOwnershipTypeUrn() throws URISyntaxException {
 
     Urn technicalOwnershipTypeUrn = new Urn(TECHNICAL_OWNER_OWNERSHIP_TYPE_URN);
+    Urn businessOwnershipTypeUrn = new Urn(BUSINESS_OWNER_OWNERSHIP_TYPE_URN);
     Urn ownerUrn1 = new Urn("urn:li:corpuser:foo");
-    Owner owner1 = new Owner();
-    owner1.setOwner(ownerUrn1);
-    owner1.setTypeUrn(technicalOwnershipTypeUrn);
 
-    assertTrue(OwnerUtils.isOwnerEqual(owner1, ownerUrn1, technicalOwnershipTypeUrn));
+    Owner ownerWithTechnicalOwnership = new Owner();
+    ownerWithTechnicalOwnership.setOwner(ownerUrn1);
+    ownerWithTechnicalOwnership.setTypeUrn(technicalOwnershipTypeUrn);
 
-    Urn businessOwnershipTypeUrn = new Urn("urn:li:ownershipType:__system__business_owner");
-    Owner owner2 = new Owner();
-    owner2.setOwner(ownerUrn1);
-    owner2.setTypeUrn(businessOwnershipTypeUrn);
-    assertTrue(OwnerUtils.isOwnerEqual(owner1, ownerUrn1, technicalOwnershipTypeUrn));
+    Owner ownerWithBusinessOwnership = new Owner();
+    ownerWithBusinessOwnership.setOwner(ownerUrn1);
+    ownerWithBusinessOwnership.setTypeUrn(businessOwnershipTypeUrn);
+
+    Owner ownerWithoutOwnershipType = new Owner();
+    ownerWithoutOwnershipType.setOwner(ownerUrn1);
+
+    assertTrue(OwnerUtils.isOwnerEqual(ownerWithTechnicalOwnership, ownerUrn1, technicalOwnershipTypeUrn));
+    assertFalse(OwnerUtils.isOwnerEqual(ownerWithBusinessOwnership, ownerUrn1, technicalOwnershipTypeUrn));
+    assertFalse(OwnerUtils.isOwnerEqual(ownerWithTechnicalOwnership, ownerUrn1, null));
+    assertTrue(OwnerUtils.isOwnerEqual(ownerWithoutOwnershipType, ownerUrn1, null));
+  }
+
+  public void testIsOwnerEqualWithBothLegacyAndNewType() throws URISyntaxException {
+    Urn technicalOwnershipTypeUrn = new Urn(TECHNICAL_OWNER_OWNERSHIP_TYPE_URN);
+    Urn businessOwnershipTypeUrn = new Urn(BUSINESS_OWNER_OWNERSHIP_TYPE_URN);
+    Urn ownerUrn1 = new Urn("urn:li:corpuser:foo");
+
+    Owner ownerWithLegacyTechnicalOwnership = new Owner();
+    ownerWithLegacyTechnicalOwnership.setOwner(ownerUrn1);
+    ownerWithLegacyTechnicalOwnership.setType(OwnershipType.TECHNICAL_OWNER);
+
+    assertTrue(OwnerUtils.isOwnerEqual(ownerWithLegacyTechnicalOwnership, ownerUrn1, technicalOwnershipTypeUrn));
+    assertFalse(OwnerUtils.isOwnerEqual(ownerWithLegacyTechnicalOwnership, ownerUrn1, businessOwnershipTypeUrn));
+
+    Owner ownerWithNewTechnicalOwnership = new Owner();
+    ownerWithLegacyTechnicalOwnership.setOwner(ownerUrn1);
+    ownerWithLegacyTechnicalOwnership.setTypeUrn(technicalOwnershipTypeUrn);
+
+    assertTrue(OwnerUtils.isOwnerEqual(ownerWithNewTechnicalOwnership, ownerUrn1, technicalOwnershipTypeUrn));
+    assertFalse(OwnerUtils.isOwnerEqual(ownerWithNewTechnicalOwnership, ownerUrn1, businessOwnershipTypeUrn));
   }
 }
