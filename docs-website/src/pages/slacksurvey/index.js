606f368fac3ddf31dc28ca6c3ed30bcbe3ef1f7a
socar-dini <0327jane@gmail.com>
2024-01-12 12:11:51 +0900

    fix: fix the misplacement

--- docs-website/src/pages/slacksurvey/index.js
+++ docs-website/src/pages/slacksurvey/index.js
@@ -1,55 +1,43 @@
-import React, { useEffect, useRef } from 'react';
-import { Helmet } from 'react-helmet-async';
+import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 function SlackSurvey() {
     const { siteConfig = {} } = useDocusaurusContext();
-    const scriptContainerRef = useRef(null);
 
     useEffect(() => {
-        if (scriptContainerRef.current) {
-            const script = document.createElement('script');
-            script.src = '//js.hsforms.net/forms/embed/v2.js';
-            script.async = true;
-            script.type = 'text/javascript';
-            script.onload = () => {
-                if (window.hbspt) {
-                    window.hbspt.forms.create({
-                        region: 'na1',
-                        portalId: '14552909',
-                        formId: '91357965-a8dc-4e20-875e-5f87e6b9defb',
-                    });
-                }
-            };
+        const script = document.createElement('script');
+        script.src = "//js.hsforms.net/forms/embed/v2.js";
+        script.async = true;
+        script.type = 'text/javascript';
+        document.body.appendChild(script);
 
-            scriptContainerRef.current.appendChild(script);
+        script.onload = () => {
+            if (window.hbspt) {
+                window.hbspt.forms.create({
+                    region: "na1",
+                    portalId: "14552909",
+                    formId: "91357965-a8dc-4e20-875e-5f87e6b9defb",
+                    target: '#hubspotForm' // Targeting the div with the specific ID
+                });
+            }
+        };
 
-            return () => {
-                if (scriptContainerRef.current) {
-                    scriptContainerRef.current.removeChild(script);
-                }
-            };
-        }
+        return () => {
+            document.body.removeChild(script);
+        };
     }, []);
 
     return (
         <Layout
             title={siteConfig.tagline}
-            description="DataHub is a data discovery application built on an extensible metadata platform that helps you tame the complexity of diverse data ecosystems."
-        >
-            <Helmet>
-                <script src="//js.hsforms.net/forms/embed/v2.js" async type="text/javascript" />
-            </Helmet>
+            description="Description of the page">
             <header className={"hero"}>
                 <div className="container">
                     <div className="hero__content">
-                        <div>
-                            <h1>Join the DataHub Slack Community!</h1>
-                            <div style={{ fontSize: "18px" }}>We will send the link to join our Slack community to your email.</div>
-                        </div>
-                        <div style={{ width: "50%", margin: "3rem auto" }} ref={scriptContainerRef}>
-                        </div>
+                        <h1>Join the DataHub Slack Community!</h1>
+                        <div style={{ fontSize: "18px" }}>We will send the link to join our Slack community to your email.</div>
+                        <div id="hubspotForm"></div> 
                     </div>
                 </div>
             </header>
