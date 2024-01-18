40412ef87695b92f592621896425c22b9b3c6ff1
David Leifker <david.leifker@acryl.io>
2024-01-18 11:19:16 -0600

    perf(entity-service): batch exists calls

--- datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/term/BatchRemoveTermsResolverTest.java
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/term/BatchRemoveTermsResolverTest.java
@@ -1,6 +1,7 @@
 package com.linkedin.datahub.graphql.resolvers.term;
 
 import static com.linkedin.datahub.graphql.TestUtils.*;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.testng.Assert.*;
 
 import com.google.common.collect.ImmutableList;
@@ -37,22 +38,26 @@ public class BatchRemoveTermsResolverTest {
 
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(null);
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(null);
 
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_1))).thenReturn(true);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_2))).thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_1)), eq(true)))
+        .thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_2)), eq(true)))
+        .thenReturn(true);
 
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_TERM_1_URN))).thenReturn(true);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_TERM_2_URN))).thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_TERM_1_URN)), eq(true)))
+        .thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_TERM_2_URN)), eq(true)))
+        .thenReturn(true);
 
     BatchRemoveTermsResolver resolver = new BatchRemoveTermsResolver(mockService);
 
@@ -65,7 +70,7 @@ public class BatchRemoveTermsResolverTest {
             ImmutableList.of(
                 new ResourceRefInput(TEST_ENTITY_URN_1, null, null),
                 new ResourceRefInput(TEST_ENTITY_URN_2, null, null)));
-    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getArgument(eq("input"))).thenReturn(input);
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
     assertTrue(resolver.get(mockEnv).get());
 
@@ -88,9 +93,9 @@ public class BatchRemoveTermsResolverTest {
 
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(oldTerms1);
 
     final GlossaryTerms oldTerms2 =
@@ -103,16 +108,20 @@ public class BatchRemoveTermsResolverTest {
 
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(oldTerms2);
 
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_1))).thenReturn(true);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_2))).thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_1)), eq(true)))
+        .thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_2)), eq(true)))
+        .thenReturn(true);
 
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_TERM_1_URN))).thenReturn(true);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_TERM_2_URN))).thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_TERM_1_URN)), eq(true)))
+        .thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_TERM_2_URN)), eq(true)))
+        .thenReturn(true);
 
     BatchRemoveTermsResolver resolver = new BatchRemoveTermsResolver(mockService);
 
@@ -125,7 +134,7 @@ public class BatchRemoveTermsResolverTest {
             ImmutableList.of(
                 new ResourceRefInput(TEST_ENTITY_URN_1, null, null),
                 new ResourceRefInput(TEST_ENTITY_URN_2, null, null)));
-    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getArgument(eq("input"))).thenReturn(input);
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
     assertTrue(resolver.get(mockEnv).get());
 
@@ -138,20 +147,23 @@ public class BatchRemoveTermsResolverTest {
 
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_1)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(null);
     Mockito.when(
             mockService.getAspect(
-                Mockito.eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
-                Mockito.eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
-                Mockito.eq(0L)))
+                eq(UrnUtils.getUrn(TEST_ENTITY_URN_2)),
+                eq(Constants.GLOSSARY_TERMS_ASPECT_NAME),
+                eq(0L)))
         .thenReturn(null);
 
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_1))).thenReturn(false);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_ENTITY_URN_2))).thenReturn(true);
-    Mockito.when(mockService.exists(Urn.createFromString(TEST_TERM_1_URN))).thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_1)), eq(true)))
+        .thenReturn(false);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_ENTITY_URN_2)), eq(true)))
+        .thenReturn(true);
+    Mockito.when(mockService.exists(eq(Urn.createFromString(TEST_TERM_1_URN)), eq(true)))
+        .thenReturn(true);
 
     BatchRemoveTermsResolver resolver = new BatchRemoveTermsResolver(mockService);
 
@@ -164,7 +176,7 @@ public class BatchRemoveTermsResolverTest {
             ImmutableList.of(
                 new ResourceRefInput(TEST_ENTITY_URN_1, null, null),
                 new ResourceRefInput(TEST_ENTITY_URN_2, null, null)));
-    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getArgument(eq("input"))).thenReturn(input);
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
 
     assertThrows(CompletionException.class, () -> resolver.get(mockEnv).join());
@@ -185,7 +197,7 @@ public class BatchRemoveTermsResolverTest {
             ImmutableList.of(
                 new ResourceRefInput(TEST_ENTITY_URN_1, null, null),
                 new ResourceRefInput(TEST_ENTITY_URN_2, null, null)));
-    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getArgument(eq("input"))).thenReturn(input);
     QueryContext mockContext = getMockDenyContext();
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
 
@@ -212,7 +224,7 @@ public class BatchRemoveTermsResolverTest {
             ImmutableList.of(
                 new ResourceRefInput(TEST_ENTITY_URN_1, null, null),
                 new ResourceRefInput(TEST_ENTITY_URN_2, null, null)));
-    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getArgument(eq("input"))).thenReturn(input);
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
 
     assertThrows(CompletionException.class, () -> resolver.get(mockEnv).join());
