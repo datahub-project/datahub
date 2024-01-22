d94c1d14016388a0f11c6835954b212c5cf01b60
kunal Kankriya <kunal.kankriya@apptware.com>
2024-01-22 22:52:08 +0530

    Fix - Refactor post creation and verification functions, enhance code readability

--- smoke-test/tests/cypress/cypress/e2e/settings/homePagePost.js
+++ smoke-test/tests/cypress/cypress/e2e/settings/homePagePost.js
@@ -1,89 +1,85 @@
 const homePageRedirection = () => {
-    cy.visit('/')
-    cy.waitTextPresent("Welcome back,")
-}
+    cy.visit('/');
+    cy.waitTextPresent("Welcome back");
+};
 
-const performTextAddAndEditAnnouncement = (text, title, discription, testId, updatedTitle) => {
-    cy.waitTextPresent(text)
-    cy.get('[data-testid="create-post-title"]').clear().type(title)
-    cy.get('[id="description"]').clear().type(discription)
+const addOrEditAnnouncement = (text, title, description, testId) => {
+    cy.waitTextPresent(text);
+    cy.get('[data-testid="create-post-title"]').clear().type(title);
+    cy.get('[id="description"]').clear().type(description);
     cy.get(`[data-testid="${testId}-post-button"]`).click({ force: true });
-    cy.reload()
+    cy.reload();
     homePageRedirection();
-    cy.waitTextPresent(updatedTitle);
-}
+};
 
-const addAnnouncement = () => {
-    cy.get('[id="posts-create-post"]').click({ force: true });
-    performTextAddAndEditAnnouncement("Create new Post", "Test Announcement Title", "Add Description to post announcement", "create", "Test Announcement Title")
-}
-
-const editAnnouncement = () => {
-    cy.get('[aria-label="more"]').first().click()
-    cy.clickOptionWithText("Edit");
-    performTextAddAndEditAnnouncement("Edit Post", "Test Announcement Title Edited", "Decription Edited", "update", "Test Announcement Title Edited")
-}
-
-const performTextAddAndEditLink = (text, title, url, imagesURL, testId, updatedTitle) => {
-    cy.waitTextPresent(text)
-    cy.get('[data-testid="create-post-title"]').clear().type(title)
-    cy.get('[data-testid="create-post-link"]').clear().type(url)
-    cy.get('[data-testid="create-post-media-location"]').clear().type(imagesURL)
+const addOrEditLink = (text, title, url, imagesURL, testId) => {
+    cy.waitTextPresent(text);
+    cy.get('[data-testid="create-post-title"]').clear().type(title);
+    cy.get('[data-testid="create-post-link"]').clear().type(url);
+    cy.get('[data-testid="create-post-media-location"]').clear().type(imagesURL);
     cy.get(`[data-testid="${testId}-post-button"]`).click({ force: true });
-    cy.reload()
+    cy.reload();
     homePageRedirection();
-    cy.waitTextPresent(updatedTitle);
-} 
+};
 
-const addLink = () => {
+const clickOnNewPost = () =>{
     cy.get('[id="posts-create-post"]').click({ force: true });
-    cy.waitTextPresent('Create new Post')
-    cy.contains('label', 'Link').click();
-    performTextAddAndEditLink("Create new Post", "Test Link Title", 'https://www.example.com', 'https://www.example.com/images/example-image.jpg',"create", "Test Link Title")
 }
 
-const editLink = () => {
-    cy.get('[aria-label="more"]').first().click()
-    cy.clickOptionWithText("Edit");
-    performTextAddAndEditLink("Edit Post", "Test Link Edited Title", 'https://www.updatedexample.com', 'https://www.updatedexample.com/images/example-image.jpg',"update", "Test Link Edited Title")
-}  
-
-const deleteFromPostDropdown = (post) => {
-    cy.get('[aria-label="more"]').first().click()
-    cy.clickOptionWithText("Delete");
-    cy.clickOptionWithText("Yes");
-    cy.reload()
-    homePageRedirection();
-    cy.ensureTextNotPresent(post);
+const clickOnMoreOption = () => {
+    cy.get('[aria-label="more"]').first().click();
 }
 
-describe("Create announcement and link posts", () => {
+describe("create announcement and link post", () => {
     beforeEach(() => {
         cy.loginWithCredentials();
         cy.goToHomePagePostSettings();
     });
 
-    it("Create and Verify Announcement Post", () => {
-        addAnnouncement();
-    })
+    it("create announcement post and verify", () => {
+        clickOnNewPost()
+        addOrEditAnnouncement("Create new Post", "Test Announcement Title", "Add Description to post announcement", "create");
+        cy.waitTextPresent("Test Announcement Title");
+    });
 
-    it("Edit and verify Announcement Post", () => {
-        editAnnouncement();
-    })
+    it("edit announced post and verify", () => {
+        clickOnMoreOption()
+        cy.clickOptionWithText("Edit");
+        addOrEditAnnouncement("Edit Post", "Test Announcement Title Edited", "Decription Edited", "update");
+        cy.waitTextPresent("Test Announcement Title Edited");
+    });
 
-    it("Delete and Verify Announcement Post", () => {
-        deleteFromPostDropdown("Test Announcement Title Edited");
-    })
+    it("delete announced post and verify", () => {
+        clickOnMoreOption()
+        cy.clickOptionWithText("Delete");
+        cy.clickOptionWithText("Yes");
+        cy.reload();
+        homePageRedirection();
+        cy.ensureTextNotPresent("Test Announcement Title Edited");
+    });
 
-    it("Create and Verify Link Post", () => {
-        addLink()
-    })
+    it("create link post and verify", () => {
+        clickOnNewPost()
+        cy.waitTextPresent('Create new Post');
+        cy.contains('label', 'Link').click();
+        addOrEditLink("Create new Post", "Test Link Title", 'https://www.example.com', 'https://www.example.com/images/example-image.jpg', "create");
+        cy.waitTextPresent("Test Link Title");
+    });
 
-    it("Edit and Verify Link Post", () => {
-        editLink()
-    })
+    it("edit linked post and verify", () => {
+        clickOnMoreOption()
+        cy.clickOptionWithText("Edit");
+        addOrEditLink("Edit Post", "Test Link Edited Title", 'https://www.updatedexample.com', 'https://www.updatedexample.com/images/example-image.jpg', "update");
+        cy.waitTextPresent("Test Link Edited Title");
+    });
 
-    it("Delete and Verify Link Post", () => {
-        deleteFromPostDropdown("Test Link Edited Title");
-    })
-})
+    it("delete linked post and verify", () => {
+        clickOnMoreOption()
+        cy.clickOptionWithText("Delete");
+        cy.clickOptionWithText("Yes");
+        cy.reload();
+        homePageRedirection();
+        cy.ensureTextNotPresent("Test Link Edited Title");
+    });
+});
+
