52da4832fe268dbc7f7b35739d64f4b1f3052c83
Aseem Bansal <asmbansal2@gmail.com>
2024-01-25 17:36:42 +0530

    fix CI

--- smoke-test/run-quickstart.sh
+++ smoke-test/run-quickstart.sh
@@ -5,6 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "$DIR"
 
 ../gradlew :smoke-test:installDev
+source venv/bin/activate
 
 mkdir -p ~/.datahub/plugins/frontend/auth/
 echo "test_user:test_pass" >> ~/.datahub/plugins/frontend/auth/user.props
