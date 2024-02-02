8f5ea637429a1fa8874a4cbd6ad1fd58634e4893
Bharti, Aakash <aabharti@visa.com>
2024-02-03 02:31:50 +0530

    business-attribute: businessattributes change propagation platform event hook

---
+++ metadata-jobs/pe-consumer/src/main/java/com/datahub/event/hook/BusinessAttributeUpdateHook.java
@@ -0,0 +1,34 @@
+package com.datahub.event.hook;
+
+import com.linkedin.gms.factory.common.GraphServiceFactory;
+import com.linkedin.gms.factory.entity.EntityServiceFactory;
+import com.linkedin.gms.factory.entityregistry.EntityRegistryFactory;
+import com.linkedin.metadata.service.BusinessAttributeUpdateService;
+import com.linkedin.mxe.PlatformEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Nonnull;
+
+@Slf4j
+@Component
+@Import({EntityServiceFactory.class, EntityRegistryFactory.class, GraphServiceFactory.class})
+public class BusinessAttributeUpdateHook implements PlatformEventHook {
+
+    protected final BusinessAttributeUpdateService _businessAttributeUpdateService;
+
+    public BusinessAttributeUpdateHook(BusinessAttributeUpdateService businessAttributeUpdateService) {
+        this._businessAttributeUpdateService = businessAttributeUpdateService;
+    }
+
+    /**
+     * Invoke the hook when a PlatformEvent is received
+     *
+     * @param event
+     */
+    @Override
+    public void invoke(@Nonnull PlatformEvent event) {
+        _businessAttributeUpdateService.handleChangeEvent(event);
+    }
+}
