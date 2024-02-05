59016d8caba829317cb29cb0fb9fdc4b88811962
Ethan Cartwright <ethan.cartwright@acryl.io>
2024-02-05 14:24:12 -0500

    add pytest setup fixture

--- metadata-ingestion/tests/unit/test_redshift_source.py
+++ metadata-ingestion/tests/unit/test_redshift_source.py
@@ -1,3 +1,4 @@
+import pytest
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.emitter.mcp import MetadataChangeProposalClass
 from datahub.ingestion.api.common import PipelineContext
@@ -5,10 +6,17 @@ from datahub.ingestion.source.redshift.config import RedshiftConfig
 from datahub.ingestion.source.redshift.redshift import RedshiftSource
 from datahub.ingestion.source.redshift.redshift_schema import RedshiftTable
 
+from typing import Iterable
+from datahub.ingestion.api.workunit import MetadataWorkUnit
 
-def test_gen_dataset_workunits_patch_custom_properties_patch():
+
+@pytest.fixture
+def redshift_source_setup(request) -> Iterable[MetadataWorkUnit]:
+    custom_props_flag = request.param
     config = RedshiftConfig(
-        host_port="localhost:5439", database="test", patch_custom_properties=True
+        host_port="localhost:5439",
+        database="test",
+        patch_custom_properties=custom_props_flag,
     )
     source: RedshiftSource = RedshiftSource(config, ctx=PipelineContext(run_id="test"))
     gen = source.gen_dataset_workunits(
@@ -23,7 +31,12 @@ def test_gen_dataset_workunits_patch_custom_properties_patch():
         sub_type="test_sub_type",
         custom_properties={"my_key": "my_value"},
     )
+    return gen
 
+
+@pytest.mark.parametrize("redshift_source_setup", [True], indirect=True)
+def test_gen_dataset_workunits_patch_custom_properties_patch(redshift_source_setup):
+    gen = redshift_source_setup
     custom_props_exist = False
     for item in gen:
         mcp = item.metadata
@@ -37,22 +50,9 @@ def test_gen_dataset_workunits_patch_custom_properties_patch():
     assert custom_props_exist
 
 
-def test_gen_dataset_workunits_patch_custom_properties_upsert():
-    config = RedshiftConfig(host_port="localhost:5439", database="test")
-    source: RedshiftSource = RedshiftSource(config, ctx=PipelineContext(run_id="test"))
-    gen = source.gen_dataset_workunits(
-        table=RedshiftTable(
-            name="category",
-            columns=[],
-            created=None,
-            comment="",
-        ),
-        database="dev",
-        schema="public",
-        sub_type="test_sub_type",
-        custom_properties={"my_key": "my_value"},
-    )
-
+@pytest.mark.parametrize("redshift_source_setup", [False], indirect=True)
+def test_gen_dataset_workunits_patch_custom_properties_upsert(redshift_source_setup):
+    gen = redshift_source_setup
     custom_props_exist = False
     for item in gen:
         assert isinstance(item.metadata, MetadataChangeProposalWrapper)
