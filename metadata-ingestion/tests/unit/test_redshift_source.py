a43cbefd73635a73defae5b9171c83d2d3898bd8
Ethan Cartwright <ethan.cartwright@acryl.io>
2024-02-05 11:06:26 -0500

    update unit tests for patch flag

--- metadata-ingestion/tests/unit/test_redshift_source.py
+++ metadata-ingestion/tests/unit/test_redshift_source.py
@@ -1,11 +1,43 @@
+from datahub.emitter.mcp import MetadataChangeProposalWrapper
+from datahub.emitter.mcp import MetadataChangeProposalClass
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.source.redshift.config import RedshiftConfig
 from datahub.ingestion.source.redshift.redshift import RedshiftSource
 from datahub.ingestion.source.redshift.redshift_schema import RedshiftTable
-from datahub.metadata.schema_classes import MetadataChangeProposalClass
 
 
-def test_gen_dataset_workunits_patch_custom_properties():
+def test_gen_dataset_workunits_patch_custom_properties_patch():
+    config = RedshiftConfig(
+        host_port="localhost:5439", database="test", patch_custom_properties=True
+    )
+    source: RedshiftSource = RedshiftSource(config, ctx=PipelineContext(run_id="test"))
+    gen = source.gen_dataset_workunits(
+        table=RedshiftTable(
+            name="category",
+            columns=[],
+            created=None,
+            comment="",
+        ),
+        database="dev",
+        schema="public",
+        sub_type="test_sub_type",
+        custom_properties={"my_key": "my_value"},
+    )
+
+    custom_props_exist = False
+    for item in gen:
+        mcp = item.metadata
+        if mcp.aspectName == "datasetProperties":
+            assert isinstance(item.metadata, MetadataChangeProposalClass)
+            assert mcp.changeType == "PATCH"
+            custom_props_exist = True
+        else:
+            assert isinstance(item.metadata, MetadataChangeProposalWrapper)
+
+    assert custom_props_exist
+
+
+def test_gen_dataset_workunits_patch_custom_properties_upsert():
     config = RedshiftConfig(host_port="localhost:5439", database="test")
     source: RedshiftSource = RedshiftSource(config, ctx=PipelineContext(run_id="test"))
     gen = source.gen_dataset_workunits(
@@ -23,10 +55,10 @@ def test_gen_dataset_workunits_patch_custom_properties():
 
     custom_props_exist = False
     for item in gen:
-        assert isinstance(item.metadata, MetadataChangeProposalClass)
+        assert isinstance(item.metadata, MetadataChangeProposalWrapper)
         mcp = item.metadata
         if mcp.aspectName == "datasetProperties":
-            assert mcp.changeType == "PATCH"
+            assert mcp.changeType == "UPSERT"
             custom_props_exist = True
 
     assert custom_props_exist
