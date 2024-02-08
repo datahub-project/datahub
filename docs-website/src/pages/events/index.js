59111fb0d4e6daec73286d1cd2fbd76a41cbbabb
socar-dini <0327jane@gmail.com>
2024-02-08 16:06:13 +0900

    fix: fix css

--- docs-website/src/pages/events/index.js
+++ docs-website/src/pages/events/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from "./events.module.scss";
 
 function Events() {
     const { siteConfig = {} } = useDocusaurusContext();
@@ -15,12 +14,16 @@ function Events() {
                     <div className="hero__content">
                         <h1>DataHub Community Event Calendar</h1>
                         <div style={{ fontSize: "18px" }}>Subscribe to join our monthly events to network and learn more about our community!</div>
-                        <div className="lumaCalendar">
+                        <div className="lumaCalendar" style={{ maxWidth: "60rem", margin: "3rem auto" }}>
                             <iframe
                                 src="https://lu.ma/embed/calendar/cal-lom9HnTVnZkKsNh/events"
                                 allowFullScreen={true}
                                 aria-hidden="false"
                                 tabIndex="0"
+                                border="1px solid #bfcbda88"
+                                borderRadius="10px"
+                                width="100%"
+                                height="600px" 
                             ></iframe>
                         </div> 
                     </div>
