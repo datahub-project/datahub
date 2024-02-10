3427be8a3c7ffd0e341aae0e1114f75f4eb5141b
John Joyce <john@ip-192-168-1-149.us-west-2.compute.internal>
2024-02-09 17:02:28 -0800

    Attempting to fix search cypress

--- smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
@@ -1,17 +1,29 @@
-const urn = "urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressHdfsDataset,PROD)";
-const datasetName = "SampleCypressHdfsDataset";
+const urn1 = "urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressHdfsDataset,PROD)";
+const datasetName1 = "SampleCypressHdfsDataset";
+
+const urn2 = "urn:li:dataset:(urn:li:dataPlatform:hive,cypress_health_test,PROD)";
+const datasetName2 = "cypress_health_test";
 
 describe("dataset health test", () => {
     it("go to dataset with failing assertions and verify health of dataset", () => {
         cy.login();
-        cy.goToDataset(urn, datasetName);
+        cy.goToDataset(urn1, datasetName1);
         // Ensure that the “Health” badge is present and there is an active incident warning
-        cy.get(`[href="/dataset/${urn}/Validation"]`).should("be.visible");
-        cy.get(`[href="/dataset/${urn}/Validation"] span`).trigger("mouseover", { force: true });
+        cy.get(`[href="/dataset/${urn1}/Validation"]`).should("be.visible");
+        cy.get(`[href="/dataset/${urn1}/Validation"] span`).trigger("mouseover", { force: true });
         cy.waitTextVisible("This asset may be unhealthy");
         cy.waitTextVisible("Assertions 1 of 1 assertions are failing");
         cy.get('[data-testid="assertions-details"]').click();
         // cy.clickOptionWithText("details");
         cy.waitTextVisible("1 failed assertions");
+    });
+    it("go to dataset with active incidents and verify health of dataset", () => {
+        cy.login();
+        cy.goToDataset(urn2, datasetName2);
+        // Ensure that the “Health” badge is present and there is an active incident warning
+        cy.get(`[href="/dataset/${urn2}/Validation"]`).should("be.visible");
+        cy.get(`[href="/dataset/${urn2}/Validation"] span`).trigger("mouseover", { force: true });
+        cy.waitTextVisible("This asset may be unhealthy");
+        cy.waitTextVisible("1 active incident");
     });
 });
