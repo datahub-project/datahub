6bccec87e45bf6a0814a02fa95ee8f4b3999dff4
Alexander Sukhoborov <qua_asukhoborov@exactsciences.com>
2024-02-13 22:30:53 +0400

    Fixes

--- metadata-ingestion/src/datahub/utilities/dialects/patched/tsql.py
+++ metadata-ingestion/src/datahub/utilities/dialects/patched/tsql.py
@@ -2,4 +2,4 @@ from sqlglot.dialects.tsql import TSQL
 
 class TSQLv2(TSQL):
     class Tokenizer(TSQL.Tokenizer):
-        IDENTIFIERS = ["'", '"', ("[", "]")]
+        IDENTIFIERS = ["'", *TSQL.Tokenizer.IDENTIFIERS]
