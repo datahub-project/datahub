e7cf36643ebb9e41b0adf9006a6b3e68ffb1d07e
John Joyce <john@ip-192-168-1-149.us-west-2.compute.internal>
2024-02-12 22:09:56 -0800

    Adding a fix for the smoke test

--- smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
@@ -1,29 +1,15 @@
-const urn1 = "urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressHdfsDataset,PROD)";
-const datasetName1 = "SampleCypressHdfsDataset";
-
-const urn2 = "urn:li:dataset:(urn:li:dataPlatform:hive,cypress_health_test,PROD)";
-const datasetName2 = "cypress_health_test";
+const urn = "urn:li:dataset:(urn:li:dataPlatform:hive,cypress_health_test,PROD)";
+const datasetName = "cypress_health_test";
 
 describe("dataset health test", () => {
-    it("go to dataset with failing assertions and verify health of dataset", () => {
+    it("go to dataset with failing assertions and active incidents and verify health of dataset", () => {
         cy.login();
-        cy.goToDataset(urn1, datasetName1);
+        cy.goToDataset(urn, datasetName);
         // Ensure that the “Health” badge is present and there is an active incident warning
-        cy.get(`[href="/dataset/${urn1}/Validation"]`).should("be.visible");
-        cy.get(`[href="/dataset/${urn1}/Validation"] span`).trigger("mouseover", { force: true });
+        cy.get(`[href="/dataset/${urn}/Validation"]`).should("be.visible");
+        cy.get(`[href="/dataset/${urn}/Validation"] span`).trigger("mouseover", { force: true });
         cy.waitTextVisible("This asset may be unhealthy");
         cy.waitTextVisible("Assertions 1 of 1 assertions are failing");
-        cy.get('[data-testid="assertions-details"]').click();
-        // cy.clickOptionWithText("details");
-        cy.waitTextVisible("1 failed assertions");
-    });
-    it("go to dataset with active incidents and verify health of dataset", () => {
-        cy.login();
-        cy.goToDataset(urn2, datasetName2);
-        // Ensure that the “Health” badge is present and there is an active incident warning
-        cy.get(`[href="/dataset/${urn2}/Validation"]`).should("be.visible");
-        cy.get(`[href="/dataset/${urn2}/Validation"] span`).trigger("mouseover", { force: true });
-        cy.waitTextVisible("This asset may be unhealthy");
         cy.waitTextVisible("1 active incident");
     });
 });
