045c76a0e4014ce159acdb0ddfb0f2e1220c7c40
Harshal Sheth <hsheth2@gmail.com>
2024-02-14 11:06:03 -0800

    feat(ingest): reporting logging tweaks (#9835)

--- metadata-ingestion/src/datahub/cli/check_cli.py
+++ metadata-ingestion/src/datahub/cli/check_cli.py
@@ -218,5 +218,7 @@ def sql_lineage(
     )
 
     logger.debug("Sql parsing debug info: %s", lineage.debug_info)
+    if lineage.debug_info.error:
+        logger.debug("Sql parsing error details", exc_info=lineage.debug_info.error)
 
     click.echo(lineage.json(indent=4))
