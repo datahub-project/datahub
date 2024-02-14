6e8910dbd4919868fd611387802dc485da6ec904
John Joyce <john@Johns-MBP.attlocal.net>
2024-02-13 17:46:53 -0800

    Checkstyle

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/entity/EntityPrivilegesResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/entity/EntityPrivilegesResolver.java
@@ -132,6 +132,12 @@ public class EntityPrivilegesResolver implements DataFetcher<CompletableFuture<E
     final EntityPrivileges result = new EntityPrivileges();
     result.setCanEditEmbed(EmbedUtils.isAuthorizedToUpdateEmbedForEntity(urn, context));
     result.setCanEditQueries(AuthorizationUtils.canCreateQuery(ImmutableList.of(urn), context));
+    result.setCanEditSchemaFieldTags(LabelUtils.isAuthorizedToUpdateTags(context, urn, "ignored"));
+    result.setCanEditSchemaFieldGlossaryTerms(
+        LabelUtils.isAuthorizedToUpdateTerms(context, urn, "ignored"));
+    result.setCanEditSchemaFieldDescription(
+        DescriptionUtils.isAuthorizedToUpdateFieldDescription(context, urn));
+    addCommonPrivileges(result, urn, context);
     return result;
   }
 
