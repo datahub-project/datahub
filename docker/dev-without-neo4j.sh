e04d0a27b254f8ecce9300c143f6c76df4f0a38f
John Joyce <john@acryl.io>
2024-02-14 15:33:29 -0800

    fix(dev): revert dev-without-neo4j.sh file for deploying locally (#2425)
    Co-authored-by: John Joyce <john@Johns-MBP.attlocal.net>

--- docker/dev-without-neo4j.sh
+++ docker/dev-without-neo4j.sh
@@ -26,16 +26,18 @@ set -x
 # Launches dev instances of DataHub images. See documentation for more details.
 # YOU MUST BUILD VIA GRADLE BEFORE RUNNING THIS.
 cd "${DIR}/../.." && \
-  touch "${DIR}/../datahub-integrations-service/.env" && \
-  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 DOCKER_DEFAULT_PLATFORM="$(uname -m)" docker compose \
+     touch "${DIR}/../datahub-integrations-service/.env" && \
+  { [[ -n "$SKIP_PULL" ]] || \
+  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 DOCKER_DEFAULT_PLATFORM="linux/$(uname -m)" docker compose \
     -f "${DIR}/docker-compose-without-neo4j.yml" \
     -f "${DIR}/docker-compose.acryl.yml" \
     -f "${DIR}/docker-compose-without-neo4j.override.yml" \
     -f "${DIR}/docker-compose.dev.yml" \
     -f "${DIR}/docker-compose.acryl.dev.yml" \
     $CONSUMERS_COMPOSE $MONITORING_COMPOSE $M1_COMPOSE pull \
+  ; } \
 && \
-  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 DOCKER_DEFAULT_PLATFORM="$(uname -m)" docker compose -p datahub \
+  COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 DOCKER_DEFAULT_PLATFORM="linux/$(uname -m)" docker compose -p datahub \
     -f "${DIR}/docker-compose-without-neo4j.yml" \
     -f "${DIR}/docker-compose.acryl.yml" \
     -f "${DIR}/docker-compose-without-neo4j.override.yml" \
