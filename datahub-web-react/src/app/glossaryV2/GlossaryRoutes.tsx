62eeb6ff2efdd9eba143fcd40f6a54d982c97155
kushagra-apptware <kushagra.porwal@apptware.com>
2024-02-14 12:57:05 +0530

    Remove glossary path header

--- datahub-web-react/src/app/glossaryV2/GlossaryRoutes.tsx
+++ datahub-web-react/src/app/glossaryV2/GlossaryRoutes.tsx
@@ -44,7 +44,6 @@ export default function GlossaryRoutes() {
                 setIsSidebarOpen,
             }}
         >
-            {!isAtRootGlossary && <GlossaryEntitiesPath />}
             <ContentWrapper>
                 <GlossarySidebar />
                 <Switch>
