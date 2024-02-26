e7b7b05a9f0975bebf776834ccbd872f45f552e2
Pedro Silva <pedro@acryl.io>
2024-02-26 10:30:07 +0000

    apply review comments

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/policy/ListPoliciesResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/policy/ListPoliciesResolver.java
@@ -5,6 +5,7 @@ import static com.linkedin.datahub.graphql.resolvers.ResolverUtils.bindArgument;
 import com.datahub.authorization.PolicyFetcher;
 import com.linkedin.datahub.graphql.QueryContext;
 import com.linkedin.datahub.graphql.exception.AuthorizationException;
+import com.linkedin.datahub.graphql.generated.AndFilterInput;
 import com.linkedin.datahub.graphql.generated.FacetFilterInput;
 import com.linkedin.datahub.graphql.generated.ListPoliciesInput;
 import com.linkedin.datahub.graphql.generated.ListPoliciesResult;
@@ -15,6 +16,7 @@ import com.linkedin.entity.client.EntityClient;
 import com.linkedin.metadata.query.filter.Filter;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -46,13 +48,17 @@ public class ListPoliciesResolver implements DataFetcher<CompletableFuture<ListP
       final Integer start = input.getStart() == null ? DEFAULT_START : input.getStart();
       final Integer count = input.getCount() == null ? DEFAULT_COUNT : input.getCount();
       final String query = input.getQuery() == null ? DEFAULT_QUERY : input.getQuery();
-      final List<FacetFilterInput> filters =
-          input.getFilters() == null ? Collections.emptyList() : input.getFilters();
-
-      log.info(
+      final List<AndFilterInput> filters =
+          input.getOrFilters() != null ? input.getOrFilters() : new ArrayList<>();
+      final List<FacetFilterInput> facetFilters =
+          filters.stream()
+              .map(AndFilterInput::getAnd)
+              .flatMap(List::stream)
+              .collect(Collectors.toList());
+      log.debug(
           "User {} listing policies with filters {}", context.getActorUrn(), filters.toString());
 
-      final Filter filter = ResolverUtils.buildFilter(filters, Collections.emptyList());
+      final Filter filter = ResolverUtils.buildFilter(facetFilters, Collections.emptyList());
 
       return _policyFetcher
           .fetchPolicies(start, query, count, filter, context.getAuthentication())
