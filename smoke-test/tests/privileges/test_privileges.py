0cd8b3c471657b7d8fd1c844e670f3ff8bf76ad7
kunal kankriya <kunal.kankriya@apptware.com>
2024-02-26 18:02:40 +0530

    Removed unnecessary lines

--- smoke-test/tests/privileges/test_privileges.py
+++ smoke-test/tests/privileges/test_privileges.py
@@ -499,7 +499,6 @@ def test_privilege_to_create_and_manage_policies():
     _ensure_cant_perform_action(user_session, create_policy, "createPolicy")
 
 
-#@pytest.mark.skip(reason="Functionality and test needs to be validated for correctness")
 @pytest.mark.dependency(depends=["test_healthchecks"])
 def test_privilege_from_group_role_can_create_and_manage_secret():
     (admin_user, admin_pass) = get_admin_credentials()
