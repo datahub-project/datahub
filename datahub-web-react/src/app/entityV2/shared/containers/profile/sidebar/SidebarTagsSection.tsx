da5439b7a8d958320e25197b7ef707575956a3ce
sumitappt <sumit.patil@apptware.com>
2024-02-26 11:30:43 +0530

    fix: OBS-534 | Glossary terms and tags are not refreshed when changed on the schema field sidebar

--- datahub-web-react/src/app/entityV2/shared/containers/profile/sidebar/SidebarTagsSection.tsx
+++ datahub-web-react/src/app/entityV2/shared/containers/profile/sidebar/SidebarTagsSection.tsx
@@ -80,6 +80,7 @@ export const SidebarTagsSection = ({ readOnly }: Props) => {
                 showAddModal={showAddModal}
                 setShowAddModal={setShowAddModal}
                 addModalType={addModalType}
+                refetch={refetch}
             />
         </div>
     );
