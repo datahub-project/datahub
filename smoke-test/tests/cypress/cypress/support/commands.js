a287f939de2ce4d074e8a1b48be5bd46e4b9df33
akarsh991 <akarsh.goel@apptware.com>
2024-02-27 19:17:13 +0530

    cypress test case changes

--- smoke-test/tests/cypress/cypress/support/commands.js
+++ smoke-test/tests/cypress/cypress/support/commands.js
@@ -40,8 +40,8 @@ Cypress.Commands.add("loginWithCredentials", (username, password) => {
     cy.get('input[data-testid=username]').type(username);
     cy.get('input[data-testid=password]').type(password);
   } else {
-    cy.get('input[data-testid=username]').type(Cypress.env('ADMIN_USERNAME'));
-    cy.get('input[data-testid=password]').type(Cypress.env('ADMIN_PASSWORD'));
+    cy.get('input[data-testid=username]').type('datahub');
+    cy.get('input[data-testid=password]').type('datahub');
   }
   cy.contains('Sign In').click();
   cy.contains('Welcome back');
