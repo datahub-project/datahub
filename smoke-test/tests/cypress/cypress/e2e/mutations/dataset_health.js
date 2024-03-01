e6eb3dcddfa86a390a0f11e37f05ab858cd60b95
David Leifker <david.leifker@acryl.io>
2024-03-01 08:48:53 -0600

    fix cypress dataset health test

--- smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
@@ -5,11 +5,13 @@ describe("dataset health test", () => {
     it("go to dataset with failing assertions and active incidents and verify health of dataset", () => {
         cy.login();
         cy.goToDataset(urn, datasetName);
-        // Ensure that the “Health” badge is present and there is an active incident warning
+        //Ensure that the “Health” badge is present and there is an active incident warning
         cy.get(`[href="/dataset/${urn}/Validation"]`).should("be.visible");
         cy.get(`[href="/dataset/${urn}/Validation"] span`).trigger("mouseover", { force: true });
         cy.waitTextVisible("This asset may be unhealthy");
         cy.waitTextVisible("Assertions 1 of 1 assertions are failing");
-        cy.waitTextVisible("1 active incident");
+        cy.get('[data-testid="assertions-details"]').click();
+        // cy.clickOptionWithText("details");
+        cy.waitTextVisible("This asset may be unhealthy");
     });
 });
