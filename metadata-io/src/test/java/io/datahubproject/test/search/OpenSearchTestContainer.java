c0aedd422f461d71df3b676c6eb9714160aa1832
Davi Arnaut <davi.arnaut@acryl.io>
2024-03-05 11:04:51 -0800

    chore(search): bump OpenSearch image version to 2.11.0 (#9984)

--- metadata-io/src/test/java/io/datahubproject/test/search/OpenSearchTestContainer.java
+++ metadata-io/src/test/java/io/datahubproject/test/search/OpenSearchTestContainer.java
@@ -7,7 +7,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 public class OpenSearchTestContainer implements SearchTestContainer {
-  private static final String OPENSEARCH_VERSION = "2.9.0";
+  private static final String OPENSEARCH_VERSION = "2.11.0";
   private static final String OPENSEARCH_IMAGE_NAME = "opensearchproject/opensearch";
   private static final String ENV_OPENSEARCH_IMAGE_FULL_NAME =
       System.getenv("OPENSEARCH_IMAGE_FULL_NAME");
