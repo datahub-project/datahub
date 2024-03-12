2f70c42b8fe362089394bab69d4cfbbb47fce638
kushagra-apptware <81357546+kushagra-apptware@users.noreply.github.com>
2024-03-12 09:14:38 +0530

    fix: add dynamic logic for sidebar width calculation (#2576)
    Co-authored-by: Andrew Sikowitz <andrew.sikowitz@acryl.io>

--- datahub-web-react/src/app/domainV2/nestedDomains/ManageDomainsSidebar.tsx
+++ datahub-web-react/src/app/domainV2/nestedDomains/ManageDomainsSidebar.tsx
@@ -1,11 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import DomainsSidebarHeader from './DomainsSidebarHeader';
 import { SidebarWrapper } from '../../sharedV2/sidebar/components';
 import DomainNavigator from './domainNavigator/DomainNavigator';
 import DomainSearch from '../DomainSearch';
 
 export default function ManageDomainsSidebarV2() {
-    const [browserWidth] = useState(window.innerWidth * 0.2);
+    const [browserWidth, setBrowserWidth] = useState(window.innerWidth * 0.2);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setBrowserWidth(window.innerWidth * 0.2);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
 
     return (
         <>
