e3155cb8d91267b9a5cd38a77709055c684d2eb3
David Leifker <david.leifker@acryl.io>
2024-03-12 12:03:52 -0500

    add systemcreated

--- metadata-io/src/main/java/com/linkedin/metadata/search/transformer/SearchDocumentTransformer.java
+++ metadata-io/src/main/java/com/linkedin/metadata/search/transformer/SearchDocumentTransformer.java
@@ -3,11 +3,13 @@ package com.linkedin.metadata.search.transformer;
 import static com.linkedin.metadata.Constants.*;
 import static com.linkedin.metadata.models.StructuredPropertyUtils.sanitizeStructuredPropertyFQN;
 import static com.linkedin.metadata.models.annotation.SearchableAnnotation.OBJECT_FIELD_TYPES;
+import static com.linkedin.metadata.search.elasticsearch.indexbuilder.MappingsBuilder.SYSTEM_CREATED_FIELD;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.linkedin.common.AuditStamp;
 import com.linkedin.common.urn.Urn;
 import com.linkedin.data.schema.DataSchema;
 import com.linkedin.data.template.RecordTemplate;
@@ -45,7 +47,6 @@ import lombok.extern.slf4j.Slf4j;
 @Setter
 @RequiredArgsConstructor
 public class SearchDocumentTransformer {
-
   // Number of elements to index for a given array.
   // The cap improves search speed when having fields with a large number of elements
   private final int maxArrayLength;
@@ -87,10 +88,22 @@ public class SearchDocumentTransformer {
     return Optional.of(searchDocument.toString());
   }
 
-  public Optional<String> transformAspect(
-      final Urn urn,
-      final RecordTemplate aspect,
-      final AspectSpec aspectSpec,
+  public static ObjectNode withSystemCreated(
+      ObjectNode searchDocument,
+      @Nonnull EntitySpec entitySpec,
+      @Nonnull AspectSpec aspectSpec,
+      @Nonnull final AuditStamp auditStamp) {
+
+    if (entitySpec.getKeyAspectName().equals(aspectSpec.getName())) {
+      searchDocument.put(SYSTEM_CREATED_FIELD, auditStamp.getTime());
+    }
+    return searchDocument;
+  }
+
+  public Optional<ObjectNode> transformAspect(
+      final @Nonnull Urn urn,
+      final @Nonnull RecordTemplate aspect,
+      final @Nonnull AspectSpec aspectSpec,
       final Boolean forDelete)
       throws RemoteInvocationException, URISyntaxException {
     final Map<SearchableFieldSpec, List<Object>> extractedSearchableFields =
@@ -98,22 +111,23 @@ public class SearchDocumentTransformer {
     final Map<SearchScoreFieldSpec, List<Object>> extractedSearchScoreFields =
         FieldExtractor.extractFields(aspect, aspectSpec.getSearchScoreFieldSpecs(), maxValueLength);
 
-    Optional<String> result = Optional.empty();
+    Optional<ObjectNode> result = Optional.empty();
 
     if (!extractedSearchableFields.isEmpty() || !extractedSearchScoreFields.isEmpty()) {
       final ObjectNode searchDocument = JsonNodeFactory.instance.objectNode();
       searchDocument.put("urn", urn.toString());
+
       extractedSearchableFields.forEach(
           (key, values) -> setSearchableValue(key, values, searchDocument, forDelete));
       extractedSearchScoreFields.forEach(
           (key, values) -> setSearchScoreValue(key, values, searchDocument, forDelete));
-      result = Optional.of(searchDocument.toString());
+      result = Optional.of(searchDocument);
     } else if (STRUCTURED_PROPERTIES_ASPECT_NAME.equals(aspectSpec.getName())) {
       final ObjectNode searchDocument = JsonNodeFactory.instance.objectNode();
       searchDocument.put("urn", urn.toString());
       setStructuredPropertiesSearchValue(
           new StructuredProperties(aspect.data()), searchDocument, forDelete);
-      result = Optional.of(searchDocument.toString());
+      result = Optional.of(searchDocument);
     }
 
     return result;
