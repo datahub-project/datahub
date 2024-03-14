27769bb04d2b99a3f3a3d58b3ccb37b3e12277b6
Hyejin Yoon <0327jane@gmail.com>
2024-03-14 16:24:05 +0900

    fix: fix rendering error

--- docs-website/src/pages/docs/index.js
+++ docs-website/src/pages/docs/index.js
@@ -1,1 +1,8 @@
-window.location.href = '/docs/next/';
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
+
+const Home = () => {
+  return <Redirect to="/docs/next" />;
+};
+
+export default Home;
