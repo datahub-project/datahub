ce2c640745c73e0b8c27123d04b8340503bbd33b
Dushyant Bhalgami <dushyant.bhalgami@apptware.com>
2024-03-15 12:23:37 +0530

    fix(ingestion/unity): fixed the review comments

--- metadata-ingestion/tests/integration/unity/test_unity_catalog_ingest.py
+++ metadata-ingestion/tests/integration/unity/test_unity_catalog_ingest.py
@@ -67,17 +67,6 @@ def register_mock_data(workspace_client):
         CatalogInfo.from_dict(d)
         for d in [
             {
-                "name": "main",
-                "owner": "account users",
-                "comment": "Main catalog (auto-created)",
-                "metastore_id": "2c983545-d403-4f87-9063-5b7e3b6d3736",
-                "created_at": 1666185153376,
-                "created_by": "abc@acryl.io",
-                "updated_at": 1666186071115,
-                "updated_by": "abc@acryl.io",
-                "catalog_type": "MANAGED_CATALOG",
-            },
-            {
                 "name": "quickstart_catalog",
                 "owner": "account users",
                 "comment": "",
@@ -87,50 +76,13 @@ def register_mock_data(workspace_client):
                 "updated_at": 1666186064332,
                 "updated_by": "abc@acryl.io",
                 "catalog_type": "MANAGED_CATALOG",
-            },
-            {
-                "name": "system",
-                "owner": SERVICE_PRINCIPAL_ID_2,
-                "comment": "System catalog (auto-created)",
-                "metastore_id": "2c983545-d403-4f87-9063-5b7e3b6d3736",
-                "created_at": 1666185153391,
-                "created_by": "System user",
-                "updated_at": 1666185153391,
-                "updated_by": "System user",
-                "catalog_type": "SYSTEM_CATALOG",
-            },
+            }
         ]
     ]
 
     workspace_client.schemas.list.return_value = [
         SchemaInfo.from_dict(d)
         for d in [
-            {
-                "name": "default",
-                "catalog_name": "quickstart_catalog",
-                "owner": "abc@acryl.io",
-                "comment": "Default schema (auto-created)",
-                "metastore_id": "2c983545-d403-4f87-9063-5b7e3b6d3736",
-                "full_name": "quickstart_catalog.default",
-                "created_at": 1666185610021,
-                "created_by": "abc@acryl.io",
-                "updated_at": 1666185610021,
-                "updated_by": "abc@acryl.io",
-                "catalog_type": "MANAGED_CATALOG",
-            },
-            {
-                "name": "information_schema",
-                "catalog_name": "quickstart_catalog",
-                "owner": SERVICE_PRINCIPAL_ID_1,
-                "comment": "Information schema (auto-created)",
-                "metastore_id": "2c983545-d403-4f87-9063-5b7e3b6d3736",
-                "full_name": "quickstart_catalog.information_schema",
-                "created_at": 1666185610024,
-                "created_by": "System user",
-                "updated_at": 1666185610024,
-                "updated_by": "System user",
-                "catalog_type": "MANAGED_CATALOG",
-            },
             {
                 "name": "quickstart_schema",
                 "catalog_name": "quickstart_catalog",
@@ -241,7 +193,7 @@ def register_mock_data(workspace_client):
                 },
                 "generation": 2,
                 "metastore_id": "2c983545-d403-4f87-9063-5b7e3b6d3736",
-                "full_name": "quickstart_catalog.quickstart_schema.quickstart_table",
+                "full_name": "quickstart_catalog.quickstart_schema.quickstart_table_external",
                 "data_access_configuration_id": "00000000-0000-0000-0000-000000000000",
                 "created_at": 1666185698688,
                 "created_by": "abc@acryl.io",
