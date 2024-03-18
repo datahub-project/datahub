01378908651ecae7345094fa9339a9f071f3841b
Aseem Bansal <asmbansal2@gmail.com>
2024-03-18 18:50:14 +0530

    docs(acryl-cloud): notes for 0.2.16

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -173,6 +173,7 @@ module.exports = {
         },
         {
           "Managed DataHub Release History": [
+            "docs/managed-datahub/release-notes/v_0_2_16",
             "docs/managed-datahub/release-notes/v_0_2_15",
             "docs/managed-datahub/release-notes/v_0_2_14",
             "docs/managed-datahub/release-notes/v_0_2_13",
