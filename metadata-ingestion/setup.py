bcf9811a69d2e07d6c86a504a5ceb020f0ff9787
Aseem Bansal <asmbansal2@gmail.com>
2024-03-20 18:00:49 +0530

    feat(ingest/datahub-gc): gc source to cleanup things

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -609,6 +609,7 @@ entry_points = {
         "ldap = datahub.ingestion.source.ldap:LDAPSource",
         "looker = datahub.ingestion.source.looker.looker_source:LookerDashboardSource",
         "lookml = datahub.ingestion.source.looker.lookml_source:LookMLSource",
+        "datahub-gc = datahub.ingestion.source.gc.datahub_gc:DataHubGcSource",
         "datahub-lineage-file = datahub.ingestion.source.metadata.lineage:LineageFileSource",
         "datahub-business-glossary = datahub.ingestion.source.metadata.business_glossary:BusinessGlossaryFileSource",
         "mlflow = datahub.ingestion.source.mlflow:MLflowSource",
