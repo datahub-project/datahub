fae3f9da360c5c3f6c99b9c495d7287757879a45
Ryan Holstien <ryan@acryl.io>
2024-03-20 11:34:12 -0500

    add json serializing

--- metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v2/models/BatchGetUrnResponse.java
+++ metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v2/models/BatchGetUrnResponse.java
@@ -1,6 +1,9 @@
 package io.datahubproject.openapi.v2.models;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
 import java.util.List;
 import lombok.Builder;
@@ -12,6 +15,10 @@ import lombok.Value;
 @Value
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonDeserialize(builder = BatchGetUrnResponse.BatchGetUrnResponseBuilder.class)
 public class BatchGetUrnResponse implements Serializable {
+  @JsonProperty("entities")
+  @Schema(
+      description = "List of entity responses")
   List<GenericEntity> entities;
 }
