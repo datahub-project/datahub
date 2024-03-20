bda46f3603a8e830d7b6399e97d18b32282d6088
Ryan Holstien <ryan@acryl.io>
2024-03-20 09:14:43 -0500

    fix

--- metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v2/models/BatchGetUrnRequest.java
+++ metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v2/models/BatchGetUrnRequest.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.io.Serializable;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
 @Value
