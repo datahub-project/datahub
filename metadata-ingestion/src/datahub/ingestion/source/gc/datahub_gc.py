334897ea9add3e176a0e87ef0ffc64ee4e81291f
Aseem Bansal <asmbansal2@gmail.com>
2024-03-21 23:09:11 +0530

    feat(ingest/gc): add helper to truncate indices

--- metadata-ingestion/src/datahub/ingestion/source/gc/datahub_gc.py
+++ metadata-ingestion/src/datahub/ingestion/source/gc/datahub_gc.py
@@ -1,10 +1,13 @@
+import datetime
+import re
 import time
 from dataclasses import dataclass
-from typing import Iterable
+from typing import Dict, Iterable
 
+import click
 from pydantic import Field
 
-from datahub.configuration.common import ConfigModel
+from datahub.configuration.common import ConfigModel, OperationalError
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.decorators import (
     SupportStatus,
@@ -20,6 +23,14 @@ class DataHubGcSourceConfig(ConfigModel):
     cleanup_expired_tokens: bool = Field(
         default=True,
         description="Whether to clean up expired tokens or not",
+    )
+    truncate_indices: bool = Field(
+        default=True,
+        description="Whether to truncate elasticsearch indices or not which can be safely truncated",
+    )
+    truncate_index_older_than_days: int = Field(
+        default=30,
+        description="Indices older than this number of days will be truncated",
     )
 
 
@@ -51,7 +62,115 @@ class DataHubGcSource(Source):
     ) -> Iterable[MetadataWorkUnit]:
         if self.config.cleanup_expired_tokens:
             self.revoke_expired_tokens()
+        if self.config.truncate_indices:
+            self.truncate_indices()
         yield from []
+
+    def truncate_indices(self) -> None:
+        self._truncate_timeseries_helper(aspect_name="operation", entity_type="dataset")
+        self._truncate_timeseries_helper(
+            aspect_name="datasetusage", entity_type="dataset"
+        )
+        self._truncate_timeseries_helper(aspect_name="chartusage", entity_type="chart")
+        self._truncate_timeseries_helper(
+            aspect_name="dashboardusage", entity_type="dashboard"
+        )
+
+    def _truncate_timeseries_helper(self, aspect_name: str, entity_type: str) -> None:
+        self._truncate_timeseries_with_watch_optional(
+            aspect_name=aspect_name, entity_type=entity_type, watch=False
+        )
+        self._truncate_timeseries_with_watch_optional(
+            aspect_name=aspect_name, entity_type=entity_type, watch=True
+        )
+
+    def _truncate_timeseries_with_watch_optional(
+        self, aspect_name: str, entity_type: str, watch: bool
+    ) -> None:
+        graph = self.graph
+        assert graph is not None
+        if watch:
+            to_delete = 1
+            while to_delete > 0:
+                response = self.truncate_timeseries_util(
+                    aspect=aspect_name,
+                    dry_run=watch,
+                    days_ago=self.config.truncate_index_older_than_days,
+                    entity_type=entity_type,
+                )
+                val = response.get("value", "")
+                if "This was a dry run" not in val or "out of" not in val:
+                    return
+                prev_to_delete = to_delete
+                to_delete, total = re.findall(r"\d+", val)[:2]
+                to_delete = int(to_delete)
+                if to_delete <= 0:
+                    click.echo("Nothing to delete.")
+                    return
+                click.echo(f"to_delete {to_delete} / {total}")
+                if to_delete == prev_to_delete:
+                    click.echo("Seems to be stuck. Ending the loop.")
+                    break
+                elif to_delete < 50000:
+                    click.echo("Too small truncation. Not going to watch.")
+                    return
+                else:
+                    time.sleep(60)
+        else:
+            self.truncate_timeseries_util(
+                aspect=aspect_name,
+                dry_run=watch,
+                days_ago=self.config.truncate_index_older_than_days,
+                entity_type=entity_type,
+            )
+
+    def x_days_ago_millis(self, days: int) -> int:
+        x_days_ago_datetime = datetime.datetime.now(
+            datetime.timezone.utc
+        ) - datetime.timedelta(days=days)
+        return int(x_days_ago_datetime.timestamp() * 1000)
+
+    def truncate_timeseries_util(
+        self,
+        aspect: str,
+        days_ago: int,
+        dry_run: bool = True,
+        entity_type: str = "dataset",
+    ) -> Dict:
+        graph = self.graph
+        assert graph is not None
+
+        gms_url = graph._gms_server
+        if not dry_run:
+            click.echo(
+                f"Going to truncate timeseries for {aspect} for {gms_url} older than {days_ago} days"
+            )
+        days_ago_millis = self.x_days_ago_millis(days_ago)
+        url = f"{gms_url}/operations?action=truncateTimeseriesAspect"
+        try:
+            response = graph._post_generic(
+                url=url,
+                payload_dict={
+                    "entityType": entity_type,
+                    "aspect": aspect,
+                    "endTimeMillis": days_ago_millis,
+                    "dryRun": dry_run,
+                },
+            )
+            # click.echo(f"Response: {response}")
+        except OperationalError:
+            response = graph._post_generic(
+                url=url,
+                payload_dict={
+                    "entityType": entity_type,
+                    "aspect": aspect,
+                    "endTimeMillis": days_ago_millis,
+                    "dryRun": dry_run,
+                    "forceDeleteByQuery": True,
+                },
+            )
+            # click.echo(f"Response: {response}")
+        return response
 
     def revoke_expired_tokens(self) -> None:
         total = 1
