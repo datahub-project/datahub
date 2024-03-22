295b91aa82449c65381f142918a1700f0e27241f
treff7es <treff7es@gmail.com>
2024-03-22 14:37:36 +0100

    Reverting accidentally downgraded package

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -215,7 +215,7 @@ pyhive_common = {
     # Instead, we put the fix in our PyHive fork, so no thrift pin is needed.
 }
 
-microsoft_common = {"msal==1.16.0"}
+microsoft_common = {"msal==1.22.0"}
 
 iceberg_common = {
     # Iceberg Python SDK
