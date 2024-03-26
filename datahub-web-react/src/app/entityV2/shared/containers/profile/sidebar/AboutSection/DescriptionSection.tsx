bfe205d4366e3071f9ea33b160267d2b86909bb5
Sam Black <sam.black@acryl.io>
2024-03-26 13:01:11 -0400

    feat(alchemy): Update DescriptionSection to use MarkdownViewer (#2716)

--- datahub-web-react/src/app/entityV2/shared/containers/profile/sidebar/AboutSection/DescriptionSection.tsx
+++ datahub-web-react/src/app/entityV2/shared/containers/profile/sidebar/AboutSection/DescriptionSection.tsx
@@ -1,8 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components/macro';
-import { Typography } from 'antd';
-import { MarkdownView } from '../../../../components/legacy/MarkdownViewer';
-import NoMarkdownViewer, { removeMarkdown } from '../../../../components/styled/StripMarkdownText';
+import MarkdownViewer from '../../../../components/legacy/MarkdownViewer';
 import { REDESIGN_COLORS } from '../../../../constants';
 
 const ABBREVIATED_LIMIT = 150;
@@ -12,14 +10,8 @@ const ContentWrapper = styled.div`
     font-weight: 500;
     color: ${REDESIGN_COLORS.DARK_GREY};
     line-height: 20px;
-    ${MarkdownView} {
-        font-size: 12px;
-    }
+    white-space: break-spaces;
 `;
-const ReadLessContainer = styled.div`
-    margin-top: 5px;
-`;
-const ReadLessText = styled(Typography.Link)``;
 
 interface Props {
     description: string;
@@ -28,44 +20,15 @@ interface Props {
 }
 
 export default function DescriptionSection({ description, isExpandable, limit }: Props) {
-    const isOverLimit = description && removeMarkdown(description).length > ABBREVIATED_LIMIT;
-    const shouldShowReadMore = isExpandable && isOverLimit;
-    const [expanded, setExpanded] = useState<boolean>(false);
-
-    function readMore() {
-        setExpanded(true);
-    }
-
     return (
         <ContentWrapper>
-            {!expanded ? (
-                <NoMarkdownViewer
-                    limit={limit || ABBREVIATED_LIMIT}
-                    readMore={
-                        shouldShowReadMore ? (
-                            <Typography.Link onClick={readMore}> Read More</Typography.Link>
-                        ) : undefined
-                    }
-                    shouldWrap
-                >
-                    {description}
-                </NoMarkdownViewer>
-            ) : (
-                <>
-                    <NoMarkdownViewer shouldWrap>{description}</NoMarkdownViewer>
-                    {isOverLimit && (
-                        <ReadLessContainer>
-                            <ReadLessText
-                                onClick={() => {
-                                    setExpanded(false);
-                                }}
-                            >
-                                Read Less
-                            </ReadLessText>
-                        </ReadLessContainer>
-                    )}
-                </>
-            )}
+            <MarkdownViewer
+                limit={limit || ABBREVIATED_LIMIT}
+                source={description}
+                editable={false}
+                ignoreLimit={!isExpandable}
+                isCompact
+            />
         </ContentWrapper>
     );
 }
