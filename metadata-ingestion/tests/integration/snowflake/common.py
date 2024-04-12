203519f03d412cfed0413a588ba854bd4d8e0a9b
Mayuri N <mayuri.nehate@gslab.com>
2024-04-12 19:05:14 +0530

    fix lint

--- metadata-ingestion/tests/integration/snowflake/common.py
+++ metadata-ingestion/tests/integration/snowflake/common.py
@@ -2,11 +2,12 @@ import json
 from datetime import datetime, timezone
 from unittest.mock import MagicMock
 
+from snowflake.connector.cursor import SnowflakeCursor
+
 from datahub.configuration.common import AllowDenyPattern
 from datahub.configuration.time_window_config import BucketDuration
 from datahub.ingestion.source.snowflake import snowflake_query
 from datahub.ingestion.source.snowflake.snowflake_query import SnowflakeQuery
-from snowflake.connector.cursor import SnowflakeCursor
 
 NUM_TABLES = 10
 NUM_VIEWS = 2
