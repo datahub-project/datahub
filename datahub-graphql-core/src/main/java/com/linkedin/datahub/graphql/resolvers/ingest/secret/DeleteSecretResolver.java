3836520f08a88060f5e7c5858935c9e6d8fdb322
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2024-04-19 00:14:30 +0530

    codeQL error secrets cannot be logged

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/ingest/secret/DeleteSecretResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/ingest/secret/DeleteSecretResolver.java
@@ -22,16 +22,16 @@ public class DeleteSecretResolver implements DataFetcher<CompletableFuture<Strin
   public CompletableFuture<String> get(final DataFetchingEnvironment environment) throws Exception {
     final QueryContext context = environment.getContext();
     if (IngestionAuthUtils.canManageSecrets(context)) {
-      final String inputUrn = environment.getArgument("urn");
-      final Urn urn = Urn.createFromString(inputUrn);
+      final String secretUrn = environment.getArgument("urn");
+      final Urn urn = Urn.createFromString(secretUrn);
       return CompletableFuture.supplyAsync(
           () -> {
             try {
               _entityClient.deleteEntity(context.getOperationContext(), urn);
-              return inputUrn;
+              return secretUrn;
             } catch (Exception e) {
               throw new RuntimeException(
-                  String.format("Failed to perform delete against secret with urn %s", inputUrn),
+                  String.format("Failed to perform delete against secret with urn %s", secretUrn),
                   e);
             }
           });
