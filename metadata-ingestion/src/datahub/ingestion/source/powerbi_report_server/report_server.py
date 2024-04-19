996e5b013014a395fd5a98f12f7098d9b4b64e01
Raj Tekal <varadaraj_tekal@optum.com>
2024-04-19 15:53:50 -0400

    chore(metadata) Addressing vulnerabilities (#10296)

--- metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/report_server.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi_report_server/report_server.py
@@ -157,7 +157,7 @@ class PowerBiReportServerAPI:
             response = requests.get(
                 url=url_https,
                 auth=self.get_auth_credentials,
-                verify=False,
+                verify=True,
             )
         except ConnectionError:
             LOGGER.info("Request to Report URL={}".format(url_http))
