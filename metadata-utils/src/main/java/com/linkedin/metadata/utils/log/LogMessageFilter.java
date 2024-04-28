8d5a6876a31ecad641b25636d85e8b3099dff35e
Masterchen09 <13187726+Masterchen09@users.noreply.github.com>
2024-04-28 17:13:15 +0200

    fix: also check exceptions/stack trace when filtering log messages

--- metadata-utils/src/main/java/com/linkedin/metadata/utils/log/LogMessageFilter.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/utils/log/LogMessageFilter.java
@@ -1,6 +1,8 @@
 package com.linkedin.metadata.utils.log;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
+import ch.qos.logback.classic.spi.ThrowableProxyUtil;
 import ch.qos.logback.core.filter.AbstractMatcherFilter;
 import ch.qos.logback.core.spi.FilterReply;
 import java.util.ArrayList;
@@ -21,7 +23,21 @@ public class LogMessageFilter extends AbstractMatcherFilter<ILoggingEvent> {
       return FilterReply.NEUTRAL;
     }
 
-    if (this.excluded.stream().anyMatch(message -> event.getFormattedMessage().contains(message))) {
+    final String formattedMessage = event.getFormattedMessage();
+
+    IThrowableProxy throwbleProxy = event.getThrowableProxy();
+    final String throwableString;
+    if (throwbleProxy != null) {
+      throwableString = ThrowableProxyUtil.asString(throwbleProxy);
+    } else {
+      throwableString = "";
+    }
+
+    if (this.excluded.stream()
+        .anyMatch(
+            message ->
+                formattedMessage.contains(message)
+                    || (!throwableString.equals("") && throwableString.contains(message)))) {
       return FilterReply.DENY;
     }
     return FilterReply.ACCEPT;
