1061f4cd05feff7d3431034afbccb0491df91c1d
david-leifker <114954101+david-leifker@users.noreply.github.com>
2024-05-03 14:18:44 -0500

    feat(search): adding percentile upgrade job (#3092)

---
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/system/acryl/UsageStoragePercentile.java
@@ -0,0 +1,112 @@
+package com.linkedin.datahub.upgrade.system.acryl;
+
+import static com.linkedin.metadata.Constants.STORAGE_FEATURES_ASPECT_NAME;
+import static com.linkedin.metadata.Constants.USAGE_FEATURES_ASPECT_NAME;
+
+import com.google.common.collect.ImmutableList;
+import com.linkedin.datahub.upgrade.UpgradeStep;
+import com.linkedin.datahub.upgrade.system.AbstractMCLStep;
+import com.linkedin.datahub.upgrade.system.NonBlockingSystemUpgrade;
+import com.linkedin.metadata.entity.AspectDao;
+import com.linkedin.metadata.entity.EntityService;
+import java.util.List;
+import javax.annotation.Nonnull;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * A job that reindexes all usageFeatures, storageFeatures aspects. This is required to support
+ * search ranking with percentiles.
+ */
+@Slf4j
+public class UsageStoragePercentile implements NonBlockingSystemUpgrade {
+
+  private final List<UpgradeStep> _steps;
+
+  public UsageStoragePercentile(
+      EntityService<?> entityService,
+      AspectDao aspectDao,
+      boolean enabled,
+      Integer batchSize,
+      Integer batchDelayMs,
+      Integer limit) {
+    if (enabled) {
+      _steps =
+          ImmutableList.of(
+              new UsagePercentileStep(entityService, aspectDao, batchSize, batchDelayMs, limit),
+              new StoragePercentileStep(entityService, aspectDao, batchSize, batchDelayMs, limit));
+    } else {
+      _steps = ImmutableList.of();
+    }
+  }
+
+  @Override
+  public String id() {
+    return this.getClass().getName();
+  }
+
+  @Override
+  public List<UpgradeStep> steps() {
+    return _steps;
+  }
+
+  @Slf4j
+  public static class UsagePercentileStep extends AbstractMCLStep {
+
+    public UsagePercentileStep(
+        EntityService<?> entityService,
+        AspectDao aspectDao,
+        Integer batchSize,
+        Integer batchDelayMs,
+        Integer limit) {
+      super(entityService, aspectDao, batchSize, batchDelayMs, limit);
+    }
+
+    @Override
+    public String id() {
+      return "usage-percentile-v1";
+    }
+
+    @Nonnull
+    @Override
+    protected String getAspectName() {
+      return USAGE_FEATURES_ASPECT_NAME;
+    }
+
+    @Nullable
+    @Override
+    protected String getUrnLike() {
+      return null;
+    }
+  }
+
+  @Slf4j
+  public static class StoragePercentileStep extends AbstractMCLStep {
+
+    public StoragePercentileStep(
+        EntityService<?> entityService,
+        AspectDao aspectDao,
+        Integer batchSize,
+        Integer batchDelayMs,
+        Integer limit) {
+      super(entityService, aspectDao, batchSize, batchDelayMs, limit);
+    }
+
+    @Override
+    public String id() {
+      return "storage-percentile-v1";
+    }
+
+    @Nonnull
+    @Override
+    protected String getAspectName() {
+      return STORAGE_FEATURES_ASPECT_NAME;
+    }
+
+    @Nullable
+    @Override
+    protected String getUrnLike() {
+      return null;
+    }
+  }
+}
