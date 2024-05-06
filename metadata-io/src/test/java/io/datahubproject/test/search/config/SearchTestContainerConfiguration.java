26fe2af04a63a5f409a8d3f2f8f713dc5c21b6c6
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2024-05-06 15:08:40 +0530

    Remove PropertySource

--- metadata-io/src/test/java/io/datahubproject/test/search/config/SearchTestContainerConfiguration.java
+++ metadata-io/src/test/java/io/datahubproject/test/search/config/SearchTestContainerConfiguration.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
 import org.testcontainers.containers.GenericContainer;
 
 /**
@@ -28,7 +27,6 @@ import org.testcontainers.containers.GenericContainer;
  * io.datahubproject.test.search.config.SearchCommonTestConfiguration} instead.
  */
 @TestConfiguration
-@PropertySource("classpath:/application.yaml")
 public class SearchTestContainerConfiguration {
   // This port is overridden by the specific test container instance
   private static final int HTTP_PORT = 9200;
