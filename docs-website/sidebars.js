d2fedfb07ebae838e8b60155d0e44620d20271d3
yoonhyejin <0327jane@gmail.com>
2024-05-09 14:12:38 +0900

    fix: merge cli guide

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -131,11 +131,6 @@ module.exports = {
           className: "saasOnly",
         },
         {
-          "Metadata Ingestion With Acryl": [
-            "docs/managed-datahub/metadata-ingestion-with-acryl/ingestion",
-          ],
-        },
-        {
           "DataHub API": [
             {
               type: "doc",
