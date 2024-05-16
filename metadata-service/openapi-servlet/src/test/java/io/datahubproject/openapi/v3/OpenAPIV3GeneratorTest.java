d106d38fc5915904a5f6141195a63f153eeba9ac
Timothy Jin <tjin316@gmail.com>
2024-05-16 10:40:32 -0700

    Fix formatting for #10514

--- metadata-service/openapi-servlet/src/test/java/io/datahubproject/openapi/v3/OpenAPIV3GeneratorTest.java
+++ metadata-service/openapi-servlet/src/test/java/io/datahubproject/openapi/v3/OpenAPIV3GeneratorTest.java
@@ -9,43 +9,39 @@ import com.linkedin.data.schema.annotation.PathSpecBasedSchemaAnnotationVisitor;
 import com.linkedin.metadata.models.registry.ConfigEntityRegistry;
 import io.swagger.v3.core.util.Yaml;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.Schema;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-
-import io.swagger.v3.oas.models.media.Schema;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class OpenAPIV3GeneratorTest {
   public static final String BASE_DIRECTORY =
-          "../../entity-registry/custom-test-model/build/plugins/models";
+      "../../entity-registry/custom-test-model/build/plugins/models";
 
   @BeforeTest
   public void disableAssert() {
     PathSpecBasedSchemaAnnotationVisitor.class
-            .getClassLoader()
-            .setClassAssertionStatus(PathSpecBasedSchemaAnnotationVisitor.class.getName(), false);
+        .getClassLoader()
+        .setClassAssertionStatus(PathSpecBasedSchemaAnnotationVisitor.class.getName(), false);
   }
 
   @Test
   public void testOpenApiSpecBuilder() throws Exception {
     ConfigEntityRegistry er =
-            new ConfigEntityRegistry(
-                    OpenAPIV3GeneratorTest.class
-                            .getClassLoader()
-                            .getResourceAsStream("entity-registry.yml"));
+        new ConfigEntityRegistry(
+            OpenAPIV3GeneratorTest.class
+                .getClassLoader()
+                .getResourceAsStream("entity-registry.yml"));
 
     OpenAPI openAPI = OpenAPIV3Generator.generateOpenApiSpec(er);
     String openapiYaml = Yaml.pretty(openAPI);
     Files.write(
-            Path.of(getClass().getResource("/").getPath(), "open-api.yaml"),
-            openapiYaml.getBytes(StandardCharsets.UTF_8));
+        Path.of(getClass().getResource("/").getPath(), "open-api.yaml"),
+        openapiYaml.getBytes(StandardCharsets.UTF_8));
 
     assertTrue(openAPI.getComponents().getSchemas().size() > 900);
     assertTrue(openAPI.getComponents().getParameters().size() > 50);
