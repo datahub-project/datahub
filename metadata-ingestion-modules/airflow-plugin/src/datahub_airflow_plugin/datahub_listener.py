8e58343b41dadd9f9abf9862263635b22a4a6d86
Harshal Sheth <hsheth2@gmail.com>
2024-05-23 16:21:20 -0700

    tweak code

--- metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/datahub_listener.py
+++ metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/datahub_listener.py
@@ -251,7 +251,7 @@ class DataHubListener:
                 logger.info(f"SQL parsing error: {error}", exc_info=error)
                 datajob.properties[
                     "datahub_sql_parser_error"
-                ] = f"{type(error)}: {error}"
+                ] = f"{type(error).__name__}: {error}"
             if not sql_parsing_result.debug_info.table_error:
                 input_urns.extend(sql_parsing_result.in_tables)
                 output_urns.extend(sql_parsing_result.out_tables)
