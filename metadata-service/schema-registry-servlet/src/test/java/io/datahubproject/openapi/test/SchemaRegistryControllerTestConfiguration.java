dfa9bd27793795dd7aa9e57da1f70c31b3e7c451
david-leifker <114954101+david-leifker@users.noreply.github.com>
2024-05-31 15:53:02 -0500

    feat(consumers): mce-consumer throttling based on mae-consumer lag (#10626)

--- metadata-service/schema-registry-servlet/src/test/java/io/datahubproject/openapi/test/SchemaRegistryControllerTestConfiguration.java
+++ metadata-service/schema-registry-servlet/src/test/java/io/datahubproject/openapi/test/SchemaRegistryControllerTestConfiguration.java
@@ -1,6 +1,7 @@
 package io.datahubproject.openapi.test;
 
 import com.linkedin.metadata.dao.producer.KafkaHealthChecker;
+import com.linkedin.metadata.models.registry.EntityRegistry;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,4 +12,6 @@ import org.springframework.test.context.TestPropertySource;
 @ComponentScan(basePackages = {"com.linkedin.gms.factory.kafka", "com.linkedin.gms.factory.config"})
 public class SchemaRegistryControllerTestConfiguration {
   @MockBean KafkaHealthChecker kafkaHealthChecker;
+
+  @MockBean EntityRegistry entityRegistry;
 }
