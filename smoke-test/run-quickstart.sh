86ee48d35f032ce19684b80b0b141c4f41f59318
David Leifker <david.leifker@acryl.io>
2024-06-03 18:16:11 -0500

    build(smoke-test): test docker profile smoke-test

--- smoke-test/run-quickstart.sh
+++ smoke-test/run-quickstart.sh
@@ -10,16 +10,6 @@ source venv/bin/activate
 mkdir -p ~/.datahub/plugins/frontend/auth/
 echo "test_user:test_pass" >> ~/.datahub/plugins/frontend/auth/user.props
 
-DATAHUB_SEARCH_IMAGE="${DATAHUB_SEARCH_IMAGE:=opensearchproject/opensearch}"
-DATAHUB_SEARCH_TAG="${DATAHUB_SEARCH_TAG:=2.9.0}"
-XPACK_SECURITY_ENABLED="${XPACK_SECURITY_ENABLED:=plugins.security.disabled=true}"
-ELASTICSEARCH_USE_SSL="${ELASTICSEARCH_USE_SSL:=false}"
-USE_AWS_ELASTICSEARCH="${USE_AWS_ELASTICSEARCH:=true}"
-
 echo "DATAHUB_VERSION = $DATAHUB_VERSION"
-DATAHUB_TELEMETRY_ENABLED=false  \
-DOCKER_COMPOSE_BASE="file://$( dirname "$DIR" )" \
-DATAHUB_SEARCH_IMAGE="$DATAHUB_SEARCH_IMAGE" DATAHUB_SEARCH_TAG="$DATAHUB_SEARCH_TAG" \
-XPACK_SECURITY_ENABLED="$XPACK_SECURITY_ENABLED" ELASTICSEARCH_USE_SSL="$ELASTICSEARCH_USE_SSL" \
-USE_AWS_ELASTICSEARCH="$USE_AWS_ELASTICSEARCH" \
-datahub docker quickstart --version ${DATAHUB_VERSION} --standalone_consumers --dump-logs-on-failure --kafka-setup
+DATAHUB_TELEMETRY_ENABLED=false \
+docker compose -f ../docker/profiles/docker-compose.yml --profile quickstart-consumers up --detach --wait
