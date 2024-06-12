4e4ae4741f0e53f73de02fd5fa1ef708dcd0f4bb
RyanHolstien <RyanHolstien@users.noreply.github.com>
2024-06-12 14:31:11 -0500

    feat(mutator): add persona mutator to prevent null

---
+++ entity-registry/src/main/java/com/linkedin/metadata/aspect/hooks/CorpUserEditableInfoMutator.java
@@ -0,0 +1,72 @@
+package com.linkedin.metadata.aspect.hooks;
+
+import com.linkedin.events.metadata.ChangeType;
+import com.linkedin.identity.CorpUserEditableInfo;
+import com.linkedin.metadata.aspect.ReadItem;
+import com.linkedin.metadata.aspect.RetrieverContext;
+import com.linkedin.metadata.aspect.batch.BatchItem;
+import com.linkedin.metadata.aspect.batch.ChangeMCP;
+import com.linkedin.metadata.aspect.plugins.config.AspectPluginConfig;
+import com.linkedin.metadata.aspect.plugins.hooks.MutationHook;
+import com.linkedin.util.Pair;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+import javax.annotation.Nonnull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
+import static com.linkedin.metadata.Constants.*;
+
+
+@Slf4j
+@Setter
+@Getter
+@Accessors(chain = true)
+public class CorpUserEditableInfoMutator extends MutationHook {
+  @Nonnull private AspectPluginConfig config;
+
+  /*
+  TODO: Ideally we are sending in patches that don't overwrite
+*/
+  @Override
+  protected Stream<Pair<ChangeMCP, Boolean>> writeMutation(
+      @Nonnull Collection<ChangeMCP> changeMCPS, @Nonnull RetrieverContext retrieverContext) {
+
+    List<Pair<ChangeMCP, Boolean>> results = new LinkedList<>();
+
+    for (ChangeMCP item : changeMCPS) {
+      if (changeTypeFilter(item) && aspectFilter(item)) {
+          results.add(Pair.of(item, processEditableCorpUserInfoAspect(item)));
+      } else {
+        // no op
+        results.add(Pair.of(item, false));
+      }
+    }
+
+    return results.stream();
+  }
+
+  private static boolean changeTypeFilter(BatchItem item) {
+    return !ChangeType.DELETE.equals(item.getChangeType())
+        && !ChangeType.PATCH.equals(item.getChangeType());
+  }
+
+  private static boolean aspectFilter(ReadItem item) {
+    return item.getAspectName().equals(CORP_USER_EDITABLE_INFO_ASPECT_NAME);
+  }
+
+  private static boolean processEditableCorpUserInfoAspect(ChangeMCP item) {
+    boolean mutated = false;
+    final CorpUserEditableInfo previous = item.getPreviousAspect(CorpUserEditableInfo.class);
+    final CorpUserEditableInfo next = item.getAspect(CorpUserEditableInfo.class);
+    if (next.getPersona() == null && previous.getPersona() != null) {
+      next.setPersona(previous.getPersona());
+      mutated = true;
+    }
+    return mutated;
+  }
+}
