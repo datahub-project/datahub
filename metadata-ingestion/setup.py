datahub-project/datahub

commit e842161849d2461aee2882672fe3efd099cfb347

feat(ingest): add fast query fingerprinting (#10619)

metadata-ingestion/src/datahub/sql_parsing/sql_parsing_aggregator.py
@@ -160,6 +160,8 @@ class SqlAggregatorReport(Report):
     # SQL parsing (over all invocations).
     num_sql_parsed: int = 0
     sql_parsing_timer: PerfTimer = dataclasses.field(default_factory=PerfTimer)
+    sql_fingerprinting_timer: PerfTimer = dataclasses.field(default_factory=PerfTimer)
+    sql_formatting_timer: PerfTimer = dataclasses.field(default_factory=PerfTimer)
 
     # Other lineage loading metrics.
     num_known_query_lineage: int = 0
@@ -381,7 +383,8 @@ class SqlParsingAggregator(Closeable):
 
     def _maybe_format_query(self, query: str) -> str:
         if self.format_queries:
-            return try_format_query(query, self.platform.platform_name)
+            with self.report.sql_formatting_timer:
+                return try_format_query(query, self.platform.platform_name)
         return query
 
     def add_known_query_lineage(
@@ -405,9 +408,12 @@ class SqlParsingAggregator(Closeable):
         self.report.num_known_query_lineage += 1
 
         # Generate a fingerprint for the query.
-        query_fingerprint = get_query_fingerprint(
-            known_query_lineage.query_text, platform=self.platform.platform_name
-        )
+        with self.report.sql_fingerprinting_timer:
+            query_fingerprint = get_query_fingerprint(
+                known_query_lineage.query_text,
+                platform=self.platform.platform_name,
+                fast=True,
+            )
         formatted_query = self._maybe_format_query(known_query_lineage.query_text)
 
         # Register the query.

metadata-ingestion/src/datahub/entrypoints.py
@@ -66,7 +66,7 @@ MAX_CONTENT_WIDTH = 120
     "--log-file",
     type=click.Path(dir_okay=False),
     default=None,
-    help="Enable debug logging.",
+    help="Write debug-level logs to a file.",
 )
 @click.version_option(
     version=datahub_package.nice_version_name(),

metadata-ingestion/src/datahub/ingestion/glossary/classification_mixin.py
@@ -300,6 +300,8 @@ def classification_workunit_processor(
     table_name = ".".join(table_id)
     if not classification_handler.is_classification_enabled_for_table(table_name):
         yield from table_wu_generator
+        return
+
     for wu in table_wu_generator:
         maybe_schema_metadata = wu.get_aspect_of_type(SchemaMetadata)
         if (

metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_v2.py
@@ -257,6 +257,7 @@ class SnowflakeV2Source(
                 graph=self.ctx.graph,
                 generate_usage_statistics=False,
                 generate_operations=False,
+                format_queries=self.config.format_sql_queries,
             )
             self.report.sql_aggregator = self.aggregator.report
 

metadata-ingestion/src/datahub/sql_parsing/sqlglot_utils.py
@@ -1,6 +1,7 @@
 import functools
 import hashlib
 import logging
+import re
 from typing import Dict, Iterable, Optional, Tuple, Union
 
 import sqlglot
@@ -109,6 +110,80 @@ def _expression_to_string(
     return expression.sql(dialect=get_dialect(platform))
 
 
+_BASIC_NORMALIZATION_RULES = {
+    # Remove /* */ comments.
+    re.compile(r"/\*.*?\*/", re.DOTALL): "",
+    # Remove -- comments.
+    re.compile(r"--.*$"): "",
+    # Replace all runs of whitespace with a single space.
+    re.compile(r"\s+"): " ",
+    # Remove leading and trailing whitespace and trailing semicolons.
+    re.compile(r"^\s+|[\s;]+$"): "",
+    # Replace anything that looks like a number with a placeholder.
+    re.compile(r"\b\d+\b"): "?",
+    # Replace anything that looks like a string with a placeholder.
+    re.compile(r"'[^']*'"): "?",
+    # Replace sequences of IN/VALUES with a single placeholder.
+    re.compile(r"\b(IN|VALUES)\s*\(\?(?:, \?)*\)", re.IGNORECASE): r"\1 (?)",
+    # Normalize parenthesis spacing.
+    re.compile(r"\( "): "(",
+    re.compile(r" \)"): ")",
+}
+_TABLE_NAME_NORMALIZATION_RULES = {
+    # Replace UUID-like strings with a placeholder (both - and _ variants).
+    re.compile(
+        r"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}",
+        re.IGNORECASE,
+    ): "00000000-0000-0000-0000-000000000000",
+    re.compile(
+        r"[0-9a-f]{8}_[0-9a-f]{4}_[0-9a-f]{4}_[0-9a-f]{4}_[0-9a-f]{12}",
+        re.IGNORECASE,
+    ): "00000000_0000_0000_0000_000000000000",
+    # GE temporary table names (prefix + 8 digits of a UUIDv4)
+    re.compile(
+        r"\b(ge_tmp_|ge_temp_|gx_temp_)[0-9a-f]{8}\b", re.IGNORECASE
+    ): r"\1abcdefgh",
+    # Date-suffixed table names (e.g. _20210101)
+    re.compile(r"\b(\w+)(19|20)\d{4}\b"): r"\1YYYYMM",
+    re.compile(r"\b(\w+)(19|20)\d{6}\b"): r"\1YYYYMMDD",
+    re.compile(r"\b(\w+)(19|20)\d{8}\b"): r"\1YYYYMMDDHH",
+    re.compile(r"\b(\w+)(19|20)\d{10}\b"): r"\1YYYYMMDDHHMM",
+}
+
+
+def generalize_query_fast(
+    expression: sqlglot.exp.ExpOrStr,
+    dialect: DialectOrStr,
+    change_table_names: bool = False,
+) -> str:
+    """Variant of `generalize_query` that only does basic normalization.
+
+    Args:
+        expression: The SQL query to generalize.
+        dialect: The SQL dialect to use.
+        change_table_names: If True, replace table names with placeholders. Note
+            that this should only be used for query filtering purposes, as it
+            violates the general assumption that the queries with the same fingerprint
+            have the same lineage/usage/etc.
+
+    Returns:
+        The generalized SQL query.
+    """
+
+    if isinstance(expression, sqlglot.exp.Expression):
+        expression = expression.sql(dialect=get_dialect(dialect))
+    query_text = expression
+
+    REGEX_REPLACEMENTS = {
+        **_BASIC_NORMALIZATION_RULES,
+        **(_TABLE_NAME_NORMALIZATION_RULES if change_table_names else {}),
+    }
+
+    for pattern, replacement in REGEX_REPLACEMENTS.items():
+        query_text = pattern.sub(replacement, query_text)
+    return query_text
+
+
 def generalize_query(expression: sqlglot.exp.ExpOrStr, dialect: DialectOrStr) -> str:
     """
     Generalize/normalize a SQL query.
@@ -172,11 +247,14 @@ def generate_hash(text: str) -> str:
 
 
 def get_query_fingerprint_debug(
-    expression: sqlglot.exp.ExpOrStr, platform: DialectOrStr
+    expression: sqlglot.exp.ExpOrStr, platform: DialectOrStr, fast: bool = False
 ) -> Tuple[str, Optional[str]]:
     try:
-        dialect = get_dialect(platform)
-        expression_sql = generalize_query(expression, dialect=dialect)
+        if not fast:
+            dialect = get_dialect(platform)
+            expression_sql = generalize_query(expression, dialect=dialect)
+        else:
+            expression_sql = generalize_query_fast(expression, dialect=platform)
     except (ValueError, sqlglot.errors.SqlglotError) as e:
         if not isinstance(expression, str):
             raise
@@ -193,7 +271,7 @@ def get_query_fingerprint_debug(
 
 
 def get_query_fingerprint(
-    expression: sqlglot.exp.ExpOrStr, platform: DialectOrStr
+    expression: sqlglot.exp.ExpOrStr, platform: DialectOrStr, fast: bool = False
 ) -> str:
     """Get a fingerprint for a SQL query.
 
@@ -215,7 +293,7 @@ def get_query_fingerprint(
         The fingerprint for the SQL query.
     """
 
-    return get_query_fingerprint_debug(expression, platform)[0]
+    return get_query_fingerprint_debug(expression, platform, fast=fast)[0]
 
 
 @functools.lru_cache(maxsize=FORMAT_QUERY_CACHE_SIZE)

metadata-ingestion/tests/integration/snowflake/test_snowflake.py
@@ -119,6 +119,7 @@ def test_snowflake_basic(pytestconfig, tmp_path, mock_time, mock_datahub_graph):
                         include_table_lineage=True,
                         include_view_lineage=True,
                         include_usage_stats=True,
+                        format_sql_queries=True,
                         validate_upstreams_against_patterns=False,
                         include_operational_stats=True,
                         email_as_user_identifier=True,
@@ -213,6 +214,7 @@ def test_snowflake_private_link(pytestconfig, tmp_path, mock_time, mock_datahub_
                         include_views=True,
                         include_view_lineage=True,
                         include_usage_stats=False,
+                        format_sql_queries=True,
                         incremental_lineage=False,
                         include_operational_stats=False,
                         platform_instance="instance1",

metadata-ingestion/tests/unit/sql_parsing/test_sqlglot_utils.py
@@ -1,5 +1,7 @@
 import textwrap
+from enum import Enum
 
+import pytest
 import sqlglot
 
 from datahub.sql_parsing.sql_parsing_common import QueryType
@@ -9,6 +11,7 @@ from datahub.sql_parsing.sqlglot_lineage import (
 )
 from datahub.sql_parsing.sqlglot_utils import (
     generalize_query,
+    generalize_query_fast,
     get_dialect,
     get_query_fingerprint,
     is_dialect_instance,
@@ -49,49 +52,67 @@ def test_query_types():
     ) == (QueryType.CREATE_VIEW, {"kind": "VIEW"})
 
 
-def test_query_generalization():
-    # Basic keyword normalization.
-    assert (
-        generalize_query("select * from foo", dialect="redshift") == "SELECT * FROM foo"
-    )
+class QueryGeneralizationTestMode(Enum):
+    FULL = "full"
+    FAST = "fast"
+    BOTH = "both"
 
-    # Comment removal and whitespace normalization.
-    assert (
-        generalize_query(
-            "/* query system = foo, id = asdf */\nselect /* inline comment */ *\nfrom foo",
-            dialect="redshift",
-        )
-        == "SELECT * FROM foo"
-    )
 
-    # Parameter normalization.
-    assert (
-        generalize_query(
+@pytest.mark.parametrize(
+    "query, dialect, expected, mode",
+    [
+        # Basic keyword normalization.
+        (
+            "select * from foo",
+            "redshift",
+            "SELECT * FROM foo",
+            QueryGeneralizationTestMode.FULL,
+        ),
+        # Comment removal and whitespace normalization.
+        (
+            "/* query system = foo, id = asdf */\nSELECT /* inline comment */ *\nFROM foo",
+            "redshift",
+            "SELECT * FROM foo",
+            QueryGeneralizationTestMode.BOTH,
+        ),
+        # Parameter normalization.
+        (
             "UPDATE  \"books\" SET page_count = page_count + 1, author_count = author_count + 1 WHERE book_title = 'My New Book'",
-            dialect="redshift",
-        )
-        == 'UPDATE "books" SET page_count = page_count + ?, author_count = author_count + ? WHERE book_title = ?'
-    )
-    assert (
-        generalize_query(
-            "select * from foo where date = '2021-01-01'", dialect="redshift"
-        )
-        == "SELECT * FROM foo WHERE date = ?"
-    )
-    assert (
-        generalize_query(
-            "select * from books where category in ('fiction', 'biography', 'fantasy')",
-            dialect="redshift",
-        )
-        == "SELECT * FROM books WHERE category IN (?)"
-    )
-    assert (
-        generalize_query(
+            "redshift",
+            'UPDATE "books" SET page_count = page_count + ?, author_count = author_count + ? WHERE book_title = ?',
+            QueryGeneralizationTestMode.BOTH,
+        ),
+        (
+            "SELECT * FROM foo WHERE date = '2021-01-01'",
+            "redshift",
+            "SELECT * FROM foo WHERE date = ?",
+            QueryGeneralizationTestMode.BOTH,
+        ),
+        (
+            "SELECT * FROM books WHERE category IN ('fiction', 'biography', 'fantasy')",
+            "redshift",
+            "SELECT * FROM books WHERE category IN (?)",
+            QueryGeneralizationTestMode.BOTH,
+        ),
+        (
+            textwrap.dedent(
+                """\
+                INSERT INTO MyTable
+                (Column1, Column2, Column3)
+                VALUES
+                ('John', 123, 'Lloyds Office');
+                """
+            ),
+            "mssql",
+            "INSERT INTO MyTable (Column1, Column2, Column3) VALUES (?)",
+            QueryGeneralizationTestMode.BOTH,
+        ),
+        (
             textwrap.dedent(
                 """\
                 /* Copied from https://stackoverflow.com/a/452934/5004662 */
                 INSERT INTO MyTable
-                ( Column1, Column2, Column3 )
+                (Column1, Column2, Column3)
                 VALUES
                 /* multiple value rows */
                 ('John', 123, 'Lloyds Office'),
@@ -100,10 +121,42 @@ def test_query_generalization():
                 ('Miranda', 126, 'Bristol Office');
                 """
             ),
-            dialect="mssql",
+            "mssql",
+            "INSERT INTO MyTable (Column1, Column2, Column3) VALUES (?), (?), (?), (?)",
+            QueryGeneralizationTestMode.FULL,
+        ),
+        # Test table name normalization.
+        # These are only supported with fast normalization.
+        (
+            "SELECT * FROM datahub_community.fivetran_interval_unconstitutional_staging.datahub_slack_mess-staging-480fd5a7-58f4-4cc9-b6fb-87358788efe6",
+            "bigquery",
+            "SELECT * FROM datahub_community.fivetran_interval_unconstitutional_staging.datahub_slack_mess-staging-00000000-0000-0000-0000-000000000000",
+            QueryGeneralizationTestMode.FAST,
+        ),
+        (
+            "SELECT * FROM datahub_community.maggie.commonroom_slack_members_20240315",
+            "bigquery",
+            "SELECT * FROM datahub_community.maggie.commonroom_slack_members_YYYYMMDD",
+            QueryGeneralizationTestMode.FAST,
+        ),
+        (
+            "SELECT COUNT(*) FROM ge_temp_aa91f1fd",
+            "bigquery",
+            "SELECT COUNT(*) FROM ge_temp_abcdefgh",
+            QueryGeneralizationTestMode.FAST,
+        ),
+    ],
+)
+def test_query_generalization(
+    query: str, dialect: str, expected: str, mode: QueryGeneralizationTestMode
+) -> None:
+    if mode in {QueryGeneralizationTestMode.FULL, QueryGeneralizationTestMode.BOTH}:
+        assert generalize_query(query, dialect=dialect) == expected
+    if mode in {QueryGeneralizationTestMode.FAST, QueryGeneralizationTestMode.BOTH}:
+        assert (
+            generalize_query_fast(query, dialect=dialect, change_table_names=True)
+            == expected
         )
-        == "INSERT INTO MyTable (Column1, Column2, Column3) VALUES (?), (?), (?), (?)"
-    )
 
 
 def test_query_fingerprint():

commit 7d2f1b8b62298244c56c0af7900a872077f263d2

fix(ingest): fix redshift query urns + reduce memory usage
Fixes a regression from https://github.com/datahub-project/datahub/pull/10619. In reality, it's not a full regression but rather an existing bug that the above PR exposed.

Also improves memory usage, which should help with https://github.com/datahub-project/datahub/issues/10435.

metadata-ingestion/setup.py
@@ -34,7 +34,7 @@ framework_common = {
     "importlib_metadata>=4.0.0; python_version < '3.10'",
     "docker",
     "expandvars>=0.6.5",
-    "avro-gen3==0.7.12",
+    "avro-gen3==0.7.13",
     # "avro-gen3 @ git+https://github.com/acryldata/avro_gen@master#egg=avro-gen3",
     "avro>=1.11.3,<1.12",
     "python-dateutil>=2.8.0",

metadata-ingestion/src/datahub/ingestion/source/redshift/lineage.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from dataclasses import dataclass, field
 from datetime import datetime
 from enum import Enum
-from typing import Dict, List, Optional, Set, Tuple, Union
+from typing import Dict, Iterable, List, Optional, Set, Tuple, Union
 from urllib.parse import urlparse
 
 import humanfriendly
@@ -661,7 +661,7 @@ class RedshiftLineageExtractor:
         if self.config.resolve_temp_table_in_lineage:
             self._init_temp_table_schema(
                 database=database,
-                temp_tables=self.get_temp_tables(connection=connection),
+                temp_tables=list(self.get_temp_tables(connection=connection)),
             )
 
         populate_calls: List[Tuple[str, LineageCollectorType]] = []
@@ -893,7 +893,7 @@ class RedshiftLineageExtractor:
 
     def get_temp_tables(
         self, connection: redshift_connector.Connection
-    ) -> List[TempTableRow]:
+    ) -> Iterable[TempTableRow]:
         ddl_query: str = self.queries.temp_table_ddl_query(
             start_time=self.config.start_time,
             end_time=self.config.end_time,
@@ -901,15 +901,11 @@ class RedshiftLineageExtractor:
 
         logger.debug(f"Temporary table ddl query = {ddl_query}")
 
-        temp_table_rows: List[TempTableRow] = []
-
         for row in RedshiftDataDictionary.get_temporary_rows(
             conn=connection,
             query=ddl_query,
         ):
-            temp_table_rows.append(row)
-
-        return temp_table_rows
+            yield row
 
     def find_temp_tables(
         self, temp_table_rows: List[TempTableRow], temp_table_names: List[str]

metadata-ingestion/src/datahub/ingestion/source/redshift/lineage_v2.py
@@ -116,7 +116,11 @@ class RedshiftSqlLineageV2:
                     default_schema=self.config.default_schema,
                     session_id=temp_row.session_id,
                     query_timestamp=temp_row.start_time,
-                    is_known_temp_table=True,
+                    # The "temp table" query actually returns all CREATE TABLE statements, even if they
+                    # aren't explicitly a temp table. As such, setting is_known_temp_table=True
+                    # would not be correct. We already have mechanisms to autodetect temp tables,
+                    # so we won't lose anything by not setting it.
+                    is_known_temp_table=False,
                 )
 
         populate_calls: List[Tuple[LineageCollectorType, str, Callable]] = []

metadata-ingestion/src/datahub/ingestion/source/redshift/query.py
@@ -502,7 +502,7 @@ class RedshiftProvisionedQuery(RedshiftCommonQuery):
                     usename as username,
                     ddl,
                     sq.query as query_id,
-                    min(si.starttime) as starttime,
+                    min(si.starttime) as timestamp,
                     ANY_VALUE(pid) as session_id
                 from
                     stl_insert as si

metadata-ingestion/src/datahub/sql_parsing/sql_parsing_aggregator.py
@@ -870,6 +870,7 @@ class SqlParsingAggregator(Closeable):
             models.DatasetLineageTypeClass.TRANSFORMED,
         ]
 
+        # Lower value = higher precedence.
         idx = query_precedence.index(query_type)
         if idx == -1:
             return len(query_precedence)
@@ -885,13 +886,17 @@ class SqlParsingAggregator(Closeable):
         ]
 
         # Sort the queries by highest precedence first, then by latest timestamp.
+        # In case of ties, prefer queries with a known query type.
         # Tricky: by converting the timestamp to a number, we also can ignore the
         # differences between naive and aware datetimes.
         queries = sorted(
+            # Sorted is a stable sort, so in the case of total ties, we want
+            # to prefer the most recently added query.
             reversed(queries),
             key=lambda query: (
                 self._query_type_precedence(query.lineage_type),
                 -(make_ts_millis(query.latest_timestamp) or 0),
+                query.query_type == QueryType.UNKNOWN,
             ),
         )
 

metadata-ingestion/tests/unit/test_redshift_lineage.py
@@ -241,7 +241,7 @@ def test_collapse_temp_lineage():
 
     lineage_extractor._init_temp_table_schema(
         database=lineage_extractor.config.database,
-        temp_tables=lineage_extractor.get_temp_tables(connection=connection),
+        temp_tables=list(lineage_extractor.get_temp_tables(connection=connection)),
     )
 
     lineage_extractor._populate_lineage_map(