2b2859578b6db71abfa734dfc6271a3b5a3b478a
kushagra-apptware <81357546+kushagra-apptware@users.noreply.github.com>
2024-06-13 07:24:09 +0530

    OBS-1060 | Implement unshare and share with multiple instances in V1 (#3390)
    Co-authored-by: Chris Collins <chriscollins3456@gmail.com>

--- datahub-web-react/src/app/entity/shared/containers/profile/sidebar/SharedEntityInfo.tsx
+++ datahub-web-react/src/app/entity/shared/containers/profile/sidebar/SharedEntityInfo.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Typography, Button, Tooltip, message } from 'antd';
+import { Typography, Button, Tooltip, message, Checkbox } from 'antd';
 import { SyncOutlined, LoadingOutlined } from '@ant-design/icons';
 import styled from 'styled-components';
 
@@ -11,152 +11,221 @@ import { sortSharedList } from '../utils';
 import analytics, { EventType } from '../../../../../analytics';
 import { useShareEntityMutation } from '../../../../../../graphql/share.generated';
 import { EntityType, ShareResult } from '../../../../../../types.generated';
+import { StyledLabel } from '../../../../../entityV2/shared/containers/profile/sidebar/shared/styledComponents';
+import StyledButton from '../../../components/styled/StyledButton';
 
-export const StyledContainer = styled.div`		
-		font-size: 11px;
+export const StyledContainer = styled.div`
+    font-size: 11px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
 
-		&:nth-child(n+3) {
-			border-top: 0px !important;
-			padding-top: 0px;
-		}
+    &:nth-child(n + 3) {
+        border-top: 0px !important;
+        padding-top: 0px;
+    }
 `;
 
-export const TitleContainer = styled.div`		
-		display: flex;
-		margin-bottom: 5px;
-		align-items: center;
+const HeaderContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Instances = styled.div``;
+
+export const TitleContainer = styled.div`
+    display: flex;
+    margin-bottom: 5px;
+    align-items: start;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
 `;
 
 export const StyledTitle = styled(Typography.Title)`
-		text-wrap: balance;
-		font-size: 13px !important;
-		margin-bottom: 0px !important;
+    text-wrap: balance;
+    font-size: 13px !important;
+    margin-bottom: 0px !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-		> span {
-			font-weight: normal;
-			color: ${ANTD_GRAY[7]};
-		}
+    > span {
+        font-weight: normal;
+        color: ${ANTD_GRAY[7]};
+    }
+`;
+
+const SharedWith = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
 `;
 
 export const ResyncBytton = styled(Button)`
-		height: 24px;
+    height: 24px;
     width: 24px;
     line-height: 0;
     padding: 0.25rem;
-		margin-left: 0.75rem;
+    margin-left: 0.75rem;
 
-		> .anticon {
-			font-size: 12px;
-		}
+    > .anticon {
+        font-size: 12px;
+    }
 `;
 
 export const ViewLink = styled(Typography.Link)`
-		display: block;
-		margin-bottom: 1rem;
+    display: block;
+    margin-bottom: 1rem;
 `;
 
 interface Props {
-	lastShareResults: ShareResult[];
-	showMore?: boolean;
+    lastShareResults: ShareResult[];
+    selectedInstancesToUnshare?: string[];
+    setSelectedInstancesToUnshare?: React.Dispatch<React.SetStateAction<string[]>>;
+    showMore?: boolean;
+    showSelectMode?: boolean;
 }
 
-export const SharedEntityInfo = ({ lastShareResults, showMore = true }: Props) => {
-	const { entityData, refetch } = useEntityContext();
-	const [shareEntityMutation] = useShareEntityMutation();
-	const [shownCount, setShownCount] = useState(showMore ? 1 : 100);
-	const [entityLoading, setEntityLoading] = useState<string>();
+export const SharedEntityInfo = ({
+    lastShareResults,
+    selectedInstancesToUnshare = [],
+    setSelectedInstancesToUnshare,
+    showMore = true,
+    showSelectMode = false,
+}: Props) => {
+    const { entityData, refetch } = useEntityContext();
+    const [shareEntityMutation] = useShareEntityMutation();
+    const [shownCount, setShownCount] = useState(showMore ? 1 : 100);
+    const [entityLoading, setEntityLoading] = useState<string>();
 
-	// Hide if no share result 
-	if (!lastShareResults || lastShareResults?.length === 0 || !lastShareResults[0]) return null;
+    // Hide if no share result
+    if (!lastShareResults || lastShareResults?.length === 0 || !lastShareResults[0]) return null;
 
-	// TODO (PRD-944): handle partial successes and have no need to filter these anymore
-	// filter results to get only those that have succeeded before
-	const filteredResults = lastShareResults.filter(result => !!result.lastSuccess?.time);
+    // TODO (PRD-944): handle partial successes and have no need to filter these anymore
+    // filter results to get only those that have succeeded before
+    const filteredResults = lastShareResults.filter((result) => !!result.lastSuccess?.time);
 
-	// Sort the list 
-	const sortedResults = sortSharedList(filteredResults);
+    // Sort the list
+    const sortedResults = sortSharedList(filteredResults);
 
-	// Handle Resync
-	const handleSubmit = (connectionUrn: string) => {
-		setEntityLoading(connectionUrn);
+    // Handle Resync
+    const handleSubmit = (connectionUrn: string) => {
+        setEntityLoading(connectionUrn);
 
-		if (entityData?.urn)
-			shareEntityMutation({
-				variables: {
-					input: {
-						entityUrn: entityData.urn,
-						connectionUrn,
-					}
-				}
-			})
-				.then(({ data, errors }) => {
-					message.destroy();
-					if (!errors && data?.shareEntity.succeeded) {
-						analytics.event({
-							type: EventType.SharedEntityEvent,
-							entityType: EntityType.DatahubConnection,
-							entityUrn: entityData.urn || '',
-							connectionUrn,
-						});
-						message.success({
-							content: `Shared entity!`,
-							duration: 3,
-						});
-						refetch();
-						setEntityLoading(undefined);
-					} else {
-						message.error({ content: `Failed to share entity`, duration: 3 });
-					}
-				})
-				.catch((e) => {
-					message.destroy();
-					message.error({ content: `Failed to share entity!: \n ${e.message || ''}`, duration: 3 });
-					setEntityLoading(undefined);
-				});
-	}
+        if (entityData?.urn)
+            shareEntityMutation({
+                variables: {
+                    input: {
+                        entityUrn: entityData.urn,
+                        connectionUrn,
+                    },
+                },
+            })
+                .then(({ data, errors }) => {
+                    message.destroy();
+                    if (!errors && data?.shareEntity.succeeded) {
+                        analytics.event({
+                            type: EventType.SharedEntityEvent,
+                            entityType: EntityType.DatahubConnection,
+                            entityUrn: entityData.urn || '',
+                            connectionUrn,
+                        });
+                        message.success({
+                            content: `Shared entity!`,
+                            duration: 3,
+                        });
+                        refetch();
+                        setEntityLoading(undefined);
+                    } else {
+                        message.error({ content: `Failed to share entity`, duration: 3 });
+                    }
+                })
+                .catch((e) => {
+                    message.destroy();
+                    message.error({ content: `Failed to share entity!: \n ${e.message || ''}`, duration: 3 });
+                    setEntityLoading(undefined);
+                });
+    };
 
+    const handleCheckboxChange = (instanceUrn: string) => {
+        if (instanceUrn && setSelectedInstancesToUnshare) {
+            if (!selectedInstancesToUnshare.includes(instanceUrn)) {
+                setSelectedInstancesToUnshare([...selectedInstancesToUnshare, instanceUrn]);
+            } else {
+                setSelectedInstancesToUnshare(selectedInstancesToUnshare.filter((urn) => urn !== instanceUrn));
+            }
+        }
+    };
 
-	return (
-		<>
-			{sortedResults.slice(0, shownCount).map((result) => {
-				const lastSuccessTime = result.lastSuccess?.time || 0;
-				const name = result.destination.details.name || result.destination.urn;
-				return (
-					<StyledContainer>
-						<TitleContainer>
-							<StyledTitle level={5}>
-								Shared with <span>{name}</span>
-							</StyledTitle>
-							<ResyncBytton
-								type="text"
-								shape="circle"
-								onClick={() => handleSubmit(result.destination.urn)}
-							>
-								{entityLoading === result.destination.urn
-									? (
-										<Tooltip title="Sharing entity…">
-											<LoadingOutlined />
-										</Tooltip>
-									)
-									: (
-										<Tooltip title="Sync entity">
-											<SyncOutlined />
-										</Tooltip>
-									)
-								}
-							</ResyncBytton>
-						</TitleContainer>
-						last synced on {toLocalDateTimeString(lastSuccessTime)}
-					</StyledContainer>
-				)
-			})}
-			{showMore && filteredResults.length > 1 && (
-				<ViewLink
-					onClick={() => setShownCount(shownCount > 1 ? 1 : filteredResults.length)}
-				>
-					{shownCount > 1 ? 'View Less' : `View All (${filteredResults.length})`}
-				</ViewLink>
-			)}
-		</>
-	);
-}
+    return (
+        <>
+            {showSelectMode && (
+                <HeaderContainer>
+                    <StyledLabel>Sharing with</StyledLabel>
+                    {selectedInstancesToUnshare.length > 0 && (
+                        <ButtonContainer>
+                            <StyledButton
+                                type="primary"
+                                onClick={() => setSelectedInstancesToUnshare && setSelectedInstancesToUnshare([])}
+                            >
+                                Clear
+                            </StyledButton>
+                        </ButtonContainer>
+                    )}
+                </HeaderContainer>
+            )}
+            <Instances>
+                {sortedResults.slice(0, shownCount).map((result) => {
+                    const lastSuccessTime = result.lastSuccess?.time || 0;
+                    const name = result.destination.details.name || result.destination.urn;
+                    return (
+                        <StyledContainer>
+                            <TitleContainer>
+                                <SharedWith>
+                                    <StyledTitle level={5}>
+                                        Shared with&nbsp;<span>{name}</span>
+                                    </StyledTitle>
+                                    <ResyncBytton
+                                        type="text"
+                                        shape="circle"
+                                        onClick={() => handleSubmit(result.destination.urn)}
+                                    >
+                                        {entityLoading === result.destination.urn ? (
+                                            <Tooltip title="Sharing entity…">
+                                                <LoadingOutlined />
+                                            </Tooltip>
+                                        ) : (
+                                            <Tooltip title="Sync entity">
+                                                <SyncOutlined />
+                                            </Tooltip>
+                                        )}
+                                    </ResyncBytton>
+                                </SharedWith>
+                                last synced on {toLocalDateTimeString(lastSuccessTime)}
+                            </TitleContainer>
+                            {showSelectMode && (
+                                <Checkbox
+                                    checked={selectedInstancesToUnshare.includes(result.destination.urn)}
+                                    onChange={() => handleCheckboxChange(result.destination.urn)}
+                                />
+                            )}
+                        </StyledContainer>
+                    );
+                })}
+            </Instances>
+            {showMore && filteredResults.length > 1 && (
+                <ViewLink onClick={() => setShownCount(shownCount > 1 ? 1 : filteredResults.length)}>
+                    {shownCount > 1 ? 'View Less' : `View All (${filteredResults.length})`}
+                </ViewLink>
+            )}
+        </>
+    );
+};
--- datahub-web-react/src/app/shared/share/items/MetadataShareItem/ShareModal.tsx
+++ datahub-web-react/src/app/shared/share/items/MetadataShareItem/ShareModal.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useEffect, useMemo } from 'react';
 
-import { Form, Modal, Select, Divider, message } from 'antd';
+import { Form, Modal, Select, Divider, message, Empty, Checkbox, Button } from 'antd';
 import styled from 'styled-components';
 
 import { DataHubConnection, EntityType } from '../../../../../types.generated';
 import { useGetSearchResultsForMultipleQuery } from '../../../../../graphql/search.generated';
 import { PLATFORM_FILTER_NAME } from '../../../../search/utils/constants';
 import { PLATFORM_CONNECTION_URN } from '../../../constants';
-import { useShareEntityMutation } from '../../../../../graphql/share.generated';
+import { useShareEntityMutation, useUnshareEntityMutation } from '../../../../../graphql/share.generated';
 import analytics, { EventType } from '../../../../analytics';
 import { useEntityContext } from '../../../../entity/shared/EntityContext';
 
 import { SharedEntityInfo } from '../../../../entity/shared/containers/profile/sidebar/SharedEntityInfo';
+import { REDESIGN_COLORS } from '../../../../entity/shared/constants';
 
 const ModalTitle = styled.span`
     font-size: 20px;
@@ -22,9 +23,61 @@ const StyledLabel = styled.span`
     font-weight: 600;
 `;
 
+const StyledSelect = styled(Select)`
+    padding-top: 8px;
+
+    .ant-select-selector {
+        border: 1px solid ${REDESIGN_COLORS.BLUE} !important;
+        display: flex;
+        align-items: center;
+    }
+
+    .ant-select-selection-placeholder {
+        color: ${REDESIGN_COLORS.BLUE};
+        font-size: 14px;
+        font-weight: 500;
+    }
+
+    .ant-select-selection-search-input {
+        height: 38px !important;
+    }
+
+    .ant-select-selection-item {
+        display: flex;
+        align-items: center;
+    }
+
+    .ant-select-arrow {
+        svg {
+            font-size: 16px;
+            color: ${REDESIGN_COLORS.BLUE};
+        }
+    }
+`;
+
+const ListOption = styled.div`
+    display: flex;
+    align-items: center;
+    padding: 6px 20px;
+    gap: 10px;
+    cursor: pointer;
+    :hover {
+        background: ${REDESIGN_COLORS.BLUE};
+    }
+`;
+
 export const StyledContainer = styled.div`
     > div:nth-child(n + 2) {
         margin-top: 1.25rem;
+    }
+`;
+
+const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+
+    .ant-btn {
+        font-weight: 500;
     }
 `;
 
@@ -34,12 +87,26 @@ interface Props {
 }
 
 export default function ShareModal({ isModalVisible, closeModal }: Props) {
-    const [selectedInstance, setSelectedInstance] = useState<string>();
+    const [selectedInstances, setSelectedInstances] = useState<string[]>([]);
+    const [selectedInstancesToUnshare, setSelectedInstancesToUnshare] = useState<string[]>([]);
+
     const { urn, entityData, refetch } = useEntityContext();
     const [shareEntityMutation, { loading }] = useShareEntityMutation();
+    const [unshareEntityMutation] = useUnshareEntityMutation();
+
     const [form] = Form.useForm();
 
     const lastShareResults = entityData?.share?.lastShareResults;
+
+    const handleSelectionChange = (instanceUrn: string) => {
+        if (instanceUrn) {
+            if (!selectedInstances.includes(instanceUrn)) {
+                setSelectedInstances([...selectedInstances, instanceUrn]);
+            } else {
+                setSelectedInstances(selectedInstances.filter((instance) => instance !== instanceUrn));
+            }
+        }
+    };
 
     // Execute search
     const { data: searchData, loading: searchLoading } = useGetSearchResultsForMultipleQuery({
@@ -84,18 +151,57 @@ export default function ShareModal({ isModalVisible, closeModal }: Props) {
 
     // Set selected if only 1 option returns in list
     useEffect(() => {
-        if (options.length === 1) setSelectedInstance(options[0].value);
+        if (options.length === 1) setSelectedInstances([options[0].value]);
     }, [options]);
+
+    const handleUnshare = () => {
+        if (selectedInstancesToUnshare) {
+            message.loading('Unsharing entity...');
+            unshareEntityMutation({
+                variables: {
+                    input: {
+                        entityUrn: urn,
+                        connectionUrns: selectedInstancesToUnshare,
+                    },
+                },
+            })
+                .then(({ data, errors }) => {
+                    message.destroy();
+                    if (!errors && data?.unshareEntity.succeeded) {
+                        analytics.event({
+                            type: EventType.UnsharedEntityEvent,
+                            entityType: EntityType.DatahubConnection,
+                            entityUrn: urn,
+                            connectionUrns: selectedInstancesToUnshare,
+                        });
+                        message.success({
+                            content: `Unshared Entity!`,
+                            duration: 3,
+                        });
+                        form.resetFields();
+                        setSelectedInstances([]);
+                        setSelectedInstancesToUnshare([]);
+                        refetch();
+                    } else {
+                        message.error({ content: `Failed to unshare entity`, duration: 3 });
+                    }
+                })
+                .catch((e) => {
+                    message.destroy();
+                    message.error({ content: `Failed to unshare entity!: \n ${e.message || ''}`, duration: 3 });
+                });
+        }
+    };
 
     // Handle the mutation
     const handleSubmit = () => {
-        if (selectedInstance) {
+        if (selectedInstances) {
             message.loading('Sharing entity...');
             shareEntityMutation({
                 variables: {
                     input: {
                         entityUrn: urn,
-                        connectionUrn: selectedInstance,
+                        connectionUrns: selectedInstances,
                     },
                 },
             })
@@ -106,14 +212,14 @@ export default function ShareModal({ isModalVisible, closeModal }: Props) {
                             type: EventType.SharedEntityEvent,
                             entityType: EntityType.DatahubConnection,
                             entityUrn: urn,
-                            connectionUrn: selectedInstance,
+                            connectionUrns: selectedInstances,
                         });
                         message.success({
                             content: `Shared Entity!`,
                             duration: 3,
                         });
                         form.resetFields();
-                        setSelectedInstance(undefined);
+                        setSelectedInstances([]);
                         refetch();
                         closeModal();
                     } else {
@@ -128,13 +234,19 @@ export default function ShareModal({ isModalVisible, closeModal }: Props) {
     };
 
     const isLoading = loading && searchLoading;
-    const isDisabled = isLoading || !selectedInstance;
+    const isDisabled = isLoading || !selectedInstances.length;
     const filteredResults = lastShareResults?.filter((result) => !!result.lastSuccess?.time);
+
+    const handleClose = () => {
+        setSelectedInstances([]);
+        setSelectedInstancesToUnshare([]);
+        closeModal();
+    };
 
     return (
         <Modal
             open={isModalVisible}
-            onCancel={closeModal}
+            onCancel={handleClose}
             onOk={handleSubmit}
             okButtonProps={{ disabled: isDisabled }}
             title={<ModalTitle>Send to another instance</ModalTitle>}
@@ -142,20 +254,52 @@ export default function ShareModal({ isModalVisible, closeModal }: Props) {
             {filteredResults && filteredResults.length > 0 && (
                 <>
                     <StyledContainer>
-                        <SharedEntityInfo lastShareResults={filteredResults} showMore={false} />
+                        <SharedEntityInfo
+                            selectedInstancesToUnshare={selectedInstancesToUnshare}
+                            setSelectedInstancesToUnshare={setSelectedInstancesToUnshare}
+                            lastShareResults={filteredResults}
+                            showMore
+                            showSelectMode
+                        />
+                        {selectedInstancesToUnshare.length > 0 && (
+                            <ButtonContainer>
+                                <Button type="primary" onClick={handleUnshare}>
+                                    Unshare
+                                </Button>
+                            </ButtonContainer>
+                        )}
                     </StyledContainer>
                     <Divider />
                 </>
             )}
             <Form form={form} layout="vertical">
                 <Form.Item label={<StyledLabel>Choose Instance</StyledLabel>}>
-                    <Select
+                    <StyledSelect
+                        labelInValue
                         options={options}
                         loading={isLoading}
-                        value={selectedInstance}
-                        onChange={(option) => setSelectedInstance(option)}
+                        value={selectedInstances}
+                        onSelect={(option: any) => handleSelectionChange(option.value)}
+                        onDeselect={(option: any) => handleSelectionChange(option.value)}
                         showSearch
                         autoFocus
+                        mode="multiple"
+                        dropdownRender={() => (
+                            <div>
+                                {options.length === 0 && (
+                                    <Empty description="No Instances" image={Empty.PRESENTED_IMAGE_SIMPLE} />
+                                )}
+                                {options.map((option) => (
+                                    <ListOption key={option.value} onClick={() => handleSelectionChange(option.value)}>
+                                        <Checkbox
+                                            checked={selectedInstances.includes(option.value)}
+                                            onChange={() => handleSelectionChange(option.value)}
+                                        />
+                                        {option.label}
+                                    </ListOption>
+                                ))}
+                            </div>
+                        )}
                     />
                 </Form.Item>
             </Form>
