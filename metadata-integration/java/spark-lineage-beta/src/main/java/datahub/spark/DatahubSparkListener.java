4f8e18011f1ecfad8fdbd3873cb0374a5132be1f
treff7es <treff7es@gmail.com>
2024-06-17 12:18:53 +0200

    Remove accidentally added import

--- metadata-integration/java/spark-lineage-beta/src/main/java/datahub/spark/DatahubSparkListener.java
+++ metadata-integration/java/spark-lineage-beta/src/main/java/datahub/spark/DatahubSparkListener.java
@@ -26,7 +26,6 @@ import io.openlineage.spark.agent.Versions;
 import io.openlineage.spark.agent.lifecycle.ContextFactory;
 import io.openlineage.spark.agent.util.ScalaConversionUtils;
 import io.openlineage.spark.api.SparkOpenLineageConfig;
-import io.openlineage.spark.api.Vendors;
 import java.net.URISyntaxException;
 import java.time.Instant;
 import java.util.HashMap;
