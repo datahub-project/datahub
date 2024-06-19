2d46952bea7aca430440326382869bab2c9e0008
Kunal-kankriya <127090035+Kunal-kankriya@users.noreply.github.com>
2024-06-19 22:15:23 +0530

    feat/ OBS-887 ported test to V2 UI (#2973)

---
+++ smoke-test/tests/cypress/cypress/e2e/observabilityV2/v2_assertion_results.js
@@ -0,0 +1,24 @@
+const urn =
+  "urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleCypressHdfsAssertionDataset,PROD)";
+const datasetName = "SampleCypressHdfsAssertionDataset";
+
+describe("dataset assertion results test", () => {
+  beforeEach(() => {
+    cy.setIsThemeV2Enabled(true);
+  });
+
+  it("go to dataset with assertions and verify assertion results", () => {
+    //View assertions list
+    cy.login();
+    cy.goToDataset(urn, datasetName);
+    cy.clickOptionWithText("Validation");
+    cy.waitTextVisible("All assertions are passing");
+    //Click on single assertion to open it up
+    cy.clickOptionWithSpecificClass(".ant-table-expanded-row", 0);
+    cy.waitTextVisible("Passing");
+    //View the results graph
+    cy.clickOptionWithText("Passing");
+    cy.waitTextVisible("Last evaluated");
+    cy.waitTextVisible("Last evaluated on");
+  });
+});
