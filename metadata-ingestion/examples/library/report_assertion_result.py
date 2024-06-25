6e1532c11641104412e3d1fafa354c4e2d0af85d
John Joyce <john@Johns-MBP.lan>
2024-06-25 10:07:00 -0700

    Lint fixing based on the real implementation

--- metadata-ingestion/examples/library/report_assertion_result.py
+++ metadata-ingestion/examples/library/report_assertion_result.py
@@ -16,13 +16,13 @@ existing_assertion_urn = "urn:li:assertion:my-unique-assertion-id"
 # Report result for assertion
 res = graph.report_assertion_result(
     urn="urn:li:assertion:<your-new-assertion-id>",  # Replace with your actual assertion URN
-    timestampMillis=int(time.time() * 1000),  # Current Unix timestamp in milliseconds
+    timestamp_millis=int(time.time() * 1000),  # Current Unix timestamp in milliseconds
     type="SUCCESS",  # Can be 'SUCCESS', 'FAILURE', 'ERROR', or 'INIT'
     properties=[{"key": "my_custom_key", "value": "my_custom_value"}],
-    externalUrl="https://my-great-expectations.com/results/1234",  # Optional: URL to the results in the external tool
+    external_url="https://my-great-expectations.com/results/1234",  # Optional: URL to the results in the external tool
     # Uncomment the following section and use if type is 'ERROR'
-    # errorType="UNKNOWN_ERROR",
-    # errorMessage="The assertion failed due to an unknown error"
+    # error_type="UNKNOWN_ERROR",
+    # error_message="The assertion failed due to an unknown error"
 )
 
 if res:
--- metadata-ingestion/examples/library/upsert_custom_assertion.py
+++ metadata-ingestion/examples/library/upsert_custom_assertion.py
@@ -13,16 +13,16 @@ graph = DataHubGraph(
 new_assertion_urn = "urn:li:assertion:my-unique-assertion-id"
 
 # Upsert the assertion
-new_assertion_urn = graph.upsert_custom_assertion(
+res = graph.upsert_custom_assertion(
     urn=new_assertion_urn,  # If the assertion already exists, provide the URN
-    entityUrn="<urn of entity being monitored>",
+    entity_urn="<urn of entity being monitored>",
     type="My Custom Category",  # This categorizes your assertion in DataHub
     description="The description of my external assertion for my dataset",
-    platformUrn="urn:li:dataPlatform:great-expectations",  # OR you can provide 'platformName="My Custom Platform"'
-    fieldPath="field_foo",  # Optional: if you want to associate it with a specific field
-    externalUrl="https://my-monitoring-tool.com/result-for-this-assertion",  # Optional: link to monitoring tool
+    platform_urn="urn:li:dataPlatform:great-expectations",  # OR you can provide 'platformName="My Custom Platform"'
+    field_path="field_foo",  # Optional: if you want to associate it with a specific field
+    external_url="https://my-monitoring-tool.com/result-for-this-assertion",  # Optional: link to monitoring tool
     logic="SELECT * FROM X WHERE Y",  # Optional: custom SQL for the assertion, rendered in the UI
-).get("urn")
+)
 
-if new_assertion_urn is not None:
-    log.info(f"Upserted assertion URN: {new_assertion_urn}")
+if res is not None:
+    log.info(f"Upserted assertion with urn: {new_assertion_urn}")
