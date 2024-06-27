2abe8fca26f88c33eadede67e6f8d6a7a8174387
John Joyce <john@ip-192-168-1-200.us-west-2.compute.internal>
2024-06-27 13:36:38 -0700

    prettier

--- smoke-test/tests/cypress/cypress/e2e/mutations/managed_ingestion.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/managed_ingestion.js
@@ -1,6 +1,9 @@
 function readyToTypeEditor() {
   // Get the first textarea within the Monaco editor and ensure it is visible
-  return cy.get(".monaco-editor textarea:first", { timeout: 10000 }).click({ timeout: 10000 }).focused();
+  return cy
+    .get(".monaco-editor textarea:first", { timeout: 10000 })
+    .click({ timeout: 10000 })
+    .focused();
 }
 
 describe("run managed ingestion", () => {
