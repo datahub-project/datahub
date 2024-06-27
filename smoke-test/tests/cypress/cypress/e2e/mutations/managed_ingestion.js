e5af95dd01b5ca2abdd512f5e043a261e77328cc
John Joyce <john@ip-192-168-1-200.us-west-2.compute.internal>
2024-06-27 12:52:47 -0700

    Add click timeout as well

--- smoke-test/tests/cypress/cypress/e2e/mutations/managed_ingestion.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/managed_ingestion.js
@@ -1,6 +1,6 @@
 function readyToTypeEditor() {
   // Get the first textarea within the Monaco editor and ensure it is visible
-  return cy.get(".monaco-editor textarea:first", { timeout: 10000 }).click().focused();
+  return cy.get(".monaco-editor textarea:first", { timeout: 10000 }).click({ timeout: 10000 }).focused();
 }
 
 describe("run managed ingestion", () => {
