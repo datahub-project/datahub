e6ad4bc4c52be64ad2e74897ffd3e30897c88845
Kunal-kankriya <127090035+Kunal-kankriya@users.noreply.github.com>
2024-07-01 18:55:36 +0530

    fix/ failed test and unskipped view_select.js (#3548)

--- smoke-test/tests/cypress/cypress/e2e/viewV2/v2_view_select.js
+++ smoke-test/tests/cypress/cypress/e2e/viewV2/v2_view_select.js
@@ -1,6 +1,6 @@
 function openViewEditDropDownAndClickId(data_id) {
   cy.get('[data-testid="view-select-item"]').first().trigger("mouseover");
-  cy.get('[data-testid="views-table-dropdown"]').first().trigger("mouseover");
+  cy.get('[data-testid="views-table-dropdown"]').first().click({ force: true });
   cy.clickOptionWithTestId(data_id);
 }
 
@@ -9,9 +9,9 @@ describe("view select", () => {
     cy.setIsThemeV2Enabled(true);
   });
 
-  it.skip("click view select, create view, clear view, make defaults, clear view", () => {
+  it("click view select, create view, clear view, make defaults, clear view", () => {
     cy.login();
-    let randomNumber = Math.floor(Math.random() * 100000);
+    const randomNumber = Math.floor(Math.random() * 100000);
     const viewName = `Test View ${randomNumber}`;
     const newViewName = `New View Name ${randomNumber}`;
 
@@ -23,44 +23,47 @@ describe("view select", () => {
       }
     });
 
-    cy.goToStarSearchList();
+    cy.visit("/");
+    cy.wait(2000);
     cy.handleIntroducePage();
+    cy.goToStarSearchList();
     cy.ensureElementPresent("#browse-v2");
-    //Create a View from the select
+    // Create a View from the select
     cy.clickOptionWithTestId("LanguageIcon");
     cy.ensureElementPresent(".select-view-icon");
     cy.clickOptionWithTestId("AddOutlinedIcon");
     cy.ensureElementPresent('[data-testid="view-name-input"]');
     cy.enterTextInTestId("view-name-input", viewName);
 
-    //Add Column Glossary Term Filter
+    // Add Column Glossary Term Filter
     cy.clickOptionWithText("Add filter");
     cy.contains("Column Term").scrollIntoView();
     cy.clickOptionWithText("Column Term");
     cy.ensureElementPresent('[placeholder="Search for Column Term"]');
     cy.get('[data-testid="search-input"]').last().type("CypressColumnInfoType");
-    cy.get(".ant-menu-title-content").click();
+    cy.get(".ant-checkbox").should("be.visible").click();
     cy.clickOptionWithTestId("update-filters");
     cy.clickOptionWithText("Save");
     cy.ensureTextNotPresent("Save");
     cy.waitTextVisible(viewName);
 
-    //Ensure the View filter has been applied correctly.
+    // Ensure the View filter has been applied correctly.
     cy.waitTextVisible("SampleCypressHdfsDataset");
     cy.waitTextVisible("cypress_logging_events");
     cy.waitTextVisible("of 3 results");
     cy.get("[class*=dataset]").should("have.length", 3);
-    cy.get('[data-testid="CloseIcon"]').last().click();
+    cy.get(".ant-btn.ant-btn-default")
+      .siblings(".close-container")
+      .click({ force: true });
     cy.ensureTextNotPresent("of 3 results");
-    cy.get("[class*=dataset]").should("have.length.gt", 3);
 
-    //Now edit the view
+    // Now edit the view
     cy.clickOptionWithTestId("LanguageIcon");
     cy.ensureElementPresent(".select-view-icon");
     openViewEditDropDownAndClickId("view-dropdown-edit");
     cy.clickOptionWithTestId("view-name-input").clear().type(newViewName);
 
-    //Update the actual filters by adding another filter
+    // Update the actual filters by adding another filter
     cy.clickOptionWithText("Add filter");
     cy.get(".anticon-file-text").eq(0).scrollIntoView().click();
     cy.enterTextInTestId("edit-text-input", "log event");
@@ -79,7 +82,7 @@ describe("view select", () => {
     cy.ensureElementPresent(".select-view-icon");
     openViewEditDropDownAndClickId("view-dropdown-remove-user-default");
 
-    //Now delete the View
+    // Now delete the View
     cy.clickOptionWithId("#v2-search-bar-views");
     cy.get('[data-testid="CloseIcon"]').last().click();
     cy.clickOptionWithTestId("LanguageIcon");
@@ -87,8 +90,9 @@ describe("view select", () => {
     openViewEditDropDownAndClickId("view-dropdown-delete");
     cy.clickOptionWithText("Yes");
 
-    //Ensure that the view was deleted
+    // Ensure that the view was deleted
     cy.goToViewsSettings();
+    cy.waitTextVisible("Permissions");
     cy.ensureTextNotPresent(newViewName);
     cy.ensureTextNotPresent("of 1 result");
   });
