e3104cb9d158c829f5a6eeb72e31b33e12e9b8bf
Sam Black <sam.black@acryl.io>
2024-07-02 07:53:02 -0400

    (devex): Init Husky for UI only commit hooks (#3567)

--- datahub-web-react/src/components/Button/components.ts
+++ datahub-web-react/src/components/Button/components.ts
@@ -5,23 +5,23 @@ import { ButtonProps } from './types';
 import { getButtonStyle } from './utils';
 
 export const ButtonBase = styled.button(
-	// Dynamic styles
-	(props: ButtonProps) => ({ ...getButtonStyle(props as ButtonProps) }),
-	{
-		// Base root styles
-		display: 'flex',
-		alignItems: 'center',
-		gap: spacing.xsm,
-		cursor: 'pointer',
-		transition: `all 0.15s ease`,
+    // Dynamic styles
+    (props: ButtonProps) => ({ ...getButtonStyle(props as ButtonProps) }),
+    {
+        // Base root styles
+        display: 'flex',
+        alignItems: 'center',
+        gap: spacing.xsm,
+        cursor: 'pointer',
+        transition: `all 0.15s ease`,
 
-		// For transitions between focus/active and hover states
-		outlineColor: 'transparent',
-		outlineStyle: 'solid',
+        // For transitions between focus/active and hover states
+        outlineColor: 'transparent',
+        outlineStyle: 'solid',
 
-		// Base Disabled styles
-		'&:disabled': {
-			cursor: 'not-allowed',
-		},
-	},
+        // Base Disabled styles
+        '&:disabled': {
+            cursor: 'not-allowed',
+        },
+    },
 );
