6c42e3f547f71ad99a2cc25e25ce0e981674cc16
ksrinath <ksrinath@users.noreply.github.com>
2024-07-03 10:18:49 +0530

    fix(restli): javadocs

--- metadata-utils/src/main/java/com/linkedin/metadata/restli/NonExceptionHttpErrorResponse.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/restli/NonExceptionHttpErrorResponse.java
@@ -9,8 +9,10 @@ import com.linkedin.restli.server.errors.ServiceError;
  * <i>exception</i> within the server. <br>
  * <br>
  * Restli apparently requires http-error-responses to be represented by {@link
- * RestLiServiceException}; thus this class is used to specify an error <i>response</i> that isn't
- * really an <i>exception</i> (in the context of the server).
+ * RestLiServiceException}; thus, we need this class to specify an error <i>response</i> that isn't
+ * really an <i>exception</i> (in the context of the server). <br>
+ * To highlight the unusual purpose of this exception, the name of this class is also deliberately
+ * unconventional (the class-name doesn't end with "Exception").
  */
 public class NonExceptionHttpErrorResponse extends RestLiServiceException {
 
