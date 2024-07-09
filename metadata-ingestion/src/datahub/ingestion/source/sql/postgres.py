c50930c8f5d10f40a3094145a12c897ddad1954d
pie1nthesky <baggins.thehobbit@gmail.com>
2024-07-09 07:49:16 +0300

    handle exceptions

--- metadata-ingestion/src/datahub/ingestion/source/sql/postgres.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/postgres.py
@@ -278,14 +278,17 @@ class PostgresSource(SQLAlchemySource):
         return f"{current_database}.{regular}"
 
     def add_profile_metadata(self, inspector: Inspector) -> None:
-        with inspector.engine.connect() as conn:
-            for row in conn.execute(
-                """SELECT table_catalog, table_schema, table_name, pg_table_size('"' || table_catalog || '"."' || table_schema || '"."' || table_name || '"') AS table_size FROM information_schema.TABLES"""
-            ):
-                self.profile_metadata_info.dataset_name_to_storage_bytes[
-                    self.get_identifier(
-                        schema=row.table_schema,
-                        entity=row.table_name,
-                        inspector=inspector,
-                    )
-                ] = row.table_size
+        try:
+            with inspector.engine.connect() as conn:
+                for row in conn.execute(
+                    """SELECT table_catalog, table_schema, table_name, pg_table_size('"' || table_catalog || '"."' || table_schema || '"."' || table_name || '"') AS table_size FROM information_schema.TABLES"""
+                ):
+                    self.profile_metadata_info.dataset_name_to_storage_bytes[
+                        self.get_identifier(
+                            schema=row.table_schema,
+                            entity=row.table_name,
+                            inspector=inspector,
+                        )
+                    ] = row.table_size
+        except Exception as e:
+            logger.error(f"failed to fetch profile metadata: {e}")
