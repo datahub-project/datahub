13cfc17a4eea8d96717b103b420d85a1c1df4640
Harshal Sheth <hsheth2@gmail.com>
2024-07-10 21:34:13 -0700

    tweak

--- docs-website/vercel-setup.sh
+++ docs-website/vercel-setup.sh
@@ -11,7 +11,6 @@ set -euxo pipefail
 # for reuse.
 
 yum groupinstall "Development Tools" -y
-yum erase openssl-devel -y
 yum install openssl openssl-devel libffi-devel bzip2-devel wget nodejs -y
 
 wget https://www.python.org/ftp/python/3.10.11/Python-3.10.11.tgz
