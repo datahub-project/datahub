69ee93832d4732064203bd826aa1ba77f2efe409
Patrick Braz <patrickfbraz@poli.ufrj.br>
2024-07-16 17:36:50 -0300

    style: adds typing ignore to fix mypy check

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_helper.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_helper.py
@@ -19,8 +19,9 @@ def unquote_and_decode_unicode_escape_seq(
     # Decode Unicode escape sequences. This avoid issues with encoding
     # This process does not handle unicode from "\U00010000" to "\U0010FFFF"
     while unicode_seq_pattern.search(string):
-        # Get the first Unicode escape sequence
-        unicode_seq = unicode_seq_pattern.search(string).group(0)
+        # Get the first Unicode escape sequence.
+        # mypy: unicode_seq_pattern.search(string) is not None because of the while loop
+        unicode_seq = unicode_seq_pattern.search(string).group(0)  # type: ignore
         # Replace the Unicode escape sequence with the decoded character
         try:
             string = string.replace(
