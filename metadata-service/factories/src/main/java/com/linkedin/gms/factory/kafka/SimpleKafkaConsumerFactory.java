a67079218cc0b26a1a7edb2f1b28422f24a78657
Peng Gao <pgao@slack-corp.com>
2024-07-19 14:19:47 -0700

    Expose more kafka consumer params

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/kafka/SimpleKafkaConsumerFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/kafka/SimpleKafkaConsumerFactory.java
@@ -48,7 +48,15 @@ public class SimpleKafkaConsumerFactory {
     customizedProperties.put(
         ConsumerConfig.MAX_PARTITION_FETCH_BYTES_CONFIG,
         kafkaConfiguration.getConsumer().getMaxPartitionFetchBytes());
-
+    customizedProperties.put(
+        ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG,
+        kafkaConfiguration.getConsumer().getSessionTimeout());
+    customizedProperties.put(
+        ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG,
+        kafkaConfiguration.getConsumer().getMaxPollInterval());
+    customizedProperties.put(
+        ConsumerConfig.MAX_POLL_RECORDS_CONFIG,
+        kafkaConfiguration.getConsumer().getMaxPollRecords());
     ConcurrentKafkaListenerContainerFactory<String, GenericRecord> factory =
         new ConcurrentKafkaListenerContainerFactory<>();
     factory.setContainerCustomizer(new ThreadPoolContainerCustomizer());
