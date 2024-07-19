5288e1f96fd4a88b778126e41259400920794fc1
Harshal Sheth <hsheth2@gmail.com>
2024-07-19 12:38:34 -0700

    feat(ingest/looker): include dashboard urns in browse v2

--- metadata-ingestion/src/datahub/ingestion/source/looker/looker_source.py
+++ metadata-ingestion/src/datahub/ingestion/source/looker/looker_source.py
@@ -670,11 +670,12 @@ class LookerDashboardSource(TestableSource, StatefulIngestionSourceBase):
             )
             chart_snapshot.aspects.append(browse_path)
 
+            dashboard_urn = self.make_dashboard_urn(dashboard)
             browse_path_v2 = BrowsePathsV2Class(
                 path=[
                     BrowsePathEntryClass("Folders"),
                     *self._get_folder_browse_path_v2_entries(dashboard.folder),
-                    BrowsePathEntryClass(id=dashboard.title),
+                    BrowsePathEntryClass(id=dashboard_urn, urn=dashboard_urn),
                 ],
             )
         elif (
@@ -819,7 +820,7 @@ class LookerDashboardSource(TestableSource, StatefulIngestionSourceBase):
 
         return proposals
 
-    def make_dashboard_urn(self, looker_dashboard):
+    def make_dashboard_urn(self, looker_dashboard: LookerDashboard):
         return builder.make_dashboard_urn(
             self.source_config.platform_name, looker_dashboard.get_urn_dashboard_id()
         )
@@ -1202,9 +1203,7 @@ class LookerDashboardSource(TestableSource, StatefulIngestionSourceBase):
     def _make_metrics_dimensions_dashboard_mcp(
         self, dashboard: LookerDashboard
     ) -> MetadataChangeProposalWrapper:
-        dashboard_urn = builder.make_dashboard_urn(
-            self.source_config.platform_name, dashboard.get_urn_dashboard_id()
-        )
+        dashboard_urn = self.make_dashboard_urn(dashboard)
         all_fields = []
         for dashboard_element in dashboard.dashboard_elements:
             all_fields.extend(
