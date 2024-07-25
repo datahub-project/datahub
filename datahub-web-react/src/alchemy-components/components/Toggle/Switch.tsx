4d7f73626b70496d0fedb00812710d7ee1289875
kushagra-apptware <81357546+kushagra-apptware@users.noreply.github.com>
2024-07-25 14:12:04 +0530

    Feat/UX-4 - Switch Component (#3699)
    Co-authored-by: Sam Black <sam.black@acryl.io>

---
+++ datahub-web-react/src/alchemy-components/components/Toggle/Switch.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import { SwitchProps } from './types';
+import { Label, Required, StyledInput, Slider, SwitchContainer, StyledIcon, IconContainer } from './components';
+
+export const switchDefaults: SwitchProps = {
+    label: 'Label',
+    colorScheme: 'violet',
+    size: 'md',
+    isSquare: false,
+    isChecked: false,
+    isDisabled: false,
+    isIntermediate: false,
+    isRequired: false,
+};
+
+export const Switch = ({
+    label = switchDefaults.label,
+    icon, // undefined by default
+    colorScheme = switchDefaults.colorScheme,
+    size = switchDefaults.size,
+    isSquare = switchDefaults.isSquare,
+    isChecked = switchDefaults.isChecked,
+    isDisabled = switchDefaults.isDisabled,
+    isIntermediate = switchDefaults.isIntermediate,
+    isRequired = switchDefaults.isRequired,
+    ...props
+}: SwitchProps) => {
+    const [checked, setChecked] = useState(isChecked);
+
+    useEffect(() => {
+        setChecked(isChecked);
+    }, [isChecked]);
+
+    return (
+        <SwitchContainer>
+            <Label>
+                {label} {isRequired && <Required>*</Required>}
+            </Label>
+            <StyledInput
+                type="checkbox"
+                checked={checked}
+                onChange={() => setChecked(!checked)}
+                customSize={size}
+                disabled={isDisabled}
+                colorScheme={colorScheme || 'violet'}
+                {...props}
+            />
+            <Slider size={size} isSquare={isSquare} isDisabled={isDisabled}>
+                <IconContainer>
+                    {icon && (
+                        <StyledIcon
+                            icon={icon}
+                            color={checked ? colorScheme || 'violet' : 'inherit'}
+                            size={size || 'md'}
+                            checked={checked}
+                        />
+                    )}
+                </IconContainer>
+            </Slider>
+        </SwitchContainer>
+    );
+};
---
+++ datahub-web-react/src/alchemy-components/components/Toggle/components.ts
@@ -0,0 +1,101 @@
+import { ColorOptions, SizeOptions } from '@components/theme/config';
+import { borders, colors, shadows, spacing, transition, typography } from '@components/theme';
+import styled from 'styled-components';
+import { Icon } from '../Icon';
+import {
+    getIconTransformPositionLeft,
+    getIconTransformPositionTop,
+    getInputHeight,
+    getSliderTransformPosition,
+    getToggleSize,
+} from './utils';
+
+export const Label = styled.div({
+    display: 'flex',
+    alignItems: 'flex-start',
+    fontWeight: typography.fontWeights.normal,
+    fontSize: typography.fontSizes.md,
+});
+
+export const SwitchContainer = styled.label({
+    display: 'flex',
+    alignItems: 'center',
+    gap: spacing.sm,
+    cursor: 'pointer',
+});
+
+export const Slider = styled.div<{ size?: SizeOptions; isSquare?: boolean; isDisabled?: boolean }>(
+    ({ size, isSquare, isDisabled }) => ({
+        '&:before': {
+            transition: `${transition.duration.slow} all`,
+            content: '""',
+            position: 'absolute',
+            minWidth: getToggleSize(size || 'md', 'slider'), // sliders width and height must be same
+            minHeight: getToggleSize(size || 'md', 'slider'),
+            borderRadius: !isSquare ? '35px' : '0px',
+            top: '50%',
+            left: spacing.xxsm,
+            transform: 'translate(0, -50%)',
+            backgroundColor: !isDisabled ? colors.white : colors.gray[200],
+        },
+        borderRadius: !isSquare ? '32px' : '0px',
+        minWidth: getToggleSize(size || 'md', 'input'),
+        minHeight: getInputHeight(size || 'md'),
+    }),
+    {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'relative',
+
+        backgroundColor: colors.gray[100],
+        padding: `${parseInt(spacing.xxsm) + 2}px`,
+        transition: `${transition.duration.slow} all`,
+    },
+);
+
+export const Required = styled.span({
+    color: colors.red[500],
+    marginLeft: spacing.xxsm,
+});
+
+export const StyledInput = styled.input<{
+    customSize?: SizeOptions;
+    disabled?: boolean;
+    colorScheme: ColorOptions;
+    checked?: boolean;
+}>`
+    opacity: 0;
+    position: absolute;
+
+    &:checked + ${Slider} {
+        background-color: ${(props) => (!props.disabled ? colors[props.colorScheme][500] : colors.gray[100])};
+
+        &:before {
+            transform: ${({ customSize }) => getSliderTransformPosition(customSize || 'md')};
+        }
+    }
+    &:focus-within + ${Slider} {
+        border-color: ${(props) => (props.checked ? colors[props.colorScheme][200] : 'transparent')};
+        outline: ${(props) => (props.checked ? `${borders['4px']} ${colors[props.colorScheme][200]}` : 'none')};
+        box-shadow: ${(props) => (props.checked ? shadows.xs : 'none')};
+    }
+`;
+
+export const StyledIcon = styled(Icon)<{ checked?: boolean; size: SizeOptions }>(
+    ({ checked, size }) => ({
+        left: getIconTransformPositionLeft(size, checked || false),
+        top: getIconTransformPositionTop(size),
+    }),
+    {
+        transition: `${transition.duration.slow} all`,
+        position: 'absolute',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+);
+
+export const IconContainer = styled.div({
+    position: 'relative',
+});
