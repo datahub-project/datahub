26e1114bfe5be5ba9c414c2daff22a7cb3199c51
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2024-07-26 13:51:22 -0700

    fix(ui/ingest): Support invalid cron jobs; validate cron schedule on upsert (#3755)

--- datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/ingest/source/UpsertIngestionSourceResolverTest.java
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/resolvers/ingest/source/UpsertIngestionSourceResolverTest.java
@@ -7,6 +7,7 @@ import static org.mockito.ArgumentMatchers.anyBoolean;
 import static org.testng.Assert.*;
 
 import com.linkedin.datahub.graphql.QueryContext;
+import com.linkedin.datahub.graphql.exception.DataHubGraphQLException;
 import com.linkedin.datahub.graphql.generated.UpdateIngestionSourceConfigInput;
 import com.linkedin.datahub.graphql.generated.UpdateIngestionSourceInput;
 import com.linkedin.datahub.graphql.generated.UpdateIngestionSourceScheduleInput;
@@ -22,14 +23,17 @@ import org.testng.annotations.Test;
 
 public class UpsertIngestionSourceResolverTest {
 
-  private static final UpdateIngestionSourceInput TEST_INPUT =
-      new UpdateIngestionSourceInput(
-          "Test source",
-          "mysql",
-          "Test source description",
-          new UpdateIngestionSourceScheduleInput("* * * * *", "UTC"),
-          new UpdateIngestionSourceConfigInput(
-              "my test recipe", "0.8.18", "executor id", false, null));
+  private static final UpdateIngestionSourceInput TEST_INPUT = makeInput();
+
+  private static UpdateIngestionSourceInput makeInput() {
+    return new UpdateIngestionSourceInput(
+        "Test source",
+        "mysql",
+        "Test source description",
+        new UpdateIngestionSourceScheduleInput("* * * * *", "UTC"),
+        new UpdateIngestionSourceConfigInput(
+            "my test recipe", "0.8.18", "executor id", false, null));
+  }
 
   @Test
   public void testGetSuccess() throws Exception {
@@ -103,5 +107,55 @@ public class UpsertIngestionSourceResolverTest {
     Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
 
     assertThrows(RuntimeException.class, () -> resolver.get(mockEnv).join());
+  }
+
+  @Test
+  public void testUpsertWithInvalidCron() throws Exception {
+    final UpdateIngestionSourceInput input = makeInput();
+    input.setSchedule(new UpdateIngestionSourceScheduleInput("* * * * 123", "UTC"));
+
+    // Create resolver
+    EntityClient mockClient = Mockito.mock(EntityClient.class);
+    UpsertIngestionSourceResolver resolver = new UpsertIngestionSourceResolver(mockClient);
+
+    // Execute resolver
+    QueryContext mockContext = getMockAllowContext();
+    DataFetchingEnvironment mockEnv = Mockito.mock(DataFetchingEnvironment.class);
+    Mockito.when(mockEnv.getArgument(Mockito.eq("urn")))
+        .thenReturn(TEST_INGESTION_SOURCE_URN.toString());
+    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
+
+    assertThrows(DataHubGraphQLException.class, () -> resolver.get(mockEnv).join());
+    Mockito.verify(mockClient, Mockito.times(0)).ingestProposal(any(), any(), anyBoolean());
+
+    input.setSchedule(new UpdateIngestionSourceScheduleInput("null", "UTC"));
+    assertThrows(DataHubGraphQLException.class, () -> resolver.get(mockEnv).join());
+    Mockito.verify(mockClient, Mockito.times(0)).ingestProposal(any(), any(), anyBoolean());
+  }
+
+  @Test
+  public void testUpsertWithInvalidTimezone() throws Exception {
+    final UpdateIngestionSourceInput input = makeInput();
+    input.setSchedule(new UpdateIngestionSourceScheduleInput("* * * * *", "Invalid"));
+
+    // Create resolver
+    EntityClient mockClient = Mockito.mock(EntityClient.class);
+    UpsertIngestionSourceResolver resolver = new UpsertIngestionSourceResolver(mockClient);
+
+    // Execute resolver
+    QueryContext mockContext = getMockAllowContext();
+    DataFetchingEnvironment mockEnv = Mockito.mock(DataFetchingEnvironment.class);
+    Mockito.when(mockEnv.getArgument(Mockito.eq("urn")))
+        .thenReturn(TEST_INGESTION_SOURCE_URN.toString());
+    Mockito.when(mockEnv.getArgument(Mockito.eq("input"))).thenReturn(input);
+    Mockito.when(mockEnv.getContext()).thenReturn(mockContext);
+
+    assertThrows(DataHubGraphQLException.class, () -> resolver.get(mockEnv).join());
+    Mockito.verify(mockClient, Mockito.times(0)).ingestProposal(any(), any(), anyBoolean());
+
+    input.setSchedule(new UpdateIngestionSourceScheduleInput("* * * * *", "America/Los_Angel"));
+    assertThrows(DataHubGraphQLException.class, () -> resolver.get(mockEnv).join());
+    Mockito.verify(mockClient, Mockito.times(0)).ingestProposal(any(), any(), anyBoolean());
   }
 }
