0598921f4b68e42dced8b73a5ae7bc6633673cc1
John Joyce <john@acryl.io>
2024-07-31 11:13:38 -0700

    Update DescriptionModal.tsx

--- datahub-web-react/src/app/entity/shared/components/legacy/DescriptionModal.tsx
+++ datahub-web-react/src/app/entity/shared/components/legacy/DescriptionModal.tsx
@@ -77,7 +77,7 @@ export default function UpdateDescriptionModal({
         >
             <Form layout="vertical">
                 <Form.Item>
-                    <StyledEditor content={updatedDesc} onChange={setDesc} onKeyDown={handleEditorKeyDown} />
+                    <StyledEditor content={updatedDesc} onChange={setDesc} dataTestId="description-editor" onKeyDown={handleEditorKeyDown} />
                 </Form.Item>
                 {!isAddDesc && description && original && (
                     <OriginalDocumentation label={<FormLabel>Original:</FormLabel>}>
