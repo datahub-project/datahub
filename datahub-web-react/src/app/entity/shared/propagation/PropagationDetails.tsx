53f462be42885cac77fd53b358355cca86b54f68
John Joyce <john@Johns-MBP.lan>
2024-07-30 18:16:06 -0700

    Adding some fluff

--- datahub-web-react/src/app/entity/shared/propagation/PropagationDetails.tsx
+++ datahub-web-react/src/app/entity/shared/propagation/PropagationDetails.tsx
@@ -1,33 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ThunderboltOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
 import { StringMapEntry } from '../../../../types.generated';
 import PropagationEntityLink from './PropagationEntityLink';
 import { usePropagationDetails } from './utils';
+import { ANTD_GRAY } from '../constants';
+import { PropagateThunderbolt, PropagateThunderboltFilled } from './PropagationIcon';
 
 const PopoverWrapper = styled.div`
     display: flex;
     flex-direction: column;
-    gap: 4px;
 `;
 
-const PropagateThunderbolt = styled(ThunderboltOutlined)`
-    color: #8088a3;
+const PopoverTitle = styled.div`
     font-weight: bold;
-    &:hover {
-        color: #4b39bc;
-    }
+    font-size: 14px;
+    padding: 6px 0px;
 `;
 
-const EntityWrapper = styled.span`
-    display: inline-flex;
-    align-items: center;
-    gap: 2px;
-`;
-
-const PropagationPrefix = styled.span`
-    color: black;
+const PopoverDescription = styled.div`
+    max-width: 340px;
+    font-size: 14px;
+    color: ${ANTD_GRAY[7]};
+    display: inline;
+    padding: 0px 0px 4px 0px;
 `;
 
 interface Props {
@@ -46,25 +42,33 @@ export default function PropagationDetails({ sourceDetail }: Props) {
     const popoverContent =
         originEntity || viaEntity ? (
             <PopoverWrapper>
-                Propagated description
-                <br />
-                {viaEntity && (
-                    <EntityWrapper>
-                        <PropagationPrefix>via:</PropagationPrefix>
-                        <PropagationEntityLink entity={viaEntity} />
-                    </EntityWrapper>
-                )}
-                {originEntity && originEntity.urn !== viaEntity?.urn && (
-                    <EntityWrapper>
-                        <PropagationPrefix>origin:</PropagationPrefix>
-                        <PropagationEntityLink entity={originEntity} />
-                    </EntityWrapper>
-                )}
+                <PopoverDescription>
+                    This description was automatically propagated{' '}
+                    {originEntity && originEntity.urn !== viaEntity?.urn && (
+                        <>
+                            from <PropagationEntityLink entity={originEntity} />
+                        </>
+                    )}
+                    {viaEntity && (
+                        <>
+                            via <PropagationEntityLink entity={viaEntity} />
+                        </>
+                    )}
+                </PopoverDescription>
             </PopoverWrapper>
         ) : undefined;
 
     return (
-        <Popover content={popoverContent}>
+        <Popover
+            showArrow={false}
+            title={
+                <PopoverTitle>
+                    <PropagateThunderboltFilled />
+                    Propagated Description
+                </PopoverTitle>
+            }
+            content={popoverContent}
+        >
             <PropagateThunderbolt data-testid="docPropagationIndicator" />
         </Popover>
     );
