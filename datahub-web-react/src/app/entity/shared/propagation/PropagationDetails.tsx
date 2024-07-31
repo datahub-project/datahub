1c86fdd632593006c5f4d9c7ce083e8ea01af99e
John Joyce <john@ip-192-168-1-200.us-west-2.compute.internal>
2024-07-31 13:50:58 -0700

    Adding propagation details fixes

--- datahub-web-react/src/app/entity/shared/propagation/PropagationDetails.tsx
+++ datahub-web-react/src/app/entity/shared/propagation/PropagationDetails.tsx
@@ -4,7 +4,6 @@ import { Popover } from 'antd';
 import { StringMapEntry } from '../../../../types.generated';
 import PropagationEntityLink from './PropagationEntityLink';
 import { usePropagationDetails } from './utils';
-import { ANTD_GRAY } from '../constants';
 import { PropagateThunderbolt, PropagateThunderboltFilled } from './PropagationIcon';
 
 const PopoverWrapper = styled.div`
@@ -16,14 +15,37 @@ const PopoverTitle = styled.div`
     font-weight: bold;
     font-size: 14px;
     padding: 6px 0px;
+    color: #EEECFA;
 `;
 
 const PopoverDescription = styled.div`
     max-width: 340px;
     font-size: 14px;
-    color: ${ANTD_GRAY[7]};
+    color: #EEECFA;
     display: inline;
-    padding: 0px 0px 4px 0px;
+    padding: 0px 0px 8px 0px;
+`;
+
+const PopoverAttributes = styled.div`
+    display: flex; 
+`;
+
+const PopoverAttribute = styled.div`
+    margin-right: 12px;
+    margin-bottom: 4px;
+`;
+
+const PopoverAttributeTitle = styled.div`
+    font-size: 14px;
+    color: #EEECFA;
+    font-weight: bold;
+    margin: 8px 0px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`;
+
+const PopoverDocumentation = styled.a`
+    margin-top: 12px;
 `;
 
 interface Props {
@@ -43,23 +65,27 @@ export default function PropagationDetails({ sourceDetail }: Props) {
         originEntity || viaEntity ? (
             <PopoverWrapper>
                 <PopoverDescription>
-                    This description was automatically propagated{' '}
+                    This description was automatically propagated from an upstream column. <PopoverDocumentation>Learn more</PopoverDocumentation>
+
+                </PopoverDescription>
+                <PopoverAttributes>
                     {originEntity && originEntity.urn !== viaEntity?.urn && (
-                        <>
-                            from <PropagationEntityLink entity={originEntity} />
-                        </>
+                        <PopoverAttribute>
+                            <PopoverAttributeTitle>Origin</PopoverAttributeTitle><PropagationEntityLink entity={originEntity} />
+                        </PopoverAttribute>
                     )}
                     {viaEntity && (
-                        <>
-                            via <PropagationEntityLink entity={viaEntity} />
-                        </>
+                        <PopoverAttribute>
+                            <PopoverAttributeTitle>Via</PopoverAttributeTitle><PropagationEntityLink entity={viaEntity} />
+                        </PopoverAttribute>
                     )}
-                </PopoverDescription>
+                </PopoverAttributes>
             </PopoverWrapper>
         ) : undefined;
 
     return (
         <Popover
+            overlayInnerStyle={{ backgroundColor: "#272D48" }}
             showArrow={false}
             title={
                 <PopoverTitle>
