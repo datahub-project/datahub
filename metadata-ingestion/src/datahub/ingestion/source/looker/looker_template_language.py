da5237a9f02b3515a54990b6e82815bfe7a22447
Siddique Bagwan <siddique.bagwan@acryl.io>
2024-08-07 16:03:54 +0530

    lookml language processing structure

--- metadata-ingestion/src/datahub/ingestion/source/looker/looker_template_language.py
+++ metadata-ingestion/src/datahub/ingestion/source/looker/looker_template_language.py
@@ -1,15 +1,30 @@
 import logging
 import re
-from typing import Any, ClassVar, Dict, Set
+from abc import ABC, abstractmethod
+from typing import Any, ClassVar, Dict, List, Set
 
+from deepmerge import always_merger
 from liquid import Undefined
 from liquid.exceptions import LiquidSyntaxError
 
+from datahub.ingestion.source.looker.looker_constant import (
+    DATAHUB_TRANSFORMED_SQL,
+    DATAHUB_TRANSFORMED_SQL_TABLE_NAME,
+    DERIVED_TABLE,
+    NAME,
+    SQL,
+    SQL_TABLE_NAME,
+    dev,
+    prod,
+)
 from datahub.ingestion.source.looker.looker_liquid_tag import (
     CustomTagException,
     create_template,
 )
-from datahub.ingestion.source.looker.lookml_config import DERIVED_VIEW_PATTERN
+from datahub.ingestion.source.looker.lookml_config import (
+    DERIVED_VIEW_PATTERN,
+    LookMLSourceConfig,
+)
 from datahub.ingestion.source.looker.str_functions import (
     remove_extra_spaces_and_newlines,
 )
@@ -95,44 +110,237 @@ def resolve_liquid_variable(text: str, liquid_variable: Dict[Any, Any]) -> str:
     return text
 
 
-def _complete_incomplete_sql(raw_view: dict, sql: str) -> str:
+class LookMLViewTransformer(ABC):
+    source_config: LookMLSourceConfig
 
-    # Looker supports sql fragments that omit the SELECT and FROM parts of the query
-    # Add those in if we detect that it is missing
-    sql_query: str = sql
+    def __init__(self, source_config: LookMLSourceConfig):
+        self.source_config = source_config
 
-    if not re.search(r"SELECT\s", sql_query, flags=re.I):
-        # add a SELECT clause at the beginning
-        sql_query = f"SELECT {sql}"
-
-    if not re.search(r"FROM\s", sql_query, flags=re.I):
-        # add a FROM clause at the end
-        sql_query = f"{sql_query} FROM {raw_view['name']}"
-
-    # Drop ${ and }
-    return re.sub(DERIVED_VIEW_PATTERN, r"\1", sql_query)
+    @abstractmethod
+    def transform(self, view: dict) -> dict:
+        pass
 
 
-def resolve_liquid_variable_in_view_dict(
-    raw_view: dict, liquid_variable: Dict[Any, Any]
-) -> None:
-    if "views" not in raw_view:
-        return
+class AddTransformedKeys(LookMLViewTransformer):
+    """
+    All transformers work either on sql_table_name or derived_table.sql attribute of views and
+    hence copy them to another key to perform transformation action.
+    """
 
-    for view in raw_view["views"]:
-        if "sql_table_name" in view:
-            view["datahub_transformed_sql_table_name"] = resolve_liquid_variable(
-                text=remove_extra_spaces_and_newlines(view["sql_table_name"]),
-                liquid_variable=liquid_variable,
-            )  # keeping original sql_table_name as is to avoid any visualization issue later
+    def transform(self, view: dict) -> dict:
+        if SQL_TABLE_NAME in view:
+            return {DATAHUB_TRANSFORMED_SQL_TABLE_NAME: view[SQL_TABLE_NAME]}
 
-        if "derived_table" in view and "sql" in view["derived_table"]:
+        if DERIVED_TABLE in view and SQL in view[DERIVED_TABLE]:
+            return {DERIVED_TABLE: {DATAHUB_TRANSFORMED_SQL: view[DERIVED_TABLE][SQL]}}
+
+        return {}
+
+
+class LiquidVariableTransformer(LookMLViewTransformer):
+    """
+    Replace the liquid variables with their values.
+    """
+
+    def transform(self, view: dict) -> dict:
+        if DATAHUB_TRANSFORMED_SQL_TABLE_NAME in view:
+            return {
+                DATAHUB_TRANSFORMED_SQL_TABLE_NAME: resolve_liquid_variable(
+                    text=remove_extra_spaces_and_newlines(
+                        view[DATAHUB_TRANSFORMED_SQL_TABLE_NAME]
+                    ),
+                    liquid_variable=self.source_config.liquid_variable,
+                )
+            }  # keeping original sql_table_name as is to avoid any visualization issue later
+
+        if DERIVED_TABLE in view and DATAHUB_TRANSFORMED_SQL in view[DERIVED_TABLE]:
             # In sql we don't need to remove the extra spaces as sql parser takes care of extra spaces and \n
             # while generating URN from sql
-            view["derived_table"]["datahub_transformed_sql"] = resolve_liquid_variable(
-                text=view["derived_table"]["sql"], liquid_variable=liquid_variable
-            )  # keeping original sql as is, so that on UI sql will be shown same is it is visible on looker portal
+            return {
+                DERIVED_TABLE: {
+                    DATAHUB_TRANSFORMED_SQL: resolve_liquid_variable(
+                        text=view[DERIVED_TABLE][DATAHUB_TRANSFORMED_SQL],
+                        liquid_variable=self.source_config.liquid_variable,
+                    )  # keeping original sql as is, so that on UI sql will be shown same is it is visible on looker
+                    # portal
+                }
+            }
 
-            view["derived_table"]["datahub_transformed_sql"] = _complete_incomplete_sql(
-                raw_view=view, sql=view["derived_table"]["datahub_transformed_sql"]
+        return {}
+
+
+class IncompleteSqlTransformer(LookMLViewTransformer):
+    """
+    lookml view may contain the fragment of sql, however for lineage generation we need a complete sql.
+    IncompleteSqlTransformer will complete the view's sql.
+    """
+
+    def transform(self, view: dict) -> dict:
+        if (
+            DERIVED_TABLE not in view
+            or DATAHUB_TRANSFORMED_SQL not in view[DERIVED_TABLE]
+        ):
+            return {}
+
+        # Looker supports sql fragments that omit the SELECT and FROM parts of the query
+        # Add those in if we detect that it is missing
+        sql_query: str = view[DERIVED_TABLE][DATAHUB_TRANSFORMED_SQL]
+
+        if not re.search(r"SELECT\s", sql_query, flags=re.I):
+            # add a SELECT clause at the beginning
+            sql_query = f"SELECT {sql_query}"
+
+        if not re.search(r"FROM\s", sql_query, flags=re.I):
+            # add a FROM clause at the end
+            sql_query = f"{sql_query} FROM {view['name']}"
+
+        return {"derived_table": {"datahub_transformed_sql": sql_query}}
+
+
+class DropDerivedViewPatternTransformer(LookMLViewTransformer):
+    """
+    drop ${} from datahub_transformed_sql_table_name and  view["derived_table"]["datahub_transformed_sql_table_name"] values.
+
+    Example: transform ${view_name}.sql_table_name to view_name.sql_table_name
+    """
+
+    def transform(self, view: dict) -> dict:
+        if DATAHUB_TRANSFORMED_SQL_TABLE_NAME in view:
+            return {
+                DATAHUB_TRANSFORMED_SQL_TABLE_NAME: re.sub(
+                    DERIVED_VIEW_PATTERN,
+                    r"\1",
+                    view[DATAHUB_TRANSFORMED_SQL_TABLE_NAME],
+                )
+            }
+
+        if DERIVED_TABLE in view and DATAHUB_TRANSFORMED_SQL in view[DERIVED_TABLE]:
+            return {
+                DERIVED_TABLE: {
+                    DATAHUB_TRANSFORMED_SQL: re.sub(
+                        DERIVED_VIEW_PATTERN,
+                        r"\1",
+                        view[DERIVED_TABLE][DATAHUB_TRANSFORMED_SQL],
+                    )
+                }
+            }
+
+        return {}
+
+
+class LookMlIfCommentTransformer(LookMLViewTransformer):
+    """
+    Evaluate the looker -- if -- comments.
+    """
+
+    evaluate_to_true_regx: str
+    remove_if_comment_line_regx: str
+
+    def __init__(self, source_config: LookMLSourceConfig):
+        super().__init__(source_config=source_config)
+
+        # This regx will keep whatever after -- if looker_environment --
+        self.evaluate_to_true_regx = r"-- if {} --".format(
+            self.source_config.looker_environment
+        )
+
+        # It will remove all other lines starts with -- if ... --
+        self.remove_if_comment_line_regx = r"-- if {} --.*?(?=\n|-- if|$)".format(
+            dev if self.source_config.looker_environment.lower() == prod else prod
+        )
+
+    def _apply_regx(self, value: str) -> str:
+        result: str = re.sub(
+            self.remove_if_comment_line_regx, "", value, flags=re.IGNORECASE | re.DOTALL
+        )
+
+        # Remove '-- if prod --' but keep the rest of the line
+        result = re.sub(self.evaluate_to_true_regx, "", result, flags=re.IGNORECASE)
+
+        return result
+
+    def transform(self, view: dict) -> dict:
+        if DATAHUB_TRANSFORMED_SQL_TABLE_NAME in view:
+            return {
+                DATAHUB_TRANSFORMED_SQL_TABLE_NAME: self._apply_regx(
+                    value=view[DATAHUB_TRANSFORMED_SQL_TABLE_NAME]
+                )
+            }
+
+        if DERIVED_TABLE in view and DATAHUB_TRANSFORMED_SQL in view:
+            return {
+                DERIVED_TABLE: {
+                    DATAHUB_TRANSFORMED_SQL: self._apply_regx(
+                        value=view[DERIVED_TABLE][DATAHUB_TRANSFORMED_SQL]
+                    )
+                }
+            }
+
+        return {}
+
+
+class TransformedLookMlView:
+    transformers: List[LookMLViewTransformer]
+    view_dict: dict
+    transformed_dict: dict
+
+    def __init__(
+        self,
+        transformers: List[LookMLViewTransformer],
+        view_dict: dict,
+    ):
+        self.transformers = transformers
+        self.view_dict = view_dict
+        self.transformed_dict = {}
+
+    def view(self) -> dict:
+        if self.transformed_dict:
+            return self.transformed_dict
+
+        self.transformed_dict = {**self.view_dict}
+
+        logger.debug(f"Processing view {self.view_dict[NAME]}")
+
+        for transformer in self.transformers:
+            logger.debug(f"Applying transformer {transformer.__class__.__name__}")
+
+            self.transformed_dict = always_merger.merge(
+                self.transformed_dict, transformer.transform(self.transformed_dict)
             )
+
+        return self.transformed_dict
+
+
+def process_lookml_template_language(
+    source_config: LookMLSourceConfig,
+    view_lkml_file_dict: dict,
+) -> None:
+    if "views" not in view_lkml_file_dict:
+        return
+
+    transformers: List[LookMLViewTransformer] = [
+        AddTransformedKeys(
+            source_config=source_config
+        ),  # Add new key to perform transformation action
+        LookMlIfCommentTransformer(
+            source_config=source_config
+        ),  # First evaluate the -- if -- comments
+        LiquidVariableTransformer(
+            source_config=source_config
+        ),  # Now resolve liquid variables
+        DropDerivedViewPatternTransformer(
+            source_config=source_config
+        ),  # Remove any ${} symbol
+        IncompleteSqlTransformer(
+            source_config=source_config
+        ),  # complete any incomplete sql
+    ]
+
+    transformed_views: List[dict] = []
+
+    for view in view_lkml_file_dict["views"]:
+        transformed_views.append(
+            TransformedLookMlView(transformers=transformers, view_dict=view).view()
+        )
+
+    view_lkml_file_dict["views"] = transformed_views
