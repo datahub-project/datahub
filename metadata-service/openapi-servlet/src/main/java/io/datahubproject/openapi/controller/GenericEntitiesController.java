1eaac69fc29104e2a6478456e5f470d410e653d4
David Leifker <david.leifker@acryl.io>
2024-08-09 12:41:43 -0500

    fix(openapi): fix openapi v2 and v3 docs update

--- metadata-service/openapi-servlet/src/main/java/io/datahubproject/openapi/controller/GenericEntitiesController.java
+++ metadata-service/openapi-servlet/src/main/java/io/datahubproject/openapi/controller/GenericEntitiesController.java
@@ -13,14 +13,11 @@ import com.datahub.authorization.AuthUtil;
 import com.datahub.authorization.AuthorizerChain;
 import com.datahub.util.RecordUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableSet;
 import com.linkedin.common.urn.Urn;
-import com.linkedin.data.ByteString;
 import com.linkedin.data.template.RecordTemplate;
 import com.linkedin.entity.EnvelopedAspect;
-import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.aspect.AspectRetriever;
 import com.linkedin.metadata.aspect.batch.AspectsBatch;
 import com.linkedin.metadata.aspect.batch.ChangeMCP;
@@ -41,7 +38,6 @@ import com.linkedin.metadata.search.ScrollResult;
 import com.linkedin.metadata.search.SearchEntityArray;
 import com.linkedin.metadata.search.SearchService;
 import com.linkedin.metadata.utils.AuditStampUtils;
-import com.linkedin.metadata.utils.GenericRecordUtils;
 import com.linkedin.metadata.utils.SearchUtil;
 import com.linkedin.mxe.SystemMetadata;
 import com.linkedin.util.Pair;
@@ -57,7 +53,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
@@ -726,28 +721,14 @@ public abstract class GenericEntitiesController<
         aspectSpec.getDataTemplateClass(), envelopedAspect.getValue().data());
   }
 
-  protected ChangeMCP toUpsertItem(
+  protected abstract ChangeMCP toUpsertItem(
       @Nonnull AspectRetriever aspectRetriever,
       Urn entityUrn,
       AspectSpec aspectSpec,
       Boolean createIfNotExists,
       String jsonAspect,
       Actor actor)
-      throws JsonProcessingException {
-    JsonNode jsonNode = objectMapper.readTree(jsonAspect);
-    String aspectJson = jsonNode.get("value").toString();
-    return ChangeItemImpl.builder()
-        .urn(entityUrn)
-        .aspectName(aspectSpec.getName())
-        .changeType(Boolean.TRUE.equals(createIfNotExists) ? ChangeType.CREATE : ChangeType.UPSERT)
-        .auditStamp(AuditStampUtils.createAuditStamp(actor.toUrnStr()))
-        .recordTemplate(
-            GenericRecordUtils.deserializeAspect(
-                ByteString.copyString(aspectJson, StandardCharsets.UTF_8),
-                GenericRecordUtils.JSON,
-                aspectSpec))
-        .build(aspectRetriever);
-  }
+      throws URISyntaxException, JsonProcessingException;
 
   protected ChangeMCP toUpsertItem(
       @Nonnull AspectRetriever aspectRetriever,
