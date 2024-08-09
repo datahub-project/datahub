28f3dedd3d32b9c4d5953675af823c9f35a0f9a6
yoonhyejin <0327jane@gmail.com>
2024-08-09 16:40:46 +0900

    fix: add cloud form

---
+++ docs-website/src/pages/cloud/Hero/index.js
@@ -0,0 +1,107 @@
+import React, { useEffect } from 'react';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import styles from "./styles.module.scss";
+import ScrollingCustomers from '../CompanyLogos';
+import './hubspotFormStyles.css';
+
+const Hero = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "//js.hsforms.net/forms/embed/v2.js";
+    script.async = true;
+    script.type = 'text/javascript';
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          region: "na1",
+          portalId: "14552909",
+          formId: "ed2447d6-e6f9-4771-8f77-825b114a9421",
+          target: '#hubspotForm',
+        });
+
+        // Modify placeholders after the form has loaded
+        setTimeout(() => {
+          const emailInput = document.querySelector('#hubspotForm .hs_email .input > input');
+          const firstNameInput = document.querySelector('#hubspotForm .hs_firstname .input > input');
+          const lastNameInput = document.querySelector('#hubspotForm .hs_lastname .input > input');
+          const phoneInput = document.querySelector('#hubspotForm .hs_phone .input > input');
+          const additionalInfoInput = document.querySelector('#hubspotForm .hs_additional_info .input > textarea');
+
+
+          if (emailInput) emailInput.placeholder = 'Company Email';
+          if (firstNameInput) firstNameInput.placeholder = 'First Name';
+          if (lastNameInput) lastNameInput.placeholder = 'Last Name';
+          if (phoneInput) phoneInput.placeholder = 'Phone';
+          if (additionalInfoInput) additionalInfoInput.placeholder = 'How can we help?';
+
+          const selectNoEElement = document.getElementById("number_of_employees-ed2447d6-e6f9-4771-8f77-825b114a9421");
+          if (selectNoEElement) {
+            const disabledOption = selectNoEElement.querySelector('option[disabled]');
+            if (disabledOption) {
+              disabledOption.text = "Select Number of Employees"; 
+              disabledOption.value = ""; 
+            }
+          }
+          const selectfamiliarityElement = document.getElementById("familiarity_with_acryl_datahub-ed2447d6-e6f9-4771-8f77-825b114a9421");
+          if (selectfamiliarityElement) {
+            const disabledOption = selectfamiliarityElement.querySelector('option[disabled]');
+            if (disabledOption) {
+              disabledOption.text = "How familiar are you with DataHub?"; 
+              disabledOption.value = ""; 
+            }
+          }
+
+        }, 1000); // Delay to ensure the form is fully loaded
+      }
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <header className={clsx("hero", styles.hero)}>
+      <div className="container">
+        <div className="hero__content">
+          <div className="row row__padded">
+            <div className="col col--7">
+              <h1 className={clsx("hero__title", styles.hero__title)}>Try DataHub Cloud</h1>
+              <div className={clsx("hero__subtitle", styles.hero__subtitle)}>
+                Introducing DataHub as a Managed Service
+                <div style={{ fontWeight: "500" }}>
+                  with Data Observability and Data Governance built-in.
+                </div>
+              </div>
+              <Link className={clsx(styles.button, "button button--primary button--lg")} to="https://www.acryldata.io/datahub-sign-up?utm_source=datahub&utm_medium=referral&utm_campaign=acryl_signup">
+                Book Demo
+              </Link>
+              <Link className={clsx(styles.button, styles.buttonLightBlue, "button button--secondary button--lg")} to="https://www.acryldata.io/tour">
+                Product Tour
+              </Link>
+              <ScrollingCustomers />
+            </div>
+            <div className="col col--5">
+              <div className={clsx(styles.formContainer)}>
+                <div className={clsx(styles.formContent)}>
+                  <div className={clsx(styles.formHeader)}>
+                    <div className={clsx(styles.formTitle)}>Try it for free!</div>
+                    <div className={clsx(styles.formSubtitle)}>
+                      Get free a demo, and a trial if you’re interested.
+                    </div>
+                  </div>
+                  <div id="hubspotForm" className={styles.hubspotForm}></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Hero;
