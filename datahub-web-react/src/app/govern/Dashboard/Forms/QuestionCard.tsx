0337a967f032fcc99267d1af4eb047120346fa1c
purnimagarg1 <139125209+purnimagarg1@users.noreply.github.com>
2024-08-14 04:55:15 +0530

    feat(ui/form): implement deletion of form question (#3903)

--- datahub-web-react/src/app/govern/Dashboard/Forms/QuestionCard.tsx
+++ datahub-web-react/src/app/govern/Dashboard/Forms/QuestionCard.tsx
@@ -1,6 +1,8 @@
 import { Icon, Text } from '@src/alchemy-components';
-import React from 'react';
+import { ConfirmationModal } from '@src/app/sharedV2/modals/ConfirmationModal';
+import React, { useContext, useState } from 'react';
 import { FormQuestion, questionTypes } from './formUtils';
+import ManageFormContext from './ManageFormContext';
 import { CardContainer, CardData, CardIcons, MandatoryTag } from './styledComponents';
 
 interface Props {
@@ -10,31 +12,57 @@ interface Props {
 }
 
 const QuestionCard = ({ question, setShowQuestionModal, setCurrentQuestion }: Props) => {
+    const { setFormValues } = useContext(ManageFormContext);
+
+    const [showConfirmDelete, setShowConfirmDelete] = useState<boolean>(false);
+
+    const handleDeleteQuestion = () => {
+        setFormValues((prev) => ({
+            ...prev,
+            questions: prev.questions?.filter((ques) => ques !== question),
+        }));
+        setShowConfirmDelete(false);
+    };
+
+    const handleDeleteClose = () => {
+        setShowConfirmDelete(false);
+    };
+
     return (
-        <CardContainer>
-            <CardData width="30%">
-                <Text color="gray" weight="medium">
-                    {question.title}
-                </Text>
-            </CardData>
-            <CardData width="30%">
-                <Text color="gray">
-                    {question.type && questionTypes.find((type) => type.value === question.type)?.label}
-                </Text>
-            </CardData>
-            <CardData width="20%">{question.required ? <MandatoryTag> Mandatory</MandatoryTag> : <></>}</CardData>
-            <CardIcons>
-                <Icon icon="Delete" size="md" />
-                <Icon
-                    icon="Edit"
-                    size="md"
-                    onClick={() => {
-                        setCurrentQuestion(question);
-                        setShowQuestionModal(true);
-                    }}
-                />
-            </CardIcons>
-        </CardContainer>
+        <>
+            <CardContainer>
+                <CardData width="30%">
+                    <Text color="gray" weight="medium">
+                        {question.title}
+                    </Text>
+                </CardData>
+                <CardData width="30%">
+                    <Text color="gray">
+                        {question.type && questionTypes.find((type) => type.value === question.type)?.label}
+                    </Text>
+                </CardData>
+                <CardData width="20%">{question.required ? <MandatoryTag> Mandatory</MandatoryTag> : <></>}</CardData>
+                <CardIcons>
+                    <Icon icon="Delete" size="md" onClick={() => setShowConfirmDelete(true)} />
+                    <Icon
+                        icon="Edit"
+                        size="md"
+                        onClick={() => {
+                            setCurrentQuestion(question);
+                            setShowQuestionModal(true);
+                        }}
+                    />
+                </CardIcons>
+            </CardContainer>
+            <ConfirmationModal
+                isOpen={showConfirmDelete}
+                handleClose={handleDeleteClose}
+                handleConfirm={handleDeleteQuestion}
+                modalTitle="Confirm Delete"
+                modalText="Are you sure you want to delete the question?"
+                isDeleteModal
+            />
+        </>
     );
 };
 
