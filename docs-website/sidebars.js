1c97361308bd25a31f294fcacb4f49414fec4456
Mayuri Nehate <33225191+mayurinehate@users.noreply.github.com>
2024-08-14 19:33:03 +0530

    Update sidebars.js

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -896,7 +896,6 @@ module.exports = {
     //"metadata-ingestion-modules/airflow-plugin/README"
     //"metadata-ingestion-modules/dagster-plugin/README"
     //"metadata-ingestion-modules/gx-plugin/README"
-    // "smoke-test/tests/openapi/README.md"
     // "metadata-ingestion/schedule_docs/datahub", // we can delete this
     // TODO: change the titles of these, removing the "What is..." portion from the sidebar"
     // "docs/what/entity",
