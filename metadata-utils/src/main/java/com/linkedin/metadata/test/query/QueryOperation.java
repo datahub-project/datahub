e4a5ff3970d8996e531376e22a8a7c037ac5bf93
RyanHolstien <RyanHolstien@users.noreply.github.com>
2024-08-15 10:53:43 -0500

    npe warning coverage

--- metadata-utils/src/main/java/com/linkedin/metadata/test/query/QueryOperation.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/test/query/QueryOperation.java
@@ -2,6 +2,7 @@ package com.linkedin.metadata.test.query;
 
 import com.linkedin.metadata.test.definition.literal.Literal;
 import com.linkedin.metadata.test.definition.operator.OperatorType;
+import java.util.Collections;
 import java.util.List;
 import lombok.Data;
 
@@ -9,5 +10,5 @@ import lombok.Data;
 public class QueryOperation {
   private final TestQuery query;
   private final OperatorType operatorType;
-  private List<Literal> values;
+  private List<Literal> values = Collections.emptyList();
 }
