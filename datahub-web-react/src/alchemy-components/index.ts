8a338c3aa62ea4e1c9ff94bd90a72341e6124add
purnimagarg1 <139125209+purnimagarg1@users.noreply.github.com>
2024-08-24 02:24:04 +0530

    feat(ui/component): Table component (#3976)

--- datahub-web-react/src/alchemy-components/index.ts
+++ datahub-web-react/src/alchemy-components/index.ts
@@ -3,11 +3,12 @@ export * from './theme';
 
 // example usage: import { Button } from '@components';
 export * from './components/Button';
+export * from './components/Checkbox';
 export * from './components/Heading';
 export * from './components/Icon';
-export * from './components/Text';
 export * from './components/Input';
-export * from './components/TextArea';
 export * from './components/Select';
 export * from './components/Switch';
-export * from './components/Checkbox';
+export * from './components/Table';
+export * from './components/Text';
+export * from './components/TextArea';
