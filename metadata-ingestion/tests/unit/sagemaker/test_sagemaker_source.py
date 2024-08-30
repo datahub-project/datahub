5d5dcae437de475a7f96aa2b85d7106ba724e4d9
Piotr Skrydalewicz <piotr.skrydalewicz@acryl.io>
2024-08-30 15:21:53 +0200

    Added stubbing of the sts client for debug logging

--- metadata-ingestion/tests/unit/sagemaker/test_sagemaker_source.py
+++ metadata-ingestion/tests/unit/sagemaker/test_sagemaker_source.py
@@ -1,5 +1,6 @@
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
+import boto3
 from botocore.stub import Stubber
 from freezegun import freeze_time
 
@@ -23,6 +24,7 @@ from tests.unit.test_sagemaker_source_stubs import (
     describe_group_response,
     describe_model_response_1,
     describe_model_response_2,
+    get_caller_identity_response,
     get_first_model_package_incoming_response,
     get_model_group_incoming_response,
     get_second_model_package_incoming_response,
@@ -54,179 +56,214 @@ def sagemaker_source() -> SagemakerSource:
 def test_sagemaker_ingest(tmp_path, pytestconfig):
     sagemaker_source_instance = sagemaker_source()
 
-    with Stubber(sagemaker_source_instance.client_factory.get_client()) as sagemaker_stubber:
+    mock_session = MagicMock()
+    mock_client_sagemaker = boto3.client("sagemaker")
+    mock_client_sts = boto3.client("sts")
+
+    def _get_client(client_type, *args, **kwargs):
+        if client_type == "sts":
+            return mock_client_sts
+        elif client_type == "sagemaker":
+            return mock_client_sagemaker
+        raise Exception("client type not handled by the mock")
+
+    mock_session.client.side_effect = _get_client
+
+    sagemaker_stubber = Stubber(mock_client_sagemaker)
+    sts_stubber = Stubber(mock_client_sts)
+
+    sts_stubber.add_response(
+        "get_caller_identity",
+        get_caller_identity_response,
+        {},
+    )
+
+    sts_stubber.add_response(
+        "get_caller_identity",
+        get_caller_identity_response,
+        {},
+    )
+
+    sts_stubber.activate()
+
+    sagemaker_stubber.add_response(
+        "list_actions",
+        list_actions_response,
+        {},
+    )
+
+    sagemaker_stubber.add_response(
+        "list_artifacts",
+        list_artifacts_response,
+        {},
+    )
+
+    sagemaker_stubber.add_response(
+        "list_contexts",
+        list_contexts_response,
+        {},
+    )
+
+    sagemaker_stubber.add_response(
+        "list_associations",
+        list_first_endpoint_incoming_response,
+        {
+            "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-first-endpoint"
+        },
+    )
+    sagemaker_stubber.add_response(
+        "list_associations",
+        list_first_endpoint_outgoing_response,
+        {
+            "SourceArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-first-endpoint"
+        },
+    )
+
+    sagemaker_stubber.add_response(
+        "list_associations",
+        list_second_endpoint_incoming_response,
+        {
+            "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-second-endpoint"
+        },
+    )
+    sagemaker_stubber.add_response(
+        "list_associations",
+        list_second_endpoint_outgoing_response,
+        {
+            "SourceArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-second-endpoint"
+        },
+    )
+
+    sagemaker_stubber.add_response(
+        "list_associations",
+        get_model_group_incoming_response,
+        {
+            "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:context/a-model-package-group-context"
+        },
+    )
+    sagemaker_stubber.add_response(
+        "list_associations",
+        get_first_model_package_incoming_response,
+        {
+            "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:artifact/the-first-model-package-artifact"
+        },
+    )
+    sagemaker_stubber.add_response(
+        "list_associations",
+        get_second_model_package_incoming_response,
+        {
+            "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:artifact/the-second-model-package-artifact"
+        },
+    )
+
+    sagemaker_stubber.add_response(
+        "list_feature_groups",
+        list_feature_groups_response,
+        {},
+    )
+    sagemaker_stubber.add_response(
+        "describe_feature_group",
+        describe_feature_group_response_1,
+        {
+            "FeatureGroupName": "test-2",
+        },
+    )
+    sagemaker_stubber.add_response(
+        "describe_feature_group",
+        describe_feature_group_response_2,
+        {
+            "FeatureGroupName": "test-1",
+        },
+    )
+    sagemaker_stubber.add_response(
+        "describe_feature_group",
+        describe_feature_group_response_3,
+        {
+            "FeatureGroupName": "test",
+        },
+    )
+
+    for job_type in job_types:
+        job = job_stubs[job_type.value]
+
+        job_info = job_type_to_info[job_type]
+
         sagemaker_stubber.add_response(
-            "list_actions",
-            list_actions_response,
+            job_info.list_command,
+            job["list"],
             {},
         )
 
-        sagemaker_stubber.add_response(
-            "list_artifacts",
-            list_artifacts_response,
-            {},
-        )
+    for job_type in job_types:
+        job = job_stubs[job_type.value]
+
+        job_info = job_type_to_info[job_type]
 
         sagemaker_stubber.add_response(
-            "list_contexts",
-            list_contexts_response,
-            {},
+            job_info.describe_command,
+            job["describe"],
+            {job_info.describe_name_key: job["describe_name"]},
         )
 
-        sagemaker_stubber.add_response(
-            "list_associations",
-            list_first_endpoint_incoming_response,
-            {
-                "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-first-endpoint"
-            },
-        )
-        sagemaker_stubber.add_response(
-            "list_associations",
-            list_first_endpoint_outgoing_response,
-            {
-                "SourceArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-first-endpoint"
-            },
-        )
+    sagemaker_stubber.add_response(
+        "list_endpoints",
+        list_endpoints_response,
+        {},
+    )
 
-        sagemaker_stubber.add_response(
-            "list_associations",
-            list_second_endpoint_incoming_response,
-            {
-                "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-second-endpoint"
-            },
-        )
-        sagemaker_stubber.add_response(
-            "list_associations",
-            list_second_endpoint_outgoing_response,
-            {
-                "SourceArn": "arn:aws:sagemaker:us-west-2:123412341234:action/deploy-the-second-endpoint"
-            },
-        )
+    sagemaker_stubber.add_response(
+        "describe_endpoint",
+        describe_endpoint_response_1,
+        {"EndpointName": "the-first-endpoint"},
+    )
 
-        sagemaker_stubber.add_response(
-            "list_associations",
-            get_model_group_incoming_response,
-            {
-                "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:context/a-model-package-group-context"
-            },
-        )
-        sagemaker_stubber.add_response(
-            "list_associations",
-            get_first_model_package_incoming_response,
-            {
-                "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:artifact/the-first-model-package-artifact"
-            },
-        )
-        sagemaker_stubber.add_response(
-            "list_associations",
-            get_second_model_package_incoming_response,
-            {
-                "DestinationArn": "arn:aws:sagemaker:us-west-2:123412341234:artifact/the-second-model-package-artifact"
-            },
-        )
+    sagemaker_stubber.add_response(
+        "describe_endpoint",
+        describe_endpoint_response_2,
+        {"EndpointName": "the-second-endpoint"},
+    )
 
-        sagemaker_stubber.add_response(
-            "list_feature_groups",
-            list_feature_groups_response,
-            {},
-        )
-        sagemaker_stubber.add_response(
-            "describe_feature_group",
-            describe_feature_group_response_1,
-            {
-                "FeatureGroupName": "test-2",
-            },
-        )
-        sagemaker_stubber.add_response(
-            "describe_feature_group",
-            describe_feature_group_response_2,
-            {
-                "FeatureGroupName": "test-1",
-            },
-        )
-        sagemaker_stubber.add_response(
-            "describe_feature_group",
-            describe_feature_group_response_3,
-            {
-                "FeatureGroupName": "test",
-            },
-        )
+    sagemaker_stubber.add_response(
+        "list_model_package_groups",
+        list_groups_response,
+        {},
+    )
 
-        for job_type in job_types:
-            job = job_stubs[job_type.value]
+    sagemaker_stubber.add_response(
+        "describe_model_package_group",
+        describe_group_response,
+        {"ModelPackageGroupName": "a-model-package-group"},
+    )
 
-            job_info = job_type_to_info[job_type]
+    sagemaker_stubber.add_response(
+        "list_models",
+        list_models_response,
+        {},
+    )
 
-            sagemaker_stubber.add_response(
-                job_info.list_command,
-                job["list"],
-                {},
-            )
+    sagemaker_stubber.add_response(
+        "describe_model",
+        describe_model_response_1,
+        {"ModelName": "the-first-model"},
+    )
 
-        for job_type in job_types:
-            job = job_stubs[job_type.value]
+    sagemaker_stubber.add_response(
+        "describe_model",
+        describe_model_response_2,
+        {"ModelName": "the-second-model"},
+    )
 
-            job_info = job_type_to_info[job_type]
+    sagemaker_stubber.activate()
 
-            sagemaker_stubber.add_response(
-                job_info.describe_command,
-                job["describe"],
-                {job_info.describe_name_key: job["describe_name"]},
-            )
-
-        sagemaker_stubber.add_response(
-            "list_endpoints",
-            list_endpoints_response,
-            {},
-        )
-
-        sagemaker_stubber.add_response(
-            "describe_endpoint",
-            describe_endpoint_response_1,
-            {"EndpointName": "the-first-endpoint"},
-        )
-
-        sagemaker_stubber.add_response(
-            "describe_endpoint",
-            describe_endpoint_response_2,
-            {"EndpointName": "the-second-endpoint"},
-        )
-
-        sagemaker_stubber.add_response(
-            "list_model_package_groups",
-            list_groups_response,
-            {},
-        )
-
-        sagemaker_stubber.add_response(
-            "describe_model_package_group",
-            describe_group_response,
-            {"ModelPackageGroupName": "a-model-package-group"},
-        )
-
-        sagemaker_stubber.add_response(
-            "list_models",
-            list_models_response,
-            {},
-        )
-
-        sagemaker_stubber.add_response(
-            "describe_model",
-            describe_model_response_1,
-            {"ModelName": "the-first-model"},
-        )
-
-        sagemaker_stubber.add_response(
-            "describe_model",
-            describe_model_response_2,
-            {"ModelName": "the-second-model"},
-        )
-
+    with patch(
+        "datahub.ingestion.source.aws.aws_common.Session", return_value=mock_session
+    ):
         # Run the test and generate the MCEs
-        mce_objects = [
-            wu.metadata for wu in sagemaker_source_instance.get_workunits()
-        ]
+        mce_objects = [wu.metadata for wu in sagemaker_source_instance.get_workunits()]
         write_metadata_file(tmp_path / "sagemaker_mces.json", mce_objects)
+
+    sagemaker_stubber.assert_no_pending_responses()
+    sts_stubber.assert_no_pending_responses()
 
     # Verify the output.
     test_resources_dir = pytestconfig.rootpath / "tests/unit/sagemaker"
