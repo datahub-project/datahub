05f4fafdc95caffb71389495655441a690b363a5
rohansun <66036924+rohansun@users.noreply.github.com>
2024-09-02 02:29:58 -0400

    Update sql_queries.py

--- metadata-ingestion/src/datahub/ingestion/source/sql_queries.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql_queries.py
@@ -182,7 +182,7 @@ class SqlQueriesSource(Source):
             schema_resolver=self.schema_resolver,
             default_db=self.config.default_db,
             default_schema=self.config.default_schema,
-	     default_dialect=self.config.default_dialect,
+	    default_dialect=self.config.default_dialect,
         )
         if result.debug_info.table_error:
             logger.info(f"Error parsing table lineage, {result.debug_info.table_error}")
