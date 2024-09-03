5ac9ec98ef131c682646df33ae8385f8270acc86
Salazar <lsalazar@ciasc.sc.gov.br>
2024-09-03 18:04:40 -0300

    feat: strategy to translate router tab labels

---
+++ datahub-web-react/src/utils/translation/translation.ts
@@ -0,0 +1,17 @@
+export function translateDisplayNames(t: any, displayName: string | null | undefined): string {
+    if (!displayName) return '';
+    const displayNameFormatted = displayName
+        .trim()
+        .replaceAll(' ', '')
+        .replaceAll(/[^a-zA-Z\s]/g, '')
+        .toLowerCase();
+
+    const FIELD_TO_DISPLAY_NAMES = {
+        groups: t('common.groups'),
+        users: t('common.users')
+    };
+
+    const entries = Object.entries(FIELD_TO_DISPLAY_NAMES);
+    const entry = entries.find(([key]) => key === displayNameFormatted);
+    return entry ? entry[1] : displayName;
+}
