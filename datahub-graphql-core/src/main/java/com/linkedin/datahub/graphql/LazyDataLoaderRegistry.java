0065d4bd3d000f05870324fa2307727a0680f1b3
David Leifker <david.leifker@acryl.io>
2024-09-03 10:45:52 -0500

    feat(graphql): Lazy dataLoaders

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/LazyDataLoaderRegistry.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/LazyDataLoaderRegistry.java
@@ -1,34 +1,53 @@
 package com.linkedin.datahub.graphql;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
 import org.dataloader.DataLoader;
 import org.dataloader.DataLoaderRegistry;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
-import java.util.function.Supplier;
+/**
+ * The purpose of this class is to avoid loading 42+ dataLoaders when many of the graphql queries do
+ * not use all of them.
+ */
+@Slf4j
+public class LazyDataLoaderRegistry extends DataLoaderRegistry {
+  private final QueryContext queryContext;
+  private final Map<String, Function<QueryContext, DataLoader<?, ?>>> dataLoaderSuppliers;
 
-public class LazyDataLoaderManager {
-    private final Map<String, Function<QueryContext, DataLoader<?, ?>>> dataLoaderSuppliers = new ConcurrentHashMap<>();
-    private final DataLoaderRegistry registry;
+  public LazyDataLoaderRegistry(
+      QueryContext queryContext,
+      Map<String, Function<QueryContext, DataLoader<?, ?>>> dataLoaderSuppliers) {
+    super();
+    this.queryContext = queryContext;
+    this.dataLoaderSuppliers = new ConcurrentHashMap<>(dataLoaderSuppliers);
+  }
 
-    public LazyDataLoaderManager(Map<String, Function<QueryContext, DataLoader<?, ?>>> dataLoaderSuppliers) {
-        this.dataLoaderSuppliers.putAll(dataLoaderSuppliers);
-        this.registry = registry;
-    }
+  @Override
+  public <K, V> DataLoader<K, V> getDataLoader(String key) {
+    return super.computeIfAbsent(
+        key,
+        k -> {
+          Function<QueryContext, DataLoader<?, ?>> supplier = dataLoaderSuppliers.get(key);
+          if (supplier == null) {
+            throw new IllegalArgumentException("No DataLoader registered for key: " + key);
+          }
+          return supplier.apply(queryContext);
+        });
+  }
 
-    public <K, V> DataLoader<K, V> getDataLoader(String key) {
-        if (!registry.getKeys().contains(key)) {
-            Function<QueryContext, DataLoader<?, ?>> supplier = dataLoaderSuppliers.get(key);
-            if (supplier == null) {
-                throw new IllegalArgumentException("No DataLoader registered for key: " + key);
-            }
-            registry.register(key, supplier.get());
-        }
-        return registry.getDataLoader(key);
-    }
+  @Override
+  public Set<String> getKeys() {
+    return Stream.concat(dataLoaders.keySet().stream(), dataLoaderSuppliers.keySet().stream())
+        .collect(Collectors.toSet());
+  }
 
-    public void clearAll() {
-        registry.getKeys().forEach(registry::unregister);
-    }
+  @Override
+  public DataLoaderRegistry combine(DataLoaderRegistry registry) {
+    throw new UnsupportedOperationException();
+  }
 }
