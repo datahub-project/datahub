52e6c9b1f96869c87a115b7c6833f4a4c85a881f
david-leifker <114954101+david-leifker@users.noreply.github.com>
2024-09-13 10:04:36 -0500

    Update RestliServletConfig.java

--- metadata-service/war/src/main/java/com/linkedin/gms/servlet/RestliServletConfig.java
+++ metadata-service/war/src/main/java/com/linkedin/gms/servlet/RestliServletConfig.java
@@ -1,12 +1,13 @@
 package com.linkedin.gms.servlet;
 
+import com.linkedin.metadata.entity.AspectDao;
 import com.linkedin.metadata.spring.YamlPropertySourceFactory;
 import com.linkedin.r2.transport.http.server.RAPJakartaServlet;
 import com.linkedin.restli.server.RestliHandlerServlet;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.HttpRequestHandler;
 
@@ -15,10 +16,10 @@ import org.springframework.web.HttpRequestHandler;
 @Configuration
 public class RestliServletConfig {
 
-  // ensure ordering by depending on required service
-  @DependsOn("entityService")
+  // ensure ordering by depending on required entityAspectDao
   @Bean("restliRequestHandler")
-  public HttpRequestHandler restliHandlerServlet(final RAPJakartaServlet r2Servlet) {
+  public HttpRequestHandler restliHandlerServlet(
+      final RAPJakartaServlet r2Servlet, @Qualifier("entityAspectDao") final AspectDao aspectDao) {
     return new RestliHandlerServlet(r2Servlet);
   }
 }
