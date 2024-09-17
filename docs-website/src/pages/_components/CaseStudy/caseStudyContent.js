02f746f5ecbff59caf85ccd0705c3a232e56181c
kushagra-apptware <81357546+kushagra-apptware@users.noreply.github.com>
2024-09-17 22:06:34 +0530

    Add Carousel for Case Study Section (#11396)

---
+++ docs-website/src/pages/_components/CaseStudy/caseStudyContent.js
@@ -0,0 +1,104 @@
+const caseStudyData = [
+  {
+    title: "Make data democratization a reality",
+    description:
+      "Enable everyone in your organization to effortlessly discover trustworthy data, with experiences tailored for each persona.<span></span>Eliminate breaking changes with detailed cross-platform and column-level lineage. <span></span> Build confidence in your data with a comprehensive view of business, operational, and technical context, all in one place.",
+    tag: "Entertainment",
+    backgroundImage:
+      "https://i0.wp.com/picjumbo.com/wp-content/uploads/logo-netflix-free-photo.jpg",
+    image: "./Netflix.png",
+    link: "",
+  },
+  {
+    title: "Build trust in your data",
+    description:
+      "Effortlessly detect data quality issues with automated checks and AI-driven anomaly detection. <span></span> Notify your team where they work when issues arise and keep stakeholders in the loop with centralized incident tracking. <span></span> Spend minutes, not days, resolving issues with detailed lineage, documentation, and ownership information all in one place.",
+    tag: "Finance",
+    backgroundImage:
+      "https://cdn.pmnewsnigeria.com/wp-content/uploads/2021/09/bitcoin-255x175.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "Medical",
+    backgroundImage:
+      "https://static.vecteezy.com/system/resources/thumbnails/002/414/650/small/hand-with-protective-gloves-holding-a-blood-samples-for-covid-test-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "E-Learning",
+    backgroundImage:
+      "https://static.vecteezy.com/system/resources/thumbnails/002/414/650/small/hand-with-protective-gloves-holding-a-blood-samples-for-covid-test-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "E-Learning",
+    backgroundImage:
+      "https://static.vecteezy.com/system/resources/thumbnails/002/414/650/small/hand-with-protective-gloves-holding-a-blood-samples-for-covid-test-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Make data democratization a reality",
+    description:
+      "Enable everyone in your organization to effortlessly discover trustworthy data, with experiences tailored for each persona.<span></span>Eliminate breaking changes with detailed cross-platform and column-level lineage. <span></span> Build confidence in your data with a comprehensive view of business, operational, and technical context, all in one place.",
+    tag: "Entertainment",
+    backgroundImage:
+      "https://i0.wp.com/picjumbo.com/wp-content/uploads/logo-netflix-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Build trust in your data",
+    description:
+      "Effortlessly detect data quality issues with automated checks and AI-driven anomaly detection. <span></span> Notify your team where they work when issues arise and keep stakeholders in the loop with centralized incident tracking. <span></span> Spend minutes, not days, resolving issues with detailed lineage, documentation, and ownership information all in one place.",
+    tag: "Finance",
+    backgroundImage:
+      "https://cdn.pmnewsnigeria.com/wp-content/uploads/2021/09/bitcoin-255x175.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "Medical",
+    backgroundImage:
+      "https://static.vecteezy.com/system/resources/thumbnails/002/414/650/small/hand-with-protective-gloves-holding-a-blood-samples-for-covid-test-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "E-Learning",
+    backgroundImage:
+      "https://static.vecteezy.com/system/resources/thumbnails/002/414/650/small/hand-with-protective-gloves-holding-a-blood-samples-for-covid-test-free-photo.jpg",
+    image: "",
+    link: "",
+  },
+  {
+    title: "Minimize compliance risk, effortlessly",
+    description:
+      "Ensure every data asset is accounted for and responsibility governed by defining and enforcing documentation standards. <span></span> Automate your governance program to automatically classify assets as they evolve over time. <span></span> Minimize redundant, manual work with GenAI documentation, AI-driven classification, smart propagation, and more.",
+    tag: "E-Learning",
+    backgroundImage:
+      "https://cdn.pmnewsnigeria.com/wp-content/uploads/2021/09/bitcoin-255x175.jpg",
+    image: "",
+    link: "",
+  },
+];
+
+export default caseStudyData;
