b50dca21ede63d25f826094a9319eac561badf25
jayacryl <159848059+jayacryl@users.noreply.github.com>
2024-10-03 11:28:30 +0530

    visa text case study

--- docs-website/src/pages/_components/CaseStudy/caseStudyContent.js
+++ docs-website/src/pages/_components/CaseStudy/caseStudyContent.js
@@ -9,16 +9,6 @@ const caseStudyData = [
     image: "https://datahubproject.io/img/logos/companies/netflix.png",
     link: "https://datahubproject.io/adoption-stories/#netflix",
   },
-  // {
-  //   title: "Scaling Data Governance",
-  //   description:
-  //     "How VISA Uses DataHub to Scale Data Governance.",
-  //   tag: "Finance",
-  //   backgroundImage:
-  //     "https://miro.medium.com/v2/resize:fit:2000/format:webp/1*mtC4j8J-jumJKWg8RuR6xQ@2x.png",
-  //   image: "https://datahubproject.io/img/logos/companies/visa.png",
-  //   link: "https://datahubproject.io/adoption-stories/#visa",
-  // },
   {
     title: "Ensuring Data Reliability",
     description:
@@ -39,16 +29,16 @@ const caseStudyData = [
     image: "https://datahubproject.io/img/logos/companies/pinterest.png",
     link: "https://www.youtube.com/watch?v=YoxTg8tQSwg",
   },
-  {
-    title: "Reliable Data Products",
-    description:
-      "How Miro leverages DataHub Cloud to deliver reliable data products.",
-    tag: "Technology",
-    backgroundImage:
-      "https://miro.com/blog/wp-content/uploads/2024/08/header-diagramming-s4-02.png",
-    image: "/img/logos/companies/miro.png",
-    link: "https://miro.com/careers/life-at-miro/tech/data-products-reliability-the-power-of-metadata/",
-  },
+  // {
+  //   title: "Reliable Data Products",
+  //   description:
+  //     "How Miro leverages DataHub Cloud to deliver reliable data products.",
+  //   tag: "Technology",
+  //   backgroundImage:
+  //     "https://miro.com/blog/wp-content/uploads/2024/08/header-diagramming-s4-02.png",
+  //   image: "/img/logos/companies/miro.png",
+  //   link: "https://miro.com/careers/life-at-miro/tech/data-products-reliability-the-power-of-metadata/",
+  // },
   {
     title: "Working with Petabyte Scale Healthcare Data",
     description:
@@ -59,16 +49,16 @@ const caseStudyData = [
     image: "https://datahubproject.io/img/logos/companies/optum.png",
     link: "https://datahubproject.io/adoption-stories/#optum",
   },
-  {
-    title: "Exploring Airtel's shift to a decentralized data architecture.",
-    description:
-      "Discover how DataHub has become integral to Airtel's datamesh architecture.",
-    tag: "Telecommunications",
-    backgroundImage:
-      "https://upload.wikimedia.org/wikipedia/commons/3/3a/Airtel_logo-01.png",
-    image: "/img/logos/companies/airtel.png",
-    link: "https://datahubproject.io/adoption-stories/#airtel",
-  },
+  // {
+  //   title: "Exploring Airtel's shift to a decentralized data architecture.",
+  //   description:
+  //     "Discover how DataHub has become integral to Airtel's datamesh architecture.",
+  //   tag: "Telecommunications",
+  //   backgroundImage:
+  //     "https://upload.wikimedia.org/wikipedia/commons/3/3a/Airtel_logo-01.png",
+  //   image: "/img/logos/companies/airtel.png",
+  //   link: "https://datahubproject.io/adoption-stories/#airtel",
+  // },
   {
     title: "Leveling Up Data Management.",
     description:
@@ -78,6 +68,16 @@ const caseStudyData = [
       "https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Zynga.svg/1200px-Zynga.svg.png",
     image: "/img/logos/companies/zynga.png",
     link: "https://datahubproject.io/adoption-stories/#zynga",
+  },
+  {
+    title: "Scaling Data Governance",
+    description:
+      "How VISA Uses DataHub to Scale Data Governance.",
+    tag: "Finance",
+    backgroundImage:
+      "https://miro.medium.com/v2/resize:fit:2000/format:webp/1*mtC4j8J-jumJKWg8RuR6xQ@2x.png",
+    image: "/img/logos/companies/visa_text.png",
+    link: "https://datahubproject.io/adoption-stories/#visa",
   },
   {
     title: "And many more...",
