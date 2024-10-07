datahub-project/datahub

commit 7bdb7bbab2b5f0e51682f61cf00967012beadf58

fix(propagation): show last modified propagated documentation (#11498)

datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/getFieldDescriptionDetails.ts
@@ -7,19 +7,29 @@ interface Props {
 }
 
 export function getFieldDescriptionDetails({ schemaFieldEntity, editableFieldInfo, defaultDescription }: Props) {
-    const documentation = schemaFieldEntity?.documentation?.documentations?.[0];
-    const isUsingDocumentationAspect = !editableFieldInfo?.description && !!documentation;
+    const documentations = schemaFieldEntity?.documentation?.documentations;
+    let latestDocumentation: typeof documentations[0] | undefined;
+
+    if (documentations && documentations.length > 0) {
+        latestDocumentation = documentations.reduce((latest, current) => {
+            const latestTime = latest.attribution?.time || 0;
+            const currentTime = current.attribution?.time || 0;
+            return currentTime > latestTime ? current : latest;
+        });
+    }
+
+    const isUsingDocumentationAspect = !editableFieldInfo?.description && !!latestDocumentation;
     const isPropagated =
         isUsingDocumentationAspect &&
-        !!documentation?.attribution?.sourceDetail?.find(
+        !!latestDocumentation?.attribution?.sourceDetail?.find(
             (mapEntry) => mapEntry.key === 'propagated' && mapEntry.value === 'true',
         );
 
     const displayedDescription =
-        editableFieldInfo?.description || documentation?.documentation || defaultDescription || '';
+        editableFieldInfo?.description || latestDocumentation?.documentation || defaultDescription || '';
 
-    const sourceDetail = documentation?.attribution?.sourceDetail;
-    const propagatedDescription = documentation?.documentation;
+    const sourceDetail = latestDocumentation?.attribution?.sourceDetail;
+    const propagatedDescription = latestDocumentation?.documentation;
 
     return { displayedDescription, isPropagated, sourceDetail, propagatedDescription };
 }

commit ed05566e554ee7ea94783dd0fc7489838752fc95

fix(lint): fix yarn lint breakage from #11498

datahub-web-react/src/app/entity/shared/tabs/Dataset/Schema/utils/getFieldDescriptionDetails.ts
@@ -8,7 +8,7 @@ interface Props {
 
 export function getFieldDescriptionDetails({ schemaFieldEntity, editableFieldInfo, defaultDescription }: Props) {
     const documentations = schemaFieldEntity?.documentation?.documentations;
-    let latestDocumentation: typeof documentations[0] | undefined;
+    let latestDocumentation: (typeof documentations)[0] | undefined;
 
     if (documentations && documentations.length > 0) {
         latestDocumentation = documentations.reduce((latest, current) => {