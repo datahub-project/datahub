8900fa391e18a9a8cea59f2711203cf362dcc97b
yoonhyejin <0327jane@gmail.com>
2024-10-08 13:59:35 +0900

    fix: fix case study design

--- docs-website/src/pages/solutions/_components/CaseStudy/index.js
+++ docs-website/src/pages/solutions/_components/CaseStudy/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./case-study.module.scss";
 import clsx from "clsx";
+import Link from '@docusaurus/Link'
 
 const CaseStudy = ({ caseStudyContent }) => {
   const { title, items } = caseStudyContent;
@@ -11,22 +12,17 @@ const CaseStudy = ({ caseStudyContent }) => {
           {title}
         </div>
 
-        <div className={clsx(styles.card_row, "row")}>
+        <div className={clsx(styles.card_row)}>
           <div className={styles.card_row_wrapper}>
             {items.map((caseStudy) => (
-              <div className={styles.card} key={caseStudy.link}>
-                <a
-                  className={styles.cardLink}
-                  href={caseStudy.link}
-                >
-                  <div className={styles.card_image}>
+              <Link className={clsx(styles.card, "row")} key={caseStudy.link} to={caseStudy.link}>
+                  <div className={clsx(styles.card_image, "col col--3")}>
                     <img src={caseStudy.imgSrc} alt={caseStudy.alt} />
                   </div>
-                  <div className={styles.card_heading_div}>
+                  <div className={clsx(styles.card_heading_div, "col col--8")}>
                     <div className={styles.card_heading}>{caseStudy.title}</div>
                   </div>
-                </a>
-              </div>
+              </Link>
             ))}
           </div>
         </div>
--- docs-website/src/pages/solutions/discovery/_content/discoveryCaseStudyContent.js
+++ docs-website/src/pages/solutions/discovery/_content/discoveryCaseStudyContent.js
@@ -1,22 +1,23 @@
 const caseStudyContent = {
       title: "See how industry leaders are using DataHub for Discovery today.",
+      backgroundColor: "#F3F3F6",
       items: [
         {
-          imgSrc: "/img/solutions/discovery/hero-discovery.png",
+          imgSrc: "/img/solutions/logo-notion.png",
           title: "How Notion Uses Acryl Data to Ensure Data Reliability.",
-          link: "/solutions/discovery/case-studies/notion",
+          link: "/adoption-stories/#notion",
           alt: "notion"
         },
         {
-          imgSrc: "/img/solutions/discovery/hero-discovery.png",
+          imgSrc: "/img/solutions/logo-myob.png",
           title: "How MYOB Improved Data Reliability for dbt and Snowflake.",
-          link: "/solutions/discovery/case-studies/pinterest",
+          link: "/adoption-stories/#myob",
           alt: "MYOB",
         },
         {
-          imgSrc: "/img/solutions/discovery/hero-discovery.png",
+          imgSrc: "/img/solutions/logo-dpg-media.png",
           title: "How Acryl Data Helped DPG Media Save 25% Per Mo in Snowflake.",
-          link: "/solutions/discovery/case-studies/netflix",
+          link: "/adoption-stories/#dpg-media",
           alt: "DPG Media",
         }
       ]
