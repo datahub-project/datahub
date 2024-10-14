bd795312854069578547cb25701c645effaadaff
David Leifker <david.leifker@acryl.io>
2024-10-14 17:38:45 -0500

    feat(validation): Ingest and schema validator

--- entity-registry/src/main/java/com/linkedin/metadata/aspect/validation/FieldPathValidator.java
+++ entity-registry/src/main/java/com/linkedin/metadata/aspect/validation/FieldPathValidator.java
@@ -2,19 +2,19 @@ package com.linkedin.metadata.aspect.validation;
 
 import static com.linkedin.metadata.Constants.*;
 
-import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.aspect.RetrieverContext;
 import com.linkedin.metadata.aspect.batch.BatchItem;
 import com.linkedin.metadata.aspect.batch.ChangeMCP;
 import com.linkedin.metadata.aspect.plugins.config.AspectPluginConfig;
 import com.linkedin.metadata.aspect.plugins.validation.AspectPayloadValidator;
 import com.linkedin.metadata.aspect.plugins.validation.AspectValidationException;
+import com.linkedin.metadata.aspect.plugins.validation.ValidationExceptionCollection;
 import com.linkedin.schema.EditableSchemaFieldInfo;
 import com.linkedin.schema.EditableSchemaMetadata;
 import com.linkedin.schema.SchemaField;
 import com.linkedin.schema.SchemaMetadata;
 import java.util.Collection;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import lombok.Getter;
@@ -22,8 +22,8 @@ import lombok.Setter;
 import lombok.experimental.Accessors;
 
 /**
- * Validates the Schema Field Path specification, specifically that all field IDs must be unique
- * across all fields within a schema.
+ * 1. Validates the Schema Field Path specification, specifically that all field IDs must be unique
+ * across all fields within a schema. 2. Validates that the field path id is not empty.
  *
  * @see <a href="https://datahubproject.io/docs/advanced/field-path-spec-v2/#requirements">Field
  *     Path V2 docs</a>
@@ -34,82 +34,83 @@ import lombok.experimental.Accessors;
 public class FieldPathValidator extends AspectPayloadValidator {
   @Nonnull private AspectPluginConfig config;
 
-  /**
-   * Prevent any MCP for SchemaMetadata where field ids are duplicated (except for MCPs with {@link
-   * ChangeType#DELETE} and {@link ChangeType#PATCH}, the latter gets handled pre-commit to the DB).
-   */
+  /** Prevent any MCP for SchemaMetadata where field ids are duplicated. */
   @Override
   protected Stream<AspectValidationException> validateProposedAspects(
       @Nonnull Collection<? extends BatchItem> mcpItems,
       @Nonnull RetrieverContext retrieverContext) {
-    return mcpItems.stream()
-        .filter(
-            i ->
-                !ChangeType.DELETE.equals(i.getChangeType())
-                    && !ChangeType.PATCH.equals(i.getChangeType()))
-        .filter(
-            i ->
-                i.getAspectName().equals(SCHEMA_METADATA_ASPECT_NAME)
-                    || i.getAspectName().equals(EDITABLE_SCHEMA_METADATA_ASPECT_NAME))
-        .map(
-            i -> {
-              if (i.getAspectName().equals(SCHEMA_METADATA_ASPECT_NAME)) {
-                return processSchemaMetadataAspect(i);
-              } else {
-                return processEditableSchemaMetadataAspect(i);
-              }
-            })
-        .filter(Objects::nonNull);
+
+    ValidationExceptionCollection exceptions = ValidationExceptionCollection.newCollection();
+
+    mcpItems.forEach(
+        i -> {
+          if (i.getAspectName().equals(SCHEMA_METADATA_ASPECT_NAME)) {
+            processSchemaMetadataAspect(i, exceptions);
+          } else {
+            processEditableSchemaMetadataAspect(i, exceptions);
+          }
+        });
+
+    return exceptions.streamAllExceptions();
   }
 
   @Override
   protected Stream<AspectValidationException> validatePreCommitAspects(
       @Nonnull Collection<ChangeMCP> changeMCPs, @Nonnull RetrieverContext retrieverContext) {
-    return changeMCPs.stream()
-        .filter(i -> ChangeType.PATCH.equals(i.getChangeType()))
-        .filter(
-            i ->
-                i.getAspectName().equals(SCHEMA_METADATA_ASPECT_NAME)
-                    || i.getAspectName().equals(EDITABLE_SCHEMA_METADATA_ASPECT_NAME))
-        .map(
-            i -> {
-              if (i.getAspectName().equals(SCHEMA_METADATA_ASPECT_NAME)) {
-                return processSchemaMetadataAspect(i);
-              } else {
-                return processEditableSchemaMetadataAspect(i);
-              }
-            })
-        .filter(Objects::nonNull);
+    return Stream.of();
   }
 
-  private static AspectValidationException processEditableSchemaMetadataAspect(BatchItem i) {
+  private static void processEditableSchemaMetadataAspect(
+      BatchItem i, ValidationExceptionCollection exceptions) {
     final EditableSchemaMetadata schemaMetadata = i.getAspect(EditableSchemaMetadata.class);
     final long uniquePaths =
-        schemaMetadata.getEditableSchemaFieldInfo().stream()
-            .map(EditableSchemaFieldInfo::getFieldPath)
-            .distinct()
-            .count();
+        validateAndCount(
+            i,
+            schemaMetadata.getEditableSchemaFieldInfo().stream()
+                .map(EditableSchemaFieldInfo::getFieldPath),
+            exceptions);
+
     if (uniquePaths != schemaMetadata.getEditableSchemaFieldInfo().size()) {
-      return AspectValidationException.forItem(
+      exceptions.addException(
           i,
           String.format(
               "Cannot perform %s action on proposal. EditableSchemaMetadata aspect has duplicated field paths",
               i.getChangeType()));
     }
-    return null;
   }
 
-  private static AspectValidationException processSchemaMetadataAspect(BatchItem i) {
+  private static void processSchemaMetadataAspect(
+      BatchItem i, ValidationExceptionCollection exceptions) {
     final SchemaMetadata schemaMetadata = i.getAspect(SchemaMetadata.class);
     final long uniquePaths =
-        schemaMetadata.getFields().stream().map(SchemaField::getFieldPath).distinct().count();
+        validateAndCount(
+            i, schemaMetadata.getFields().stream().map(SchemaField::getFieldPath), exceptions);
+
     if (uniquePaths != schemaMetadata.getFields().size()) {
-      return AspectValidationException.forItem(
+      exceptions.addException(
           i,
           String.format(
               "Cannot perform %s action on proposal. SchemaMetadata aspect has duplicated field paths",
               i.getChangeType()));
     }
-    return null;
+  }
+
+  private static long validateAndCount(
+      BatchItem i, Stream<String> fieldPaths, ValidationExceptionCollection exceptions) {
+    return fieldPaths
+        .distinct()
+        // inspect the stream of fieldPath validation errors since we're already iterating
+        .peek(
+            fieldPath ->
+                validateFieldPath(fieldPath)
+                    .ifPresent(message -> exceptions.addException(i, message)))
+        .count();
+  }
+
+  private static Optional<String> validateFieldPath(String fieldPath) {
+    if (fieldPath == null || fieldPath.isEmpty()) {
+      return Optional.of("SchemaMetadata aspect has empty field path.");
+    }
+    return Optional.empty();
   }
 }
--- entity-registry/src/test/java/com/linkedin/metadata/aspect/validators/FieldPathValidatorTest.java
+++ entity-registry/src/test/java/com/linkedin/metadata/aspect/validators/FieldPathValidatorTest.java
@@ -4,16 +4,14 @@ import static com.linkedin.metadata.Constants.*;
 import static org.mockito.Mockito.*;
 import static org.testng.Assert.*;
 
-import com.google.common.collect.ImmutableList;
-import com.linkedin.common.UrnArray;
 import com.linkedin.common.urn.DatasetUrn;
 import com.linkedin.common.urn.UrnUtils;
-import com.linkedin.domain.Domains;
 import com.linkedin.events.metadata.ChangeType;
 import com.linkedin.metadata.aspect.AspectRetriever;
 import com.linkedin.metadata.aspect.GraphRetriever;
 import com.linkedin.metadata.aspect.RetrieverContext;
 import com.linkedin.metadata.aspect.plugins.config.AspectPluginConfig;
+import com.linkedin.metadata.aspect.plugins.validation.AspectValidationException;
 import com.linkedin.metadata.aspect.validation.CreateIfNotExistsValidator;
 import com.linkedin.metadata.aspect.validation.FieldPathValidator;
 import com.linkedin.metadata.models.registry.EntityRegistry;
@@ -34,6 +32,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -51,15 +50,21 @@ public class FieldPathValidatorTest {
           .build();
   private EntityRegistry entityRegistry;
   private RetrieverContext mockRetrieverContext;
-  private DatasetUrn testDatasetUrn;
+  private static final DatasetUrn TEST_DATASET_URN;
   private final FieldPathValidator test = new FieldPathValidator().setConfig(validatorConfig);
 
-  @BeforeTest
-  public void init() throws URISyntaxException {
-    testDatasetUrn =
-        DatasetUrn.createFromUrn(
-            UrnUtils.getUrn("urn:li:dataset:(urn:li:dataPlatform:hive,test,PROD)"));
+  static {
+    try {
+      TEST_DATASET_URN =
+          DatasetUrn.createFromUrn(
+              UrnUtils.getUrn("urn:li:dataset:(urn:li:dataPlatform:hive,test,PROD)"));
+    } catch (URISyntaxException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
+  @BeforeTest
+  public void init() {
     entityRegistry = new TestEntityRegistry();
     AspectRetriever mockAspectRetriever = mock(AspectRetriever.class);
     when(mockAspectRetriever.getEntityRegistry()).thenReturn(entityRegistry);
@@ -70,29 +75,6 @@ public class FieldPathValidatorTest {
   }
 
   @Test
-  public void testValidateIncorrectAspect() {
-    final Domains domains =
-        new Domains()
-            .setDomains(new UrnArray(ImmutableList.of(UrnUtils.getUrn("urn:li:domain:123"))));
-    assertEquals(
-        test.validateProposed(
-                Set.of(
-                    TestMCP.builder()
-                        .changeType(ChangeType.UPSERT)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
-                        .aspectSpec(
-                            entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
-                                .getAspectSpec(DOMAINS_ASPECT_NAME))
-                        .recordTemplate(domains)
-                        .build()),
-                mockRetrieverContext)
-            .count(),
-        0);
-  }
-
-  @Test
   public void testValidateNonDuplicatedSchemaFieldPath() {
     final SchemaMetadata schema = getMockSchemaMetadataAspect(false);
     assertEquals(
@@ -100,11 +82,11 @@ public class FieldPathValidatorTest {
                 Set.of(
                     TestMCP.builder()
                         .changeType(ChangeType.UPSERT)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
+                        .urn(TEST_DATASET_URN)
+                        .entitySpec(entityRegistry.getEntitySpec(TEST_DATASET_URN.getEntityType()))
                         .aspectSpec(
                             entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
+                                .getEntitySpec(TEST_DATASET_URN.getEntityType())
                                 .getAspectSpec(SCHEMA_METADATA_ASPECT_NAME))
                         .recordTemplate(schema)
                         .build()),
@@ -122,39 +104,17 @@ public class FieldPathValidatorTest {
                 Set.of(
                     TestMCP.builder()
                         .changeType(ChangeType.UPSERT)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
+                        .urn(TEST_DATASET_URN)
+                        .entitySpec(entityRegistry.getEntitySpec(TEST_DATASET_URN.getEntityType()))
                         .aspectSpec(
                             entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
+                                .getEntitySpec(TEST_DATASET_URN.getEntityType())
                                 .getAspectSpec(SCHEMA_METADATA_ASPECT_NAME))
                         .recordTemplate(schema)
                         .build()),
                 mockRetrieverContext)
             .count(),
         1);
-  }
-
-  @Test
-  public void testValidateDeleteDuplicatedSchemaFieldPath() {
-    final SchemaMetadata schema = getMockSchemaMetadataAspect(true);
-
-    assertEquals(
-        test.validateProposed(
-                Set.of(
-                    TestMCP.builder()
-                        .changeType(ChangeType.DELETE)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
-                        .aspectSpec(
-                            entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
-                                .getAspectSpec(SCHEMA_METADATA_ASPECT_NAME))
-                        .recordTemplate(schema)
-                        .build()),
-                mockRetrieverContext)
-            .count(),
-        0);
   }
 
   @Test
@@ -165,11 +125,11 @@ public class FieldPathValidatorTest {
                 Set.of(
                     TestMCP.builder()
                         .changeType(ChangeType.UPSERT)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
+                        .urn(TEST_DATASET_URN)
+                        .entitySpec(entityRegistry.getEntitySpec(TEST_DATASET_URN.getEntityType()))
                         .aspectSpec(
                             entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
+                                .getEntitySpec(TEST_DATASET_URN.getEntityType())
                                 .getAspectSpec(EDITABLE_SCHEMA_METADATA_ASPECT_NAME))
                         .recordTemplate(schema)
                         .build()),
@@ -187,11 +147,11 @@ public class FieldPathValidatorTest {
                 Set.of(
                     TestMCP.builder()
                         .changeType(ChangeType.UPSERT)
-                        .urn(testDatasetUrn)
-                        .entitySpec(entityRegistry.getEntitySpec(testDatasetUrn.getEntityType()))
+                        .urn(TEST_DATASET_URN)
+                        .entitySpec(entityRegistry.getEntitySpec(TEST_DATASET_URN.getEntityType()))
                         .aspectSpec(
                             entityRegistry
-                                .getEntitySpec(testDatasetUrn.getEntityType())
+                                .getEntitySpec(TEST_DATASET_URN.getEntityType())
                                 .getAspectSpec(EDITABLE_SCHEMA_METADATA_ASPECT_NAME))
                         .recordTemplate(schema)
                         .build()),
@@ -200,7 +160,37 @@ public class FieldPathValidatorTest {
         1);
   }
 
-  private SchemaMetadata getMockSchemaMetadataAspect(boolean duplicateFields) {
+  @Test
+  public void testEmptySchemaFieldPath() {
+    final SchemaMetadata schema = getMockSchemaMetadataAspect(false, "");
+    TestMCP testItem =
+        TestMCP.builder()
+            .changeType(ChangeType.UPSERT)
+            .urn(TEST_DATASET_URN)
+            .entitySpec(entityRegistry.getEntitySpec(TEST_DATASET_URN.getEntityType()))
+            .aspectSpec(
+                entityRegistry
+                    .getEntitySpec(TEST_DATASET_URN.getEntityType())
+                    .getAspectSpec(SCHEMA_METADATA_ASPECT_NAME))
+            .recordTemplate(schema)
+            .build();
+
+    Set<AspectValidationException> exceptions =
+        test.validateProposed(Set.of(testItem), mockRetrieverContext).collect(Collectors.toSet());
+
+    assertEquals(
+        exceptions,
+        Set.of(
+            AspectValidationException.forItem(
+                testItem, "SchemaMetadata aspect has empty field path.")));
+  }
+
+  private static SchemaMetadata getMockSchemaMetadataAspect(boolean duplicateFields) {
+    return getMockSchemaMetadataAspect(duplicateFields, null);
+  }
+
+  private static SchemaMetadata getMockSchemaMetadataAspect(
+      boolean duplicateFields, @Nullable String fieldPath) {
     List<SchemaField> fields = new ArrayList<>();
     fields.add(
         new SchemaField()
@@ -209,7 +199,7 @@ public class FieldPathValidatorTest {
                     .setType(SchemaFieldDataType.Type.create(new StringType())))
             .setNullable(false)
             .setNativeDataType("string")
-            .setFieldPath("test"));
+            .setFieldPath(fieldPath == null ? "test" : fieldPath));
 
     if (duplicateFields) {
       fields.add(
@@ -219,15 +209,16 @@ public class FieldPathValidatorTest {
                       .setType(SchemaFieldDataType.Type.create(new StringType())))
               .setNullable(false)
               .setNativeDataType("string")
-              .setFieldPath("test"));
+              .setFieldPath(fieldPath == null ? "test" : fieldPath));
     }
 
     return new SchemaMetadata()
-        .setPlatform(testDatasetUrn.getPlatformEntity())
+        .setPlatform(TEST_DATASET_URN.getPlatformEntity())
         .setFields(new SchemaFieldArray(fields));
   }
 
-  private EditableSchemaMetadata getMockEditableSchemaMetadataAspect(boolean duplicateFields) {
+  private static EditableSchemaMetadata getMockEditableSchemaMetadataAspect(
+      boolean duplicateFields) {
 
     List<EditableSchemaFieldInfo> fields = new ArrayList<>();
     fields.add(new EditableSchemaFieldInfo().setFieldPath("test"));
