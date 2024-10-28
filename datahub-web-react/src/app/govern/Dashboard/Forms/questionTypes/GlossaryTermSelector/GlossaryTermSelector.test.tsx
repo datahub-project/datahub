9d73c885052f659bee2ec66bb2d90309c1232184
Amit Gaikwad <amit.gaikwad@apptware.com>
2024-10-28 20:16:34 +0530

    feat(ui/glossary-term-test-cases): add initial test cases for glossary term selector component

--- datahub-web-react/src/app/govern/Dashboard/Forms/questionTypes/GlossaryTermSelector/GlossaryTermSelector.test.tsx
+++ datahub-web-react/src/app/govern/Dashboard/Forms/questionTypes/GlossaryTermSelector/GlossaryTermSelector.test.tsx
@@ -44,9 +44,84 @@ const mocks = [
 ];
 
 describe('GlossaryTermsSelector', () => {
+    const initialOptions = [
+        {
+            value: 'urn:li:glossaryTerm:ac4728586e658ed7c0977969e12d09e1',
+            label: 'AccountType',
+            id: 'urn:li:glossaryTerm:ac4728586e658ed7c0977969e12d09e1',
+            isParent: false,
+            parentId: 'urn:li:glossaryNode:6f63035e8a24155de46020549e9fd075',
+            entity: {
+                urn: 'urn:li:glossaryTerm:ac4728586e658ed7c0977969e12d09e1',
+                type: 'GLOSSARY_TERM',
+                name: 'ac4728586e658ed7c0977969e12d09e1',
+                hierarchicalName: 'ac4728586e658ed7c0977969e12d09e1',
+                properties: {
+                    name: 'AccountType',
+                    __typename: 'GlossaryTermProperties',
+                },
+                parentNodes: {
+                    nodes: [
+                        {
+                            urn: 'urn:li:glossaryNode:6f63035e8a24155de46020549e9fd075',
+                            __typename: 'GlossaryNode',
+                        },
+                    ],
+                    __typename: 'ParentNodesResult',
+                },
+                __typename: 'GlossaryTerm',
+            },
+        },
+        {
+            value: 'urn:li:glossaryTerm:37c7edbc0b5129ab6b4da4021dfed7d3',
+            label: 'Currency',
+            id: 'urn:li:glossaryTerm:37c7edbc0b5129ab6b4da4021dfed7d3',
+            isParent: false,
+            parentId: 'urn:li:glossaryNode:6f63035e8a24155de46020549e9fd075',
+            entity: {
+                urn: 'urn:li:glossaryTerm:37c7edbc0b5129ab6b4da4021dfed7d3',
+                type: 'GLOSSARY_TERM',
+                name: '37c7edbc0b5129ab6b4da4021dfed7d3',
+                hierarchicalName: '37c7edbc0b5129ab6b4da4021dfed7d3',
+                properties: {
+                    name: 'Currency',
+                    __typename: 'GlossaryTermProperties',
+                },
+                parentNodes: {
+                    nodes: [
+                        {
+                            urn: 'urn:li:glossaryNode:6f63035e8a24155de46020549e9fd075',
+                            __typename: 'GlossaryNode',
+                        },
+                    ],
+                    __typename: 'ParentNodesResult',
+                },
+                __typename: 'GlossaryTerm',
+            },
+        },
+
+        {
+            value: 'urn:li:glossaryTerm:Classification.Sensitive',
+            label: 'Sensitive',
+            id: 'urn:li:glossaryTerm:Classification.Sensitive',
+            isParent: false,
+            entity: {
+                type: 'GLOSSARY_TERM',
+                urn: 'urn:li:glossaryTerm:Classification.Sensitive',
+                name: 'Sensitive',
+                hierarchicalName: 'Classification.Sensitive',
+                properties: {
+                    name: 'Sensitive',
+                    description: 'Sensitive Data',
+                    __typename: 'GlossaryTermProperties',
+                },
+                __typename: 'GlossaryTerm',
+            },
+        },
+    ];
     it('renders GlossaryTermsSelector without none select', async () => {
         const { getByText } = render(
-            <MockedProvider mocks={mocks} addTypename={true}>
+            <MockedProvider mocks={mocks} addTypename={false}>
                 <TestPageContainer>
                     <GlossaryTermsSelector
                         initialOptions={[]}
@@ -60,4 +135,69 @@ describe('GlossaryTermsSelector', () => {
         await waitFor(() => expect(getByText('Glossary Terms')).toBeInTheDocument());
         await waitFor(() => expect(getByText('Select allowed glossary terms')).toBeInTheDocument());
     });
+    it.skip('show number of selected pills in glossary term selector', async () => {
+        const { getByText, getAllByTestId } = render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <TestPageContainer>
+                    <GlossaryTermsSelector
+                        initialOptions={initialOptions}
+                        label="Glossary Terms"
+                        placeholder="Select allowed glossary terms"
+                    />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+        const selectedGlossaryItemPills = getAllByTestId('selected-item-pill');
+        screen.debug();
+
+        await waitFor(() => expect(getByText('Glossary Terms')).toBeInTheDocument());
+        await waitFor(() => expect(selectedGlossaryItemPills.length).toBe(initialOptions.length));
+    });
+    it.skip('check selected glossary term clear all the selected items ', async () => {
+        const { getByTestId, getAllByTestId } = render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <TestPageContainer>
+                    <GlossaryTermsSelector
+                        initialOptions={initialOptions}
+                        label="Glossary Terms"
+                        placeholder="Select allowed glossary terms"
+                    />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+        const selectedGlossaryItemPills = getAllByTestId('selected-item-pill');
+        await waitFor(() => expect(selectedGlossaryItemPills.length).toBe(initialOptions.length));
+
+        const dropdownClearIcon = getByTestId('dropdown-option-clear-icon');
+        fireEvent.click(dropdownClearIcon);
+        const selectedItemPill = screen.queryByTestId('selected-item-pill');
+        expect(selectedItemPill).not.toBeInTheDocument();
+    });
+
+    it.skip('check selected glossary term showing when user clicks on dropdown ', async () => {
+        const { getByTestId, getAllByTestId } = render(
+            <MockedProvider mocks={mocks} addTypename={false}>
+                <TestPageContainer>
+                    <GlossaryTermsSelector
+                        initialOptions={initialOptions}
+                        label="Glossary Terms"
+                        placeholder="Select allowed glossary terms"
+                    />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+        // screen.debug();
+        const dropdownContainer = getByTestId('nested-options-dropdown-container');
+        fireEvent.click(dropdownContainer);
+        screen.debug();
+        const selectedGlossaryItemPills = getAllByTestId('selected-item-pill');
+        // await waitFor(() => expect(selectedGlossaryItemPills.length).toBe(initialOptions.length));
+
+        // const dropdownClearIcon = getByTestId('dropdown-option-clear-icon');
+        // fireEvent.click(dropdownClearIcon);
+        // const selectedItemPill = screen.queryByTestId('selected-item-pill');
+        // expect(selectedItemPill).not.toBeInTheDocument();
+    });
+
+    // const mockOnSubmit = vi.fn(); // Create a mock function
 });
