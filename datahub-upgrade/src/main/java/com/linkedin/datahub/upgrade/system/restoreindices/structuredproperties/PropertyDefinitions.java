b700201353f4b64c70b0150029e9f60998fba0a0
David Leifker <david.leifker@acryl.io>
2024-10-29 17:41:53 -0500

    fix(struct-prop): fix unintended struct prop ES mutation

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/system/restoreindices/structuredproperties/PropertyDefinitions.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/system/restoreindices/structuredproperties/PropertyDefinitions.java
@@ -1,4 +1,4 @@
-package com.linkedin.datahub.upgrade.system.graph.edgestatus;
+package com.linkedin.datahub.upgrade.system.restoreindices.structuredproperties;
 
 import com.google.common.collect.ImmutableList;
 import com.linkedin.datahub.upgrade.UpgradeStep;
@@ -11,16 +11,15 @@ import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * A job that reindexes all status aspects as part of the graph edges containing status information.
- * This is required to make sure previously written status information is present in the graph
- * index.
+ * A job that reindexes all domain aspects as part of reindexing descriptions This is required to
+ * fix the analytics for domains
  */
 @Slf4j
-public class ReindexEdgeStatus implements NonBlockingSystemUpgrade {
+public class PropertyDefinitions implements NonBlockingSystemUpgrade {
 
   private final List<UpgradeStep> _steps;
 
-  public ReindexEdgeStatus(
+  public PropertyDefinitions(
       @Nonnull OperationContext opContext,
       EntityService<?> entityService,
       AspectDao aspectDao,
@@ -31,7 +30,7 @@ public class ReindexEdgeStatus implements NonBlockingSystemUpgrade {
     if (enabled) {
       _steps =
           ImmutableList.of(
-              new ReindexReindexEdgeStatusStep(
+              new PropertyDefinitionsStep(
                   opContext, entityService, aspectDao, batchSize, batchDelayMs, limit));
     } else {
       _steps = ImmutableList.of();
