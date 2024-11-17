3cc9d37c27c3120ec0ea2c7c6ca110d10f33f466
yoonhyejin <0327jane@gmail.com>
2024-11-17 16:03:12 +0900

    fix: add required css file

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -328,6 +328,7 @@ module.exports = {
             require.resolve("./src/styles/config-table.scss"),
             require.resolve("./src/components/SecondNavbar/styles.module.scss"),
             require.resolve("./src/components/SolutionsDropdown/styles.module.css"),
+            require.resolve("./src/components/CardDropdown/styles.module.css"),
           ],
         },
         pages: {
