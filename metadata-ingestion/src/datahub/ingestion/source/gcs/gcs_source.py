1c6f0404c95e444f216727c65c3c83b1b9921042
Jos Gesenhues <jos.gesenhues@rtl.de>
2024-11-18 12:55:11 +0100

    fix(ingestion/gcs): fix stateful ingestion for GCS source
    Remove pipeline name before passing context to equivalent s3 source to avoid error "Checkpointing provider DatahubIngestionCheckpointingProvider already registered."

--- metadata-ingestion/src/datahub/ingestion/source/gcs/gcs_source.py
+++ metadata-ingestion/src/datahub/ingestion/source/gcs/gcs_source.py
@@ -88,6 +88,7 @@ class GCSSource(StatefulIngestionSourceBase):
         super().__init__(config, ctx)
         self.config = config
         self.report = GCSSourceReport()
+        self.platform: str = PLATFORM_GCS
         self.s3_source = self.create_equivalent_s3_source(ctx)
 
     @classmethod
@@ -135,7 +136,7 @@ class GCSSource(StatefulIngestionSourceBase):
 
     def create_equivalent_s3_source(self, ctx: PipelineContext) -> S3Source:
         config = self.create_equivalent_s3_config()
-        return self.s3_source_overrides(S3Source(config, ctx))
+        return self.s3_source_overrides(S3Source(config, PipelineContext(ctx.run_id)))
 
     def s3_source_overrides(self, source: S3Source) -> S3Source:
         source.source_config.platform = PLATFORM_GCS
