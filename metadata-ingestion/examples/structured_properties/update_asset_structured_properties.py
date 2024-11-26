f189f577f484864ede63bec8d21fa44cf4aa0a30
Shirshanka Das <shirshanka@apache.org>
2024-11-26 13:08:11 -0800

    change the sdk example to use patch

--- metadata-ingestion/examples/structured_properties/update_asset_structured_properties.py
+++ metadata-ingestion/examples/structured_properties/update_asset_structured_properties.py
@@ -1,14 +1,10 @@
 import logging
 
-from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.emitter.rest_emitter import DatahubRestEmitter
 from datahub.ingestion.graph.client import DatahubClientConfig, DataHubGraph
 
 # Imports for metadata model classes
-from datahub.metadata.schema_classes import (
-    StructuredPropertiesClass,
-    StructuredPropertyValueAssignmentClass,
-)
+from datahub.specific.structured_properties import StructuredPropertiesAssetPatchBuilder
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -22,47 +18,24 @@ graph = DataHubGraph(DatahubClientConfig(server=gms_endpoint))
 
 # get the structuredProperties aspect for a given asset
 asset_urn = "urn:li:glossaryTerm:test123"
-structured_properties = graph.get_aspect(
-    entity_urn=asset_urn, aspect_type=StructuredPropertiesClass
-)
 
-# if it doesn't exist, default to an empty list of properties
-if structured_properties is None:
-    structured_properties = StructuredPropertiesClass(properties=[])
 
+patch_builder = StructuredPropertiesAssetPatchBuilder(asset_urn)
 
 # remove a property we don't want on the asset anymore
 property_to_remove = "urn:li:structuredProperty:removeMe"
-updated_properties = [
-    prop
-    for prop in structured_properties.properties
-    if prop.propertyUrn != property_to_remove
-]
-structured_properties.properties = updated_properties
 
+patch_builder = patch_builder.remove_structured_property(property_to_remove)
 
-# edit an existing property's value on an asset
+# edit an existing property's value on an asset (editMe is a structured property
+# of type String and single cardinality)
 edited_urn = "urn:li:structuredProperty:editMe"
-edited_property = [
-    prop for prop in structured_properties.properties if prop.propertyUrn == edited_urn
-][0]
-edited_property.values = ["new value"]
-
-for index, property in enumerate(structured_properties.properties):
-    if property.propertyUrn == edited_urn:
-        structured_properties.properties[index] = edited_property
-
+patch_builder.set_structured_property(edited_urn, "new value")
 
 # add a new property to an asset
 new_property_urn = "urn:li:structuredProperty:addMe"
-new_property = StructuredPropertyValueAssignmentClass(
-    propertyUrn=new_property_urn, values=["newly added"]
-)
-structured_properties.properties.append(new_property)
+patch_builder.add_structured_property(new_property_urn, "newly added")
 
 
-event: MetadataChangeProposalWrapper = MetadataChangeProposalWrapper(
-    entityUrn=str(asset_urn),
-    aspect=structured_properties,
-)
-rest_emitter.emit(event)
+for mcp in patch_builder.build():
+    rest_emitter.emit(mcp)
--- metadata-ingestion/src/datahub/specific/structured_properties.py
+++ metadata-ingestion/src/datahub/specific/structured_properties.py
@@ -1,6 +1,7 @@
-from typing import Generic, List, TypeVar, Union
+from typing import Generic, List, Optional, TypeVar, Union
 
 from datahub.emitter.mcp_patch_builder import MetadataPatchProposal
+from datahub.metadata._schema_classes import KafkaAuditHeaderClass, SystemMetadataClass
 from datahub.metadata.schema_classes import StructuredPropertyValueAssignmentClass
 from datahub.utilities.urns.structured_properties_urn import (
     make_structured_property_urn,
@@ -10,10 +11,12 @@ _Parent = TypeVar("_Parent", bound=MetadataPatchProposal)
 
 
 class StructuredPropertiesPatchHelper(Generic[_Parent]):
+    DEFAULT_ASPECT_NAME = "structuredProperties"
+
     def __init__(
         self,
         parent: _Parent,
-        aspect_name: str = "structuredProperties",
+        aspect_name: str = DEFAULT_ASPECT_NAME,
     ) -> None:
         self.aspect_name = aspect_name
         self._parent = parent
@@ -50,4 +53,53 @@ class StructuredPropertiesPatchHelper(Generic[_Parent]):
                 values=value if isinstance(value, list) else [value],
             ),
         )
+        return self
+
+
+class StructuredPropertiesAssetPatchBuilder(MetadataPatchProposal):
+    """
+    A Patch builder for the structuredProperties aspect of any asset.
+    """
+
+    def __init__(
+        self,
+        urn: str,
+        system_metadata: Optional[SystemMetadataClass] = None,
+        audit_header: Optional[KafkaAuditHeaderClass] = None,
+    ) -> None:
+        super().__init__(
+            urn, system_metadata=system_metadata, audit_header=audit_header
+        )
+        self.structured_properties_patch_helper = StructuredPropertiesPatchHelper(self)
+
+    def set_structured_property(
+        self, property_name: str, value: Union[str, float, List[Union[str, float]]]
+    ) -> "StructuredPropertiesAssetPatchBuilder":
+        """
+        This is a helper method to set a structured property.
+        @param property_name: the name of the property (either bare or urn form)
+        @param value: the value of the property (for multi-valued properties, this can be a list)
+        """
+        self.structured_properties_patch_helper.set_property(property_name, value)
+        return self
+
+    def add_structured_property(
+        self, property_name: str, value: Union[str, float]
+    ) -> "StructuredPropertiesAssetPatchBuilder":
+        """
+        This is a helper method to add a structured property.
+        @param property_name: the name of the property (either bare or urn form)
+        @param value: the value of the property (for multi-valued properties, this value will be appended to the list)
+        """
+        self.structured_properties_patch_helper.add_property(property_name, value)
+        return self
+
+    def remove_structured_property(
+        self, property_name: str
+    ) -> "StructuredPropertiesAssetPatchBuilder":
+        """
+        This is a helper method to remove a structured property.
+        @param property_name: the name of the property (either bare or urn form)
+        """
+        self.structured_properties_patch_helper.remove_property(property_name)
         return self
