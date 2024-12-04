4ee8c5aaab846b9f7e965b111946203fcb8b1941
sagar-salvi-apptware <sagar.salvi@apptware.com>
2024-12-04 18:22:25 +0530

    fix: fixed unit-tests

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -150,6 +150,13 @@ sql_common = (
     | classification_lib
 )
 
+sqllineage_lib = {
+    "sqllineage==1.3.8",
+    # We don't have a direct dependency on sqlparse but it is a dependency of sqllineage.
+    # There have previously been issues from not pinning sqlparse, so it's best to pin it.
+    # Related: https://github.com/reata/sqllineage/issues/361 and https://github.com/reata/sqllineage/pull/360
+    "sqlparse==0.4.4",
+}
 
 aws_common = {
     # AWS Python SDK
