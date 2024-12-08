61ce5f54d6721b27ad3edf61731bc8703b37f9d1
Kamalaseshan Radha <meenakshi.kamalaseshanradha@optum.com>
2024-12-08 18:52:53 +0530

    Allow custom userContext states to be added

---
+++ datahub-web-react/src/app/context/CustomUserContext.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+/**
+ * Custom User Context State - This is a custom user context state and can be overriden in specific fork of DataHub.
+ */
+export type CustomUserContextState = {
+};
+
+export const DEFAULT_CUSTOM_STATE: CustomUserContextState = {
+}
