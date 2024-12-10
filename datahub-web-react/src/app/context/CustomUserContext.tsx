528337fcbf75c6cfc4304016e128da60b63f2566
mkamalas <Meenakshi.KamalaseshanRadha@optum.com>
2024-12-10 18:15:42 +0530

    Custom User Context - linting fix

--- datahub-web-react/src/app/context/CustomUserContext.tsx
+++ datahub-web-react/src/app/context/CustomUserContext.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-
 /**
  * Custom User Context State - This is a custom user context state and can be overriden in specific fork of DataHub.
+ * The below type can be customized with specific object properties as well if needed.
  */
-export type CustomUserContextState = {};
+export type CustomUserContextState = Record<string, any>;
 
 export const DEFAULT_CUSTOM_STATE: CustomUserContextState = {};
