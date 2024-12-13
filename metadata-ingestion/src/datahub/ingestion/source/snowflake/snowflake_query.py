e4b74925f05aa310dd4e81c4997be397104a32a7
Mayuri N <mayuri.nehate@gslab.com>
2024-12-13 16:36:09 +0530

    feat(ingest/snowflake): secure view lineage without owner permissions

--- metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_query.py
+++ metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_query.py
@@ -238,6 +238,15 @@ LIMIT {limit} {from_clause};
 """
 
     @staticmethod
+    def get_secure_view_definitions() -> str:
+        # https://docs.snowflake.com/en/sql-reference/account-usage/views
+        return """
+            SELECT TABLE_CATALOG, TABLE_SCHEMA, TABLE_NAME, VIEW_DEFINITION
+            FROM SNOWFLAKE.ACCOUNT_USAGE.VIEWS
+            WHERE IS_SECURE = 'YES' AND VIEW_DEFINITION !='' AND DELETED IS NULL
+        """
+
+    @staticmethod
     def columns_for_schema(
         schema_name: str,
         db_name: str,
