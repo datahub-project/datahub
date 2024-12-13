ce944bbdd7a0380a243e4929aec6287549fc519c
Tamas Nemeth <treff7es@gmail.com>
2024-12-13 17:51:42 +0100

    fix(ingest/dagster): Fix Dagster build (#12121)

--- metadata-ingestion-modules/dagster-plugin/setup.py
+++ metadata-ingestion-modules/dagster-plugin/setup.py
@@ -123,7 +123,7 @@ setuptools.setup(
     ],
     # Package info.
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     package_dir={"": "src"},
     packages=setuptools.find_namespace_packages(where="./src"),
     entry_points=entry_points,
