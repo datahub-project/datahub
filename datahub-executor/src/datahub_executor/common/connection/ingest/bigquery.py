1e5441b10d9709b214052161bcd8abb069f7f519
Aseem Bansal <asmbansal2@gmail.com>
2024-12-13 17:35:03 +0530

    fix(observe): minor tweaks to logs (#4743)
    Co-authored-by: Mayuri Nehate <33225191+mayurinehate@users.noreply.github.com>

--- datahub-executor/src/datahub_executor/common/connection/ingest/bigquery.py
+++ datahub-executor/src/datahub_executor/common/connection/ingest/bigquery.py
@@ -12,19 +12,17 @@ from datahub_executor.common.connection.connection import Connection
 logger = logging.getLogger(__name__)
 
 
-# Returns None if a connection cannot be extracted.
 def extract_connection_from_bigquery_recipe(
     connection_urn: str, recipe: Dict, graph: DataHubGraph
 ) -> Optional[Connection]:
     # Create a dictionary representing a bigquery connection
     # Here we simply reuse the base model provided inside of our ingestion library.
     try:
-        if "source" in recipe:
-            source = recipe["source"]
-            if "config" in source:
-                source_config = source["config"]
-                bigquery_config = BigQueryV2Config.parse_obj_allow_extras(source_config)
-                return BigQueryConnection(connection_urn, bigquery_config, graph)
+        source_config = recipe.get("source", {}).get("config")
+        if not source_config:
+            return None
+        bigquery_config = BigQueryV2Config.parse_obj_allow_extras(source_config)
+        return BigQueryConnection(connection_urn, bigquery_config, graph)
     except Exception:
         logger.exception("Failed to extract connection details from BigQuery recipe!")
 
--- datahub-executor/src/datahub_executor/common/connection/ingest/snowflake.py
+++ datahub-executor/src/datahub_executor/common/connection/ingest/snowflake.py
@@ -12,19 +12,17 @@ from datahub_executor.common.connection.snowflake.snowflake_connection import (
 logger = logging.getLogger(__name__)
 
 
-# Returns None if a connection cannot be extracted.
 def extract_connection_from_snowflake_recipe(
     connection_urn: str, recipe: Dict, graph: DataHubGraph
 ) -> Optional[Connection]:
     # Create a dictionary representing a snowflake connection
     # Here we simply reuse the base model provided inside of our ingestion library.
     try:
-        if "source" in recipe:
-            source = recipe["source"]
-            if "config" in source:
-                source_config = source["config"]
-                snowflake_config = SnowflakeConfig.parse_obj_allow_extras(source_config)
-                return SnowflakeConnection(connection_urn, snowflake_config, graph)
+        source_config = recipe.get("source", {}).get("config")
+        if not source_config:
+            return None
+        snowflake_config = SnowflakeConfig.parse_obj_allow_extras(source_config)
+        return SnowflakeConnection(connection_urn, snowflake_config, graph)
     except Exception:
         logger.exception("Failed to extract connection details from Snowflake recipe!")
 
