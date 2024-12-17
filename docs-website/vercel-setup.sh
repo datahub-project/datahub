02bfe46cc8a6a11517ac0a2027bc1f0aa4b20634
Harshal Sheth <hsheth2@gmail.com>
2024-12-16 20:52:13 -0500

    ci(vercel): use prebuilt python

--- docs-website/vercel-setup.sh
+++ docs-website/vercel-setup.sh
@@ -8,27 +8,20 @@ set -euxo pipefail
 yum install java-17-amazon-corretto-devel -y
 javac --version
 
-# Build python from source.
-# Amazon Linux 2 has Python 3.8, but it's version of OpenSSL is super old and hence it
-# doesn't work with the packages we use. As such, we have to build Python from source.
-# TODO: This process is extremely slow - ideally we should cache the built Python binary
-# for reuse.
-
 yum groupinstall "Development Tools" -y
-yum install openssl openssl-devel libffi-devel bzip2-devel wget nodejs -y
+yum install openssl openssl-devel libffi-devel bzip2-devel wget nodejs tar -y
 
-wget https://www.python.org/ftp/python/3.10.11/Python-3.10.11.tgz
-tar -xf Python-3.10.11.tgz
-cd Python-3.10.11
-./configure #--enable-optimizations
+# Amazon Linux 2 has Python 3.8, but it's version of OpenSSL is super old and hence it
+# doesn't work with the packages we use. As such, we want to install Python ourselves.
+# Using uv to install Python from https://github.com/indygreg/python-build-standalone
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
 
-make -j $(nproc)
-
-make install
+uv python install 3.10
 
 # Set python3.10 as the default version.
 py3="$(which python3)"
 rm "$py3"
-ln "$(which python3.10)" "$py3"
+ln "$(uv python find 3.10)" "$py3"
 python3 --version
 
