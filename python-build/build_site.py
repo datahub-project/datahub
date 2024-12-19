3e4bdb93885dc02edbab4b79c96604c1ae208059
Harshal Sheth <hsheth2@gmail.com>
2024-12-18 20:53:56 -0500

    add build context

--- python-build/build_site.py
+++ python-build/build_site.py
@@ -1,6 +1,9 @@
+import contextlib
+import json
 import pathlib
 import shutil
-from datetime import datetime
+import subprocess
+from datetime import datetime, timezone
 
 PYTHON_BUILD_DIR = pathlib.Path(__file__).parent
 WHEEL_DIR = PYTHON_BUILD_DIR / "wheels"
@@ -17,6 +20,33 @@ for wheel_file in WHEEL_DIR.glob("*"):
 
 def package_name(wheel_file: pathlib.Path) -> str:
     return wheel_file.name.split("-")[0].replace("_", "-")
+
+
+# Get some extra context about the build
+ts = datetime.now(timezone.utc).isoformat()
+context_info: dict = {
+    "timestamp": ts,
+}
+with contextlib.suppress(Exception):
+    branch_info = subprocess.check_output(
+        ["git", "branch", "--show-current"], text=True
+    )
+    context_info["branch"] = branch_info.strip()
+with contextlib.suppress(Exception):
+    commit_info = subprocess.check_output(
+        ["git", "log", "-1", "--pretty=%H%n%B"], text=True
+    )
+    commit_hash, commit_msg = commit_info.strip().split("\n", 1)
+    context_info["commit"] = {
+        "hash": commit_hash,
+        "message": commit_msg.strip(),
+    }
+with contextlib.suppress(Exception):
+    pr_info = subprocess.check_output(
+        ["gh", "pr", "view", "--json", "title,number,url"], text=True
+    )
+    pr_info = json.loads(pr_info)
+    context_info["pr"] = pr_info
 
 
 newline = "\n"
@@ -36,8 +66,11 @@ newline = "\n"
       <p>
         These prebuilt wheel files can be used to install our Python packages as of a specific commit.
       </p>
-      <p>Built at {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.</p>
 
+      <h2>Build context</h2>
+      <pre id="context-info">{json.dumps(context_info, indent=2)}</pre>
+
+      <h2>Usage</h2>
       <p>
       Current base URL: <span class="base-url">unknown</span>
       </p>
@@ -57,7 +90,7 @@ newline = "\n"
             <tr>
               <td><code>{package_name(wheel_file)}</code></td>
               <td>{wheel_file.stat().st_size / 1024 / 1024:.3f} MB</td>
-              <td><code>pip install '{package_name(wheel_file)} @ <span class="base-url">&lt;base-url&gt;</span>/artifacts/wheels/{wheel_file.name}'</code></td>
+              <td><code>uv pip install '{package_name(wheel_file)} @ <span class="base-url">&lt;base-url&gt;</span>/artifacts/wheels/{wheel_file.name}'</code></td>
             </tr>
             '''
             for wheel_file in sorted(WHEEL_DIR.glob("*.whl"))
