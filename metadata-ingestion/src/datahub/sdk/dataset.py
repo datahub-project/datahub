de22b8c252fe92f7329581deda634b16aa7a8f39
Harshal Sheth <hsheth2@gmail.com>
2024-12-20 22:32:52 -0500

    add dataset props support

--- metadata-ingestion/src/datahub/sdk/dataset.py
+++ metadata-ingestion/src/datahub/sdk/dataset.py
@@ -1,9 +1,10 @@
-from typing import Optional, Type
+from datetime import datetime
+from typing import Dict, Optional, Type
 
 import pytest
 
 import datahub.metadata.schema_classes as models
-from datahub.emitter.mce_builder import DEFAULT_ENV
+from datahub.emitter.mce_builder import DEFAULT_ENV, make_ts_millis, parse_ts_millis
 from datahub.ingestion.graph.client import DataHubGraph
 from datahub.metadata.urns import DatasetUrn, Urn
 from datahub.sdk._shared import (
@@ -24,6 +25,18 @@ class DatasetUpdater:
     pass
 
 
+def _make_time_stamp(ts: Optional[datetime]) -> Optional[models.TimeStampClass]:
+    if ts is None:
+        return None
+    return models.TimeStampClass(time=make_ts_millis(ts))
+
+
+def _parse_time_stamp(ts: Optional[models.TimeStampClass]) -> Optional[datetime]:
+    if ts is None:
+        return None
+    return parse_ts_millis(ts.time)
+
+
 class Dataset(HasSubtype, HasOwnership, Entity):
     __slots__ = ()
 
@@ -40,16 +53,24 @@ class Dataset(HasSubtype, HasOwnership, Entity):
         platform_instance: Optional[str] = None,
         env: str = DEFAULT_ENV,
         # TODO have an urn-based variant? probably not, since we need to know the raw platform instance
-        # Attributes.
-        # description
-        # external_url
+        # Dataset properties.
+        description: Optional[str] = None,
+        display_name: Optional[str] = None,
+        qualified_name: Optional[str] = None,
+        external_url: Optional[str] = None,
+        custom_properties: Optional[Dict[str, str]] = None,
+        created: Optional[datetime] = None,
+        last_modified: Optional[datetime] = None,
+        # Standard aspects.
         subtype: Optional[str] = None,
         owners: Optional[OwnersInputType] = None,
-        # tags
-        # terms
+        # TODO tags
+        # TODO terms
         # structured_properties
-        # TODO: schema -> how do we make this feel nice
         # TODO container / browse path generation?
+        # Dataset-specific aspects.
+        # TODO: schema -> how do we make this feel nice
+        # TODO: lineage?
     ):
         urn = DatasetUrn.create_from_ids(
             platform_id=platform,
@@ -59,14 +80,28 @@ class Dataset(HasSubtype, HasOwnership, Entity):
         )
         super().__init__(urn)
 
-        # TODO:
         if platform_instance is not None:
-            # force create dataPlatformInstance aspect
+            # TODO: force create dataPlatformInstance aspect
             pass
+
+        self._ensure_dataset_props()
+        if description is not None:
+            self.set_description(description)
+        if display_name is not None:
+            self.set_display_name(display_name)
+        if qualified_name is not None:
+            self.set_qualified_name(qualified_name)
+        if external_url is not None:
+            self.set_external_url(external_url)
+        if custom_properties is not None:
+            self.set_custom_properties(custom_properties)
+        if created is not None:
+            self.set_created(created)
+        if last_modified is not None:
+            self.set_last_modified(last_modified)
 
         if subtype is not None:
             self.set_subtype(subtype)
-
         if owners is not None:
             self.set_owners(owners)
 
@@ -80,6 +115,62 @@ class Dataset(HasSubtype, HasOwnership, Entity):
         if dataPlatformInstance and dataPlatformInstance.instance:
             return dataPlatformInstance.instance
         return None
+
+    def _ensure_dataset_props(self) -> models.DatasetPropertiesClass:
+        dataset_props = self._get_aspect(models.DatasetPropertiesClass)
+        if dataset_props is None:
+            dataset_props = models.DatasetPropertiesClass()
+            self._set_aspect(dataset_props)
+        return dataset_props
+
+    @property
+    def description(self) -> Optional[str]:
+        return self._ensure_dataset_props().description
+
+    def set_description(self, description: str) -> None:
+        self._ensure_dataset_props().description = description
+
+    @property
+    def display_name(self) -> Optional[str]:
+        return self._ensure_dataset_props().name
+
+    def set_display_name(self, display_name: str) -> None:
+        self._ensure_dataset_props().name = display_name
+
+    @property
+    def qualified_name(self) -> Optional[str]:
+        return self._ensure_dataset_props().qualifiedName
+
+    def set_qualified_name(self, qualified_name: str) -> None:
+        self._ensure_dataset_props().qualifiedName = qualified_name
+
+    @property
+    def external_url(self) -> Optional[str]:
+        return self._ensure_dataset_props().externalUrl
+
+    def set_external_url(self, external_url: str) -> None:
+        self._ensure_dataset_props().externalUrl = external_url
+
+    @property
+    def custom_properties(self) -> Optional[Dict[str, str]]:
+        return self._ensure_dataset_props().customProperties
+
+    def set_custom_properties(self, custom_properties: Dict[str, str]) -> None:
+        self._ensure_dataset_props().customProperties = custom_properties
+
+    @property
+    def created(self) -> Optional[datetime]:
+        return _parse_time_stamp(self._ensure_dataset_props().created)
+
+    def set_created(self, created: datetime) -> None:
+        self._ensure_dataset_props().created = _make_time_stamp(created)
+
+    @property
+    def last_modified(self) -> Optional[datetime]:
+        return _parse_time_stamp(self._ensure_dataset_props().lastModified)
+
+    def set_last_modified(self, last_modified: datetime) -> None:
+        self._ensure_dataset_props().lastModified = _make_time_stamp(last_modified)
 
 
 def graph_get_dataset(graph: DataHubGraph, urn: UrnOrStr) -> Dataset:
