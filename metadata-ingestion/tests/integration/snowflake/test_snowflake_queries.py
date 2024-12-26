afaa90b796fd9c9df20e72a8e27ee8b90a12917e
Mayuri N <mayuri.nehate@gslab.com>
2024-12-26 13:47:58 +0530

    remove unnecessary patching, test for missing email_domain

--- metadata-ingestion/tests/integration/snowflake/test_snowflake_queries.py
+++ metadata-ingestion/tests/integration/snowflake/test_snowflake_queries.py
@@ -26,45 +26,48 @@ def test_source_close_cleans_tmp(snowflake_connect, tmp_path):
 
 @patch("snowflake.connector.connect")
 def test_user_identifiers_email_as_identifier(snowflake_connect, tmp_path):
-    with patch("tempfile.tempdir", str(tmp_path)):
-        source = SnowflakeQueriesSource.create(
-            {
-                "connection": {
-                    "account_id": "ABC12345.ap-south-1.aws",
-                    "username": "TST_USR",
-                    "password": "TST_PWD",
-                },
-                "email_as_user_identifier": True,
-                "email_domain": "example.com",
+    source = SnowflakeQueriesSource.create(
+        {
+            "connection": {
+                "account_id": "ABC12345.ap-south-1.aws",
+                "username": "TST_USR",
+                "password": "TST_PWD",
             },
-            PipelineContext("run-id"),
-        )
-        assert (
-            source.identifiers.get_user_identifier("username", "username@example.com")
-            == "username@example.com"
-        )
-        assert (
-            source.identifiers.get_user_identifier("username", None)
-            == "username@example.com"
-        )
+            "email_as_user_identifier": True,
+            "email_domain": "example.com",
+        },
+        PipelineContext("run-id"),
+    )
+    assert (
+        source.identifiers.get_user_identifier("username", "username@example.com")
+        == "username@example.com"
+    )
+    assert (
+        source.identifiers.get_user_identifier("username", None)
+        == "username@example.com"
+    )
+
+    # We'd do best effort to form email, but would keep username as is,
+    # if email can't be formed.
+    source.identifiers.identifier_config.email_domain = None
+    assert source.identifiers.get_user_identifier("username", None) == "username"
 
 
 @patch("snowflake.connector.connect")
 def test_user_identifiers_username_as_identifier(snowflake_connect, tmp_path):
-    with patch("tempfile.tempdir", str(tmp_path)):
-        source = SnowflakeQueriesSource.create(
-            {
-                "connection": {
-                    "account_id": "ABC12345.ap-south-1.aws",
-                    "username": "TST_USR",
-                    "password": "TST_PWD",
-                },
-                "email_as_user_identifier": False,
+    source = SnowflakeQueriesSource.create(
+        {
+            "connection": {
+                "account_id": "ABC12345.ap-south-1.aws",
+                "username": "TST_USR",
+                "password": "TST_PWD",
             },
-            PipelineContext("run-id"),
-        )
-        assert (
-            source.identifiers.get_user_identifier("username", "username@example.com")
-            == "username"
-        )
-        assert source.identifiers.get_user_identifier("username", None) == "username"
+            "email_as_user_identifier": False,
+        },
+        PipelineContext("run-id"),
+    )
+    assert (
+        source.identifiers.get_user_identifier("username", "username@example.com")
+        == "username"
+    )
+    assert source.identifiers.get_user_identifier("username", None) == "username"
