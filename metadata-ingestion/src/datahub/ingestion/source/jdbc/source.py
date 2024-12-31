8916dec6915613875f30a16ef13964c5f86e4ccb
Jonny Dixon <jonny.dixon@acryl.io>
2024-12-31 16:05:24 +0000

    Update source.py

--- metadata-ingestion/src/datahub/ingestion/source/jdbc/source.py
+++ metadata-ingestion/src/datahub/ingestion/source/jdbc/source.py
@@ -62,7 +62,7 @@ from datahub.sql_parsing.sql_parsing_aggregator import SqlParsingAggregator
 logger = logging.getLogger(__name__)
 
 
-@platform_name("JDBC")
+@platform_name("jdbc")
 @config_class(JDBCSourceConfig)
 @support_status(SupportStatus.INCUBATING)
 @capability(SourceCapability.CONTAINERS, "Enabled by default")
