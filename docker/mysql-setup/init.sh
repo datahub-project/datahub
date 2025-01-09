e72e9307a1ad0fa5fa8219805f665259e62410aa
david-leifker <114954101+david-leifker@users.noreply.github.com>
2025-01-09 14:32:18 -0600

    chore(alpine): bump alpine images 3.21 (#12302)

--- docker/mysql-setup/init.sh
+++ docker/mysql-setup/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 : ${MYSQL_PORT:=3306}
+: ${MYSQL_ARGS:=--ssl=0}
 
 sed -e "s/DATAHUB_DB_NAME/${DATAHUB_DB_NAME}/g" \
  /init.sql | tee /tmp/init-final.sql
@@ -10,8 +11,8 @@ if [[ $CREATE_USER == true ]]; then
     -e "s/USER_NAME/${MYSQL_USERNAME}/g" \
     -e "s/PASSWORD/${MYSQL_PASSWORD}/g" \
    /user.sql | tee /tmp/user-final.sql
-  mysql -u $MYSQL_ROOT_USERNAME -p"$MYSQL_ROOT_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT < /tmp/init-final.sql
-  mysql -u $MYSQL_ROOT_USERNAME -p"$MYSQL_ROOT_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT < /tmp/user-final.sql
+  mariadb -u $MYSQL_ROOT_USERNAME -p"$MYSQL_ROOT_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT $MYSQL_ARGS < /tmp/init-final.sql
+  mariadb -u $MYSQL_ROOT_USERNAME -p"$MYSQL_ROOT_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT $MYSQL_ARGS < /tmp/user-final.sql
 else
-  mysql -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT < /tmp/init-final.sql
+  mariadb -u $MYSQL_USERNAME -p"$MYSQL_PASSWORD" -h $MYSQL_HOST -P $MYSQL_PORT $MYSQL_ARGS < /tmp/init-final.sql
 fi
