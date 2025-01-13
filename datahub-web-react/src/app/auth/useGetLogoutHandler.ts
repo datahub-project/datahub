91df419ede64a5093ec9b3d20ee763e03c231bf7
Chris Collins <chriscollins3456@gmail.com>
2025-01-13 17:41:26 -0500

    fix(ui) Properly log out in nav bar redesign (#4918)

--- datahub-web-react/src/app/auth/useGetLogoutHandler.ts
+++ datahub-web-react/src/app/auth/useGetLogoutHandler.ts
@@ -1,17 +1,21 @@
 import { useCallback } from 'react';
 import Cookies from 'js-cookie';
 import { GlobalCfg } from '@src/conf';
+import { useHistory } from 'react-router-dom';
 import analytics, { EventType } from '../analytics';
 import { useUserContext } from '../context/useUserContext';
 import { isLoggedInVar } from './checkAuthStatus';
 
 export default function useGetLogoutHandler() {
     const me = useUserContext();
+    const history = useHistory();
 
     return useCallback(() => {
         analytics.event({ type: EventType.LogOutEvent });
         isLoggedInVar(false);
         Cookies.remove(GlobalCfg.CLIENT_AUTH_COOKIE);
         me.updateLocalState({ selectedViewUrn: undefined });
-    }, [me]);
+        // must go to /logOut for datahub-frontend to fully log you out
+        history.replace('/logOut');
+    }, [me, history]);
 }
