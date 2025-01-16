4280fa42dbfcc9d066b2c70e51a95db1b9c6f397
Hyejin Yoon <0327jane@gmail.com>
2025-01-17 07:37:07 +0900

    fix core link

--- docs-website/src/components/SolutionsDropdown/SolutionsDropdownContent/solutionsDropdownContent.js
+++ docs-website/src/components/SolutionsDropdown/SolutionsDropdownContent/solutionsDropdownContent.js
@@ -24,7 +24,7 @@ const solutionsDropdownContent = {
             title: "DataHub Core",
             description: "Get started with the Open Source platform.",
             iconImage: "/img/solutions/icon-dropdown-core.png",
-            href: "/",
+            href: "/docs/quickstart",
         },
         {
             title: "Cloud vs Core",
