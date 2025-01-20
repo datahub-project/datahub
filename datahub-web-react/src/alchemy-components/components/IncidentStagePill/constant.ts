507ff3b1b81facf0f36153d10ca194aacea71419
Amit Gaikwad <amit.gaikwad@apptware.com>
2025-01-20 17:53:34 +0530

    feat(ui/incident-stage-pill): change resolved to fixed for stage

--- datahub-web-react/src/alchemy-components/components/IncidentStagePill/constant.ts
+++ datahub-web-react/src/alchemy-components/components/IncidentStagePill/constant.ts
@@ -2,7 +2,7 @@ import { IncidentStage } from '@src/types.generated';
 
 export const IncidentStageLabel = {
     [IncidentStage.Triage]: 'Triage',
-    [IncidentStage.Fixed]: 'Resolved',
+    [IncidentStage.Fixed]: 'Fixed',
     [IncidentStage.Investigation]: 'Investigation',
     [IncidentStage.NoActionRequired]: 'No action',
     [IncidentStage.WorkInProgress]: 'In progress',
