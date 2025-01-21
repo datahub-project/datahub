5eb2e0a704b36dd3f3f40fbd4e2b07c89786dc6b
Aseem Bansal <asmbansal2@gmail.com>
2025-01-21 19:10:20 +0530

    fix(ui): null deref (#4963)

--- datahub-web-react/src/app/entityV2/shared/tabs/Dataset/Stats/StatsTabV2/StatsSectionsContext.tsx
+++ datahub-web-react/src/app/entityV2/shared/tabs/Dataset/Stats/StatsTabV2/StatsSectionsContext.tsx
@@ -76,7 +76,7 @@ export const StatsSectionsContextProvider = ({ children }: Props) => {
     const statsEntity: any =
         baseEntity.dataset?.urn !== statsEntityUrn
             ? baseEntity.dataset?.siblingsSearch?.searchResults.find((res) => res.entity.urn === statsEntityUrn)
-                  ?.entity || baseEntity.dataset?.siblingsSearch?.searchResults[0].entity
+                  ?.entity || baseEntity.dataset?.siblingsSearch?.searchResults[0]?.entity
             : baseEntity.dataset;
 
     const { data, loading } = useGetTimeseriesCapabilities(statsEntityUrn);
