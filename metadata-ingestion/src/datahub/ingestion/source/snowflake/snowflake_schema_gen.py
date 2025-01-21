d6ae3a59190471162dee049149edfcc96d78dcab
Mayuri N <mayuri.nehate@gslab.com>
2025-01-21 20:50:48 +0530

    fix(ingest): fix reporting for missing secure view lineage
    Secure Views in databases created from Snowflake Shares will always
    be missing on view definitions and that need not be reported as
    warning. We would report such views as info in pipeline run.

--- metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_schema_gen.py
+++ metadata-ingestion/src/datahub/ingestion/source/snowflake/snowflake_schema_gen.py
@@ -491,15 +491,23 @@ class SnowflakeSchemaGenerator(SnowflakeStructuredReportMixin):
         try:
             view_definitions = self.data_dictionary.get_secure_view_definitions()
             return view_definitions[db_name][schema_name][table_name]
+        except KeyError:
+            # Received secure view definitions but the view is not present in results
+            self.structured_reporter.info(
+                "Secure view definition not found. Lineage will be missing for the view.",
+                context=f"{db_name}.{schema_name}.{table_name}",
+            )
+            return None
         except Exception as e:
             if isinstance(e, SnowflakePermissionError):
                 error_msg = (
                     "Failed to get secure views definitions. Please check permissions."
                 )
             else:
-                error_msg = "Failed to get secure views definitions"
+                error_msg = "Failed to get secure views definitions."
             self.structured_reporter.warning(
-                error_msg,
+                error_msg + " Lineage will be missing for the view.",
+                context=f"{db_name}.{schema_name}.{table_name}",
                 exc=e,
             )
             return None
--- metadata-ingestion/tests/integration/snowflake/test_snowflake_failures.py
+++ metadata-ingestion/tests/integration/snowflake/test_snowflake_failures.py
@@ -260,3 +260,55 @@ def test_snowflake_missing_snowflake_operations_permission_causes_pipeline_failu
         assert "usage-permission-error" in [
             failure.message for failure in pipeline.source.get_report().failures
         ]
+
+
+@freeze_time(FROZEN_TIME)
+def test_snowflake_missing_snowflake_secure_view_definitions_raises_pipeline_info(
+    pytestconfig,
+    snowflake_pipeline_config,
+):
+    with mock.patch("snowflake.connector.connect") as mock_connect:
+        sf_connection = mock.MagicMock()
+        sf_cursor = mock.MagicMock()
+        mock_connect.return_value = sf_connection
+        sf_connection.cursor.return_value = sf_cursor
+
+        # Error in getting access history date range
+        sf_cursor.execute.side_effect = query_permission_response_override(
+            default_query_results,
+            [snowflake_query.SnowflakeQuery.get_secure_view_definitions()],
+            [],
+        )
+        pipeline = Pipeline(snowflake_pipeline_config)
+        pipeline.run()
+
+        pipeline.raise_from_status(raise_warnings=True)
+        assert (
+            "Secure view definition not found. Lineage will be missing for the view."
+            in [info.message for info in pipeline.source.get_report().infos]
+        )
+
+
+@freeze_time(FROZEN_TIME)
+def test_snowflake_failed_secure_view_definitions_query_raises_pipeline_warning(
+    pytestconfig,
+    snowflake_pipeline_config,
+):
+    with mock.patch("snowflake.connector.connect") as mock_connect:
+        sf_connection = mock.MagicMock()
+        sf_cursor = mock.MagicMock()
+        mock_connect.return_value = sf_connection
+        sf_connection.cursor.return_value = sf_cursor
+
+        # Error in getting access history date range
+        sf_cursor.execute.side_effect = query_permission_error_override(
+            default_query_results,
+            [snowflake_query.SnowflakeQuery.get_secure_view_definitions()],
+            "Database 'SNOWFLAKE' does not exist or not authorized.",
+        )
+        pipeline = Pipeline(snowflake_pipeline_config)
+        pipeline.run()
+        assert (
+            "Failed to get secure views definitions. Please check permissions. "
+            "Lineage will be missing for the view."
+        ) in [info.message for info in pipeline.source.get_report().warnings]
