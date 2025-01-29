eff9287a42497311c05d6c973b1914b47bb0f418
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2025-01-29 12:15:43 -0800

    fix(ui/lineageV2): Properly set adjacencyList edges for queries after filtering nodes

--- datahub-web-react/src/app/lineageV2/useComputeGraph/filterNodes.ts
+++ datahub-web-react/src/app/lineageV2/useComputeGraph/filterNodes.ts
@@ -1,9 +1,11 @@
+import { globalEntityRegistryV2 } from '@app/EntityRegistryProvider';
 import {
     addToAdjacencyList,
     EdgeId,
     getEdgeId,
     isGhostEntity,
     isTransformational,
+    isUrnQuery,
     LineageAuditStamp,
     LineageEdge,
     NodeContext,
@@ -53,6 +55,8 @@ export default function hideNodes(
         }));
     }
     if (hideDataProcessInstances) {
+        // Note: Will only pick one query node if there is lineage t1 -> q1 -> dpi1 -> q2 -> t2
+        // Currently data process instances can't have lineage to queries so this is fine
         newNodes = new Map(
             Array.from(newNodes).filter(
                 ([urn, node]) => urn === rootUrn || node?.entity?.type !== EntityType.DataProcessInstance,
@@ -117,6 +121,9 @@ function connectEdges(rootUrn: string, { nodes, edges, adjacencyList }: ContextS
         seen.add(id);
 
         adjacencyList[direction].get(id)?.forEach((neighbor) => {
+            if (isUrnQuery(neighbor, globalEntityRegistryV2)) {
+                return;
+            }
             if (nodes.has(neighbor)) {
                 addToAdjacencyList(newAdjacencyList, direction, id, neighbor);
                 const edgeId = getEdgeId(id, neighbor, direction);
@@ -135,6 +142,7 @@ function connectEdges(rootUrn: string, { nodes, edges, adjacencyList }: ContextS
                         created: getLatestTimestamp(firstEdge?.created, secondEdge?.created),
                         updated: getLatestTimestamp(firstEdge?.updated, secondEdge?.updated),
                         isDisplayed: (firstEdge?.isDisplayed && secondEdge?.isDisplayed) ?? false,
+                        via: firstEdge?.via || secondEdge?.via,
                     };
                     newEdges.set(edgeId, mergeEdges(newEdge, existingEdge));
                 });
@@ -146,6 +154,15 @@ function connectEdges(rootUrn: string, { nodes, edges, adjacencyList }: ContextS
     buildNewAdjacencyList(rootUrn, LineageDirection.Upstream);
     seen.clear();
     buildNewAdjacencyList(rootUrn, LineageDirection.Downstream);
+
+    newEdges.forEach((edge, edgeId) => {
+        const [upstream, downstream] = parseEdgeId(edgeId);
+        if (edge.via && nodes.has(edge.via) && nodes.get(edge.via)?.type === EntityType.Query) {
+            setDefault(newAdjacencyList[LineageDirection.Upstream], edge.via, new Set()).add(upstream);
+            setDefault(newAdjacencyList[LineageDirection.Downstream], edge.via, new Set()).add(downstream);
+        }
+    });
+
     return { newAdjacencyList, newEdges };
 }
 
@@ -156,6 +173,7 @@ function mergeEdges(edgeA?: LineageEdge, edgeB?: LineageEdge): LineageEdge {
         created: getLatestTimestamp(edgeA?.created, edgeB?.created),
         updated: getLatestTimestamp(edgeA?.updated, edgeB?.updated),
         isDisplayed: (edgeA?.isDisplayed || edgeB?.isDisplayed) ?? false,
+        via: edgeA?.via || edgeB?.via,
     };
 }
 
@@ -171,6 +189,10 @@ function removeHiddenEdges({ edges }: ContextSubset) {
         if (edge.isDisplayed) {
             addToAdjacencyList(newAdjacencyList, LineageDirection.Upstream, downstream, upstream);
             newEdges.set(edgeId, edge);
+            if (edge.via) {
+                setDefault(newAdjacencyList[LineageDirection.Upstream], edge.via, new Set()).add(upstream);
+                setDefault(newAdjacencyList[LineageDirection.Downstream], edge.via, new Set()).add(downstream);
+            }
         }
     });
     return { newEdges, newAdjacencyList };
