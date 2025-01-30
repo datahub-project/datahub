f527c5ed5564b068a5243c5069f959708327aa0e
david-leifker <114954101+david-leifker@users.noreply.github.com>
2025-01-30 04:08:15 -0600

    feat(iceberg-rest): implement iceberg REST catalog api (#12500)
    Co-authored-by: ksrinath <ksrinath@users.noreply.github.com>
    Co-authored-by: Chakravarthy Racharla <chakru.racharla@acryl.io>

--- metadata-service/schema-registry-servlet/src/main/java/io/datahubproject/openapi/schema/registry/config/SpringWebSchemaRegistryConfig.java
+++ metadata-service/schema-registry-servlet/src/main/java/io/datahubproject/openapi/schema/registry/config/SpringWebSchemaRegistryConfig.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
@@ -16,13 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.converter.ByteArrayHttpMessageConverter;
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Slf4j
 @EnableWebMvc
@@ -35,7 +28,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
     havingValue = InternalSchemaRegistryFactory.TYPE)
 @Configuration
 @ComponentScan(basePackages = {"io.datahubproject.openapi.schema.registry"})
-public class SpringWebSchemaRegistryConfig implements WebMvcConfigurer {
+public class SpringWebSchemaRegistryConfig {
 
   @Bean
   public SchemaRegistryController schemaRegistryController(
@@ -43,13 +36,5 @@ public class SpringWebSchemaRegistryConfig implements WebMvcConfigurer {
       HttpServletRequest request,
       @Qualifier("schemaRegistryService") SchemaRegistryService schemaRegistryService) {
     return new SchemaRegistryController(objectMapper, request, schemaRegistryService);
-  }
-
-  @Override
-  public void configureMessageConverters(List<HttpMessageConverter<?>> messageConverters) {
-    messageConverters.add(new StringHttpMessageConverter());
-    messageConverters.add(new ByteArrayHttpMessageConverter());
-    messageConverters.add(new FormHttpMessageConverter());
-    messageConverters.add(new MappingJackson2HttpMessageConverter());
   }
 }
