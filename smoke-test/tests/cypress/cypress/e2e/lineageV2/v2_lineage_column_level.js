0e7add96fb3b666c0aa6dfa5d375d20e0fd6751e
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2025-01-29 21:22:57 -0800

    fix flake

--- smoke-test/tests/cypress/cypress/e2e/lineageV2/v2_lineage_column_level.js
+++ smoke-test/tests/cypress/cypress/e2e/lineageV2/v2_lineage_column_level.js
@@ -5,7 +5,7 @@ const DATASET_URN =
 const clickDownAndUpArrow = (asset, arrow) => {
   cy.contains(".react-flow__node-lineage-entity", asset)
     .find(`svg[data-testid="${arrow}"]`)
-    .click();
+    .click({ force: true });
 };
 
 describe("column-level lineage graph test", () => {
