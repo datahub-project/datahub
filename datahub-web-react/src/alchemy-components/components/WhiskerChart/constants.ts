fdcdc94f175b70113d62732c5428984df916d707
v-tarasevich-blitz-brain <v.tarasevich@blitz-brain.com>
2025-01-31 23:47:25 +0300

    feat(UI): add Whisker Chart (#206) (#5051)

---
+++ datahub-web-react/src/alchemy-components/components/WhiskerChart/constants.ts
@@ -0,0 +1,27 @@
+import { ColorSchemeSettings, WhiskerMetricType } from './types';
+
+export const DEFAULT_COLOR_SHEME: ColorSchemeSettings = {
+    box: '#705EE4',
+    boxAlternative: '#CAC3F1',
+    medianLine: '#2200F9',
+    alternative: '#533FD1',
+};
+
+export const WHISKER_METRIC_NAMES = {
+    [WhiskerMetricType.Max]: 'Max',
+    [WhiskerMetricType.FirstQuartile]: 'First Quartile',
+    [WhiskerMetricType.Median]: 'Median',
+    [WhiskerMetricType.ThirdQuartile]: 'Third Quartile',
+    [WhiskerMetricType.Min]: 'Min',
+};
+
+export const WHISKER_METRIC_ATTRIBUTE_NAMES = {
+    [WhiskerMetricType.Max]: 'max',
+    [WhiskerMetricType.FirstQuartile]: 'firstQuartile',
+    [WhiskerMetricType.Median]: 'median',
+    [WhiskerMetricType.ThirdQuartile]: 'thirdQuartile',
+    [WhiskerMetricType.Min]: 'min',
+};
+
+export const DEFAULT_BOX_SIZE = 34;
+export const DEFAULT_GAP_BETWEEN_WHISKERS = 10;
