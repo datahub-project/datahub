46c2871357d2558ad9d25246bf229ccd031dcc40
Harshal Sheth <hsheth2@gmail.com>
2025-02-05 22:53:11 -0800

    fix bug with end keyword

--- metadata-ingestion/src/datahub/sql_parsing/split_statements.py
+++ metadata-ingestion/src/datahub/sql_parsing/split_statements.py
@@ -2,6 +2,10 @@ import re
 from enum import Enum
 from typing import Iterator, List, Tuple
 
+SELECT_KEYWORD = "SELECT"
+CASE_KEYWORD = "CASE"
+END_KEYWORD = "END"
+
 CONTROL_FLOW_KEYWORDS = [
     "GO",
     r"BEGIN\w+TRY",
@@ -9,15 +13,18 @@ CONTROL_FLOW_KEYWORDS = [
     "BEGIN",
     r"END\w+TRY",
     r"END\w+CATCH",
-    "IF",  # This isn't strictly correct, but we assume that IF | (condition) | (block) should all be split up
+    # This isn't strictly correct, but we assume that IF | (condition) | (block) should all be split up
     # This mainly ensures that IF statements don't get tacked onto the previous statement incorrectly
+    "IF",
+    # For things like CASE, END does not mean the end of a statement.
+    # We have special handling for this.
+    END_KEYWORD,
     # "ELSE",  # else is also valid in CASE, so we we can't use it here.
-    # "END",  # for things like CASE, END does not mean the end of a statement
 ]
 
 # There's an exception to this rule, which is when the statement
-# is preceded by a CTE.
-SELECT_KEYWORD = "SELECT"
+# is preceded by a CTE. For those, we have to check if the character
+# before this is a ")".
 NEW_STATEMENT_KEYWORDS = [
     # SELECT is used inside queries as well, so we can't include it here.
     "CREATE",
@@ -49,11 +56,22 @@ class _StatementSplitter:
     def __init__(self, sql: str):
         self.sql = sql
 
-        # Parser state.
+        # Main parser state.
         self.i = 0
         self.state = ParserState.NORMAL
         self.current_statement: List[str] = []
+
+        # Additional parser state.
+
+        # If we see a SELECT, should we start a new statement?
+        # If we previously saw a drop/truncate/etc, a SELECT does mean a new statement.
+        # But if we're in a select/create/etc, a select could just be a subquery.
         self.does_select_mean_new_statement = False
+
+        # The END keyword terminates CASE and BEGIN blocks.
+        # We need to match the CASE statements with END blocks to determine
+        # what a given END is closing.
+        self.current_case_statements = 0
 
     def _is_keyword_at_position(self, pos: int, keyword: str) -> bool:
         """
@@ -88,8 +106,15 @@ class _StatementSplitter:
     def _yield_if_complete(self) -> Iterator[str]:
         statement = "".join(self.current_statement).strip()
         if statement:
+            # Subtle - to avoid losing full whitespace, they get merged into the next statement.
             yield statement
             self.current_statement.clear()
+
+        # Reset current_statement-specific state.
+        self.does_select_mean_new_statement = False
+        if self.current_case_statements != 0:
+            breakpoint()
+        self.current_case_statements = 0
 
     def process(self) -> Iterator[str]:
         if not self.sql or not self.sql.strip():
@@ -156,10 +181,20 @@ class _StatementSplitter:
     def _process_normal(self, most_recent_real_char: str) -> Iterator[str]:
         c = self.sql[self.i]
 
+        if self._is_keyword_at_position(self.i, CASE_KEYWORD):
+            self.current_case_statements += 1
+
         is_control_keyword, keyword, keyword_len = self._look_ahead_for_keywords(
             keywords=CONTROL_FLOW_KEYWORDS
         )
-        if is_control_keyword:
+        if (
+            is_control_keyword
+            and keyword == END_KEYWORD
+            and self.current_case_statements > 0
+        ):
+            # If we're closing a CASE statement with END, we can just decrement the counter and continue.
+            self.current_case_statements -= 1
+        elif is_control_keyword:
             # Yield current statement if any
             yield from self._yield_if_complete()
             # Yield keyword as its own statement
@@ -200,7 +235,7 @@ class _StatementSplitter:
             self.i += keyword_len
             raise _AlreadyIncremented()
 
-        elif c == ";":
+        if c == ";":
             yield from self._yield_if_complete()
         else:
             self.current_statement.append(c)
