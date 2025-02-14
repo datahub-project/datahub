b227b5afbdefa0c0340672cf96c86a97612b38b8
amit-apptware <132869468+amit-apptware@users.noreply.github.com>
2025-02-14 17:11:47 +0530

    feat(ui/alchemy-component):IconLabel - Refactor code and fix style issue (#5110)

--- datahub-web-react/src/alchemy-components/components/IncidentPriorityLabel/IncidentPriorityLabel.tsx
+++ datahub-web-react/src/alchemy-components/components/IncidentPriorityLabel/IncidentPriorityLabel.tsx
@@ -1,43 +1,38 @@
 import React from 'react';
-import { ExclamationMark } from '@phosphor-icons/react';
-import colors from '@src/alchemy-components/theme/foundations/colors';
+
+import CriticalIcon from '@src/images/incident-critical.svg';
+import HighIcon from '@src/images/incident-chart-bar-three.svg';
+import MediumIcon from '@src/images/incident-chart-bar-two.svg';
+import LowIcon from '@src/images/incident-chart-bar-one.svg';
+import { Label, StyledImage } from './components';
 
 import { IconLabel } from '../IconLabel';
 import { IncidentPriorityLabelProps } from './types';
-import { Bar } from '../Bar';
 import { PRIORITIES } from './constant';
 import { IconType } from '../IconLabel/types';
 
-const PRIORITY_LEVEL = {
-    [PRIORITIES.HIGH]: 3,
-    [PRIORITIES.MEDIUM]: 2,
-    [PRIORITIES.LOW]: 1,
+// 🔄 Map priorities to icons for cleaner code
+const priorityIcons = {
+    [PRIORITIES.CRITICAL]: CriticalIcon,
+    [PRIORITIES.HIGH]: HighIcon,
+    [PRIORITIES.MEDIUM]: MediumIcon,
+    [PRIORITIES.LOW]: LowIcon,
+    [PRIORITIES.NONE]: null,
 };
 
-const renderBars = (priority: string) => {
-    return <Bar size="default" color={colors.gray[1800]} coloredBars={PRIORITY_LEVEL[priority]} />;
-};
-
-const Icons = {
-    [PRIORITIES.CRITICAL]: {
-        icon: <ExclamationMark weight="fill" color={colors.gray[1800]} size={22} />,
-        type: IconType.ICON,
-    },
-    [PRIORITIES.HIGH]: {
-        icon: renderBars(PRIORITIES.HIGH),
-        type: IconType.ICON,
-    },
-    [PRIORITIES.MEDIUM]: {
-        icon: renderBars(PRIORITIES.MEDIUM),
-        type: IconType.ICON,
-    },
-    [PRIORITIES.LOW]: {
-        icon: renderBars(PRIORITIES.LOW),
-        type: IconType.ICON,
-    },
-};
+// 🚀 Dynamically generate the Icons object
+const Icons = Object.fromEntries(
+    Object.entries(priorityIcons).map(([priority, iconSrc]) => [
+        priority,
+        {
+            icon: iconSrc ? <StyledImage src={iconSrc} alt={priority} /> : null,
+            type: IconType.ICON,
+        },
+    ]),
+);
 
 export const IncidentPriorityLabel = ({ priority, title, style }: IncidentPriorityLabelProps) => {
     const { icon, type } = Icons[priority] || {};
+    if (!icon) return <Label>{title}</Label>;
     return <IconLabel style={style} icon={icon} name={title} type={type} />;
 };
---
+++ datahub-web-react/src/alchemy-components/components/IncidentPriorityLabel/components.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const StyledImage = styled.img`
+    cursor: pointer;
+`;
+
+export const Label = styled.span``;
