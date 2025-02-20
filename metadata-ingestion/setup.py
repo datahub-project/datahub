5aa51e21680d74a74d9f55b31c017e3ec904478b
Harshal Sheth <hsheth2@gmail.com>
2025-02-20 14:11:47 -0800

    bump to latest

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -101,7 +101,7 @@ sqlglot_lib = {
     # We heavily monkeypatch sqlglot.
     # We used to maintain an acryl-sqlglot fork: https://github.com/tobymao/sqlglot/compare/main...hsheth2:sqlglot:main?expand=1
     # but not longer do.
-    "sqlglot[rs]==26.3.9",
+    "sqlglot[rs]==26.6.0",
     "patchy==2.8.0",
 }
 
