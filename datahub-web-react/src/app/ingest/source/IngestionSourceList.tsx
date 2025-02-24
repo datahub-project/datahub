9f8e8a3fb3a479cf42a7153d9212d0af583640be
Ryota Egashira <ryota.egashira@acryl.io>
2025-02-24 12:26:22 -0800

    removing unnecesary commits

--- datahub-web-react/src/app/ingest/source/IngestionSourceList.tsx
+++ datahub-web-react/src/app/ingest/source/IngestionSourceList.tsx
@@ -98,6 +98,11 @@ export const IngestionSourceList = () => {
     const [sort, setSort] = useState<SortCriterion>();
     const [hideSystemSources, setHideSystemSources] = useState(true);
 
+    // When source filter changes, reset page to 1
+    useEffect(() => {
+        setPage(1);
+    }, [sourceFilter]);
+
     /**
      * Show or hide system ingestion sources using a hidden command S command.
      */
