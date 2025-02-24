6db2ec349387c3cb19444cec89550412aee38bc6
Aseem Bansal <asmbansal2@gmail.com>
2025-02-24 13:42:30 +0530

    dev: explicit set ruff version, revert other updates (#5195)

--- datahub-integrations-service/scripts/lockfile.sh
+++ datahub-integrations-service/scripts/lockfile.sh
@@ -6,8 +6,6 @@ set -euxo pipefail
 # Note that requirements-local.in already includes `-e .`.
 # We still need to specify pyproject.toml again in the dev target to tell
 # it which package to get `[dev]` dependencies from.
-rm requirements.txt
-rm requirements-dev.txt
 uv pip compile --universal -o requirements.txt requirements-local.in $@
 uv pip compile --universal -o requirements-dev.txt --extra dev pyproject.toml requirements-local.in requirements.txt $@
 
