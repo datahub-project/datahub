2a95584a5b254457f37759078d0e24cf25538744
Ryota Egashira <ryota.egashira@acryl.io>
2025-02-26 10:32:35 -0800

    refactor and changed GCP credential to pass project_id

--- metadata-ingestion/src/datahub/ingestion/source/vertexai.py
+++ metadata-ingestion/src/datahub/ingestion/source/vertexai.py
@@ -1,7 +1,9 @@
+import json
 import logging
 import os
+import tempfile
 import time
-from typing import Any, Iterable, List, Optional, TypeVar
+from typing import Any, Iterable, List, Optional, TypeVar, Dict
 
 from google.cloud import aiplatform
 from google.cloud.aiplatform import (
@@ -14,11 +16,13 @@ from google.cloud.aiplatform import (
 )
 from google.cloud.aiplatform.base import VertexAiResourceNoun
 from google.cloud.aiplatform.models import Model, VersionInfo
-from pydantic import PrivateAttr
+from pydantic import PrivateAttr, root_validator
 from pydantic.fields import Field
 
 import datahub.emitter.mce_builder as builder
+from datahub.configuration import ConfigModel
 from datahub.configuration.source_common import EnvConfigMixin
+from datahub.configuration.validate_multiline_string import pydantic_multiline_string
 from datahub.emitter.mcp import MetadataChangeProposalWrapper
 from datahub.ingestion.api.common import PipelineContext
 from datahub.ingestion.api.decorators import (
@@ -31,7 +35,6 @@ from datahub.ingestion.api.decorators import (
 from datahub.ingestion.api.source import Source, SourceCapability, SourceReport
 from datahub.ingestion.api.source_helpers import auto_workunit
 from datahub.ingestion.api.workunit import MetadataWorkUnit
-from datahub.ingestion.source.common.configs import GCPCredential
 from datahub.metadata._schema_classes import (
     AuditStampClass,
     DataProcessInstanceInputClass,
@@ -50,6 +53,51 @@ from datahub.metadata.schema_classes import (
 T = TypeVar("T")
 
 logger = logging.getLogger(__name__)
+
+class GCPCredential(ConfigModel):
+
+    private_key_id: str = Field(description="Private key id")
+    private_key: str = Field(
+        description="Private key in a form of '-----BEGIN PRIVATE KEY-----\\nprivate-key\\n-----END PRIVATE KEY-----\\n'"
+    )
+    client_email: str = Field(description="Client email")
+    client_id: str = Field(description="Client Id")
+    auth_uri: str = Field(
+        default="https://accounts.google.com/o/oauth2/auth",
+        description="Authentication uri",
+    )
+    token_uri: str = Field(
+        default="https://oauth2.googleapis.com/token", description="Token uri"
+    )
+    auth_provider_x509_cert_url: str = Field(
+        default="https://www.googleapis.com/oauth2/v1/certs",
+        description="Auth provider x509 certificate url",
+    )
+    type: str = Field(default="service_account", description="Authentication type")
+    client_x509_cert_url: Optional[str] = Field(
+        default=None,
+        description="If not set it will be default to https://www.googleapis.com/robot/v1/metadata/x509/client_email",
+    )
+
+    _fix_private_key_newlines = pydantic_multiline_string("private_key")
+
+    @root_validator(skip_on_failure=True)
+    def validate_config(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        if values.get("client_x509_cert_url") is None:
+            values["client_x509_cert_url"] = (
+                f"https://www.googleapis.com/robot/v1/metadata/x509/{values['client_email']}"
+            )
+        return values
+
+    def create_credential_temp_file(self, project_id:str) -> str:
+
+        # Adding project_id from the top level config
+        configs = self.dict()
+        configs["project_id"] = project_id
+        with tempfile.NamedTemporaryFile(delete=False) as fp:
+            cred_json = json.dumps(configs, indent=4, separators=(",", ": "))
+            fp.write(cred_json.encode())
+            return fp.name
 
 
 class VertexAIConfig(EnvConfigMixin):
@@ -75,7 +123,7 @@ class VertexAIConfig(EnvConfigMixin):
         super().__init__(**data)
 
         if self.credential:
-            self._credentials_path = self.credential.create_credential_temp_file()
+            self._credentials_path = self.credential.create_credential_temp_file(self.project_id)
             logger.debug(
                 f"Creating temporary credential file at {self._credentials_path}"
             )
