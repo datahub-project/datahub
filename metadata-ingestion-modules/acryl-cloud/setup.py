a9a7d2a1f43f0d7fabc49c8092ce2b7fd293a98d
Aseem Bansal <asmbansal2@gmail.com>
2025-02-28 13:08:10 +0530

    feat(proposal): action request owner source (#5052)

--- metadata-ingestion-modules/acryl-cloud/setup.py
+++ metadata-ingestion-modules/acryl-cloud/setup.py
@@ -73,6 +73,7 @@ setup(
                 "datahub-metadata-sharing = acryl_datahub_cloud.datahub_metadata_sharing.metadata_sharing_source:DataHubMetadataSharingSource",
                 "acryl-cs-issues = acryl_datahub_cloud.acryl_cs_issues.source:AcrylCSIssuesSource",
                 "datahub-restore = acryl_datahub_cloud.datahub_restore.source:DataHubRestoreSource",
+                "datahub-action-request-owner = acryl_datahub_cloud.action_request.action_request_owner_source:ActionRequestOwnerSource",
             ],
         },
         "include_package_data": True,
