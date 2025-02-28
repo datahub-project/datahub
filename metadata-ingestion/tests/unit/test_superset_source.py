f816fdfe8fee7875eed4e39855bd4bacb0d478a5
PeteMango <whcpeterwang@gmail.com>
2025-02-28 10:20:23 -0500

    owner dict tests

--- metadata-ingestion/tests/unit/test_superset_source.py
+++ metadata-ingestion/tests/unit/test_superset_source.py
@@ -1,4 +1,5 @@
-from datahub.ingestion.source.superset import SupersetConfig
+from datahub.ingestion.api.common import PipelineContext
+from datahub.ingestion.source.superset import SupersetConfig, SupersetSource
 
 
 def test_default_values():
@@ -19,3 +20,60 @@ def test_set_display_uri():
 
     assert config.connect_uri == "http://localhost:8088"
     assert config.display_uri == display_uri
+
+
+def test_superset_login(requests_mock):
+    login_url = "http://localhost:8088/api/v1/security/login"
+    requests_mock.post(login_url, json={"access_token": "dummy_token"}, status_code=200)
+
+    dashboard_url = "http://localhost:8088/api/v1/dashboard/"
+    requests_mock.get(dashboard_url, json={}, status_code=200)
+
+    for entity in ["dataset", "dashboard", "chart"]:
+        requests_mock.get(
+            f"http://localhost:8088/api/v1/{entity}/related/owners",
+            json={},
+            status_code=200,
+        )
+
+    source = SupersetSource(
+        ctx=PipelineContext(run_id="superset-source-test"), config=SupersetConfig()
+    )
+    assert source.platform == "superset"
+
+
+def test_superset_build_owners_info(requests_mock):
+    login_url = "http://localhost:8088/api/v1/security/login"
+    requests_mock.post(login_url, json={"access_token": "dummy_token"}, status_code=200)
+
+    dashboard_url = "http://localhost:8088/api/v1/dashboard/"
+    requests_mock.get(dashboard_url, json={}, status_code=200)
+
+    for entity in ["dataset", "dashboard", "chart"]:
+        requests_mock.get(
+            f"http://localhost:8088/api/v1/{entity}/related/owners",
+            json={
+                "count": 2,
+                "result": [
+                    {
+                        "extra": {"active": "false", "email": "test_user1@example.com"},
+                        "text": "Test User1",
+                        "value": 1,
+                    },
+                    {
+                        "extra": {"active": "false", "email": "test_user2@example.com"},
+                        "text": "Test User2",
+                        "value": 2,
+                    },
+                ],
+            },
+            status_code=200,
+        )
+
+    source = SupersetSource(
+        ctx=PipelineContext(run_id="superset-source-test"), config=SupersetConfig()
+    )
+    assert source.owner_info == {
+        1: "test_user1@example.com",
+        2: "test_user2@example.com",
+    }
