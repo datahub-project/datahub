20a213e3bfbc9dbaa37540f37e8bf607ab9d7236
Harshal Sheth <hsheth2@gmail.com>
2025-02-27 21:08:36 -0800

    setup compile filters helper

--- metadata-ingestion/src/datahub/sdk/search_client.py
+++ metadata-ingestion/src/datahub/sdk/search_client.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 from typing import (
     TYPE_CHECKING,
+    Dict,
     Iterable,
+    List,
     Optional,
 )
 
+from datahub.ingestion.graph.filters import RawSearchFilterRule
 from datahub.metadata.urns import Urn
 from datahub.sdk.search_filters import Filter
 
@@ -17,25 +20,30 @@ class SearchClient:
     def __init__(self, client: DataHubClient):
         self._client = client
 
+    @classmethod
+    def _compile_filters(
+        cls, filter: Optional[Filter]
+    ) -> Optional[List[Dict[str, List[RawSearchFilterRule]]]]:
+        # TODO: not every filter type is supported for every entity type
+        # if we can detect issues with the filters at compile time, we should
+        # raise an error
+
+        if filter is None:
+            return None
+
+        initial_filters = filter.compile()
+        return [
+            {"and": [rule.to_raw() for rule in andClause["and"]]}
+            for andClause in initial_filters
+        ]
+
     def get_urns(
         self,
         query: Optional[str] = None,
         filter: Optional[Filter] = None,
     ) -> Iterable[Urn]:
-        # TODO: not every filter type is supported for every entity type
-        # if we can detect issues with the filters at compile time, we should
-        # raise an error
-
-        compiled_filters = None
-        if filter:
-            initial_filters = filter.compile()
-            compiled_filters = [
-                {"and": [rule.to_raw() for rule in andClause["and"]]}
-                for andClause in initial_filters
-            ]
-
         for urn in self._client._graph.get_urns_by_filter(
             query=query,
-            extra_or_filters=compiled_filters,
+            extra_or_filters=self._compile_filters(filter),
         ):
             yield Urn.from_string(urn)
--- metadata-ingestion/src/datahub/sdk/search_filters.py
+++ metadata-ingestion/src/datahub/sdk/search_filters.py
@@ -26,8 +26,10 @@ _OrFilters = List[_AndSearchFilterRule]
 
 class _BaseFilter(ConfigModel):
     class Config:
-        allow_population_by_field_name = True
-        populate_by_name = True
+        if PYDANTIC_VERSION_2:
+            populate_by_name = True
+        else:
+            allow_population_by_field_name = True
 
     @abc.abstractmethod
     def compile(self) -> _OrFilters:
