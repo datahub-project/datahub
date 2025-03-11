2eaf95dd6ea3155c72583632b7a95587afd674a0
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2025-03-11 13:56:35 -0700

    fix(graphql/search): Remove schema field and data process instance from default search types

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchUtils.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchUtils.java
@@ -90,9 +90,7 @@ public class SearchUtils {
           EntityType.DOMAIN,
           EntityType.DATA_PRODUCT,
           EntityType.NOTEBOOK,
-          EntityType.BUSINESS_ATTRIBUTE,
-          EntityType.SCHEMA_FIELD,
-          EntityType.DATA_PLATFORM_INSTANCE);
+          EntityType.BUSINESS_ATTRIBUTE);
 
   /** Entities that are part of autocomplete by default in Auto Complete Across Entities */
   public static final List<EntityType> AUTO_COMPLETE_ENTITY_TYPES =
