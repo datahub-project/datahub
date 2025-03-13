a66f7a9de29bc033721e2e98c9dec7a9abb5c064
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-03-13 16:35:14 +0300

    feat(UI): add autocomplete component to components library

---
+++ datahub-web-react/src/alchemy-components/components/Utils/ClickOutside/ClickOutside.tsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useRef } from 'react';
+import { ClickOutsideProps } from './types';
+
+export default function ClickOutside({
+    children,
+    onClickOutside,
+    outsideSelector,
+    ignoreSelector,
+    ignoreWrapper,
+}: React.PropsWithChildren<ClickOutsideProps>) {
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        /**
+         * Handles click events outside the wrapper or based on selectors.
+         */
+        const handleClickOutside = (event: MouseEvent): void => {
+            const target = event.target as HTMLElement;
+
+            const isInsideOfWrapper = (wrapperRef.current as HTMLDivElement).contains((event.target as Node) || null);
+            console.log(event.target, wrapperRef.current);
+
+            // Ignore clicks on elements matching `ignoreSelector`
+            if (ignoreSelector && target.closest(ignoreSelector)) {
+                return;
+            }
+
+            // Trigger `onClickOutside` if the click is on an element matching `outsideSelector`
+            if (outsideSelector && target.closest(outsideSelector)) {
+                return;
+            }
+
+            // Trigger `onClickOutside` if the click is outside the wrapper
+            if (!ignoreWrapper && !isInsideOfWrapper) {
+                onClickOutside(event);
+            }
+        };
+
+        if (wrapperRef && wrapperRef.current) {
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+        return () => document.removeEventListener('mousedown', handleClickOutside);
+    }, [onClickOutside, ignoreSelector, outsideSelector, ignoreWrapper]);
+
+    return <div ref={wrapperRef}>{children}</div>;
+}
