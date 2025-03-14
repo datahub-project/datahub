1a4dbadf27edc484d99a53cca59f41a6ce00f173
Victor Tarasevich <v.tarasevitch@invento.by>
2025-03-14 11:46:20 +0300

    feat(searchBarAutocomplete): add autocomplete entity item component

---
+++ datahub-web-react/src/app/searchV2/autoCompleteV2/components/icon/UserEntityIcon.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Avatar } from '@src/alchemy-components';
+import { isItCorpUserEntity } from '@src/app/entityV2/user/utils';
+import { useEntityRegistryV2 } from '@src/app/useEntityRegistry';
+import { EntityIconProps } from './types';
+
+export default function UserEntityIcon({ entity }: EntityIconProps) {
+    const entityRegistry = useEntityRegistryV2();
+
+    if (!isItCorpUserEntity(entity)) return null;
+
+    const imageUrl = entity?.editableProperties?.pictureLink;
+    const displayName = entityRegistry.getDisplayName(entity.type, entity);
+
+    return <Avatar name={displayName} imageUrl={imageUrl} size="md" />;
+}
