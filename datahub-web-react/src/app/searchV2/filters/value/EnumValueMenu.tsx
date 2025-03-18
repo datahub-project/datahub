764f9838f47801054769b5bba9ee5f1160436692
Saketh Varma <sakethvarma397@gmail.com>
2025-03-18 16:58:51 -0300

    fix(UI/Proposals): Pass EntityTypes through default values aggregation query (#5354)

--- datahub-web-react/src/app/searchV2/filters/value/EnumValueMenu.tsx
+++ datahub-web-react/src/app/searchV2/filters/value/EnumValueMenu.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { EntityType } from '@src/types.generated';
 import { FilterField, FilterValue, FilterValueOption } from '../types';
 import { mapFilterOption } from '../mapFilterOption';
 import { useEntityRegistry } from '../../../useEntityRegistry';
@@ -16,6 +17,7 @@ interface Props {
     type?: 'card' | 'default';
     includeCount?: boolean;
     className?: string;
+    aggregationsEntityTypes?: Array<EntityType>;
 }
 
 export default function EnumValueMenu({
@@ -27,6 +29,7 @@ export default function EnumValueMenu({
     onChangeValues,
     onApply,
     className,
+    aggregationsEntityTypes,
 }: Props) {
     const entityRegistry = useEntityRegistry();
     const displayName = useFilterDisplayName(field);
@@ -35,7 +38,12 @@ export default function EnumValueMenu({
     const [searchQuery, setSearchQuery] = useState<string | undefined>(undefined);
 
     // Here we optionally load the aggregation options, which are the options that are displayed by default.
-    const { options: aggOptions, loading: aggLoading } = useLoadAggregationOptions(field, true, includeCount);
+    const { options: aggOptions, loading: aggLoading } = useLoadAggregationOptions({
+        field,
+        visible: true,
+        includeCounts: includeCount,
+        aggregationsEntityTypes,
+    });
 
     const allOptions = [...defaultOptions, ...deduplicateOptions(defaultOptions, aggOptions)];
 
