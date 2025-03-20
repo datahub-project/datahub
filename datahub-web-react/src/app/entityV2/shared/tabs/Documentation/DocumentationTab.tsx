f3b3c7ee4d213272aa7a3f56d440591e141c646e
Chris Collins <chriscollins3456@gmail.com>
2025-03-20 18:03:45 -0400

    fix generate with ai docs button in documentation tab alignment

--- datahub-web-react/src/app/entityV2/shared/tabs/Documentation/DocumentationTab.tsx
+++ datahub-web-react/src/app/entityV2/shared/tabs/Documentation/DocumentationTab.tsx
@@ -144,7 +144,7 @@ export const DocumentationTab = ({ properties }: { properties?: Props }) => {
                         </Button>
                         {shouldShowInferenceButton && (
                             <InferDocsButton
-                                style={{ display: 'inline-block', top: 3, marginLeft: 8, height: 40 }}
+                                style={{ height: 38 }}
                                 surface="entity-docs-tab"
                                 onClick={() =>
                                     routeToTab({
