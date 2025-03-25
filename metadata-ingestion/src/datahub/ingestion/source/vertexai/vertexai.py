f3a37a74e8ec77b4802a914a8f788e8d52e4cc32
Ryota Egashira <ryota.egashira@acryl.io>
2025-03-25 00:57:48 -0700

    adding minor change

--- metadata-ingestion/src/datahub/ingestion/source/vertexai/vertexai.py
+++ metadata-ingestion/src/datahub/ingestion/source/vertexai/vertexai.py
@@ -107,7 +107,6 @@ class ContainerKeyWithId(ContainerKey):
     SourceCapability.DESCRIPTIONS,
     "Extract descriptions for Vertex AI Registered Models and Model Versions",
 )
-@capability(SourceCapability.TAGS, "Extract tags for Vertex AI Registered Model Stages")
 class VertexAISource(Source):
     platform: str = "vertexai"
 
