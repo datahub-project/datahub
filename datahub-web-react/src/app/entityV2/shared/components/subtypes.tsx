29ddaa520df6f1b236b007b8a3ccc090cf664813
Ryota Egashira <ryota.egashira@acryl.io>
2025-03-28 15:06:23 -0700

    fixing sql icon in task heading in task card

--- datahub-web-react/src/app/entityV2/shared/components/subtypes.tsx
+++ datahub-web-react/src/app/entityV2/shared/components/subtypes.tsx
@@ -10,6 +10,7 @@ import ViewComfyOutlinedIcon from '@mui/icons-material/ViewComfyOutlined';
 import TableauWorkbookLogo from '../../../../images/tableau-workbook.svg?react';
 import TableauEmbeddedDataSourceLogo from '../../../../images/tableau-embedded-data-source.svg?react';
 import TableauPublishedDataSourceLogo from '../../../../images/tableau-published-data-source.svg?react';
+import VertexAILogo from '../../../../images/vertexai.png?react';
 
 export enum SubType {
     Database = 'Database',
@@ -25,6 +26,7 @@ export enum SubType {
     LookerExplore = 'Explore',
     Looker = 'Look',
     DbtSource = 'Source',
+    VertexAIPipelineTask = 'Pipeline Task',
 }
 
 export const TYPE_ICON_CLASS_NAME = 'typeIcon';
@@ -61,6 +63,12 @@ export function getSubTypeIcon(subType?: string): JSX.Element | undefined {
     }
     if (lowerSubType === SubType.TableauWorksheet.toLowerCase()) {
         return <LineChartOutlined className={TYPE_ICON_CLASS_NAME} />;
+    }
+    if (lowerSubType === SubType.TableauWorksheet.toLowerCase()) {
+        return <LineChartOutlined className={TYPE_ICON_CLASS_NAME} />;
+    }
+    if (lowerSubType === SubType.VertexAIPipelineTask.toLowerCase()) {
+        return <Icon component={VertexAILogo} className={TYPE_ICON_CLASS_NAME} />;
     }
     return undefined;
 }
