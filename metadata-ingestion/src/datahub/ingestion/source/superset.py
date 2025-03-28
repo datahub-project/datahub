e0c01aaeb6a3956d0b752181e80c4e0711693b93
PeteMango <whcpeterwang@gmail.com>
2025-03-27 22:18:42 -0400

    ruff check

--- metadata-ingestion/src/datahub/ingestion/source/superset.py
+++ metadata-ingestion/src/datahub/ingestion/source/superset.py
@@ -1,6 +1,6 @@
-import os
 import json
 import logging
+import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import dataclass, field
 from datetime import datetime
