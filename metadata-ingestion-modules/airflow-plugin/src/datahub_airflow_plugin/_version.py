890b38fdff96a3ce04eedb108002bea440d7b0a0
최경륜 매니저/데이터엔지니어링팀 <kyungryun@hyundai.com>
2025-04-02 08:33:58 +0900

    :art: fix: airflow plugin 이름 변경
    - dsp-datahub-airflow-plugin 이름 변경

--- metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/_version.py
+++ metadata-ingestion-modules/airflow-plugin/src/datahub_airflow_plugin/_version.py
@@ -1,3 +1,3 @@
 # Published at https://pypi.org/project/acryl-datahub-airflow-plugin/.
-__package_name__ = "datahub-datahub-airflow-plugin"
+__package_name__ = "dsp-datahub-airflow-plugin"
 __version__ = "1.0.0"
