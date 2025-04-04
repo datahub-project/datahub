530aa79c76e396985df544e78161c2384128a87f
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-04-04 13:57:08 +0300

    feat(searchBarAutocomplete/select): extract dropdown select all component

--- datahub-web-react/src/alchemy-components/components/Select/components.ts
+++ datahub-web-react/src/alchemy-components/components/Select/components.ts
@@ -153,17 +153,6 @@ export const OptionLabel = styled.label<{
 }>(({ isSelected, isMultiSelect, isDisabled, applyHoverWidth }) => ({
     ...getOptionLabelStyle(isSelected, isMultiSelect, isDisabled, applyHoverWidth),
 }));
-export const SelectAllOption = styled.div<{ isSelected: boolean; isDisabled?: boolean }>(
-    ({ isSelected, isDisabled }) => ({
-        cursor: isDisabled ? 'not-allowed' : 'pointer',
-        padding: spacing.xsm,
-        color: isSelected ? colors.violet[700] : colors.gray[500],
-        fontWeight: typography.fontWeights.semiBold,
-        fontSize: typography.fontSizes.md,
-        display: 'flex',
-        alignItems: 'center',
-    }),
-);
 
 export const SelectLabel = styled.label({
     ...formLabelTextStyles,
