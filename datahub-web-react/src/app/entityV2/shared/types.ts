318af6d78760faa07f2d0daf3d002f67ab6cebfe
Annadoesdesign <annaerocca@gmail.com>
2025-04-04 10:01:20 -0700

    Match navigation with sidebar icons fill when selected

--- datahub-web-react/src/app/entityV2/shared/types.ts
+++ datahub-web-react/src/app/entityV2/shared/types.ts
@@ -70,6 +70,7 @@ export type EntitySidebarTab = {
     name: string;
     component: React.FunctionComponent<EntityTabProps>;
     icon: React.FunctionComponent<any>;
+    selectedIcon?: React.FunctionComponent<any>; // Icon to use when this tab is selected
     display?: {
         visible: (GenericEntityProperties, T) => boolean; // Whether the tab is visible on the UI. Defaults to true.
         enabled: (GenericEntityProperties, T) => boolean; // Whether the tab is enabled on the UI. Defaults to true.
