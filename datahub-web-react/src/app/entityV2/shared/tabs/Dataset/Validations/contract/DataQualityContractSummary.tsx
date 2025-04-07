bc89e1f76f20eb14c8de2b39b58628c91cb4a96b
vladimirivkovic <vlado.ivkovic@live.com>
2025-04-07 09:04:15 +0200

    feat(ui): add custom dataset assertion description to data contract summary

--- datahub-web-react/src/app/entityV2/shared/tabs/Dataset/Validations/contract/DataQualityContractSummary.tsx
+++ datahub-web-react/src/app/entityV2/shared/tabs/Dataset/Validations/contract/DataQualityContractSummary.tsx
@@ -51,6 +51,7 @@ export const DataQualityContractSummary = ({ contracts, showAction = false }: Pr
                 <>
                     {assertion.info?.datasetAssertion && (
                         <DatasetAssertionDescription
+                            description={assertion.info?.description?.value}
                             assertionInfo={assertion.info?.datasetAssertion as DatasetAssertionInfo}
                         />
                     )}
