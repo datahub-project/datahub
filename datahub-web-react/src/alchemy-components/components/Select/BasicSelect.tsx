81f36d82248e587a1d5df0d680a2a4e419319bcf
Anna Everhart <149417426+annadoesdesign@users.noreply.github.com>
2025-04-07 16:27:42 -0700

    refactor(ui): Updating icons, gaps, and pills in selects (#13098)

--- datahub-web-react/src/alchemy-components/components/Select/BasicSelect.tsx
+++ datahub-web-react/src/alchemy-components/components/Select/BasicSelect.tsx
@@ -43,7 +43,7 @@ const SelectActionButtons = ({
                     onClick={handleClearSelection}
                 />
             )}
-            <StyledIcon icon="ChevronLeft" rotate={isOpen ? '90' : '270'} size="lg" />
+            <StyledIcon icon="CaretDown" source="phosphor" rotate={isOpen ? '180' : '0'} size="md" color="gray" />
         </ActionButtonsContainer>
     );
 };
