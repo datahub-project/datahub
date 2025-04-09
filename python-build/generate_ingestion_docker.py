a9021f48f5544cbde934c5f50becaf8fd96191a2
Harshal Sheth <hsheth2@gmail.com>
2025-04-09 00:08:23 -0700

    more dockerfile updates

--- python-build/generate_ingestion_docker.py
+++ python-build/generate_ingestion_docker.py
@@ -1,5 +1,7 @@
 import re
+import sys
 from pathlib import Path
+from typing import Optional
 
 _repo_root = Path(__file__).parent.parent
 assert (_repo_root / ".git").exists(), "Unable to find git repo root"
@@ -15,36 +17,71 @@ def _load_file(path: str, context_dir: Path) -> str:
         )
 
 
-def update_template(template_file: Path) -> None:
+def update_template(
+    template_file: Path,
+    outfile: Optional[Path] = None,
+    check_only: bool = False,
+) -> None:
     """
     Update a template file in-place, injecting content from files referenced in inline directives.
 
     Args:
         template_file: Path to the template file that will be modified
     """
+
+    render_mode = bool(outfile)
+
+    subs = 0
     content = template_file.read_text()
 
     def handle_multiline(match: re.Match) -> str:
+        nonlocal subs
+        subs += 1
+
         path = match.group(2)
         replacement = _load_file(path, template_file.parent).strip()
         replacement = replacement.strip() + "\n"
-        return f"{match.group(1)}{replacement}{match.group(3)}"
+
+        if render_mode:
+            return f"{replacement}"
+        else:
+            return f"{match.group(1)}{replacement}{match.group(3)}"
 
     # Handle multiline inline directives
     content = re.sub(
-        r"(# INLINE-BEGIN (.*?))\n.*?\n([ \t]*# INLINE-END \2)",
+        r"^([ \t]*# INLINE-BEGIN (.*?)\n).*?^([ \t]*# INLINE-END)$",
         handle_multiline,
         content,
-        flags=re.DOTALL,
+        flags=re.DOTALL | re.MULTILINE,
     )
 
-    template_file.write_text(content)
+    # if subs == 0:
+    #     raise ValueError(f"No templates found in {template_file}")
+
+    output = outfile or template_file
+    if check_only:
+        if output.read_text() != content:
+            print(f"ERROR: {template_file} is out of date")
+            sys.exit(1)
+    else:
+        print(f"Applied {subs} substitutions while processing {template_file}")
+        output.write_text(content)
 
 
 if __name__ == "__main__":
-    files = [
+    if len(sys.argv) > 1 and sys.argv[1] == "--check":
+        check_only = True
+    else:
+        check_only = False
+
+    update_template(
+        Path(_repo_root / "docker/snippets/ingestion_base.template"),
+        outfile=Path(_repo_root / "docker/snippets/ingestion_base"),
+        check_only=check_only,
+    )
+
+    for file in [
         "docker/datahub-ingestion-base/Dockerfile",
         "docker/datahub-ingestion/Dockerfile",
-    ]
-    for file in files:
-        update_template(Path(_repo_root / file))
+    ]:
+        update_template(Path(_repo_root / file), check_only=check_only)
