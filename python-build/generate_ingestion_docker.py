38eb705c0ff1462d4856677fa9e492c257b231fc
Harshal Sheth <hsheth2@gmail.com>
2025-04-08 23:57:51 -0700

    more tweaks

--- python-build/generate_ingestion_docker.py
+++ python-build/generate_ingestion_docker.py
@@ -24,32 +24,15 @@ def update_template(template_file: Path) -> None:
     """
     content = template_file.read_text()
 
-    def handle_single_line(match: re.Match) -> str:
-        path = match.group(1)
-        replacement = _load_file(path, template_file.parent)
-
-        replacement = replacement.strip()
-        if "\n" in replacement:
-            raise ValueError(
-                f"{path} contains a newline, so it cannot be used in a single-line inline directive"
-            )
-
-        return f"# INLINE-SINGLE-LINE {path}\n{replacement.strip()}\n"
-
     def handle_multiline(match: re.Match) -> str:
         path = match.group(2)
         replacement = _load_file(path, template_file.parent).strip()
         replacement = replacement.strip() + "\n"
         return f"{match.group(1)}{replacement}{match.group(3)}"
 
-    # Handle single-line inline directives
-    content = re.sub(
-        r"# INLINE-SINGLE-LINE (.*?)\n(.*?)\n", handle_single_line, content
-    )
-
     # Handle multiline inline directives
     content = re.sub(
-        r"(# INLINE-BEGIN (.*?)\n).*?(# INLINE-END)",
+        r"(# INLINE-BEGIN (.*?))\n.*?\n([ \t]*# INLINE-END \2)",
         handle_multiline,
         content,
         flags=re.DOTALL,
@@ -62,7 +45,6 @@ if __name__ == "__main__":
     files = [
         "docker/datahub-ingestion-base/Dockerfile",
         "docker/datahub-ingestion/Dockerfile",
-        "docker/datahub-ingestion/Dockerfile-slim-only",
     ]
     for file in files:
         update_template(Path(_repo_root / file))
