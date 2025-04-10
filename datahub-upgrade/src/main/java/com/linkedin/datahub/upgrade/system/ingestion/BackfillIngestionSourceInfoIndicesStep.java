22f8f1a04b7c82e4c50f7b64edb74fdd70b6732d
David Leifker <david.leifker@acryl.io>
2025-04-10 12:31:03 -0500

    lint

--- datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/system/ingestion/BackfillIngestionSourceInfoIndicesStep.java
+++ datahub-upgrade/src/main/java/com/linkedin/datahub/upgrade/system/ingestion/BackfillIngestionSourceInfoIndicesStep.java
@@ -15,7 +15,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class BackfillIngestionSourceInfoIndicesStep extends AbstractMCLStep {
 
-  private static final String UPGRADE_ID = BackfillIngestionSourceInfoIndices.class.getSimpleName() + "-v2";
+  private static final String UPGRADE_ID =
+      BackfillIngestionSourceInfoIndices.class.getSimpleName() + "-v2";
   private static final Urn UPGRADE_ID_URN = BootstrapStep.getUpgradeUrn(UPGRADE_ID);
 
   public BackfillIngestionSourceInfoIndicesStep(
