c8d09d1fa71b5dec996c00d38236dd200f419b86
treff7es <treff7es@gmail.com>
2025-04-11 13:39:25 +0200

    Ignore more file from doc

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -1030,6 +1030,8 @@ module.exports = {
     //"docs/how/graph-onboarding",
     //"docs/demo/graph-onboarding",
     //"datahub-actions/README",
+    //"datahub-actions/src/datahub_actions/plugin/action/tag/README",
+    //"datahub-actions/src/datahub_actions/plugin/action/term/README",
     //"metadata-integration/java/spark-lineage/README",
     // "metadata-integration/java/acryl-spark-lineage/README.md
     // "metadata-integration/java/openlineage-converter/README"
