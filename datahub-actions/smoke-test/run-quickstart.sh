8e8b143a0f9b0b6e6c7ef1c3a81249b349c4fe71
treff7es <treff7es@gmail.com>
2025-04-11 12:24:28 +0200

    Remove DATAHIB_ACTIONS_VERSION variable

--- datahub-actions/smoke-test/run-quickstart.sh
+++ datahub-actions/smoke-test/run-quickstart.sh
@@ -10,10 +10,7 @@ source venv/bin/activate
 mkdir -p ~/.datahub/plugins/frontend/auth/
 echo "test_user:test_pass" >> ~/.datahub/plugins/frontend/auth/user.props
 
-echo "DATAHUB_ACTIONS_VERSION = $DATAHUB_ACTIONS_VERSION"
-
 DATAHUB_TELEMETRY_ENABLED=false  \
-ACTIONS_VERSION=${DATAHUB_ACTIONS_VERSION}  \
 DATAHUB_ACTIONS_IMAGE=acryldata/datahub-actions-slim \
 datahub docker quickstart
 
@@ -24,7 +21,6 @@ python inject_actions_env_file.py ~/.datahub/quickstart/docker-compose.yml
 # Then we run quickstart again with the modified docker-compose file
 
 DATAHUB_TELEMETRY_ENABLED=false \
-ACTIONS_VERSION=${DATAHUB_ACTIONS_VERSION}  \
 ACTIONS_ENV_FILE=`pwd`/tests/resources/actions.env  \
 DATAHUB_ACTIONS_IMAGE=acryldata/datahub-actions-slim \
 datahub docker quickstart -f ~/.datahub/quickstart/docker-compose.yml
