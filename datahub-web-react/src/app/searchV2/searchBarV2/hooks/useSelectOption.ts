a3b3a91ee656d9a2c84f8bf43fe00f372961d876
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-04-14 12:56:25 +0300

    refactoring(searchBarAutocomplete): move state from searchablePage to the new searchBar

--- datahub-web-react/src/app/searchV2/searchBarV2/hooks/useSelectOption.ts
+++ datahub-web-react/src/app/searchV2/searchBarV2/hooks/useSelectOption.ts
@@ -5,8 +5,10 @@ import { EXACT_AUTOCOMPLETE_OPTION_TYPE, RELEVANCE_QUERY_OPTION_TYPE } from '@ap
 import filterSearchQuery from '@app/searchV2/utils/filterSearchQuery';
 import analytics, { Event, EventType } from '@src/app/analytics';
 import { getEntityPath } from '@src/app/entityV2/shared/containers/profile/utils';
+import { isEntityType } from '@src/app/entityV2/shared/utils';
 import { useEntityRegistryV2 } from '@src/app/useEntityRegistry';
 import { FacetFilterInput } from '@src/types.generated';
+import { Option } from '@app/searchV2/searchBarV2/types';
 
 export default function useSelectOption(
     onSearch: (query: string, filters?: FacetFilterInput[]) => void,
@@ -17,15 +19,17 @@ export default function useSelectOption(
     const entityRegistry = useEntityRegistryV2();
 
     return useCallback(
-        (value, option) => {
+        (value: string, option: Option) => {
             // If the autocomplete option type is NOT an entity, then render as a normal search query.
             if (option.type === EXACT_AUTOCOMPLETE_OPTION_TYPE || option.type === RELEVANCE_QUERY_OPTION_TYPE) {
-                onSearch(`${filterSearchQuery(value as string)}`, filters);
+                onSearch(`${filterSearchQuery(value)}`, filters);
                 analytics.event({
                     type: EventType.SelectAutoCompleteOption,
                     optionType: option.type,
                 } as Event);
             } else {
+                if (!isEntityType(option.type)) return console.warn('Unknown entity type', option.type);
+
                 // Navigate directly to the entity profile.
                 history.push(getEntityPath(option.type, value, entityRegistry, false, false));
                 onClear();
