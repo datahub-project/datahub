5e9959913b748233abb14f69328bfa590a642a74
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-04-14 15:41:07 +0300

    refactoring(searchBarAutocomplete/autocompleteItem): fix unique entities hook and add tests

---
+++ datahub-web-react/src/app/searchV2/autoCompleteV2/components/icon/__tests__/useUniqueEntitiesByPlatformUrn.test.tsx
@@ -0,0 +1,105 @@
+import { MockedProvider } from '@apollo/client/testing';
+import { CorpGroup, Dataset, EntityType, FabricType } from '@src/types.generated';
+import TestPageContainer from '@src/utils/test-utils/TestPageContainer';
+import { renderHook } from '@testing-library/react-hooks';
+import useUniqueEntitiesByPlatformUrn from '../useUniqueEntitiesByPlatformUrn';
+
+function generateSampleEntity(urn: string, platformUrn: string): Dataset {
+    return {
+        urn: urn,
+        type: EntityType.Dataset,
+        name: 'Test',
+        platform: {
+            name: 'Test',
+            type: EntityType.DataPlatform,
+            urn: platformUrn,
+        },
+        origin: FabricType.Test,
+    };
+}
+
+function generateSampleEntityWithoutPlatform(urn: string): CorpGroup {
+    return {
+        urn: urn,
+        type: EntityType.Dataset,
+        name: 'Test',
+    };
+}
+
+describe('useUniqueEntitiesByPlatformUrn', () => {
+    const wrapper = ({ children }) => (
+        <MockedProvider>
+            <TestPageContainer>{children}</TestPageContainer>
+        </MockedProvider>
+    );
+
+    it('should return entities with unique urns of platforms', () => {
+        const response = renderHook(
+            () =>
+                useUniqueEntitiesByPlatformUrn([
+                    generateSampleEntity('dataset1', 'platform1'),
+                    generateSampleEntity('dataset2', 'platform2'),
+                    generateSampleEntity('dataset3', 'platform2'),
+                ]),
+            { wrapper },
+        ).result.current;
+
+        expect(response).toStrictEqual([
+            {
+                urn: 'dataset1',
+                type: 'DATASET',
+                name: 'Test',
+                platform: { name: 'Test', type: 'DATA_PLATFORM', urn: 'platform1' },
+                origin: 'TEST',
+            },
+            {
+                urn: 'dataset2',
+                type: 'DATASET',
+                name: 'Test',
+                platform: { name: 'Test', type: 'DATA_PLATFORM', urn: 'platform2' },
+                origin: 'TEST',
+            },
+        ]);
+    });
+
+    it("should handle entities without platform's urn", () => {
+        const response = renderHook(
+            () =>
+                useUniqueEntitiesByPlatformUrn([
+                    generateSampleEntity('dataset1', 'platform1'),
+                    generateSampleEntity('dataset2', 'platform2'),
+                    generateSampleEntityWithoutPlatform('dataset3'),
+                ]),
+            { wrapper },
+        ).result.current;
+
+        expect(response).toStrictEqual([
+            {
+                urn: 'dataset1',
+                type: 'DATASET',
+                name: 'Test',
+                platform: { name: 'Test', type: 'DATA_PLATFORM', urn: 'platform1' },
+                origin: 'TEST',
+            },
+            {
+                urn: 'dataset2',
+                type: 'DATASET',
+                name: 'Test',
+                platform: { name: 'Test', type: 'DATA_PLATFORM', urn: 'platform2' },
+                origin: 'TEST',
+            },
+        ]);
+    });
+
+    it('should handle empty array', () => {
+        const response = renderHook(() => useUniqueEntitiesByPlatformUrn([]), { wrapper }).result.current;
+
+        expect(response).toStrictEqual([]);
+    });
+
+    it('should handle undefined input', () => {
+        const response = renderHook(() => useUniqueEntitiesByPlatformUrn(undefined), { wrapper }).result.current;
+
+        expect(response).toStrictEqual([]);
+    });
+});
