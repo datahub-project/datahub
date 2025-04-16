618ce840134895518b73fd88e586951575e02d38
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-04-16 11:21:54 +0300

    refactor(searchBarAutocomplete): adjust styles of loading state of the search bar

--- datahub-web-react/src/app/searchV2/searchBarV2/components/Skeleton.tsx
+++ datahub-web-react/src/app/searchV2/searchBarV2/components/Skeleton.tsx
@@ -1,11 +1,11 @@
 import { Skeleton as AntdSkeleton } from 'antd';
 import React from 'react';
 import styled from 'styled-components';
+import { useShowNavBarRedesign } from '@src/app/useShowNavBarRedesign';
 
-const SkeletonContainer = styled.div`
-    height: 40px;
-    width: 100%;
-    max-width: 620px;
+const SkeletonContainer = styled.div<{ $width: string }>`
+    height: 44px;
+    width: ${(props) => props.$width};
 `;
 
 const SkeletonButton = styled(AntdSkeleton.Button)`
@@ -16,8 +16,10 @@ const SkeletonButton = styled(AntdSkeleton.Button)`
 `;
 
 export default function Skeleton() {
+    const isShowNavBarRedesign = useShowNavBarRedesign();
+
     return (
-        <SkeletonContainer>
+        <SkeletonContainer $width={isShowNavBarRedesign ? '664px' : '620px'}>
             <SkeletonButton shape="square" active block />
         </SkeletonContainer>
     );
