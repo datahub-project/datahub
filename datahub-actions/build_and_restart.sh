2d25bc0ee267dac08f47bda5c851a195bd74b9c8
treff7es <treff7es@gmail.com>
2025-04-16 10:58:09 +0200

    Pr review fixes:

--- datahub-actions/build_and_restart.sh
+++
@@ -1,9 +0,0 @@
-set -euo pipefail
-
-cd ../acryl-executor
-RELEASE_SKIP_UPLOAD=1 RELEASE_VERSION=0.0.0.dev0 ./scripts/release.sh
-cd -
-docker build .. -f docker/datahub-actions/Dockerfile --progress plain -t acryldata/datahub-actions:debug
-DATAHUB_VERSION=head ../datahub/metadata-ingestion/venv/bin/datahub docker quickstart --no-pull-images
-
-
