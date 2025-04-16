4c6a19910b86d48119f4cdc1c2dba439eceb1d37
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2025-04-15 17:42:20 -0700

    attempt 2

--- docs-website/generateDocsDir.ts
+++ docs-website/generateDocsDir.ts
@@ -284,7 +284,7 @@ function markdown_add_slug(
 // }
 
 function trim_anchor_link(url: string): string {
-  return url.replace(/#.+$/, "").replace(/^<(.+)>$/, "$1");
+  return url.replace(/#.+$/, "").replace(/^(.*)<(.+)>$/, "$1$2");
 }
 
 function new_url(original: string, filepath: string): string {
