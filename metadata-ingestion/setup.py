9f662ab281c43ce162538aa9468767e457b6929f
Aseem Bansal <asmbansal2@gmail.com>
2025-04-22 20:14:11 +0530

    use mypy 1.10.1 explicitly

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -610,7 +610,7 @@ lint_requirements = {
     # This is pinned only to avoid spurious errors in CI.
     # We should make an effort to keep it up to date.
     "ruff==0.11.6",
-    "mypy==1.11.2",
+    "mypy==1.10.1",
 }
 
 base_dev_requirements = {
