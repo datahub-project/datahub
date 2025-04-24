31d62c93c06905eac5d2f6075bcb7d3263039e2f
Aseem Bansal <asmbansal2@gmail.com>
2025-04-24 14:54:21 +0530

    chore: update lint dependencies

--- metadata-ingestion-modules/airflow-plugin/setup.py
+++ metadata-ingestion-modules/airflow-plugin/setup.py
@@ -74,7 +74,7 @@ dev_requirements = {
     *mypy_stubs,
     "coverage>=5.1",
     "ruff==0.11.6",
-    "mypy==1.10.1",
+    "mypy==1.12.1",
     # pydantic 1.8.2 is incompatible with mypy 0.910.
     # See https://github.com/samuelcolvin/pydantic/pull/3175#issuecomment-995382910.
     "pydantic>=1.10",
