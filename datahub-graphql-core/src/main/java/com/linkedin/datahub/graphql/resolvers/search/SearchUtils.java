067912ef4a8e3960ee9c8a4e4f2652c28f3b0b58
Chris Collins <chriscollins3456@gmail.com>
2025-04-24 11:55:37 -0400

    fix(search) Remove schema field as default searchable entity type

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchUtils.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/search/SearchUtils.java
@@ -90,8 +90,7 @@ public class SearchUtils {
           EntityType.DOMAIN,
           EntityType.DATA_PRODUCT,
           EntityType.NOTEBOOK,
-          EntityType.BUSINESS_ATTRIBUTE,
-          EntityType.SCHEMA_FIELD);
+          EntityType.BUSINESS_ATTRIBUTE);
 
   /** Entities that are part of autocomplete by default in Auto Complete Across Entities */
   public static final List<EntityType> AUTO_COMPLETE_ENTITY_TYPES =
