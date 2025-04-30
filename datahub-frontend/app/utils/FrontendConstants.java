6325dd70179b98e7505c2db39d1d59bf96e4fcfe
RyanHolstien <RyanHolstien@users.noreply.github.com>
2025-04-30 14:02:10 -0500

    unused constant

--- datahub-frontend/app/utils/FrontendConstants.java
+++ datahub-frontend/app/utils/FrontendConstants.java
@@ -9,5 +9,4 @@ public class FrontendConstants {
   public static final String SIGN_UP_LINK_LOGIN = "signUpLinkLogin";
   public static final String GUEST_LOGIN = "guestLogin";
   public static final String SSO_LOGIN = "ssoLogin";
-  public static final String OIDC_IMPLICIT_LOGIN = "oidcImplicit";
 }
