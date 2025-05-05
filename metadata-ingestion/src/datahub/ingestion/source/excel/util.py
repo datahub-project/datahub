03850868f1ca27c38e9a985d5c1a021dd60ac61a
Michael Minichino <36450266+mminichino@users.noreply.github.com>
2025-05-05 15:31:44 -0500

    feat(ingest/excel): Add Excel Source

--- metadata-ingestion/src/datahub/ingestion/source/excel/util.py
+++ metadata-ingestion/src/datahub/ingestion/source/excel/util.py
@@ -1,9 +1,18 @@
-import re
+import os
 
 
 def gen_dataset_name(path: str, sheet_name: str, lower_case: bool) -> str:
-    sheet_name = re.sub(r"\s+", "_", sheet_name)
-    name = f"{path}:{sheet_name}"
+    sheet_name = sheet_name.strip()
+    directory, filename = os.path.split(path)
+
+    if not directory:
+        excel_path = f"[{filename}]"
+    else:
+        excel_path = os.path.join(directory, f"[{filename}]")
+
+    name = f"{excel_path}{sheet_name}"
+
     if lower_case:
         name = name.lower()
+
     return name
