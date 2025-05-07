2c612f7076d84e39f132fea8286d111658c37c1d
Hyejin Yoon <0327jane@gmail.com>
2025-05-07 16:32:22 +0900

    fix typo

--- metadata-ingestion/examples/library/lineage_dataset_transform.py
+++ metadata-ingestion/examples/library/lineage_dataset_transform.py
@@ -9,9 +9,9 @@ lineage_client = LineageClient(client=client)
 lineage_client.add_dataset_transform_lineage(
     upstream=DatasetUrn(platform="snowflake", name="source_table"),
     downstream=DatasetUrn(platform="snowflake", name="target_table"),
-    column_mapping={
+    column_lineage={
         "customer_id": ["id"],
         "full_name": ["first_name", "last_name"],
     },
 )
-# column_mapping is optional -- if not provided, table-level lineage is inferred.
+# column_lineage is optional -- if not provided, table-level lineage is inferred.
