e2a88f1662b37a79a44450730364aa7fb7f83dd4
Hyejin Yoon <0327jane@gmail.com>
2025-05-08 18:28:56 +0900

    add default_db and schema for example

--- metadata-ingestion/examples/library/lineage_dataset_from_sql.py
+++ metadata-ingestion/examples/library/lineage_dataset_from_sql.py
@@ -17,4 +17,11 @@ JOIN customers c ON s.customer_id = c.id
 GROUP BY p.product_name, c.customer_segment
 """
 
-lineage_client.add_dataset_lineage_from_sql(query_text=sql_query, platform="snowflake")
+# sales_summary will be assumed to be in the default db/schema
+# e.g. prod_db.public.sales_summary
+lineage_client.add_dataset_lineage_from_sql(
+    query_text=sql_query,
+    platform="snowflake",
+    default_db="prod_db",
+    default_schema="public",
+)
