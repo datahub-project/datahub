d943c9e227ff7b9b8385291d7c5cd011bece5a64
jayacryl <159848059+jayacryl@users.noreply.github.com>
2025-05-20 14:44:01 -0400

    lint

--- datahub-web-react/src/app/entityV2/glossaryNode/useGlossaryChildren.ts
+++ datahub-web-react/src/app/entityV2/glossaryNode/useGlossaryChildren.ts
@@ -44,7 +44,11 @@ export default function useGlossaryChildren({ entityUrn, skip }: Props) {
     const [searchData, setSearchData] = useState<Entity[]>([]);
     const [dataUrnsSet, setDataUrnsSet] = useState<Set<string>>(new Set());
     const [data, setData] = useState<Entity[]>([]);
-    const { data: scrollData, loading, refetch } = useScrollAcrossEntitiesQuery({
+    const {
+        data: scrollData,
+        loading,
+        refetch,
+    } = useScrollAcrossEntitiesQuery({
         variables: {
             ...getGlossaryChildrenScrollInput(entityUrn || '', scrollId),
         },
