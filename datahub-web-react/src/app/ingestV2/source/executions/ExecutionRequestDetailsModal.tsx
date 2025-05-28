82f2ab5c938f86771b6482152936194ebefb0efd
purnimagarg1 <purnima.garg@apptware.com>
2025-05-28 18:43:40 +0530

    Updates copy from Sync Details to Execution Run Details and use modal component

--- datahub-web-react/src/app/ingestV2/source/executions/ExecutionRequestDetailsModal.tsx
+++ datahub-web-react/src/app/ingestV2/source/executions/ExecutionRequestDetailsModal.tsx
@@ -1,6 +1,6 @@
 import { DownloadOutlined } from '@ant-design/icons';
-import { Icon, Pill } from '@components';
-import { Button, Modal, Typography, message } from 'antd';
+import { Icon, Modal, Pill } from '@components';
+import { Button, Typography, message } from 'antd';
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import YAML from 'yamljs';
@@ -25,11 +25,6 @@ import { Message } from '@app/shared/Message';
 import { useGetIngestionExecutionRequestQuery } from '@graphql/ingestion.generated';
 import { ExecutionRequestResult } from '@types';
 
-const StyledTitle = styled(Typography.Title)`
-    padding: 0px;
-    margin: 0px;
-`;
-
 const Section = styled.div`
     display: flex;
     flex-direction: column;
@@ -53,8 +48,6 @@ const SectionSubHeader = styled.div`
 const SubHeaderParagraph = styled(Typography.Paragraph)`
     margin-bottom: 0px;
 `;
-
-const HeaderSection = styled.div``;
 
 const StatusSection = styled.div`
     border-bottom: 1px solid ${ANTD_GRAY[4]};
@@ -180,19 +173,15 @@ export const ExecutionDetailsModal = ({ urn, open, onClose }: Props) => {
     return (
         <Modal
             width={800}
-            footer={<Button onClick={onClose}>Close</Button>}
             style={modalStyle}
             bodyStyle={modalBodyStyle}
-            title={
-                <HeaderSection>
-                    <StyledTitle level={4}>Sync Details</StyledTitle>
-                </HeaderSection>
-            }
+            title="Execution Run Details"
             open={open}
             onCancel={onClose}
+            buttons={[{ text: 'Close', variant: 'outline', onClick: onClose }]}
         >
-            {!data && loading && <Message type="loading" content="Loading sync details..." />}
-            {error && message.error('Failed to load sync details :(')}
+            {!data && loading && <Message type="loading" content="Loading execution run details..." />}
+            {error && message.error('Failed to load execution run details :(')}
             <Section>
                 <StatusSection>
                     <Typography.Title level={5}>Status</Typography.Title>
