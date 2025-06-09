1385ed590e3a337f93ae70e4ddea644d55372851
Gabe Lyons <gabe.lyons@acryl.io>
2025-06-09 06:22:36 -0700

    fixing test

--- metadata-io/src/test/java/com/linkedin/metadata/search/query/request/SearchRequestHandlerTest.java
+++ metadata-io/src/test/java/com/linkedin/metadata/search/query/request/SearchRequestHandlerTest.java
@@ -718,7 +718,8 @@ public class SearchRequestHandlerTest extends AbstractTestNGSpringContextTests {
             "glossaryTerms",
             "editedFieldTags",
             "displayName",
-            "title");
+            "title",
+            "applications");
 
     Map<EntityType, Set<String>> expectedQueryByDefault =
         ImmutableMap.<EntityType, Set<String>>builder()
