f7d5584df45251f0bc46d920edc3563163ca8afb
Gabe Lyons <gabe.lyons@acryl.io>
2025-06-08 18:06:18 -0700

    fixing application lint

--- smoke-test/tests/cypress/cypress/e2e/application/applications.js
+++ smoke-test/tests/cypress/cypress/e2e/application/applications.js
@@ -6,7 +6,9 @@ describe("applications", () => {
 
   it("can see elements inside the application", () => {
     cy.login();
-    cy.goToApplication("urn:li:application:d63587c6-cacc-4590-851c-4f51ca429b51/Assets");
+    cy.goToApplication(
+      "urn:li:application:d63587c6-cacc-4590-851c-4f51ca429b51/Assets",
+    );
     cy.contains("cypress_logging_events");
     cy.contains("1 - 1 of 1");
   });
