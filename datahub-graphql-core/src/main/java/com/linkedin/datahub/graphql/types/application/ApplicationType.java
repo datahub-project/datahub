bfdf63774f72616bcf313f5d5ee026c28203db10
Gabe Lyons <gabe.lyons@acryl.io>
2025-06-10 12:42:43 -0700

    adjusting applicatino to new search signature

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/application/ApplicationType.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/application/ApplicationType.java
@@ -108,7 +108,7 @@ public class ApplicationType
       @Nonnull String query,
       @Nullable String field,
       @Nullable Filter filters,
-      int limit,
+      @Nullable Integer limit,
       @Nonnull final QueryContext context)
       throws Exception {
     final AutoCompleteResult result =
@@ -122,7 +122,7 @@ public class ApplicationType
       @Nonnull String query,
       @Nullable List<FacetFilterInput> filters,
       int start,
-      int count,
+      @Nullable Integer count,
       @Nonnull final QueryContext context)
       throws Exception {
     throw new NotImplementedException(
