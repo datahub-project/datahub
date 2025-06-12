6686760ba3fce831a1514a56b946f31c0df0fb3b
Hyejin Yoon <0327jane@gmail.com>
2025-06-12 12:34:57 +0900

    add nit commnet

--- metadata-ingestion/examples/library/dataset_remove_structured_properties.py
+++ metadata-ingestion/examples/library/dataset_remove_structured_properties.py
@@ -4,6 +4,7 @@ from datahub.sdk.dataset import Dataset
 client = DataHubClient.from_env()
 
 # NOTE: structured porperties should be created before adding them to the dataset
+# client.entities.get() also works to retrieve the dataset
 dataset = Dataset(
     name="example_dataset",
     platform="snowflake",
