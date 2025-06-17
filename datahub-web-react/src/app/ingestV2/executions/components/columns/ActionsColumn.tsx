5dd450bd1ec3e498ab6e2577624f9a3ea7216d34
Chris Collins <chriscollins3456@gmail.com>
2025-06-17 19:49:21 -0400

    fix(ui) Fix deleting a secret with ingestv2 turned on (#6081)

--- datahub-web-react/src/app/ingestV2/executions/components/columns/ActionsColumn.tsx
+++ datahub-web-react/src/app/ingestV2/executions/components/columns/ActionsColumn.tsx
@@ -25,7 +25,7 @@ export function ActionsColumn({ record, setFocusExecutionUrn, handleRollback, ha
                         navigator.clipboard.writeText(record.urn);
                     }}
                 >
-                    Copy URN
+                    Copy Urn
                 </MenuItem>
             ),
         },
