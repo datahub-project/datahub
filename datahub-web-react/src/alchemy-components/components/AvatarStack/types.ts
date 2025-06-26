b34d48bd12ee4770b8376e45d23727909a4fd74b
Jay <159848059+jayacryl@users.noreply.github.com>
2025-06-26 17:25:56 -0400

    fix(web): ui crash on avatar stack (#6140)

--- datahub-web-react/src/alchemy-components/components/AvatarStack/types.ts
+++ datahub-web-react/src/alchemy-components/components/AvatarStack/types.ts
@@ -12,7 +12,7 @@ export interface AvatarItemProps {
 }
 
 export type AvatarStackProps = {
-    avatars: AvatarItemProps[];
+    avatars?: AvatarItemProps[];
     size?: AvatarSizeOptions;
     showRemainingNumber?: boolean;
     maxToShow?: number;
