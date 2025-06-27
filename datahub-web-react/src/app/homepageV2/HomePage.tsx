834c35bc22ea8ac808a81b777d875c2480a5bd3b
purnimagarg1 <purnima.garg@apptware.com>
2025-06-27 19:58:08 +0530

    render skeleton of home page

--- datahub-web-react/src/app/homepageV2/HomePage.tsx
+++ datahub-web-react/src/app/homepageV2/HomePage.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import PersonalizationLoadingModal from '@app/homeV2/persona/PersonalizationLoadingModal';
+import HomePageContent from '@app/homepageV2/HomePageContent';
+import HomePageHeader from '@app/homepageV2/HomePageHeader';
+import { PageWrapper } from '@app/homepageV2/styledComponents';
+import { SearchablePage } from '@app/searchV2/SearchablePage';
+
 export const HomePage = () => {
-    return <div>Home Page</div>;
+    return (
+        <>
+            <SearchablePage hideSearchBar>
+                <PageWrapper>
+                    <HomePageHeader />
+                    <HomePageContent />
+                </PageWrapper>
+            </SearchablePage>
+            <PersonalizationLoadingModal />
+        </>
+    );
 };
