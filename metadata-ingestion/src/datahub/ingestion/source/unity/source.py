7345af898d4ba5da194eae50cf95f77e3373e3f1
Aseem Bansal <asmbansal2@gmail.com>
2025-06-30 15:16:08 +0530

    feat(ingest): generate capability summary (#13881)

--- metadata-ingestion/src/datahub/ingestion/source/unity/source.py
+++ metadata-ingestion/src/datahub/ingestion/source/unity/source.py
@@ -159,7 +159,7 @@ logger: logging.Logger = logging.getLogger(__name__)
 )
 @capability(
     SourceCapability.DELETION_DETECTION,
-    "Optionally enabled via `stateful_ingestion.remove_stale_metadata`",
+    "Enabled by default via stateful ingestion",
     supported=True,
 )
 @capability(SourceCapability.TEST_CONNECTION, "Enabled by default")
