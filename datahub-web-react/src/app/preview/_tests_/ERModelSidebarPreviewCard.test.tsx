0aef96f4be73eb13ade6577918728fb34b5a1718
pjain155_uhg <anshul_p@optum.com>
2025-07-07 21:43:12 +0530

    refactor: Improve code coverage

--- datahub-web-react/src/app/preview/_tests_/ERModelSidebarPreviewCard.test.tsx
+++ datahub-web-react/src/app/preview/_tests_/ERModelSidebarPreviewCard.test.tsx
@@ -1,11 +1,22 @@
 import { MockedProvider } from '@apollo/client/testing';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
+import { PreviewType } from '@app/entity/Entity';
 import ERModelSidebarPreviewCard from '@app/preview/ERModelSidebarPreviewCard';
 import TestPageContainer from '@utils/test-utils/TestPageContainer';
 
-import { Domain, EntityType, GlobalTags, GlossaryTermAssociation, GlossaryTerms, TagAssociation } from '@types';
+import {
+    Domain,
+    EntityType,
+    ErModelRelationshipCardinality,
+    GlobalTags,
+    GlossaryTermAssociation,
+    GlossaryTerms,
+    Owner,
+    OwnerType,
+    TagAssociation,
+} from '@types';
 
 const mockTags: GlobalTags = {
     tags: [{ tag: { name: 'Tag1' } }, { tag: { name: 'Tag2' } }] as TagAssociation[],
@@ -20,6 +31,20 @@ const mockDomain: Domain = {
     id: 'domain',
     type: EntityType.Domain,
 };
+
+const mockOwners: Owner[] = [
+    {
+        owner: {
+            urn: 'urn:li:corpuser:owner1',
+            type: EntityType.CorpUser,
+        } as OwnerType,
+        associatedUrn: 'urn:li:corpuser:owner1',
+    },
+];
+
+const mockCardinality: ErModelRelationshipCardinality = ErModelRelationshipCardinality.OneN;
+const mockDescription =
+    'This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes.';
 
 describe('ERModelSidebarPreviewCard', () => {
     it('renders tags, glossary terms, and domain', () => {
@@ -43,6 +68,18 @@ describe('ERModelSidebarPreviewCard', () => {
         expect(screen.getByText('GlossaryTerm1')).toBeInTheDocument();
         expect(screen.getByText('GlossaryTerm2')).toBeInTheDocument();
         expect(screen.getByText('domain')).toBeInTheDocument();
+    });
+
+    it('renders owners in the right column when provided', () => {
+        render(
+            <MockedProvider>
+                <TestPageContainer>
+                    <ERModelSidebarPreviewCard name="Test Entity" urn="test-urn" url="/test-url" owners={mockOwners} />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+
+        expect(screen.getByText('Owners')).toBeInTheDocument();
     });
 
     it('renders only tags when glossary terms and domain are absent', () => {
@@ -80,7 +117,25 @@ describe('ERModelSidebarPreviewCard', () => {
         expect(screen.queryByText('domain')).not.toBeInTheDocument();
     });
 
-    it('renders nothing when no tags, glossary terms, or domain are provided', () => {
+    it('renders cardinality when provided', () => {
+        render(
+            <MockedProvider>
+                <TestPageContainer>
+                    <ERModelSidebarPreviewCard
+                        name="Test Entity"
+                        urn="test-urn"
+                        url="/test-url"
+                        cardinality={mockCardinality}
+                    />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+
+        expect(screen.getByText('Cardinality:')).toBeInTheDocument();
+        expect(screen.getByText(ErModelRelationshipCardinality.OneN)).toBeInTheDocument();
+    });
+
+    it('renders nothing when no tags, glossary terms, domain, or cardinality are provided', () => {
         render(
             <MockedProvider>
                 <TestPageContainer>
@@ -92,5 +147,42 @@ describe('ERModelSidebarPreviewCard', () => {
         expect(screen.queryByText('Tag1')).not.toBeInTheDocument();
         expect(screen.queryByText('GlossaryTerm1')).not.toBeInTheDocument();
         expect(screen.queryByText('domain')).not.toBeInTheDocument();
+        expect(screen.queryByText('Cardinality:')).not.toBeInTheDocument();
+    });
+
+    it('renders description and toggles "Show More" and "Show Less"', () => {
+        render(
+            <MockedProvider>
+                <TestPageContainer>
+                    <ERModelSidebarPreviewCard
+                        name="Test Entity"
+                        urn="test-urn"
+                        url="/test-url"
+                        description={mockDescription}
+                        previewType={PreviewType.HOVER_CARD}
+                    />
+                </TestPageContainer>
+            </MockedProvider>,
+        );
+
+        // Verify the description is rendered
+        expect(
+            screen.getByText(
+                'This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purposes. This is a sample description for testing purpo...',
+            ),
+        ).toBeInTheDocument();
+
+        // Verify "Show More" link is rendered
+        const showMoreLink = screen.getByText('Show More');
+        expect(showMoreLink).toBeInTheDocument();
+
+        // Click "Show More" and verify "Show Less" link appears
+        fireEvent.click(showMoreLink);
+        const showLessLink = screen.getByText('Show Less');
+        expect(showLessLink).toBeInTheDocument();
+
+        // Click "Show Less" and verify "Show More" link appears again
+        fireEvent.click(showLessLink);
+        expect(screen.getByText('Show More')).toBeInTheDocument();
     });
 });
