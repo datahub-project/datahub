c342323ea0ce2f4d31c7cce0ba7ecdfcdd931b4c
RyanHolstien <RyanHolstien@users.noreply.github.com>
2025-07-08 16:36:12 -0500

    fix

--- metadata-service/configuration/src/main/java/com/linkedin/metadata/config/search/BuildIndicesConfiguration.java
+++ metadata-service/configuration/src/main/java/com/linkedin/metadata/config/search/BuildIndicesConfiguration.java
@@ -1,9 +1,16 @@
 package com.linkedin.metadata.config.search;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 @Builder(toBuilder = true)
 public class BuildIndicesConfiguration {
 
