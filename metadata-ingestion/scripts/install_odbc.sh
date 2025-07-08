68d75c3270f92246a373311919e4ac060ccd60c3
sleeperdeep <ingayb@gmail.com>
2025-07-08 17:00:43 +0000

    feat(mssql): quote_schemas & sqlalchemy_uri field with related logic were added to SQLServerConfig

--- metadata-ingestion/scripts/install_odbc.sh
+++ metadata-ingestion/scripts/install_odbc.sh
@@ -4,6 +4,8 @@ then
     exit;
 fi
 
+sudo su
+
 # Download the package to configure the Microsoft repo
 curl -sSL -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)/packages-microsoft-prod.deb
 # Install the package
