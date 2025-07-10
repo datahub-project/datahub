3e2e00663f5b274de0ea31c5190b16e0824f7cfe
RyanHolstien <RyanHolstien@users.noreply.github.com>
2025-07-09 21:09:23 -0500

    fix(systemUpdate): add configuration for opensearch clusters with zone awareness enabled (#13996) (#6201)

--- metadata-service/configuration/src/main/java/com/linkedin/metadata/config/search/BuildIndicesConfiguration.java
+++ metadata-service/configuration/src/main/java/com/linkedin/metadata/config/search/BuildIndicesConfiguration.java
@@ -1,12 +1,21 @@
 package com.linkedin.metadata.config.search;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
+@Builder(toBuilder = true)
 public class BuildIndicesConfiguration {
 
   private boolean cloneIndices;
   private boolean allowDocCountMismatch;
   private String retentionUnit;
   private Long retentionValue;
+  private boolean reindexOptimizationEnabled;
 }
