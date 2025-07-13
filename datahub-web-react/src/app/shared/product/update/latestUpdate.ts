626e8d4b3f1cf7ad519ff8615826f57de9586912
John Joyce <john@Mac-4711.lan>
2025-07-13 10:31:55 -0700

    Adding latest update changes for OSS

--- datahub-web-react/src/app/shared/product/update/latestUpdate.ts
+++ datahub-web-react/src/app/shared/product/update/latestUpdate.ts
@@ -10,14 +10,15 @@ export type ProductUpdate = {
     ctaLink: string;
 };
 
+// NOTE: This is a place that OSS and Cloud diverge.
 /* Important: Change this section to adjust the system announcement shown in the bottom left corner of the product! */
 // TODO: Migrate this to be served via an aspect!
 export const latestUpdate: ProductUpdate = {
     enabled: true,
-    id: 'v0.3.12-3', // Very important, when changed it will trigger the announcement to be re-displayed for a user.
+    id: 'v1.2.0', // Very important, when changed it will trigger the announcement to be re-displayed for a user.
     title: "What's New In DataHub",
-    description: 'Explore version v0.3.12',
+    description: 'Explore version v1.2.0',
     image: SampleImage, // Import and use image.,
     ctaText: 'Read updates',
-    ctaLink: 'https://datahub.com/blog/datahub-cloud-v0-3-12/',
+    ctaLink: 'https://docs.datahub.com/docs/releases#v1-2-0',
 };
