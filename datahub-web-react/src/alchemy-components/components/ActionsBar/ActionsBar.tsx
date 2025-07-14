dc987d00cdaa59b3439ecc66782c59ade3cdbf97
Chris Collins <chriscollins3456@gmail.com>
2025-07-14 17:59:12 -0400

    feat(ui) Add ability to edit global template on homepage

---
+++ datahub-web-react/src/alchemy-components/components/ActionsBar/ActionsBar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ActionsContainer = styled.div`
+    display: flex;
+    padding: 4px;
+    justify-content: center;
+    align-items: center;
+    gap: 8px;
+    width: fit-content;
+    align-self: center;
+    border-radius: 12px;
+    box-shadow: 0px 4px 12px 0px rgba(9, 1, 61, 0.12);
+
+    background-color: white;
+    position: absolute;
+    left: 50%;
+    bottom: 2px;
+    transform: translateX(-55%);
+`;
+
+export type ActionsBarProps = {};
+
+export const ActionsBar = ({ children }: React.PropsWithChildren<ActionsBarProps>) => {
+    return <ActionsContainer>{children}</ActionsContainer>;
+};
---
+++ datahub-web-react/src/app/homeV3/EditDefaultTemplateBar.tsx
@@ -0,0 +1,36 @@
+import { Button, Icon, Tooltip, colors } from '@components';
+import React from 'react';
+import styled from 'styled-components';
+
+import { ActionsBar } from '@components/components/ActionsBar/ActionsBar';
+
+import { usePageTemplateContext } from '@app/homeV3/context/PageTemplateContext';
+
+const Warning = styled.div`
+    padding: 8px;
+    background-color: ${colors.red[0]};
+    color: ${colors.red[1000]};
+    display: flex;
+    align-items: center;
+    gap: 8px;
+    font-weight: 600;
+    font-size: 14px;
+    border-radius: 8px;
+`;
+
+export default function EditDefaultTemplateBar() {
+    const { setIsEditingGlobalTemplate, isEditingGlobalTemplate } = usePageTemplateContext();
+
+    // TODO: also hide this if you don't have permissions - CH-510
+    if (!isEditingGlobalTemplate) return null;
+
+    return (
+        <ActionsBar>
+            <Warning>
+                <Icon icon="ExclamationMark" color="red" weight="fill" source="phosphor" />
+                <span>Editing default user view</span>
+            </Warning>
+            <Button onClick={() => setIsEditingGlobalTemplate(false)}>Done</Button>
+        </ActionsBar>
+    );
+}
