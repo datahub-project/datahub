5a80b86abb10ebcc024731286622b7af56de07d4
Chris Collins <chriscollins3456@gmail.com>
2025-07-23 18:52:14 -0400

    move where we get templates

--- datahub-web-react/src/app/homeV3/context/HomePageProvider.tsx
+++ datahub-web-react/src/app/homeV3/context/HomePageProvider.tsx
@@ -1,29 +1,9 @@
 import React from 'react';
 
-import { useGlobalSettings } from '@app/context/GlobalSettingsContext';
-import { useUserContext } from '@app/context/useUserContext';
 import { PageTemplateProvider } from '@app/homeV3/context/PageTemplateContext';
 
 const HomePageProvider = ({ children }: { children: React.ReactNode }) => {
-    const { settings, loaded: globalSettingsLoaded } = useGlobalSettings();
-    const { user, loaded: userLoaded } = useUserContext();
-
-    const personalTemplate = user?.settings?.homePage?.pageTemplate || null;
-    const globalTemplate = settings.globalHomePageSettings?.defaultTemplate || null;
-
-    if (!userLoaded || !globalSettingsLoaded)
-        // adding key to force reload on PageTemplateProvider once settings are both loaded
-        return (
-            <PageTemplateProvider key="0" personalTemplate={null} globalTemplate={null}>
-                {children}
-            </PageTemplateProvider>
-        );
-
-    return (
-        <PageTemplateProvider key="1" personalTemplate={personalTemplate} globalTemplate={globalTemplate}>
-            {children}
-        </PageTemplateProvider>
-    );
+    return <PageTemplateProvider>{children}</PageTemplateProvider>;
 };
 
 export default HomePageProvider;
