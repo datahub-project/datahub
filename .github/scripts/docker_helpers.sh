3f18bcc3c9441a4734af3a832325cf3536246300
Harshal Sheth <hsheth2@gmail.com>
2025-07-24 15:32:00 -0700

    fix(docker): always use epoch number for SaaS python builds (#6297)

--- .github/scripts/docker_helpers.sh
+++ .github/scripts/docker_helpers.sh
@@ -31,7 +31,7 @@ function get_tag_full {
     echo $(echo ${REF} | sed -e "s,refs/heads/${MAIN_BRANCH},${MAIN_BRANCH_TAG}-full,g" -e 's,refs/tags/\(.*\),\1-full,g' -e 's,refs/heads/\(.*\),\1-full,g' -e 's,refs/pull/\([0-9]*\).*,pr\1-full,g'  -e 's,/,-,g')
 }
 
-function get_python_docker_release_v() {
+function _get_python_docker_release_v_core() {
     echo "$(echo "${REF}" | \
         sed -e "s,refs/heads/${MAIN_BRANCH},1\!0.0.0+docker.${SHORT_SHA},g" \
             -e 's,refs/heads/\(.*\),1!0.0.0+docker.\1,g' \
@@ -40,18 +40,36 @@ function get_python_docker_release_v() {
             -e 's,/,-,g'
             )"
 }
+
+function get_python_docker_release_v() {
+    # SaaS Only: for tagged releases, we also want to use the 1! epoch number prefix.
+    # Many of our Python packages depend on acryl-datahub >= 1.x. This is fine in OSS
+    # because the version numbers are also 1.x, but causes issues in SaaS because
+    # our SaaS version numbers are 0.x. By adding the 1! epoch number prefix, we
+    # ensure that the SaaS version number is always >= 1.x.
+
+    if [[ "${REF}" == refs/tags/v* ]]; then
+        echo "$(echo "${REF}" | \
+            sed -e "s,refs/tags/v\([0-9a-zA-Z.]*\).*,1!\1+docker,g" \
+                -e 's,/,-,g'
+                )"
+    else
+        _get_python_docker_release_v_core
+    fi
+}
+
 # To run these, set TEST_DOCKER_HELPERS=1 GITHUB_REF=test and run this script
 if [ ${TEST_DOCKER_HELPERS:-0} -eq 1 ]; then
-    REF="refs/pull/4788/merge"     get_python_docker_release_v # '1!0.0.0+docker.pr4788'
-    REF="refs/tags/v0.1.2-test"    get_python_docker_release_v # '0.1.2'
-    REF="refs/tags/v0.1.2.1-test"  get_python_docker_release_v # '0.1.2.1'
-    REF="refs/tags/v0.1.2rc1-test" get_python_docker_release_v # '0.1.2rc1'
-    REF="refs/heads/branch-name"   get_python_docker_release_v # '1!0.0.0+docker.branch-name'
-    REF="refs/heads/releases/branch-name" get_python_docker_release_v # 1!0.0.0+docker.releases-branch-name'
+    REF="refs/pull/4788/merge"     _get_python_docker_release_v_core # '1!0.0.0+docker.pr4788'
+    REF="refs/tags/v0.1.2-test"    _get_python_docker_release_v_core # '0.1.2+dockerdocker'
+    REF="refs/tags/v0.1.2.1-test"  _get_python_docker_release_v_core # '0.1.2.1+docker'
+    REF="refs/tags/v0.1.2rc1-test" _get_python_docker_release_v_core # '0.1.2rc1+docker'
+    REF="refs/heads/branch-name"   _get_python_docker_release_v_core # '1!0.0.0+docker.branch-name'
+    REF="refs/heads/releases/branch-name" _get_python_docker_release_v_core # 1!0.0.0+docker.releases-branch-name'
 
-    REF="refs/tags/v0.1.2rc1"    get_tag # '0.1.2rc1'
-    REF="refs/tags/v0.1.2rc1"    get_tag_slim # '0.1.2rc1-slim'
-    REF="refs/tags/v0.1.2rc1"    get_tag_full # '0.1.2rc1-full'
+    REF="refs/tags/v0.1.2rc1"    get_tag # 'v0.1.2rc1'
+    REF="refs/tags/v0.1.2rc1"    get_tag_slim # 'v0.1.2rc1-slim'
+    REF="refs/tags/v0.1.2rc1"    get_tag_full # 'v0.1.2rc1-full'
 
     REF="refs/pull/4788/merge"    get_tag # 'pr4788'
     REF="refs/pull/4788/merge"    get_tag_slim # 'pr4788-slim'
@@ -64,6 +82,13 @@ if [ ${TEST_DOCKER_HELPERS:-0} -eq 1 ]; then
     REF="refs/heads/releases/branch-name" get_tag # 'releases-branch-name'
     REF="refs/heads/releases/branch-name" get_tag_slim # 'releases-branch-name-slim'
     REF="refs/heads/releases/branch-name" get_tag_full # 'releases-branch-name-full'
+
+    # SaaS Only Tests.
+    REF="refs/pull/4788/merge"     get_python_docker_release_v # '1!0.0.0+docker.pr4788'
+    REF="refs/tags/v0.1.2-test"    get_python_docker_release_v # '1!0.1.2+docker'
+    REF="refs/tags/v0.1.2.1-test"  get_python_docker_release_v # '1!0.1.2.1+docker'
+    REF="refs/tags/v0.1.2rc1-test" get_python_docker_release_v # '1!0.1.2rc1+docker'
+    REF="refs/heads/releases/branch-name" get_python_docker_release_v # 1!0.0.0+docker.releases-branch-name'
 fi
 
 function get_unique_tag {
