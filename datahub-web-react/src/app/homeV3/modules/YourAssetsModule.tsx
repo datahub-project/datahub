fe514ecf6e94f9510170c0dba681b1d35a2e17aa
purnimagarg1 <purnima.garg@apptware.com>
2025-07-24 20:28:18 +0530

    fix empty states of home page modules

--- datahub-web-react/src/app/homeV3/modules/YourAssetsModule.tsx
+++ datahub-web-react/src/app/homeV3/modules/YourAssetsModule.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useHistory } from 'react-router';
 
 import { useUserContext } from '@app/context/useUserContext';
 import { useGetAssetsYouOwn } from '@app/homeV2/reference/sections/assets/useGetAssetsYouOwn';
@@ -7,11 +8,17 @@ import EntityItem from '@app/homeV3/module/components/EntityItem';
 import LargeModule from '@app/homeV3/module/components/LargeModule';
 import { ModuleProps } from '@app/homeV3/module/types';
 import useSearchYourAssets from '@app/homeV3/modules/useSearchYourAssets';
+import { navigateToSearchUrl } from '@app/searchV2/utils/navigateToSearchUrl';
 
 export default function YourAssetsModule(props: ModuleProps) {
     const { user } = useUserContext();
     const { originEntities, loading } = useGetAssetsYouOwn(user);
     const searchForYourAssets = useSearchYourAssets();
+    const history = useHistory();
+
+    const navigateToSearch = useCallback(() => {
+        navigateToSearchUrl({ query: '*', history });
+    }, [history]);
 
     return (
         <LargeModule {...props} loading={loading} onClickViewAll={searchForYourAssets}>
@@ -20,8 +27,8 @@ export default function YourAssetsModule(props: ModuleProps) {
                     icon="User"
                     title="No Owned Assets"
                     description="Select an asset and add yourself as an owner to see the assets in this list"
-                    linkText="Discover assets to subscribe to"
-                    onLinkClick={() => {}}
+                    linkText="Discover the assets you want to own"
+                    onLinkClick={navigateToSearch}
                 />
             ) : (
                 originEntities.map((entity) => <EntityItem entity={entity} key={entity.urn} />)
