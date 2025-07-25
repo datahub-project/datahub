9bdc8cdea3eaca1cc47a5af866920f26d34c4330
Aseem Bansal <asmbansal2@gmail.com>
2025-07-25 18:31:39 +0530

    feat(ingest): add structured log type

--- metadata-ingestion/src/datahub/ingestion/source/fivetran/fivetran.py
+++ metadata-ingestion/src/datahub/ingestion/source/fivetran/fivetran.py
@@ -16,7 +16,11 @@ from datahub.ingestion.api.decorators import (
     platform_name,
     support_status,
 )
-from datahub.ingestion.api.source import MetadataWorkUnitProcessor, SourceReport
+from datahub.ingestion.api.source import (
+    MetadataWorkUnitProcessor,
+    SourceReport,
+    StructuredLogType,
+)
 from datahub.ingestion.api.workunit import MetadataWorkUnit
 from datahub.ingestion.source.fivetran.config import (
     KNOWN_DATA_PLATFORM_MAPPING,
@@ -98,6 +102,7 @@ class FivetranSource(StatefulIngestionSourceBase):
                     message="We encountered a connector type that we don't fully support yet. "
                     "We will attempt to guess the platform based on the connector type.",
                     context=f"{connector.connector_name} (connector_id: {connector.connector_id}, connector_type: {connector.connector_type})",
+                    log_type=StructuredLogType.LINEAGE,
                 )
                 source_details.platform = connector.connector_type
 
