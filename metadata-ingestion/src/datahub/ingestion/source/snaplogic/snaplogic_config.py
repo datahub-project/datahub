0a112a0a26d92ecced0849248198a2b073050232
Salim Abdul <sabdul@snaplogic.com>
2025-07-25 16:47:16 +0300

    feat(ingestion/snaplogic): Add snaplogic as a source for metadata ingestion

---
+++ metadata-ingestion/src/datahub/ingestion/source/snaplogic/snaplogic_config.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from pydantic import Field
+
+from datahub.ingestion.source.state.stale_entity_removal_handler import (
+    StatefulIngestionConfigBase,
+    StatefulStaleMetadataRemovalConfig,
+)
+from datahub.ingestion.source.state.stateful_ingestion_base import (
+    StatefulLineageConfigMixin,
+    StatefulUsageConfigMixin,
+)
+
+
+class SnaplogicConfig(
+    StatefulIngestionConfigBase, StatefulLineageConfigMixin, StatefulUsageConfigMixin
+):
+    platform: str = "Snaplogic"
+    username: str = Field(description="Username")
+    password: str = Field(description="Password")
+    base_url: str = Field(
+        default="https://elastic.snaplogic.com",
+        description="Url to your Snaplogic instance: `https://elastic.snaplogic.com`, or similar. Used for making API calls to Snaplogic.",
+    )
+    org_name: str = Field(description="Organization name from Snaplogic instance")
+    namespace_mapping: dict = Field(
+        default={}, description="Mapping of namespaces to platform instances"
+    )
+    case_insensitive_namespaces: list = Field(
+        default=[],
+        description="List of namespaces that should be treated as case insensitive",
+    )
+    stateful_ingestion: Optional[StatefulStaleMetadataRemovalConfig] = None
