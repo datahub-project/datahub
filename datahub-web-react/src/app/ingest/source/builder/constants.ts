b434b4ae4e1b8fa2bebab365283b60f61bc00598
Salim Abdul <sabdul@snaplogic.com>
2025-07-25 16:47:16 +0300

    feat(ingestion/snaplogic): Add snaplogic as a source for metadata ingestion

--- datahub-web-react/src/app/ingest/source/builder/constants.ts
+++ datahub-web-react/src/app/ingest/source/builder/constants.ts
@@ -42,6 +42,7 @@ import tableauLogo from '@images/tableaulogo.png';
 import trinoLogo from '@images/trinologo.png';
 import vertexAI from '@images/vertexai.png';
 import verticaLogo from '@images/verticalogo.png';
+import snaplogic from '@images/snaplogic.png';
 
 export const ATHENA = 'athena';
 export const ATHENA_URN = `urn:li:dataPlatform:${ATHENA}`;
@@ -149,6 +150,8 @@ export const NEO4J = 'neo4j';
 export const NEO4J_URN = `urn:li:dataPlatform:${NEO4J}`;
 export const VERTEX_AI = 'vertexai';
 export const VERTEXAI_URN = `urn:li:dataPlatform:${VERTEX_AI}`;
+export const SNAPLOGIC = 'snaplogic';
+export const SNAPLOGIC_URN = `urn:li:dataPlatform:${SNAPLOGIC}`;
 
 export const PLATFORM_URN_TO_LOGO = {
     [ATHENA_URN]: athenaLogo,
@@ -196,6 +199,7 @@ export const PLATFORM_URN_TO_LOGO = {
     [DATAHUB_URN]: datahubLogo,
     [NEO4J_URN]: neo4j,
     [VERTEXAI_URN]: vertexAI,
+    [SNAPLOGIC_URN]: snaplogic,
 };
 
 export const SOURCE_TO_PLATFORM_URN = {
