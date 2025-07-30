14717398dff2cc26045ce460204dad817c8d8461
Jay <159848059+jayacryl@users.noreply.github.com>
2025-07-30 12:49:16 -0400

    Update sidebars.js

--- docs-website/sidebars.js
+++ docs-website/sidebars.js
@@ -49,6 +49,11 @@ module.exports = {
           link: { type: "doc", id: "docs/managed-datahub/observe/assertions" },
           items: [
             {
+              label: "Overview",
+              type: "doc",
+              id: "docs/managed-datahub/observe/assertions",
+            },
+            {
               label: "Column Assertions",
               type: "doc",
               id: "docs/managed-datahub/observe/column-assertions",
@@ -89,6 +94,12 @@ module.exports = {
               type: "doc",
               id: "docs/managed-datahub/observe/data-health-dashboard",
               className: "saasOnly",
+            },
+            {
+              label: "Assertion Notes (Troubleshooting & Documentation)",
+              type: "doc",
+              id: "docs/managed-datahub/observe/assertion-notes",
+              className: "saasOnly"
             },
             {
               label: "Open Assertions Specification",
