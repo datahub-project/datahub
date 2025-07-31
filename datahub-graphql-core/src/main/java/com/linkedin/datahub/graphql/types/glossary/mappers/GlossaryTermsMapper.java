d1f653385c67da2c11a95d2102280b51f7b134c9
Deepak Garg <deepgarg@visa.com>
2025-07-31 21:41:31 +0530

    feat(schemaField-terms-propagation): model terms with schemaField

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/glossary/mappers/GlossaryTermsMapper.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/types/glossary/mappers/GlossaryTermsMapper.java
@@ -9,6 +9,7 @@ import com.linkedin.datahub.graphql.generated.CorpUser;
 import com.linkedin.datahub.graphql.generated.EntityType;
 import com.linkedin.datahub.graphql.generated.GlossaryTerm;
 import com.linkedin.datahub.graphql.generated.GlossaryTerms;
+import com.linkedin.datahub.graphql.types.common.mappers.MetadataAttributionMapper;
 import com.linkedin.datahub.graphql.types.glossary.GlossaryTermUtils;
 import java.util.stream.Collectors;
 import javax.annotation.Nonnull;
@@ -40,31 +41,38 @@ public class GlossaryTermsMapper {
             .filter(
                 association ->
                     context == null || canView(context.getOperationContext(), association.getUrn()))
-            .map(association -> this.mapGlossaryTermAssociation(association, entityUrn))
+            .map(association -> this.mapGlossaryTermAssociation(context, association, entityUrn))
             .collect(Collectors.toList()));
     return result;
   }
 
   private com.linkedin.datahub.graphql.generated.GlossaryTermAssociation mapGlossaryTermAssociation(
-      @Nonnull final GlossaryTermAssociation input, @Nonnull final Urn entityUrn) {
+      @Nonnull final QueryContext context,
+      @Nonnull final GlossaryTermAssociation glossaryTermAssociation,
+      @Nonnull final Urn entityUrn) {
     final com.linkedin.datahub.graphql.generated.GlossaryTermAssociation result =
         new com.linkedin.datahub.graphql.generated.GlossaryTermAssociation();
 
     final GlossaryTerm resultGlossaryTerm = new GlossaryTerm();
     resultGlossaryTerm.setType(EntityType.GLOSSARY_TERM);
-    resultGlossaryTerm.setUrn(input.getUrn().toString());
+    resultGlossaryTerm.setUrn(glossaryTermAssociation.getUrn().toString());
     resultGlossaryTerm.setName(
-        GlossaryTermUtils.getGlossaryTermName(input.getUrn().getNameEntity()));
+        GlossaryTermUtils.getGlossaryTermName(glossaryTermAssociation.getUrn().getNameEntity()));
     result.setTerm(resultGlossaryTerm);
 
-    if (input.hasActor()) {
+    if (glossaryTermAssociation.hasActor()) {
       CorpUser actor = new CorpUser();
-      actor.setUrn(input.getActor().toString());
+      actor.setUrn(glossaryTermAssociation.getActor().toString());
       actor.setType(EntityType.CORP_USER);
       result.setActor(actor);
     }
     if (entityUrn != null) {
       result.setAssociatedUrn(entityUrn.toString());
+    }
+    if (glossaryTermAssociation.hasAttribution()
+        && glossaryTermAssociation.getAttribution() != null) {
+      result.setAttribution(
+          MetadataAttributionMapper.map(context, glossaryTermAssociation.getAttribution()));
     }
 
     return result;
