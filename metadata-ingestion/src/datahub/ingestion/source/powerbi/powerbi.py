fcaaeaeb5472ff9156c651b66e025ebb8ab7fa19
Aseem Bansal <asmbansal2@gmail.com>
2025-07-31 21:47:15 +0530

    fix lint

--- metadata-ingestion/src/datahub/ingestion/source/powerbi/powerbi.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi/powerbi.py
@@ -40,7 +40,6 @@ from datahub.ingestion.api.workunit import MetadataWorkUnit
 from datahub.ingestion.source.common.subtypes import (
     BIAssetSubTypes,
     BIContainerSubTypes,
-    SourceCapabilityModifier,
 )
 from datahub.ingestion.source.powerbi.config import (
     Constant,
@@ -1230,10 +1229,6 @@ class Mapper:
 @capability(
     SourceCapability.CONTAINERS,
     "Enabled by default",
-    subtype_modifier=[
-        SourceCapabilityModifier.POWERBI_WORKSPACE,
-        SourceCapabilityModifier.POWERBI_DATASET,
-    ],
 )
 @capability(SourceCapability.DESCRIPTIONS, "Enabled by default")
 @capability(SourceCapability.OWNERSHIP, "Enabled by default")
