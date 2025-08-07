cd932028c2c425383c5d24d7f6c27c1b2080bcfa
purnimagarg1 <purnima.garg@apptware.com>
2025-08-07 17:08:17 +0530

    update the query usage

--- datahub-web-react/src/app/homeV3/modules/assetCollection/AssetCollectionModule.tsx
+++ datahub-web-react/src/app/homeV3/modules/assetCollection/AssetCollectionModule.tsx
@@ -6,7 +6,7 @@ import EntityItem from '@app/homeV3/module/components/EntityItem';
 import LargeModule from '@app/homeV3/module/components/LargeModule';
 import { ModuleProps } from '@app/homeV3/module/types';
 
-import { useGetSearchResultsForMultipleLazyQuery } from '@graphql/search.generated';
+import { useGetSearchResultsForMultipleQuery } from '@graphql/search.generated';
 import { DataHubPageModuleType, Entity } from '@types';
 
 const DEFAULT_PAGE_SIZE = 6;
@@ -20,33 +20,40 @@ const AssetCollectionModule = (props: ModuleProps) => {
         [props.module.properties.params.assetCollectionParams?.assetUrns],
     );
 
-    const [searchQuery, { loading }] = useGetSearchResultsForMultipleLazyQuery();
+    const { loading, refetch } = useGetSearchResultsForMultipleQuery({
+        variables: {
+            input: {
+                start: 0,
+                count: DEFAULT_PAGE_SIZE,
+                query: '*',
+                filters: [{ field: 'urn', values: assetUrns }],
+            },
+        },
+        skip: assetUrns.length === 0,
+    });
 
     const fetchEntities = useCallback(
         async (start: number, count: number): Promise<Entity[]> => {
+            if (assetUrns.length === 0) return [];
             const urnSlice = assetUrns.slice(start, start + count);
-            if (urnSlice.length === 0) return [];
-
-            const { data } = await searchQuery({
-                variables: {
-                    input: {
-                        start: 0,
-                        count: urnSlice.length,
-                        query: '*',
-                        filters: [{ field: 'urn', values: urnSlice }],
-                    },
+            const result = await refetch({
+                input: {
+                    start: 0,
+                    count: urnSlice.length,
+                    query: '*',
+                    filters: [{ field: 'urn', values: urnSlice }],
                 },
             });
 
             const results =
-                data?.searchAcrossEntities?.searchResults
+                result.data?.searchAcrossEntities?.searchResults
                     ?.map((res) => res.entity)
                     .filter((entity): entity is Entity => !!entity) || [];
 
             const urnToEntity = new Map(results.map((e) => [e.urn, e]));
             return urnSlice.map((urn) => urnToEntity.get(urn)).filter((entity): entity is Entity => !!entity);
         },
-        [assetUrns, searchQuery],
+        [assetUrns, refetch],
     );
 
     return (
