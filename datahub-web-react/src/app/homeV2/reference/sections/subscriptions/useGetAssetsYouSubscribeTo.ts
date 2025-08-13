2f4f41afae6e61464b921283ca17173aa01ebd45
purnimagarg1 <purnima.garg@apptware.com>
2025-08-13 18:50:50 +0530

    add infinite scroll to subscriptions module on custom homepage

--- datahub-web-react/src/app/homeV2/reference/sections/subscriptions/useGetAssetsYouSubscribeTo.ts
+++ datahub-web-react/src/app/homeV2/reference/sections/subscriptions/useGetAssetsYouSubscribeTo.ts
@@ -1,28 +1,38 @@
 import { WatchQueryFetchPolicy } from '@apollo/client';
+import { useCallback, useMemo } from 'react';
 
 import { useEntityRegistry } from '@app/useEntityRegistry';
 
 import { useListSubscriptionsQuery } from '@graphql/subscriptions.generated';
-import { CorpUser, EntityType } from '@types';
+import { CorpUser, Entity, EntityType } from '@types';
 
 const MAX_ASSETS_TO_FETCH = 50;
 
 interface Props {
     user?: CorpUser | null;
-    count?: number;
+    initialCount?: number;
     fetchPolicy?: WatchQueryFetchPolicy;
     onCompleted?: () => void;
 }
 
 // TODO: Add Group subscriptions here as well.
-export function useGetAssetsYouSubscribeTo({ user, count = MAX_ASSETS_TO_FETCH, fetchPolicy, onCompleted }: Props) {
-    const { loading, data, error } = useListSubscriptionsQuery({
-        variables: {
+export function useGetAssetsYouSubscribeTo({
+    user,
+    initialCount = MAX_ASSETS_TO_FETCH,
+    fetchPolicy,
+    onCompleted,
+}: Props) {
+    const getVariables = useCallback(
+        (start: number, count: number) => ({
             input: {
-                start: 0,
+                start,
                 count,
             },
-        },
+        }),
+        [],
+    );
+    const { loading, data, error, refetch } = useListSubscriptionsQuery({
+        variables: getVariables(0, initialCount),
         skip: !user?.urn,
         fetchPolicy: fetchPolicy ?? 'cache-first',
         nextFetchPolicy: 'cache-first',
@@ -30,7 +40,10 @@ export function useGetAssetsYouSubscribeTo({ user, count = MAX_ASSETS_TO_FETCH, 
     });
 
     const entityRegistry = useEntityRegistry();
-    const originEntities = data?.listSubscriptions?.subscriptions?.map((result) => result.entity) || [];
+    const originEntities = useMemo(
+        () => data?.listSubscriptions?.subscriptions?.map((result) => result.entity) || [],
+        [data?.listSubscriptions?.subscriptions],
+    );
 
     const entities =
         originEntities.map((subscription) =>
@@ -38,5 +51,19 @@ export function useGetAssetsYouSubscribeTo({ user, count = MAX_ASSETS_TO_FETCH, 
         ) || [];
     const total = data?.listSubscriptions?.total || 0;
 
-    return { originEntities, entities, loading, error, total };
+    // For fetching paginated subscriptions based on start and count
+    const fetchSubscriptions = useCallback(
+        async (start: number, count: number): Promise<Entity[]> => {
+            if (start === 0) {
+                return originEntities;
+            }
+
+            const result = await refetch(getVariables(start, count));
+
+            return result.data?.listSubscriptions?.subscriptions?.map((res) => res.entity) || [];
+        },
+        [refetch, getVariables, originEntities],
+    );
+
+    return { originEntities, entities, loading, error, total, fetchSubscriptions };
 }
