9d8b330ab3489f4cc12655d541253eae5141fa2b
Aseem Bansal <asmbansal2@gmail.com>
2025-08-14 18:44:23 +0530

    fix lint, only print changed files in prettier

--- datahub-web-react/src/app/ingestV2/executions/components/BaseTab.tsx
+++ datahub-web-react/src/app/ingestV2/executions/components/BaseTab.tsx
@@ -34,12 +34,12 @@ export const ScrollableDetailsContainer = styled(DetailsContainer)`
         max-height: 300px;
         overflow-y: scroll;
     }
-    
+
     pre::-webkit-scrollbar-track {
         background: rgba(193, 196, 208, 0.3) !important;
         border-radius: 10px;
     }
-    
+
     pre::-webkit-scrollbar-thumb {
         background: rgba(193, 196, 208, 0.8) !important;
         border-radius: 10px;
