5b03a35d40a09bfd741c756c32169c4aa5ca952a
purnimagarg1 <purnima.garg@apptware.com>
2025-08-14 17:24:20 +0530

    fix lint

--- datahub-web-react/src/app/entity/shared/components/styled/ExpandedOwner/ExpandedOwner.tsx
+++ datahub-web-react/src/app/entity/shared/components/styled/ExpandedOwner/ExpandedOwner.tsx
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
 import analytics, { EntityActionType, EventType } from '@app/analytics';
-import { useUserContext } from '@app/context/useUserContext';
 import { useEntityData } from '@app/entity/shared/EntityContext';
 import OwnerContent from '@app/entity/shared/components/styled/ExpandedOwner/OwnerContent';
 import { getNameFromType } from '@app/entity/shared/containers/profile/sidebar/Ownership/ownershipUtils';
-import { usePageTemplateContext } from '@app/homeV3/context/PageTemplateContext';
 import { useEmbeddedProfileLinkProps } from '@app/shared/useEmbeddedProfileLinkProps';
 import { useEntityRegistry } from '@app/useEntityRegistry';
 
@@ -37,8 +35,6 @@ export const ExpandedOwner = ({ entityUrn, owner, hidePopOver, refetch, readOnly
     const { entityType } = useEntityData();
     const linkProps = useEmbeddedProfileLinkProps();
     const [removeOwnerMutation] = useRemoveOwnerMutation();
-    const { setReloadHomepageModules } = usePageTemplateContext();
-    const { user } = useUserContext();
 
     let name = '';
     let ownershipTypeName = '';
@@ -76,8 +72,6 @@ export const ExpandedOwner = ({ entityUrn, owner, hidePopOver, refetch, readOnly
                 entityType,
                 entityUrn,
             });
-            const isCurrentUserRemoved = user?.urn === owner.owner.urn;
-            if (isCurrentUserRemoved) setReloadHomepageModules(true);
         } catch (e: unknown) {
             message.destroy();
             if (e instanceof Error) {
