f9fd6f8d7700872211bedc47d073e95540b0b7f2
v-tarasevich-blitz-brain <v.tarasevich@blitz-brain.com>
2025-08-15 23:35:25 +0300

    feat(customHomePage/hierarchy): add support of pagination for root level nodes (#6441)
    Co-authored-by: Chris Collins <chriscollins3456@gmail.com>

--- datahub-web-react/src/app/homeV3/modules/hierarchyViewModule/treeView/TreeNodesRenderer.tsx
+++ datahub-web-react/src/app/homeV3/modules/hierarchyViewModule/treeView/TreeNodesRenderer.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 
 import TreeNodeRenderer from '@app/homeV3/modules/hierarchyViewModule/treeView/TreeNodeRenderer';
+import TreeNodesViewLoader from '@app/homeV3/modules/hierarchyViewModule/treeView/components/TreeNodesViewLoader';
+import NodesLoaderWrapper from '@app/homeV3/modules/hierarchyViewModule/treeView/components/itemsLoaderWrapper/NodesLoaderWrapper';
 import useTreeViewContext from '@app/homeV3/modules/hierarchyViewModule/treeView/context/useTreeViewContext';
+import { TreeNode } from '@app/homeV3/modules/hierarchyViewModule/treeView/types';
 
 const Wrapper = styled.div`
     display: flex;
@@ -17,15 +20,39 @@ const InlineBlockWrapper = styled.div<{ $hasExpanded: boolean }>`
 `;
 
 export default function TreeNodesRenderer() {
-    const { nodes, hasAnyExpanded } = useTreeViewContext();
+    const {
+        nodes,
+        hasAnyExpanded,
+        loadBatchSize: numberOfChildrenToLoad,
+        loadRootNodes,
+        rootNodesLength,
+        rootNodesTotal,
+        rootNodesLoading,
+        loadingTriggerType,
+    } = useTreeViewContext();
+
+    const renderNode = useCallback((node: TreeNode) => <TreeNodeRenderer node={node} depth={0} key={node.value} />, []);
 
     return (
         <InlineBlockWrapper $hasExpanded={hasAnyExpanded}>
-            <Wrapper>
-                {nodes.map((node) => (
-                    <TreeNodeRenderer node={node} depth={0} key={node.value} />
-                ))}
-            </Wrapper>
+        <Wrapper>
+            {loadRootNodes ? (
+                <NodesLoaderWrapper
+                    trigger={loadingTriggerType}
+                    total={rootNodesTotal}
+                    current={rootNodesLength}
+                    enabled={!rootNodesLoading}
+                    depth={0}
+                    onLoad={loadRootNodes}
+                    pageSize={numberOfChildrenToLoad}
+                >
+                    {nodes.map(renderNode)}
+                </NodesLoaderWrapper>
+            ) : (
+                nodes.map(renderNode)
+            )}
+            {rootNodesLoading && <TreeNodesViewLoader depth={0} />}
+        </Wrapper>
         </InlineBlockWrapper>
     );
 }
