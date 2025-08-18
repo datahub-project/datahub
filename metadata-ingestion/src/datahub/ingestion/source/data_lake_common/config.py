e23f50e1606cef399965257e1297d23fde9baa7a
Jonny Dixon <jonny.dixon@acryl.io>
2025-08-18 23:27:16 +0100

    Update config.py

--- metadata-ingestion/src/datahub/ingestion/source/data_lake_common/config.py
+++ metadata-ingestion/src/datahub/ingestion/source/data_lake_common/config.py
@@ -15,16 +15,6 @@ from datahub.ingestion.source.data_lake_common.path_spec import PathSpec
 
 
 class PathSpecsConfigMixin(ConfigModel):
-    """
-    Mixin for configuration classes that use path specifications.
-
-    This provides the common path_specs field that is used by data lake sources
-    to specify which paths to ingest from.
-    """
-
     path_specs: List[PathSpec] = Field(
         description="List of PathSpec. See [below](#path-spec) the details about PathSpec"
     )
-
-
-# ExternalFileConnectionConfig removed - connectors can extend DataLakeConnectionConfig directly
