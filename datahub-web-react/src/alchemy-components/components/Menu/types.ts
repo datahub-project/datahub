210320574a129e3be3eae70d882e5e43a4dfd8de
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-08-20 15:48:20 +0300

    feat(components): add Menu component

---
+++ datahub-web-react/src/alchemy-components/components/Menu/types.ts
@@ -0,0 +1,44 @@
+import { DropdownProps } from '@components/components/Dropdown/types';
+import { IconNames } from '@components/components/Icon';
+
+export interface BaseItemType {
+    type: string;
+    key: string;
+}
+
+export interface MenuItemType extends BaseItemType {
+    type: 'item';
+    title: string;
+    icon?: IconNames;
+    description?: string;
+    tooltip?: string;
+    disabled?: boolean;
+    danger?: boolean;
+    children?: ItemType[];
+
+    onClick?: () => void;
+}
+
+export interface GroupItemType extends BaseItemType {
+    type: 'group';
+    title: string;
+    children?: ItemType[];
+}
+
+export interface DividerType extends BaseItemType {
+    type: 'divider';
+}
+
+export type ItemType = MenuItemType | GroupItemType | DividerType;
+
+export type MenuProps = Omit<DropdownProps, 'menu'> & {
+    items?: ItemType[];
+};
+
+export interface MenuItemRendererProps {
+    item: MenuItemType;
+}
+
+export interface GroupItemRendererProps {
+    item: GroupItemType;
+}
