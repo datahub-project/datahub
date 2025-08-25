d013bad83cd0b325ed38934ca989412ffdf87e38
Aseem Bansal <asmbansal2@gmail.com>
2025-08-25 18:55:45 +0530

    feat(ui/ingest): add analytics events

--- datahub-web-react/src/app/ingestV2/source/builder/SelectTemplateStep.tsx
+++ datahub-web-react/src/app/ingestV2/source/builder/SelectTemplateStep.tsx
@@ -3,6 +3,7 @@ import { Input } from 'antd';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+import analytics, { EventType } from '@app/analytics';
 import { ANTD_GRAY } from '@app/entity/shared/constants';
 import { DataPlatformCard } from '@app/ingestV2/source/builder/DataPlatformCard';
 import { CUSTOM } from '@app/ingestV2/source/builder/constants';
@@ -92,6 +93,10 @@ export const SelectTemplateStep = ({ state, updateState, goTo, cancel, ingestion
             type,
         };
         updateState(newState);
+        analytics.event({
+            type: EventType.IngestionSourceTemplateSelectedEvent,
+            sourceType: type,
+        });
         goTo(IngestionSourceBuilderStep.DEFINE_RECIPE);
     };
 
