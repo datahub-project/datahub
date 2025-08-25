bf54016d919007fe17d07a4abc3d5f31a6940972
jjia <jjia@netflix.com>
2025-08-25 16:58:13 -0700

    remove comment

--- metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v3/models/GenericEntityAspectsBodyV3.java
+++ metadata-service/openapi-servlet/models/src/main/java/io/datahubproject/openapi/v3/models/GenericEntityAspectsBodyV3.java
@@ -14,7 +14,5 @@ public class GenericEntityAspectsBodyV3 {
   @Nullable private Set<String> entities;
   @Nullable private Set<String> aspects;
   @Nullable private Filter filter;
-
-  /** This will be ignored if sorting criterion is also specified as param on URL path. */
   @Nullable private List<SortCriterion> sortCriteria;
 }
