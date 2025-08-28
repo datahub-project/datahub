2d4940d61a9d21b254c6c7307f8286637c8e7bc4
Chris Collins <chriscollins3456@gmail.com>
2025-08-28 15:45:41 -0400

    Implement frontend for updating templates on summary pages - no properties header

--- datahub-web-react/src/app/homeV3/context/hooks/useTemplateState.ts
+++ datahub-web-react/src/app/homeV3/context/hooks/useTemplateState.ts
@@ -2,21 +2,31 @@ import { useEffect, useMemo, useState } from 'react';
 
 import { useGlobalSettings } from '@app/context/GlobalSettingsContext';
 import { useUserContext } from '@app/context/useUserContext';
+import { useEntityContext } from '@app/entity/shared/EntityContext';
 import { filterOutNonExistentModulesFromTemplate } from '@app/homeV3/context/hooks/utils/moduleOperationsUtils';
+import { getDefaultSummaryPageTemplate } from '@app/homeV3/context/hooks/utils/utils';
 import { DEFAULT_TEMPLATE } from '@app/homeV3/modules/constants';
 
 import { PageTemplateFragment } from '@graphql/template.generated';
+import { PageTemplateSurfaceType } from '@types';
 
-export function useTemplateState() {
-    const [areTemplatesInitialized, setAreTemplatedInitialized] = useState(false);
+export function useTemplateState(templateType: PageTemplateSurfaceType) {
+    const [areTemplatesInitialized, setAreTemplatesInitialized] = useState(false);
     const [personalTemplate, setPersonalTemplate] = useState<PageTemplateFragment | null>(null);
     const [globalTemplate, setGlobalTemplate] = useState<PageTemplateFragment | null>(null);
 
+    const { entityType, entityData } = useEntityContext();
     const { settings, loaded: globalSettingsLoaded } = useGlobalSettings();
     const { user, loaded: userLoaded } = useUserContext();
 
+    // setting default and local templates for home page
     useEffect(() => {
-        if (globalSettingsLoaded && userLoaded && !areTemplatesInitialized) {
+        if (
+            globalSettingsLoaded &&
+            userLoaded &&
+            !areTemplatesInitialized &&
+            templateType === PageTemplateSurfaceType.HomePage
+        ) {
             setGlobalTemplate(
                 filterOutNonExistentModulesFromTemplate(settings.globalHomePageSettings?.defaultTemplate) ||
                     DEFAULT_TEMPLATE,
@@ -24,7 +34,7 @@ export function useTemplateState() {
             setPersonalTemplate(
                 filterOutNonExistentModulesFromTemplate(user?.settings?.homePage?.pageTemplate) || null,
             );
-            setAreTemplatedInitialized(true);
+            setAreTemplatesInitialized(true);
         }
     }, [
         globalSettingsLoaded,
@@ -33,6 +43,14 @@ export function useTemplateState() {
         settings.globalHomePageSettings?.defaultTemplate,
         user?.settings?.homePage?.pageTemplate,
     ]);
+
+    // setting default and local templates for asset summary page
+    useEffect(() => {
+        if (templateType === PageTemplateSurfaceType.AssetSummary && !!entityData) {
+            setGlobalTemplate(getDefaultSummaryPageTemplate(entityType));
+            setPersonalTemplate(entityData?.settings?.assetSummary?.templates?.[0].template || null);
+        }
+    }, [areTemplatesInitialized, entityType, entityData]);
 
     const [isEditingGlobalTemplate, setIsEditingGlobalTemplate] = useState(false);
 
