907ef919cc0a3b345d7e0d7e31e14c9be48dd5e6
Saketh Varma <sakethvarma397@gmail.com>
2025-08-30 15:12:21 -0700

    Lint

--- datahub-web-react/src/app/homeV3/module/components/__tests__/ModuleMenu.test.tsx
+++ datahub-web-react/src/app/homeV3/module/components/__tests__/ModuleMenu.test.tsx
@@ -29,12 +29,13 @@ vi.mock('@components', () => ({
     Text: (props: any) => <p {...props} />,
     Button: (props: any) => <button type="button" data-testid="confirm" {...props} />,
     Heading: (props: any) => <p {...props} />,
-    Modal: ({ children, buttons, onCancel, ...props }: any) => (
+    Modal: ({ children, buttons, ...props }: any) => (
         <div data-testid="modal" {...props}>
             {children}
             {buttons &&
                 buttons.map((button: any, index: number) => (
                     <button
+                        // eslint-disable-next-line
                         key={index}
                         data-testid={button.buttonDataTestId || `modal-button-${index}`}
                         onClick={button.onClick}
