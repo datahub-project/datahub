a0fd8f3ded4422ed715a476f7f5a7c593cc79ac0
Chris Collins <chriscollins3456@gmail.com>
2025-09-04 10:44:22 -0400

    minor tweaks to child hierarchy module

--- datahub-web-react/src/app/entityV2/summary/modules/childHierarchy/ChildHierarchyModule.tsx
+++ datahub-web-react/src/app/entityV2/summary/modules/childHierarchy/ChildHierarchyModule.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 
 import { useEntityData } from '@app/entity/shared/EntityContext';
+import { getChildHierarchyModule } from '@app/entityV2/summary/modules/childHierarchy/utils';
 import { ModuleProps } from '@app/homeV3/module/types';
 import HierarchyViewModule from '@app/homeV3/modules/hierarchyViewModule/HierarchyViewModule';
 
 export default function ChildHierarchyModule(props: ModuleProps) {
-    const { urn } = useEntityData();
-    const module = {
-        ...props.module,
-        properties: {
-            ...props.module.properties,
-            params: {
-                ...props.module.properties.params,
-                hierarchyViewParams: { assetUrns: [urn], showRelatedEntities: true },
-            },
-        },
-    };
+    const { urn, entityType } = useEntityData();
+    const module = getChildHierarchyModule(props.module, urn, entityType);
 
     return <HierarchyViewModule {...props} module={module} />;
 }
---
+++ datahub-web-react/src/app/entityV2/summary/modules/childHierarchy/utils.ts
@@ -0,0 +1,16 @@
+import { PageModuleFragment } from '@graphql/template.generated';
+import { EntityType } from '@types';
+
+export function getChildHierarchyModule(module: PageModuleFragment, urn: string, entityType: EntityType) {
+    return {
+        ...module,
+        properties: {
+            ...module.properties,
+            name: entityType === EntityType.GlossaryNode ? 'Contents' : 'Domains',
+            params: {
+                ...module.properties.params,
+                hierarchyViewParams: { assetUrns: [urn], showRelatedEntities: entityType === EntityType.GlossaryNode },
+            },
+        },
+    };
+}
