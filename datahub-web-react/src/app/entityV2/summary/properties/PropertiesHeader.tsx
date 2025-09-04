465bfd8e011010de5b082b8092d0700689e34b4c
Chris Collins <chriscollins3456@gmail.com>
2025-09-04 11:47:56 -0400

    feat(ui) Tie together custom summary frontend with updated backend (#14640)

--- datahub-web-react/src/app/entityV2/summary/properties/PropertiesHeader.tsx
+++ datahub-web-react/src/app/entityV2/summary/properties/PropertiesHeader.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
 
-import { useEntityData } from '@app/entity/shared/EntityContext';
 import PropertiesWithDividerWrapper from '@app/entityV2/summary/properties/PropertiesWithDividerWrapper';
-import AssetPropertiesProvider from '@app/entityV2/summary/properties/context/AssetPropertiesProvider';
 
 export default function PropertiesHeader() {
-    const { entityData } = useEntityData();
-
-    const editable = !!entityData?.privileges?.canManageAssetSummary;
-
-    return (
-        <AssetPropertiesProvider editable={editable}>
-            <PropertiesWithDividerWrapper />
-        </AssetPropertiesProvider>
-    );
+    return <PropertiesWithDividerWrapper />;
 }
--- datahub-web-react/src/app/homeV3/context/PageTemplateContext.tsx
+++ datahub-web-react/src/app/homeV3/context/PageTemplateContext.tsx
@@ -1,29 +1,42 @@
 import React, { ReactNode, createContext, useContext, useMemo, useState } from 'react';
 
+import { useEntityData } from '@app/entity/shared/EntityContext';
+import { useAssetSummaryOperations } from '@app/homeV3/context/hooks/useAssetSummaryOperations';
 import { useModuleModalState } from '@app/homeV3/context/hooks/useModuleModalState';
 import { useModuleOperations } from '@app/homeV3/context/hooks/useModuleOperations';
 import { useTemplateOperations } from '@app/homeV3/context/hooks/useTemplateOperations';
 import { useTemplateState } from '@app/homeV3/context/hooks/useTemplateState';
 import { PageTemplateContextState } from '@app/homeV3/context/types';
 
+import { PageTemplateSurfaceType } from '@types';
+
 const PageTemplateContext = createContext<PageTemplateContextState | undefined>(undefined);
 
-export const PageTemplateProvider = ({ children }: { children: ReactNode }) => {
+interface Props {
+    children: ReactNode;
+    templateType: PageTemplateSurfaceType;
+}
+
+export const PageTemplateProvider = ({ children, templateType }: Props) => {
+    const { entityData } = useEntityData();
+    const editable = !!entityData?.privileges?.canManageAssetSummary;
+    const isTemplateEditable = templateType === PageTemplateSurfaceType.AssetSummary ? editable : true;
     // Template state management
     const {
         personalTemplate,
         globalTemplate,
         template,
         isEditingGlobalTemplate,
+        summaryElements,
         setIsEditingGlobalTemplate,
         setPersonalTemplate,
         setGlobalTemplate,
         setTemplate,
-    } = useTemplateState();
+    } = useTemplateState(templateType);
 
     // Template operations
     const { updateTemplateWithModule, removeModuleFromTemplate, upsertTemplate, resetTemplateToDefault } =
-        useTemplateOperations(setPersonalTemplate, personalTemplate);
+        useTemplateOperations(setPersonalTemplate, personalTemplate, templateType);
 
     // Modal state
     const moduleModalState = useModuleModalState();
@@ -42,11 +55,22 @@ export const PageTemplateProvider = ({ children }: { children: ReactNode }) => {
         moduleModalState.initialState,
     );
 
+    // Asset summary operations
+    const { addSummaryElement, removeSummaryElement, replaceSummaryElement } = useAssetSummaryOperations(
+        isEditingGlobalTemplate,
+        personalTemplate,
+        globalTemplate,
+        setPersonalTemplate,
+        setGlobalTemplate,
+        upsertTemplate,
+    );
+
     // If modules should be reloaded
     const [reloadHomepageModules, setReloadHomepageModules] = useState(false);
 
     const value = useMemo(
         () => ({
+            isTemplateEditable,
             personalTemplate,
             globalTemplate,
             template,
@@ -63,8 +87,14 @@ export const PageTemplateProvider = ({ children }: { children: ReactNode }) => {
             resetTemplateToDefault,
             reloadHomepageModules,
             setReloadHomepageModules,
+            // Asset summary operations
+            summaryElements,
+            addSummaryElement,
+            removeSummaryElement,
+            replaceSummaryElement,
         }),
         [
+            isTemplateEditable,
             personalTemplate,
             globalTemplate,
             template,
@@ -81,6 +111,11 @@ export const PageTemplateProvider = ({ children }: { children: ReactNode }) => {
             resetTemplateToDefault,
             reloadHomepageModules,
             setReloadHomepageModules,
+            // Asset summary operations
+            summaryElements,
+            addSummaryElement,
+            removeSummaryElement,
+            replaceSummaryElement,
         ],
     );
 
