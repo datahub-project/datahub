94c56decdc36d70d564f6e4c6e30baa39578e7f4
purnimagarg1 <139125209+purnimagarg1@users.noreply.github.com>
2025-09-04 19:51:01 +0530

    feat: add new permission for managing asset summary (#14627)
    Co-authored-by: Chris Collins <chriscollins3456@gmail.com>

--- datahub-web-react/src/app/entityV2/summary/properties/PropertiesHeader.tsx
+++ datahub-web-react/src/app/entityV2/summary/properties/PropertiesHeader.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { useEntityData } from '@app/entity/shared/EntityContext';
 import PropertiesWithDividerWrapper from '@app/entityV2/summary/properties/PropertiesWithDividerWrapper';
 import AssetPropertiesProvider from '@app/entityV2/summary/properties/context/AssetPropertiesProvider';
 
 export default function PropertiesHeader() {
-    // TODO: use permissions
-    const editable = true;
+    const { entityData } = useEntityData();
+
+    const editable = !!entityData?.privileges?.canManageAssetSummary;
 
     return (
         <AssetPropertiesProvider editable={editable}>
