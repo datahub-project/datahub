1cf415c09d791691211336f20e286404d130f202
Peter Wang <petemango@ip-10-0-0-138.us-west-2.compute.internal>
2025-09-09 17:51:20 -0400

    add not in to the dropdown

--- datahub-web-react/src/app/entity/shared/tabs/Dataset/Validations/assertion/builder/steps/field/utils.ts
+++ datahub-web-react/src/app/entity/shared/tabs/Dataset/Validations/assertion/builder/steps/field/utils.ts
@@ -110,6 +110,15 @@ export const FIELD_VALUES_OPERATOR_CONFIG = {
             },
         },
         {
+            label: 'Is not in set',
+            value: AssertionStdOperator.NotIn,
+            parameters: {
+                value: {
+                    type: AssertionStdParameterType.Set,
+                },
+            },
+        },
+        {
             label: 'Not empty',
             value: AssertionStdOperator.GreaterThanOrEqualTo,
             parameters: {
@@ -230,6 +239,16 @@ export const FIELD_VALUES_OPERATOR_CONFIG = {
         {
             label: 'Is in set',
             value: AssertionStdOperator.In,
+            parameters: {
+                value: {
+                    type: AssertionStdParameterType.Set,
+                },
+            },
+            inputType: 'number',
+        },
+        {
+            label: 'Is not in set',
+            value: AssertionStdOperator.NotIn,
             parameters: {
                 value: {
                     type: AssertionStdParameterType.Set,
