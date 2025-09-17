e3c8d3788e9f9e97e04edca695440488a7329a30
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-09-17 11:59:51 -0400

    refactor(ingest/s3): make get_folder_info work with uris

--- metadata-ingestion/tests/unit/s3/test_s3_source.py
+++ metadata-ingestion/tests/unit/s3/test_s3_source.py
@@ -314,7 +314,8 @@ def test_get_folder_info_returns_latest_file_in_each_folder(s3_resource):
 
     # act
     res = _get_s3_source(path_spec).get_folder_info(
-        path_spec, bucket, prefix="my-folder"
+        path_spec,
+        "s3://my-bucket/my-folder",
     )
     res = list(res)
 
@@ -343,7 +344,7 @@ def test_get_folder_info_ignores_disallowed_path(s3_resource, caplog):
     s3_source = _get_s3_source(path_spec)
 
     # act
-    res = s3_source.get_folder_info(path_spec, bucket, prefix="my-folder")
+    res = s3_source.get_folder_info(path_spec, "s3://my-bucket/my-folder")
     res = list(res)
 
     # assert
@@ -377,7 +378,8 @@ def test_get_folder_info_returns_expected_folder(s3_resource):
 
     # act
     res = _get_s3_source(path_spec).get_folder_info(
-        path_spec, bucket, prefix="my-folder"
+        path_spec,
+        "s3://my-bucket/my-folder",
     )
     res = list(res)
 
