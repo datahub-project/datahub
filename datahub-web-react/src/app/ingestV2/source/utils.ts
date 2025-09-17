76975d44e78583411bd2edce36771997c4cdc387
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-09-17 13:01:04 +0300

    fix buildOwnerEntities

--- datahub-web-react/src/app/ingestV2/source/utils.ts
+++ datahub-web-react/src/app/ingestV2/source/utils.ts
@@ -610,6 +610,7 @@ export const buildOwnerEntities = (urn: string, owners?: Entity[], defaultOwnerT
             associatedUrn: urn,
             type: owner.type,
             ownershipType: defaultOwnerType ?? null,
+            attribution: owner.attribution ?? null,
             __typename: 'Owner' as const,
         })) || []
     );
