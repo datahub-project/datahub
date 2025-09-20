a23460ee90701b3e28097196d0d2a48296f4c815
rahul MALAWADKAR <49741611+relaxedboi@users.noreply.github.com>
2025-09-20 21:52:36 +0530

    Update SanitizingPatternLayoutEncoder.java

--- metadata-utils/src/main/java/com/linkedin/metadata/utils/log/SanitizingPatternLayoutEncoder.java
+++ metadata-utils/src/main/java/com/linkedin/metadata/utils/log/SanitizingPatternLayoutEncoder.java
@@ -2,15 +2,14 @@ package com.linkedin.metadata.utils.log;
 
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
 
-
 public class SanitizingPatternLayoutEncoder extends PatternLayoutEncoder {
-    @Override
-    public void start() {
-        SanitizingPatternLayout layout = new SanitizingPatternLayout();
-        layout.setContext(getContext());
-        layout.setPattern(getPattern());
-        layout.start();
-        this.layout = layout; // replace internal layout
-        super.start();
-    }
+  @Override
+  public void start() {
+    SanitizingPatternLayout layout = new SanitizingPatternLayout();
+    layout.setContext(getContext());
+    layout.setPattern(getPattern());
+    layout.start();
+    this.layout = layout; // replace internal layout
+    super.start();
+  }
 }
