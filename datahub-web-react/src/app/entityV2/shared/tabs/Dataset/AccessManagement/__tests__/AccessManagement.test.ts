0efd4f77e3f47c7e15df9b096065f6ab0c53ae5e
cclaude-dev <cclaude@ip-172-31-40-7>
2025-09-23 15:58:22 +0000

    Merge upstream commit 57250477bf with conflict resolution
    Original commit: feat(access-request): enable groups to be granted to role and grey button when granted (#14622)
    Original author: Jonny Dixon <45681293+acrylJonny@users.noreply.github.com>
    
    Conflicts resolved with Claude assistance.

--- datahub-web-react/src/app/entityV2/shared/tabs/Dataset/AccessManagement/__tests__/AccessManagement.test.ts
+++ datahub-web-react/src/app/entityV2/shared/tabs/Dataset/AccessManagement/__tests__/AccessManagement.test.ts
@@ -42,87 +42,8 @@ describe('handleAccessRoles (EntityV2)', () => {
             },
         };
 
-<<<<<<< HEAD
-        const GetMeQueryUser: GetMeQuery = {
-            me: {
-                corpUser: {
-                    urn: 'urn:li:corpuser:datahub',
-                    username: 'datahub',
-                    info: {
-                        active: true,
-                        displayName: 'DataHub',
-                        title: 'DataHub Root User',
-                        firstName: null,
-                        lastName: null,
-                        fullName: null,
-                        email: null,
-                        __typename: 'CorpUserInfo',
-                    },
-                    editableProperties: {
-                        displayName: null,
-                        title: null,
-                        pictureLink:
-                            'https://raw.githubusercontent.com/datahub-project/datahub/master/datahub-web-react/src/images/default_avatar.png',
-                        teams: [],
-                        skills: [],
-                        __typename: 'CorpUserEditableProperties',
-                    },
-                    settings: {
-                        appearance: {
-                            showSimplifiedHomepage: false,
-                            __typename: 'CorpUserAppearanceSettings',
-                        },
-                        views: null,
-                        __typename: 'CorpUserSettings',
-                    },
-                    __typename: 'CorpUser',
-                },
-                platformPrivileges: {
-                    viewAnalytics: true,
-                    managePolicies: true,
-                    viewMetadataProposals: true,
-                    manageIdentities: true,
-                    generatePersonalAccessTokens: true,
-                    manageIngestion: true,
-                    manageSecrets: true,
-                    manageTokens: true,
-                    manageDomains: true,
-                    manageTests: true,
-                    manageGlossaries: true,
-                    manageUserCredentials: true,
-                    manageTags: true,
-                    viewTests: false,
-                    viewManageTags: true,
-                    createDomains: true,
-                    createTags: true,
-                    manageGlobalSettings: true,
-                    manageGlobalViews: true,
-                    manageOwnershipTypes: true,
-                    manageGlobalAnnouncements: true,
-                    manageDocumentationForms: true,
-                    viewDocumentationFormsPage: true,
-                    createBusinessAttributes: true,
-                    manageBusinessAttributes: true,
-                    manageStructuredProperties: true,
-                    viewStructuredPropertiesPage: true,
-                    manageApplications: true,
-                    manageFeatures: true,
-                    manageHomePageTemplates: true,
-                    manageOrganizationDisplayPreferences: true,
-                    proposeCreateGlossaryTerm: true,
-                    proposeCreateGlossaryNode: true,
-                    canViewIngestionPage: true,
-                    createSupportTickets: true,
-                    __typename: 'PlatformPrivileges',
-                },
-                __typename: 'AuthenticatedUser',
-            },
-        };
-        const externalRole = handleAccessRoles(externalRolesQuery, GetMeQueryUser);
-=======
         const externalRole = handleAccessRoles(externalRolesQuery);
         expect(externalRole).toHaveLength(2);
->>>>>>> 57250477bfcbd08895244931a063a4bb3f7bbbf3
         expect(externalRole).toMatchObject([
             {
                 name: 'Test Role Granted',
@@ -151,89 +72,9 @@ describe('handleAccessRoles (EntityV2)', () => {
             },
         };
 
-<<<<<<< HEAD
-        const GetMeQueryUser: GetMeQuery = {
-            me: {
-                corpUser: {
-                    urn: 'urn:li:corpuser:datahub',
-                    username: 'datahub',
-                    info: {
-                        active: true,
-                        displayName: 'DataHub',
-                        title: 'DataHub Root User',
-                        firstName: null,
-                        lastName: null,
-                        fullName: null,
-                        email: null,
-                        __typename: 'CorpUserInfo',
-                    },
-                    editableProperties: {
-                        displayName: null,
-                        title: null,
-                        pictureLink:
-                            'https://raw.githubusercontent.com/datahub-project/datahub/master/datahub-web-react/src/images/default_avatar.png',
-                        teams: [],
-                        skills: [],
-                        __typename: 'CorpUserEditableProperties',
-                    },
-                    settings: {
-                        appearance: {
-                            showSimplifiedHomepage: false,
-                            __typename: 'CorpUserAppearanceSettings',
-                        },
-                        views: null,
-                        __typename: 'CorpUserSettings',
-                    },
-                    __typename: 'CorpUser',
-                },
-                platformPrivileges: {
-                    viewAnalytics: true,
-                    managePolicies: true,
-                    viewMetadataProposals: true,
-                    manageIdentities: true,
-                    generatePersonalAccessTokens: true,
-                    manageIngestion: true,
-                    manageSecrets: true,
-                    manageTokens: true,
-                    manageDomains: true,
-                    manageTests: true,
-                    manageGlossaries: true,
-                    manageUserCredentials: true,
-                    manageTags: true,
-                    viewTests: false,
-                    viewManageTags: true,
-                    createDomains: true,
-                    createTags: true,
-                    manageGlobalSettings: true,
-                    manageGlobalViews: true,
-                    manageOwnershipTypes: true,
-                    manageGlobalAnnouncements: true,
-                    manageDocumentationForms: true,
-                    viewDocumentationFormsPage: true,
-                    createBusinessAttributes: true,
-                    manageBusinessAttributes: true,
-                    manageStructuredProperties: true,
-                    viewStructuredPropertiesPage: true,
-                    manageApplications: true,
-                    manageFeatures: true,
-                    manageHomePageTemplates: true,
-                    manageOrganizationDisplayPreferences: true,
-                    proposeCreateGlossaryNode: true,
-                    proposeCreateGlossaryTerm: true,
-                    canViewIngestionPage: true,
-                    createSupportTickets: true,
-                    __typename: 'PlatformPrivileges',
-                },
-                __typename: 'AuthenticatedUser',
-            },
-        };
-        const externalRole = handleAccessRoles(externalRolesQuery, GetMeQueryUser);
-        expect(externalRole).toMatchObject([]);
-=======
         const externalRole = handleAccessRoles(externalRolesQuery);
         expect(externalRole).toHaveLength(0);
         expect(externalRole).toEqual([]);
->>>>>>> 57250477bfcbd08895244931a063a4bb3f7bbbf3
     });
 
     it('should handle roles without isAssignedToMe field gracefully', () => {
@@ -295,94 +136,6 @@ describe('handleAccessRoles (EntityV2)', () => {
             },
         };
 
-<<<<<<< HEAD
-        const GetMeQueryUser: GetMeQuery = {
-            me: {
-                corpUser: {
-                    urn: 'urn:li:corpuser:datahub',
-                    username: 'datahub',
-                    info: {
-                        active: true,
-                        displayName: 'DataHub',
-                        title: 'DataHub Root User',
-                        firstName: null,
-                        lastName: null,
-                        fullName: null,
-                        email: null,
-                        __typename: 'CorpUserInfo',
-                    },
-                    editableProperties: {
-                        displayName: null,
-                        title: null,
-                        pictureLink:
-                            'https://raw.githubusercontent.com/datahub-project/datahub/master/datahub-web-react/src/images/default_avatar.png',
-                        teams: [],
-                        skills: [],
-                        __typename: 'CorpUserEditableProperties',
-                    },
-                    settings: {
-                        appearance: {
-                            showSimplifiedHomepage: false,
-                            __typename: 'CorpUserAppearanceSettings',
-                        },
-                        views: null,
-                        __typename: 'CorpUserSettings',
-                    },
-                    __typename: 'CorpUser',
-                },
-                platformPrivileges: {
-                    viewAnalytics: true,
-                    managePolicies: true,
-                    viewMetadataProposals: true,
-                    manageIdentities: true,
-                    generatePersonalAccessTokens: true,
-                    manageIngestion: true,
-                    manageSecrets: true,
-                    manageTokens: true,
-                    manageDomains: true,
-                    manageTests: true,
-                    manageGlossaries: true,
-                    manageUserCredentials: true,
-                    manageTags: true,
-                    viewTests: false,
-                    viewManageTags: true,
-                    createDomains: true,
-                    createTags: true,
-                    manageGlobalSettings: true,
-                    manageGlobalViews: true,
-                    manageOwnershipTypes: true,
-                    manageGlobalAnnouncements: true,
-                    manageDocumentationForms: true,
-                    viewDocumentationFormsPage: true,
-                    createBusinessAttributes: true,
-                    manageBusinessAttributes: true,
-                    manageStructuredProperties: true,
-                    viewStructuredPropertiesPage: true,
-                    manageApplications: true,
-                    manageFeatures: true,
-                    manageHomePageTemplates: true,
-                    manageOrganizationDisplayPreferences: true,
-                    proposeCreateGlossaryNode: true,
-                    proposeCreateGlossaryTerm: true,
-                    canViewIngestionPage: true,
-                    createSupportTickets: true,
-                    __typename: 'PlatformPrivileges',
-                },
-                __typename: 'AuthenticatedUser',
-            },
-        };
-        const externalRole = handleAccessRoles(externalRolesQuery, GetMeQueryUser);
-        expect(externalRole).toMatchObject([
-            {
-                name: 'accessRole',
-                description:
-                    'This role access is required by the developers to test and deploy the code also adding few more details to check the description length for the given data and hence check the condition of read more and read less ',
-                accessType: 'READ',
-                hasAccess: false,
-                url: 'https://www.google.com/',
-            },
-        ]);
-=======
         const externalRole = handleAccessRoles(externalRolesQuery);
         expect(externalRole).toHaveLength(1);
         expect(externalRole[0]).toMatchObject({
@@ -392,6 +145,5 @@ describe('handleAccessRoles (EntityV2)', () => {
             hasAccess: false,
             url: undefined, // Should be undefined when requestUrl is null
         });
->>>>>>> 57250477bfcbd08895244931a063a4bb3f7bbbf3
     });
 });
