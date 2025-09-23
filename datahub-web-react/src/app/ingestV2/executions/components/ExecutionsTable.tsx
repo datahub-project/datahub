eecb77d7cd4a504b8bdfccce1744708d01a3c2f8
purnimagarg1 <purnima.garg@apptware.com>
2025-09-23 18:48:28 +0530

    add cypress tests for redesigned ingestion flow

--- datahub-web-react/src/app/ingestV2/executions/components/ExecutionsTable.tsx
+++ datahub-web-react/src/app/ingestV2/executions/components/ExecutionsTable.tsx
@@ -149,6 +149,7 @@ export default function ExecutionsTable({
                         <TableFooter hiddenItemsMessage="Some executions may be hidden" colSpan={tableColumns.length} />
                     ) : null
                 }
+                data-testid="executions-table"
             />
             <RollbackExecutionConfirmation
                 isOpen={!!runIdOfRollbackConfirmation}
