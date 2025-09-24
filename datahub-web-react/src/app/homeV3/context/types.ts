9d1a3cc23ac483dfaa9a91ea3b3e7309a4c41faa
Chris Collins <chriscollins3456@gmail.com>
2025-09-24 16:37:03 -0400

    remove unnecessary additions

--- datahub-web-react/src/app/homeV3/context/types.ts
+++ datahub-web-react/src/app/homeV3/context/types.ts
@@ -68,8 +68,6 @@ export type PageTemplateContextState = {
     removeModule: (input: RemoveModuleInput) => void;
     moveModule: (input: MoveModuleInput) => void;
     resetTemplateToDefault: () => void;
-    reloadHomepageModules: boolean;
-    setReloadHomepageModules: (val: boolean) => void;
     moduleContext: TemplateUpdateContext;
     // Asset summary operations
     summaryElements?: AssetProperty[];
