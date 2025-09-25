87ee1426633004a4fe5dcc9872025abef0b16d95
Chakru <161002324+chakru-r@users.noreply.github.com>
2025-09-26 01:09:59 +0530

    wait_for_sync_Writes fix

--- smoke-test/tests/consistency_utils.py
+++ smoke-test/tests/consistency_utils.py
@@ -44,7 +44,7 @@ def wait_for_writes_to_sync(
         time.sleep(1)  # micro-sleep
 
         cmd = (
-            f"docker exec {KAFKA_BROKER_CONTAINER} /bin/kafka-consumer-groups --bootstrap-server {KAFKA_BOOTSTRAP_SERVER} --all-groups --describe | grep -E '(generic-mce-consumer-job-client|cdc-consumer-job-client)'  "
+            f"docker exec {KAFKA_BROKER_CONTAINER} /bin/kafka-consumer-groups --bootstrap-server {KAFKA_BOOTSTRAP_SERVER} --all-groups --describe | grep -E '({consumer_group}|cdc-consumer-job-client)'  "
             + "| awk '{print $6}'"
         )
         try:
