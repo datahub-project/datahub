9eb047a92541d00759b0d5f31d3500cac3cd0a47
David Leifker <david.leifker@acryl.io>
2025-10-01 17:05:56 -0500

    systemUpdate cdc mode relocation

--- metadata-io/src/main/java/com/linkedin/metadata/entity/EntityServiceImpl.java
+++ metadata-io/src/main/java/com/linkedin/metadata/entity/EntityServiceImpl.java
@@ -242,9 +242,7 @@ public class EntityServiceImpl implements EntityService<ChangeItemImpl> {
     this.aspectDao = aspectDao;
     this.producer = producer;
     this.alwaysEmitChangeLog = alwaysEmitChangeLog;
-    String systemUpdateCDCMode = System.getenv("SYSTEM_UPDATE_CDC_MODE");
-    this.cdcModeChangeLog =
-        systemUpdateCDCMode != null ? Boolean.parseBoolean(systemUpdateCDCMode) : cdcModeChangeLog;
+    this.cdcModeChangeLog = cdcModeChangeLog;
     this.preProcessHooks = preProcessHooks;
     ebeanMaxTransactionRetry = retry != null ? retry : DEFAULT_MAX_TRANSACTION_RETRY;
     this.enableBrowseV2 = enableBrowseV2;
