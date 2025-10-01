276da40a73ed3c143cf355dc8aaebadf9a57a69d
Saketh Varma <sakethvarma397@gmail.com>
2025-10-01 15:56:09 +0530

    Update updateQueryParams.ts

--- datahub-web-react/src/app/shared/updateQueryParams.ts
+++ datahub-web-react/src/app/shared/updateQueryParams.ts
@@ -5,6 +5,7 @@ type QueryParam = {
     [key: string]: string | undefined;
 };
 
+// Doesn't support the newParams with special characters
 export default function updateQueryParams(newParams: QueryParam, location: Location, history: History) {
     const parsedParams = QueryString.parse(location.search, { arrayFormat: 'comma', decode: false });
     const updatedParams = {
