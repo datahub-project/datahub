268da82ba502afa08e685c0c32092b22e545fc00
Chris Collins <chriscollins3456@gmail.com>
2025-10-03 11:24:33 -0400

    fix linting error

--- datahub-web-react/src/app/homeV3/settings/HomePageSettingsButtonWrapper.tsx
+++ datahub-web-react/src/app/homeV3/settings/HomePageSettingsButtonWrapper.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export default function HomePageSettingsButtonWrapper() {
     // There is no settings button in OSS because editing is disabled in OSS.
     return null;
