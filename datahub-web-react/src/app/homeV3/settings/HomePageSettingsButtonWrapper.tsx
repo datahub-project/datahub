8461f28d7c0b0feabcd18be95561c318f1bd6a85
Chris Collins <chriscollins3456@gmail.com>
2025-10-03 11:07:45 -0400

    add react import

--- datahub-web-react/src/app/homeV3/settings/HomePageSettingsButtonWrapper.tsx
+++ datahub-web-react/src/app/homeV3/settings/HomePageSettingsButtonWrapper.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function HomePageSettingsButtonWrapper() {
     // There is no settings button in OSS because editing is disabled in OSS.
     return null;
