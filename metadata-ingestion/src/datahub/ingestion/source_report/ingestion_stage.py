8c75384ecc3e6c728251b1ab47a6c1d9f5a2ed3b
Aseem Bansal <asmbansal2@gmail.com>
2025-10-03 18:29:04 +0530

    feat(ingest): add high level stage for ingestion (#14862)

--- metadata-ingestion/src/datahub/ingestion/source_report/ingestion_stage.py
+++ metadata-ingestion/src/datahub/ingestion/source_report/ingestion_stage.py
@@ -1,7 +1,10 @@
 import logging
+from collections import defaultdict
 from contextlib import AbstractContextManager
 from dataclasses import dataclass, field
 from datetime import datetime, timezone
+from enum import Enum
+from typing import Tuple
 
 from datahub.utilities.perf_timer import PerfTimer
 from datahub.utilities.stats_collections import TopKDict
@@ -20,31 +23,70 @@ QUERIES_EXTRACTION = "Queries Extraction"
 PROFILING = "Profiling"
 
 
+class IngestionHighStage(Enum):
+    """
+    The high-level stages at the framework level
+    Team to add more stages as needed
+    """
+
+    PROFILING = "Profiling"
+    _UNDEFINED = "Ingestion"
+
+
 @dataclass
 class IngestionStageReport:
-    ingestion_stage_durations: TopKDict[str, float] = field(default_factory=TopKDict)
+    ingestion_high_stage_seconds: dict[IngestionHighStage, float] = field(
+        default_factory=lambda: defaultdict(float)
+    )
+    ingestion_stage_durations: TopKDict[Tuple[IngestionHighStage, str], float] = field(
+        default_factory=TopKDict
+    )
 
-    def new_stage(self, stage: str) -> "IngestionStageContext":
-        return IngestionStageContext(stage, self)
+    def new_stage(
+        self, stage: str, high_stage: IngestionHighStage = IngestionHighStage._UNDEFINED
+    ) -> "IngestionStageContext":
+        return IngestionStageContext(stage, self, high_stage)
+
+    def new_high_stage(self, stage: IngestionHighStage) -> "IngestionStageContext":
+        return IngestionStageContext("", self, stage)
 
 
 @dataclass
 class IngestionStageContext(AbstractContextManager):
-    def __init__(self, stage: str, report: IngestionStageReport):
-        self._ingestion_stage = f"{stage} at {datetime.now(timezone.utc)}"
+    def __init__(
+        self,
+        stage: str,
+        report: IngestionStageReport,
+        high_stage: IngestionHighStage = IngestionHighStage._UNDEFINED,
+    ):
+        self._high_stage = high_stage
+        self._ingestion_stage = (
+            f"{stage} at {datetime.now(timezone.utc)}" if stage else ""
+        )
         self._timer: PerfTimer = PerfTimer()
         self._report = report
 
     def __enter__(self) -> "IngestionStageContext":
-        logger.info(f"Stage started: {self._ingestion_stage}")
+        if self._ingestion_stage:
+            logger.info(f"Stage started: {self._ingestion_stage}")
+        else:
+            logger.info(f"High stage started: {self._high_stage.value}")
         self._timer.start()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         elapsed = self._timer.elapsed_seconds(digits=2)
-        logger.info(
-            f"Time spent in stage <{self._ingestion_stage}>: {elapsed} seconds",
-            stacklevel=2,
-        )
-        self._report.ingestion_stage_durations[self._ingestion_stage] = elapsed
-        return None
+        if self._ingestion_stage:
+            logger.info(
+                f"Time spent in stage <{self._ingestion_stage}>: {elapsed} seconds",
+                stacklevel=2,
+            )
+            self._report.ingestion_stage_durations[
+                (self._high_stage, self._ingestion_stage)
+            ] = elapsed
+        else:
+            logger.info(
+                f"Time spent in stage <{self._high_stage.value}>: {elapsed} seconds",
+                stacklevel=2,
+            )
+        self._report.ingestion_high_stage_seconds[self._high_stage] += elapsed
