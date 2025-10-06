40b51ac2da4f84dddc65fac2a538d1486b18bd02
Anush Kumar <anush.kumar@datahub.com>
2025-10-06 10:06:37 -0700

    feat(ingestion): Added Databricks support to Fivetran source (#14897)

--- metadata-ingestion/src/datahub/ingestion/source/fivetran/fivetran_query.py
+++ metadata-ingestion/src/datahub/ingestion/source/fivetran/fivetran_query.py
@@ -6,6 +6,21 @@ MAX_COLUMN_LINEAGE_PER_CONNECTOR = 1000
 MAX_JOBS_PER_CONNECTOR = 500
 
 
+"""
+------------------------------------------------------------------------------------------------------------
+Fivetran Platform Connector Handling
+------------------------------------------------------------------------------------------------------------
+Current Query Change Log: August 2025 (See: https://fivetran.com/docs/changelog/2025/august-2025)
+
+All queries have to be updated as per Fivetran Platform Connector release if any. We expect customers
+and fivetran to keep platform connector configured for DataHub with auto sync enabled to get latest changes.
+
+References:
+- Fivetran Release Notes: https://fivetran.com/docs/changelog (Look for "Fivetran Platform Connector")
+- Latest Platform Connector Schema: https://fivetran.com/docs/logs/fivetran-platform?erdModal=open
+"""
+
+
 class FivetranLogQuery:
     # Note: All queries are written in Snowflake SQL.
     # They will be transpiled to the target database's SQL dialect at runtime.
@@ -30,17 +45,17 @@ class FivetranLogQuery:
     def get_connectors_query(self) -> str:
         return f"""\
 SELECT
-  connector_id,
+  connection_id,
   connecting_user_id,
   connector_type_id,
-  connector_name,
+  connection_name,
   paused,
   sync_frequency,
   destination_id
-FROM {self.schema_clause}connector
+FROM {self.schema_clause}connection
 WHERE
   _fivetran_deleted = FALSE
-QUALIFY ROW_NUMBER() OVER (PARTITION BY connector_id ORDER BY _fivetran_synced DESC) = 1
+QUALIFY ROW_NUMBER() OVER (PARTITION BY connection_id ORDER BY _fivetran_synced DESC) = 1
 """
 
     def get_users_query(self) -> str:
@@ -63,20 +78,20 @@ FROM {self.schema_clause}user
         return f"""\
 WITH ranked_syncs AS (
     SELECT
-        connector_id,
+        connection_id,
         sync_id,
         MAX(CASE WHEN message_event = 'sync_start' THEN time_stamp END) as start_time,
         MAX(CASE WHEN message_event = 'sync_end' THEN time_stamp END) as end_time,
         MAX(CASE WHEN message_event = 'sync_end' THEN message_data END) as end_message_data,
-        ROW_NUMBER() OVER (PARTITION BY connector_id ORDER BY MAX(time_stamp) DESC) as rn
+        ROW_NUMBER() OVER (PARTITION BY connection_id ORDER BY MAX(time_stamp) DESC) as rn
     FROM {self.schema_clause}log
     WHERE message_event in ('sync_start', 'sync_end')
     AND time_stamp > CURRENT_TIMESTAMP - INTERVAL '{syncs_interval} days'
-    AND connector_id IN ({formatted_connector_ids})
-    GROUP BY connector_id, sync_id
+    AND connection_id IN ({formatted_connector_ids})
+    GROUP BY connection_id, sync_id
 )
 SELECT
-    connector_id,
+    connection_id,
     sync_id,
     start_time,
     end_time,
@@ -85,7 +100,7 @@ FROM ranked_syncs
 WHERE rn <= {MAX_JOBS_PER_CONNECTOR}
     AND start_time IS NOT NULL
     AND end_time IS NOT NULL
-ORDER BY connector_id, end_time DESC
+ORDER BY connection_id, end_time DESC
 """
 
     def get_table_lineage_query(self, connector_ids: List[str]) -> str:
@@ -97,7 +112,7 @@ SELECT
     *
 FROM (
     SELECT
-        stm.connector_id as connector_id,
+        stm.connection_id as connection_id,
         stm.id as source_table_id,
         stm.name as source_table_name,
         ssm.name as source_schema_name,
@@ -105,18 +120,18 @@ FROM (
         dtm.name as destination_table_name,
         dsm.name as destination_schema_name,
         tl.created_at as created_at,
-        ROW_NUMBER() OVER (PARTITION BY stm.connector_id, stm.id, dtm.id ORDER BY tl.created_at DESC) as table_combo_rn
+        ROW_NUMBER() OVER (PARTITION BY stm.connection_id, stm.id, dtm.id ORDER BY tl.created_at DESC) as table_combo_rn
     FROM {self.schema_clause}table_lineage as tl
-    JOIN {self.schema_clause}source_table_metadata as stm on tl.source_table_id = stm.id
-    JOIN {self.schema_clause}destination_table_metadata as dtm on tl.destination_table_id = dtm.id
-    JOIN {self.schema_clause}source_schema_metadata as ssm on stm.schema_id = ssm.id
-    JOIN {self.schema_clause}destination_schema_metadata as dsm on dtm.schema_id = dsm.id
-    WHERE stm.connector_id IN ({formatted_connector_ids})
+    JOIN {self.schema_clause}source_table as stm on tl.source_table_id = stm.id -- stm: source_table_metadata
+    JOIN {self.schema_clause}destination_table as dtm on tl.destination_table_id = dtm.id -- dtm: destination_table_metadata
+    JOIN {self.schema_clause}source_schema as ssm on stm.schema_id = ssm.id -- ssm: source_schema_metadata
+    JOIN {self.schema_clause}destination_schema as dsm on dtm.schema_id = dsm.id -- dsm: destination_schema_metadata
+    WHERE stm.connection_id IN ({formatted_connector_ids})
 )
 -- Ensure that we only get back one entry per source and destination pair.
 WHERE table_combo_rn = 1
-QUALIFY ROW_NUMBER() OVER (PARTITION BY connector_id ORDER BY created_at DESC) <= {MAX_TABLE_LINEAGE_PER_CONNECTOR}
-ORDER BY connector_id, created_at DESC
+QUALIFY ROW_NUMBER() OVER (PARTITION BY connection_id ORDER BY created_at DESC) <= {MAX_TABLE_LINEAGE_PER_CONNECTOR}
+ORDER BY connection_id, created_at DESC
 """
 
     def get_column_lineage_query(self, connector_ids: List[str]) -> str:
@@ -131,25 +146,25 @@ SELECT
     destination_column_name
 FROM (
     SELECT
-        stm.connector_id as connector_id,
+        stm.connection_id as connection_id,
         scm.table_id as source_table_id,
         dcm.table_id as destination_table_id,
         scm.name as source_column_name,
         dcm.name as destination_column_name,
         cl.created_at as created_at,
-        ROW_NUMBER() OVER (PARTITION BY stm.connector_id, cl.source_column_id, cl.destination_column_id ORDER BY cl.created_at DESC) as column_combo_rn
+        ROW_NUMBER() OVER (PARTITION BY stm.connection_id, cl.source_column_id, cl.destination_column_id ORDER BY cl.created_at DESC) as column_combo_rn
     FROM {self.schema_clause}column_lineage as cl
-    JOIN {self.schema_clause}source_column_metadata as scm
+    JOIN {self.schema_clause}source_column as scm -- scm: source_column_metadata
       ON cl.source_column_id = scm.id
-    JOIN {self.schema_clause}destination_column_metadata as dcm
+    JOIN {self.schema_clause}destination_column as dcm -- dcm: destination_column_metadata
       ON cl.destination_column_id = dcm.id
-    -- Only joining source_table_metadata to get the connector_id.
-    JOIN {self.schema_clause}source_table_metadata as stm
+    -- Only joining source_table to get the connection_id.
+    JOIN {self.schema_clause}source_table as stm -- stm: source_table_metadata
       ON scm.table_id = stm.id
-    WHERE stm.connector_id IN ({formatted_connector_ids})
+    WHERE stm.connection_id IN ({formatted_connector_ids})
 )
 -- Ensure that we only get back one entry per (connector, source column, destination column) pair.
 WHERE column_combo_rn = 1
-QUALIFY ROW_NUMBER() OVER (PARTITION BY connector_id ORDER BY created_at DESC) <= {MAX_COLUMN_LINEAGE_PER_CONNECTOR}
-ORDER BY connector_id, created_at DESC
+QUALIFY ROW_NUMBER() OVER (PARTITION BY connection_id ORDER BY created_at DESC) <= {MAX_COLUMN_LINEAGE_PER_CONNECTOR}
+ORDER BY connection_id, created_at DESC
 """
