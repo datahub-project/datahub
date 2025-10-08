bb66709dcd28e8283aa4604e5db3e7e9a83ea50b
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-10-08 10:29:55 -0400

    basic db2 source

---
+++ metadata-ingestion/src/datahub/ingestion/source/sql/db2.py
@@ -0,0 +1,52 @@
+from typing import Dict
+
+import pydantic
+
+from datahub.ingestion.api.common import PipelineContext
+from datahub.ingestion.api.decorators import (
+    SourceCapability,
+    SupportStatus,
+    capability,
+    config_class,
+    platform_name,
+    support_status,
+)
+from datahub.ingestion.source.sql.sql_common import SQLAlchemySource
+from datahub.ingestion.source.sql.sql_config import BasicSQLAlchemyConfig
+from datahub.ingestion.source.sql.sqlalchemy_uri import make_sqlalchemy_uri
+
+
+class Db2Config(BasicSQLAlchemyConfig):
+    # Override defaults
+    host_port: str = pydantic.Field(default="localhost:50000")
+    scheme: str = pydantic.Field(default="db2+ibm_db")  # TODO: hide this?
+    username: str
+    password: str
+    database: str
+
+    def get_sql_alchemy_url(self):
+        return make_sqlalchemy_uri(
+            self.scheme,
+            self.username,
+            self.password,
+            self.host_port,
+        )
+
+
+@platform_name("IBM Db2", id="db2")
+@config_class(Db2Config)
+@support_status(SupportStatus.TESTING)
+@capability(SourceCapability.PLATFORM_INSTANCE, "Enabled by default")
+@capability(SourceCapability.DOMAINS, "Supported via the `domain` config field")
+@capability(SourceCapability.DATA_PROFILING, "Optionally enabled via configuration")
+@capability(
+    SourceCapability.DELETION_DETECTION, "Enabled by default via stateful ingestion"
+)
+class Db2Source(SQLAlchemySource):
+    def __init__(self, config: Db2Config, ctx: PipelineContext):
+        super().__init__(config, ctx, "db2")
+
+    @classmethod
+    def create(cls, config_dict: Dict, ctx: PipelineContext) -> "Db2Source":
+        config = Db2Config.parse_obj(config_dict)
+        return cls(config, ctx)
