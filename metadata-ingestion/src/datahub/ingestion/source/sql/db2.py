7957352c7ff4ff6e4b0898e746e24f43d4559e19
Michael Maltese <michaeljosephmaltese@gmail.com>
2025-10-08 15:25:08 -0400

    get_view_default_db_schema stuff

--- metadata-ingestion/src/datahub/ingestion/source/sql/db2.py
+++ metadata-ingestion/src/datahub/ingestion/source/sql/db2.py
@@ -1,3 +1,4 @@
+import logging
 from typing import (
     Dict,
     Iterable,
@@ -22,6 +23,8 @@ from datahub.ingestion.api.decorators import (
 from datahub.ingestion.source.sql.sql_common import SQLAlchemySource
 from datahub.ingestion.source.sql.sql_config import BasicSQLAlchemyConfig
 from datahub.ingestion.source.sql.sqlalchemy_uri import make_sqlalchemy_uri
+
+logger = logging.getLogger(__name__)
 
 
 class Db2(SQLGlotDialect):
@@ -73,9 +76,26 @@ class Db2Source(SQLAlchemySource):
         config = Db2Config.parse_obj(config_dict)
         return cls(config, ctx)
 
-    def get_db_schema(self, dataset_identifier: str) -> Tuple[Optional[str], str]:
-        schema, _view = dataset_identifier.split(".", 1)
-        return None, schema
+    def get_view_default_db_schema(
+        self, _dataset_name: str, inspector: Inspector, schema: str, view: str
+    ) -> Tuple[Optional[str], str]:
+        try:
+            result = inspector.bind.execute(
+                """
+                select QUALIFIER
+                from SYSCAT.VIEWS
+                where VIEWSCHEMA = ?
+                and VIEWNAME = ?
+            """,
+                (schema, view),
+            )
+            return None, result.scalar()
+        except Exception as e:
+            logger.warning(
+                f"Failed to get qualifier for unqualified names for view {schema}.{view}: {e}",
+                exc_info=e,
+            )  # TODO add to report
+        return super().get_view_default_db_schema(inspector, schema, view)
 
     def get_inspectors(self) -> Iterable[Inspector]:
         for inspector in super().get_inspectors():
