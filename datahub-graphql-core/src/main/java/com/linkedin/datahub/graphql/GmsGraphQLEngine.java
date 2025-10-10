26fb897df363b0a3b98d2e7a77b5389e70b3f438
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2025-10-10 11:34:27 +0300

    fix tests

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
@@ -630,7 +630,8 @@ public class GmsGraphQLEngine {
     this.stsClient = args.stsClient;
 
     // TODO: Add logic to get real role
-    String gmsRoleArn = "arn:aws:iam::000000000000:root";
+    // String gmsRoleArn = "arn:aws:iam::000000000000:root";
+    String gmsRoleArn = null;
     if (gmsRoleArn == null || gmsRoleArn.trim().isEmpty() || this.stsClient == null) {
       log.info("GMS role ARN is not set or STS client is not available, not creating S3Util");
       this.s3Util = null;
