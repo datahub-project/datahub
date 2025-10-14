a85c4ffca8936dd6403477ae2737fa29dd7de5ca
Aseem Bansal <asmbansal2@gmail.com>
2025-10-14 15:52:02 +0530

    refactor(test): refactor smoke test (#14998)

--- smoke-test/tests/incidents/incidents_test.py
+++ smoke-test/tests/incidents/incidents_test.py
@@ -3,21 +3,15 @@ from typing import Any, Dict
 
 import pytest
 
-from tests.utils import (
-    delete_entity,
-    delete_urns_from_file,
-    execute_graphql,
-    ingest_file_via_rest,
-)
+from conftest import _ingest_cleanup_data_impl
+from tests.utils import delete_entity, execute_graphql
 
 
 @pytest.fixture(scope="module", autouse=True)
-def ingest_cleanup_data(auth_session, graph_client, request):
-    print("ingesting incidents test data")
-    ingest_file_via_rest(auth_session, "tests/incidents/data.json")
-    yield
-    print("removing incidents test data")
-    delete_urns_from_file(graph_client, "tests/incidents/data.json")
+def ingest_cleanup_data(auth_session, graph_client):
+    yield from _ingest_cleanup_data_impl(
+        auth_session, graph_client, "tests/incidents/data.json", "incidents"
+    )
 
 
 TEST_DATASET_URN = (
