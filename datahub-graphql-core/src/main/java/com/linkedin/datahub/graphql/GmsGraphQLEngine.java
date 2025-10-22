fa5a95263075d32a49357e051408fe9237f96362
Ben Blazke <ben.blazke@datahub.com>
2025-10-22 09:30:00 -0700

    bug(telemetry): UX fixes

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/GmsGraphQLEngine.java
@@ -54,7 +54,6 @@ import com.linkedin.datahub.graphql.resolvers.chart.BrowseV2Resolver;
 import com.linkedin.datahub.graphql.resolvers.chart.ChartStatsSummaryResolver;
 import com.linkedin.datahub.graphql.resolvers.config.AppConfigResolver;
 import com.linkedin.datahub.graphql.resolvers.config.ProductUpdateResolver;
-import com.linkedin.datahub.graphql.resolvers.config.RefreshProductUpdateResolver;
 import com.linkedin.datahub.graphql.resolvers.connection.UpsertConnectionResolver;
 import com.linkedin.datahub.graphql.resolvers.container.ContainerEntitiesResolver;
 import com.linkedin.datahub.graphql.resolvers.container.ParentContainersResolver;
@@ -1432,10 +1431,7 @@ public class GmsGraphQLEngine {
                   "updateApplicationsSettings",
                   new UpdateApplicationsSettingsResolver(this.settingsService))
               .dataFetcher(
-                  "updateAssetSettings", new UpdateAssetSettingsResolver(this.entityClient))
-              .dataFetcher(
-                  "refreshProductUpdate",
-                  new RefreshProductUpdateResolver(this.productUpdateService, this.featureFlags));
+                  "updateAssetSettings", new UpdateAssetSettingsResolver(this.entityClient));
 
           if (featureFlags.isBusinessAttributeEntityEnabled()) {
             typeWiring
