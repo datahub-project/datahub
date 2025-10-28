1e33705b6b695634b86aa34207226a1dbbbe4e65
Anirudh Reddy Malgari <amalgari@Anirudhs-MacBook-Pro.local>
2025-10-28 12:54:09 -0700

    Fixed more CI cypress failures

--- smoke-test/tests/cypress/cypress/e2e/manage_tagsV2/helpers/tags_page_helper.js
+++ smoke-test/tests/cypress/cypress/e2e/manage_tagsV2/helpers/tags_page_helper.js
@@ -21,7 +21,8 @@ export default class TagsPageHelper {
       // Wait for modal to close - check that add button is available again
       cy.getWithTestId("add-tag-button").should("be.visible");
     } else {
-      cy.waitTextVisible("Failed to create tag. An unexpected error occurred");
+      // Wait for any error message that starts with "Failed to create tag."
+      cy.contains(/Failed to create tag\./).should("be.visible");
       cy.clickOptionWithTestId("create-tag-modal-cancel-button");
     }
   }
