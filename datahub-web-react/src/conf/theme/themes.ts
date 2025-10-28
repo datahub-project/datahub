bb26875e1876ca1d5e4d08fdf06cca7fb8f4e43e
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2025-10-28 14:08:09 -0700

    prepare for merge later

--- datahub-web-react/src/conf/theme/themes.ts
+++ datahub-web-react/src/conf/theme/themes.ts
@@ -3,4 +3,6 @@ import themeV2 from '@conf/theme/themeV2';
 import themeV2Dark from '@conf/theme/themeV2Dark';
 
 // Add custom themes here
-export default { themeV1, themeV2, themeV2Dark };
+const themes = { themeV1, themeV2, themeV2Dark };
+
+export default themes;
