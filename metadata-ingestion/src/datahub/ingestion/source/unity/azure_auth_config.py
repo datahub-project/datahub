536b9abfb4a442585eed134076b5121abfc08b22
pjain155_uhg <anshul_p@optum.com>
2025-10-28 15:36:42 +0530

    refactor: Fix build lint issues

--- metadata-ingestion/src/datahub/ingestion/source/unity/azure_auth_config.py
+++ metadata-ingestion/src/datahub/ingestion/source/unity/azure_auth_config.py
@@ -7,9 +7,7 @@ class AzureAuthConfig(ConfigModel):
     client_secret: str = Field(description="Azure client secret")
     client_id: str = Field(
         description="Azure client (Application) ID",
-        default=None,
     )
     tenant_id: str = Field(
         description="Azure tenant (Directory) ID required when a `client_secret` is used as a credential.",
-        default=None,
     )
