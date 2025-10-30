20a5346b30b4a3732b98c477cfe37531c595224d
purnimagarg1 <purnima.garg@apptware.com>
2025-10-30 17:48:10 +0530

    fix cypress test for dataset_health

--- smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
+++ smoke-test/tests/cypress/cypress/e2e/mutations/dataset_health.js
@@ -7,12 +7,11 @@ describe("dataset health test", () => {
     cy.login();
     cy.goToDataset(urn, datasetName);
     // Ensure that the “Health” badge is present and there is an active incident warning
-    cy.get(`[href="/dataset/${urn}/Quality"]`).should("be.visible");
-    cy.get(`[href="/dataset/${urn}/Quality"] span`).trigger("mouseover", {
+    cy.get(`[data-testid="${urn}-health-icon"]`).first().should("be.visible");
+    cy.get(`[data-testid="${urn}-health-icon"]`).first().trigger("mouseover", {
       force: true,
     });
-    cy.waitTextVisible("This asset may be unhealthy");
-    cy.waitTextVisible("Assertions 1 of 1 assertions are failing");
+    cy.waitTextVisible("1 of 1 assertions are failing");
     cy.waitTextVisible("1 active incident");
   });
 });
