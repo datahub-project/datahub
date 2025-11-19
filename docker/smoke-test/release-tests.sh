577779df42382725bc46df9575f82a5dc3bf88b2
Aseem Bansal <asmbansal2@gmail.com>
2025-11-19 15:30:33 +0530

    test(release): run read only tests with release tests (#7265)

--- docker/smoke-test/release-tests.sh
+++ docker/smoke-test/release-tests.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 # add additional paths of tests that work in remote mode. Most current smoke pytests dont work in remote mode.
-pytest -rP --durations=20 -vvs --log-cli-level=INFO --tb=long --showlocals --continue-on-collection-errors -m "release_tests" tests/release_tests
+pytest -rP --durations=20 -vvs --log-cli-level=INFO --tb=long --showlocals --continue-on-collection-errors -m "release_tests or read_only" tests
