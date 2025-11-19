969f420212483270cad0bbc5436a9917245f1256
Rodriguez, Jonathan (CCB, USA) <jonathan.rodriguez@jpmchase.com>
2025-11-19 15:12:02 -0500

    DATAMGT-5231: mfe contribution (frontend)

---
+++ datahub-frontend/app/controllers/MfeConfigController.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import com.typesafe.config.Config;
+import java.nio.file.Files;
+import javax.annotation.Nonnull;
+import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
+import play.Environment;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Results;
+
+@Slf4j
+public class MfeConfigController extends Controller {
+
+  private final String configFilePath;
+  private final Environment environment;
+
+  @Inject
+  public MfeConfigController(@Nonnull Config configs, Environment environment) {
+    this.configFilePath = configs.getString("mfeConfigFilePath");
+    this.environment = environment;
+  }
+
+  public Result getMfeConfig() {
+    if (configFilePath == null) {
+      log.error("MfeConfigController Config File Path not set!");
+      return Results.internalServerError("MfeConfigController error! Config File Path not set!");
+    }
+    String yamlContent = readFromYMLConfig(configFilePath);
+    if (yamlContent.isEmpty()) {
+      log.error("MFEConfigController unable to read config file at: {}", configFilePath);
+      return Results.internalServerError("MFEConfigController error! Unable to read config file!");
+    } else {
+      log.info("MfeConfigController read yaml success");
+      return Results.ok(yamlContent);
+    }
+  }
+
+  public String readFromYMLConfig(String configFilePath) {
+    log.info("Attempting to read YAML config from: {}", configFilePath);
+    try {
+      String content = Files.readString(environment.getFile(configFilePath).toPath());
+      log.info("Successfully read YAML config");
+      return content;
+    } catch (Exception e) {
+      log.error(
+          "MfeConfigController error! Failed to read YAML config file at: {}. Error: {}.",
+          configFilePath,
+          e.getMessage());
+      return "";
+    }
+  }
+}
