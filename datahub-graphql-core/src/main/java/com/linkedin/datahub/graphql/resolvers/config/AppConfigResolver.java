5d0ee1da2ab5fcb878458ef0d2648d554018acf9
Anirudh Reddy Malgari <anirudh.malgari@datahub.com>
2025-11-26 11:29:35 -0800

    added trial config data to appConfig resolver

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/config/AppConfigResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/config/AppConfigResolver.java
@@ -378,6 +378,15 @@ public class AppConfigResolver implements DataFetcher<CompletableFuture<AppConfi
     classificationConfig.setAutomations(automations);
     appConfig.setClassificationConfig(classificationConfig);
 
+    // Trial Configuration
+    final TrialConfig trialConfig = new TrialConfig();
+    trialConfig.setTrialEnabled(_datahubConfiguration.isFreeTrialInstance());
+    // TODO: Get the number of days left from the /expiration_timestamp API from Free Trial service
+    trialConfig.setDaysLeft(5);
+    // TODO: Get the sample data enabled from the new API
+    trialConfig.setSampleDataEnabled(true);
+    appConfig.setTrialConfig(trialConfig);
+
     return CompletableFuture.completedFuture(appConfig);
   }
 
