58c7494a9ea9a09e7bf219bd6e11278e1354271a
jmacryl <javier.muguruza@acryl.io>
2025-12-01 10:55:29 -0800

    fix tests

--- metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/ElasticSearchServiceFactory.java
+++ metadata-service/factories/src/main/java/com/linkedin/gms/factory/search/ElasticSearchServiceFactory.java
@@ -30,9 +30,13 @@ public class ElasticSearchServiceFactory {
   @Qualifier("baseElasticSearchComponents")
   private BaseElasticSearchComponentsFactory.BaseElasticSearchComponents components;
 
-  @Autowired
+  @Autowired(required = false)
   @Qualifier("settingsBuilder")
   private SettingsBuilder settingsBuilder;
+
+  @Autowired(required = false)
+  @Qualifier("mappingsBuilder")
+  private MappingsBuilder mappingsBuilder;
 
   @Autowired
   @Qualifier("entityRegistry")
@@ -72,10 +76,8 @@ public class ElasticSearchServiceFactory {
   }
 
   @Bean
-  protected ESWriteDAO esWriteDAO(
-      final ConfigurationProvider configurationProvider,
-      @Qualifier("mappingsBuilder") final MappingsBuilder mappingsBuilder,
-      @Qualifier("settingsBuilder") final SettingsBuilder settingsBuilder) {
+  protected ESWriteDAO esWriteDAO(final ConfigurationProvider configurationProvider) {
+    // Builders may be null in test contexts - that's fine, ESWriteDAO handles null builders
     ESWriteDAO esWriteDAO =
         new ESWriteDAO(
             components.getConfig(),
@@ -98,11 +100,9 @@ public class ElasticSearchServiceFactory {
       final ElasticSearchConfiguration elasticSearchConfiguration,
       @Nullable final CustomSearchConfiguration customSearchConfiguration,
       final ESSearchDAO esSearchDAO,
-      final ESWriteDAO esWriteDAO,
-      @Qualifier("mappingsBuilder") final MappingsBuilder mappingsBuilder,
-      @Qualifier("settingsBuilder") final SettingsBuilder settingsBuilder)
+      final ESWriteDAO esWriteDAO)
       throws IOException {
-
+    // Use the field-injected builders (may be null in test contexts)
     return new ElasticSearchService(
         components.getIndexBuilder(),
         configurationProvider.getSearchService(),
