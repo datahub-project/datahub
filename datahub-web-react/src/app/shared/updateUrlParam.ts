af590e7e1d34b401c4c57fd64cd4d8808cf1c6c4
v-tarasevich-blitz-brain <v.tarasevich@blitz-brain.com>
2025-12-03 20:16:47 +0300

    feat(ingestion): improve create/update source handling (#15446)

--- datahub-web-react/src/app/shared/updateUrlParam.ts
+++ datahub-web-react/src/app/shared/updateUrlParam.ts
@@ -1,8 +1,8 @@
 import { RouteComponentProps } from 'react-router';
 
-export function updateUrlParam(history: RouteComponentProps['history'], key: string, value: string) {
+export function updateUrlParam(history: RouteComponentProps['history'], key: string, value: string, state?: any) {
     const url = new URL(window.location.href);
     const { searchParams } = url;
     searchParams.set(key, value);
-    history.replace(url.search);
+    history.replace(url.search, state);
 }
