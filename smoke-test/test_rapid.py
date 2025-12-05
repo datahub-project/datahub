b6ac66fb188b19721251bb646803cab36ae82567
varunpatil578 <varun.patil@tailorbird.us>
2025-12-05 14:32:01 +0530

    refactor: Unify get_current_checkpoint_from_pipeline helper

--- smoke-test/test_rapid.py
+++ smoke-test/test_rapid.py
@@ -1,12 +1,6 @@
-import pytest
-import tenacity
 from typing import Any, Dict
 
-from tests.utils import (
-    execute_graphql,
-    ingest_file_via_rest,
-    with_test_retry,
-)
+from tests.utils import execute_graphql, ingest_file_via_rest, with_test_retry
 
 bootstrap_small = "test_resources/bootstrap_single.json"
 bootstrap_small_2 = "test_resources/bootstrap_single2.json"
