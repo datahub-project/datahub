67c3be6242e3db8ed0dcb13a495381a70bc5121e
John Joyce <john@Mac-16.lan>
2025-12-12 12:16:30 -0800

    Addig related documents

--- datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/knowledge/RelatedDocumentsResolver.java
+++ datahub-graphql-core/src/main/java/com/linkedin/datahub/graphql/resolvers/knowledge/RelatedDocumentsResolver.java
@@ -92,8 +92,11 @@ public class RelatedDocumentsResolver
 
             // Build filter that combines user filters with ownership constraints
             // Filter logic: (PUBLISHED) OR (UNPUBLISHED AND owned-by-user-or-groups)
+            // Note: applyShowInGlobalContext=false because related documents should show
+            // all context documents (those are meant to be discovered through their related entities)
             Filter filter =
-                DocumentSearchFilterUtils.buildCombinedFilter(baseUserCriteria, userAndGroupUrns);
+                DocumentSearchFilterUtils.buildCombinedFilter(
+                    baseUserCriteria, userAndGroupUrns, false);
 
             // Step 1: Search using service to get URNs
             // Sort by lastModifiedAt descending to show most recently updated documents first
