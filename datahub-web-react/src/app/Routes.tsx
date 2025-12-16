a667e9f53170b6b9ef9ca24fe149d96161984d20
purnimagarg1 <purnima.garg@apptware.com>
2025-12-16 19:01:40 +0530

    redesign the user sign up page

--- datahub-web-react/src/app/Routes.tsx
+++ datahub-web-react/src/app/Routes.tsx
@@ -9,6 +9,8 @@ import { LogIn } from '@app/auth/LogIn';
 import { ResetCredentials } from '@app/auth/ResetCredentials';
 import { SignUp } from '@app/auth/SignUp';
 import { isLoggedInVar } from '@app/auth/checkAuthStatus';
+import SignUpV2 from '@app/auth/signupV2/SignUpV2';
+import { useIngestionOnboardingRedesignV1 } from '@app/ingestV2/hooks/useIngestionOnboardingRedesignV1';
 import { NoPageFound } from '@app/shared/NoPageFound';
 import { PageRoutes } from '@conf/Global';
 import { resolveRuntimePath } from '@utils/runtimeBasePath';
@@ -37,11 +39,12 @@ const ProtectedRoute = ({
 export const Routes = (): JSX.Element => {
     useTrackPageView();
     const isLoggedIn = useReactiveVar(isLoggedInVar);
+    const showIngestionOnboardingRedesignV1 = useIngestionOnboardingRedesignV1();
 
     return (
         <Switch>
             <Route path={PageRoutes.LOG_IN} component={LogIn} />
-            <Route path={PageRoutes.SIGN_UP} component={SignUp} />
+            <Route path={PageRoutes.SIGN_UP} component={showIngestionOnboardingRedesignV1 ? SignUpV2 : SignUp} />
             <Route path={PageRoutes.RESET_CREDENTIALS} component={ResetCredentials} />
             <ProtectedRoute
                 isLoggedIn={isLoggedIn}
---
+++ datahub-web-react/src/app/auth/signupV2/types.ts
@@ -0,0 +1,6 @@
+export type FormValues = {
+    fullName: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+};
