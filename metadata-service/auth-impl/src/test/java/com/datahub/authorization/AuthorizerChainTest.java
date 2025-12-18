9bde2693aa2e318c44c9860217a828eb2ed0495d
Gallyam Biktashev <gbiktashev@ozon.ru>
2025-12-18 11:02:39 +0300

    Add test

--- metadata-service/auth-impl/src/test/java/com/datahub/authorization/AuthorizerChainTest.java
+++ metadata-service/auth-impl/src/test/java/com/datahub/authorization/AuthorizerChainTest.java
@@ -1,12 +1,17 @@
 package com.datahub.authorization;
 
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.testng.Assert.*;
+
 import com.datahub.plugins.auth.authorization.Authorizer;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AuthorizerChainTest {
@@ -21,16 +26,16 @@ public class AuthorizerChainTest {
         new AuthorizationRequest(actorUrn, privilege1, Optional.empty(), List.of());
     AuthorizationRequest request2 =
         new AuthorizationRequest(actorUrn, privilege2, Optional.empty(), List.of());
-    Mockito.when(authorizer1.authorize(request1))
+    when(authorizer1.authorize(request1))
         .thenReturn(
             new AuthorizationResult(request1, AuthorizationResult.Type.ALLOW, "from authorizer1"));
-    Mockito.when(authorizer1.authorize(request2))
+    when(authorizer1.authorize(request2))
         .thenReturn(
             new AuthorizationResult(request2, AuthorizationResult.Type.DENY, "from authorizer1"));
-    Mockito.when(authorizer2.authorize(request1))
+    when(authorizer2.authorize(request1))
         .thenReturn(
             new AuthorizationResult(request1, AuthorizationResult.Type.DENY, "from authorizer2"));
-    Mockito.when(authorizer2.authorize(request2))
+    when(authorizer2.authorize(request2))
         .thenReturn(
             new AuthorizationResult(request2, AuthorizationResult.Type.ALLOW, "from authorizer2"));
     AuthorizerChain authorizerChain = new AuthorizerChain(List.of(authorizer1, authorizer2), null);
@@ -39,14 +44,12 @@ public class AuthorizerChainTest {
             new BatchAuthorizationRequest(
                 actorUrn, Set.of(privilege1, privilege2), Optional.empty(), List.of()));
 
-    Assert.assertNotNull(result);
-    Assert.assertEquals(
-        result.getResults().get(privilege1).getType(), AuthorizationResult.Type.ALLOW);
-    Assert.assertEquals(
-        result.getResults().get(privilege2).getType(), AuthorizationResult.Type.ALLOW);
+    assertNotNull(result);
+    assertEquals(result.getResults().get(privilege1).getType(), AuthorizationResult.Type.ALLOW);
+    assertEquals(result.getResults().get(privilege2).getType(), AuthorizationResult.Type.ALLOW);
 
-    Mockito.verify(authorizer1, Mockito.times(2)).authorize(ArgumentMatchers.any());
-    Mockito.verify(authorizer2, Mockito.times(1)).authorize(ArgumentMatchers.any());
+    verify(authorizer1, Mockito.times(2)).authorize(ArgumentMatchers.any());
+    verify(authorizer2, Mockito.times(1)).authorize(ArgumentMatchers.any());
   }
 
   @Test
@@ -60,13 +63,13 @@ public class AuthorizerChainTest {
         new AuthorizationRequest(actorUrn, privilege1, Optional.empty(), List.of());
     AuthorizationRequest request2 =
         new AuthorizationRequest(actorUrn, privilege2, Optional.empty(), List.of());
-    Mockito.when(authorizer1.authorize(request1))
+    when(authorizer1.authorize(request1))
         .thenReturn(
             new AuthorizationResult(request1, AuthorizationResult.Type.ALLOW, "from authorizer1"));
-    Mockito.when(authorizer1.authorize(request2))
+    when(authorizer1.authorize(request2))
         .thenReturn(
             new AuthorizationResult(request2, AuthorizationResult.Type.DENY, "from authorizer1"));
-    Mockito.when(authorizer2.authorizeBatch(ArgumentMatchers.any()))
+    when(authorizer2.authorizeBatch(ArgumentMatchers.any()))
         .thenThrow(new IllegalArgumentException("failed to execute authorization logic"));
     AuthorizerChain authorizerChain = new AuthorizerChain(List.of(authorizer1, authorizer2), null);
     BatchAuthorizationResult result =
@@ -74,14 +77,28 @@ public class AuthorizerChainTest {
             new BatchAuthorizationRequest(
                 actorUrn, Set.of(privilege1, privilege2), Optional.empty(), List.of()));
 
-    Assert.assertNotNull(result);
-    Assert.assertEquals(
-        result.getResults().get(privilege1).getType(), AuthorizationResult.Type.ALLOW);
-    Assert.assertEquals(
-        result.getResults().get(privilege2).getType(), AuthorizationResult.Type.DENY);
+    assertNotNull(result);
+    assertEquals(result.getResults().get(privilege1).getType(), AuthorizationResult.Type.ALLOW);
+    assertEquals(result.getResults().get(privilege2).getType(), AuthorizationResult.Type.DENY);
 
-    Mockito.verify(authorizer1, Mockito.times(2)).authorize(ArgumentMatchers.any());
-    Mockito.verify(authorizer2, Mockito.times(0)).authorize(ArgumentMatchers.any());
-    Mockito.verify(authorizer2, Mockito.times(1)).authorizeBatch(ArgumentMatchers.any());
+    verify(authorizer1, Mockito.times(2)).authorize(ArgumentMatchers.any());
+    verify(authorizer2, Mockito.times(0)).authorize(ArgumentMatchers.any());
+    verify(authorizer2, Mockito.times(1)).authorizeBatch(ArgumentMatchers.any());
+  }
+
+  @Test
+  public void testAuthorizerDelegatesToTheAuthorizeBatch() {
+    AuthorizerChain chain = Mockito.spy(new AuthorizerChain(List.of(), null));
+    BatchAuthorizationResult batchAuthorizationResult =
+        new BatchAuthorizationResult(
+            null, Map.of("p1", new AuthorizationResult(null, AuthorizationResult.Type.ALLOW, "")));
+    doReturn(batchAuthorizationResult).when(chain).authorizeBatch(any());
+
+    AuthorizationResult result =
+        chain.authorize(new AuthorizationRequest(null, "p1", Optional.empty(), List.of()));
+
+    assertNotNull(result);
+    assertEquals(result.getType(), AuthorizationResult.Type.ALLOW);
+    verify(chain).authorizeBatch(any());
   }
 }
