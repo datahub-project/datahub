d01a94db1eaa1692b62fce7238d7673f006bef6e
purnimagarg1 <purnima.garg@apptware.com>
2025-12-18 18:35:41 +0530

    add telemetry events for the new ingestion flow

--- datahub-web-react/src/app/homeV2/layout/navBarRedesign/NavSidebar.tsx
+++ datahub-web-react/src/app/homeV2/layout/navBarRedesign/NavSidebar.tsx
@@ -308,6 +308,12 @@ export const NavSidebar = () => {
                         icon: <Plugs />,
                         selectedIcon: <Plugs weight="fill" />,
                         link: ingestionLink,
+                        onClick: () => {
+                            analytics.event({
+                                type: EventType.EnterIngestionFlowEvent,
+                                entryPoint: 'nav_menu',
+                            });
+                        },
                     },
                     {
                         type: NavBarMenuItemTypes.Item,
