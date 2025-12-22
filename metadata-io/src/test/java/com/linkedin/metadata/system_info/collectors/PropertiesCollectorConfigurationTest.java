307e02862f762162fa923f9b972c6334611d8822
Lianna Boncore Edwards <lianna.boncoreedwards@esimplicity.com>
2025-12-22 15:17:53 -0700

    add visualConfig.accessibilityMode to NON_SENSITIVE_PROPERTIES

--- metadata-io/src/test/java/com/linkedin/metadata/system_info/collectors/PropertiesCollectorConfigurationTest.java
+++ metadata-io/src/test/java/com/linkedin/metadata/system_info/collectors/PropertiesCollectorConfigurationTest.java
@@ -863,6 +863,7 @@ public class PropertiesCollectorConfigurationTest extends AbstractTestNGSpringCo
           "visualConfig.searchResult.enableNameHighlight",
           "visualConfig.showFullTitleInLineage",
           "visualConfig.theme.themeId",
+          "visualConfig.accessibilityMode",
 
           // Gradle and test-specific properties
           "org.gradle.internal.worker.tmpdir",
