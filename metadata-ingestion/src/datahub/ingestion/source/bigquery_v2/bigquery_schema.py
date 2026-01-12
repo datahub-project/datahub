017048f8183ad1c4870c4ee694ebc00740bfe62d
Alok Ranjan <alok.ranjan@datahub.com>
2026-01-12 23:31:54 +0530

    fix: added comments for project_id filtering

--- metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_schema.py
+++ metadata-ingestion/src/datahub/ingestion/source/bigquery_v2/bigquery_schema.py
@@ -299,9 +299,16 @@ class BigQuerySchemaApi:
                 )
             return result
 
+    # This method is not used anywhere
     def get_datasets_for_project_id_with_information_schema(
         self, project_id: str
     ) -> List[BigqueryDataset]:
+        """
+        This method is not used as of now, due to below limitation.
+        Current query only fetches datasets in US region.
+        We'll need region-wise separate queries to fetch all datasets.
+        https://cloud.google.com/bigquery/docs/information-schema-datasets-schemata
+        """
         schemas = self.get_query_result(
             BigqueryQuery.datasets_for_project_id.format(project_id=project_id),
         )
