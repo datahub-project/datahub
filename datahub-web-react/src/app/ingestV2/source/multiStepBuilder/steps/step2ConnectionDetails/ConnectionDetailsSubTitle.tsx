096017da584d25d27fa1efa16a121794ddbf30d4
Chris Collins <chriscollins3456@gmail.com>
2026-01-13 17:41:15 -0500

    fix(ui) Add ingestion links and executor pools dropdown to ingestion (#7885)

--- datahub-web-react/src/app/ingestV2/source/multiStepBuilder/steps/step2ConnectionDetails/ConnectionDetailsSubTitle.tsx
+++ datahub-web-react/src/app/ingestV2/source/multiStepBuilder/steps/step2ConnectionDetails/ConnectionDetailsSubTitle.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@components';
 import React from 'react';
 
 import { useIngestionSources } from '@app/ingestV2/source/builder/useIngestionSources';
@@ -11,8 +12,21 @@ export function ConnectionDetailsSubTitle() {
     const { ingestionSources } = useIngestionSources();
     const sourceConfigs = getSourceConfigs(ingestionSources, type as string);
     const sourceDisplayName = sourceConfigs?.displayName;
+    const docsUrl = sourceConfigs?.docsUrl;
 
-    const displayName = sourceDisplayName === CUSTOM_SOURCE_DISPLAY_NAME ? 'this source' : sourceDisplayName;
+    const isCustomSource = sourceDisplayName === CUSTOM_SOURCE_DISPLAY_NAME;
+    const displayName = isCustomSource ? 'this source' : sourceDisplayName;
+    const linkDisplayName = isCustomSource ? 'Metadata Ingestion' : displayName;
 
-    return <>Provide credentials and define what metadata to collect from {displayName}.</>;
+    return (
+        <>
+            Provide credentials and define what metadata to collect from {displayName}.
+            {docsUrl && (
+                <>
+                    {' '}
+                    Check out the <Link href={docsUrl}>{linkDisplayName} Guide</Link> for more information.
+                </>
+            )}
+        </>
+    );
 }
