439a099829adc129dc5edfaa68ba8aa992928b10
Devashish Chandra <devashish2203@users.noreply.github.com>
2026-01-21 20:52:09 +0530

    Log input context at start of test run

--- docker/smoke-test/release-tests.sh
+++ docker/smoke-test/release-tests.sh
@@ -9,4 +9,4 @@ PYTEST_EXTRA_ARGS="${PYTEST_EXTRA_ARGS:-}"
 # add additional paths of tests that work in remote mode. Most current smoke pytests dont work in remote mode. 
 # TODO (devashish.chandra) Add specific markers for remote enabled tests
 # shellcheck disable=SC2086
-pytest -ra --junit-prefix=release_tests --log-cli-level=INFO --continue-on-collection-errors -m "${PYTEST_MARKERS}" ${PYTEST_TEST_PATHS} ${PYTEST_EXTRA_ARGS}
+pytest -ra --junit-prefix=release_tests --continue-on-collection-errors -m "${PYTEST_MARKERS}" ${PYTEST_TEST_PATHS} ${PYTEST_EXTRA_ARGS}
