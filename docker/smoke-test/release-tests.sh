45543e777256f1018c6f6e4db3302667deaa4643
Devashish Chandra <devashish2203@users.noreply.github.com>
2026-01-22 10:50:29 +0530

    Fix review comments

--- docker/smoke-test/release-tests.sh
+++ docker/smoke-test/release-tests.sh
@@ -6,7 +6,7 @@ PYTEST_MARKERS="${PYTEST_MARKERS:-release_tests or read_only}"
 PYTEST_TEST_PATHS="${PYTEST_TEST_PATHS:-tests/read_only tests/release_tests tests/metrics/test_metrics.py}"
 PYTEST_EXTRA_ARGS="${PYTEST_EXTRA_ARGS:-}"
 
-# add additional paths of tests that work in remote mode. Most current smoke pytests dont work in remote mode. 
+# add additional paths of tests that work in remote mode. Most current smoke pytests dont work in remote mode.
 # TODO (devashish.chandra) Add specific markers for remote enabled tests
 # shellcheck disable=SC2086
 pytest -ra --junit-prefix=release_tests --continue-on-collection-errors -m "${PYTEST_MARKERS}" ${PYTEST_TEST_PATHS} ${PYTEST_EXTRA_ARGS}
