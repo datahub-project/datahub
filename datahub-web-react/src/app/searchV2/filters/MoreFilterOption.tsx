1380b86d3d0f8bc1410bb549c7f8eb39fca7cd28
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2026-01-23 13:10:28 -0800

    fix(ui/search/filters): Fix tooltip on more filters dropdown

--- datahub-web-react/src/app/searchV2/filters/MoreFilterOption.tsx
+++ datahub-web-react/src/app/searchV2/filters/MoreFilterOption.tsx
@@ -4,6 +4,8 @@ import { Typography } from 'antd';
 import React, { useRef } from 'react';
 import styled from 'styled-components';
 
+import colors from '@components/theme/foundations/colors';
+
 import { IconWrapper } from '@app/searchV2/filters/SearchFilterView';
 import { MoreFilterOptionLabel } from '@app/searchV2/filters/styledComponents';
 import { FilterPredicate } from '@app/searchV2/filters/types';
@@ -72,22 +74,23 @@ export default function MoreFilterOption({ filter, filterPredicates, activeFilte
                 data-testid={`more-filter-${displayName?.replace(/\s/g, '-')}`}
                 ref={labelRef}
             >
-                <Tooltip title={displayName} placement="left">
-                    <IconNameWrapper>
-                        {filterIcon && <IconWrapper>{filterIcon}</IconWrapper>}
-                        <Typography.Text
-                            ellipsis={{
-                                tooltip: {
-                                    title: displayName,
-                                    showArrow: false,
-                                },
-                            }}
-                        >
-                            {displayName} {numActiveFilters ? `(${numActiveFilters}) ` : ''}
-                        </Typography.Text>
-                    </IconNameWrapper>
-                    <StyledRightOutlined />
-                </Tooltip>
+                <IconNameWrapper>
+                    {filterIcon && <IconWrapper>{filterIcon}</IconWrapper>}
+                    <Typography.Text
+                        ellipsis={{
+                            tooltip: {
+                                title: displayName,
+                                showArrow: false,
+                                color: 'white',
+                                overlayInnerStyle: { color: colors.gray[1700] },
+                                placement: 'left',
+                            },
+                        }}
+                    >
+                        {displayName} {numActiveFilters ? `(${numActiveFilters}) ` : ''}
+                    </Typography.Text>
+                </IconNameWrapper>
+                <StyledRightOutlined />
             </MoreFilterOptionLabel>
         </StyledValueSelector>
     );
