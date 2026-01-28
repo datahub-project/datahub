4b156f2e2e84cd0a2c367b6697e47a9f2df71c18
Anirudh Reddy Malgari <anirudh.malgari@datahub.com>
2026-01-28 10:47:58 -0800

    CAT-1292: allow configuration of reset password expiration

--- metadata-io/src/test/java/com/linkedin/metadata/system_info/collectors/PropertiesCollectorConfigurationTest.java
+++ metadata-io/src/test/java/com/linkedin/metadata/system_info/collectors/PropertiesCollectorConfigurationTest.java
@@ -176,6 +176,7 @@ public class PropertiesCollectorConfigurationTest extends AbstractTestNGSpringCo
           "authentication.enforceExistenceEnabled",
           "authentication.excludedPaths",
           "authentication.logAuthenticatorExceptions",
+          "authentication.passwordResetTokenExpirationMs",
           "authentication.sessionTokenDurationMs",
           "authentication.tokenService.issuer",
           "authentication.tokenService.signingAlgorithm",
