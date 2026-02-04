0449b4f4198ad7c54e40c9550a201110c65b5fef
Brock Griffey <52086127+brock-acryl@users.noreply.github.com>
2026-02-04 10:28:52 -0500

    - Implemented unit tests to validate the functionality and accuracy of the new mappers.

---
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/types/application/mappers/ApplicationMapperTest.java
@@ -0,0 +1,53 @@
+package com.linkedin.datahub.graphql.types.application.mappers;
+
+import com.linkedin.common.UrnArray;
+import com.linkedin.common.urn.Urn;
+import com.linkedin.datahub.graphql.generated.Application;
+import com.linkedin.domain.Domains;
+import com.linkedin.entity.Aspect;
+import com.linkedin.entity.EntityResponse;
+import com.linkedin.entity.EnvelopedAspect;
+import com.linkedin.entity.EnvelopedAspectMap;
+import com.linkedin.metadata.Constants;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ApplicationMapperTest {
+  private static final Urn TEST_APPLICATION_URN =
+      Urn.createFromTuple(Constants.APPLICATION_ENTITY_NAME, "application1");
+
+  @Test
+  public void testMapDomainsAssociations() throws URISyntaxException {
+    final Urn domainUrn1 = Urn.createFromString("urn:li:domain:engineering");
+    final Urn domainUrn2 = Urn.createFromString("urn:li:domain:marketing");
+
+    Domains domains = new Domains();
+    UrnArray domainUrns = new UrnArray();
+    domainUrns.add(domainUrn1);
+    domainUrns.add(domainUrn2);
+    domains.setDomains(domainUrns);
+
+    final Map<String, EnvelopedAspect> aspects = new HashMap<>();
+    aspects.put(
+        Constants.DOMAINS_ASPECT_NAME, new EnvelopedAspect().setValue(new Aspect(domains.data())));
+    final EntityResponse response =
+        new EntityResponse()
+            .setEntityName(Constants.APPLICATION_ENTITY_NAME)
+            .setUrn(TEST_APPLICATION_URN)
+            .setAspects(new EnvelopedAspectMap(aspects));
+
+    final Application actual = ApplicationMapper.map(null, response);
+
+    Assert.assertNotNull(actual.getDomainsAssociations());
+    Assert.assertEquals(actual.getDomainsAssociations().getDomains().size(), 2);
+    Assert.assertEquals(
+        actual.getDomainsAssociations().getDomains().get(0).getDomain().getUrn(),
+        domainUrn1.toString());
+    Assert.assertEquals(
+        actual.getDomainsAssociations().getDomains().get(1).getDomain().getUrn(),
+        domainUrn2.toString());
+  }
+}
---
+++ datahub-graphql-core/src/test/java/com/linkedin/datahub/graphql/types/chart/mappers/ChartMapperTest.java
@@ -0,0 +1,53 @@
+package com.linkedin.datahub.graphql.types.chart.mappers;
+
+import com.linkedin.common.UrnArray;
+import com.linkedin.common.urn.Urn;
+import com.linkedin.datahub.graphql.generated.Chart;
+import com.linkedin.domain.Domains;
+import com.linkedin.entity.Aspect;
+import com.linkedin.entity.EntityResponse;
+import com.linkedin.entity.EnvelopedAspect;
+import com.linkedin.entity.EnvelopedAspectMap;
+import com.linkedin.metadata.Constants;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ChartMapperTest {
+  private static final Urn TEST_CHART_URN =
+      Urn.createFromTuple(Constants.CHART_ENTITY_NAME, "chart1");
+
+  @Test
+  public void testMapDomainsAssociations() throws URISyntaxException {
+    final Urn domainUrn1 = Urn.createFromString("urn:li:domain:engineering");
+    final Urn domainUrn2 = Urn.createFromString("urn:li:domain:marketing");
+
+    Domains domains = new Domains();
+    UrnArray domainUrns = new UrnArray();
+    domainUrns.add(domainUrn1);
+    domainUrns.add(domainUrn2);
+    domains.setDomains(domainUrns);
+
+    final Map<String, EnvelopedAspect> aspects = new HashMap<>();
+    aspects.put(
+        Constants.DOMAINS_ASPECT_NAME, new EnvelopedAspect().setValue(new Aspect(domains.data())));
+    final EntityResponse response =
+        new EntityResponse()
+            .setEntityName(Constants.CHART_ENTITY_NAME)
+            .setUrn(TEST_CHART_URN)
+            .setAspects(new EnvelopedAspectMap(aspects));
+
+    final Chart actual = ChartMapper.map(null, response);
+
+    Assert.assertNotNull(actual.getDomainsAssociations());
+    Assert.assertEquals(actual.getDomainsAssociations().getDomains().size(), 2);
+    Assert.assertEquals(
+        actual.getDomainsAssociations().getDomains().get(0).getDomain().getUrn(),
+        domainUrn1.toString());
+    Assert.assertEquals(
+        actual.getDomainsAssociations().getDomains().get(1).getDomain().getUrn(),
+        domainUrn2.toString());
+  }
+}
