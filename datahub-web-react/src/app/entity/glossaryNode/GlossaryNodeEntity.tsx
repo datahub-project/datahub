60cc482613d7a4d16eb98e928981abc8277a3b92
Chris Collins <chriscollins3456@gmail.com>
2026-02-11 17:06:24 -0500

    refactor(ui/forms): Request forms for all entity profiles in a separate query (#16180)
    Co-authored-by: Andrew Sikowitz <andrew.sikowitz@acryl.io>

--- datahub-web-react/src/app/entity/glossaryNode/GlossaryNodeEntity.tsx
+++ datahub-web-react/src/app/entity/glossaryNode/GlossaryNodeEntity.tsx
@@ -171,6 +171,7 @@ class GlossaryNodeEntity implements Entity<GlossaryNode> {
             EntityCapabilityType.OWNERS,
             EntityCapabilityType.DEPRECATION,
             EntityCapabilityType.SOFT_DELETE,
+            EntityCapabilityType.FORMS,
         ]);
     };
 
