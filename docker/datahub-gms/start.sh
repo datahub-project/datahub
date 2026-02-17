5000393b28921dc91e3aef122caf17f32f64bb55
Dutt23 <29917752+Dutt23@users.noreply.github.com>
2026-02-17 22:45:33 +0530

    Update start.sh

--- docker/datahub-gms/start.sh
+++ docker/datahub-gms/start.sh
@@ -67,7 +67,7 @@ fi
 # JAR extraction optimization - extract to tmpfs for faster class loading
 JAR_EXTRACTION_OPTS=""
 if [[ $EXTRACT_JAR_ENABLED == true ]]; then
-  WORK_DIR="/tmp/gms/extractio"
+  WORK_DIR="/tmp/gms/extraction"
   JAR_PATH="/datahub/datahub-gms/bin/war.war"
 
   # Log WAR size and available resources
