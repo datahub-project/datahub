b71d29b84be0be8627d05db3504cf163f4535962
Diogo Vala <diogo.vala@swisscom.com>
2026-02-17 11:19:26 +0100

    Run spotlessApply

--- datahub-frontend/app/auth/sso/oidc/RequiredGroupsException.java
+++ datahub-frontend/app/auth/sso/oidc/RequiredGroupsException.java
@@ -1,10 +1,8 @@
 package auth.sso.oidc;
 
-/**
- * Exception thrown when a user does not belong to any of the required groups for OIDC login.
- */
+/** Exception thrown when a user does not belong to any of the required groups for OIDC login. */
 public class RequiredGroupsException extends RuntimeException {
-	public RequiredGroupsException(String message) {
-		super(message);
-	}
+  public RequiredGroupsException(String message) {
+    super(message);
+  }
 }
