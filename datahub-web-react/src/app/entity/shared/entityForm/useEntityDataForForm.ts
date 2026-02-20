8a9bdea0498a4f1403d48f261c4ced534069f06a
Chris Collins <chriscollins3456@gmail.com>
2026-02-20 12:30:37 -0500

    fix(ui) Fix fetching forms data from task center (#8309)

--- datahub-web-react/src/app/entity/shared/entityForm/useEntityDataForForm.ts
+++ datahub-web-react/src/app/entity/shared/entityForm/useEntityDataForForm.ts
@@ -3,6 +3,8 @@ import { useEntityRegistryV2 } from '@src/app/useEntityRegistry';
 import { useGetEntityQuery } from '@src/graphql/entity.generated';
 import { Entity } from '@src/types.generated';
 
+import { useGetFormsForEntityQuery } from '@graphql/form.generated';
+
 interface Props {
     selectedEntity?: Entity;
 }
@@ -23,6 +25,12 @@ export default function useEntityDataForForm({ selectedEntity }: Props) {
         fetchPolicy: 'no-cache',
     });
 
+    const { data: formsData, loading: formsLoading } = useGetFormsForEntityQuery({
+        variables: { urn: selectedEntity?.urn || '' },
+        fetchPolicy: 'no-cache',
+        skip: !selectedEntity,
+    });
+
     const isOnEntityProfilePage = !!selectedEntity && selectedEntity.urn === entityData?.urn;
     const selectedEntityGraphName = selectedEntity ? entityRegistry.getGraphNameFromType(selectedEntity.type) : '';
     const fetchedEntityData = fetchedData ? fetchedData[selectedEntityGraphName] : null;
@@ -32,5 +40,10 @@ export default function useEntityDataForForm({ selectedEntity }: Props) {
             : null;
     const selectedEntityData = isOnEntityProfilePage ? entityData : fetchedGenericEntityData;
 
-    return { selectedEntityData, isOnEntityProfilePage, entityRefetch, entityLoading };
+    return {
+        selectedEntityData: !fetchedData || !formsData ? undefined : { ...formsData?.entity, ...selectedEntityData },
+        isOnEntityProfilePage,
+        entityRefetch,
+        entityLoading: entityLoading || formsLoading,
+    };
 }
