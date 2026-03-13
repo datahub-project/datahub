1c6bd648ae8db4f92886d7324f806466e093a06f
Ben Blazke <ben.blazke@datahub.com>
2026-03-13 11:08:20 -0700

    ci

--- datahub-web-react/src/app/homeV3/module/components/__tests__/ModuleMenu.test.tsx
+++ datahub-web-react/src/app/homeV3/module/components/__tests__/ModuleMenu.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable rulesdir/no-hardcoded-colors */
 import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import { describe, expect, it, vi } from 'vitest';
--- datahub-web-react/src/app/ingestV2/executions/components/reporting/__tests__/StructuredReport.test.tsx
+++ datahub-web-react/src/app/ingestV2/executions/components/reporting/__tests__/StructuredReport.test.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable rulesdir/no-hardcoded-colors */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
