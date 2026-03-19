364dd1f45e8d25bd5da20b2251a0a5944bf2c11a
purnimagarg1 <purnima.garg@apptware.com>
2026-03-20 00:32:25 +0530

    remove eslint disable from utils file

--- datahub-web-react/src/alchemy-components/components/CalendarChart/utils.ts
+++ datahub-web-react/src/alchemy-components/components/CalendarChart/utils.ts
@@ -1,5 +1,3 @@
-// Disabling hardcoded colors rule in stories files
-/* eslint-disable rulesdir/no-hardcoded-colors */
 import { scaleLinear } from '@visx/scale';
 import * as d3interpolate from '@visx/vendor/d3-interpolate';
 import dayjs from 'dayjs';
--- datahub-web-react/src/alchemy-components/components/Carousel/Carousel.stories.tsx
+++ datahub-web-react/src/alchemy-components/components/Carousel/Carousel.stories.tsx
@@ -1,3 +1,4 @@
+// Disabling hardcoded colors rule in stories files
 /* eslint-disable rulesdir/no-hardcoded-colors */
 import { Carousel } from '@components';
 import type { Meta, StoryObj } from '@storybook/react';
