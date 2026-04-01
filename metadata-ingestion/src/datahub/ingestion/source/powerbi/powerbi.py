0b9925eca2966873ab251b0fff9b9e16ad146e6b
avi <252974426+aviraj-gour@users.noreply.github.com>
2026-04-01 19:54:33 +0530

    fix(powerbi): conditionally convert column names to lowercase in lineage mapping

--- metadata-ingestion/src/datahub/ingestion/source/powerbi/powerbi.py
+++ metadata-ingestion/src/datahub/ingestion/source/powerbi/powerbi.py
@@ -198,9 +198,12 @@ class Mapper:
             upstreams = [
                 builder.make_schema_field_urn(
                     self.lineage_urn_to_lowercase(column_ref.table),
-                    column_ref.column,
+                    column_ref.column.lower()
+                    if self.__config.convert_lineage_urns_to_lowercase
+                    else column_ref.column,
                 )
                 for column_ref in cll_info.upstreams
+                if column_ref.column
             ]
 
             fine_grained_lineages.append(
