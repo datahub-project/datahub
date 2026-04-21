5f3b53754fe64c3ee56862763821bb8f8103b548
dutt23 <29917752+Dutt23@users.noreply.github.com>
2026-04-21 19:19:34 +0530

    fix spark startup

--- metadata-integration/java/acryl-spark-lineage/spark-smoke-test/setup_spark_smoke_test.sh
+++ metadata-integration/java/acryl-spark-lineage/spark-smoke-test/setup_spark_smoke_test.sh
@@ -37,7 +37,10 @@ echo "Executing spark-submit jobs"
 echo "--------------------------------------------------------------------"
 
 #Execute spark-submit jobs
-docker run --network datahub_network spark-submit
+docker run \
+  -e SPARK_DRIVER_EXTRA_JAVA_OPTIONS="--add-opens java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.net=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED" \
+  --network datahub_network \
+  spark-submit
 
 echo "--------------------------------------------------------------------"
 echo "Starting pytest"
