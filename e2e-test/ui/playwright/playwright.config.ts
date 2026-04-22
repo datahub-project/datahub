4873fcdd5ea868572db17c9dad52a9cbf842de58
Priyabrata Das <260647720+priyabratadas-dh@users.noreply.github.com>
2026-04-23 02:12:08 +0530

    Fix

--- e2e-test/ui/playwright/playwright.config.ts
+++ e2e-test/ui/playwright/playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: process.env.CI ? 2 : 4,
+  workers: process.env.CI ? 1 : 4,
   reporter: process.env.CI
     ? [
         ['blob'],
