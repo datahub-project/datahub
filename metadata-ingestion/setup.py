15f5d038da7a4c42693fb843bc3f7769eaf5a183
Kyungsoo Lee <kyungsoo.lee@datahub.com>
2026-05-06 09:16:18 -0700

    fix: revert back to removed entry in setup.py

--- metadata-ingestion/setup.py
+++ metadata-ingestion/setup.py
@@ -580,7 +580,9 @@ plugins: Dict[str, Set[str]] = {
         "azure-identity>=1.21.0,<2.0",
         # upper bound added to pass check-python-deps.yml github workflow
         "requests>=2.28.0,<3.0",
-    },
+    }
+    | sqlglot_lib
+    | usage_common,
     "bigquery": sql_common
     | bigquery_common
     | sqlglot_lib
