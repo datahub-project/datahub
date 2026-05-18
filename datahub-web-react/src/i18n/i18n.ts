7ed828f81c1c6f0f75bd8a28e8c512295c688f20
Victor Tarasevich <v.tarasevich@blitz-brain.com>
2026-05-18 19:34:58 +0300

    feat(i18n): add language select + translate preferences page

--- datahub-web-react/src/i18n/i18n.ts
+++ datahub-web-react/src/i18n/i18n.ts
@@ -2,15 +2,19 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import deCommonActions from '@src/i18n/locales/de/common.actions.json';
+import deSettingsPreferences from '@src/i18n/locales/de/settings.preferences.json';
 import enCommonActions from '@src/i18n/locales/en/common.actions.json';
+import enSettingsPreferences from '@src/i18n/locales/en/settings.preferences.json';
 
 i18n.use(initReactI18next).init({
     resources: {
         en: {
             'common.actions': enCommonActions,
+            'settings.preferences': enSettingsPreferences,
         },
         de: {
             'common.actions': deCommonActions,
+            'settings.preferences': deSettingsPreferences,
         },
     },
     fallbackLng: 'en',
