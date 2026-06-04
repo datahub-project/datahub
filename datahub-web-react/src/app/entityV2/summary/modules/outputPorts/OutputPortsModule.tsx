0ebef663c13a8130448dc854cb474a212fa8832b
mihai103 <mihai103@noreply.com>
2026-06-04 17:32:13 +0200

    remove unused emptystate section

--- datahub-web-react/src/app/entityV2/summary/modules/outputPorts/OutputPortsModule.tsx
+++ datahub-web-react/src/app/entityV2/summary/modules/outputPorts/OutputPortsModule.tsx
@@ -1,9 +1,7 @@
 import { InfiniteScrollList } from '@components';
-import { PlugsConnected } from '@phosphor-icons/react/dist/csr/PlugsConnected';
 import React from 'react';
 
 import { useGetOutputPorts } from '@app/entityV2/summary/modules/outputPorts/useGetOutputPorts';
-import EmptyContent from '@app/homeV3/module/components/EmptyContent';
 import EntityItem from '@app/homeV3/module/components/EntityItem';
 import LargeModule from '@app/homeV3/module/components/LargeModule';
 import { ModuleProps } from '@app/homeV3/module/types';
@@ -27,13 +25,6 @@ export default function OutputPortsModule(props: ModuleProps) {
                     <EntityItem entity={entity} key={entity.urn} moduleType={DataHubPageModuleType.OutputPorts} />
                 )}
                 pageSize={DEFAULT_PAGE_SIZE}
-                emptyState={
-                    <EmptyContent
-                        icon={PlugsConnected}
-                        title="No Output Ports"
-                        description="Add output ports to this data product to see them in this list"
-                    />
-                }
                 totalItemCount={total}
             />
         </LargeModule>
