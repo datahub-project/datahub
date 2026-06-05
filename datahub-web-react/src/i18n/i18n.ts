23d5b37375571a4239c7ea7ff21e8c57398c8eff
v-tarasevich-blitz-brain <v.tarasevich@blitz-brain.com>
2026-06-05 19:10:06 +0300

    feat(i18n): extract keys from entity v1 shared components (#17739)

--- datahub-web-react/src/i18n/i18n.ts
+++ datahub-web-react/src/i18n/i18n.ts
@@ -30,6 +30,7 @@ export const NAMESPACES = [
     'entity.shared.entityDropdown',
     'entity.types',
     'entity.views',
+    'entityV1.shared.components',
     'entityV1.shared.entityDropdown',
     'governance.domain',
     'governance.glossary',
