1949341551cb7d506897065577e0c4c209498a70
Andrew Sikowitz <andrew.sikowitz@acryl.io>
2026-07-06 11:49:05 -0700

    fix(ece/structured-property): Refactor change event generation to always set parameters

--- metadata-io/src/test/java/com/linkedin/metadata/timeline/eventgenerator/StructuredPropertiesChangeEventGeneratorTest.java
+++ metadata-io/src/test/java/com/linkedin/metadata/timeline/eventgenerator/StructuredPropertiesChangeEventGeneratorTest.java
@@ -11,11 +11,32 @@ import com.linkedin.structured.PrimitivePropertyValueArray;
 import com.linkedin.structured.StructuredProperties;
 import com.linkedin.structured.StructuredPropertyValueAssignment;
 import com.linkedin.structured.StructuredPropertyValueAssignmentArray;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
 
 public class StructuredPropertiesChangeEventGeneratorTest extends AbstractTestNGSpringContextTests {
+
+  private static StructuredPropertyValueAssignment buildAssignment(
+      String propertyUrn, String... values) throws Exception {
+    StructuredPropertyValueAssignment assignment = new StructuredPropertyValueAssignment();
+    assignment.setPropertyUrn(new Urn(propertyUrn));
+    PrimitivePropertyValueArray valueArray = new PrimitivePropertyValueArray();
+    for (String value : values) {
+      valueArray.add(PrimitivePropertyValue.create(value));
+    }
+    assignment.setValues(valueArray);
+    return assignment;
+  }
+
+  private static StructuredProperties buildStructuredProperties(
+      StructuredPropertyValueAssignment... assignments) {
+    StructuredProperties properties = new StructuredProperties();
+    properties.setProperties(
+        new StructuredPropertyValueAssignmentArray(Arrays.asList(assignments)));
+    return properties;
+  }
 
   @Test
   public void testNoChange() throws Exception {
@@ -309,5 +330,151 @@ public class StructuredPropertiesChangeEventGeneratorTest extends AbstractTestNG
     String ea = (String) actual.get(0).getParameters().get("propertyValues");
     assertEquals("REMOVE", actual.get(0).getOperation().toString());
     assertEquals(expectedValue, ea);
+  }
+
+  @Test
+  public void testDeletePropertySortingBeforeRemainingProperty() throws Exception {
+    StructuredPropertyChangeEventGenerator test = new StructuredPropertyChangeEventGenerator();
+
+    Urn urn =
+        Urn.createFromString("urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleHdfsDataset,PROD)");
+    String entity = "dataset";
+    String aspect = "structedProperties";
+    AuditStamp auditStamp =
+        new AuditStamp()
+            .setActor(Urn.createFromString("urn:li:corpuser:__datahub_system"))
+            .setTime(1683829509553L);
+
+    // Removed property sorts lexicographically BEFORE the property that remains.
+    StructuredPropertyValueAssignment removedProp = new StructuredPropertyValueAssignment();
+    removedProp.setPropertyUrn(new Urn("urn:li:structuredProperty:io.acryl.aRemovedProperty"));
+    PrimitivePropertyValueArray removedValues = new PrimitivePropertyValueArray();
+    removedValues.add(PrimitivePropertyValue.create("42"));
+    removedProp.setValues(removedValues);
+
+    StructuredPropertyValueAssignment remainingProp = new StructuredPropertyValueAssignment();
+    remainingProp.setPropertyUrn(
+        new Urn("urn:li:structuredProperty:io.acryl.privacy.retentionTime"));
+    PrimitivePropertyValueArray remainingValues = new PrimitivePropertyValueArray();
+    remainingValues.add(PrimitivePropertyValue.create("90"));
+    remainingProp.setValues(remainingValues);
+
+    StructuredProperties fromProperties = new StructuredProperties();
+    StructuredPropertyValueAssignmentArray fromAssignmentArray =
+        new StructuredPropertyValueAssignmentArray();
+    fromAssignmentArray.add(removedProp);
+    fromAssignmentArray.add(remainingProp);
+    fromProperties.setProperties(fromAssignmentArray);
+
+    StructuredProperties structuredPropertiesTo = new StructuredProperties();
+    StructuredPropertyValueAssignmentArray toAssignmentArray =
+        new StructuredPropertyValueAssignmentArray();
+    toAssignmentArray.add(remainingProp);
+    structuredPropertiesTo.setProperties(toAssignmentArray);
+
+    Aspect<StructuredProperties> from = new Aspect<>(fromProperties, new SystemMetadata());
+    Aspect<StructuredProperties> to = new Aspect<>(structuredPropertiesTo, new SystemMetadata());
+
+    List<ChangeEvent> actual = test.getChangeEvents(urn, entity, aspect, from, to, auditStamp);
+
+    assertEquals(1, actual.size());
+    assertEquals("REMOVE", actual.get(0).getOperation().toString());
+    assertNotNull(actual.get(0).getParameters());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.aRemovedProperty",
+        actual.get(0).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"42\"]", actual.get(0).getParameters().get("propertyValues"));
+  }
+
+  @Test
+  public void testAllPropertiesRemoved() throws Exception {
+    StructuredPropertyChangeEventGenerator test = new StructuredPropertyChangeEventGenerator();
+
+    Urn urn =
+        Urn.createFromString("urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleHdfsDataset,PROD)");
+    String entity = "dataset";
+    String aspect = "structedProperties";
+    AuditStamp auditStamp =
+        new AuditStamp()
+            .setActor(Urn.createFromString("urn:li:corpuser:__datahub_system"))
+            .setTime(1683829509553L);
+
+    StructuredProperties fromProperties =
+        buildStructuredProperties(
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyA", "1"),
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyB", "2"));
+
+    // Aspect deletion: the new value is null.
+    Aspect<StructuredProperties> from = new Aspect<>(fromProperties, new SystemMetadata());
+    Aspect<StructuredProperties> to = new Aspect<>(null, new SystemMetadata());
+
+    List<ChangeEvent> actual = test.getChangeEvents(urn, entity, aspect, from, to, auditStamp);
+
+    assertEquals(2, actual.size());
+
+    assertEquals("REMOVE", actual.get(0).getOperation().toString());
+    assertNotNull(actual.get(0).getParameters());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.propertyA",
+        actual.get(0).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"1\"]", actual.get(0).getParameters().get("propertyValues"));
+
+    assertEquals("REMOVE", actual.get(1).getOperation().toString());
+    assertNotNull(actual.get(1).getParameters());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.propertyB",
+        actual.get(1).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"2\"]", actual.get(1).getParameters().get("propertyValues"));
+  }
+
+  @Test
+  public void testAddRemoveAndModifyInSingleChange() throws Exception {
+    StructuredPropertyChangeEventGenerator test = new StructuredPropertyChangeEventGenerator();
+
+    Urn urn =
+        Urn.createFromString("urn:li:dataset:(urn:li:dataPlatform:hdfs,SampleHdfsDataset,PROD)");
+    String entity = "dataset";
+    String aspect = "structedProperties";
+    AuditStamp auditStamp =
+        new AuditStamp()
+            .setActor(Urn.createFromString("urn:li:corpuser:__datahub_system"))
+            .setTime(1683829509553L);
+
+    StructuredProperties fromProperties =
+        buildStructuredProperties(
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyA", "1"),
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyC", "3"));
+
+    StructuredProperties toProperties =
+        buildStructuredProperties(
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyB", "2"),
+            buildAssignment("urn:li:structuredProperty:io.acryl.propertyC", "3", "4"));
+
+    Aspect<StructuredProperties> from = new Aspect<>(fromProperties, new SystemMetadata());
+    Aspect<StructuredProperties> to = new Aspect<>(toProperties, new SystemMetadata());
+
+    List<ChangeEvent> actual = test.getChangeEvents(urn, entity, aspect, from, to, auditStamp);
+
+    // Events are ordered by property urn: A removed, B added, C modified.
+    assertEquals(3, actual.size());
+
+    assertEquals("REMOVE", actual.get(0).getOperation().toString());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.propertyA",
+        actual.get(0).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"1\"]", actual.get(0).getParameters().get("propertyValues"));
+
+    assertEquals("ADD", actual.get(1).getOperation().toString());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.propertyB",
+        actual.get(1).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"2\"]", actual.get(1).getParameters().get("propertyValues"));
+
+    // MODIFY carries the new (multi-)values.
+    assertEquals("MODIFY", actual.get(2).getOperation().toString());
+    assertEquals(
+        "urn:li:structuredProperty:io.acryl.propertyC",
+        actual.get(2).getParameters().get("propertyUrn").toString());
+    assertEquals("[\"3\",\"4\"]", actual.get(2).getParameters().get("propertyValues"));
   }
 }
