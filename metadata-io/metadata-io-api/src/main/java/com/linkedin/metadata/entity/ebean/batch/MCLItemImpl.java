47625ac32bc7df97f255a0eafb7f4258b79b794b
dutt23 <29917752+Dutt23@users.noreply.github.com>
2026-07-15 14:26:51 +0530

    revert changes for testing

--- metadata-io/metadata-io-api/src/main/java/com/linkedin/metadata/entity/ebean/batch/MCLItemImpl.java
+++ metadata-io/metadata-io-api/src/main/java/com/linkedin/metadata/entity/ebean/batch/MCLItemImpl.java
@@ -92,12 +92,12 @@ public class MCLItemImpl implements MCLItem {
       log.debug("entity type = {}", urn.getEntityType());
 
       entitySpec(entityRegistry.getEntitySpec(urn.getEntityType()));
-      log.debug("entity spc = {}", this.entitySpec);
+      log.debug("entity spec = {}", this.entitySpec);
 
       aspectSpec(
           ValidationApiUtils.validateAspect(
               this.entitySpec, this.metadataChangeLog.getAspectName()));
-      log.debug("aspect sp = {}", this.aspectSpec);
+      log.debug("aspect spec = {}", this.aspectSpec);
 
       Pair<RecordTemplate, RecordTemplate> aspects =
           convertToRecordTemplate(this.metadataChangeLog, aspectSpec);
